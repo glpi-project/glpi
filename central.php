8b8306f3d67c14b59b8bfdd7c131fe33f4f7892b
lbaaz <lbaaz>
2004-02-14 22:27:55 +0000

    modif de central.php suppression des variables inutiles vides dans l'appel des fonctions d'affichage

--- central.php
+++ central.php
@@ -53,12 +53,12 @@ echo "<hr noshade>";
 $db= new DB;
 
 // Show last events
-showEvents($_SERVER["PHP_SELF"],$result,$sort);
+showEvents($_SERVER["PHP_SELF"],$_GET["order"],$_GET["sort"]);
 
 
 if ($cfg_features["jobs_at_login"]==1)
 {
-	showJobList($_SESSION["glpiname"],"individual",$contains,$item);
+	showJobList($_SESSION["glpiname"],"individual","","");
 }
 
 commonFooter();
