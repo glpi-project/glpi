8dbc2dfb965ab41813802902881321b276d08d80
silvermat <silvermat>
2004-02-19 22:49:59 +0000

    modif entetes

--- glpi/common/functions.php
+++ glpi/common/functions.php
@@ -399,7 +399,9 @@ function nullHeader($title,$url) {
 	}
 
 	// Start the page
-	echo "<html><head><title>glpi: ".$title."</title>";
+       	echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN \">";
+        echo "<html><head><title>glpi: ".$title."</title>";
+        echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1 \" />";
 
 	// Send extra expires header if configured
 	if ($cft_features["sendexpire"]) {
