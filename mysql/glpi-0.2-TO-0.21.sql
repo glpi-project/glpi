43c319c8d9447d9e4011aea417bfaf59baf93761
baaz <baaz@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2004-06-03 09:49:57 +0000

    Menage dans les dumps mysql
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@125 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- mysql/glpi-0.2-TO-0.21.sql
+++
@@ -1,5 +0,0 @@
-#Script a executer pour passer de la base de donnée de la version 0.2
-# a celle de la version 0.21 sans perdre de données.
- Alter table users drop can_assign_job;
- Alter table users add can_assign_job enum('yes','no') NOT NULL default 'no';
- Update users set can_assign_job = 'yes' where type = 'admin';
