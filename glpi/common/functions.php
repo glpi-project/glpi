8a5c345a25d25cc969230c0ca8411e344a154117
silvermat <silvermat@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2004-06-04 22:30:24 +0000

    correction bugs #408 rechercher votre num�ro de machine
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@139 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- glpi/common/functions.php
+++ glpi/common/functions.php
@@ -860,7 +860,7 @@ function printHelpDesk ($name) {
 	echo "</td></tr>";
 
 	echo "<tr class='tab_bg_1'>";
-	echo "<td>".$lang["help"][12]." <a href=\"\" onClick=\"window.open('".$cfg_install["root"]."/find_num.php','Help','scrollbars=1,resizable=1,width=400,height=400')\"><img src=\"".$cfg_install["root"]."/pics/aide.png\" border='0' alt=\"help\"></a></td>";
+	echo "<td>".$lang["help"][12]." <a href=\"#\" onClick=\"window.open('".$cfg_install["root"]."/find_num.php','Help','scrollbars=1,resizable=1,width=400,height=400')\"><img src=\"".$cfg_install["root"]."/pics/aide.png\" border='0' alt=\"help\"></a></td>";
 	echo "<td><input name='computer' size='10'>";
 	echo "</td>";
 	echo "</tr>";
