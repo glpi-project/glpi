50f88fcdd30bde61789b09e71922c01d73a05078
baaz <baaz@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2004-07-12 19:59:36 +0000

    Nouveau fichier config
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@187 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- glpi/config/config.php
+++ glpi/config/config.php
@@ -39,165 +39,16 @@ This file is part of GLPI.
 
  
 
-//Toutes les options notifiées "non supportées par glpi v0.2 sont liées
-// au fonctions LDAP et mail (Qmail) présentes dans IRMA
-// GLPI  CONFIGURATION OPTIONS
-
-
-// *************************** Eléments à renseigner impérativement ********
-// *************************************************************************
-// *************************************************************************
-
 // Basic MYSQL configuration change as you need/want.
 // Configuration standard changez selon vos besoins.
-class DB extends DBmysql {
+require_once ('config_db.php');
 
-	var $dbhost	= "localhost";
-	var $dbuser 	= "glpiuser";
-	var $dbpassword	= "";
-	var $dbdefault	= "glpidb";
-}
+//Statics config options
+// options d'installation statiques
+// dicts
+// dictionnaires
+$cfg_install["languages"] = array("english","deutsch","french","italian");
 
-
-//root document
-//document root
-$cfg_install["root"]		= "/glpi";
-
-
-// *************************** Eléments optionnels  **********************
-// ***********************************************************************
-// ***********************************************************************
-
-// Navigation Functions
-// Fonctions du menu
-class baseFunctions {
-	// Could have inventory, maintain, admin and settings, 
-	//changes these values on the dicts on header menu.
-
-	
-	var $inventory	= true;
-
-	var $maintain	= true;
-
-	var $settings	= true;
-
-				
-}
-
-// Gestion de source d'information alternatives pour le login
-// telles que des serveurs de mail en imap pop...
-// ports standards : pop 110 , imap 993
-// Dans tous les cas le dernier type de login testé est la base de données
-// Dans le cas où le login est incorrect dans la base mais est correct
-// sur la source alternative, l'utilisateur est ajouté ou son mot de passe
-// est modifié
-// Si plusieurs sources alternatives sont définies, seule la première
-// fournissant un login correct est utilisé
-
-$cfg_login['imap']['auth_server'] = "";
-$cfg_login['imap']['host'] = "";
-
-// LDAP setup.
-// We can use LDAP both for authentication and for user information
-$cfg_login['ldap']['host'] = "";
-$cfg_login['ldap']['basedn'] = "";
-$cfg_login['ldap']['rootdn'] ="";
-$cfg_login['ldap']['pass'] ="";
-// some lDAP server (eg, M$ Active Directory) does not like anonymous
-// bind
-//$cfg_login['ldap']['rootdn'] = "cn=admin,ou=People,dc=sv,dc=lnf,dc=it";
-//$cfg_login['ldap']['pass'] = "secret";
-// relation between the GLPI users table field and the LDAP field
-$cfg_login['ldap']['fields'] = "";
-// $cfg_login['ldap']['fields'] = array( "name" => "uid", "email" => "mail", "location" => "physicaldeliveryofficename", "phone" => "telephonenumber", "realname" => "cn");
-
-
-//other sources
-//$cfg_login['other_source']...
-
-// Features configuration
-
-// Log level :
-// Niveau de log :
-
-// 1 - Critical (login failures) |  (erreur de loging seulement)
-// 2 - Severe - not used  | (non utilisé)
-// 3 - Important - (sucessfull logins)  |  importants (loging réussis)
-// 4 - Notice (updates, adds, deletes, tracking) | classique
-// 5 - Junk (i.e., setup dropdown fields, update users or templates) | log tout (ou presque)
-$cfg_features["event_loglevel"]	= 5;
-
-// Utilisation des fonctions mailing ou non, mettez 1 si vous voulez utiliser les 
-//notifications par mail.
-//Necessite que votre fonction mail() fonctionne.
-$cfg_features["mailing"]	= 0;	
-// Addresse de l'administrateur (obligatoire si mailing activé)
-$cfg_mailing["admin_email"]	= " ";
-// Signature for automatic generated E-Mails
-$cfg_mailing["signature"]	= "SIGNATURE";
-
-
-// A utiliser  uniquement si $cfg_features["mailing"] = 1;
-// Définition des envois des mails d'informations
-// admin : vers le mail $cfg_features["admin_email"]
-// all_admin : tous les utilisateurs en mode admin
-// all_normal : toutes les utilisateurs en mode normal
-// attrib : personne responsable de la tache
-// user : utilisateur demandeur
-// 1 pour l'envoi et 0 dans le cas contraire 
-
-$cfg_mailing["new"]["admin"]=1;
-$cfg_mailing["attrib"]["admin"]=1;
-$cfg_mailing["followup"]["admin"]=1;
-$cfg_mailing["finish"]["admin"]=1;
-
-$cfg_mailing["new"]["all_admin"]=0;
-$cfg_mailing["attrib"]["all_admin"]=0;
-$cfg_mailing["followup"]["all_admin"]=0;
-$cfg_mailing["finish"]["all_admin"]=0;
-
-
-$cfg_mailing["new"]["all_normal"]=0;
-$cfg_mailing["attrib"]["all_normal"]=0;
-$cfg_mailing["followup"]["all_normal"]=0;
-$cfg_mailing["finish"]["all_normal"]=0;
-
-$cfg_mailing["attrib"]["attrib"]=1;
-$cfg_mailing["followup"]["attrib"]=1;
-$cfg_mailing["finish"]["attrib"]=1;
-
-$cfg_mailing["new"]["user"]=1;
-$cfg_mailing["attrib"]["user"]=1;
-$cfg_mailing["followup"]["user"]=1;
-$cfg_mailing["finish"]["user"]=1;
-
-
-
-// Show jobs at login.
-// Montrer les interventions au loging (1 = oui | 0 = non)
-$cfg_features["jobs_at_login"]	= 1;
-
-// Show last num_of_events on login.
-// Nombre des derniers evenements presents dans le tableau au loging
-$cfg_features["num_of_events"]	= 10;
-
-// Send Expire Headers and set Meta-Tags for proper content expiration.
-$cfg_features["sendexpire"]		= 1;
-
-// In listings, cut long text fields after cut characters.
-$cfg_features["cut"]			= 80;	
-
-// Expire events older than this days at every login
-// (only admin-level login, set to 0 to disable expiration).
-// Temps durant lequel on log les actions ayant eu lieu
-// mettez cette variable a 0 pour conserver tous les logs (prend beaucoup de place dans la bdd)
-$cfg_features["expire_events"]	= 30;
-
-// Threshold for long listings, activates pager.
-//Nombre d'occurence (ordinateurs, imprimantes etc etc...) qui apparaitrons dans
-//la liste de recherche par page.
-$cfg_features["list_limit"]		= 15;	
-									
 
 // Report generation
 // Default Report included
@@ -218,20 +69,165 @@ $report_list["Par_annee"]["file"] = "reports/parAnnee.php";
 $report_list["excel"]["name"] = "excel";
 $report_list["excel"]["file"] = "reports/geneExcel.php";
 
-// options d'installation
-// Installation  option
+// *************************** Eléments optionnels  **********************
+// ***********************************************************************
+// ***********************************************************************
 
- // dicts
-//dictionnaires
-$cfg_install["languages"]	= array("english","deutsch","french");
+// Navigation Functions
+// Fonctions du menu
+class baseFunctions {
+	// Could have inventory, maintain, admin and settings, 
+	//changes these values on the dicts on header menu.
 
-// END OF CONFIGURATION
+	var $inventory	= true;
 
+	var $maintain	= true;
+
+	var $settings	= true;
+}
+
+//Options gérée dynamiquement, ne pas toucher cette partie.
+//Options from DB, do not touch this part.
+
+$db = new DB;
+$query = "select * from glpi_config";
+$result = $db->query($query);
+if($result)
+{
+
+//root document
+//document root
+$cfg_install["root"] = $db->result($result,0,"root_doc");
+
+// Gestion de source d'information alternatives pour le login
+// telles que des serveurs de mail en imap pop...
+// ports standards : pop 110 , imap 993
+// Dans tous les cas le dernier type de login testé est la base de données
+// Dans le cas où le login est incorrect dans la base mais est correct
+// sur la source alternative, l'utilisateur est ajouté ou son mot de passe
+// est modifié
+// Si plusieurs sources alternatives sont définies, seule la première
+// fournissant un login correct est utilisé
+
+$cfg_login['imap']['auth_server'] = $db->result($result,0,"imap_auth_server");
+$cfg_login['imap']['host'] = $db->result($result,0,"imap_host");
+
+// LDAP setup.
+// We can use LDAP both for authentication and for user information
+
+$cfg_login['ldap']['host'] = $db->result($result,0,"ldap_host");
+$cfg_login['ldap']['basedn'] = $db->result($result,0,"ldap_basedn");
+
+$cfg_login['ldap']['rootdn'] = $db->result($result,0,"ldap_rootdn");
+$cfg_login['ldap']['pass'] = $db->result($result,0,"ldap_pass");
+
+// some lDAP server (eg, M$ Active Directory) does not like anonymous
+// bind
+//$cfg_login['ldap']['rootdn'] = "cn=admin,ou=People,dc=sv,dc=lnf,dc=it";
+//$cfg_login['ldap']['pass'] = "secret";
+// relation between the GLPI users table field and the LDAP field
+
+$cfg_login['ldap']['fields'] = array( "name" => $db->result($result,0,"ldap_field_name"), "email" => $db->result($result,0,"ldap_field_email"), "location" => $db->result($result,0,"ldap_field_location"), "phone" => $db->result($result,0,"ldap_field_phone"), "realname" => $db->result($result,0,"ldap_field_realname"));
+
+
+
+//other sources
+//$cfg_login['other_source']...
+
+
+
+// Utilisation des fonctions mailing ou non, mettez 1 si vous voulez utiliser les 
+//notifications par mail.
+//Necessite que votre fonction mail() fonctionne.
+$cfg_features["mailing"]	= $db->result($result,0,"mailing");	
+// Addresse de l'administrateur (obligatoire si mailing activé)
+
+$cfg_mailing["admin_email"]	= $db->result($result,0,"admin_email");
+
+// Signature for automatic generated E-Mails
+$cfg_mailing["signature"]	= $db->result($result,0,"mailing_signature");
+
+
+// A utiliser  uniquement si $cfg_features["mailing"] = 1;
+// Définition des envois des mails d'informations
+// admin : vers le mail $cfg_features["admin_email"]
+// all_admin : tous les utilisateurs en mode admin
+// all_normal : toutes les utilisateurs en mode normal
+// attrib : personne responsable de la tache
+// user : utilisateur demandeur
+// 1 pour l'envoi et 0 dans le cas contraire 
+
+$cfg_mailing["new"]["admin"]= $db->result($result,0,"mailing_new_admin");
+$cfg_mailing["attrib"]["admin"]=$db->result($result,0,"mailing_attrib_admin");
+$cfg_mailing["followup"]["admin"]=$db->result($result,0,"mailing_followup_admin");
+$cfg_mailing["finish"]["admin"]=$db->result($result,0,"mailing_finish_admin");
+
+$cfg_mailing["new"]["all_admin"]=$db->result($result,0,"mailing_new_all_admin");
+$cfg_mailing["attrib"]["all_admin"]=$db->result($result,0,"mailing_attrib_all_admin");
+$cfg_mailing["followup"]["all_admin"]=$db->result($result,0,"mailing_followup_all_admin");
+$cfg_mailing["finish"]["all_admin"]=$db->result($result,0,"mailing_finish_all_admin");
+
+
+$cfg_mailing["new"]["all_normal"]=$db->result($result,0,"mailing_new_all_normal");
+$cfg_mailing["attrib"]["all_normal"]=$db->result($result,0,"mailing_attrib_all_normal");
+$cfg_mailing["followup"]["all_normal"]=$db->result($result,0,"mailing_followup_all_normal");
+$cfg_mailing["finish"]["all_normal"]=$db->result($result,0,"mailing_finish_all_normal");
+
+$cfg_mailing["attrib"]["attrib"]=$db->result($result,0,"mailing_attrib_attrib");
+$cfg_mailing["followup"]["attrib"]=$db->result($result,0,"mailing_followup_attrib");
+$cfg_mailing["finish"]["attrib"]=$db->result($result,0,"mailing_finish_attrib");
+
+$cfg_mailing["new"]["user"]=$db->result($result,0,"mailing_new_user");
+$cfg_mailing["attrib"]["user"]=$db->result($result,0,"mailing_attrib_user");
+$cfg_mailing["followup"]["user"]=$db->result($result,0,"mailing_followup_user");
+$cfg_mailing["finish"]["user"]=$db->result($result,0,"mailing_finish_user");
+
+
+
+// Features configuration
+
+// Log level :
+// Niveau de log :
+
+// 1 - Critical (login failures) |  (erreur de loging seulement)
+// 2 - Severe - not used  | (non utilisé)
+// 3 - Important - (sucessfull logins)  |  importants (loging réussis)
+// 4 - Notice (updates, adds, deletes, tracking) | classique
+// 5 - Junk (i.e., setup dropdown fields, update users or templates) | log tout (ou presque)
+$cfg_features["event_loglevel"]	= $db->result($result,0,"event_loglevel");
+
+// Show jobs at login.
+// Montrer les interventions au loging (1 = oui | 0 = non)
+$cfg_features["jobs_at_login"]	= $db->result($result,0,"jobs_at_login");
+
+// Show last num_of_events on login.
+// Nombre des derniers evenements presents dans le tableau au loging
+$cfg_features["num_of_events"]	= $db->result($result,0,"num_of_events");
+
+//++ not on the config
+// Send Expire Headers and set Meta-Tags for proper content expiration.
+$cfg_features["sendexpire"]		= $db->result($result,0,"sendexpire");
+
+// In listings, cut long text fields after cut characters.
+$cfg_features["cut"]			= $db->result($result,0,"cut");	
+
+// Expire events older than this days at every login
+// (only admin-level login, set to 0 to disable expiration).
+// Temps en jours durant lequel on log les actions ayant eu lieu
+// mettez cette variable a 0 pour conserver tous les logs (prend beaucoup de place dans la bdd)
+$cfg_features["expire_events"]	= $db->result($result,0,"expire_events");
+
+// Threshold for long listings, activates pager.
+//Nombre d'occurence (ordinateurs, imprimantes etc etc...) qui apparaitrons dans
+//la liste de recherche par page.
+
+$cfg_features["list_limit"]		= $db->result($result,0,"list_limit");	
 
 // version number
 // numero de version
-$cfg_install["version"]		=" 0.31";
-$cfg_layout["logotxt"]		= "GLPI powered by indepnet";
 
+$cfg_install["version"]		= $db->result($result,0,"version");
+$cfg_layout["logotxt"]		= $db->result($result,0,"logotxt");
 
+}
 ?>
