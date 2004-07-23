f878fda81578ebcc8a98ebaa54e4c8ccad76d571
moyooo <moyo@indepnet.net>
2004-07-23 21:04:48 +0000

    Modified Files: 	glpi/glpi/tracking/functions.php  glpi/glpi/tracking/classes.php ---------------------------------------------------------------------- Une petite fonction de plus pour que ca soit plus propre

--- glpi/tracking/classes.php
+++ glpi/tracking/classes.php
@@ -157,6 +157,11 @@ class Job {
 			return false;
 		}
 	}
+	
+	function getRealtime(){
+		global $lang;	
+		return floor($job->realtime)." ".$lang["job"][21]." ".(($job->realtime-floor($job->realtime))*60)." ".$lang["job"][22];
+		}
 
 	function assignTo($user) {
 		// assign Job to user
