6c3fa5412bd4eb04d36c293ac80fa3f3c645b2c0
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2004-08-29 20:47:27 +0000

    Rapport reseau OK
     Modified Files:
     	glpi/reports/reports/convexcel/rapport-reseaux.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@473 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- reports/reports/convexcel/rapport-reseaux.php
+++ reports/reports/convexcel/rapport-reseaux.php
@@ -40,10 +40,15 @@ This file is part of GLPI.
 include ("_relpos.php");
 
 include ($phproot . "/glpi/includes.php");
-include ($phproot . "/glpi/dicts/french.php");
+include ($phproot . "/glpi/includes_networking.php");
+
+checkAuthentication("normal");
 
 $db = new DB;
-$query = "select * from glpi_networking";
+//$query = "select * from glpi_networking";
+$query = "select glpi_networking.*, glpi_networking_ports.ifaddr, glpi_networking_ports.ifmac ";
+$query.= "from glpi_networking LEFT JOIN glpi_networking_ports ON (glpi_networking_ports.device_type = 2 AND glpi_networking_ports.on_device = glpi_networking.ID) ORDER by glpi_networking.ID";
+
 $result = $db->query($query);
 $num_field= $db->num_fields($result);
 //set_time_limit(10);
@@ -95,27 +100,54 @@ $border2->set_merge(); # This is the key feature
 # Only one cell should contain text, the others should be blank.
 $worksheet->write(0, 0, $lang["networking"][42], $border1);
 $worksheet->write(0, 1, $lang["networking"][0], $border1);
-$worksheet->write(0, 2, $lang["networking"][2],   $border1);
-$worksheet->write(0, 3, $lang["networking"][5],  $border1);
-$worksheet->write(0, 4, $lang["networking"][1],  $border1);
-$worksheet->write(0, 5, $lang["networking"][6], $border1);
-$worksheet->write(0, 6, $lang["networking"][7], $border1);
-$worksheet->write(0, 7, $lang["networking"][3], $border1);
-$worksheet->write(0, 8, $lang["networking"][4], $border1);
-$worksheet->write(0, 9, $lang["networking"][9], $border1);
-$worksheet->write(0, 10, $lang["networking"][8], $border1);
-$worksheet->write(0, 11, $lang["networking"][39], $border1);
-$worksheet->write(0, 12, $lang["networking"][40], $border1);
-$worksheet->write(0, 13, $lang["networking"][41], $border1);
+$worksheet->write(0, 2, $lang["networking"][5],  $border1);
+$worksheet->write(0, 3, $lang["networking"][6], $border1);
+$worksheet->write(0, 4, $lang["networking"][7], $border1);
+$worksheet->write(0, 5, $lang["networking"][3], $border1);
+$worksheet->write(0, 6, $lang["networking"][4], $border1);
+$worksheet->write(0, 7, $lang["networking"][9], $border1);
+$worksheet->write(0, 8, $lang["networking"][8], $border1);
+$worksheet->write(0, 9, $lang["networking"][39], $border1);
+$worksheet->write(0, 10, $lang["networking"][40], $border1);
+$worksheet->write(0, 11, $lang["networking"][41], $border1);
+$worksheet->write(0, 12, $lang["networking"][1],  $border1);
+$worksheet->write(0, 13, $lang["networking"][2],   $border1);
+$worksheet->write(0, 14, $lang["networking"][14], $border1);
+$worksheet->write(0, 15, $lang["networking"][15], $border1);
 
 
 $y=1;
+$old_ID=-1;
+$nb_skip=0; // Skip multiple interface computers
+$table=array();
 while($ligne = $db->fetch_array($result))
 {
-	for($i=0;$i<$num_field;$i++)
-	{
-		$worksheet->write($y, $i, $ligne[$i]);
+	// New computer
+	if ($old_ID!=$ligne[0]){
+		// reinit data
+		for($i=0;$i<$num_field;$i++) {
+					$name=$db->field_name($result,$i);
+			if($name == "firmware") {
+				$table[$i]=getDropdownName("glpi_dropdown_firmware",$ligne[$i]);
+			}
+			elseif($name == "location") {
+				$table[$i]=getDropdownName("glpi_dropdown_locations",$ligne[$i]);
+			}
+			elseif($name == "type") {
+				$table[$i]=getDropdownName("glpi_type_networking",$ligne[$i]);
+			}
+			else $table[$i]=$ligne[$i];
+	}		
+		$old_ID=$ligne[0];
+	} 
+	else { // Same computer
+		$nb_skip++;
+		// Add the new interface :
+		for($i=$num_field-2;$i<$num_field;$i++)
+		if ($ligne[$i]!="") $table[$i].="\n".$ligne[$i];
 	}
+	$worksheet->write_row($y-$nb_skip, 0, $table);
+	
 	$y++;
 }
 
