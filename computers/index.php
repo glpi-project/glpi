047d1c3c24a1317e5c1038d2664d269ede92370e
moyooo <moyo@indepnet.net>
2004-08-31 09:09:26 +0000

    Encore une petite couille dans l'affichage initial des computers
     Modified Files:
     	glpi/computers/index.php
    ----------------------------------------------------------------------

--- computers/index.php
+++ computers/index.php
@@ -52,7 +52,7 @@ if(!isset($_GET["start"])) $_GET["start"] = 0;
 if (!isset($_GET["order"])) $_GET["order"] = "ASC";
 if (!isset($_GET["field"])) $_GET["field"] = "all";
 if (!isset($_GET["phrasetype"])) $_GET["phrasetype"] = "contains";
-if (!isset($_GET["contains"])) $_GET["contains"] = " ";
+if (!isset($_GET["contains"])) $_GET["contains"] = "";
 if (!isset($_GET["sort"])) $_GET["sort"] = "comp.name";
 
 showComputerList($_SERVER["PHP_SELF"],$_SESSION["glpiname"],$_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"]);
