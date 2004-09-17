42e6d8010aa4602771de658c2ead815bf1e59674
moyooo <moyo@indepnet.net>
2004-09-17 19:04:12 +0000

    Avec les bonnes entetes
     Modified Files:
     	glpi/reports/reports/convexcel/rapport-sylk.php

--- reports/reports/convexcel/rapport-sylk.php
+++ reports/reports/convexcel/rapport-sylk.php
@@ -1,5 +1,31 @@
 <?php
+/*
+ 
+  ----------------------------------------------------------------------
+GLPI - Gestionnaire libre de parc informatique
+ Copyright (C) 2002 by the INDEPNET Development Team.
+ Bazile Lebeau, baaz@indepnet.net - Jean-Mathieu Doléans, jmd@indepnet.net
+ http://indepnet.net/   http://glpi.indepnet.org
+ ----------------------------------------------------------------------
+ LICENSE
 
+This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ----------------------------------------------------------------------
+// Based on
 // ------------------------------------------------------------------------- //
 // Génération d'un fichier SYLK à partir de données MySQL en vue d'une       //
 // récupération sous Excel.                                                  //
