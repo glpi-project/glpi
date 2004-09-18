c4d100d98b058a4878d7b47a2a4417db2157f735
moyooo <moyo@indepnet.net>
2004-09-18 16:54:08 +0000

    Une nouvelle version qui doit g�rer les \
     Modified Files:
     	glpi/glpi/common/classes.php glpi/glpi/common/functions.php

--- glpi/common/functions.php
+++ glpi/common/functions.php
@@ -117,20 +117,19 @@ function searchUserbyType($authtype) {
 
    function stripslashes_deep($value)
    {
-//   	echo "strip";
+//echo "strip";
        $value = is_array($value) ?
                    array_map('stripslashes_deep', $value) :
                    stripslashes($value);
-
+                   
        return $value;
    }
    function addslashes_deep($value)
    {
-//   	echo "add";
+//echo "add";
        $value = is_array($value) ?
                    array_map('addslashes_deep', $value) :
                    addslashes($value);
-
        return $value;
    }
     
@@ -138,23 +137,36 @@ function checkAuthentication($authtype) {
 	// Universal method to have a magic-quote-gpc system
 	global $_POST, $_GET,$_COOKIE,$tab;
 	// Clean array and addslashes
+	
+if (get_magic_quotes_gpc()) {
 	if (isset($_POST)){
 	$_POST = array_map('stripslashes_deep', $_POST);
-	$_POST = array_map('addslashes_deep', $_POST);
 	}
 	if (isset($_GET)){
     $_GET = array_map('stripslashes_deep', $_GET);
-    $_GET = array_map('addslashes_deep', $_GET);
 	}
-	if (isset($_COOKIE)){
+/*	if (isset($_COOKIE)){
     $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
     $_COOKIE = array_map('addslashes_deep', $_COOKIE);
 	}
-	if (isset($tab)){
+*/	if (isset($tab)){
     $tab = array_map('stripslashes_deep', $tab);    
+    }
+}    
+
+	if (isset($_POST)){
+	$_POST = array_map('addslashes_deep', $_POST);
+	}
+	if (isset($_GET)){
+    $_GET = array_map('addslashes_deep', $_GET);
+	}
+/*	if (isset($_COOKIE)){
+    $_COOKIE = array_map('addslashes_deep', $_COOKIE);
+	}
+*/	if (isset($tab)){
     $tab = array_map('addslashes_deep', $tab);    
     }
-    
+
 	// Checks a GLOBAL user and password against the database
 	// If $authtype is "normal" or "admin", it checks if the user
 	// has the privileges to do something. Should be used in every 
