0dfd7738f7145d311e75d7924e7f682c261e30b3
baaz <baaz@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2004-09-18 13:14:10 +0000

    Bazouillerie dans l'install pour les noob
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@665 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install.php
+++ install.php
@@ -588,7 +588,9 @@ function step6($root_doc, $event_loglevel, $num_of_events, $expire_events,$jobs_
 function step7() {
 
 	global $lang;
-	
+	$root_doc = ereg_replace("/install.php","",$_SERVER['REQUEST_URI']);
+	$query = "update glpi_config set root_doc = '".$root_doc."'";
+	$db->query($query);
 	echo "<h2>".$lang["install"][55]."</h2>";
 	echo "<p>".$lang["install"][57]."</p>";
 	echo "<p><ul><li> ".$lang["install"][58]."</li>";
