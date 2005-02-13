ce128252ce382b5c352ce2c503cfc625dc30b933
moyooo <moyo@indepnet.net>
2005-02-13 00:32:49 +0000

    Delete des fichiers temporaires
     Removed Files:
     	glpi/change_db_config.sql glpi/knowbase/change_db_knowbase.sql
     	glpi/reservation/change_db_resa.sql
     	glpi/software/change_db_oem_update.sql
     	glpi/tracking/change_db_tracking.sql

--- tracking/change_db_tracking.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE glpi_dropdown_tracking_category (
-  ID int(11) NOT NULL auto_increment,
-  name varchar(255) default NULL,
-  PRIMARY KEY  (ID)
-);
-
-ALTER TABLE `glpi_tracking` ADD `category` INT( 11 ) ;
