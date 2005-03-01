7fe63eaa7ea70af3d17fef400c427edd1762705a
silvermat <silvermat>
2005-03-01 22:18:46 +0000

    Modif infocoms :
    + ajout icones manquants pour edit et web
    + entr�es dans dico

--- glpi/infocoms/functions.php
+++ glpi/infocoms/functions.php
@@ -60,10 +60,11 @@ function showInfocomForm ($target,$device_type,$dev_ID,$show_immo=1) {
 			if (!empty($ent->fields['website'])){
 				if (!ereg("https*://",$ent->fields['website']))	$website="http://".$ent->fields['website'];
 				else $website=$ent->fields['website'];
-				echo "<a href='$website'>SITE WEB</a>";
+				echo "&nbsp;&nbsp;";
+				echo "<a href='$website'><img src='".$HTMLRel."pics/web.png' style='vertical-align:middle;' alt='".$lang["common"][4]."' title='".$lang["common"][4]."' ></a>";
 			}
-		echo "&nbsp;&nbsp;";
-		echo "<a href='".$HTMLRel."enterprises/enterprises-info-form.php?ID=".$ent->fields['ID']."'>MODIF</a>";
+		echo "&nbsp;&nbsp;&nbsp;&nbsp;";
+		echo "<a href='".$HTMLRel."enterprises/enterprises-info-form.php?ID=".$ent->fields['ID']."'><img src='".$HTMLRel."pics/edit.png' style='vertical-align:middle;' alt='".$lang["buttons"][14]."' title='".$lang["buttons"][14]."'></a>";
 		}
 		echo "</td>";
 		
