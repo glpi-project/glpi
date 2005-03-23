d3c9d9dd831e09374c813438c98fc8cb8d9d1ccb
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-03-23 17:26:19 +0000

    Bug 2139 : probleme avec suivi par email et changement category et priorit� pour les interventions
     Modified Files:
     	glpi/tracking/tracking-category-form.php
     	glpi/tracking/tracking-priority-form.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1251 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- tracking/tracking-category-form.php
+++ tracking/tracking-category-form.php
@@ -35,7 +35,14 @@
 include ("_relpos.php");
 include ($phproot . "/glpi/includes.php");
 include ($phproot . "/glpi/includes_tracking.php");
+include ($phproot . "/glpi/includes_setup.php");
 include ($phproot . "/glpi/includes_computers.php");
+include ($phproot . "/glpi/includes_printers.php");
+include ($phproot . "/glpi/includes_monitors.php");
+include ($phproot . "/glpi/includes_peripherals.php");
+include ($phproot . "/glpi/includes_networking.php");
+include ($phproot . "/glpi/includes_software.php");
+
 
 checkAuthentication("admin");
 
