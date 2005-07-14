5176a8bc806f0bdc4b010ea6853152d5d03c3f1d
moyooo <moyo@indepnet.net>
2005-07-14 01:18:07 +0000

    Encore des petits hack pour l'UTF8
     Modified Files:
     	glpi/install.php glpi/update.php glpi/update_content.php

--- update_content.php
+++ update_content.php
@@ -226,6 +226,10 @@ return TRUE;
 //########################### Script start ################################
 
 loadLang();
+
+		// Send UTF8 Headers
+		header("Content-Type: text/html; charset=UTF-8");
+
 //style and co
         echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">";
         echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\">";
