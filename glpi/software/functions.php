6fb098edbda15f0b702879ad3d2ebbcf28310b5a
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-07-26 22:44:49 +0000

    Fin onglets + pas debug dans CVS
     Modified Files:
     	glpi/cartridges/cartridge-info-form.php
     	glpi/glpi/cartridges/functions.php glpi/glpi/config/config.php
     	glpi/glpi/software/functions.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1651 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- glpi/software/functions.php
+++ glpi/software/functions.php
@@ -51,7 +51,7 @@ function titleSoftware(){
 }
 
 function showSoftwareOnglets($target,$withtemplate,$actif){
-	global $lang;
+	global $lang,$HTMLRel;
 	
 	$template="";
 	if(!empty($withtemplate)){
@@ -75,8 +75,8 @@ function showSoftwareOnglets($target,$withtemplate,$actif){
 	$next=getNextItem("glpi_software",$ID);
 	$prev=getPreviousItem("glpi_software",$ID);
 	$cleantarget=preg_replace("/\?ID=([0-9]+)/","",$target);
-		if ($prev>0) echo "<li><a href='$cleantarget?ID=$prev'><</a></li>";
-	if ($next>0) echo "<li><a href='$cleantarget?ID=$next'>></a></li>";
+		if ($prev>0) echo "<li><a href='$cleantarget?ID=$prev'><img src=\"".$HTMLRel."pics/left.png\" alt='".$lang["buttons"][12]."' title='".$lang["buttons"][12]."'></a></li>";
+	if ($next>0) echo "<li><a href='$cleantarget?ID=$next'><img src=\"".$HTMLRel."pics/right.png\" alt='".$lang["buttons"][11]."' title='".$lang["buttons"][11]."'></a></li>";
 	}
 	echo "</ul></div>";
 	
