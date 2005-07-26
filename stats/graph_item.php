9ddc347e4cf75bfbd3a2ff9f5f2e89e99f01002d
moyooo <moyo@indepnet.net>
2005-07-26 17:27:18 +0000

    Fichier manquant pour les stats
     Added Files:
     	graph_item.php

---
+++ stats/graph_item.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2005 by the INDEPNET Development Team.
+ 
+ http://indepnet.net/   http://glpi.indepnet.org
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// ----------------------------------------------------------------------
+// Original Author of file: Julien Dombre
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+include ("_relpos.php");
+include ($phproot . "/glpi/includes.php");
+include ($phproot . "/glpi/includes_tracking.php");
+include ($phproot . "/glpi/includes_enterprises.php");
+include ($phproot . "/glpi/includes_setup.php");
+require ("functions.php");
+
+checkAuthentication("normal");
+header("Content-Type: text/html; charset=UTF-8");
+
+if(empty($_POST["date1"])&&empty($_POST["date2"])) {
+$year=date("Y")-2;
+$_POST["date1"]=date("Y-m-d",mktime(1,0,0,date("m"),date("d"),$year));
+
+$_POST["date2"]=date("Y-m-d");
+}
+
+if(empty($_POST["date1"])) $_POST["date1"] = "";
+if(empty($_POST["date2"])) $_POST["date2"] = "";
+if ($_POST["date1"]!=""&&$_POST["date2"]!=""&&strcmp($_POST["date2"],$_POST["date1"])<0){
+$tmp=$_POST["date1"];
+$_POST["date1"]=$_POST["date2"];
+$_POST["date2"]=$tmp;
+}
+
+
+echo "<div align='center'><form method=\"post\" name=\"form\" action=\"".$_SERVER["REQUEST_URI"]."\">";
+echo "<table class='tab_cadre'><tr class='tab_bg_2'><td align='right'>";
+echo $lang["search"][8]." :</td><td>";
+showCalendarForm("form","date1",$_POST["date1"]);
+echo "</td><td rowspan='2' align='center'><input type=\"submit\" class='button' name\"submit\" Value=\"". $lang["buttons"][7] ."\" /></td></tr>";
+echo "<tr class='tab_bg_2'><td align='right'>".$lang["search"][9]." :</td><td>";
+showCalendarForm("form","date2",$_POST["date2"]);
+echo "</td></tr>";
+echo "</table></form></div>";
+
+switch($_GET["type"]){
+case "technicien":
+	$val1=$_GET["ID"];
+	$val2=$_GET["assign_type"];
+	break;
+case "user":
+	$val1=$_GET["ID"];
+	$val2="";
+	break;	
+case "category":
+	$val1=$_GET["ID"];
+	$val2="";
+	break;	
+case "device":
+	$val1=$_GET["ID"];
+	$val2=$_GET["device"];
+	break;
+case "comp_champ":
+	$val1=$_GET["ID"];
+	$val2=$_GET["champ"];
+	break;
+
+}
+
+
+///////// Stats nombre intervention
+// Total des interventions
+$entrees_total=constructEntryValues("inter_total",$_POST["date1"],$_POST["date2"],$_GET["type"],$val1,$val2);
+	if (count($entrees_total)>0)
+graphBy($entrees_total,$lang["stats"][5],$lang["stats"][35],1,"month");
+
+// Total des interventions résolues
+$entrees_solved=constructEntryValues("inter_solved",$_POST["date1"],$_POST["date2"],$_GET["type"],$val1,$val2);
+	if (count($entrees_solved)>0)
+graphBy($entrees_solved,$lang["stats"][11],$lang["stats"][35],1,"month");
+
+//Temps moyen de resolution d'intervention
+$entrees_avgtime=constructEntryValues("inter_avgsolvedtime",$_POST["date1"],$_POST["date2"],$_GET["type"],$val1,$val2);
+	if (count($entrees_avgtime)>0)
+graphBy($entrees_avgtime,$lang["stats"][6],$lang["stats"][32],0,"month");
+
+//Temps moyen d'intervention réel
+$entrees_avgtime=constructEntryValues("inter_avgrealtime",$_POST["date1"],$_POST["date2"],$_GET["type"],$val1,$val2);
+	if (count($entrees_avgtime)>0)
+graphBy($entrees_avgtime,$lang["stats"][25],$lang["stats"][33],0,"month");
+
+//Temps moyen de prise en compte de l'intervention
+$entrees_avgtime=constructEntryValues("inter_avgtakeaccount",$_POST["date1"],$_POST["date2"],$_GET["type"],$val1,$val2);
+	if (count($entrees_avgtime)>0)
+graphBy($entrees_avgtime,$lang["stats"][30],$lang["stats"][32],0,"month");
+
+
+?>
