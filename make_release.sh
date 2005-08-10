e5f09a7875187bb06b094631db889d94f1177580
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-08-10 12:49:41 +0000

    make_release et changelog
     Modified Files:
     	glpi/CHANGELOG.txt glpi/make_release.sh
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1732 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- make_release.sh
+++ make_release.sh
@@ -53,7 +53,8 @@ echo "Delete bigdumps and older sql files";
 \rm mysql/updatedb.back;
 \rm mysql/glpi-0.3*-*;
 \rm mysql/glpi-0.4*-*;
-\rm mysql/glpi-0.5-*;
+\rm mysql/glpi-0.5*;
+\rm mysql/glpi-*-default*;
 
 echo "Delete LDAP directories";
 \rm -rf ldap;
