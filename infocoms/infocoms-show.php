328b6db4380bdac466fe1dffb700052265706284
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-08-11 17:55:11 +0000

    FIchier manquant
     Added Files:
     	glpi/infocoms/infocoms-show.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1751 35612d4c-9d10-0410-837c-bf9dd1be60d7

---
+++ infocoms/infocoms-show.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2005 by the INDEPNET Development Team.
+ 
+ http://indepnet.net/   http://glpi.indepnet.org
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+ 
+// ----------------------------------------------------------------------
+// Original Author of file: Julien Dombre
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+include ("_relpos.php");
+include ($phproot . "/glpi/includes.php");
+include ($phproot . "/glpi/includes_financial.php");
+include ($phproot . "/glpi/includes_computers.php");
+include ($phproot . "/glpi/includes_printers.php");
+include ($phproot . "/glpi/includes_monitors.php");
+include ($phproot . "/glpi/includes_peripherals.php");
+include ($phproot . "/glpi/includes_networking.php");
+include ($phproot . "/glpi/includes_software.php");
+include ($phproot . "/glpi/includes_cartridges.php");
+include ($phproot . "/glpi/includes_consumables.php");
+
+
+checkAuthentication("normal");
+
+nullHeader($lang["title"][21],$_SERVER["PHP_SELF"]);
+
+$ci=new CommonItem();
+
+if(!isset($_GET["device_type"])||!isset($_GET["device_id"])||!$ci->getFromDB($_GET["device_type"],$_GET["device_id"]))
+echo "<div align='center'>".$lang["financial"][85]."</div>";
+else {
+	
+	echo "<div align='center'><strong>".$ci->getType()." - ".$ci->getLink()."</strong></div>";
+	if (isset($_GET["update"])&&$_GET["update"]==1) $withtemplate=0;
+	else $withtemplate=2;
+	showInfocomForm ($HTMLRel."infocoms/infocoms-info-form.php",$_GET["device_type"],$_GET["device_id"],1,$withtemplate);
+}
+
+
+commonFooter();
+
+?>
