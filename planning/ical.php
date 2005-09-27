f2b5b875889365bee7f82b08d112bb5984123910
silvermat <silvermat>
2005-09-27 14:39:17 +0000

    Debut g�n�ration ical et webcal pour planning

---
+++ planning/ical.php
@@ -0,0 +1,30 @@
+<?php
+
+
+include ("_relpos.php");
+include ($phproot . "/glpi/includes.php");
+include ($phproot . "/glpi/includes_planning.php");
+include ($phproot . "/glpi/includes_tracking.php");
+include ($phproot . "/glpi/includes_users.php");
+include ($phproot . "/glpi/includes_computers.php");
+include ($phproot . "/glpi/includes_printers.php");
+include ($phproot . "/glpi/includes_monitors.php");
+include ($phproot . "/glpi/includes_peripherals.php");
+include ($phproot . "/glpi/includes_networking.php");
+include ($phproot . "/glpi/includes_software.php");
+include ($phproot . "/glpi/includes_enterprises.php");
+
+
+checkAuthentication("normal");
+
+//export ICAL
+
+if (!isset($_GET["uID"])) $_GET["uID"]=$_SESSION["glpiID"];
+
+//@header ("content-type:text/calendar");
+
+echo generateIcal($_GET["uID"]);
+
+
+
+?>
