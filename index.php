d3b7831e4d780dd88c4c456a6cf67a574706f482
silvermat <silvermat@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-10-17 20:48:07 +0000

    Internationalisation et refonte graphique + xhtml strict de la page de login
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1895 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- index.php
+++ index.php
@@ -41,89 +41,96 @@ if(!file_exists($phproot ."/glpi/config/config_db.php")) {
 }
 else
 {
-include ($phproot . "/glpi/includes.php");
-loadLanguage();
-// Using CAS server
-if (!empty($cfg_login['cas']['host'])&&!isset($_GET["noCAS"])) {
-	glpi_header("login.php");
-}
+	include ($phproot . "/glpi/includes.php");
+	// load default dictionnary 
+	loadLanguage();
+	// Using CAS server
+	if (!empty($cfg_login['cas']['host'])&&!isset($_GET["noCAS"])) {
+		glpi_header("login.php");
+		}
 
 
-// Send UTF8 Headers
+	// Send UTF8 Headers
 	header("Content-Type: text/html; charset=UTF-8");
-// Start the page
-echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
-echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\">";
-echo "<head><title>GLPI Login</title>\n";
-echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8 \" />\n";
-echo "<meta http-equiv=\"Content-Script-Type\" content=\"text/javascript\" />\n";
-echo "<link rel='shortcut icon' type='images/x-icon' href='".$HTMLRel."pics/favicon.ico' />";
-
-// Appel CSS
+	// Start the page
 
 
-echo "<link rel='stylesheet'  href='".$HTMLRel."styles.css' type='text/css' media='screen' />";
+	echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">";
+	echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\">";
+	echo "<head><title>GLPI Login</title>\n";
+	echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8 \" />\n";
+	echo "<meta http-equiv=\"Content-Script-Type\" content=\"text/javascript\" />\n";
+	echo "<link rel='shortcut icon' type='images/x-icon' href='".$HTMLRel."pics/favicon.ico' />";
 
+	// Appel CSS
+	echo "<link rel='stylesheet'  href='".$HTMLRel."styles.css' type='text/css' media='screen' />";
+	echo "</head>";
 
+	// Body with configured stuff
+	echo "<body>";
+	// contenu
+	
+	
 
-echo "</head>";
+	echo "<div id='contenulogin'>";
 
-// Body with configured stuff
-
-echo "<body>";
-
-
-// contenu
-
-echo "<div id='contenulogin'>";
-
-echo "<div id='logo-login'>";
-echo "<img src=\"".$HTMLRel."pics/logo-glpi-login.png\"  alt=\"Logo GLPI Powered By Indepnet\" title=\"Powered By Indepnet\" /><br />";
-echo "<a href=\"http://GLPI.indepnet.org/\" class='sous_logo'>";
-	echo "GLPI version ".$cfg_install["version"]."";
-	echo "</a>";
-echo "</div>";
-
-echo "<div id='boxlogin'>";
-
-echo "<form action='login.php' method='post'>";
-
-if (isset($_GET["noCAS"])) echo "<input type='hidden' name='noCAS' value='1' />";
-
-echo "<fieldset>";
-echo "<legend>Identification</legend>";
-
-
-echo "<div class='row'><span class='label'><label>".$lang["login"][6]." :  </label></span><span class='formw'> <input type='text' name='login_name' id='login_name' maxlength='30' /></span></div>";
-
-
-echo "<div class='row'><span class='label'><label>".$lang["login"][7]." : </label></span><span class='formw'><input type='password' name='login_password' id='login_password' maxlength='30' /> </span></div>";
-
-echo "</fieldset>";
-
-echo "<p ><span> <input type='submit' name='submit' value='Login' class='submit' /></span></p>";
-echo "</form>";
-
- 
-echo "<p > <img src='".$HTMLRel."pics/key.png' alt='keys' /> </p>";
-if ($cfg_features['public_faq'])
-	echo "<p><a href='faq.php'>".$lang["knowbase"][24]."</a></p>";
-
-
-echo "</div>";
-echo "</div>";
-
-
-// fin contenu
-
-
-
-// End
+	echo "<div id='logo-login'>";
+	//echo "<a href=\"http://GLPI.indepnet.org/\" class='sous_logo'><img src=\"".$HTMLRel."pics/logo-glpi-login.png\"  alt=\"Logo GLPI Powered By Indepnet\" title=\"Powered By Indepnet\" /><br />";
+	//echo "</a>";
+	
+	echo "<ul>";
+	// Affichage autorisé FAQ
+	if ($cfg_features['public_faq']){
+		echo "<li><a href='faq.php'>".$lang["knowbase"][24]."</a></li>";}
+	echo "</ul>";
+	echo "</div>";
 
 	
+
+
+	echo "<div id='boxlogin'>";
+	
+	echo "<form action='login.php' method='post'>";
+	// authentification CAS 
+	if (isset($_GET["noCAS"])) echo "<input type='hidden' name='noCAS' value='1' />";
+
+	echo "<fieldset>";
+	echo "<legend>".$lang["login"][10]."</legend>";
+
+	
+	echo "<div class='row'><span class='label'><label>".$lang["login"][6]." :  </label></span><span class='formw'> <input type='text' name='login_name' id='login_name' size='20' /></span></div>";
+	
+	
+	echo "<div class='row'><span class='label'><label>".$lang["login"][7]." : </label></span><span class='formw'><input type='password' name='login_password' id='login_password' size='20' /> </span></div>";
+	
+	
+	
+	
+	
+	echo "</fieldset>";
+echo "<p ><span> <input type='submit' name='submit' value='Login' class='submit' /></span></p>";
+	echo "</form>";
+	
+
+
+
+	echo "</div>";  // fin box login
+
+	
+
+	echo "</div>"; // fin contenu login
+		
+	
+
+	echo "<div id='footer-login'>";
+	echo "<a href=\"http://GLPI.indepnet.org/\" title=\"Powered By Indepnet\"  >";
+	echo "GLPI version ".$cfg_install["version"]."";
+	echo "</a>";
+	echo "</div>";
 	
 }
 echo "</body></html>";
 
+// End
 
 ?>
