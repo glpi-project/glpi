a8cad7f06772d0d103b6122ce49d35dce91033b5
moyooo <moyo@indepnet.net>
2005-10-18 22:12:30 +0000

    Correction Silvermaterie
     Modified Files:
     	glpi/computers/index.php

--- computers/index.php
+++ computers/index.php
@@ -40,7 +40,6 @@ include ($phproot . "/glpi/includes.php");
 include ($phproot . "/glpi/includes_computers.php");
 include ($phproot . "/glpi/includes_financial.php");
 include ($phproot . "/glpi/includes_state.php");
-include ($phproot . "/glpi/includes_search.php");
 
 checkAuthentication("normal");
 
