722658be7e0ca3edd721cd5c970653459c739866
silvermat <silvermat>
2005-10-19 19:40:26 +0000

    Impl�mentation de la nouvelle fonction de recherche aux printers

--- printers/index.php
+++ printers/index.php
@@ -42,25 +42,31 @@ include ($phproot . "/glpi/includes_financial.php");
 include ($phproot . "/glpi/includes_state.php");
 
 checkAuthentication("normal");
-
 commonHeader($lang["title"][8],$_SERVER["PHP_SELF"]);
+titlePrinters();
+include ($phproot . "/glpi/includes_search.php");
+searchForm(PRINTER_TYPE,$cfg_install["root"]."/printers/printers-search.php");
+
+
+
   
 if(!isset($_GET["start"])) $_GET["start"] = 0;
 if (!isset($_GET["order"])) $_GET["order"] = "ASC";
-if (!isset($_GET["field"])) $_GET["field"] = array(0 => "printer.name");
+if (!isset($_GET["field"])) $_GET["field"] = array(0 => "view");
 if (!isset($_GET["phrasetype"])) $_GET["phrasetype"] = "contains";
 if (!isset($_GET["contains"])) $_GET["contains"] = array(0=>"");
-if (!isset($_GET["sort"])) $_GET["sort"] = "printer.name";
+if (!isset($_GET["sort"])) $_GET["sort"] = 1;
 if (!isset($_GET["deleted"])) $_GET["deleted"] = "N";
 if (!isset($_GET["link"])) $_GET["link"] = "";
 
-titlePrinters();
 
 
-searchFormPrinters();
 
-showPrintersList($_SERVER["PHP_SELF"],$_SESSION["glpiname"],$_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
+//searchFormPrinters();
 
+//showPrintersList($_SERVER["PHP_SELF"],$_SESSION["glpiname"],$_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
+
+showList(PRINTER_TYPE,$_SERVER["PHP_SELF"],$_GET["field"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
 
 commonFooter();
 ?>
