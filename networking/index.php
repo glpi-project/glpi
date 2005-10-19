b28ec6d5b02d295ad984daa9e993462d8c5ab095
silvermat <silvermat@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-10-19 19:27:18 +0000

    Impl�mentation de la nouvelle fonction de recherche aux network
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1923 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- networking/index.php
+++ networking/index.php
@@ -48,23 +48,29 @@ commonHeader($lang["title"][6],$_SERVER["PHP_SELF"]);
 
 titleNetdevices();
 
+
+include ($phproot . "/glpi/includes_search.php");
+searchForm(NETWORKING_TYPE,$cfg_install["root"]."/networking/networking-search.php");
+
+
 //listNetdevices();
 
 
 if(!isset($_GET["start"])) $_GET["start"] = 0;
 if (!isset($_GET["order"])) $_GET["order"] = "ASC";
-if (!isset($_GET["field"])) $_GET["field"] = array(0 => "glpi_networking.name");
+if (!isset($_GET["field"])) $_GET["field"] = array(0 => "view");
 if (!isset($_GET["phrasetype"])) $_GET["phrasetype"] = "contains";
 if (!isset($_GET["contains"])) $_GET["contains"] = array(0=>"");
-if (!isset($_GET["sort"])) $_GET["sort"] = "glpi_networking.name";
+if (!isset($_GET["sort"])) $_GET["sort"] = 1;
 if (!isset($_GET["deleted"])) $_GET["deleted"] = "N";
 if (!isset($_GET["link"])) $_GET["link"] = "";
 
 
-searchFormNetworking();
+//searchFormNetworking();
 
-showNetworkingList($_SERVER["PHP_SELF"],$_SESSION["glpiname"],$_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
+//showNetworkingList($_SERVER["PHP_SELF"],$_SESSION["glpiname"],$_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
 
+showList(NETWORKING_TYPE,$_SERVER["PHP_SELF"],$_GET["field"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
 
 commonFooter();
 ?>
