a01714819a5c1832d8a908bde04b36b4bc5569d2
silvermat <silvermat>
2005-10-19 21:20:56 +0000

    Impl�mentation de la nouvelle fonction de recherche aux consomables

--- consumables/consumables-search.php
+++ consumables/consumables-search.php
@@ -50,8 +50,15 @@ if (!isset($_GET["link"])) $_GET["link"] = "";
 
 titleConsumable();
 
-searchFormConsumable($_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["deleted"],$_GET["link"]);
-showConsumableList($_SERVER["PHP_SELF"],$_SESSION["glpiname"],$_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
+include ($phproot . "/glpi/includes_search.php");
+
+searchForm(CONSUMABLE_TYPE,$cfg_install["root"]."/consumables/consumables-search.php",$_GET["field"],$_GET["contains"],$_GET["sort"],$_GET["deleted"],$_GET["link"]);
+showList(CONSUMABLE_TYPE,$_SERVER["PHP_SELF"],$_GET["field"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
+
+
+
+//searchFormConsumable($_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["deleted"],$_GET["link"]);
+//showConsumableList($_SERVER["PHP_SELF"],$_SESSION["glpiname"],$_GET["field"],$_GET["phrasetype"],$_GET["contains"],$_GET["sort"],$_GET["order"],$_GET["start"],$_GET["deleted"],$_GET["link"]);
 
 commonFooter();
 ?>
