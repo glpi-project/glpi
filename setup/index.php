f55f3b90d8893353d6f62c5bca257dc212eca914
silvermat <silvermat@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-10-21 13:02:12 +0000

    Mise en forme de la config des champs de recherche pas d�faut. + Correction d'occurences du dico
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1941 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- setup/index.php
+++ setup/index.php
@@ -58,18 +58,24 @@ echo "<tr class='tab_bg_1'><td  align='center'><a href=\"setup-dropdowns.php\"><
 echo "<tr class='tab_bg_1'><td align='center'><a href=\"".$HTMLRel."devices/\"><b>".$lang["setup"][222]."</b></a></td> </tr>";
 
 //echo "<tr class='tab_bg_1'><td  align='center'><a href=\"setup-templates.php\"><b>".$lang["setup"][1]."</b></a></td></tr>";
+echo "<tr class='tab_bg_1'><td align='center'><a href=\"setup-display.php\"><b>".$lang["setup"][250]."</b></a></td> </tr>";
 
-echo "<tr class='tab_bg_1'><td  align='center'><a href=\"setup-config.php?next=extsources\"><b>".$lang["setup"][67]."</b></a></td></tr>";
+echo "<tr class='tab_bg_1'><td align='center'><a href=\"".$HTMLRel."typedocs/\"><b>".$lang["document"][7]."</b></a></td> </tr>";
+
+
+
+
 
 echo "<tr class='tab_bg_1'><td  align='center'><a href=\"setup-config.php?next=mailing\"><b>".$lang["setup"][68]."</b></a></td></tr>";
 
 echo "<tr class='tab_bg_1'><td align='center'><a href=\"setup-config.php?next=confgen\"><b>".$lang["setup"][70]."</b></a></td> </tr>";
 
-echo "<tr class='tab_bg_1'><td align='center'><a href=\"".$HTMLRel."typedocs/\"><b>".$lang["document"][7]."</b></a></td> </tr>";
+echo "<tr class='tab_bg_1'><td  align='center'><a href=\"setup-config.php?next=extsources\"><b>".$lang["setup"][67]."</b></a></td></tr>";
+
 
 echo "<tr class='tab_bg_1'><td align='center'><a href=\"".$HTMLRel."links/\"><b>".$lang["setup"][87]."</b></a></td> </tr>";
 
-echo "<tr class='tab_bg_1'><td align='center'><a href=\"setup-display.php\"><b>".$lang["setup"][94]."</b></a></td> </tr>";
+
 
 
 echo "</table></div>";
