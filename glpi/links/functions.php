1bab88cc7a29172a04ccb9a9bedffec34515b6d1
silvermat <silvermat@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-10-21 22:45:31 +0000

    elargissement du champs pour saisie lien externe
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@1959 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- glpi/links/functions.php
+++ glpi/links/functions.php
@@ -312,7 +312,7 @@ function showLinkForm ($target,$ID) {
 	echo "</tr>";
 
 	echo "<tr class='tab_bg_1'><td>".$lang["links"][1].":	</td>";
-	echo "<td><input type='text' name='name' value=\"".$con->fields["name"]."\" size='30'></td>";
+	echo "<td><input type='text' name='name' value=\"".$con->fields["name"]."\" size='80'></td>";
 	echo "</tr>";
 
 	if ($ID=="") {
