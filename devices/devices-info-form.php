1fd99b53156c101b3cc7760a0d6226921e68d2fb
moyooo <moyo@indepnet.net>
2005-10-22 23:30:59 +0000

    Correction redirection devices-info-form.php
     Modified Files:
     	glpi/devices/devices-info-form.php

--- devices/devices-info-form.php
+++ devices/devices-info-form.php
@@ -46,7 +46,7 @@ if(isset($_GET)) $tab = $_GET;
 if(empty($tab) && isset($_POST)) $tab = $_POST;
 if(!isset($tab["ID"])) $tab["ID"] = "";
 if(empty($tab["device_type"])) {
-	 glpi_header($phproot . "/devices/");
+	 glpi_header($cfg_install['root'] . "/devices/");
 }
 
 if (isset($_SERVER["HTTP_REFERER"])) $REFERER=$_SERVER["HTTP_REFERER"];
