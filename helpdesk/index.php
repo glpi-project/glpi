f34b50296fe0afa05bbbba7884c2d8c49f0ac871
moyooo <moyo@indepnet.net>
2005-11-26 23:42:38 +0000

    Task 374: Compl�ter le helpdesk central
     Modified Files:
     	glpi/glpi/tracking/functions.php glpi/helpdesk/index.php
     	glpi/tracking/tracking-add-form.php

--- helpdesk/index.php
+++ helpdesk/index.php
@@ -41,12 +41,61 @@ include ($phproot . "/glpi/includes_printers.php");
 include ($phproot . "/glpi/includes_networking.php");
 include ($phproot . "/glpi/includes_peripherals.php");
 include ($phproot . "/glpi/includes_monitors.php");
+include ($phproot . "/glpi/includes_users.php");
 
 checkAuthentication("normal");
 
 commonHeader("Helpdesk",$_SERVER["PHP_SELF"]);
 
-printHelpDesk($_SESSION["glpiname"],0);
+if (!isset($_GET["user"])) $user=$_SESSION["glpiID"];
+else $user=$_GET["user"];
+if (!isset($_GET["assign"])) $assign=0;
+else $assign=$_GET["assign"];
+
+if(empty($_GET["isgroup"])) $_GET["isgroup"] = "";
+if(empty($_GET["status"])) $_GET["status"] = "new";
+if(empty($_GET["uemail"])) $_GET["uemail"] = "";
+if(empty($_GET["emailupdates"])) $_GET["emailupdates"] = "";
+$error = "";
+
+
+if (isset($_GET["priority"]) && empty($_GET["contents"]))
+{
+	$error=$lang["tracking"][8] ;
+	addFormTracking(0,0,$user,$assign,$_SERVER["PHP_SELF"],$error);
+}
+elseif (isset($_GET["priority"]) && !empty($_GET["contents"]))
+{
+	$uemail="";
+	if (isset($_GET["emailupdates"])&&$_GET["emailupdates"]=='yes'){
+		$u=new User;		
+		$u->getfromDB($_GET["user"]);
+		$uemail=$u->fields['email'];
+		}
+		
+	if (isset($_GET["hour"])&&isset($_GET["minute"]))
+	$realtime=$_GET["hour"]+$_GET["minute"]/60;
+
+	if (!isset($_GET["computer"])||$_GET["computer"]==0){
+		$_GET["device_type"]==0;
+		$_GET["computer"]=0;
+		}
+
+	if (postJob($_GET["device_type"],$_GET["computer"],$_GET["user"],$_GET["status"],$_GET["priority"],$_GET["isgroup"],$uemail,$_GET["emailupdates"],$_GET["contents"],$_GET["assign"],$realtime))
+	{
+		$error=$lang["tracking"][9];
+		addFormTracking(0,0,$user,$assign,$_SERVER["PHP_SELF"],$error);
+	}
+	else
+	{
+		$error=$lang["tracking"][10];
+		addFormTracking(0,0,$user,$assign,$_SERVER["PHP_SELF"],$error);
+	}
+} 
+else
+{
+	addFormTracking(0,0,$user,$assign,$_SERVER["PHP_SELF"],$error);
+}
 
 commonFooter();
 
