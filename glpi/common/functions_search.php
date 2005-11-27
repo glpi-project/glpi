d1c15df42a0efd95756fa31e104f8481a43719af
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-11-27 00:59:47 +0000

    Correction effacement commit JMD
     Modified Files:
     	glpi/glpi/common/functions_search.php
     	glpi/glpi/dicts/french.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@2096 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- glpi/common/functions_search.php
+++ glpi/common/functions_search.php
@@ -174,16 +174,16 @@ function searchForm($type,$target,$field="",$contains="",$sort= "",$deleted= "",
 	echo "<table>";
 	if (in_array($LINK_ID_TABLE[$type],$deleted_tables)){
 		echo "<tr><td><select name='deleted'>";
-		echo "<option value='Y' ".($deleted=='Y'?" selected ":"").">".$lang["buttons"][27]."</option>";
-		echo "<option value='N' ".($deleted=='N'?" selected ":"").">".$lang["buttons"][28]."</option>";
+		echo "<option value='Y' ".($deleted=='Y'?" selected ":"").">".$lang["choice"][0]."</option>";
+		echo "<option value='N' ".($deleted=='N'?" selected ":"").">".$lang["choice"][1]."</option>";
 		echo "</select>";
 		echo "<img src=\"".$HTMLRel."pics/showdeleted.png\" alt='".$lang["common"][3]."' title='".$lang["common"][3]."'>";
 		echo "</td></tr>";
 	}
 	
 	echo "<tr><td><select name='distinct'>";
-	echo "<option value='Y' ".($distinct=='Y'?" selected ":"").">".$lang["buttons"][27]."</option>";
-	echo "<option value='N' ".($distinct=='N'?" selected ":"").">".$lang["buttons"][28]."</option>";
+	echo "<option value='Y' ".($distinct=='Y'?" selected ":"").">".$lang["choice"][0]."</option>";
+	echo "<option value='N' ".($distinct=='N'?" selected ":"").">".$lang["choice"][1]."</option>";
 	echo "</select>";
 	echo "<img src=\"".$HTMLRel."pics/doublons.png\" alt='".$lang["common"][12]."' title='".$lang["common"][12]."'>";
 	echo "</td></tr></table>";
