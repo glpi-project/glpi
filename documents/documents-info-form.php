4be436913b229dc8b416257ca56dd78c1bc6d744
silvermat <silvermat>
2005-12-22 02:02:23 +0000

    Correction bug : les onglets ne s'affichent pas dans l'inforform des documents :
    - Comment� $ci=new CommonItem();  qui a �t� rajout� r�cemment.
    
    La classe commonitem ne g�re pas les documents...
    
    A voir si on l'�tend ou pas ?

--- documents/documents-info-form.php
+++ documents/documents-info-form.php
@@ -130,8 +130,9 @@ else
 	}
 
 	commonHeader($lang["title"][25],$_SERVER["PHP_SELF"]);
-	$ci=new CommonItem();
-	if ($ci->getFromDB(DOCUMENT_TYPE,$tab["ID"]))
+	// ???
+	//$ci=new CommonItem();
+	//if ($ci->getFromDB(DOCUMENT_TYPE,$tab["ID"]))
 	showDocumentOnglets($_SERVER["PHP_SELF"]."?ID=".$tab["ID"], "",$_SESSION['glpi_onglet'] );
 
 	showDocumentForm($_SERVER["PHP_SELF"],$tab["ID"]);
