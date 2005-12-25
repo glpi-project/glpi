daa1bdc14304c388f06491fc117bbc9a96d22d56
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-12-25 22:20:15 +0000

    Bug rapport reseau
    http://glpi.indepnet.org/forum/viewtopic.php?pid=7925#p7925
    
     Modified Files:
     	glpi/reports/reports/reseau.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@2194 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- reports/reports/reseau.php
+++ reports/reports/reseau.php
@@ -49,7 +49,7 @@ commonHeader($lang["Menu"][6],$_SERVER["PHP_SELF"]);
 echo "<div align='center'>";
 echo "<table class='tab_cadre' >";
 echo "<tr><th align='center' colspan='3' >".$lang["reports"][33]."</th></tr>";
-
+echo "</table>";
 // 3. Selection d'affichage pour generer la liste
 
 
@@ -63,36 +63,43 @@ $report_list["prise"]["name"] = $lang["reports"][37];
 $report_list["prise"]["file"] = "parPrises.php";
 
 echo "<form name='form' method='post' action='parLieu-list.php'>";
+echo "<table class='tab_cadre'  width='400'>";
 echo "<tr class='tab_bg_1'><td>".$lang["reports"][39]."</td>";
 echo "<td>";
 dropdownvalue("glpi_dropdown_locations","location","");
-echo "</td><td>";
+echo "</td><td align='center' width='120'>";
 echo "<input type='submit' value='".$lang["reports"][15]."' class='submit'>";
-echo "</td></tr></form>";
+	echo "</td></tr>";
+	echo "</table>";
+	echo "</form>";
 
 
-echo "<form name='form' method='post' action='parSwitch-list.php'>";
+echo "<form name='form2' method='post' action='parSwitch-list.php'>";
+echo "<table class='tab_cadre' width='400'>";
 echo "<tr class='tab_bg_1'><td>".$lang["reports"][41]."</td>";
 echo "<td>";
 dropdownValue("glpi_networking", "switch", "");
-echo "</td><td>";
+echo "</td><td align='center' width='120'>";
 echo "<input type='submit' value='".$lang["reports"][15]."' class='submit'>";
-echo "</td></tr></form>";
+	echo "</td></tr>";
+	echo "</table>";
+	echo "</form>";
 
 
 if (countElementsInTable("glpi_dropdown_netpoint")>0){
-	echo "<form name='form' method='post' action='parPrises-list.php'>";
+	echo "<form name='form3' method='post' action='parPrises-list.php'>";
+	echo "<table class='tab_cadre'  width='400'>";
 	echo "<tr class='tab_bg_1'><td>".$lang["reports"][42]."</td>";
 	echo "<td>";
 	dropdownValue("glpi_dropdown_netpoint", "prise", "");
-	echo "</td><td>";
+	echo "</td><td align='center' width='120'>";
 	echo "<input type='submit' value='".$lang["reports"][15]."' class='submit'>";
-	echo "</td></tr></form>";
+	echo "</td></tr>";
+	echo "</table>";
+	echo "</form>";
 }
-echo "</table>";
 echo "</div>";
 
-echo "</form>";
 
 
 
