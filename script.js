7e34a9a3bd89795c2ba3b37dc5a07f073ee47f2d
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2005-12-29 02:54:38 +0000

    Hide et View des select pour bug avec IE
     Modified Files:
     	glpi/script.js
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@2231 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ window.onload=montre;
 function montre(id) {
 var d = document.getElementById(id);
 	for (var i = 1; i<=10; i++) {
-		if (document.getElementById('smenu'+i)) {document.getElementById('smenu'+i).style.display='none';
+		if (document.getElementById('smenu'+i)) {document.getElementById('smenu'+i).style.display='none';displaySelectBoxes();
 	}
 	}
-if (d) {d.style.display='block'; }
+if (d) {d.style.display='block'; hideSelectBoxes();}
 }
 
 /**
 * Hides all drop down form select boxes on the screen so they do not appear above the mask layer.
 * IE has a problem with wanted select form tags to always be the topmost z-index or layer
-
-function displaySelectBoxes(){
+*/
+function hideSelectBoxes(){
 for(var i = 0; i < document.forms.length; i++) {
 for(var e = 0; e < document.forms[i].length; e++){
 if(document.forms[i].elements[e].tagName == "SELECT") {
@@ -21,10 +21,11 @@ document.forms[i].elements[e].style.visibility="hidden";
 }
 }
 }
-*/
+
 /**
 * Makes all drop down form select boxes on the screen visible so they do not reappear after the dialog is closed.
 * IE has a problem with wanted select form tags to always be the topmost z-index or layer
+*/
 
 function displaySelectBoxes() {
 for(var i = 0; i < document.forms.length; i++) {
@@ -35,7 +36,6 @@ document.forms[i].elements[e].style.visibility="visible";
 }
 }
 }
-*/
 
 function jumpTo(URL_List){ var URL = URL_List.options[URL_List.selectedIndex].value;  window.location.href = URL; }
 
