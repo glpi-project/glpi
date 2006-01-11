5731f04f64198ddea28273af75ac8cacbc4fb3be
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-01-11 18:48:58 +0000

    Gestion des " dans les textfields
     Modified Files:
     	glpi/glpi/common/functions_dropdown.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@2373 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- glpi/common/functions_dropdown.php
+++ glpi/common/functions_dropdown.php
@@ -777,13 +777,13 @@ function autocompletionTextField($myname,$table,$field,$value='',$size=20,$optio
 
 	if ($cfg_features["use_ajax"]&&$cfg_features["ajax_autocompletion"]){
 		$rand=mt_rand();
-		echo "<input $option id='textfield_$myname$rand' type='text' name='$myname' value=\"".$value."\" size='$size'>\n";
+		echo "<input $option id='textfield_$myname$rand' type='text' name='$myname' value=\"".ereg_replace("\"","''",$value)."\" size='$size'>\n";
 		echo "<div id='textfieldupdate_$myname$rand' style='display:none;border:1px solid black;background-color:white;'></div>\n";
 		echo "<script type='text/javascript' language='javascript' charset='utf-8'>";
 	    echo "new Ajax.Autocompleter('textfield_$myname$rand','textfieldupdate_$myname$rand','".$HTMLRel."/ajax/autocompletion.php',{parameters:'table=$table&field=$field&myname=$myname'});";
 		echo "</script>";
 	}	else {
-		echo "<input $option type='text' name='$myname' value=\"".$value."\" size='$size'>\n";
+		echo "<input $option type='text' name='$myname' value=\"".ereg_replace("\"","''",$value)."\" size='$size'>\n";
 	}
 }
 
