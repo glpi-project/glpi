08c1a5a2bda5aab82de190f4032ad9d145982eec
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-01-12 12:50:36 +0000

    AJout element dans suivi par mail
     Modified Files:
     	glpi/glpi/dicts/french.php glpi/glpi/tracking/functions.php
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@2377 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- glpi/dicts/french.php
+++ glpi/dicts/french.php
@@ -769,6 +769,8 @@ $lang["mailing"][24]="Statut :";
 $lang["mailing"][25]="Planification :";
 $lang["mailing"][26]="Catégorie :";
 $lang["mailing"][27]="Changement de statut du ticket";
+$lang["mailing"][101]="Le suivi par email a été activé";
+$lang["mailing"][102]="Le suivi par email a été désactivé";
 
 $lang["Menu"][0]="Ordinateurs";
 $lang["Menu"][1]="Réseaux";
