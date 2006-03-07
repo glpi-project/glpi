30be29d120244d9dd8f42aefcabe93bafeeb527f
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-03-07 20:41:15 +0000

    Correction bug + update scriptaculous
     Modified Files:
     	glpi/glpi/tracking/functions.php
     	glpi/scriptaculous/controls.js glpi/scriptaculous/dragdrop.js
     	glpi/scriptaculous/effects.js
     	glpi/scriptaculous/scriptaculous.js
     	glpi/scriptaculous/slider.js glpi/scriptaculous/unittest.js
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@2846 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- scriptaculous/dragdrop.js
+++ scriptaculous/dragdrop.js
@@ -128,7 +128,7 @@ var Draggables = {
     this.activeDraggable = draggable;
   },
   
-  deactivate: function(draggbale) {
+  deactivate: function() {
     this.activeDraggable = null;
   },
   
@@ -199,6 +199,9 @@ Draggable.prototype = {
       },
       zindex: 1000,
       revert: false,
+      scroll: false,
+      scrollSensitivity: 20,
+      scrollSpeed: 15,
       snap: false   // false, or xy or [x,y] or function(x,y){ return [x,y] }
     }, arguments[1] || {});
 
@@ -208,6 +211,8 @@ Draggable.prototype = {
       this.handle = Element.childrenWithClassName(this.element, options.handle)[0];  
     if(!this.handle) this.handle = $(options.handle);
     if(!this.handle) this.handle = this.element;
+    
+    if(options.scroll) options.scroll = $(options.scroll);
 
     Element.makePositioned(this.element); // fix IE    
 
@@ -239,6 +244,7 @@ Draggable.prototype = {
       if(src.tagName && (
         src.tagName=='INPUT' ||
         src.tagName=='SELECT' ||
+        src.tagName=='OPTION' ||
         src.tagName=='BUTTON' ||
         src.tagName=='TEXTAREA')) return;
         
@@ -270,6 +276,11 @@ Draggable.prototype = {
       this.element.parentNode.insertBefore(this._clone, this.element);
     }
     
+    if(this.options.scroll) {
+      this.originalScrollLeft = this.options.scroll.scrollLeft;
+      this.originalScrollTop = this.options.scroll.scrollTop;
+    }
+    
     Draggables.notify('onStart', this, event);
     if(this.options.starteffect) this.options.starteffect(this.element);
   },
@@ -282,8 +293,25 @@ Draggable.prototype = {
     this.draw(pointer);
     if(this.options.change) this.options.change(this);
     
+    if(this.options.scroll) {
+      //if(this.scrollInterval) this.scroll();
+      this.stopScrolling();
+      var p = Position.page(this.options.scroll);
+      p[0] += this.options.scroll.scrollLeft;
+      p[1] += this.options.scroll.scrollTop;
+      p.push(p[0]+this.options.scroll.offsetWidth);
+      p.push(p[1]+this.options.scroll.offsetHeight);
+      var speed = [0,0];
+      if(pointer[0] < (p[0]+this.options.scrollSensitivity)) speed[0] = pointer[0]-(p[0]+this.options.scrollSensitivity);
+      if(pointer[1] < (p[1]+this.options.scrollSensitivity)) speed[1] = pointer[1]-(p[1]+this.options.scrollSensitivity);
+      if(pointer[0] > (p[2]-this.options.scrollSensitivity)) speed[0] = pointer[0]-(p[2]-this.options.scrollSensitivity);
+      if(pointer[1] > (p[3]-this.options.scrollSensitivity)) speed[1] = pointer[1]-(p[3]-this.options.scrollSensitivity);
+      this.startScrolling(speed);
+    }
+    
     // fix AppleWebKit rendering
     if(navigator.appVersion.indexOf('AppleWebKit')>0) window.scrollBy(0,0);
+    
     Event.stop(event);
   },
   
@@ -321,13 +349,14 @@ Draggable.prototype = {
   },
   
   keyPress: function(event) {
-    if(!event.keyCode==Event.KEY_ESC) return;
+    if(event.keyCode!=Event.KEY_ESC) return;
     this.finishDrag(event, false);
     Event.stop(event);
   },
   
   endDrag: function(event) {
     if(!this.dragging) return;
+    this.stopScrolling();
     this.finishDrag(event, true);
     Event.stop(event);
   },
@@ -337,7 +366,14 @@ Draggable.prototype = {
     var d = this.currentDelta();
     pos[0] -= d[0]; pos[1] -= d[1];
     
-    var p = [0,1].map(function(i){ return (point[i]-pos[i]-this.offset[i]) }.bind(this));
+    if(this.options.scroll) {
+      pos[0] -= this.options.scroll.scrollLeft-this.originalScrollLeft;
+      pos[1] -= this.options.scroll.scrollTop-this.originalScrollTop;
+    }
+    
+    var p = [0,1].map(function(i){ 
+      return (point[i]-pos[i]-this.offset[i]) 
+    }.bind(this));
     
     if(this.options.snap) {
       if(typeof this.options.snap == 'function') {
@@ -358,6 +394,34 @@ Draggable.prototype = {
     if((!this.options.constraint) || (this.options.constraint=='vertical'))
       style.top  = p[1] + "px";
     if(style.visibility=="hidden") style.visibility = ""; // fix gecko rendering
+  },
+  
+  stopScrolling: function() {
+    if(this.scrollInterval) {
+      clearInterval(this.scrollInterval);
+      this.scrollInterval = null;
+    }
+  },
+  
+  startScrolling: function(speed) {
+    this.scrollSpeed = [speed[0]*this.options.scrollSpeed,speed[1]*this.options.scrollSpeed];
+    this.lastScrolled = new Date();
+    this.scrollInterval = setInterval(this.scroll.bind(this), 10);
+  },
+  
+  scroll: function() {
+    var current = new Date();
+    var delta = current - this.lastScrolled;
+    this.lastScrolled = current;
+    this.options.scroll.scrollLeft += this.scrollSpeed[0] * delta / 1000;
+    this.options.scroll.scrollTop  += this.scrollSpeed[1] * delta / 1000;
+    
+    Position.prepare();
+    Droppables.show(Draggables._lastPointer, this.element);
+    Draggables.notify('onDrag', this);
+    this.draw(Draggables._lastPointer);    
+    
+    if(this.options.change) this.options.change(this);
   }
 }
 
@@ -414,7 +478,8 @@ var Sortable = {
       only:        false,
       hoverclass:  null,
       ghosting:    false,
-      format:      null,
+      scroll:      false,
+      format:      /^[^_]*_(.*)$/,
       onChange:    Prototype.emptyFunction,
       onUpdate:    Prototype.emptyFunction
     }, arguments[1] || {});
@@ -425,6 +490,7 @@ var Sortable = {
     // build options for the draggables
     var options_for_draggable = {
       revert:      true,
+      scroll:      options.scroll,
       ghosting:    options.ghosting,
       constraint:  options.constraint,
       handle:      options.handle };
@@ -568,18 +634,41 @@ var Sortable = {
     Element.show(Sortable._marker);
   },
 
+  sequence: function(element) {
+    element = $(element);
+    var options = Object.extend(this.options(element), arguments[1] || {});
+    
+    return $(this.findElements(element, options) || []).map( function(item) {
+      return item.id.match(options.format) ? item.id.match(options.format)[1] : '';
+    });
+  },
+
+  setSequence: function(element, new_sequence) {
+    element = $(element);
+    var options = Object.extend(this.options(element), arguments[2] || {});
+    
+    var nodeMap = {};
+    this.findElements(element, options).each( function(n) {
+        if (n.id.match(options.format))
+            nodeMap[n.id.match(options.format)[1]] = [n, n.parentNode];
+        n.parentNode.removeChild(n);
+    });
+   
+    new_sequence.each(function(ident) {
+        var n = nodeMap[ident];
+        if (n) {
+            n[1].appendChild(n[0]);
+            delete nodeMap[ident];
+        }
+    });
+  },
+
   serialize: function(element) {
     element = $(element);
-    var sortableOptions = this.options(element);
-    var options = Object.extend({
-      tag:  sortableOptions.tag,
-      only: sortableOptions.only,
-      name: element.id,
-      format: sortableOptions.format || /^[^_]*_(.*)$/
-    }, arguments[1] || {});
-    return $(this.findElements(element, options) || []).map( function(item) {
-      return (encodeURIComponent(options.name) + "[]=" + 
-              encodeURIComponent(item.id.match(options.format) ? item.id.match(options.format)[1] : ''));
-    }).join("&");
+    var name = encodeURIComponent(
+      (arguments[1] && arguments[1].name) ? arguments[1].name : element.id);
+    return Sortable.sequence(element, arguments[1]).map( function(item) {
+      return name + "[]=" + encodeURIComponent(item);
+    }).join('&');
   }
 }
