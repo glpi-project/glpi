67eb975cb1f847dddacec4232fbea42ca442be79
moyooo <moyo@indepnet.net>
2006-04-02 14:46:21 +0000

    Fichier manquant nouvelle archi de plugins
     Added Files:
     	glpi/setup/setup-plugins.php

---
+++ setup/setup-plugins.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * @version $Id$
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2006 by the INDEPNET Development Team.
+ 
+ http://indepnet.net/   http://glpi.indepnet.org
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// ----------------------------------------------------------------------
+// Original Author of file:
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+include ("_relpos.php");
+include ($phproot . "/glpi/includes.php");
+include ($phproot . "/glpi/includes_setup.php");
+
+checkauthentication("super-admin");
+
+
+commonHeader($lang["title"][2],$_SERVER["PHP_SELF"]);
+	echo "<div align='center'>";
+    if (isset($plugin_hooks["config_page"]) && is_array($plugin_hooks["config_page"])) {
+	foreach ($plugin_hooks["config_page"] as $plug => $page){
+		$function="plugin_version_$plug";
+		$version=$function();
+		echo "<a href='".$HTMLRel."plugins/$plug/$page'>".$version["name"]."</a><br>";
+	}
+    }
+	echo "</div>";
+
+commonFooter();
+?>
