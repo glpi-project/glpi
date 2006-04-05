1123f23469af2c35b233880c7d6a7fc9b6989625
moyooo <moyo@indepnet.net>
2006-04-05 05:28:10 +0000

    Intégration CommonDBTM dans cartouches et consommables

--- glpi/consumables/classes.php
+++ glpi/consumables/classes.php
@@ -42,250 +42,45 @@ include ("_relpos.php");
 	\see Consumable
 	\author Julien Dombre
 */
-class ConsumableType {
+class ConsumableType extends CommonDBTM {
 
-	//! Fields of ConsumableType
-	/**
-	Fields are :
-	- ID 
-  	- name : its name
-  	- ref : its reference
-  	- type : 
-  	- FK_glpi_enterprise : FK to the manufacturer
-  	- deleted : enum('Y','N') NOT NULL default 'N',
-  	- comments : some comments
-	*/
-	var $fields	= array();
-	//! Fields tu update
-	var $updates	= array();
-	
-   //! Get the item from table glpi_consumable_type from the database
-    /*!
-      \param ID ID of the CartridgeType.
-      \return Is the item correctly loaded
-    */
-	function getfromDB ($ID) {
+	function ConsumableType () {
+		$this->table="glpi_consumables_type";
+		$this->type=CONSUMABLE_TYPE;
+	}
 
-		// Make new database object and fill variables
+	function cleanDBonPurge($ID) {
 		global $db;
-		$query = "SELECT * FROM glpi_consumables_type WHERE (ID = '$ID')";
-		
-		if ($result = $db->query($query)) {
-		if ($db->numrows($result)==1){
-			$data = $db->fetch_array($result);
-			foreach ($data as $key => $val) {
-				$this->fields[$key] = $val;
-			}
-			return true;
-		} else return false;
-		} else {
-			return false;
-		}
-	}
-	
-	function getEmpty () {
-	global $db,$cfg_glpi;
-	
-	$fields = $db->list_fields("glpi_consumables_type");
-	$columns = $db->num_fields($fields);
-		for ($i = 0; $i < $columns; $i++) {
-			$name = $db->field_name($fields, $i);
-			$this->fields[$name] = "";
-		}
-		
-	$this->fields["alarm"]=$cfg_glpi["cartridges_alarm"];
-	return true;
+		// Delete cartridconsumablesges
+		$query = "DELETE FROM glpi_consumables WHERE (FK_glpi_consumables_type = '$ID')";
+		$db->query($query);
 	}
 
-	function countConsumables() {
-		global $db;
-		$query = "SELECT * FROM glpi_consumables WHERE (FK_glpi_consumables_type = '".$this->fields["ID"]."')";
-		if ($result = $db->query($query)) {
-			$number = $db->numrows($result);
-			return $number;
-		} else {
-			return false;
-		}
-	}
-	function restoreInDB($ID) {
-		global $db;
-		$query = "UPDATE glpi_consumables_type SET deleted='N' WHERE (ID = '$ID')";
-		if ($result = $db->query($query)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	function updateInDB($updates)  {
-
-		global $db;
-
-		for ($i=0; $i < count($updates); $i++) {
-			$query  = "UPDATE glpi_consumables_type SET ";
-			$query .= $updates[$i];
-			$query .= "='";
-			$query .= $this->fields[$updates[$i]];
-			$query .= "' WHERE ID='";
-			$query .= $this->fields["ID"];	
-			$query .= "'";
-			$result=$db->query($query);
-		}
-		
-	}
-	
-	function addToDB() {
-		
-		global $db;
-
-		// Build query
-		$query = "INSERT INTO glpi_consumables_type (";
-		$i=0;
-		foreach ($this->fields as $key => $val) {
-			$fields[$i] = $key;
-			$values[$i] = $val;
-			$i++;
-		}		
-		for ($i=0; $i < count($fields); $i++) {
-			$query .= $fields[$i];
-			if ($i!=count($fields)-1) {
-				$query .= ",";
-			}
-		}
-		$query .= ") VALUES (";
-		for ($i=0; $i < count($values); $i++) {
-			$query .= "'".$values[$i]."'";
-			if ($i!=count($values)-1) {
-				$query .= ",";
-			}
-		}
-		$query .= ")";
-
-		$result=$db->query($query);
-		return $db->insert_id();
-
+	function post_getEmpty () {
+		global $cfg_glpi;
+		$this->fields["alarm"]=$cfg_glpi["cartridges_alarm"];
 	}
 
-	function deleteFromDB($ID,$force=0) {
-
-		global $db;
-		$this->getFromDB($ID);		
-		if ($force==1/*||$this->countCartridges()==0*/){
-			$query = "DELETE from glpi_consumables_type WHERE ID = '$ID'";
-			if ($result = $db->query($query)) {
-				// Delete consumables
-				if ($force==1){
-				$query3 = "DELETE FROM glpi_consumables WHERE (FK_glpi_consumables_type = \"$ID\")";
-				$result3 = $db->query($query3);
-				} 
-			} else {
-				return false;
-			}
-		} else {
-		$query = "UPDATE glpi_consumables_type SET deleted='Y' WHERE ID = '$ID'";		
-		return ($result = $db->query($query));
-		}
-	}
-	
 }
+
 //!  Consumable Class
 /**
   This class is used to manage the consumables.
   \see ConsumableType
   \author Julien Dombre
 */
+class Consumable extends CommonDBTM {
 
-class Consumable {
-
-
-	var $fields	= array();
-	var $updates	= array();
-	
-	function getfromDB ($ID) {
-
-		// Make new database object and fill variables
-		global $db;
-		$query = "SELECT * FROM glpi_consumables WHERE (ID = '$ID')";
-		if ($result = $db->query($query)) {
-		if ($db->numrows($result)==1){
-			$data = $db->fetch_array($result);
-			foreach ($data as $key => $val) {
-				$this->fields[$key] = $val;
-			}
-			return true;
-		} else return false;
-		} else {
-			return false;
-		}
-	}
-
-
-	function updateInDB($updates)  {
-
-		global $db;
-
-		for ($i=0; $i < count($updates); $i++) {
-			$query  = "UPDATE glpi_consumables SET ";
-			$query .= $updates[$i];
-			$query .= "='";
-			$query .= $this->fields[$updates[$i]];
-			$query .= "' WHERE ID='";
-			$query .= $this->fields["ID"];	
-			$query .= "'";
-			
-			$result=$db->query($query);
-		}
-		
+	function Consumable () {
+		$this->table="glpi_consumables";
+		$this->type=CONSUMABLE_ITEM_TYPE;
 	}
 	
-	function addToDB() {
-		
+
+	function cleanDBonPurge($ID) {
 		global $db;
-
-		// Build query
-		$query = "INSERT INTO glpi_consumables (";
-		$i=0;
-		foreach ($this->fields as $key => $val) 
-		if (!is_integer($key)){
-			$fields[$i] = $key;
-			$values[$i] = $val;
-			$i++;
-		}		
-		for ($i=0; $i < count($fields); $i++) {
-			$query .= $fields[$i];
-			if ($i!=count($fields)-1) {
-				$query .= ",";
-			}
-		}
-		$query .= ") VALUES (";
-		for ($i=0; $i < count($values); $i++) {
-			$query .= "'".$values[$i]."'";
-			if ($i!=count($values)-1) {
-				$query .= ",";
-			}
-		}
-		$query .= ")";
-
-		$result=$db->query($query);
-		return $db->insert_id();
-
-	}
-
-
-	function deleteFromDB($ID) {
-
-		global $db;
-
-		$query = "DELETE from glpi_consumables WHERE ID = '$ID'";
-		if ($result = $db->query($query)) {
-
-				$query = "DELETE FROM glpi_infocoms WHERE (FK_device = '$ID' AND device_type='".CONSUMABLE_ITEM_TYPE."')";
-				$result = $db->query($query);
-
-				return true;
-		} else {
-			return false;
-		}
+		$query = "DELETE FROM glpi_infocoms WHERE (FK_device = '$ID' AND device_type='".CONSUMABLE_ITEM_TYPE."')";
+		$result = $db->query($query);
 	}
 
 }
