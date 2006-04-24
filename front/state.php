72473bea6f5efabedf3f8d92c9e55d4874c995d2
silvermat <silvermat>
2006-04-24 03:10:58 +0000

    correction suite nouvelle archi

--- front/state.php
+++ front/state.php
@@ -35,7 +35,7 @@
 
 include ("_relpos.php");
 
-$NEEDED_ITEMS=array("user","computer","printer","monitor","periphal","networking","phone");
+$NEEDED_ITEMS=array("state","user","computer","printer","monitor","periphal","networking","phone");
 include ($phproot . "/inc/includes.php");
 
 if(isset($_GET)) $tab = $_GET;
