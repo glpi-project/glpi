769ebbe35f9f122d7bcf6034c55e198f2afb58cf
silvermat <silvermat@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-04-28 13:00:05 +0000

    correction ortho
    git-svn-id: https://forge.indepnet.net/svn/glpi/glpi/trunk@3402 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/dbmysql.class.php
+++ inc/dbmysql.class.php
@@ -61,7 +61,7 @@ class DBmysql {
 		mysql_select_db($this->dbdefault) or $this->error = 1;
 		else {
 			nullHeader("Mysql Error",$_SERVER['PHP_SELF']);
-			echo "<div align='center'><p><strong>A link to the Mysql server could not be established. Please Check your configuration.</strong></p><p><strong>Le serveur Mysql est inacessible. V&eacute;rifiez votre configuration</strong></p></div>";
+			echo "<div align='center'><p><strong>A link to the Mysql server could not be established. Please Check your configuration.</strong></p><p><strong>Le serveur Mysql est inaccessible. V&eacute;rifiez votre configuration</strong></p></div>";
 			nullFooter("Mysql Error",$_SERVER['PHP_SELF']);
 			die();
 		}
