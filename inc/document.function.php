7e496ba29bc3dd0a3dd96b2de1241fec7ad85277
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-06-21 16:46:45 +0000

    Correction droits accès
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@3605 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/document.function.php
+++ inc/document.function.php
@@ -265,6 +265,7 @@ $result = $db->query($query);
 function showDocumentAssociated($device_type,$ID,$withtemplate=''){
 
 	global $db,$cfg_glpi, $lang,$HTMLRel;
+	if (!haveRight("document","r")) return false;
 	if (!haveTypeRight($device_type,"r")&&$device_type!=KNOWBASE_TYPE)	return false;
 	$canread=haveTypeRight($device_type,"r");
 	$canedit=haveTypeRight($device_type,"w");
