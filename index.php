bdd574fb3f14401dfaca4e41fd5c5d8e0bcd96f4
moyooo <moyo@indepnet.net>
2006-08-19 04:19:36 +0000

    test commit

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 * @version $Id$
  ----------------------------------------------------------------------
  GLPI - Gestionnaire Libre de Parc Informatique
+
  Copyright (C) 2003-2006 by the INDEPNET Development Team.
  
  http://indepnet.net/   http://glpi-project.org
