6a17c1aaa8aef5dec757fe1c02571ab023181278
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-08-22 03:57:36 +0000

    clean headers
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@3795 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/contact.vcard.php
+++ front/contact.vcard.php
@@ -1,31 +1,31 @@
 <?php
 /*
-	* @version $Id: HEADER 3576 2006-06-12 08:40:44Z moyo $
-	-------------------------------------------------------------------------
-	GLPI - Gestionnaire Libre de Parc Informatique
-	Copyright (C) 2003-2006 by the INDEPNET Development Team.
+ * @version $Id: HEADER 3794 2006-08-22 03:47:26Z moyo $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2006 by the INDEPNET Development Team.
 
-	http://indepnet.net/   http://glpi-project.org
-	-------------------------------------------------------------------------
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
 
-	LICENSE
+ LICENSE
 
-	This file is part of GLPI.
+ This file is part of GLPI.
 
-	GLPI is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-	GLPI is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with GLPI; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-	--------------------------------------------------------------------------
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
 */
 // ----------------------------------------------------------------------
 // Original Author of file: Jean-Mathieu Doléans
