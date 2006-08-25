92331470bd797be4900dd58f16d38409a82f96b7
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-08-25 20:47:06 +0000

    Recherche par status pour tracking simple
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@3828 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/helpdesk.public.php
+++ front/helpdesk.public.php
@@ -67,7 +67,9 @@ if (isset($_GET["show"]) && strcmp($_GET["show"],"user") == 0)
 
 	if (!isset($_GET["ID"])) {
 		if (!isset($_GET["start"])) $_GET["start"]=0;
-		showTrackingList($_SERVER["PHP_SELF"],$_GET["start"],"","DESC","all",$_SESSION["glpiID"],-1);
+		if (!isset($_GET["status"])) $_GET["status"]="all";
+		searchSimpleFormTracking($_SERVER["PHP_SELF"],$_GET["status"]);
+		showTrackingList($_SERVER["PHP_SELF"],$_GET["start"],"","DESC",$_GET["status"],$_SESSION["glpiID"],-1);
 	}
 	else {
 		if (isset($_POST["update"])){
