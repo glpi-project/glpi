8354db3d70f101b0a7a1657a94ebdfd1331526c2
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-10-01 17:31:37 +0000

    clean code + update scripaculous
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@3928 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/display.function.php
+++ inc/display.function.php
@@ -154,7 +154,7 @@ function commonHeader($title,$url)
 	echo "<link rel='stylesheet' type='text/css' media='print' href='".$HTMLRel."css/print.css' >";
 	echo "<link rel='shortcut icon' type='images/x-icon' href='".$HTMLRel."pics/favicon.ico' >";
 	// AJAX library
-	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/prototype.js'></script>";
+	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/prototype.js'></script>";
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/scriptaculous.js'></script>";
 	// Some Javascript-Functions which we may need later
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."script.js'></script>";
@@ -385,7 +385,7 @@ function helpHeader($title,$url) {
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."script.js'></script>";
 
 	// AJAX library
-	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/prototype.js'></script>";
+	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/prototype.js'></script>";
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/scriptaculous.js'></script>";
 
 
@@ -540,7 +540,7 @@ function nullHeader($title,$url) {
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."script.js'></script>";
 
 	// AJAX library
-	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/prototype.js'></script>";
+	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/prototype.js'></script>";
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/scriptaculous.js'></script>";
 
 	// Appel CSS
@@ -621,7 +621,7 @@ function popHeader($title,$url)
 	echo "<link rel='stylesheet' type='text/css' media='print' href='".$HTMLRel."css/print.css' >";
 	echo "<link rel='shortcut icon' type='images/x-icon' href='".$HTMLRel."pics/favicon.ico' >";
 	// AJAX library
-	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/prototype.js'></script>";
+	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/prototype.js'></script>";
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."lib/scriptaculous/scriptaculous.js'></script>";
 	// Some Javascript-Functions which we may need later
 	echo "<script type=\"text/javascript\" src='".$HTMLRel."script.js'></script>";
