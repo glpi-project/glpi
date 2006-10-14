8e7d47ecc5e3012ef64a531dbbdeaf1ffe5351af
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-10-14 21:31:16 +0000

    Corrections effets collateraux
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@3980 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update.php
+++ install/update.php
@@ -37,12 +37,12 @@
 if (!defined('GLPI_ROOT'))
 	define('GLPI_ROOT', '..');
 
-include (GLPI_ROOT . "/inc/dbmysql.class.php");
-include (GLPI_ROOT . "/inc/common.function.php");
-include (GLPI_ROOT . "/inc/db.function.php");
-include (GLPI_ROOT . "/inc/display.function.php");
-include (GLPI_ROOT . "/config/based_config.php");
-include (GLPI_CONFIG_DIR . "/config_db.php");
+include_once (GLPI_ROOT . "/inc/dbmysql.class.php");
+include_once (GLPI_ROOT . "/inc/common.function.php");
+include_once (GLPI_ROOT . "/inc/db.function.php");
+include_once (GLPI_ROOT . "/inc/display.function.php");
+include_once (GLPI_ROOT . "/config/based_config.php");
+include_once (GLPI_CONFIG_DIR . "/config_db.php");
 
 session_save_path(GLPI_DOC_DIR."/_sessions");
 
