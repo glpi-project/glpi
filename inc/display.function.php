ed119fa84f393293454fbdaaa0128b22d2aeb92c
moyooo <moyo@indepnet.net>
2006-10-17 17:59:54 +0000

    Correction fonction titleDisplay

--- inc/display.function.php
+++ inc/display.function.php
@@ -56,15 +56,15 @@ if (!defined('GLPI_ROOT')){
 function displayTitle($ref_pic_link="",$ref_pic_text="",$ref_title="",$ref_btts="") {
         echo "<div align='center'><table border='0'><tr>";
         if ($ref_pic_link!="")
-                echo "<td><img src=\"".$ref_pic_link."\" alt='".$ref_pic_text."'
-title='".$ref_pic_text."' ></td>"; 
+                echo "<td><img src=\"".$ref_pic_link."\" alt=\"".$ref_pic_text."\"
+title=\"".$ref_pic_text."\" ></td>"; 
         if ($ref_title!="")
                 echo "<td><span class='icon_sous_nav'><b>".$ref_title."</b></span></td>"; 
 	if (is_array($ref_btts)&&count($ref_btts))
         foreach ($ref_btts as $key => $val) { 
                 echo "<td><a class='icon_consol' href=\"".$key."\">".$val."</a></td>"; 
         }        
-        echo "</tr></table></div'>";
+        echo "</tr></table></div>";
 }
 
 /**
