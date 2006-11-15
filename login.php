e28079e15b5719c02440db88009f56106ca29a8d
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2006-11-15 18:21:13 +0000

    erreur on auth
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@4103 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- login.php
+++ login.php
@@ -202,7 +202,7 @@ if ($auth_succeded)
 // we have done at least a good login? No, we exit.
 if ( ! $auth_succeded ) {
 	nullHeader("Login",$_SERVER['PHP_SELF']);
-	echo '<div align="center"><b>'.$identificat->getErr().'.</b><br><br>';
+	echo '<div align="center"><b>'.$identificat->getErr().'</b><br><br>';
 	echo '<b><a href="'.$CFG_GLPI["root_doc"].'/logout.php">'.$LANG["login"][1].'</a></b></div>';
 	nullFooter();
 	if ($CFG_GLPI["debug"]==DEMO_MODE)
