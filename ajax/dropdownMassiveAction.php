e4e64d9579d8befde70e310241eb26722e2abaab
moyooo <moyo@indepnet.net>
2006-11-28 16:41:19 +0000

    petite precaution

--- ajax/dropdownMassiveAction.php
+++ ajax/dropdownMassiveAction.php
@@ -41,9 +41,12 @@ include (GLPI_ROOT."/inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
 header_nocache();
 
-checkTypeRight($_POST["type"],"w");
 
-if (isset($_POST["action"])){
+if (isset($_POST["action"])&&isset($_POST["type"])&&!empty($_POST["type"])){
+
+	checkTypeRight($_POST["type"],"w");
+	
+
 	echo "<input type='hidden' name='action' value='".$_POST["action"]."'>";
 	echo "<input type='hidden' name='device_type' value='".$_POST["type"]."'>";
 	switch($_POST["action"]){
