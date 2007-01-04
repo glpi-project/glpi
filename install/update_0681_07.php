a6ca1f7a38fa713571f08e8b12d67084d8b2981d
walid <walid@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-01-04 19:39:06 +0000

    see #814 : ajout d'un champs helpdesk_visible et tri des applis à afficher dans le helpdesk
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@4260 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_0681_07.php
+++ install/update_0681_07.php
@@ -365,6 +365,10 @@ function update0681to07() {
 			}
 		}
 	}	
+	
+	$query = "ALTER TABLE glpi_software ADD `helpdesk_visible` tinyint(1) NOT NULL default '1'";
+	$DB->query($query) or die("0.7 add helpdesk_visible in glpi_software " . $LANG["update"][90] . $DB->error());
+	
 	// TODO Enterprises -> dropdown manufacturer + update import OCS
 	// TODO Split Config -> config general + config entity
 	// TODO AUto assignment profile based on rules
