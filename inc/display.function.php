77c69d9e9fad1877d5d76639f5213b030df98082
jmd <jmd@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-01-06 16:53:32 +0000

    Bouton image pour formulaire recherche :
    + Ajout image
    + Modif css mineures
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@4286 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/display.function.php
+++ inc/display.function.php
@@ -250,8 +250,8 @@ function commonHeader($title,$url)
 		//-- Le moteur de recherche -->
 		echo "<div id='c_recherche' >\n"; 
 		echo "<form method='get' action='".$CFG_GLPI["root_doc"]."/front/search.php'>\n"; 
-		echo "	<div id='boutonRecherche'><input type='submit' value='OK' /></div>\n"; 
-		echo "	<div id='champRecherche'><input type='text' name='globalsearch' value='".$LANG["buttons"][0]."' /></div>	\n"; 		
+		echo "	<div id='boutonRecherche'><input type='image' src='".$CFG_GLPI["root_doc"]."/pics/ok2.png'  value='OK'   title=\"".$LANG["buttons"][2]."\"  alt=\"".$LANG["buttons"][2]."\"  /></div>\n"; 
+		echo "	<div id='champRecherche'><input type='text' name='globalsearch' value='".$LANG["buttons"][0]."' onfocus=\"this.value='';\" /></div>	\n"; 		
 		echo "</form>\n"; 
 		echo "<div class='sep'></div>\n"; 
 		echo "</div>";
