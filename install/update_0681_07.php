38ed5a1af29caf8b7cad795a56b4b208c113d965
walid <walid@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-02-06 08:56:34 +0000

    Ajout du champs name à glpi_ocs_config
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@4367 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_0681_07.php
+++ install/update_0681_07.php
@@ -471,8 +471,10 @@ function update0681to07() {
 	}
 	
 	if (!FieldExists("glpi_ocs_config", "tplname")) {
-		$query = "ALTER TABLE glpi_ocs_config ADD `is_template` enum('0','1') NOT NULL default '0', ADD `tplname` varchar(200) default NULL, ADD `date_mod` datetime default NULL";
+		$query = "ALTER TABLE glpi_ocs_config ADD `name` varchar(200) default NULL AFTER `ID`, ADD `is_template` enum('0','1') NOT NULL default '0', ADD `tplname` varchar(200) default NULL, ADD `date_mod` datetime default NULL";
 		$DB->query($query) or die("0.7 add ocs_server_id in glpi_ocs_link " . $LANG["update"][90] . $DB->error());
+		$query="update glpi_ocs_config set name=ID";
+		$DB->query($query) or die("0.7 add name in glpi_ocs_link " . $LANG["update"][90] . $DB->error());
 	}
 	
 	// TODO Split Config -> config general + config entity
