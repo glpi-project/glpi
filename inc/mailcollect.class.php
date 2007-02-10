018fdf8ffa8f9b26d5cda37b995b210c846ef1cc
jmd <jmd@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-02-10 00:44:37 +0000

    recup ticket via mail  on continue
    Pb utf8 réglé
    
    
    
    
    
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@4396 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/mailcollect.class.php
+++ inc/mailcollect.class.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * @version $Id: display.function.php 4323 2007-01-18 23:12:38Z jmd $
+ *  $Id$
  -------------------------------------------------------------------------
  GLPI - Gestionnaire Libre de Parc Informatique
  Copyright (C) 2003-2006 by the INDEPNET Development Team.
@@ -46,33 +46,35 @@ class MailCollect  extends receivemail {
 */
 function MailCollect(){
 	
-		$this->receiveMail('','','','','pop3','110');
+		$this->receiveMail('','','','','','');
 		//example receiveMail('abc@example.com','XXX','abc@example.com','mail.example.com','pop3','110');
+		
 		//Connect to the Mail Box
 		$this->connect();
-		echo "toto";
-		// Get Total Number of Unread Email in mail box
-		$tot=$this->getTotalMails(); //Total Mails in Inbox Return integer value
-		echo "Total Mails:: $tot<br>";
-		if (isset ($tot))
-		{
-				
-			for($i=1;$i<=$tot;$i++)
+		if ($this->marubox){
+			echo "Connection OK <br>";
+			// Get Total Number of Unread Email in mail box
+			$tot=$this->getTotalMails(); //Total Mails in Inbox Return integer value
+			echo "Total Mails:: $tot<br>";
+			if (isset ($tot))
 			{
-
-				
-				$tkt= $this->buildTicket($i);
-				
-				//$this->deleteMails($i); // Delete Mail from Mail box
-				
-				print_r($tkt);
-				$track=new job;
-				$track->add($tkt);
-			}
 					
+				for($i=1;$i<=$tot;$i++)
+				{
+					$tkt= $this->buildTicket($i);
+					//$this->deleteMails($i); // Delete Mail from Mail box
+					print_r($tkt);
+					$track=new job;
+					$track->add($tkt);
+				}
+						
+			}
+			$this->close_mailbox();   //Close Mail Box
 		}
-		$this->close_mailbox();   //Close Mail Box
-	
+		else
+		{
+			echo "Connection error";
+		}
 } // end function MailCollect
 
 
@@ -87,17 +89,13 @@ function buildTicket($i)
 	global $DB;
 
 	$head=$this->getHeaders($i);  // Get Header Info Return Array Of Headers **Key Are (subject,to,toOth,toNameOth,from,fromName)
+	
 	echo "<br>----------------------------------------- Header  -------------------------------------------------<BR>";
 	echo "Subjects :: ".$head['subject']."<br>";
-	echo "TO :: ".$head['to']."<br>";
-	echo "To Other :: ".$head['toOth']."<br>";
-	echo "ToName Other :: ".$head['toNameOth']."<br>";
 	echo "From :: ".$head['from']."<br>";
-	echo "FromName :: ".$head['fromName']."<br>";
-	
 	echo "<br>----------------------------------------- BODY -------------------------------------------------<BR>";
 	echo $this->getBody($i);  // Get Body Of Mail number Return String Get Mail id in interger
-	/*$str=$obj->GetAttech($i,"./"); // Get attached File from Mail Return name of file in comma separated string  args. (mailid, Path to store file)
+	/*$str=$obj->GetAttech($i,"./"); // Get attached File from Mail Return name of file in comma separated string  args. (mailid, Path to store file)  !! Not use for the moment !!
 	$ar=explode(",",$str);
 	foreach($ar as $key=>$value)
 		echo ($value=="")?"":"Atteched File :: ".$value."<br>";
@@ -126,16 +124,20 @@ function buildTicket($i)
 			$FK_entities=$DB->result($result,0,"FK_entities");
 	$tkt['FK_entities']=$FK_entities;
 
-	//$tkt['Subject']= $head['subject'];   // PAS ENCORE UTILISE pour le sujet mais  CA VA VENIR en attendant on le met dans le content
-	$tkt['contents']=textCleaner($head['subject'])." : ";
+	//$tkt['Subject']= $head['subject'];   // not use for the moment
+	$tkt['contents']=$this->textCleaner($head['subject'])." : ";
 	$tkt['priority']= "3";
 	$tkt['device_type']="0";
 	$tkt['request_type']="2";
-	$tkt['contents'].= $this->getBody($i);
+		if (!seems_utf8($this->getBody($i))){
+		$tkt['contents'].= utf8_encode($this->getBody($i));	
+		}else{
+		$tkt['contents'].= $this->getBody($i);
+		}
 	
-	// TODO récupérer le charset et encoder en utf8 si nécessaire
+	$tkt=addslashes_deep($tkt);
 	
-	return addslashes_deep($tkt);
+	return $tkt;
 }
 
 
