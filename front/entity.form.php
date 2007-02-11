3ce4ddc9975508ae25d2cc02baad8d1a03f81b0d
moyooo <moyo@indepnet.net>
2007-02-11 18:39:08 +0000

    fichier manquant

---
+++ front/entity.form.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2006 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file:
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+$NEEDED_ITEMS=array("entity");
+
+define('GLPI_ROOT', '..');
+include (GLPI_ROOT . "/inc/includes.php");
+
+checkRight("config","r");
+
+$entity=new Entity();
+$entitydata=new EntityData();
+
+if (isset($_POST["update"]))
+{
+	checkRight("printer","w");
+	$entitydata->update($_POST);
+	logEvent($_POST["ID"], "entities", 4, "config", $_SESSION["glpiname"]." ".$LANG["log"][21]);
+	glpi_header($_SERVER['HTTP_REFERER']);
+}
+
+commonHeader($LANG["Menu"][37],$_SERVER['PHP_SELF'],"admin");
+
+if (!isset($_SESSION['glpi_onglet'])) $_SESSION['glpi_onglet']=1;
+if (isset($_GET['onglet'])) {
+	$_SESSION['glpi_onglet']=$_GET['onglet'];
+}	
+
+
+
+$entity->showForm($_SERVER['PHP_SELF'],$_GET["ID"]);
+
+commonFooter();
+?>
