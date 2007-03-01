e5cba2c0121f09576972c4075296407b7935522f
walid <walid@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-03-01 14:35:55 +0000

    Ajout des types pour le moteur de regles
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@4495 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- config/define.php
+++ config/define.php
@@ -212,5 +212,20 @@
 	define("NOT_YET_AUTHENTIFIED",-1);
 
 
+	//Generic rules engine
+	define("PATTERN_IS",0);
+	define("PATTERN_IS_NOT",1);
+	define("PATTERN_CONTAIN",2);
+	define("PATTERN_NOT_CONTAIN",3);
+	define("PATTERN_BEGIN",4);
+	define("PATTERN_END",5);	
 
+	define("RULES_ACTION_ASSIGN",0);	
+	define("RULES_ACTION_STATUS",1);
+	define("RULES_ACTION_CATEGORY",2);
+	define("RULES_ACTION_GROUP",3);
+	define("RULES_ACTION_PRIORITY",4);		
+	
+	define("RULE_OCS_AFFECT_COMPUTER",0);
+	define("RULE_LDAP_AFFECT_RIGHT",1);
 ?>
