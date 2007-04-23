a67b86e39854eaf91c4257d15b3d19a6827f90b5
moyooo <moyo@indepnet.net>
2007-04-23 14:40:51 +0000

    Correction relations dans le cas de relations multiples entre tables

--- inc/commondbtm.class.php
+++ inc/commondbtm.class.php
@@ -288,8 +288,15 @@ class CommonDBTM {
 		$RELATION=getDbRelations();
 		if (isset($RELATION[$this->table])){
 			foreach ($RELATION[$this->table] as $tablename => $field){
-				$query="UPDATE $tablename SET $field = 0 WHERE $field=$ID ";
-				$DB->query($query);
+				if (!is_array($field)){
+					$query="UPDATE `$tablename` SET `$field` = NULL WHERE `$field`='$ID' ";
+					$DB->query($query);
+				} else {
+					foreach ($field as $f){
+						$query="UPDATE `$tablename` SET `$f` = NULL WHERE `$f`='$ID' ";
+						$DB->query($query);
+					}
+				}
 			}
 		}
 	}
