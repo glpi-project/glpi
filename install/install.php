5c990f5d4d4b12bf08b86914d64f994d9cb68e57
moyooo <moyo@indepnet.net>
2007-04-25 19:32:47 +0000

    Petit test

--- install/install.php
+++ install/install.php
@@ -40,6 +40,7 @@ error_reporting(0);
 define('GLPI_ROOT', '..');
 include_once (GLPI_ROOT . "/config/based_config.php");
 include_once (GLPI_ROOT . "/inc/common.function.php");
+include_once (GLPI_ROOT . "/inc/db.function.php");
 include_once (GLPI_ROOT . "/inc/display.function.php");
 
 session_save_path(GLPI_SESSION_DIR);
