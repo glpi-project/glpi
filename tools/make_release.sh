4ee5338874ef9b4220afe23aaa2f3089e370af9d
moyooo <moyo@indepnet.net>
2007-05-13 17:53:19 +0000

    split scripts files

--- tools/make_release.sh
+++ tools/make_release.sh
@@ -78,7 +78,7 @@ echo "Delete bigdumps and older sql files";
 \rm install/mysql/irm*;
 
 echo "Delete various scripts and directories"
-\rm -rf scripts;
+\rm -rf tools;
 
 echo "Must be root to generate a clean tarball - Please login"
 echo "cd /tmp; chown -R root.root /tmp/glpi; tar czvf /tmp/glpi-$RELEASE.tar.gz glpi; \rm -rf /tmp/glpi" | sudo -s
