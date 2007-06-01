5007066e17439f49139b43b0088bb85028d9d2c1
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-06-01 22:57:31 +0000

    Correction mienure
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@5093 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- ajax/autocompletion.php
+++ ajax/autocompletion.php
@@ -35,6 +35,10 @@
 
 
 define('GLPI_ROOT','..');
+// Include plugin if it is a plugin table
+if (!ereg("plugin",$_POST['table'])){
+	$AJAX_INCLUDE=1;
+}
 $AJAX_INCLUDE=1;
 include (GLPI_ROOT."/inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
