aa72afdcb00c9561b833a8aa7cbd988189dc2090
Walid Nouh <wnouh@teclib.com>
2007-07-17 15:10:16 +0000

    Affichage des catégories de softs

--- script.js
+++ script.js
@@ -321,3 +321,26 @@ function getTop(MyObject){
         return (MyObject.offsetTop);
 }
 
+function showHideDiv(id) {
+	//safe function to hide an element with a specified id
+	if (document.getElementById) { // DOM3 = IE5, NS6
+		if (document.getElementById(id).style.display == 'none')
+			document.getElementById(id).style.display = 'block';
+		else
+			document.getElementById(id).style.display = 'none';
+	}
+	else {
+		if (document.layers) { // Netscape 4
+			if (document.id.display == 'none')
+				document.id.display = 'block';
+			else	
+				document.id.display = 'none';
+		}
+		else { // IE 4
+			if (document.all.id.style.display == 'none')
+				document.all.id.style.display = 'none';
+			else
+				document.all.id.style.display = 'block';	
+		}
+	}
+}
