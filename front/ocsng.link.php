d1c50588adf3d015231e9874818b9b86f098d272
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-08-31 14:27:53 +0000

    Correction include
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@5417 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/ocsng.link.php
+++ front/ocsng.link.php
@@ -34,7 +34,7 @@
 // ----------------------------------------------------------------------
 
 
-$NEEDED_ITEMS=array("ocsng","rulesengine","rule.ocs","computer","device","printer","networking","peripheral","monitor","software","infocom","phone","tracking","enterprise","reservation","setup","admininfo","registry","rule.softwarecategories");
+$NEEDED_ITEMS=array("ocsng","rulesengine","rule.ocs","computer","device","printer","networking","peripheral","monitor","software","infocom","phone","tracking","enterprise","reservation","setup","admininfo","registry","rule.softwarecategories","group");
 
 define('GLPI_ROOT', '..');
 include (GLPI_ROOT . "/inc/includes.php");
