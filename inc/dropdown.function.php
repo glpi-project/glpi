d4afebfd7d208c36706ff28d75962c31c7779f7a
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-09-24 11:31:57 +0000

    Correction transfer
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@5515 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/dropdown.function.php
+++ inc/dropdown.function.php
@@ -1293,7 +1293,7 @@ function dropdownMassiveAction($device_type,$deleted=0){
 		}
 		if (haveRight('transfer','w') && isMultiEntitiesMode() && 
 				in_array($device_type, 	array(CARTRIDGE_TYPE,COMPUTER_TYPE,CONSUMABLE_TYPE,CONTACT_TYPE,CONTRACT_TYPE,ENTERPRISE_TYPE,
-				MONITOR_TYPE,NETWORKING_TYPE,PERIPHERAL_TYPE,PHONE_TYPE,PRINTER_TYPE,SOFTWARE_TYPE,TRACKING_TYPE))
+				MONITOR_TYPE,NETWORKING_TYPE,PERIPHERAL_TYPE,PHONE_TYPE,PRINTER_TYPE,SOFTWARE_TYPE,TRACKING_TYPE,DOCUMENT_TYPE))
 				&& $isadmin
 			){
 			echo "<option value=\"add_transfer_list\">".$LANG["buttons"][48]."</option>";
