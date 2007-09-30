221b2357bbc70eacc59a4a1216bdbaa3f0028035
moyooo <moyo@indepnet.net>
2007-09-30 16:08:21 +0000

    Add greek dict

--- locales/el_EL.php
+++ locales/el_EL.php
@@ -26,7 +26,7 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ------------------------------------------------------------------------
  */
-//gr_GR
+//el_EL
 
 $LANG=array();
 
