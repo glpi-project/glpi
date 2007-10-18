95642cd0d0ab458660bcbb9691f31db0aa52bcb8
moyooo <moyo@indepnet.net>
2007-10-18 19:01:57 +0000

    Preview OCS et ajax

--- inc/ocsng.function.php
+++ inc/ocsng.function.php
@@ -46,7 +46,9 @@ function ocsShowNewComputer($ocs_server_id, $advanced, $check, $start, $tolinked
 		return false;
 
 	$cfg_ocs = getOcsConf($ocs_server_id);
-
+	if ($advanced){
+		$CFG_GLPI['use_ajax']=0;
+	}
 	$WHERE = "";
 	if (!empty ($cfg_ocs["tag_limit"])) {
 		$splitter = explode("$", $cfg_ocs["tag_limit"]);
