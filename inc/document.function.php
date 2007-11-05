f4fb4f9941ea13902c0212a203a50ff9bf5802a8
moyooo <moyo@indepnet.net>
2007-11-05 22:01:54 +0000

    Correction mineure

--- inc/document.function.php
+++ inc/document.function.php
@@ -423,17 +423,18 @@ function showDocumentAssociated($device_type,$ID,$withtemplate=''){
 
 	echo "</table></div>"    ;
 	
-	echo "<div class='center'>";
-	echo "<table width='950px'>";
-	echo "<tr><td><img src=\"".$CFG_GLPI["root_doc"]."/pics/arrow-left.png\" alt=''></td><td class='center'><a onclick= \"if ( markAllRows('document_form') ) return false;\" href='".$_SERVER['PHP_SELF']."?ID=$ID&amp;select=all'>".$LANG["buttons"][18]."</a></td>";
-		
-	echo "<td>/</td><td class='center'><a onclick= \"if ( unMarkAllRows('document_form') ) return false;\" href='".$_SERVER['PHP_SELF']."?ID=$ID&amp;select=none'>".$LANG["buttons"][19]."</a>";
-	echo "</td><td align='left' width='80%'>";
-	echo "<input type='submit' name='deleteitem' value=\"".$LANG["buttons"][6]."\" class='submit'>";
-	echo "</td>";
-	echo "</table>";
-	
-	echo "</div>";
+	if ($canedit){
+		echo "<div class='center'>";
+		echo "<table width='950px'>";
+		echo "<tr><td><img src=\"".$CFG_GLPI["root_doc"]."/pics/arrow-left.png\" alt=''></td><td class='center'><a onclick= \"if ( markAllRows('document_form') ) return false;\" href='".$_SERVER['PHP_SELF']."?ID=$ID&amp;select=all'>".$LANG["buttons"][18]."</a></td>";
+			
+		echo "<td>/</td><td class='center'><a onclick= \"if ( unMarkAllRows('document_form') ) return false;\" href='".$_SERVER['PHP_SELF']."?ID=$ID&amp;select=none'>".$LANG["buttons"][19]."</a>";
+		echo "</td><td align='left' width='80%'>";
+		echo "<input type='submit' name='deleteitem' value=\"".$LANG["buttons"][6]."\" class='submit'>";
+		echo "</td>";
+		echo "</table>";
+		echo "</div>";
+	}
 	
 	echo "</form>";
 
