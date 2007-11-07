244fa57f2a3cedd00ad88790eb8fd36dbae0df4e
moyooo <moyo@indepnet.net>
2007-11-07 14:23:58 +0000

    Correction export knowbase

--- inc/knowbase.function.php
+++ inc/knowbase.function.php
@@ -292,7 +292,7 @@ function showKbItemList($target,$field,$phrasetype,$contains,$sort,$order,$start
 					echo displaySearchItem($output_type,"<div class='left'><a ".($data['faq']?" class='pubfaq' ":" class='knowbase' ")." href=\"".$target."?ID=".$data["ID"]."\">".resume_text($data["question"],80)."</a></div><div class='kb_resume'>".resume_text(html_clean(unclean_cross_side_scripting_deep($data["answer"])),600)."</div>",$item_num,$row_num);
 				} else {
 					echo displaySearchItem($output_type,$data["question"],$item_num,$row_num);
-					echo displaySearchItem($output_type,html_entity_decode(html_clean(unclean_cross_side_scripting_deep($data["answer"]))),$item_num,$row_num);
+					echo displaySearchItem($output_type,html_clean(unclean_cross_side_scripting_deep(html_entity_decode($data["answer"],ENT_COMPAT,'UTF-8'))),$item_num,$row_num);
 				}
 				// le cumul de fonction me plait pas TODO à optimiser.
 				
