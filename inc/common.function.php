fcf5b841a876773dd3bb3fc090a0bb1272255060
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-11-20 19:02:35 +0000

    export KB UTF8 PHP4
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@5903 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/common.function.php
+++ inc/common.function.php
@@ -164,6 +164,36 @@ function utf8_strlen($str)
     return $count;
 }
 
+function utf8_html_entity_decode($string)
+{
+    static $trans_tbl;
+   
+    // replace numeric entities
+    $string = preg_replace('~&#x([0-9a-f]+);~ei', 'code2utf(hexdec("\\1"))', $string);
+    $string = preg_replace('~&#([0-9]+);~e', 'code2utf(\\1)', $string);
+
+    // replace literal entities
+    if (!isset($trans_tbl))
+    {
+        $trans_tbl = array();
+       
+        foreach (get_html_translation_table(HTML_ENTITIES) as $val=>$key)
+            $trans_tbl[$key] = utf8_encode($val);
+    }
+   
+    return strtr($string, $trans_tbl);
+}
+
+// Returns the utf string corresponding to the unicode value (from php.net, courtesy - romans@void.lv)
+function code2utf($num)
+{
+    if ($num < 128) return chr($num);
+    if ($num < 2048) return chr(($num >> 6) + 192) . chr(($num & 63) + 128);
+    if ($num < 65536) return chr(($num >> 12) + 224) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);
+    if ($num < 2097152) return chr(($num >> 18) + 240) . chr((($num >> 12) & 63) + 128) . chr((($num >> 6) & 63) + 128) . chr(($num & 63) + 128);
+    return '';
+}
+
 /**
  * Clean cache cron function
  *
