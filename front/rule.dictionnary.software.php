36386744ffaf7f8c574eb3020574990ab3b520fb
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2007-12-24 11:24:44 +0000

    fix includes (+tag yllen)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@6185 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/rule.dictionnary.software.php
+++ front/rule.dictionnary.software.php
@@ -33,7 +33,7 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-$NEEDED_ITEMS=array("rulesengine","rule.dictionnary.software","software","rule.dictionnary.manufacturer","rule.softwarecategories","ocs");
+$NEEDED_ITEMS=array("rulesengine","rule.dictionnary.software","software","rule.dictionnary.manufacturer","rule.softwarecategories","ocsng","setup");
 
 define('GLPI_ROOT', '..');
 include (GLPI_ROOT . "/inc/includes.php");
