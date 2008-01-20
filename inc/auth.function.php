5e33183e699d3659d1fd8b33dce392360428be24
moyooo <moyo@indepnet.net>
2008-01-20 17:23:41 +0000

    correction load groups

--- inc/auth.function.php
+++ inc/auth.function.php
@@ -641,8 +641,17 @@ function changeActiveEntities($ID="all",$recursive=false) {
 function loadGroups() {
 	global $DB;
 
+
+	$first=true;
+	$entities="";
+	foreach ($_SESSION['glpiactiveentities'] as $val){
+		if (!$first) $entities.=",";
+		else $first=false;
+		$entities.=$val;
+	}	
+
 	$_SESSION["glpigroups"] = array ();
-	$query_gp = "SELECT * FROM glpi_users_groups LEFT JOIN glpi_groups ON (glpi_users_groups.FK_groups = glpi_groups.ID) WHERE FK_users='" . $_SESSION['glpiID'] . "' AND glpi_groups.FK_entities='" . $_SESSION["glpiactive_entity"] . "'";
+	$query_gp = "SELECT * FROM glpi_users_groups LEFT JOIN glpi_groups ON (glpi_users_groups.FK_groups = glpi_groups.ID) WHERE FK_users='" . $_SESSION['glpiID'] . "' AND glpi_groups.FK_entities IN ($entities)";
 
 	$result_gp = $DB->query($query_gp);
 	if ($DB->numrows($result_gp)) {
