4d76674998f066ff5edfb11acff3e10ab3668b45
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2008-02-04 18:33:03 +0000

    correction hard coded string in profile
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@6472 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/profile.class.php
+++ inc/profile.class.php
@@ -367,7 +367,7 @@ class Profile extends CommonDBTM{
 		// Assistance / Tracking-helpdesk
 		echo "<tr><td class='tab_bg_1' colspan='6' align='center'><strong>".$LANG["title"][24]."</strong></td></tr>";
 
-		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>Création</strong></td></tr>";
+		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>".$LANG["profiles"][41]."</strong></td></tr>";
 
 		echo "<tr class='tab_bg_2'>";
 		echo "<td>".$LANG["profiles"][5].":</td><td>";
@@ -381,7 +381,7 @@ class Profile extends CommonDBTM{
 		echo "</td>";
 		echo "</tr>";
 
-		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>Edition</strong></td></tr>";
+		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>".$LANG["profiles"][40]."</strong></td></tr>";
 
 		echo "<tr class='tab_bg_2'>";
 		echo "<td>".$LANG["profiles"][18].":</td><td>";
@@ -394,7 +394,7 @@ class Profile extends CommonDBTM{
 		dropdownYesNo("update_followups",$this->fields["update_followups"]);
 		echo "</td></tr>";
 
-		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>Attribution</strong></td></tr>";
+		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>".$LANG["profiles"][39]."</strong></td></tr>";
 
 		echo "<tr class='tab_bg_2'>";
 		
@@ -408,7 +408,7 @@ class Profile extends CommonDBTM{
 		dropdownYesNo("assign_ticket",$this->fields["assign_ticket"]);
 		echo "</td></tr>";
 
-		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>Association</strong></td></tr>";
+		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>".$LANG["profiles"][42]."</strong></td></tr>";
 		
 		echo "<tr class='tab_bg_2'>";
 		echo "<td>".$LANG["profiles"][27]."</td><td>";
@@ -439,7 +439,7 @@ class Profile extends CommonDBTM{
 
 
 		
-		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>Vision</strong></td></tr>";
+		echo "<tr><td class='tab_bg_5' colspan='6' ><strong>".$LANG["profiles"][38]."</strong></td></tr>";
 
 		echo "<tr class='tab_bg_2'>";
 		
