dc9dd33a6041b0d739f5f539d73e0e6e9434aa2b
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2008-03-10 21:43:32 +0000

    Mandatories and helpdesk fixed #1034
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@6543 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/helpdesk.php
+++ front/helpdesk.php
@@ -42,16 +42,6 @@ checkRight("create_ticket","1");
 
 commonHeader("Helpdesk",$_SERVER['PHP_SELF'],"maintain","helpdesk");
 
-if (!isset($_POST["user"])) $user=$_SESSION["glpiID"];
-else $user=$_POST["user"];
-
-if (!isset($_POST["FK_group"])) $group=0;
-else $group=$_POST["FK_group"];
-if (!isset($_POST["assign"])) $assign=0;
-else $assign=$_POST["assign"];
-if (!isset($_POST["assign_group"])) $assign_group=0;
-else $assign_group=$_POST["assign_group"];
-
 if (isset($_POST["_my_items"])&&!empty($_POST["_my_items"])){
 	$splitter=split("_",$_POST["_my_items"]);
 	if (count($splitter)==2){
@@ -62,63 +52,72 @@ if (isset($_POST["_my_items"])&&!empty($_POST["_my_items"])){
 
 
 if (isset($_GET["device_type"])) $device_type=$_GET["device_type"];
-else if (isset($_POST["device_type"])) $device_type=$_POST["device_type"];
+else if (isset($_SESSION["helpdeskSaved"]["device_type"])) $device_type=$_SESSION["helpdeskSaved"]["device_type"];
 else $device_type=0;
 
 if (isset($_GET["computer"])) $computer=$_GET["computer"];
-else if (isset($_POST["computer"])) $computer=$_POST["computer"];
+else if (isset($_SESSION["helpdeskSaved"]["computer"])) $computer=$_SESSION["helpdeskSaved"]["computer"];
 else $computer=0;
 
-if(empty($_POST["status"])) $_POST["status"] = "new";
+if (!isset($_SESSION["helpdeskSaved"]["user"])) $user=$_SESSION["glpiID"];
+else $user=$_SESSION["helpdeskSaved"]["user"];
 
-$error = "";
+if (!isset($_SESSION["helpdeskSaved"]["FK_group"])) $group=0;
+else $group=$_SESSION["helpdeskSaved"]["FK_group"];
+
+if (!isset($_SESSION["helpdeskSaved"]["assign"])) $assign=0;
+else $assign=$_SESSION["helpdeskSaved"]["assign"];
+
+if (!isset($_SESSION["helpdeskSaved"]["assign_group"])) $assign_group=0;
+else $assign_group=$_SESSION["helpdeskSaved"]["assign_group"];
+
+if (!isset($_SESSION["helpdeskSaved"]["minute"])) $minute=0;
+else $minute=$_SESSION["helpdeskSaved"]["minute"];
+
+if (!isset($_SESSION["helpdeskSaved"]["hour"])) $hour=0;
+else $hour=$_SESSION["helpdeskSaved"]["hour"];
+
+if (!isset($_SESSION["helpdeskSaved"]["category"])) $category=0;
+else $category=$_SESSION["helpdeskSaved"]["category"];
+
+if (!isset($_SESSION["helpdeskSaved"]["priority"])) $priority=3;
+else $priority=$_SESSION["helpdeskSaved"]["priority"];
+
+if (!isset($_SESSION["helpdeskSaved"]["request_type"])) $request_type=1;
+else $request_type=$_SESSION["helpdeskSaved"]["request_type"];
+
+if (!isset($_SESSION["helpdeskSaved"]["name"])) $name='';
+else $name=stripslashes($_SESSION["helpdeskSaved"]["name"]);
+
+if (!isset($_SESSION["helpdeskSaved"]["contents"])) $contents='';
+else $contents=stripslashes($_SESSION["helpdeskSaved"]["contents"]);
+
+
+if (isset($_SESSION["helpdeskSaved"])&&count($_GET)==0){
+	unset($_SESSION["helpdeskSaved"]);
+}
+
+
 $REFERER="";
 if (isset($_SERVER['HTTP_REFERER']))
 $REFERER=$_SERVER['HTTP_REFERER'];
 if (isset($_POST["_referer"])) $REFERER=$_POST["_referer"];
 $REFERER=preg_replace("/&/","&amp;",$REFERER);
 
+
 $track=new Job();
 
-if (isset($_POST["priority"]) && $CFG_GLPI["ticket_content_mandatory"] && empty($_POST["contents"]))
-{
-	$error=$LANG["tracking"][8] ;
-	addFormTracking($device_type,$computer,$user,$group,$assign,$assign_group,$_SERVER['PHP_SELF'],$error);
-}
-elseif (isset($_POST["priority"]) && $CFG_GLPI["ticket_title_mandatory"] && empty($_POST["name"]))
-{
-	$error=$LANG["help"][40] ;
-	addFormTracking($device_type,$computer,$user,$group,$assign,$assign_group,$_SERVER['PHP_SELF'],$error);
-}
-elseif (isset($_POST["priority"]) && $CFG_GLPI["ticket_category_mandatory"] && !$_POST["category"])
-{
-	$error=$LANG["help"][41] ;
-	addFormTracking($device_type,$computer,$user,$group,$assign,$assign_group,$_SERVER['PHP_SELF'],$error);
-}
-elseif (isset($_POST["priority"]) && !empty($_POST["contents"]))
-{
-
+if (isset($_POST["priority"])){
 	if ($newID=$track->add($_POST)){
-		$linkin='';
-		$linkout='';
-		if (haveRight('show_all_ticket','1')){
-			$linkin="<a href='tracking.form.php?ID=$newID'>";
-			$linkout='</a>';
-		}
-		$error=$LANG["tracking"][9]." (".$LANG["common"][2]." $linkin$newID$linkout)";
-		displayMessageAfterRedirect();
-		addFormTracking($device_type,$computer,$user,$group,$assign,$assign_group,$_SERVER['PHP_SELF'],$error);
+		logEvent($newID, "tracking", 4, "tracking", $_SESSION["glpiname"]." ".$LANG["log"][20]." $newID.");
 	}
-	else {
-		$error=$LANG["tracking"][10];
-		displayMessageAfterRedirect();
-		addFormTracking($device_type,$computer,$user,$group,$assign,$assign_group,$_SERVER['PHP_SELF'],$error);
-	}
-} 
-else
-{
-	addFormTracking($device_type,$computer,$user,$group,$assign,$assign_group,$_SERVER['PHP_SELF'],$error);
+	glpi_header($_SERVER['PHP_SELF']);
+
+
+} else {
+	addFormTracking($device_type,$computer,$_SERVER['PHP_SELF'],$user,$group,$assign,$assign_group,$name,$contents,$category,$priority,$request_type,$hour,$minute);
 }
+
 
 commonFooter();
 
