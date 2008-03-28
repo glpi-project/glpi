2be8226336b5ac803512debbae87d61fcc1b14d5
moyooo <moyo@indepnet.net>
2008-03-28 18:35:47 +0000

    Complete previous changeset : see #1044

--- front/computer.php
+++ front/computer.php
@@ -34,7 +34,7 @@
 // ----------------------------------------------------------------------
 
 
-$NEEDED_ITEMS=array("search","computer","infocom","contract");
+$NEEDED_ITEMS=array("search","computer","infocom");
 
 define('GLPI_ROOT', '..');
 include (GLPI_ROOT . "/inc/includes.php");
