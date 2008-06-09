117208e47e7ec46a05e35e2208920a7742041651
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2008-06-09 15:57:58 +0000

    update to Cache_Lite 1.7.4 (allow running GLPI with system lib)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@6967 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- lib/cache_lite/Lite/File.php
+++ lib/cache_lite/Lite/File.php
@@ -75,7 +75,7 @@ class Cache_Lite_File extends Cache_Lite
     * @return string data of the cache (or false if no cache available)
     * @access public
     */
-    function get($id, $group = 'default', $doNotTestCacheValidity = false) 
+    function get($id, $group = 'default') 
     {
         if ($data = parent::get($id, $group, true)) {
             if ($filemtime = $this->lastModified()) {
