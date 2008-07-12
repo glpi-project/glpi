9a68447adb0f5eb42a044fd3e3de543278942428
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2008-07-12 12:36:37 +0000

    clean HTML
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@7066 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/display.function.php
+++ inc/display.function.php
@@ -104,8 +104,8 @@ function includeCommonHtmlHeader($title=''){
 	echo "<script type='text/javascript'>";
 	// DO not get it from extjs website
 	echo "Ext.BLANK_IMAGE_URL = '".$CFG_GLPI["root_doc"]."/lib/extjs/s.gif';\n";
-	// Loading indicator in span not in div
-	echo "Ext.UpdateManager.defaults.indicatorText = '<span class=\"loading-indicator\">".$LANG["common"][80]."</span>';\n";
+	// TODO : Loading indicator in span not in div : HTML Error if doing that / maybe load a image ?
+	//echo "Ext.UpdateManager.defaults.indicatorText = \"<span class='loading-indicator'>".$LANG["common"][80]."</span>\";\n";
 	echo "</script>\n";
 
 	// Some Javascript-Functions which we may need later
