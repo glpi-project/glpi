ec0ac85b8bea9cac5b0f5822c8c47c6b9141496c
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2008-09-16 17:13:36 +0000

    fix some default options to avoid error during migration
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@7288 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- config/define.php
+++ config/define.php
@@ -367,4 +367,8 @@
 
 	$CFG_GLPI["recursive_type"]=array(CONTACT_TYPE=>"glpi_contacts", ENTERPRISE_TYPE=>"glpi_enterprises", CONTRACT_TYPE=>"glpi_contracts", 
 		DOCUMENT_TYPE=>"glpi_docs", KNOWBASE_TYPE=>"glpi_kbitems", NETWORKING_TYPE => "glpi_networking", GROUP_TYPE => "glpi_groups");
+		
+	// New config options which can be missing during migration
+	$CFG_GLPI["numberformat"]=0;
+	$CFG_GLPI["decimal_number"]=2;
 ?>
