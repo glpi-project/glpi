92b29c8d82318006dc6acca31d01a59accf94b31
tsmr <tsmr@thetsmr.fr>
2008-09-22 21:34:03 +0000

    tab for entities

--- inc/rule.ocs.class.php
+++ inc/rule.ocs.class.php
@@ -306,7 +306,7 @@ class OcsAffectEntityRule extends Rule {
 
 		if ($canedit) {
 			echo "<div class='center'>";
-			echo "<table width='80%'>";
+			echo "<table class='tab_glpi' width='80%'>";
 			echo "<tr><td><img src=\"" . $CFG_GLPI["root_doc"] . "/pics/arrow-left.png\" alt=''></td><td class='center'><a onclick= \"if ( markAllRows('entityaffectation_form') ) return false;\" href='" . $_SERVER['PHP_SELF'] . "?ID=$ID&amp;select=all'>" . $LANG["buttons"][18] . "</a></td>";
 
 			echo "<td>/</td><td class='center'><a onclick= \"if ( unMarkAllRows('entityaffectation_form') ) return false;\" href='" . $_SERVER['PHP_SELF'] . "?ID=$ID&amp;select=none'>" . $LANG["buttons"][19] . "</a>";
