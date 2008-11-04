381ac60496955935ad18e2865e1e5e701660f672
jmdoleans <Jmd@indepnet.Net>
2008-11-04 23:39:40 +0000

    Correct last commit

--- inc/setup.function.php
+++ inc/setup.function.php
@@ -1206,7 +1206,7 @@ function showFormExtAuthList($target) {
 			$out .= $input['server_type'];
 		if (isset ($input['server_ssl']))
 			$out .= $input['server_ssl'];
-		if (isset ($input['server_cert'])&&  ( !empty($input['server_ssl']) | !empty($input['server_tls'])))
+		if (isset ($input['server_cert'])&&  ( !empty($input['server_ssl']) || !empty($input['server_tls'])))
 			$out .= $input['server_cert'];
 		if (isset ($input['server_tls']))
 			$out .= $input['server_tls'];
