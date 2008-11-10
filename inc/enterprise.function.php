d5f30e12fe4965edaeefba522d56b7aef57d76c7
moyooo <moyo@indepnet.net>
2008-11-10 17:54:19 +0000

    correct form showing hardware of an supplier

--- inc/enterprise.function.php
+++ inc/enterprise.function.php
@@ -62,7 +62,7 @@ function showInfocomEnterprise($instID) {
 
 	echo "<br><br><div class='center'><table class='tab_cadrehov'>";
 	echo "<tr><th colspan='2'>";
-	printPagerForm($_SERVER["REQUEST_URI"]);
+	printPagerForm($CFG_GLPI['root_doc'].'/front/enterprise.form.php?ID='.$instID);
 	echo "</th><th colspan='3'>".$LANG["document"][19].":</th></tr>";
 	echo "<tr><th>".$LANG["common"][17]."</th>";
 	echo "<th>".$LANG["entity"][0]."</th>";
