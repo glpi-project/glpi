6117fa6b1368d3c89f2a862fe3dd866a276bcd4b
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-01-15 21:14:46 +0000

    correct search engine
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@7827 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/search.function.php
+++ inc/search.function.php
@@ -2798,7 +2798,7 @@ function addLeftJoin ($type,$ref_table,&$already_link_tables,$new_table,$linkfie
 
 		case "glpi_enterprises_infocoms":
 			$out=addLeftJoin($type,$ref_table,$already_link_tables,"glpi_infocoms",$linkfield);
-		return $out." LEFT JOIN glpi_enterprises AS glpi_enterprises_infocoms ON (glpi_infocoms.FK_enterprise = $nt.ID) ";
+		return $out." LEFT JOIN glpi_enterprises $AS ON (glpi_infocoms.FK_enterprise = $nt.ID) ";
 		break;
 		case "glpi_dropdown_budget":
 			$out=addLeftJoin($type,$ref_table,$already_link_tables,"glpi_infocoms",$linkfield);
