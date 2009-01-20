62569cad46e54103b24a5f00a9dd41e573a8ca13
moyooo <moyo@indepnet.net>
2009-01-20 20:24:42 +0000

    corrections diverses merci jotape99

--- inc/export.function.php
+++ inc/export.function.php
@@ -187,7 +187,7 @@ function displaySearchFooter($type,$title=""){
 			global $PDF_HEADER,$PDF_ARRAY;
 			$pdf= new Cezpdf('a4','portrait');
 			$pdf->selectFont(GLPI_ROOT."/lib/ezpdf/fonts/Helvetica.afm");
-			$pdf->ezStartPageNumbers(750,10,10,'left',"GLPI PDF export - ".convDate(date("Y-m-d"))." - ".count($PDF_ARRAY)." ".utf8_decode($LANG["pager"][5])."- {PAGENUM}/{TOTALPAGENUM}");
+			$pdf->ezStartPageNumbers(550,10,10,'left',"GLPI PDF export - ".convDate(date("Y-m-d"))." - ".count($PDF_ARRAY)." ".utf8_decode($LANG["pager"][5])."- {PAGENUM}/{TOTALPAGENUM}");
 			$options=array('fontSize'=>8,'colGap'=>2,'maxWidth'=>565,'titleFontSize'=>8,);
 			$pdf->ezTable($PDF_ARRAY,$PDF_HEADER,utf8_decode($title),$options);
 			$pdf->ezStream();
