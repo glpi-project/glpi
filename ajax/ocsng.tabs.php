38160c9d3d49820cb0dcc133eb79a74dcd33d3de
Walid Nouh <wnouh@teclib.com>
2009-01-27 14:09:21 +0000

    fixed #1266 Use ajax tabs in mode OCS configuration

---
+++ ajax/ocsng.tabs.php
@@ -0,0 +1,72 @@
+<?php
+
+
+/*
+ * @version $Id: config.tabs.php 7765 2009-01-06 18:46:30Z moyo $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file:
+// Purpose of file:
+// ----------------------------------------------------------------------
+$NEEDED_ITEMS = array (
+	"setup",
+	"ocsng",
+	"user",
+	"search",
+	"admininfo"
+);
+
+define('GLPI_ROOT', '..');
+include (GLPI_ROOT . "/inc/includes.php");
+
+if (!isset ($_POST["ID"])) {
+	exit ();
+}
+
+checkRight("ocsng", "w");
+
+$ocs = new Ocsng();
+
+switch ($_POST['glpi_tab']) {
+	case -1 :
+		$ocs->ocsFormConfig($_SERVER['PHP_SELF'], $_POST["ID"],$_POST["withtemplate"]);
+		$ocs->ocsFormAutomaticLinkConfig($_SERVER['PHP_SELF'], $_POST["ID"],$_POST["withtemplate"],$_POST["templateid"]);
+		break;
+	case 1:
+		$ocs->ocsFormConfig($_SERVER['PHP_SELF'], $_POST["ID"],$_POST["withtemplate"],$_POST["templateid"]);
+		break;
+	case 2 :
+		$ocs->ocsFormAutomaticLinkConfig($_SERVER['PHP_SELF'], $_POST["ID"],$_POST["withtemplate"],$_POST["templateid"]);
+		break;	
+	default :
+		break;
+}
+
+ajaxFooter();
+?>
