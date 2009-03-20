75e74f1c680839dda3cde5ce1596b0d1c000d232
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-03-20 12:55:13 +0000

    Don't delete dropdown ID=0 (call to replaceDropDropDown crash the DB)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@8101 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/setup.dropdowns.php
+++ front/setup.dropdowns.php
@@ -116,7 +116,7 @@ if (isset($_POST['mass_delete'])){
 	logEvent(0, "dropdown", 5, "setup", $_SESSION["glpiname"]." ".$LANG['log'][20]." ".$_POST["value"]);
 	glpi_header($_SERVER['PHP_SELF']."?which=$which&value2=$value2&tomove=$tomove&where=$where&type=$type&FK_entities=$FK_entities");
 
-} else if (isset($_POST["delete"])) {
+} else if (isset($_POST["delete"]) && $_POST["ID"]>0) {
 	if(dropdownUsed($_POST["tablename"], $_POST["ID"]) && empty($_POST["forcedelete"])) {
 		if (!strpos($_SERVER['PHP_SELF'],"popup")){
 			commonHeader($LANG['common'][12],$_SERVER['PHP_SELF'],"config","dropdowns");
