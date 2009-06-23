052c8f8905fbc63551cde6f6536cb3929c78d0c3
moyooo <moyo@indepnet.net>
2009-06-23 19:00:07 +0000

    syntax

--- front/rule.common.php
+++ front/rule.common.php
@@ -133,8 +133,7 @@ if (isset($_GET["action"])){
 		$end=explode(" ",microtime());
 		$duree=round($end[0]+$end[1]-$start);
 
-		changeProgressBarMessage($LANG['rulesengine'][91]." (".timestampToString($duree).
-			")");		
+		changeProgressBarMessage($LANG['rulesengine'][91]." (".timestampToString($duree).")");		
 		echo "<a href='".$_SERVER['PHP_SELF']."'>".$LANG['buttons'][13]."</a>";
 	} else {
 		// Need more work
