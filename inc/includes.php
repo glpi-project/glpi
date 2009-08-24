6797d1a5f400fe98f66d7a4964295929fd96f2c3
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-08-24 09:15:10 +0000

    Coding Standards, see #1486 (yllen)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@8752 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/includes.php
+++ inc/includes.php
@@ -33,9 +33,9 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-if (!defined('GLPI_ROOT')){
-	die("Sorry. You can't access directly to this file");
-	}
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
 
 include_once (GLPI_ROOT . "/inc/timer.class.php");
 
@@ -60,98 +60,95 @@ include_once (GLPI_ROOT . "/inc/plugin.class.php");
 // Load Language file
 loadLanguage();
 
-if ($_SESSION['glpi_use_mode']==DEBUG_MODE){
-	$SQL_TOTAL_REQUEST=0;
-	$DEBUG_SQL["queries"]=array();
-	$DEBUG_SQL["errors"]=array();
-	$DEBUG_SQL["times"]=array();
+if ($_SESSION['glpi_use_mode']==DEBUG_MODE) {
+   $SQL_TOTAL_REQUEST=0;
+   $DEBUG_SQL["queries"]=array();
+   $DEBUG_SQL["errors"]=array();
+   $DEBUG_SQL["times"]=array();
 }
 
-
-
-if (!isset($AJAX_INCLUDE)){
-	include_once (GLPI_ROOT . "/inc/auth.class.php");
-	include_once (GLPI_ROOT . "/inc/connection.class.php");
-	include_once (GLPI_ROOT . "/inc/connection.function.php");
-	include_once (GLPI_ROOT . "/inc/mailing.class.php");
-	include_once (GLPI_ROOT . "/inc/mailing.function.php");
-	include_once (GLPI_ROOT . "/inc/export.function.php");
-	include_once (GLPI_ROOT . "/inc/log.function.php");
-	include_once (GLPI_ROOT . "/inc/bookmark.function.php");
-	include_once (GLPI_ROOT . "/inc/bookmark.class.php");
-	include_once (GLPI_ROOT . "/inc/alert.class.php");
+if (!isset($AJAX_INCLUDE)) {
+   include_once (GLPI_ROOT . "/inc/auth.class.php");
+   include_once (GLPI_ROOT . "/inc/connection.class.php");
+   include_once (GLPI_ROOT . "/inc/connection.function.php");
+   include_once (GLPI_ROOT . "/inc/mailing.class.php");
+   include_once (GLPI_ROOT . "/inc/mailing.function.php");
+   include_once (GLPI_ROOT . "/inc/export.function.php");
+   include_once (GLPI_ROOT . "/inc/log.function.php");
+   include_once (GLPI_ROOT . "/inc/bookmark.function.php");
+   include_once (GLPI_ROOT . "/inc/bookmark.class.php");
+   include_once (GLPI_ROOT . "/inc/alert.class.php");
 }
 
 // Security system
-if (isset($_POST)){
-	if (get_magic_quotes_gpc()){
-		$_POST = array_map('stripslashes_deep', $_POST);
-	}
+if (isset($_POST)) {
+   if (get_magic_quotes_gpc()) {
+      $_POST = array_map('stripslashes_deep', $_POST);
+   }
 
-	$_POST = array_map('addslashes_deep', $_POST);
-	$_POST = array_map('clean_cross_side_scripting_deep', $_POST);
+   $_POST = array_map('addslashes_deep', $_POST);
+   $_POST = array_map('clean_cross_side_scripting_deep', $_POST);
 }
-if (isset($_GET)){
-	if (get_magic_quotes_gpc()){
-		$_GET = array_map('stripslashes_deep', $_GET);
-	}
-	$_GET = array_map('addslashes_deep', $_GET);
-	$_GET = array_map('clean_cross_side_scripting_deep', $_GET);
+if (isset($_GET)) {
+   if (get_magic_quotes_gpc()) {
+      $_GET = array_map('stripslashes_deep', $_GET);
+   }
+   $_GET = array_map('addslashes_deep', $_GET);
+   $_GET = array_map('clean_cross_side_scripting_deep', $_GET);
 }
-
-
 
 // Mark if Header is loaded or not :
 $HEADER_LOADED=false;
 $FOOTER_LOADED=false;
-if (isset($AJAX_INCLUDE)){
-	$HEADER_LOADED=true;
+if (isset($AJAX_INCLUDE)) {
+   $HEADER_LOADED=true;
 }
 
-if (isset($NEEDED_ITEMS)&&is_array($NEEDED_ITEMS)){
-	foreach ($NEEDED_ITEMS as $item){
-		if (file_exists(GLPI_ROOT . "/inc/$item.class.php")){
-			include_once (GLPI_ROOT . "/inc/$item.class.php");
-		}
-		if (file_exists(GLPI_ROOT . "/inc/$item.function.php")){
-			include_once (GLPI_ROOT . "/inc/$item.function.php");
-		}
-	}
+if (isset($NEEDED_ITEMS) && is_array($NEEDED_ITEMS)) {
+   foreach ($NEEDED_ITEMS as $item) {
+      if (file_exists(GLPI_ROOT . "/inc/$item.class.php")) {
+         include_once (GLPI_ROOT . "/inc/$item.class.php");
+      }
+      if (file_exists(GLPI_ROOT . "/inc/$item.function.php")) {
+         include_once (GLPI_ROOT . "/inc/$item.function.php");
+      }
+   }
 }
 
 /* On startup, register all plugins configured for use. */
-if (!isset($AJAX_INCLUDE)&&!isset($PLUGINS_INCLUDED)){
-	// PLugin already included
-	$PLUGINS_INCLUDED=1;
-	$LOADED_PLUGINS=array();
-	if (!isset($_SESSION["glpi_plugins"])) {
-		initPlugins();
-	}
-	if (isset($_SESSION["glpi_plugins"]) && is_array($_SESSION["glpi_plugins"])) {
-		//doHook("config");
-
-		if (count($_SESSION["glpi_plugins"])){
-			foreach ($_SESSION["glpi_plugins"] as $name) {
-				usePlugin($name);
-			}
-		}
-	}
+if (!isset($AJAX_INCLUDE) && !isset($PLUGINS_INCLUDED)) {
+   // PLugin already included
+   $PLUGINS_INCLUDED=1;
+   $LOADED_PLUGINS=array();
+   if (!isset($_SESSION["glpi_plugins"])) {
+      initPlugins();
+   }
+   if (isset($_SESSION["glpi_plugins"]) && is_array($_SESSION["glpi_plugins"])) {
+      //doHook("config");
+      if (count($_SESSION["glpi_plugins"])) {
+         foreach ($_SESSION["glpi_plugins"] as $name) {
+            usePlugin($name);
+         }
+      }
+   }
 }
 
 // Get search_option array / need to be included after plugin definition
-if (isset($NEEDED_ITEMS)&&in_array('search', $NEEDED_ITEMS)){
-	$SEARCH_OPTION=getSearchOptions();
+if (isset($NEEDED_ITEMS) && in_array('search', $NEEDED_ITEMS)) {
+   $SEARCH_OPTION=getSearchOptions();
 }
 
-if (!isset($_SESSION["MESSAGE_AFTER_REDIRECT"])) $_SESSION["MESSAGE_AFTER_REDIRECT"]="";
+if (!isset($_SESSION["MESSAGE_AFTER_REDIRECT"])) {
+   $_SESSION["MESSAGE_AFTER_REDIRECT"]="";
+}
 
 // Manage tabs
 if (isset($_REQUEST['glpi_tab'])) {
-	$_SESSION['glpi_tab']=$_REQUEST['glpi_tab'];
+   $_SESSION['glpi_tab']=$_REQUEST['glpi_tab'];
 }
 // Override list-limit if choosen
 if (isset($_REQUEST['glpilist_limit'])) {
-	$_SESSION['glpilist_limit']=$_REQUEST['glpilist_limit'];
+   $_SESSION['glpilist_limit']=$_REQUEST['glpilist_limit'];
 }
 
 ?>
