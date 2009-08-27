f309db5bbc588fdfee05741e6f5870e6a5608a3a
moyooo <moyo@indepnet.net>
2009-08-27 11:17:37 +0000

    Problem of items display in networking port connection when view ID is activated fixed #1627

--- ajax/dropdownConnectPortDevice.php
+++ ajax/dropdownConnectPortDevice.php
@@ -103,7 +103,7 @@ if (isset($LINK_ID_TABLE[$_POST["type"]])&&$_POST["type"]>0){
 			$output = $data['name'];
 			$ID = $data['ID'];
 			if ($_SESSION["glpiview_ID"]||empty($output)) {
-				$output="($ID)";
+				$output.=" ($ID)";
 			}
 			echo "<option value=\"$ID\" title=\"".cleanInputText($output)."\">".substr($output,0,$_SESSION["glpidropdown_limit"])."</option>";
 		}
