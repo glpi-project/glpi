a5c2101cfc8d6b356ced538ca3c31d2c16e8649d
remi <remi>
2009-09-03 17:28:06 +0000

    Coding Standard + is_alreadyplanned fix + SQL optimization, see #1486 (yllen)

--- inc/planning.class.php
+++ inc/planning.class.php
@@ -34,225 +34,229 @@
 // ----------------------------------------------------------------------
 
 if (!defined('GLPI_ROOT')){
-	die("Sorry. You can't access directly to this file");
-	}
-
+   die("Sorry. You can't access directly to this file");
+}
 
 // CLASSES PlanningTracking
 
 class PlanningTracking extends CommonDBTM {
 
-	/**
-	 * Constructor
-	**/
-	function __construct () {
-		$this->table="glpi_ticketsplannings";
-	}
+   /**
+    * Constructor
+   **/
+   function __construct () {
+      $this->table="glpi_ticketsplannings";
+   }
+
+   function prepareInputForUpdate($input) {
+
+      $this->getFromDB($input["id"]);
+      // Save fields
+      $oldfields=$this->fields;
+      // Needed for test already planned
+      $this->fields["users_id"] = $input["users_id"];
+      $this->fields["begin"] = $input["begin"];
+      $this->fields["end"] = $input["end"];
+
+      if (!$this->test_valid_date()) {
+         $this->displayError("date");
+         return false;
+      }
+      if ($this->is_alreadyplanned()) {
+         $this->displayError("is_res");
+         return false;
+      }
+      // Restore fields
+      $this->fields=$oldfields;
+
+      return $input;
+   }
+
+   function post_updateItem($input,$updates,$history=1) {
+      global $CFG_GLPI;
+
+      // Auto update Status
+      $job=new Job();
+      $job->getFromDB($input["tickets_id"]);
+      if ($job->fields["status"]=="new" || $job->fields["status"]=="assign") {
+         $job->fields["status"]="plan";
+         $updates[]="status";
+         $job->updateInDB($updates);
+      }
+
+      // Auto update realtime
+      $fup=new Followup();
+      $fup->getFromDB($input["ticketsfollowups_id"]);
+      $tmp_beg=explode(" ",$input["begin"]);
+      $tmp_end=explode(" ",$input["end"]);
+      $tmp_dbeg=explode("-",$tmp_beg[0]);
+      $tmp_dend=explode("-",$tmp_end[0]);
+      $tmp_hbeg=explode(":",$tmp_beg[1]);
+      $tmp_hend=explode(":",$tmp_end[1]);
+
+      $dateDiff = mktime($tmp_hend[0],$tmp_hend[1],$tmp_hend[2],$tmp_dend[1],$tmp_dend[2],
+                         $tmp_dend[0])
+                  - mktime($tmp_hbeg[0],$tmp_hbeg[1],$tmp_hbeg[2],$tmp_dbeg[1],$tmp_dbeg[2],
+                           $tmp_dbeg[0]);
+      $updates2[]="realtime";
+      $fup->fields["realtime"]=$dateDiff/60/60;
+      $fup->updateInDB($updates2);
+      $job->updateRealTime($input["tickets_id"]);
+
+      if ((!isset($input["_nomail"]) || $input["_nomail"]==0)
+          && count($updates)>0 && $CFG_GLPI["use_mailing"]) {
+
+         $user=new User;
+         $user->getFromDB($_SESSION["glpiID"]);
+         $mail = new Mailing("followup",$job,$user,$fup->fields["is_private"]);
+         $mail->send();
+      }
+   }
+
+   function prepareInputForAdd($input) {
+
+      // Needed for test already planned
+      $this->fields["users_id"] = $input["users_id"];
+      $this->fields["begin"] = $input["begin"];
+      $this->fields["end"] = $input["end"];
+
+      if (!$this->test_valid_date()) {
+         $this->displayError("date");
+         return false;
+      }
+      if ($this->is_alreadyplanned()) {
+         $this->displayError("is_res");
+         return false;
+      }
+      return $input;
+   }
+
+   function post_addItem($newID,$input) {
+      global $CFG_GLPI;
+
+      // Auto update Status
+      $job=new Job();
+      $job->getFromDB($input["tickets_id"]);
+      if ($job->fields["status"]=="new" || $job->fields["status"]=="assign") {
+         $job->fields["status"]="plan";
+         $updates[]="status";
+         $job->updateInDB($updates);
+      }
+
+      // Auto update realtime
+      $fup=new Followup();
+      $fup->getFromDB($input["ticketsfollowups_id"]);
+      if ($fup->fields["realtime"]==0) {
+         $tmp_beg=explode(" ",$this->fields["begin"]);
+         $tmp_end=explode(" ",$this->fields["end"]);
+         $tmp_dbeg=explode("-",$tmp_beg[0]);
+         $tmp_dend=explode("-",$tmp_end[0]);
+         $tmp_hbeg=explode(":",$tmp_beg[1]);
+         $tmp_hend=explode(":",$tmp_end[1]);
+
+         $dateDiff = mktime($tmp_hend[0],$tmp_hend[1],$tmp_hend[2],$tmp_dend[1],$tmp_dend[2],
+                            $tmp_dend[0])
+                     - mktime($tmp_hbeg[0],$tmp_hbeg[1],$tmp_hbeg[2],$tmp_dbeg[1],$tmp_dbeg[2],
+                              $tmp_dbeg[0]);
+         $updates2[]="realtime";
+         $fup->fields["realtime"]=$dateDiff/60/60;
+         $fup->updateInDB($updates2);
+         $job->updateRealTime($input["tickets_id"]);
+      }
+
+      if ((!isset($input["_nomail"]) || $input["_nomail"]==0)
+          && $CFG_GLPI["use_mailing"]) {
+
+         $user=new User;
+         $user->getFromDB($_SESSION["glpiID"]);
+         $mail = new Mailing("followup",$job,$user,$fup->fields["is_private"]);
+         $mail->send();
+      }
+   }
+
+   function pre_deleteItem($ID) {
+
+      if ($this->getFromDB($ID)) {
+         if (isset($this->fields["users_id"]) &&
+             ($this->fields["users_id"]==$_SESSION["glpiID"] || haveRight("comment_all_ticket","1"))) {
+
+            // Auto update realtime
+            $fup=new Followup();
+            $fup->getFromDB($this->fields["ticketsfollowups_id"]);
+            $updates2[]="realtime";
+            $fup->fields["realtime"]=0;
+            $fup->updateInDB($updates2);
+         }
+      }
+      return true;
+   }
 
 
-	function prepareInputForUpdate($input) {
-		$this->getFromDB($input["id"]);
-		// Save fields
-		$oldfields=$this->fields;
+   // SPECIFIC FUNCTIONS
 
-		// Needed for test already planned
-		$this->fields["users_id"] = $input["users_id"];
-		$this->fields["begin"] = $input["begin"];
-		$this->fields["end"] = $input["end"];
+   /**
+    * Is the user assigned to the current planning already planned ?
+    *
+    *@return boolean
+    **/
+   function is_alreadyplanned() {
+      global $DB;
 
-		if (!$this->test_valid_date()){
-			$this->displayError("date");
-			return false;
-		}
+      if (!isset($this->fields["users_id"]) || empty($this->fields["users_id"])) {
+         return true;
+      }
 
-		if ($this->is_alreadyplanned()){
-			$this->displayError("is_res");
-			return false;
-		}
-		// Restore fields
-		$this->fields=$oldfields;
+      // When modify a planning do not itself take into account
+      $ID_where="";
+      if (isset($this->fields["id"])) {
+         $ID_where=" (`id` <> '".$this->fields["id"]."') AND ";
+      }
+      $query = "SELECT *
+                FROM `".$this->table."`
+                WHERE $ID_where
+                      `users_id` = '".$this->fields["users_id"]."'
+                      AND '".$this->fields["end"]."' > `begin`
+                      AND '".$this->fields["begin"]."' < `end`";
 
-		return $input;
-	}
+      if ($result=$DB->query($query)) {
+         return ($DB->numrows($result)>0);
+      }
+      return true;
+   }
 
-	function post_updateItem($input,$updates,$history=1) {
-		global $CFG_GLPI;
+   /**
+    * Current dates are valid ? begin before end
+    *
+    *@return boolean
+    **/
+   function test_valid_date() {
+      return (!empty($this->fields["begin"]) && !empty($this->fields["end"])
+              && strtotime($this->fields["begin"]) < strtotime($this->fields["end"]));
+   }
 
-		// Auto update Status
-		$job=new Job();
+   /**
+    * Add error message to message after redirect
+    * @param $type error type : date / is_res / other
+    *@return nothing
+    **/
+   function displayError($type) {
+      global $LANG;
 
-		$job->getFromDB($input["tickets_id"]);
-		if ($job->fields["status"]=="new"||$job->fields["status"]=="assign"){
-			$job->fields["status"]="plan";
-			$updates[]="status";
-			$job->updateInDB($updates);		
-		}
+      switch ($type) {
+         case "date" :
+            addMessageAfterRedirect($LANG['planning'][1],false,ERROR);
+            break;
 
-		// Auto update realtime
-		$fup=new Followup();
-		$fup->getFromDB($input["ticketsfollowups_id"]);
-		$tmp_beg=explode(" ",$input["begin"]);
-		$tmp_end=explode(" ",$input["end"]);
-		$tmp_dbeg=explode("-",$tmp_beg[0]);
-		$tmp_dend=explode("-",$tmp_end[0]);
-		$tmp_hbeg=explode(":",$tmp_beg[1]);
-		$tmp_hend=explode(":",$tmp_end[1]);
+         case "is_res" :
+            addMessageAfterRedirect($LANG['planning'][0],false,ERROR);
+            break;
 
-		$dateDiff = mktime($tmp_hend[0],$tmp_hend[1],$tmp_hend[2],$tmp_dend[1],$tmp_dend[2],$tmp_dend[0]) 
-			- mktime($tmp_hbeg[0],$tmp_hbeg[1],$tmp_hbeg[2],$tmp_dbeg[1],$tmp_dbeg[2],$tmp_dbeg[0]);		
-		$updates2[]="realtime";
-		$fup->fields["realtime"]=$dateDiff/60/60;
-		$fup->updateInDB($updates2);
-		$job->updateRealTime($input["tickets_id"]);
-
-		if ((!isset($input["_nomail"])||$input["_nomail"]==0)
-		&&count($updates)>0 && $CFG_GLPI["use_mailing"]){
-			$user=new User;
-			$user->getFromDB($_SESSION["glpiID"]);
-			$mail = new Mailing("followup",$job,$user,$fup->fields["is_private"]);
-			$mail->send();
-		}
-
-	}
-
-
-	function prepareInputForAdd($input) {
-		// Needed for test already planned
-		$this->fields["users_id"] = $input["users_id"];
-		$this->fields["begin"] = $input["begin"];
-		$this->fields["end"] = $input["end"];
-
-		if (!$this->test_valid_date()){
-			$this->displayError("date");
-			return false;
-		}
-
-		if ($this->is_alreadyplanned()){
-			$this->displayError("is_res");
-			return false;
-		}
-
-		return $input;
-	}
-
-
-	function post_addItem($newID,$input) {
-		global $CFG_GLPI;
-		
-		// Auto update Status
-		$job=new Job();
-		$job->getFromDB($input["tickets_id"]);
-		if ($job->fields["status"]=="new"||$job->fields["status"]=="assign"){
-			$job->fields["status"]="plan";
-			$updates[]="status";
-			$job->updateInDB($updates);		
-		}
-
-		// Auto update realtime
-		$fup=new Followup();
-		$fup->getFromDB($input["ticketsfollowups_id"]);
-
-		if ($fup->fields["realtime"]==0){
-			$tmp_beg=explode(" ",$this->fields["begin"]);
-			$tmp_end=explode(" ",$this->fields["end"]);
-			$tmp_dbeg=explode("-",$tmp_beg[0]);
-			$tmp_dend=explode("-",$tmp_end[0]);
-			$tmp_hbeg=explode(":",$tmp_beg[1]);
-			$tmp_hend=explode(":",$tmp_end[1]);
-
-			$dateDiff = mktime($tmp_hend[0],$tmp_hend[1],$tmp_hend[2],$tmp_dend[1],$tmp_dend[2],$tmp_dend[0]) 
-				- mktime($tmp_hbeg[0],$tmp_hbeg[1],$tmp_hbeg[2],$tmp_dbeg[1],$tmp_dbeg[2],$tmp_dbeg[0]);		
-			$updates2[]="realtime";
-			$fup->fields["realtime"]=$dateDiff/60/60;
-			$fup->updateInDB($updates2);
-			$job->updateRealTime($input["tickets_id"]);
-		}
-
-		if ((!isset($input["_nomail"])||$input["_nomail"]==0)&&$CFG_GLPI["use_mailing"]){
-			$user=new User;
-			$user->getFromDB($_SESSION["glpiID"]);
-			$mail = new Mailing("followup",$job,$user,$fup->fields["is_private"]);
-			$mail->send();
-		}
-
-	}
-
-	function pre_deleteItem($ID) {
-
-		if ($this->getFromDB($ID)){
-			if (isset($this->fields["users_id"])&&($this->fields["users_id"]==$_SESSION["glpiID"]||haveRight("comment_all_ticket","1"))){
-				// Auto update realtime
-				$fup=new Followup();
-				$fup->getFromDB($this->fields["ticketsfollowups_id"]);
-				$updates2[]="realtime";
-				$fup->fields["realtime"]=0;
-				$fup->updateInDB($updates2);
-			}
-		} 
-		return true;
-	}
-
-
-	// SPECIFIC FUNCTIONS
-
-	/**
-	 * Is the user assigned to the current planning already planned ?
-	 *
-	 *@return boolean
-	 **/
-	function is_alreadyplanned(){
-		global $DB;
-		if (!isset($this->fields["users_id"])||empty($this->fields["users_id"]))
-			return true;
-
-		// When modify a planning do not itself take into account 
-		$ID_where="";
-		if(isset($this->fields["id"]))
-			$ID_where=" (id <> '".$this->fields["id"]."') AND ";
-
-		$query = "SELECT * FROM ".$this->table." ".
-			" WHERE $ID_where (users_id = '".$this->fields["users_id"]."') AND ".
-			" ( ('".$this->fields["begin"]."' < begin AND '".$this->fields["end"]."' > begin) ".
-			" OR ('".$this->fields["begin"]."' < end AND '".$this->fields["end"]."' >= end) ".
-			" OR ('".$this->fields["begin"]."' >= begin AND '".$this->fields["end"]."' < end))";
-		//		echo $query."<br>";
-		if ($result=$DB->query($query)){
-			return ($DB->numrows($result)>0);
-		}
-		return true;
-	}
-	/**
-	 * Current dates are valid ? begin before end
-	 *
-	 *@return boolean
-	 **/
-	function test_valid_date(){
-		return (!empty($this->fields["begin"])&&!empty($this->fields["end"])
-			&&strtotime($this->fields["begin"])<strtotime($this->fields["end"]));
-	}
-
-	/**
-	 * Add error message to message after redirect
-	 * @param $type error type : date / is_res / other
-	 *@return nothing
-	 **/
-	function displayError($type){
-		global $LANG;
-
-		switch ($type){
-			case "date":
-				addMessageAfterRedirect($LANG['planning'][1],false,ERROR);
-			break;
-			case "is_res":
-				addMessageAfterRedirect($LANG['planning'][0],false,ERROR);
-			break;
-			default :
-				addMessageAfterRedirect($LANG['common'][61],false,ERROR);
-			break;
-		}
-	}
+         default :
+            addMessageAfterRedirect($LANG['common'][61],false,ERROR);
+            break;
+      }
+   }
 
 }
-
 
 ?>
