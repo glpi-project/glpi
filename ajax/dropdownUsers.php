695703d59299851ac8b8dd08c9ba7f558c2f6efd
moyooo <moyo@indepnet.net>
2009-09-06 17:33:48 +0000

    w3c

--- ajax/dropdownUsers.php
+++ ajax/dropdownUsers.php
@@ -109,7 +109,7 @@ if ($_POST['searchText']!=$CFG_GLPI["ajax_wildcard"]
 if ($_POST['all']==0) {
    echo "<option value='0'>------</option>";
 } else if($_POST['all']==1) {
-   echo "<option value='0''>[".$LANG['common'][66]."]</option>";
+   echo "<option value='0'>[".$LANG['common'][66]."]</option>";
 }
 
 if (isset($_POST['value'])) {
