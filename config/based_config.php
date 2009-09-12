b22796a35d1a219c620edfb44d5118aa28bc5b8a
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-09-12 21:19:39 +0000

    Coding Standard, see #1486 (yllen)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@8958 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- config/based_config.php
+++ config/based_config.php
@@ -29,89 +29,89 @@
  --------------------------------------------------------------------------
  */
 
-
-if (!defined('GLPI_ROOT')){
-	die("Sorry. You can't access directly to this file");
-	}
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
 
 // Notice problem  for date function :
-if (function_exists('date_default_timezone_set')){
-	$tz=ini_get('date.timezone');
-	if (!empty($tz)){
-		date_default_timezone_set($tz);
-	} else {
-		date_default_timezone_set(@date_default_timezone_get());
-	}
+if (function_exists('date_default_timezone_set')) {
+   $tz=ini_get('date.timezone');
+   if (!empty($tz)) {
+      date_default_timezone_set($tz);
+   } else {
+      date_default_timezone_set(@date_default_timezone_get());
+   }
 }
 
 // If this file exists, it is load, allow to set configdir/dumpdir elsewhere
-if(file_exists(GLPI_ROOT ."/config/config_path.php")) {
-	include_once(GLPI_ROOT ."/config/config_path.php");
+if (file_exists(GLPI_ROOT ."/config/config_path.php")) {
+   include_once(GLPI_ROOT ."/config/config_path.php");
 }
 
 // Default location for database configuration : config_db.php
-if (!defined("GLPI_CONFIG_DIR")){
-	define("GLPI_CONFIG_DIR",GLPI_ROOT . "/config");
+if (!defined("GLPI_CONFIG_DIR")) {
+   define("GLPI_CONFIG_DIR",GLPI_ROOT . "/config");
 }
 
 // Default location for backup dump
-if (!defined("GLPI_DUMP_DIR")){
-	define("GLPI_DUMP_DIR",GLPI_ROOT . "/files/_dumps");
+if (!defined("GLPI_DUMP_DIR")) {
+   define("GLPI_DUMP_DIR",GLPI_ROOT . "/files/_dumps");
 }
 
 // Path for documents storage
-if (!defined("GLPI_DOC_DIR")){
-	define("GLPI_DOC_DIR",GLPI_ROOT . "/files");
+if (!defined("GLPI_DOC_DIR")) {
+   define("GLPI_DOC_DIR",GLPI_ROOT . "/files");
 }
 
 // Path for cache storage
-if (!defined("GLPI_CACHE_DIR")){
-	// Need / at the end for Cache Lite compatibility
-	define("GLPI_CACHE_DIR",GLPI_ROOT . "/files/_cache/");
+if (!defined("GLPI_CACHE_DIR")) {
+   // Need / at the end for Cache Lite compatibility
+   define("GLPI_CACHE_DIR",GLPI_ROOT . "/files/_cache/");
 }
 
 // Path for cron storage
-if (!defined("GLPI_CRON_DIR")){
-	define("GLPI_CRON_DIR",GLPI_ROOT . "/files/_cron");
+if (!defined("GLPI_CRON_DIR")) {
+   define("GLPI_CRON_DIR",GLPI_ROOT . "/files/_cron");
 }
 
 // Path for sessions storage
-if (!defined("GLPI_SESSION_DIR")){
-	define("GLPI_SESSION_DIR",GLPI_ROOT . "/files/_sessions");
+if (!defined("GLPI_SESSION_DIR")) {
+   define("GLPI_SESSION_DIR",GLPI_ROOT . "/files/_sessions");
 }
 
 // Path for plugins documents storage
-if (!defined("GLPI_PLUGIN_DOC_DIR")){
-	define("GLPI_PLUGIN_DOC_DIR",GLPI_ROOT . "/files/_plugins");
+if (!defined("GLPI_PLUGIN_DOC_DIR")) {
+   define("GLPI_PLUGIN_DOC_DIR",GLPI_ROOT . "/files/_plugins");
 }
 // Path for cache storage
-if (!defined("GLPI_LOCK_DIR")){
-	define("GLPI_LOCK_DIR",GLPI_ROOT . "/files/_lock");
+if (!defined("GLPI_LOCK_DIR")) {
+   define("GLPI_LOCK_DIR",GLPI_ROOT . "/files/_lock");
 }
 
 // Path for log storage
-if (!defined("GLPI_LOG_DIR")){
-	define("GLPI_LOG_DIR",GLPI_ROOT . "/files/_log");
+if (!defined("GLPI_LOG_DIR")) {
+   define("GLPI_LOG_DIR",GLPI_ROOT . "/files/_log");
 }
 
 // Default location scripts
-if (!defined("GLPI_SCRIPT_DIR")){
-	define("GLPI_SCRIPT_DIR",GLPI_ROOT . "/scripts");
+if (!defined("GLPI_SCRIPT_DIR")) {
+   define("GLPI_SCRIPT_DIR",GLPI_ROOT . "/scripts");
 }
 
 // Default cache_lite installation dir
-if (!defined("GLPI_CACHE_LITE_DIR")){
-	define("GLPI_CACHE_LITE_DIR", GLPI_ROOT."/lib/cache_lite");
+if (!defined("GLPI_CACHE_LITE_DIR")) {
+   define("GLPI_CACHE_LITE_DIR", GLPI_ROOT."/lib/cache_lite");
 
-	# if PEAR + Cache_Lite installed, use (in config_path.php)
-	# define("GLPI_CACHE_LITE_DIR", "Cache");
+   # if PEAR + Cache_Lite installed, use (in config_path.php)
+   # define("GLPI_CACHE_LITE_DIR", "Cache");
 }
 
 // Default PHPMailer installation dir
-if (!defined("GLPI_PHPMAILER_DIR")){
-	define("GLPI_PHPMAILER_DIR", GLPI_ROOT."/lib/phpmailer");
+if (!defined("GLPI_PHPMAILER_DIR")) {
+   define("GLPI_PHPMAILER_DIR", GLPI_ROOT."/lib/phpmailer");
 
-	# if PHPMailer installed, use (in config_path.php)
-	# define("GLPI_PHPMAILER_DIR", "/usr/share/php/phpmailer");
+   # if PHPMailer installed, use (in config_path.php)
+   # define("GLPI_PHPMAILER_DIR", "/usr/share/php/phpmailer");
+
 }
 ?>
--- config/config.php
+++ config/config.php
@@ -33,178 +33,167 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-	if (!defined('GLPI_ROOT')){
-		die("Sorry. You can't access directly to this file");
-		}
-	include_once (GLPI_ROOT."/config/based_config.php");
-	include_once (GLPI_ROOT."/inc/dbreplicate.function.php");
-	include (GLPI_ROOT."/config/define.php");
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
 
-	setGlpiSessionPath();
-	startGlpiSession();
+include_once (GLPI_ROOT."/config/based_config.php");
+include_once (GLPI_ROOT."/inc/dbreplicate.function.php");
+include (GLPI_ROOT."/config/define.php");
 
-	if(!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
-		nullHeader("DB Error",$_SERVER['PHP_SELF']);
-		if (!isCommandLine()){
-			echo "<div class='center'>";
-			echo "<p>Error : GLPI seems to not be installed properly.</p><p> config_db.php file is missing.</p><p>Please restart the install process.</p>";
-			echo "<p><a class='red' href='".GLPI_ROOT."'>Click here to proceed</a></p>";			
-			echo "</div>";
-		} else {
-			echo "Error : GLPI seems to not be installed properly.</p><p> config_db.php file is missing.\n";
-			echo "Please restart the install process.\n";
-			
-		}
-		nullFooter("DB Error",$_SERVER['PHP_SELF']);
-	
-		die();
-	} else {
-	
-		require_once (GLPI_CONFIG_DIR . "/config_db.php");
-		include_once (GLPI_CACHE_LITE_DIR."/Lite/Output.php");
-		include_once (GLPI_CACHE_LITE_DIR."/Lite/File.php");
+setGlpiSessionPath();
+startGlpiSession();
 
-		//Database connection
-		establishDBConnection((isset($USEDBREPLICATE)?$USEDBREPLICATE:0),
-		(isset($DBCONNECTION_REQUIRED)?$DBCONNECTION_REQUIRED:0));
+if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
+   nullHeader("DB Error",$_SERVER['PHP_SELF']);
+   if (!isCommandLine()) {
+      echo "<div class='center'>";
+      echo "<p>Error : GLPI seems to not be installed properly.</p>";
+      echo "<p> config_db.php file is missing.</p>";
+      echo "<p>Please restart the install process.</p>";
+      echo "<p><a class='red' href='".GLPI_ROOT."'>Click here to proceed</a></p>";
+      echo "</div>";
+   } else {
+      echo "Error : GLPI seems to not be installed properly.\n";
+      echo "config_db.php file is missing.\n";
+      echo "Please restart the install process.\n";
+   }
+   nullFooter("DB Error",$_SERVER['PHP_SELF']);
 
+   die();
+} else {
+   require_once (GLPI_CONFIG_DIR . "/config_db.php");
+   include_once (GLPI_CACHE_LITE_DIR."/Lite/Output.php");
+   include_once (GLPI_CACHE_LITE_DIR."/Lite/File.php");
 
-		// *************************** Statics config options **********************
-		// ********************options d'installation statiques*********************
-		// ***********************************************************************		
-
-		//Options from DB, do not touch this part.
+   //Database connection
+   establishDBConnection((isset($USEDBREPLICATE) ? $USEDBREPLICATE : 0),
+                         (isset($DBCONNECTION_REQUIRED) ? $DBCONNECTION_REQUIRED : 0));
 
 
-		// Default Use mode
-		if (!isset($_SESSION['glpi_use_mode'])){
-			$_SESSION['glpi_use_mode']=NORMAL_MODE;
-		}
+   // *************************** Statics config options **********************
+   // ********************options d'installation statiques*********************
+   // *************************************************************************
 
-		$config_object=new Config();
-      $config_ok=false;
+   //Options from DB, do not touch this part.
 
-      if (isset($TRY_OLD_CONFIG_FIRST) // index case
-         || (isset($_SESSION['TRY_OLD_CONFIG_FIRST']) && $_SESSION['TRY_OLD_CONFIG_FIRST'])) { // backup case
-         if (isset($_SESSION['TRY_OLD_CONFIG_FIRST'])){
-            unset($_SESSION['TRY_OLD_CONFIG_FIRST']);
-         }
-         // First try old config table : for update proces management from < 0.80 to >= 0.80
-         $config_object->table='glpi_config';
-         if($config_object->getFromDB(1)) {
+   // Default Use mode
+   if (!isset($_SESSION['glpi_use_mode'])) {
+      $_SESSION['glpi_use_mode']=NORMAL_MODE;
+   }
+
+   $config_object=new Config();
+   $config_ok=false;
+
+   if (isset($TRY_OLD_CONFIG_FIRST) // index case
+       || (isset($_SESSION['TRY_OLD_CONFIG_FIRST']) && $_SESSION['TRY_OLD_CONFIG_FIRST'])) { // backup case
+
+      if (isset($_SESSION['TRY_OLD_CONFIG_FIRST'])) {
+         unset($_SESSION['TRY_OLD_CONFIG_FIRST']);
+      }
+      // First try old config table : for update proces management from < 0.80 to >= 0.80
+      $config_object->table='glpi_config';
+      if ($config_object->getFromDB(1)) {
+         $config_ok=true;
+      } else {
+         $config_object->table='glpi_configs';
+         if ($config_object->getFromDB(1)) {
             $config_ok=true;
-         } else {
-            $config_object->table='glpi_configs';
-            if($config_object->getFromDB(1)) {
-               $config_ok=true;
-            }
-         }
-      } else { // Normal load process : use normal config table. If problem try old one
-         if($config_object->getFromDB(1)) {
-            $config_ok=true;
-         } else {
-            // Manage glpi_config table before 0.80
-            $config_object->table='glpi_config';
-            if($config_object->getFromDB(1)) {
-               $config_ok=true;
-            }
          }
       }
+   } else { // Normal load process : use normal config table. If problem try old one
+      if($config_object->getFromDB(1)) {
+         $config_ok=true;
+      } else {
+         // Manage glpi_config table before 0.80
+         $config_object->table='glpi_config';
+         if ($config_object->getFromDB(1)) {
+            $config_ok=true;
+         }
+      }
+   }
 
-      if ($config_ok){
-			$CFG_GLPI=array_merge($CFG_GLPI,$config_object->fields);
+   if ($config_ok) {
+      $CFG_GLPI=array_merge($CFG_GLPI,$config_object->fields);
+      if ( !isset($_SERVER['REQUEST_URI']) ) {
+         $_SERVER['REQUEST_URI'] = $_SERVER['PHP_SELF'];
+      }
+      $currentdir=getcwd();
+      chdir(GLPI_ROOT);
+      $glpidir=str_replace(str_replace('\\', '/',getcwd()),"",str_replace('\\', '/',$currentdir));
+      chdir($currentdir);
+      $globaldir=preg_replace("/\/[0-9a-zA-Z\.\-\_]+\.php/","",$_SERVER['REQUEST_URI']);
+      $globaldir=preg_replace("/\?.*/","",$globaldir);
+      $CFG_GLPI["root_doc"]=str_replace($glpidir,"",$globaldir);
+      $CFG_GLPI["root_doc"]=preg_replace("/\/$/","",$CFG_GLPI["root_doc"]);
+      // urldecode for space redirect to encoded URL : change entity
+      $CFG_GLPI["root_doc"]=urldecode($CFG_GLPI["root_doc"]);
+      // Path for icon of document type
+      $CFG_GLPI["typedoc_icon_dir"] = $CFG_GLPI["root_doc"]."/pics/icones";
+   } else {
+      echo "Error accessing config table";
+      exit();
+   }
 
-			if ( !isset($_SERVER['REQUEST_URI']) ) {
-				$_SERVER['REQUEST_URI'] = $_SERVER['PHP_SELF'];
-			}
-			$currentdir=getcwd();
-			chdir(GLPI_ROOT);
-			$glpidir=str_replace(str_replace('\\', '/',getcwd()),"",str_replace('\\', '/',$currentdir));
-			chdir($currentdir);
-			
-			$globaldir=preg_replace("/\/[0-9a-zA-Z\.\-\_]+\.php/","",$_SERVER['REQUEST_URI']);
-			$globaldir=preg_replace("/\?.*/","",$globaldir);
-			$CFG_GLPI["root_doc"]=str_replace($glpidir,"",$globaldir);
-			$CFG_GLPI["root_doc"]=preg_replace("/\/$/","",$CFG_GLPI["root_doc"]);
-			// urldecode for space redirect to encoded URL : change entity
-			$CFG_GLPI["root_doc"]=urldecode($CFG_GLPI["root_doc"]);
-	
-			// Path for icon of document type
-                        $CFG_GLPI["typedoc_icon_dir"] = $CFG_GLPI["root_doc"]."/pics/icones";
+   // If debug mode activated : display some informations
+   if ($_SESSION['glpi_use_mode']==DEBUG_MODE) {
+      // display_errors only need for for E_ERROR, E_PARSE, ... which cannot be catched
+      ini_set('display_errors','On');
+      // Recommended development settings
+      error_reporting(E_ALL | E_STRICT);
+      set_error_handler('userErrorHandlerDebug');
+   } else {
+      // Recommended production settings
+      ini_set('display_errors','Off');
+      error_reporting(E_ALL);
+      set_error_handler('userErrorHandlerNormal');
+   }
 
-		} else {
-			echo "Error accessing config table";
-			exit();
-		}
+   if (isset($_SESSION["glpiroot"]) && $CFG_GLPI["root_doc"]!=$_SESSION["glpiroot"]) {
+      glpi_header($_SESSION["glpiroot"]);
+   }
 
-		// If debug mode activated : display some informations
-		if ($_SESSION['glpi_use_mode']==DEBUG_MODE){
-			// display_errors only need for for E_ERROR, E_PARSE, ... which cannot be catched
-			ini_set('display_errors','On'); 
-			// Recommended development settings
-			error_reporting(E_ALL | E_STRICT); 
-			set_error_handler('userErrorHandlerDebug'); 
-		}else{
-			// Recommended production settings
-			ini_set('display_errors','Off'); 
-			error_reporting(E_ALL); 
-			set_error_handler('userErrorHandlerNormal'); 
-		}
-	
-		if (isset($_SESSION["glpiroot"])&&$CFG_GLPI["root_doc"]!=$_SESSION["glpiroot"]) {
-			glpi_header($_SESSION["glpiroot"]);
-		}
-	
-	
-	
-		// Override cfg_features by session value
-		foreach ($CFG_GLPI['user_pref_field'] as $field){
-			if (!isset($_SESSION["glpi$field"]) && isset($CFG_GLPI[$field])){
-				$_SESSION["glpi$field"]=$CFG_GLPI[$field];
-			}
-		}
+   // Override cfg_features by session value
+   foreach ($CFG_GLPI['user_pref_field'] as $field) {
+      if (!isset($_SESSION["glpi$field"]) && isset($CFG_GLPI[$field])) {
+         $_SESSION["glpi$field"]=$CFG_GLPI[$field];
+      }
+   }
 
-		if ((!isset($CFG_GLPI["version"])||trim($CFG_GLPI["version"])!=GLPI_VERSION)&&!isset($_GET["donotcheckversion"])){
-			loadLanguage();
-			
-			if (isCommandLine()){
-				echo $LANG['update'][88] . "\n";
-			} else {			
-				nullHeader("UPDATE NEEDED",$_SERVER['PHP_SELF']);
-				echo "<div class='center'>";
-		
-		
-				echo "<table class='tab_check'>";
-		
-				$error=commonCheckForUseGLPI();
-		
-				echo "</table><br>";
-		
-				if (!$error){
-					if (!isset($CFG_GLPI["version"])||trim($CFG_GLPI["version"])<GLPI_VERSION){
-						echo "<form method='post' action='".$CFG_GLPI["root_doc"]."/install/update.php'>";
-						echo "<p class='red'>";
-						echo $LANG['update'][88];
-						echo "</p>";						
-						echo "<input type='submit' name='from_update' value='".$LANG['install'][4]."' class='submit'>";
-						echo "</form>";
-					} else if (trim($CFG_GLPI["version"])>GLPI_VERSION){
-						echo "<p class='red'>";
-						echo $LANG['update'][89];
-						
-						echo "</p>";
-					}
-				} else {
-					echo "<form action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">";
-					echo "<input type=\"submit\" name=\"submit\" class=\"submit\" value=\"".$LANG['install'][27]."\" />";
-					echo "</form>";
-				}
-				echo "</div>";
-				nullFooter();
-			}
-			exit();
-		} 
-	}
+   if ((!isset($CFG_GLPI["version"]) || trim($CFG_GLPI["version"])!=GLPI_VERSION)
+       && !isset($_GET["donotcheckversion"])) {
 
+      loadLanguage();
+      if (isCommandLine()) {
+         echo $LANG['update'][88] . "\n";
+      } else {
+         nullHeader("UPDATE NEEDED",$_SERVER['PHP_SELF']);
+         echo "<div class='center'>";
+         echo "<table class='tab_check'>";
+         $error=commonCheckForUseGLPI();
+         echo "</table><br>";
+
+         if (!$error) {
+            if (!isset($CFG_GLPI["version"]) || trim($CFG_GLPI["version"])<GLPI_VERSION) {
+               echo "<form method='post' action='".$CFG_GLPI["root_doc"]."/install/update.php'>";
+               echo "<p class='red'>".$LANG['update'][88]."</p>";
+               echo "<input type='submit' name='from_update' value='".
+                      $LANG['install'][4]."' class='submit'>";
+               echo "</form>";
+            } else if (trim($CFG_GLPI["version"])>GLPI_VERSION) {
+               echo "<p class='red'>".$LANG['update'][89]."</p>";
+            }
+         } else {
+            echo "<form action=\"".$_SERVER['PHP_SELF']."\" method='post'>";
+            echo "<input type='submit' name='submit' class='submit' value=\"".
+                   $LANG['install'][27]."\" />";
+            echo "</form>";
+         }
+         echo "</div>";
+         nullFooter();
+      }
+      exit();
+   }
+}
 
 ?>
