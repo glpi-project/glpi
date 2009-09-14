2355e474ccf0eb4d584e7b034daaabb95afbb5e4
Walid Nouh <wnouh@teclib.com>
2009-09-14 12:57:30 +0000

    [0.72] Remove ajaxinclude in order to be used with plugin types

--- ajax/dropdownConnect.php
+++ ajax/dropdownConnect.php
@@ -36,7 +36,6 @@
 
 if(strpos($_SERVER['PHP_SELF'],"dropdownConnect.php")){
 	define('GLPI_ROOT','..');
-	$AJAX_INCLUDE=1;
 	include (GLPI_ROOT."/inc/includes.php");
 	header("Content-Type: text/html; charset=UTF-8");
 	header_nocache();
