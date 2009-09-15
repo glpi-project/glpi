d915766514578df61b34467e610b2728f28ff195
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-09-15 16:32:15 +0000

    Coding Standard, see #1486 (yllen)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@8982 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/rule.tracking.class.php
+++ inc/rule.tracking.class.php
@@ -33,115 +33,120 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 if (!defined('GLPI_ROOT')) {
-	die("Sorry. You can't access directly to this file");
+   die("Sorry. You can't access directly to this file");
 }
 
 class TrackingBusinessRuleCollection extends RuleCollection {
 
-	/**
-	 * Constructor
-	**/
-	function __construct() {
-		parent::__construct(RULE_TRACKING_AUTO_ACTION);
-		$this->rule_class_name="TrackingBusinessRule";
-		$this->right="rule_ticket";
-		$this->use_output_rule_process_as_next_input=true;
-	}
+   /**
+    * Constructor
+   **/
+   function __construct() {
+      parent::__construct(RULE_TRACKING_AUTO_ACTION);
 
-	function getTitle() {
-		global $LANG;
-		return $LANG['rulesengine'][28];
-	}
+      $this->rule_class_name="TrackingBusinessRule";
+      $this->right="rule_ticket";
+      $this->use_output_rule_process_as_next_input=true;
+   }
 
-	function preProcessPreviewResults($output)
-	{
-		return showPreviewAssignAction($output);
-	}
+   function getTitle() {
+      global $LANG;
+
+      return $LANG['rulesengine'][28];
+   }
+
+   function preProcessPreviewResults($output) {
+      return showPreviewAssignAction($output);
+   }
+
 }
+
 
 class TrackingBusinessRule extends Rule {
 
-	/**
-	 * Constructor
-	**/
-	function __construct() {
-		$this->table = "glpi_rules";
-		$this->type = -1;
-		$this->sub_type = RULE_TRACKING_AUTO_ACTION;
-		$this->right="rule_ticket";
-		$this->can_sort=true;		
+   /**
+    * Constructor
+   **/
+   function __construct() {
 
-	}
+      $this->table = "glpi_rules";
+      $this->type = -1;
+      $this->sub_type = RULE_TRACKING_AUTO_ACTION;
+      $this->right="rule_ticket";
+      $this->can_sort=true;
+   }
 
-	function maxActionsCount(){
-		global $RULES_ACTIONS;
-		return count($RULES_ACTIONS[RULE_TRACKING_AUTO_ACTION]);
-	}
+   function maxActionsCount() {
+      global $RULES_ACTIONS;
 
-	function addSpecificParamsForPreview($input,$params)
-	{
-		if (!isset($params["entities_id"]))
-			$params["entities_id"] = $_SESSION["glpiactive_entity"];
-		return $params;
-	}
+      return count($RULES_ACTIONS[RULE_TRACKING_AUTO_ACTION]);
+   }
 
-	/**
-	 * Function used to display type specific criterias during rule's preview
-	 * @param $fields fields values
-	 */
-	function showSpecificCriteriasForPreview($fields){
-		echo "<input type='hidden' name='entities_id' value='".$_SESSION["glpiactive_entity"]."'>";
-	}
+   function addSpecificParamsForPreview($input,$params) {
 
-	function executeActions($output,$params,$regex_results)
-	{
-		if (count($this->actions)){
-			foreach ($this->actions as $action){
-				switch ($action->fields["action_type"]){
-					case "assign" :
-						$output[$action->fields["field"]] = $action->fields["value"];
-					break;
-					case "affectbyip":
-					case "affectbyfqdn":
-					case "affectbymac":
-						if (!isset($output["entities_id"]))
-							$output["entities_id"]=$params["entities_id"];
-						$regexvalue = getRegexResultById($action->fields["value"],$regex_results);
-						
-						switch ($action->fields["action_type"])
-						{
-							case "affectbyip":
-								$result = getUniqueObjectIDByIPAddressOrMac($regexvalue,"IP",$output["entities_id"]);
-								break;
-							case "affectbyfqdn":
-								$result= getUniqueObjectIDByFQDN($regexvalue,$output["entities_id"]);	
-								break;
-							case "affectbymac":
-								$result = getUniqueObjectIDByIPAddressOrMac($regexvalue,"MAC",$output["entities_id"]);
-								break;
-							default:
-								$result=array();	
-								break;
-						}
-					
-						if (!empty($result))
-						{
-							$output["itemtype"]=$result["itemtype"];
-							$output["items_id"]=$result["id"];
-						}
-					break;
-				}
-			}
-		}
-		return $output;
-	}
+      if (!isset($params["entities_id"])) {
+         $params["entities_id"] = $_SESSION["glpiactive_entity"];
+      }
+      return $params;
+   }
 
-	function preProcessPreviewResults($output)
-	{
-		return showPreviewAssignAction($output);
-	}
+   /**
+    * Function used to display type specific criterias during rule's preview
+    * @param $fields fields values
+    */
+   function showSpecificCriteriasForPreview($fields) {
+      echo "<input type='hidden' name='entities_id' value='".$_SESSION["glpiactive_entity"]."'>";
+   }
+
+   function executeActions($output,$params,$regex_results) {
+
+      if (count($this->actions)) {
+         foreach ($this->actions as $action) {
+            switch ($action->fields["action_type"]) {
+               case "assign" :
+                  $output[$action->fields["field"]] = $action->fields["value"];
+                  break;
+
+               case "affectbyip" :
+               case "affectbyfqdn" :
+               case "affectbymac" :
+                  if (!isset($output["entities_id"])) {
+                     $output["entities_id"]=$params["entities_id"];
+                  }
+                  $regexvalue = getRegexResultById($action->fields["value"],$regex_results);
+                  switch ($action->fields["action_type"]) {
+                     case "affectbyip" :
+                        $result = getUniqueObjectIDByIPAddressOrMac($regexvalue,"IP",
+                                                                    $output["entities_id"]);
+                        break;
+
+                     case "affectbyfqdn" :
+                        $result= getUniqueObjectIDByFQDN($regexvalue,$output["entities_id"]);
+                        break;
+
+                     case "affectbymac" :
+                        $result = getUniqueObjectIDByIPAddressOrMac($regexvalue,"MAC",
+                                                                    $output["entities_id"]);
+                        break;
+
+                     default:
+                        $result=array();
+                  }
+                  if (!empty($result)) {
+                     $output["itemtype"]=$result["itemtype"];
+                     $output["items_id"]=$result["id"];
+                  }
+                  break;
+            }
+         }
+      }
+      return $output;
+   }
+
+   function preProcessPreviewResults($output) {
+      return showPreviewAssignAction($output);
+   }
+
 }
-
-
 
 ?>
