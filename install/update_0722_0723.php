3a596d5315d0123b21ae87891320a0b078243014
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-09-15 19:37:41 +0000

    correct update for doc device
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@8991 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_0722_0723.php
+++ install/update_0722_0723.php
@@ -38,7 +38,7 @@
 /// Update from 0.72.2 to 0.72.3
 
 function update0722to0723() {
-	global $DB, $CFG_GLPI, $LANG,$LINK_ID_TABLE;
+	global $DB, $CFG_GLPI, $LANG;
 
 
 	echo "<h3>".$LANG['install'][4]." -&gt; 0.72.3</h3>";
@@ -54,6 +54,49 @@ function update0722to0723() {
       $query = "UPDATE `glpi_display` SET num=".$data['to']." WHERE num=".$data['from']." AND type=".$data['type'].";";
       $DB->query($query) or die("0.72.3 reorder search.constant " . $LANG['update'][90] . $DB->error());
    }
+
+	$LINK_ID_TABLE=array(
+			COMPUTER_TYPE=> "glpi_computers",
+			NETWORKING_TYPE => "glpi_networking",
+			PRINTER_TYPE => "glpi_printers",
+			MONITOR_TYPE => "glpi_monitors",
+			PERIPHERAL_TYPE => "glpi_peripherals",
+			SOFTWARE_TYPE => "glpi_software",
+			CONTACT_TYPE => "glpi_contacts",
+			ENTERPRISE_TYPE => "glpi_enterprises",
+			INFOCOM_TYPE => "glpi_infocoms",
+			CONTRACT_TYPE => "glpi_contracts",
+			CARTRIDGE_TYPE => "glpi_cartridges_type",
+			TYPEDOC_TYPE => "glpi_type_docs",
+			DOCUMENT_TYPE => "glpi_docs",
+			KNOWBASE_TYPE => "glpi_kbitems",
+			USER_TYPE => "glpi_users",
+			TRACKING_TYPE => "glpi_tracking",
+			CONSUMABLE_TYPE => "glpi_consumables_type",
+			CONSUMABLE_ITEM_TYPE => "glpi_consumables",
+			CARTRIDGE_ITEM_TYPE => "glpi_cartridges",
+			SOFTWARELICENSE_TYPE => "glpi_softwarelicenses",
+			LINK_TYPE => "glpi_links",
+			STATE_TYPE => "",
+			PHONE_TYPE => "glpi_phones",
+			DEVICE_TYPE => "???",
+			REMINDER_TYPE => "glpi_reminder",
+			STAT_TYPE => "???",
+			GROUP_TYPE => "glpi_groups",
+			ENTITY_TYPE => "glpi_entities",
+			RESERVATION_TYPE => "glpi_reservation_item",
+			OCSNG_TYPE => "glpi_ocs_config",
+			REGISTRY_TYPE => "glpi_registry",
+			PROFILE_TYPE => "glpi_profiles",
+			MAILGATE_TYPE => "glpi_mailgate",
+			RULE_TYPE => "glpi_rules_descriptions",
+			TRANSFER_TYPE => "glpi_transfers",
+			SOFTWAREVERSION_TYPE => "glpi_softwareversions",
+			COMPUTERDISK_TYPE => "glpi_computerdisks",
+			NETWORKING_PORT_TYPE => "glpi_networking_ports",
+			FOLLOWUP_TYPE => "glpi_followups",
+			);
+
 
    $query="SELECT DISTINCT device_type FROM glpi_doc_device";
 	if ($result = $DB->query($query)) {
