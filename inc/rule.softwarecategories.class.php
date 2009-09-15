2bc3398878e963b35325acff77ea2c3b24208aaf
remi <remi>
2009-09-15 16:32:15 +0000

    Coding Standard, see #1486 (yllen)

--- inc/rule.softwarecategories.class.php
+++ inc/rule.softwarecategories.class.php
@@ -33,74 +33,78 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 if (!defined('GLPI_ROOT')) {
-	die("Sorry. You can't access directly to this file");
+   die("Sorry. You can't access directly to this file");
 }
 
 
 class SoftwareCategoriesRuleCollection extends RuleCollection {
 
-	/**
-	 * Constructor
-	**/
-	function __construct(){
-		$this->sub_type = RULE_SOFTWARE_CATEGORY;
-		$this->rule_class_name = 'SoftwareCategoriesRule';
-		$this->stop_on_first_match=true;
-		$this->right="rule_softwarescategories";
-	}
+   /**
+    * Constructor
+   **/
+   function __construct() {
+      $this->sub_type = RULE_SOFTWARE_CATEGORY;
+      $this->rule_class_name = 'SoftwareCategoriesRule';
+      $this->stop_on_first_match=true;
+      $this->right="rule_softwarescategories";
+   }
 
-	function getTitle(){
-		global $LANG;
-		return $LANG['rulesengine'][37];
-	}
+   function getTitle() {
+      global $LANG;
 
-	/**
-	 * Get the attributes needed for processing the rules
-	 * @param $input input data
-	 * @param $software software data array
-	 * @return an array of attributes
-	 */
-	function prepareInputDataForProcess($input,$software){
-		$params["name"]=$software["name"];
-		
-		if (isset($software["comment"]))
-			$params["comment"]=$software["comment"];
-			
-		if (isset($software["manufacturers_id"]))
-			$params["manufacturer"]=getDropdownName("glpi_manufacturers",$software["manufacturers_id"]);
-		return $params;
-	}
+      return $LANG['rulesengine'][37];
+   }
+
+   /**
+    * Get the attributes needed for processing the rules
+    * @param $input input data
+    * @param $software software data array
+    * @return an array of attributes
+    */
+   function prepareInputDataForProcess($input,$software) {
+
+      $params["name"]=$software["name"];
+      if (isset($software["comment"])) {
+         $params["comment"]=$software["comment"];
+      }
+      if (isset($software["manufacturers_id"])) {
+         $params["manufacturer"]=getDropdownName("glpi_manufacturers",$software["manufacturers_id"]);
+      }
+      return $params;
+   }
+
 }
+
 
 /**
 * Rule class store all informations about a GLPI rule :
 *   - description
 *   - criterias
 *   - actions
-* 
+*
 **/
 class SoftwareCategoriesRule extends Rule {
 
+   /**
+    * Constructor
+   **/
+   function __construct() {
+      parent::__construct(RULE_SOFTWARE_CATEGORY);
 
-	/**
-	 * Constructor
-	**/
-	function __construct() {
-		parent::__construct(RULE_SOFTWARE_CATEGORY);
+      $this->right="rule_softwarescategories";
+      $this->can_sort=true;
+   }
 
-		$this->right="rule_softwarescategories";
-		$this->can_sort=true;
-	}
+   function getTitle() {
+      global $LANG;
 
-	function getTitle() {
-		global $LANG;
-		return $LANG['rulesengine'][37];
-	}
+      return $LANG['rulesengine'][37];
+   }
 
-	function maxActionsCount(){
-		return 1;
-	}
+   function maxActionsCount() {
+      return 1;
+   }
+
 }
-
 
 ?>
