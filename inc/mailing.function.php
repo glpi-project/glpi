c640809192e8303349ef4b5a66e889bf43243725
moyooo <moyo@indepnet.net>
2009-10-06 15:49:12 +0000

    [0.72] email address validity see #1833

--- inc/mailing.function.php
+++ inc/mailing.function.php
@@ -206,11 +206,75 @@ function testMail(){
 	}
 
 
+
 /**
  * Determine if email is valid
  * @param $email email to check
- * @return boolean 
+ * @param $checkdns check dns entry
+ * @return boolean
+ * from http://www.linuxjournal.com/article/9585
  */
+function isValidEmail($email,$checkdns=false)
+{
+   $isValid = true;
+   $atIndex = strrpos($email, "@");
+   if (is_bool($atIndex) && !$atIndex) {
+      $isValid = false;
+   } else {
+      $domain = substr($email, $atIndex+1);
+      $local = substr($email, 0, $atIndex);
+      $localLen = strlen($local);
+      $domainLen = strlen($domain);
+      if ($localLen < 1 || $localLen > 64) {
+         // local part length exceeded
+         $isValid = false;
+      }
+      else if ($domainLen < 1 || $domainLen > 255)
+      {
+         // domain part length exceeded
+         $isValid = false;
+      }
+      else if ($local[0] == '.' || $local[$localLen-1] == '.') {
+         // local part starts or ends with '.'
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $local)) {
+         // local part has two consecutive dots
+         $isValid = false;
+      }
+      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
+         // character not valid in domain part
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $domain)) {
+         // domain part has two consecutive dots
+         $isValid = false;
+      }
+      else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+                 str_replace("\\\\","",$local))) {
+         // character not valid in local part unless 
+         // local part is quoted
+         if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\","",$local))) {
+            $isValid = false;
+         }
+      }
+      if ($checkdns) {
+         if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A"))) {
+            // domain not found in DNS
+            $isValid = false;
+         }
+      }
+   }
+   return $isValid;
+}
+
+
+/**
+ * Determine if email is valid
+ * @param $email email to check
+ * @return boolean
+ */
+/*
 function isValidEmail($email="")
 {
 	if( !preg_match( "/^" .
@@ -226,7 +290,7 @@ function isValidEmail($email="")
 	}
 	else return true;
 }
-
+*/
 function isAuthorMailingActivatedForHelpdesk(){
 	global $DB,$CFG_GLPI;
 
