ce0029e0e3350bd90730e35a57b0437facad5ff5
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-10-24 09:09:51 +0000

    clean previous
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9143 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/typedoc.class.php
+++ inc/typedoc.class.php
@@ -37,10 +37,7 @@ if (!defined('GLPI_ROOT')) {
    die("Sorry. You can't access directly to this file");
 }
 
-
-// CLASSES peripherals
-
-
+/// TypeDoc class
 class Typedoc  extends CommonDBTM {
 
    /**
@@ -71,7 +68,6 @@ class Typedoc  extends CommonDBTM {
     *@return boolean : typedoc found
     **/
    function showForm ($target,$ID) {
-
       global $CFG_GLPI, $LANG;
 
       if (!haveRight("typedoc","r")) {
@@ -89,20 +85,20 @@ class Typedoc  extends CommonDBTM {
       $this->showTabs($ID, '',getActiveTab($this->type));
       $this->showFormHeader($target,$ID,'',2);
 
-      echo "<tr class='tab_bg_1'><td>".$LANG['common'][16].":	</td><td>";
+      echo "<tr class='tab_bg_1'><td>".$LANG['common'][16]."&nbsp;: </td><td>";
       autocompletionTextField("name",$this->table,"name",$this->fields["name"],40);
       echo "</td>";
-      echo "<td rowspan='6' class='middle'>".$LANG['common'][25]."&nbsp;:</td>";
+      echo "<td rowspan='6' class='middle'>".$LANG['common'][25]."&nbsp;: </td>";
       echo "<td class='center middle' rowspan='6'>";
       echo "<textarea cols='45' rows='7' name='comment' >".$this->fields["comment"];
       echo "</textarea></td></tr>\n";
 
-      echo "<tr class='tab_bg_1'><td>".$LANG['document'][10].":	</td><td>";
+      echo "<tr class='tab_bg_1'><td>".$LANG['document'][10]."&nbsp;: </td><td>";
       dropdownIcons("icon",$this->fields["icon"],GLPI_ROOT."/pics/icones");
       if (!empty($this->fields["icon"])) echo "&nbsp;<img style='vertical-align:middle;' alt='' src='".$CFG_GLPI["typedoc_icon_dir"]."/".$this->fields["icon"]."'>";
       echo "</td></tr>\n";
 
-      echo "<tr class='tab_bg_1'><td>".$LANG['document'][11].":	</td><td>";
+      echo "<tr class='tab_bg_1'><td>".$LANG['document'][11]."&nbsp;: </td><td>";
       dropdownYesNo("is_uploadable",$this->fields["is_uploadable"]);
       echo "</td></tr>\n";
 
@@ -110,7 +106,7 @@ class Typedoc  extends CommonDBTM {
       autocompletionTextField("ext",$this->table,"ext",$this->fields["ext"],40);
       echo "</td></tr>\n";
 
-      echo "<tr class='tab_bg_1'><td>".$LANG['document'][4].": </td><td>";
+      echo "<tr class='tab_bg_1'><td>".$LANG['document'][4]."&nbsp;: </td><td>";
       autocompletionTextField("mime",$this->table,"mime",$this->fields["mime"],40);
       echo "</td></tr>\n";
 
