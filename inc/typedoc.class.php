bcebfeb94b7ed814a7bd0103c4448cc9891c8605
remi <remi>
2009-10-24 09:05:40 +0000

    add comment field to glpi_documentstypes (like other dropdown)

--- inc/typedoc.class.php
+++ inc/typedoc.class.php
@@ -33,9 +33,9 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-if (!defined('GLPI_ROOT')){
-	die("Sorry. You can't access directly to this file");
-	}
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
 
 
 // CLASSES peripherals
@@ -43,115 +43,85 @@ if (!defined('GLPI_ROOT')){
 
 class Typedoc  extends CommonDBTM {
 
+   /**
+    * Constructor
+    **/
+   function __construct () {
+      $this->table="glpi_documentstypes";
+      $this->type=TYPEDOC_TYPE;
+   }
 
-	/**
-	 * Constructor
-	**/
-	function __construct () {
-		$this->table="glpi_documentstypes";
-		$this->type=TYPEDOC_TYPE;
-	}
 
+   function defineTabs($ID,$withtemplate){
+      global $LANG;
 
-	function defineTabs($ID,$withtemplate){
-		global $LANG;
+      $ong=array();
 
-		$ong=array();
+      $ong[1]=$LANG['title'][26];
 
-		$ong[1]=$LANG['title'][26];
+      return $ong;
+   }
 
-		return $ong;
-	}
+   /**
+    * Print the typedoc form
+    *
+    *@param $target form target
+    *@param $ID Integer : Id of the typedoc
+    *
+    *@return boolean : typedoc found
+    **/
+   function showForm ($target,$ID) {
 
-	/**
-	 * Print the typedoc form
-	 *
-	 *@param $target form target
-	 *@param $ID Integer : Id of the typedoc
-	 *
-	 *@return boolean : typedoc found
-	 **/
-	function showForm ($target,$ID) {
+      global $CFG_GLPI, $LANG;
 
-		global $CFG_GLPI, $LANG;
+      if (!haveRight("typedoc","r")) {
+         return false;
+      }
 
-		if (!haveRight("typedoc","r")) return false;
+      if ($ID > 0) {
+         $this->check($ID,'r');
+      } else {
+         // Create item
+         $this->check(-1,'w');
+         $this->getEmpty();
+      }
 
-		if ($ID > 0){
-			$this->check($ID,'r');
-		} else {
-			// Create item 
-			$this->check(-1,'w');
-			$this->getEmpty();
-		} 
+      $this->showTabs($ID, '',getActiveTab($this->type));
+      $this->showFormHeader($target,$ID,'',2);
 
-		echo "<div class='center'>";
-		$this->showTabs($ID, '',getActiveTab($this->type));
+      echo "<tr class='tab_bg_1'><td>".$LANG['common'][16].":	</td><td>";
+      autocompletionTextField("name",$this->table,"name",$this->fields["name"],40);
+      echo "</td>";
+      echo "<td rowspan='6' class='middle'>".$LANG['common'][25]."&nbsp;:</td>";
+      echo "<td class='center middle' rowspan='6'>";
+      echo "<textarea cols='45' rows='7' name='comment' >".$this->fields["comment"];
+      echo "</textarea></td></tr>\n";
 
-		echo "<form method='post' name=form action=\"$target\">";
+      echo "<tr class='tab_bg_1'><td>".$LANG['document'][10].":	</td><td>";
+      dropdownIcons("icon",$this->fields["icon"],GLPI_ROOT."/pics/icones");
+      if (!empty($this->fields["icon"])) echo "&nbsp;<img style='vertical-align:middle;' alt='' src='".$CFG_GLPI["typedoc_icon_dir"]."/".$this->fields["icon"]."'>";
+      echo "</td></tr>\n";
 
-		echo "<table class='tab_cadre_fixe' cellpadding='2'>";
+      echo "<tr class='tab_bg_1'><td>".$LANG['document'][11].":	</td><td>";
+      dropdownYesNo("is_uploadable",$this->fields["is_uploadable"]);
+      echo "</td></tr>\n";
 
-		echo "<tr><th align='center'  colspan='2'>";
-		if (empty($ID)){
-			echo $LANG['document'][17];
-		} else {
-			echo $LANG['common'][2]." ".$this->fields["id"];
-		}
+      echo "<tr class='tab_bg_1'><td>".$LANG['document'][9]."&nbsp;: </td><td>";
+      autocompletionTextField("ext",$this->table,"ext",$this->fields["ext"],40);
+      echo "</td></tr>\n";
 
-		echo "</th><th  align='center'  colspan='2'>".$LANG['common'][26]." : ".convDateTime($this->fields["date_mod"]);
-		echo "</th></tr>";
+      echo "<tr class='tab_bg_1'><td>".$LANG['document'][4].": </td><td>";
+      autocompletionTextField("mime",$this->table,"mime",$this->fields["mime"],40);
+      echo "</td></tr>\n";
 
-		echo "<tr class='tab_bg_1'><td>".$LANG['common'][16].":	</td><td>";
-		autocompletionTextField("name",$this->table,"name",$this->fields["name"],40);
-		echo "</td>";
+      echo "<tr class='tab_bg_1'>";
+      echo "<td colspan='2' class='center'>".$LANG['common'][26] . "&nbsp;: ";
+      echo convDateTime($this->fields["date_mod"])."</td></tr>\n";
 
-		echo "<td>".$LANG['document'][9].":	</td><td>";
-		autocompletionTextField("ext",$this->table,"ext",$this->fields["ext"],40);
+      $this->showFormButtons($ID,'',2);
 
-		echo "</td></tr>";
-
-		echo "<tr class='tab_bg_1'><td>".$LANG['document'][10].":	</td><td>";
-                dropdownIcons("icon",$this->fields["icon"],GLPI_ROOT."/pics/icones");
-		if (!empty($this->fields["icon"])) echo "&nbsp;<img style='vertical-align:middle;' alt='' src='".$CFG_GLPI["typedoc_icon_dir"]."/".$this->fields["icon"]."'>";
-		echo "</td>";
-
-		echo "<td>".$LANG['document'][4].":	</td><td>";
-		autocompletionTextField("mime",$this->table,"mime",$this->fields["mime"],40);
-		echo "</td></tr>";
-
-		echo "<tr class='tab_bg_1'><td>".$LANG['document'][11].":	</td><td>";
-		dropdownYesNo("is_uploadable",$this->fields["is_uploadable"]);
-		echo "</td><td colspan='2'>&nbsp;</td></tr>";
-
-		if (haveRight("typedoc","w")) {
-			echo "<tr>";
-			if(empty($ID)){
-
-				echo "<td class='tab_bg_2' valign='top' colspan='4'>";
-				echo "<div class='center'><input type='submit' name='add' value=\"".$LANG['buttons'][8]."\" class='submit'></div>";
-				echo "</td>";
-
-			} else {
-
-				echo "<td class='tab_bg_2' colspan='2' valign='top' align='center'>";
-				echo "<input type='hidden' name='id' value=\"$ID\">\n";
-				echo "<input type='submit' name='update' value=\"".$LANG['buttons'][7]."\" class='submit'>";
-				echo "</td>";
-				echo "<td class='tab_bg_2' colspan='2' valign='top'>\n";
-				echo "<div class='center'>";
-				echo "<input type='submit' name='delete' value=\"".$LANG['buttons'][6]."\" class='submit'>";
-				echo "</div>";
-				echo "</td>";
-			}
-			echo "</tr>";
-		}
-
-		echo "</table></form></div>";
-
-		return true;	
-
-	}
+      return true;
+   }
 
 }
 
