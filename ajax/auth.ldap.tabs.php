d111eb0cc0e09b0f3eafa855bc97ef8798191d24
Walid Nouh <wnouh@teclib.com>
2009-11-14 16:45:45 +0000

    Improve last commit (to be continued)

---
+++ ajax/auth.ldap.tabs.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * @version $Id: cartridge.tabs.php 9269 2009-11-08 20:48:53Z remi $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2009 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Julien Dombre
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+
+$NEEDED_ITEMS = array ('extauth');
+
+define('GLPI_ROOT', '..');
+include (GLPI_ROOT . "/inc/includes.php");
+header("Content-Type: text/html; charset=UTF-8");
+header_nocache();
+
+if(!isset($_POST['id'])) {
+   exit();
+}
+
+checkRight('config','r');
+
+$authldap = new AuthLDAP;
+if ($_POST['id']>0 && $authldap->can($_POST['id'],'r')) {
+   switch($_REQUEST['glpi_tab']) {
+      case -1 :
+         $authldap->showFormTestLDAP ($_POST['id'], $_POST['target']);
+         $authldap->showFormReplicatesConfig($_POST['id'], $_POST['target']);
+         $authldap->showFormUserConfig($_POST['id'],$_POST['target']);
+         $authldap->showFormGroupsConfig($_POST['id'],$_POST['target']);
+         break;
+
+		case 1 :
+		   $authldap->showFormTestLDAP ($_POST['id'], $_POST['target']);
+         break;
+		
+      case 2 :
+         $authldap->showFormReplicatesConfig($_POST['id'], $_POST['target']);
+         break;
+
+      case 3 :
+         $authldap->showFormUserConfig($_POST['id'],$_POST['target']);
+         break;
+
+      case 4 :
+         $authldap->showFormGroupsConfig($_POST['id'],$_POST['target']);
+         break;
+
+      default :
+         if (!displayPluginAction(AUTH_LDAP_TYPE,$_POST['id'],$_REQUEST['glpi_tab'])) {
+         }
+         break;
+   }
+}
+ajaxFooter();
+
+?>
