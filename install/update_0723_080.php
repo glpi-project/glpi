1d4ce3c25bcf4cf6f1b80acd584d9f217fe50843
moyooo <moyo@indepnet.net>
2009-11-26 21:31:20 +0000

    manage bookmark update due to typedoc file changes

--- install/update_0723_080.php
+++ install/update_0723_080.php
@@ -1696,6 +1696,10 @@ function update0723to080() {
          }
       }
 
+      // Update bookmarks due to FHS change
+      $query2="UPDATE glpi_bookmarks SET path='front/documenttype.php' WHERE path='front/typedoc.php';";
+      $DB->query($query2) or die("0.80 update typedoc bookmarks " . $LANG['update'][90] . $DB->error());
+
    }
 
    //// Upgrade rules datas
