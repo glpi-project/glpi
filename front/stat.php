de551060433ad5c1d7691f8369bd05aba68f8ec9
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-12-05 19:31:38 +0000

    Coding Standards (yllen)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9482 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/stat.php
+++ front/stat.php
@@ -33,7 +33,6 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-
 $NEEDED_ITEMS = array('stat', 'tracking');
 
 define('GLPI_ROOT', '..');
@@ -43,36 +42,40 @@ commonHeader($LANG['Menu'][13],$_SERVER['PHP_SELF'],"maintain","stat");
 
 checkRight("statistic","1");
 
-//Affichage du tableau de pr�entation des stats
-echo "<table class='tab_cadre' cellpadding='5'>";
-echo "<tr><th>".$LANG['stats'][0].":</th></tr>";
+//Affichage du tableau de presentation des stats
+echo "<table class='tab_cadre_fixe'>";
+echo "<tr><th>".$LANG['stats'][0]."&nbsp;:</th></tr>";
+echo "<tr class='tab_bg_1'><td class='center b'><a href='stat.global.php'>".
+      $LANG['stats'][1]."</a></td></tr>";
+echo "<tr class='tab_bg_1'><td class='center b'><a href='stat.tracking.php'>".
+      $LANG['stats'][47]."</a></td></tr>";
+echo "<tr class='tab_bg_1'><td class='center'><a href='stat.location.php'><b>".
+      $LANG['stats'][3]."</b></a><br> (".$LANG['common'][15].", ".$LANG['common'][17].", ".
+      $LANG['computers'][9].", ".$LANG['computers'][21].", ".$LANG['computers'][36].", ".
+      $LANG['devices'][2].", ".$LANG['devices'][5].")</td></tr>";
+echo "<tr class='tab_bg_1'><td class='center b'><a href='stat.item.php'>".$LANG['stats'][45].
+      "</a></td></tr>";
 
-
-echo  "<tr class='tab_bg_1'><td align='center'><a href=\"stat.global.php\"><b>".$LANG['stats'][1]."</b></a></td></tr>";
-echo  "<tr class='tab_bg_1'><td align='center'><a href=\"stat.tracking.php\"><b>".$LANG['stats'][47]."</b></a></td></tr>";
-echo  "<tr class='tab_bg_1'><td align='center'><a href=\"stat.location.php\"><b>".$LANG['stats'][3]."</b></a><br> (".$LANG['common'][15]
-.", ".$LANG['common'][17].", ".$LANG['computers'][9].", ".$LANG['computers'][21].", ".$LANG['computers'][36]
-.", ".$LANG['devices'][2].", ".$LANG['devices'][5].")</td></tr>";
-echo  "<tr class='tab_bg_1'><td align='center'><a href=\"stat.item.php\"><b>".$LANG['stats'][45]."</b></a></td></tr>";
-
-$names=array();
+$names = array();
 if (isset($PLUGIN_HOOKS["stats"]) && is_array($PLUGIN_HOOKS["stats"])) {
-	foreach ($PLUGIN_HOOKS["stats"] as $plug => $pages){
-		$function="plugin_version_$plug";
-		$plugname=$function();
-		if (is_array($pages)&&count($pages)){
-			foreach ($pages as $page => $name){
-				$names[$plug.'/'.$page]=$plugname['name'].' - '.$name;
-			}
-		}
-	}
-	asort($names);
+   foreach ($PLUGIN_HOOKS["stats"] as $plug => $pages) {
+      $function = "plugin_version_$plug";
+      $plugname = $function();
+      if (is_array($pages) && count($pages)) {
+         foreach ($pages as $page => $name) {
+            $names[$plug.'/'.$page] = $plugname['name'].' - '.$name;
+         }
+      }
+   }
+   asort($names);
 }
 
 foreach ($names as $key => $val) {
-	echo "<tr class='tab_bg_1'><td align='center'><a href='".$CFG_GLPI["root_doc"]."/plugins/$key'><strong>".$val."</strong></a></td></tr>";
+   echo "<tr class='tab_bg_1'><td class='center b'><a href='".$CFG_GLPI["root_doc"].
+         "/plugins/$key'>$val</a></td></tr>";
 }
 echo "</table>";
 
 commonFooter();
+
 ?>
--- front/stat.tracking.php
+++ front/stat.tracking.php
@@ -33,130 +33,123 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-
 $NEEDED_ITEMS = array('stat', 'tracking', 'user');
 
 define('GLPI_ROOT', '..');
 include (GLPI_ROOT . "/inc/includes.php");
 
-
 commonHeader($LANG['Menu'][13],$_SERVER['PHP_SELF'],"maintain","stat");
 
 checkRight("statistic","1");
 
-
-if (isset($_GET["date1"])) $_POST["date1"] = $_GET["date1"];
-if (isset($_GET["date2"])) $_POST["date2"] = $_GET["date2"];
-if (isset($_GET["type"])) $_POST["type"] = $_GET["type"];
-if (empty($_POST["type"])) $_POST["type"]="user";
-
-if(empty($_POST["date1"])&&empty($_POST["date2"])) {
-	$year=date("Y")-1;
-	$_POST["date1"]=date("Y-m-d",mktime(1,0,0,date("m"),date("d"),$year));
-
-	$_POST["date2"]=date("Y-m-d");
+if (isset($_GET["date1"])) {
+   $_POST["date1"] = $_GET["date1"];
+}
+if (isset($_GET["date2"])) {
+   $_POST["date2"] = $_GET["date2"];
+}
+if (isset($_GET["type"])) {
+   $_POST["type"] = $_GET["type"];
+}
+if (empty($_POST["type"])) {
+   $_POST["type"] = "user";
 }
 
-if (!empty($_POST["date1"])&&!empty($_POST["date2"])&&strcmp($_POST["date2"],$_POST["date1"])<0){
-	$tmp=$_POST["date1"];
-	$_POST["date1"]=$_POST["date2"];
-	$_POST["date2"]=$tmp;
+if (empty($_POST["date1"]) && empty($_POST["date2"])) {
+   $year = date("Y")-1;
+   $_POST["date1"] = date("Y-m-d",mktime(1,0,0,date("m"),date("d"),$year));
+   $_POST["date2"] = date("Y-m-d");
 }
 
-if(!isset($_GET["start"])) $_GET["start"] = 0;
+if (!empty($_POST["date1"])
+    && !empty($_POST["date2"])
+    && strcmp($_POST["date2"],$_POST["date1"]) < 0) {
 
+   $tmp = $_POST["date1"];
+   $_POST["date1"] = $_POST["date2"];
+   $_POST["date2"] = $tmp;
+}
 
-$items=array(
-	$LANG['job'][4]=> array(
-		"user"=>array(
-			"title"=>$LANG['common'][37],
-			"field"=>"glpi_tickets.users_id"
-			),
-		"users_id_recipient"=>array(
-			"title"=>$LANG['job'][3],
-			"field"=>"glpi_tickets.users_id_recipient"
-			),
-		"group"=>array(
-			"title"=>$LANG['common'][35],
-			"field"=>"glpi_tickets.groups_id"
-			),
-		"usertitles_id"=>array(
-			"title"=>$LANG['users'][1],
-			"field"=>"glpi_usertitles.usertitles_id"
-			),
-		"usercategories_id"=>array(
-			"title"=>$LANG['users'][2],
-			"field"=>"glpi_users.usercategories_id"
-			),
-	),
-	$LANG['common'][32] => array(
-		"ticketcategories_id"=>array(
-			"title"=>$LANG['common'][36],
-			"field"=>"glpi_tickets.ticketcategories_id"
-			),
-		"priority"=>array(
-			"title"=>$LANG['joblist'][2],
-			"field"=>"glpi_tickets.priority"
-			),
-		"requesttypes_id"=>array(
-			"title"=>$LANG['job'][44],
-			"field"=>"glpi_tickets.requesttypes_id"
-			),
-	),
-	$LANG['job'][5] => array(
-		"technicien"=>array(
-			"title"=>$LANG['job'][6]." ".$LANG['stats'][48],
-			"field"=>"glpi_tickets.users_id_assign"
-			),
-		"technicien_followup"=>array(
-				"title"=>$LANG['job'][6]." ".$LANG['stats'][49],
-				"field"=>"glpi_followup.users_id"
-				),
-		"groups_id_assign"=>array(
-			"title"=>$LANG['common'][35],
-			"field"=>"glpi_tickets.groups_id_assign"
-			),
-		"enterprise"=>array(
-				"title"=>$LANG['financial'][26],
-				"field"=>"glpi_tickets.suppliers_id_assign"
-				),
-	)
-		);
-	$INSELECT="";
-	foreach ($items as $label => $tab){
-		$INSELECT.= "<optgroup label=\"$label\">";
-		foreach ($tab as $key => $val){
-			// Current field
-			if ($key==$_POST["type"]){
-				$field=$val["field"];
-			}
-			$INSELECT.= "<option value='$key' ".($key==$_POST["type"]?"selected":"").">".$val['title']."</option>";
-		}
-		$INSELECT.= "</optgroup>";
-	}
+if (!isset($_GET["start"])) {
+   $_GET["start"] = 0;
+}
 
+$items =
+   array($LANG['job'][4] => array('user'  => array('title' => $LANG['common'][37],
+                                                  'field' => 'glpi_tickets.users_id'),
+                                  'users_id_recipient'
+                                          => array('title' => $LANG['job'][3],
+                                                   'field' => 'glpi_tickets.users_id_recipient'),
+                                  'group' => array('title' => $LANG['common'][35],
+                                                   'field' => 'glpi_tickets.groups_id'),
+                                  'usertitles_id'
+                                          => array('title' => $LANG['users'][1],
+                                                   'field' => 'glpi_usertitles.usertitles_id'),
+                                  'usercategories_id'
+                                          => array('title' => $LANG['users'][2],
+                                                   'field' => 'glpi_users.usercategories_id')),
+         $LANG['common'][32] => array('ticketcategories_id'
+                                          => array('title' => $LANG['common'][36],
+                                                   'field' => 'glpi_tickets.ticketcategories_id'),
+                                      'priority' => array('title' => $LANG['joblist'][2],
+                                                          'field' => 'glpi_tickets.priority'),
+                                      'requesttypes_id'
+                                          => array('title' => $LANG['job'][44],
+                                                   'field' => 'glpi_tickets.requesttypes_id')),
+         $LANG['job'][5] => array('technicien'
+                                       => array('title' => $LANG['job'][6]." ".$LANG['stats'][48],
+                                                'field' => 'glpi_tickets.users_id_assign'),
+                                  'technicien_followup'
+                                       => array('title' => $LANG['job'][6]." ".$LANG['stats'][49],
+                                                'field' => 'glpi_followup.users_id'),
+                                  'groups_id_assign'
+                                       => array('title' => $LANG['common'][35],
+                                                'field' => 'glpi_tickets.groups_id_assign'),
+                                  'enterprise'
+                                       => array('title' => $LANG['financial'][26],
+                                                'field' => 'glpi_tickets.suppliers_id_assign')));
+$INSELECT = "";
+foreach ($items as $label => $tab) {
+   $INSELECT .= "<optgroup label=\"$label\">";
+   foreach ($tab as $key => $val) {
+      // Current field
+      if ($key == $_POST["type"]) {
+         $field = $val["field"];
+      }
+      $INSELECT .= "<option value='$key' ".($key==$_POST["type"]?"selected":"").">".$val['title'].
+                   "</option>";
+   }
+   $INSELECT .= "</optgroup>";
+}
 
-	echo "<div align='center'><form method=\"post\" name=\"form\" action=\"stat.tracking.php\">";
-	echo "<table class='tab_cadre'><tr class='tab_bg_2'>";
-	echo "<td rowspan='2' align='center'>";
-	echo "<select name='type'>";
-	echo $INSELECT;
-	echo "</td>";
-	echo "<td align='right'>";
-	echo $LANG['search'][8]." :</td><td>";
-	showDateFormItem("date1",$_POST["date1"]);
-	echo "</td><td rowspan='2' align='center'><input type=\"submit\" class='button' name=\"submit\" Value=\"". $LANG['buttons'][7] ."\"></td></tr>";
-	echo "<tr class='tab_bg_2'><td align='right'>".$LANG['search'][9]." :</td><td>";
-	showDateFormItem("date2",$_POST["date2"]);
-	echo "</td></tr>";
-	echo "</table></form></div>";
+echo "<div class='center'><form method='post' name='form' action='stat.tracking.php'>";
+echo "<table class='tab_cadre'><tr class='tab_bg_2'>";
+echo "<td rowspan='2' class='center'>";
+echo "<select name='type'>";
+echo $INSELECT;
+echo "</select></td>";
+echo "<td class='right'>".$LANG['search'][8]."&nbsp;:</td><td>";
+showDateFormItem("date1",$_POST["date1"]);
+echo "</td><td rowspan='2' class='center'>";
+echo "<input type='submit' class='button' name='submit' value='". $LANG['buttons'][7] ."'></td></tr>";
+echo "<tr class='tab_bg_2'><td class='right'>".$LANG['search'][9]."&nbsp;:</td><td>";
+showDateFormItem("date2",$_POST["date2"]);
+echo "</td></tr>";
+echo "</table></form></div>";
 
+$val = getStatsItems($_POST["date1"],$_POST["date2"],$_POST["type"]);
+$params = array('type'  => $_POST["type"],
+                'field' => $field,
+                'date1' => $_POST["date1"],
+                'date2' => $_POST["date2"],
+                'start' => $_GET["start"]);
 
-	$val=getStatsItems($_POST["date1"],$_POST["date2"],$_POST["type"]);
-	$params=array("type"=>$_POST["type"],"field"=>$field,"date1"=>$_POST["date1"],"date2"=>$_POST["date2"],"start"=>$_GET["start"]);
-	printPager($_GET['start'],count($val),$_SERVER['PHP_SELF'],"date1=".$_POST["date1"]."&amp;date2=".$_POST["date2"]."&amp;type=".$_POST["type"],STAT_TYPE,$params);
+printPager($_GET['start'],count($val),$_SERVER['PHP_SELF'],
+           "date1=".$_POST["date1"]."&amp;date2=".$_POST["date2"]."&amp;type=".$_POST["type"],
+           'Stat',$params);
 
-	displayStats($_POST["type"],$_POST["date1"],$_POST["date2"],$_GET['start'],$val);
+displayStats($_POST["type"],$_POST["date1"],$_POST["date2"],$_GET['start'],$val);
 
-	commonFooter();
+commonFooter();
+
 ?>
