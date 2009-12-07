df09c896f0e18382fa834b49e286dc727f230f87
walid <walid@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-12-07 13:12:41 +0000

    Move planning functions in TicketPlanning
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9519 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ticket.function.php
+++ inc/ticket.function.php
@@ -2431,7 +2431,7 @@ function showFollowupsSummary($tID) {
             echo "}";
             echo "</script>\n";
 
-            echo getPlanningState($data2["state"])."<br>".convDateTime($data2["begin"])."<br>->".
+            echo TicketPlanning::getState($data2["state"])."<br>".convDateTime($data2["begin"])."<br>->".
                  convDateTime($data2["end"])."<br>".getUserName($data2["users_id"]);
          }
          echo "</td>";
@@ -2734,7 +2734,7 @@ function showUpdateFollowupForm($ID) {
             echo "<div id='plan' onClick='showPlan".$ID."()'>\n";
             echo "<span class='showplan'>";
          }
-         echo getPlanningState($fup->fields2["state"])."<br>".convDateTime($fup->fields2["begin"]).
+         echo TicketPlanning::getState($fup->fields2["state"])."<br>".convDateTime($fup->fields2["begin"]).
               "<br>->".convDateTime($fup->fields2["end"])."<br>".
               getUserName($fup->fields2["users_id"]);
          if ($canplan) {
