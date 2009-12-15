13a9d2b6e5ce4f4b2e618cbed84f64e57c29d5e1
tsmr <tsmr@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-12-15 20:45:45 +0000

    Correct dropdown.class.php includes on glpi install (for showLanguages)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9714 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/install.php
+++ install/install.php
@@ -41,6 +41,7 @@ include_once (GLPI_ROOT . "/config/based_config.php");
 include_once (GLPI_ROOT . "/inc/timer.class.php");
 include_once (GLPI_ROOT . "/inc/common.function.php");
 include_once (GLPI_ROOT . "/inc/db.function.php");
+include_once (GLPI_ROOT . "/inc/dropdown.class.php");
 include_once (GLPI_ROOT . "/inc/display.function.php");
 include_once (GLPI_ROOT . "/inc/dropdown.function.php");
 
