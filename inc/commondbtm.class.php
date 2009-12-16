4bb35c6f182a1f3aea3eb5cff6de59b75aa013c6
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-12-16 11:49:56 +0000

    minor fix
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9742 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/commondbtm.class.php
+++ inc/commondbtm.class.php
@@ -675,7 +675,7 @@ class CommonDBTM extends CommonGLPI {
                if ( is_null($this->fields[$key]) && $input[$key]=='NULL') {
                   $this->fields[$key]='NULL';
                }
-               if (mysql_real_escape_string($this->fields[$key]) != $this->input[$key]) {
+               if (mysql_real_escape_string($this->fields[$key]) != $input[$key]) {
                   if ($key!="id") {
                      // Store old values
                      if (!in_array($key,$this->history_blacklist)) {
