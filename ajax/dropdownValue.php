0d0b9e9999c14eced5c8ea9c5a5c7f22d41ed109
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-12-17 17:23:15 +0000

    minor fix
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9787 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- ajax/dropdownValue.php
+++ ajax/dropdownValue.php
@@ -123,7 +123,8 @@ if ($item instanceof CommonTreeDropdown) {
 
       echo "<select id='dropdown_".$_POST["myname"].$_POST["rand"]."' name=\"".
             $_POST['myname']."\" size='1'";
-      if ($_POST['table'] == "glpi_entities" && $_POST["auto_submit"]==1) {
+      /// TODO : why auto_sumbit only for entities ?
+      if ($_POST['table'] == "glpi_entities" && isset($_POST["auto_submit"] && $_POST["auto_submit"]==1) {
          echo " onChange='submit()'";
       }
       echo ">";
