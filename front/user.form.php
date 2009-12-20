dd3fb16879a6cd1db3063a9c81242b48e28446f6
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-12-20 12:01:38 +0000

    generateUserVcard() => ->generateVcard()
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9824 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/user.form.php
+++ front/user.form.php
@@ -68,8 +68,8 @@ if (isset($_REQUEST['getvcard'])) {
    if (empty($_GET["id"])) {
       glpi_header($CFG_GLPI["root_doc"]."/front/user.php");
    }
-   checkRight("user","r");
-   generateUserVcard($_GET["id"]);
+   $user->check($_GET['id'],'r');
+   $user->generateVcard($_GET["id"]);
 
 } else if (isset($_POST["add"])) {
    $user->check(-1,'w',$_POST);
