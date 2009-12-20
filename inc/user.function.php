b5bdd120aad7261cbe97ec6fa91e1109671c7431
remi <remi>
2009-12-20 11:05:35 +0000

    changeUserAuthMethod() => User::changeAuthMethod() + fix Sql

--- inc/user.function.php
+++ inc/user.function.php
@@ -366,20 +366,5 @@ function getUserLanguage($lang) {
 }
 
 
-function changeUserAuthMethod($IDs=array(), $authtype=1 ,$server=-1) {
-   global $DB;
-
-   if (!empty($IDs) && in_array($authtype, array(AUTH_DB_GLPI,
-                                                 AUTH_LDAP,
-                                                 AUTH_MAIL,
-                                                 AUTH_EXTERNAL))) {
-      $where = implode(',',$IDs);
-      $query = "UPDATE
-                `glpi_users`
-                SET `authtype` = '$authtype', `auths_id` = '$server'
-                WHERE `id` IN $where";
-      $DB->query($query);
-   }
-}
 
 ?>
