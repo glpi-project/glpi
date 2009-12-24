f69b30e66a5066651ef97e9260f23c51ee6c3899
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2009-12-24 16:27:52 +0000

    coding standard + correct function name
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@9926 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/report.networking.php
+++ front/report.networking.php
@@ -29,14 +29,10 @@
  */
 /*!
   \brief affiche les diffents choix de rapports reseaux
-
  */
-
 
 define('GLPI_ROOT', '..');
 include (GLPI_ROOT . "/inc/includes.php");
-
-
 
 checkRight("reports","r");
 
@@ -44,54 +40,43 @@ commonHeader($LANG['Menu'][6],$_SERVER['PHP_SELF'],"utils","report");
 
 # Titre
 
-
-
-
 echo "<table class='tab_cadre' >";
-echo "<tr><th align='center' colspan='3' >".$LANG['reports'][33]."</th></tr>";
+echo "<tr><th colspan='3'>&nbsp;".$LANG['reports'][33]."&nbsp;</th></tr>";
 echo "</table><br>";
+
 // 3. Selection d'affichage pour generer la liste
 
 echo "<form name='form' method='post' action='report.location.list.php'>";
-echo "<table class='tab_cadre'  width='500'>";
-echo "<tr class='tab_bg_1'><td>".$LANG['reports'][39]."</td>";
+echo "<table class='tab_cadre' width='500'>";
+echo "<tr class='tab_bg_1'><td width='120'>".$LANG['reports'][39]."</td>";
 echo "<td>";
 Dropdown::show('Location', array('entity' => $_SESSION["glpiactive_entity"]));
-echo "</td><td align='center' width='120'>";
+echo "</td><td class='center' width='120'>";
 echo "<input type='submit' value='".$LANG['reports'][15]."' class='submit'>";
 echo "</td></tr>";
-echo "</table>";
-echo "</form>";
-
+echo "</table></form>";
 
 echo "<form name='form2' method='post' action='report.switch.list.php'>";
 echo "<table class='tab_cadre' width='500'>";
-echo "<tr class='tab_bg_1'><td>".$LANG['reports'][41]."</td>";
+echo "<tr class='tab_bg_1'><td width='120'>".$LANG['reports'][41]."</td>";
 echo "<td>";
 Dropdown::show('NetworkEquipment', array('name' => 'switch'));
-echo "</td><td align='center' width='120'>";
+echo "</td><td class='center' width='120'>";
 echo "<input type='submit' value='".$LANG['reports'][15]."' class='submit'>";
 echo "</td></tr>";
-echo "</table>";
-echo "</form>";
+echo "</table></form>";
 
-
-if (countElementsInTableForMyEntities("glpi_netpoints")>0){
-	echo "<form name='form3' method='post' action='report.netpoint.list.php'>";
-	echo "<table class='tab_cadre'  width='500'>";
-	echo "<tr class='tab_bg_1'><td>".$LANG['reports'][42]."</td>";
-	echo "<td>";
-	Netpoint::dropdownNetpoint("prise",0,-1,1,$_SESSION["glpiactive_entity"]);
-	echo "</td><td align='center' width='120'>";
-	echo "<input type='submit' value='".$LANG['reports'][15]."' class='submit'>";
-	echo "</td></tr>";
-	echo "</table>";
-	echo "</form>";
+if (countElementsInTableForMyEntities("glpi_netpoints") > 0) {
+   echo "<form name='form3' method='post' action='report.netpoint.list.php'>";
+   echo "<table class='tab_cadre' width='500'>";
+   echo "<tr class='tab_bg_1'><td width='120'>".$LANG['reports'][42]."</td>";
+   echo "<td>";
+   Netpoint::dropdownNetpoint("prise",0,-1,1,$_SESSION["glpiactive_entity"]);
+   echo "</td><td class='center' width='120'>";
+   echo "<input type='submit' value='".$LANG['reports'][15]."' class='submit'>";
+   echo "</td></tr>";
+   echo "</table></form>";
 }
-
-
-
-
 
 commonFooter();
 
