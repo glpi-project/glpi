b0c5d2f6f26538f48af732db7e86611ba7816e19
yllen <yllen>
2009-12-27 12:34:34 +0000

    coding standard

--- front/report.year.php
+++ front/report.year.php
@@ -40,22 +40,19 @@ checkRight("reports","r");
 commonHeader($LANG['Menu'][6],$_SERVER['PHP_SELF'],"utils","report");
 
 
-
 # Titre
-
 
 echo "<form name='form' method='post' action='report.year.list.php'>";
 
-echo "<div align='center'>";
 echo "<table class='tab_cadre' >";
-echo "<tr><th align='center' colspan='2' ><big><b>".$LANG['reports'][58]."</b></big></th></tr>";
+echo "<tr><th colspan='2'><big>".$LANG['reports'][58]."</big></th></tr>";
 
 # 3. Selection d'affichage pour generer la liste
 
 echo "<tr class='tab_bg_2'>";
-echo "<td width='150'  align='center'>";
-echo "<p><b>".$LANG['reports'][12]."</b></p> ";
-echo "<p><select name='item_type[]' size='8'  multiple>";
+echo "<td width='150' class='center'>";
+echo "<p class='b'>".$LANG['reports'][12]."</p> ";
+echo "<p><select name='item_type[]' size='8' multiple>";
 echo "<option value='0' selected>".$LANG['common'][66]."</option>";
 echo "<option value='Computer'>".$LANG['Menu'][0]."</option>";
 echo "<option value='Printer'>".$LANG['Menu'][2]."</option>";
@@ -66,24 +63,19 @@ echo "<option value='Software'>".$LANG['Menu'][4]."</option>";
 echo "<option value='Phone'>".$LANG['Menu'][34]."</option>";
 echo "</select> </p></td>";
 
-echo "<td width='150' align='center'><p><b>".$LANG['reports'][13]."</b></p> ";
-echo "<p> <select name='annee[]'  size='8' multiple>";
-echo " <option value='toutes' selected>".$LANG['common'][66]."</option>";
+echo "<td width='150' class='center'><p class='b'>".$LANG['reports'][13]."</p> ";
+echo "<p><select name='annee[]' size='8' multiple>";
+echo "<option value='toutes' selected>".$LANG['common'][66]."</option>";
 $y = date("Y");
-for ($i=$y-10;$i<$y+10;$i++)
-{
-	echo " <option value='$i'>$i</option>";
+for ($i=$y-10 ; $i<$y+10 ; $i++) {
+   echo " <option value='$i'>$i</option>";
 }
 echo "</select></p></td></tr>";
 
-echo "<tr class='tab_bg_2'><td colspan='2'  align='center'><p><input type='submit' value='".$LANG['reports'][15]."' class='submit'></p></td></tr>";
+echo "<tr class='tab_bg_2'><td colspan='2' class='center'>";
+echo "<input type='submit' value='".$LANG['reports'][15]."' class='submit'></td></tr>";
 
-
-echo "</table>";
-echo "</div>";
-echo "</form>";
-
-
+echo "</table></form>";
 
 commonFooter();
 
