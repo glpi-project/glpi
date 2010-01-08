3a7e805da696a6d077fcf76abfd57aa0299f1a5b
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-01-08 16:53:42 +0000

    more phpunit tests (+ security test in reminder class)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@10074 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- tools/phpunit/CommonDBTM/DeleteRestore.php
+++ tools/phpunit/CommonDBTM/DeleteRestore.php
@@ -30,22 +30,83 @@
 class CommonDBTM_DeleteRestore extends PHPUnit_Framework_TestCase {
 
    /**
-    * Check right on Recursive object
+    * Check delete / purge on a pinter
     */
    public function testPrinter() {
 
       $printer = new Printer();
 
-      $id[0] = $printer->add(array('name'         => "Printer 1",
+      // Create
+      $id[0] = $printer->add(array('name'         => "Printer",
                                    'entities_id'  => 0,
-                                   'is_recursive' => 0));
+                                   'is_template'  => 0));
       $this->assertGreaterThan(0, $id[0], "Fail to create Printer 1");
-      $this->assertTrue($printer->getFromDB($id[0]), "Fail to read Printer 1");
+      $this->assertTrue($printer->getFromDB($id[0]), "Fail: can't read Printer");
+      $this->assertArrayHasKey('is_deleted',$printer->fields, "Fail: no is_deleted field");
+      $this->assertArrayHasKey('is_template',$printer->fields, "Fail: no is_template field");
+      $this->assertEquals(0, $printer->fields['is_deleted'], "Fail: is_deleted set");
+      $this->assertEquals(0, $printer->fields['is_template'], "Fail: is_template set");
+
+      // Delete
+      $this->assertTrue($printer->delete(array('id'=>$id[0])), "Fail: can't delete Printer");
+      $this->assertTrue($printer->getFromDB($id[0]), "Fail: can't read Printer");
+      $this->assertEquals(1, $printer->fields['is_deleted'], "Fail: is_deleted not set");
+
+      // Restore
+      $this->assertTrue($printer->restore(array('id'=>$id[0])), "Fail: can't restore Printer");
+      $this->assertTrue($printer->getFromDB($id[0]), "Fail: can't read Printer");
       $this->assertEquals(0, $printer->fields['is_deleted'], "Fail: is_deleted set");
 
-      $this->assertTrue($printer->delete(array('id'=>$id[0]),0), "Fail to delete Printer 1");
-      $this->assertTrue($printer->getFromDB($id[0]), "Fail to read Printer 1");
+      // Delete again
+      $this->assertTrue($printer->delete(array('id'=>$id[0])), "Fail: can't delete Printer");
+      $this->assertTrue($printer->getFromDB($id[0]), "Fail: can't read Printer");
       $this->assertEquals(1, $printer->fields['is_deleted'], "Fail: is_deleted not set");
+
+      // Purge
+      $this->assertTrue($printer->delete(array('id'=>$id[0]),1), "Fail: to purge Printer");
+      $this->assertFalse($printer->getFromDB($id[0]), "Fail: can read Printer (purged)");
+   }
+
+   /**
+    * Check delete / purge on a template of printer
+    */
+   public function testPrinterTemplate() {
+
+      $printer = new Printer();
+
+      // Create
+      $id[0] = $printer->add(array('name'         => "Printer 1",
+                                   'entities_id'  => 0,
+                                   'is_template'  => 1));
+      $this->assertGreaterThan(0, $id[0], "Fail to create Printer Template");
+      $this->assertTrue($printer->getFromDB($id[0]), "Fail: can't read Template");
+      $this->assertEquals(0, $printer->fields['is_deleted'], "Fail: is_deleted set");
+      $this->assertEquals(1, $printer->fields['is_template'], "Fail: is_template not set");
+
+      // Delete (= purge)
+      $this->assertTrue($printer->delete(array('id'=>$id[0]),0), "Fail: can't delete Template");
+      $this->assertFalse($printer->getFromDB($id[0]), "Fail: can read Template (deleted)");
+   }
+
+   /**
+    * Check delete / purge on a Reminder (no is_template, no is_deleted)
+    */
+   public function testReminder() {
+
+      $reminder = new Reminder();
+
+      // Create
+      $id[0] = $reminder->add(array('name'         => "Reminder",
+                                    'entities_id'  => 0,
+                                    'users_id'     => $_SESSION['glpiID']));
+      $this->assertGreaterThan(0, $id[0], "Fail to create Reminder");
+      $this->assertTrue($reminder->getFromDB($id[0]), "Fail: can't read Reminder");
+      $this->assertArrayNotHasKey('is_deleted',$reminder->fields, "Fail: is_deleted field");
+      $this->assertArrayNotHasKey('is_template',$reminder->fields, "Fail: is_template field");
+
+      // Delete (= purge)
+      $this->assertTrue($reminder->delete(array('id'=>$id[0])), "Fail: can't delete Reminder");
+      $this->assertFalse($reminder->getFromDB($id[0]), "Fail: can read Reminder (deleted)");
    }
 }
 ?>
