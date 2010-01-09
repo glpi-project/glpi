64fcd94809b9927b5b64b41e00a57518e47d4f3b
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-01-09 11:58:24 +0000

    swicth to isTemplate()
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@10086 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/infocom.class.php
+++ inc/infocom.class.php
@@ -261,7 +261,7 @@ class Infocom extends CommonDBTM {
                }
 
                // define message alert / Not for template items
-               if ($item->getField('is_template')!=1) {
+               if (!$item->isTemplate()) {
                   $message[$entity].=$LANG['mailing'][40]." ".
                                      $item->getTypeName()." - ".$item->getName()." : ".
                                      getWarrantyExpir($data["buy_date"],$data["warranty_duration"])."<br>";
