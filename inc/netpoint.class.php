abe430060a5d465a554ae31fc9828df79146e1cc
yllen <yllen>
2010-01-26 22:12:23 +0000

    coding standard

--- inc/netpoint.class.php
+++ inc/netpoint.class.php
@@ -94,7 +94,7 @@ class Netpoint extends CommonDropdown {
 
       $this->check(-1,'w',$input);
       for ($i=$input["_from"] ; $i<=$input["_to"] ; $i++) {
-         $input["name"]=$input["_before"].$i.$input["_after"];
+         $input["name"] = $input["_before"].$i.$input["_after"];
          $this->add($input);
       }
       Event::log(0, "dropdown", 5, "setup", $_SESSION["glpiname"]." ".$LANG['log'][20]);
@@ -114,65 +114,67 @@ class Netpoint extends CommonDropdown {
     * @return nothing (display the select box)
     *
     */
-   static function dropdownNetpoint($myname,$value=0,$locations_id=-1,$display_comment=1,$entity_restrict=-1,
-                             $devtype=-1) {
+   static function dropdownNetpoint($myname,$value=0,$locations_id=-1,$display_comment=1,
+                                    $entity_restrict=-1,$devtype=-1) {
+      global $CFG_GLPI,$LANG;
 
-      global $DB,$CFG_GLPI,$LANG;
-
-      $rand=mt_rand();
-      $name="------";
-      $comment="";
-      $limit_length=$_SESSION["glpidropdown_chars_limit"];
+      $rand = mt_rand();
+      $name = "------";
+      $comment = "";
+      $limit_length = $_SESSION["glpidropdown_chars_limit"];
       if (empty($value)) {
-         $value=0;
+         $value = 0;
       }
-      if ($value>0) {
-         $tmpname=Dropdown::getDropdownName("glpi_netpoints",$value,1);
-         if ($tmpname["name"]!="&nbsp;") {
-            $name=$tmpname["name"];
-            $comment=$tmpname["comment"];
-            $limit_length=max(utf8_strlen($name),$_SESSION["glpidropdown_chars_limit"]);
+      if ($value > 0) {
+         $tmpname = Dropdown::getDropdownName("glpi_netpoints",$value,1);
+         if ($tmpname["name"] != "&nbsp;") {
+            $name = $tmpname["name"];
+            $comment = $tmpname["comment"];
+            $limit_length = max(utf8_strlen($name),$_SESSION["glpidropdown_chars_limit"]);
          }
       }
-      $use_ajax=false;
+      $use_ajax = false;
       if ($CFG_GLPI["use_ajax"]) {
          if ($locations_id < 0 || $devtype == 'NetworkEquipment') {
-            $nb=countElementsInTableForEntity("glpi_netpoints",$entity_restrict);
+            $nb = countElementsInTableForEntity("glpi_netpoints",$entity_restrict);
          } else if ($locations_id > 0) {
-            $nb=countElementsInTable("glpi_netpoints", "locations_id=$locations_id ");
+            $nb = countElementsInTable("glpi_netpoints", "locations_id=$locations_id ");
          } else {
-            $nb=countElementsInTable("glpi_netpoints",
-                  "locations_id=0 ".getEntitiesRestrictRequest(" AND ","glpi_netpoints",'',$entity_restrict));
+            $nb = countElementsInTable("glpi_netpoints",
+                                       "locations_id=0 ".getEntitiesRestrictRequest(" AND ",
+                                                                                    "glpi_netpoints",
+                                                                                    '',
+                                                                                    $entity_restrict));
          }
-         if ($nb>$CFG_GLPI["ajax_limit_count"]) {
-            $use_ajax=true;
+         if ($nb > $CFG_GLPI["ajax_limit_count"]) {
+            $use_ajax = true;
          }
       }
 
-      $params=array('searchText'       => '__VALUE__',
-                    'value'            => $value,
-                    'locations_id'     => $locations_id,
-                    'myname'           => $myname,
-                    'limit'            => $limit_length,
-                    'comment'          => $display_comment,
-                    'rand'             => $rand,
-                    'entity_restrict'  => $entity_restrict,
-                    'devtype'          => $devtype,);
+      $params = array('searchText'      => '__VALUE__',
+                      'value'           => $value,
+                      'locations_id'    => $locations_id,
+                      'myname'          => $myname,
+                      'limit'           => $limit_length,
+                      'comment'         => $display_comment,
+                      'rand'            => $rand,
+                      'entity_restrict' => $entity_restrict,
+                      'devtype'         => $devtype,);
 
-      $default="<select name='$myname'><option value='$value'>$name</option></select>";
+      $default = "<select name='$myname'><option value='$value'>$name</option></select>";
       ajaxDropdown($use_ajax,"/ajax/dropdownNetpoint.php",$params,$default,$rand);
 
       // Display comment
       if ($display_comment) {
-         echo "<img alt='' src='".$CFG_GLPI["root_doc"]."/pics/aide.png'
-                onmouseout=\"cleanhide('comment_$myname$rand')\"
-                onmouseover=\"cleandisplay('comment_$myname$rand')\" >";
+         echo "<img alt='' src='".$CFG_GLPI["root_doc"]."/pics/aide.png'".
+                "onmouseout='cleanhide('comment_$myname$rand')'".
+                "onmouseover='cleandisplay('comment_$myname$rand')'>";
 
          $item = new Netpoint();
          if ($item->canCreate()) {
             echo "<img alt='' title='".$LANG['buttons'][8]."' src='".$CFG_GLPI["root_doc"].
-                  "/pics/add_dropdown.png' style='cursor:pointer; margin-left:2px;'  onClick=\"var w = window.open('".
-                  $item->getFormURL().
+                  "/pics/add_dropdown.png' style='cursor:pointer; margin-left:2px;' ".
+                  "onClick=\"var w = window.open('".$item->getFormURL().
                   "?popup=1&amp;rand=$rand' ,'glpipopup', 'height=400, ".
                   "width=1000, top=100, left=100, scrollbars=yes' );w.focus();\">";
          }
@@ -191,7 +193,6 @@ class Netpoint extends CommonDropdown {
       global $DB;
 
       if (!empty($input["name"])) {
-
          $query = "SELECT `id`
                    FROM `".$this->getTable()."`
                    WHERE `name` = '".$input["name"]."'
@@ -208,6 +209,7 @@ class Netpoint extends CommonDropdown {
       }
       return -1;
    }
+
 }
 
 ?>
