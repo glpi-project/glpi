592f3164317aedb811e6c3258146e70a686b1fc0
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-02-02 14:27:43 +0000

    work on ticket rights / use only ticket.form.php for both interface
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@10354 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/helpdesk.public.php
+++ front/helpdesk.public.php
@@ -67,59 +67,9 @@ if (isset($_GET["redirect"])) {
    manageRedirect($_GET["redirect"]);
 }
 
-if (isset($_GET["show"]) && strcmp($_GET["show"],"user") == 0) {
-   checkHelpdeskAccess();
-
-   //*******************
-   // Affichage interventions en cours
-   //******************
-   if (isset($_POST['add']) && haveRight("add_followups","1")) {
-      $fup = new TicketFollowup();
-      $newID = $fup->add($_POST);
-
-      Event::log($_POST["tickets_id"], "tracking", 4, "tracking",
-               $_SESSION["glpiname"]." ".$LANG['log'][20]." $newID.");
-      glpi_header($CFG_GLPI["root_doc"]."/front/helpdesk.public.php?show=user&id=".
-                  $_POST["tickets_id"]);
-   }
-//    if (!isset($_GET["start"])) {
-//       $_GET["start"] = 0;
-//    }
-
-   helpHeader($LANG['title'][1],$_SERVER['PHP_SELF'],$_SESSION["glpiname"]);
-
-    if (!isset($_GET["id"])) {
-
-
-   // Manage default value : search not old tickets : show=user in GET param by default
-   if (!isset($_GET) || !is_array($_GET) || count($_GET)<=1) {
-      $_GET=array('field'      => array(0=>12),
-                  'searchtype' => array(0=>'equals'),
-                  'contains'   => array(0=>'notold'),);
-   }
-
-   Search::show('Ticket');
-
-   } else {
-      $track = new Ticket();
-
-      if (isset($_POST["update"])) {
-         $track->update($_POST);
-         glpi_header($_SERVER['PHP_SELF']."?show=user&id=".$_POST["id"]);
-      }
-      $track = new Ticket();
-      $track->showForm($_GET["id"]);
-   }
-
-//*******************
-// fin  Affichage Module reservation
-//*******************
-
-} else {
-   checkHelpdeskAccess();
-   helpHeader($LANG['title'][1],$_SERVER['PHP_SELF'],$_SESSION["glpiname"]);
-   printHelpDesk($_SESSION["glpiID"],1);
-}
+checkHelpdeskAccess();
+helpHeader($LANG['title'][1],$_SERVER['PHP_SELF'],$_SESSION["glpiname"]);
+printHelpDesk($_SESSION["glpiID"],1);
 
 helpFooter();
 
