6ed236fbdf44e90a7507ad4109ce4598ae2dad43
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-02-09 14:40:55 +0000

    fix last commit
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@10442 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/includes.php
+++ inc/includes.php
@@ -96,7 +96,6 @@ include_once (GLPI_ROOT . "/inc/display.function.php");
 include_once (GLPI_ROOT . "/inc/ajax.function.php");
 include_once (GLPI_ROOT . "/inc/dropdown.function.php");
 include_once (GLPI_ROOT . "/inc/setup.function.php");
-include_once (GLPI_ROOT . "/inc/networkequipment.function.php");
 
 // Standard includes
 include_once (GLPI_ROOT . "/inc/dbmysql.class.php");
