ca73f2e624ea6397f7fbf5a206fb7fbe40d7e7cf
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-02-09 08:04:27 +0000

    svn properties
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@10414 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/notificationmailsetting.form.php
+++ front/notificationmailsetting.form.php
@@ -2,7 +2,7 @@
 
 
 /*
- * @version $Id: HEADER 7762 2009-01-06 18:30:32Z moyo $
+ * @version $Id$
  -------------------------------------------------------------------------
  GLPI - Gestionnaire Libre de Parc Informatique
  Copyright (C) 2003-2010 by the INDEPNET Development Team.
