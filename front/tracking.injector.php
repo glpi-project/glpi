557fb56ade727d2edf567f37f4cf87341a92c89d
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-03-04 21:59:22 +0000

    correct link for helpdesk
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@10895 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/tracking.injector.php
+++ front/tracking.injector.php
@@ -81,7 +81,7 @@ if ($newID = $track->add($_POST)){
       echo "<div class='center b'>";
       echo "<img src=\"".$CFG_GLPI["root_doc"]."/pics/ok.png\" alt=\"OK\"><br><br>";
       echo $LANG['help'][18]." (".$LANG['job'][38];
-      echo "<a href='helpdesk.public.php?show=user&amp;id=$newID'>$newID</a>)<br>";
+      echo "<a href='".$CFG_GLPI["root_doc"]."/front/ticket.form.php?id=$newID'>$newID</a>)<br>";
       echo $LANG['help'][19]."</div>";
    }
 } else {
