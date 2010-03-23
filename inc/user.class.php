0fc8f0d2a8ddd3e31fb97d5c517ab939addb61e0
moyooo <moyo@indepnet.net>
2010-03-23 09:42:42 +0000

    w3c on ticket + show close date

--- inc/user.class.php
+++ inc/user.class.php
@@ -1875,7 +1875,7 @@ class User extends CommonDBTM {
          echo "<img alt='' title='".$LANG['ldap'][35]."' src='".$CFG_GLPI["root_doc"].
                "/pics/add_dropdown.png' style='cursor:pointer; margin-left:2px;'
                 onClick=\"var w = window.open('".$CFG_GLPI['root_doc'].
-               "/front/popup.php?popup=add_ldapuser&rand=$rand&ampentity=".$_SESSION['glpiactive_entity']."' ,
+               "/front/popup.php?popup=add_ldapuser&amp;rand=$rand&amp;entity=".$_SESSION['glpiactive_entity']."' ,
                'glpipopup', 'height=400, ".
                "width=1000, top=100, left=100, scrollbars=yes' );w.focus();\">";
       }
