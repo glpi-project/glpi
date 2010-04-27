58cc6bd6b862d66a22a11b3e3adde75e15123972
moyooo <moyo@indepnet.net>
2010-04-27 07:45:04 +0000

    add entity informations to networkports (sql errors on networkport dropdown) : need a complete DB update

--- inc/computer.class.php
+++ inc/computer.class.php
@@ -44,7 +44,7 @@ class Computer extends CommonDBTM {
 
    // From CommonDBTM
    public $dohistory=true;
-   protected $forward_entity_to=array('Infocom','ComputerDisk','ReservationItem');
+   protected $forward_entity_to=array('Infocom','ComputerDisk','ReservationItem','NetworkPort');
    // Specific ones
    ///Device container - format $device = array(ID,"device type","ID in device table","specificity value")
    var $devices	= array();
