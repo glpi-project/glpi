eab2de61f4bb6847dcf7a49ec5c638122e7e34a7
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-06-10 16:48:43 +0000

    minor fix
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@11624 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/config.class.php
+++ inc/config.class.php
@@ -282,7 +282,7 @@ class Config extends CommonDBTM {
       echo "<tr class='tab_bg_2'><td>" . $LANG['setup'][112] . "&nbsp;:</td><td>";
       Dropdown::showInteger('cut', $CFG_GLPI["cut"], 50, 500,50);
       echo "</td>";
-      echo "</td><td>".$LANG['setup'][10]."&nbsp;</td><td>";
+      echo "</td><td>".$LANG['setup'][10]."&nbsp;:</td><td>";
       $values = array (REALNAME_BEFORE=>$LANG['common'][48]." ".$LANG['common'][43],
                        FIRSTNAME_BEFORE=>$LANG['common'][43]." ".$LANG['common'][48]);
       Dropdown::showFromArray('names_format',$values,$CFG_GLPI["names_format"]);
