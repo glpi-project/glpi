3ac021b5608a5a34d9d12153b01d4aafe27722d1
remi <remi>
2010-06-18 08:10:05 +0000

    fix is_global rank

--- inc/printer.class.php
+++ inc/printer.class.php
@@ -644,16 +644,16 @@ class Printer  extends CommonDBTM {
       $tab[80]['linkfield'] = 'entities_id';
       $tab[80]['name']      = $LANG['entity'][0];
 
-      $tab[83]['table']     = $this->getTable();
-      $tab[83]['field']     = 'is_global';
-      $tab[83]['linkfield'] = '';
-      $tab[83]['name']      = $LANG['common'][59];
-      $tab[83]['datatype']  = 'bool';
+      $tab[82]['table']     = $this->getTable();
+      $tab[82]['field']     = 'is_global';
+      $tab[82]['linkfield'] = '';
+      $tab[82]['name']      = $LANG['peripherals'][31];
+      $tab[82]['datatype']  = 'bool';
 
       $tab[86]['table']     = $this->getTable();
       $tab[86]['field']     = 'is_recursive';
       $tab[86]['linkfield'] = 'is_recursive';
-      $tab[86]['name']      = $LANG['peripherals'][31];
+      $tab[86]['name']      = $LANG['entity'][9];
       $tab[86]['datatype']  = 'bool';
 
       return $tab;
