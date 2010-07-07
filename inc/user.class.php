f70bf046122de62b3356681cdca08a4c0e317ba3
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-07-07 07:04:08 +0000

    first try to improve stringsin user list
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@11862 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/user.class.php
+++ inc/user.class.php
@@ -1610,13 +1610,13 @@ class User extends CommonDBTM {
       $tab[20]['table']        = 'glpi_profiles';
       $tab[20]['field']        = 'name';
       $tab[20]['linkfield']    = '';
-      $tab[20]['name']         = $LANG['Menu'][35];
+      $tab[20]['name']         = $LANG['Menu'][35]." (- ".$LANG['entity'][0].")";
       $tab[20]['forcegroupby'] = true;
 
       $tab[80]['table']        = 'glpi_entities';
       $tab[80]['field']        = 'completename';
       $tab[80]['linkfield']    = 'entities_id';
-      $tab[80]['name']         = $LANG['entity'][0];
+      $tab[80]['name']         = $LANG['entity'][0]." (- ".$LANG['Menu'][35].")";
       $tab[80]['forcegroupby'] = true;
 
       $tab[81]['table']     = 'glpi_usertitles';
