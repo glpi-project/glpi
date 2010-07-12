2213a84db5e3ed93d475b9196ed116c6a2c0098e
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-07-12 12:40:36 +0000

    add comments
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@11885 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/dropdown.class.php
+++ inc/dropdown.class.php
@@ -101,6 +101,7 @@ class Dropdown {
       $limit_length=$_SESSION["glpidropdown_chars_limit"];
 
 
+      // Check default value for dropdown : need to be a numeric
       if (strlen($params['value'])==0 || !is_numeric($params['value'])) {
          $params['value']=-1;
       }
