3186dc3ac45164ade53d1fedf34288d9670178eb
yllen <yllen>
2010-08-05 08:43:13 +0000

    coding standard

--- inc/reservationitem.class.php
+++ inc/reservationitem.class.php
@@ -46,15 +46,17 @@ class ReservationItem extends CommonDBTM {
       return $LANG['Menu'][17];
    }
 
+
    // From CommonDBTM
    /**
     * Retrieve an item from the database for a specific item
     *
     *@param $ID ID of the item
     *@param $itemtype type of the item
+    *
     *@return true if succeed else false
    **/
-   function getFromDBbyItem($itemtype,$ID) {
+   function getFromDBbyItem($itemtype, $ID) {
       global $DB;
 
       $query = "SELECT *
@@ -71,6 +73,7 @@ class ReservationItem extends CommonDBTM {
       return false;
    }
 
+
    function cleanDBonPurge() {
       global $DB;
 
@@ -80,16 +83,18 @@ class ReservationItem extends CommonDBTM {
       $result2 = $DB->query($query2);
    }
 
+
    function prepareInputForAdd($input) {
 
-      if (!$this->getFromDBbyItem($input['itemtype'],$input['items_id'])) {
+      if (!$this->getFromDBbyItem($input['itemtype'], $input['items_id'])) {
          if (!isset($input['is_active'])) {
-            $input['is_active']=1;
+            $input['is_active'] = 1;
          }
          return $input;
       }
       return false;
    }
+
 
    function getSearchOptions() {
       global $LANG;
@@ -115,7 +120,7 @@ class ReservationItem extends CommonDBTM {
       $tab[2]['linkfield'] = 'id';
       $tab[2]['name']      = $LANG['common'][2];
 
-      $tab+=Location::getSearchOptionsToAdd();
+      $tab += Location::getSearchOptionsToAdd();
 
       $tab[16]['table']     = 'reservation_types';
       $tab[16]['field']     = 'comment';
@@ -158,8 +163,8 @@ class ReservationItem extends CommonDBTM {
    }
 
 
-   static function showActivationFormForItem($itemtype,$items_id) {
-      global $CFG_GLPI,$LANG;
+   static function showActivationFormForItem($itemtype, $items_id) {
+      global $CFG_GLPI, $LANG;
 
       if (!haveRight("reservation_central","w")) {
          return false;
@@ -175,39 +180,44 @@ class ReservationItem extends CommonDBTM {
                return false;
             }
          }
+
       } else {
          return false;
       }
-      $ri=new ReservationItem;
+
+      $ri = new ReservationItem;
 
       echo "<div><form method='post' name=form action='".getItemTypeFormURL('ReservationItem')."'>";
       echo "<table class='tab_cadre_fixe'>";
       echo "<tr><th>".$LANG['reservation'][9]."</th></tr>";
       echo "<tr class='tab_bg_1'>";
       if ($ri->getFromDBbyItem($itemtype,$items_id)) {
-         echo "<td align='center'>";
+         echo "<td class='center'>";
          //Switch reservation state
          echo "<input type='hidden' name='id' value='".$ri->fields['id']."'>";
          if ($ri->fields["is_active"]) {
             echo "<input type='hidden' name='is_active' value='0'>";
-            echo "<input type='submit' name='update' value=\"".$LANG['reservation'][3]."\" class='submit'>";
+            echo "<input type='submit' name='update' value='".$LANG['reservation'][3]."'
+                   class='submit'>";
          } else {
             echo "<input type='hidden' name='is_active' value='1'>";
-            echo "<input type='submit' name='update' value=\"".$LANG['reservation'][5]."\" class='submit'>";
+            echo "<input type='submit' name='update' value='".$LANG['reservation'][5]."'
+                   class='submit'>";
          }
          echo "&nbsp;&nbsp;&nbsp;";
-         echo "<input type='submit' name='delete' value=\"".$LANG['reservation'][6]."\"
+         echo "<input type='submit' name='delete' value='".$LANG['reservation'][6]."'
                class='submit' OnClick='return window.confirm(\"".$LANG['reservation'][38]. "\")'>";
          echo "</td>";
       } else {
-         echo "<td align='center'>";
+         echo "<td class='center'>";
          echo "<input type='hidden' name='items_id' value='$items_id'>";
          echo "<input type='hidden' name='itemtype' value='$itemtype'>";
-         echo "<input type='submit' name='add' value=\"".$LANG['reservation'][7]."\" class='submit'>";
+         echo "<input type='submit' name='add' value='".$LANG['reservation'][7]."' class='submit'>";
          echo "</td>";
       }
       echo "</tr></table></form></div>";
    }
+
 
    function showForm($ID, $options=array()) {
       global $LANG;
@@ -216,7 +226,7 @@ class ReservationItem extends CommonDBTM {
          return false;
       }
 
-      $r=new ReservationItem;
+      $r = new ReservationItem;
 
       if ($r->getFromDB($ID)) {
          $type = $r->fields["itemtype"];
@@ -225,7 +235,7 @@ class ReservationItem extends CommonDBTM {
             $item = new $r->fields["itemtype"]();
             $type = $item->getTypeName();
             if ($item->getFromDB($r->fields["items_id"])) {
-               $name=$item->getName();
+               $name = $item->getName();
             }
          }
 
@@ -233,22 +243,22 @@ class ReservationItem extends CommonDBTM {
          echo "<input type='hidden' name='id' value='$ID'>";
          echo "<table class='tab_cadre'>";
          echo "<tr><th colspan='2'>".$LANG['reservation'][22]."</th></tr>";
+
          // Ajouter le nom du materiel
          echo "<tr class='tab_bg_1'><td>".$LANG['common'][1]."&nbsp;:</td>";
-         echo "<td><strong>$type - $name</strong></td></tr>\n";
+         echo "<td class='b'>$type - $name</td></tr>\n";
 
          echo "<tr class='tab_bg_1'><td>".$LANG['common'][25]."&nbsp;:</td>";
-         echo "<td>";
-         echo "<textarea name='comment' cols='30' rows='10' >".$r->fields["comment"]."</textarea>";
+         echo "<td><textarea name='comment' cols='30' rows='10' >".$r->fields["comment"]."</textarea>";
          echo "</td></tr>\n";
 
-         echo "<tr class='tab_bg_2'>";
-         echo "<td colspan='2' class='top center'>";
-         echo "<input type='submit' name='update' value=\"".$LANG['buttons'][14]."\" class='submit'>";
+         echo "<tr class='tab_bg_2'><td colspan='2' class='top center'>";
+         echo "<input type='submit' name='update' value='".$LANG['buttons'][14]."' class='submit'>";
          echo "</td></tr>\n";
 
          echo "</table></form></div>";
          return true;
+
       } else {
          return false;
       }
@@ -256,15 +266,15 @@ class ReservationItem extends CommonDBTM {
 
 
    static function showListSimple() {
-      global $DB,$LANG,$CFG_GLPI;
+      global $DB, $LANG, $CFG_GLPI;
 
       if (!haveRight("reservation_helpdesk","1")) {
          return false;
       }
 
-      $ri=new ReservationItem;
-      $ok=false;
-      $showentity=isMultiEntitiesMode();
+      $ri = new ReservationItem;
+      $ok = false;
+      $showentity = isMultiEntitiesMode();
 
       echo "<div class='center'><form name='form' method='get' action='reservation.form.php'>";
       echo "<table class='tab_cadre'>";
@@ -274,36 +284,40 @@ class ReservationItem extends CommonDBTM {
          if (!class_exists($itemtype)) {
             continue;
          }
-         $item=new $itemtype();
-         $itemtable=getTableForItemType($itemtype);
-         $query = "SELECT `glpi_reservationitems`.`id`, `glpi_reservationitems`.`comment`,
-                        `$itemtable`.`name` AS name,
-                        `$itemtable`.`entities_id` AS entities_id,
-                        `glpi_locations`.`completename` AS location,
-                        `glpi_reservationitems`.`items_id` AS items_id
-                  FROM `glpi_reservationitems`
-                  INNER JOIN `$itemtable`
+         $item = new $itemtype();
+         $itemtable = getTableForItemType($itemtype);
+         $query = "SELECT `glpi_reservationitems`.`id`,
+                          `glpi_reservationitems`.`comment`,
+                          `$itemtable`.`name` AS name,
+                          `$itemtable`.`entities_id` AS entities_id,
+                          `glpi_locations`.`completename` AS location,
+                          `glpi_reservationitems`.`items_id` AS items_id
+                   FROM `glpi_reservationitems`
+                   INNER JOIN `$itemtable`
                         ON (`glpi_reservationitems`.`itemtype` = '$itemtype'
-                           AND `glpi_reservationitems`.`items_id` = `$itemtable`.`id`)
-                  LEFT JOIN `glpi_locations`
+                            AND `glpi_reservationitems`.`items_id` = `$itemtable`.`id`)
+                   LEFT JOIN `glpi_locations`
                         ON (`$itemtable`.`locations_id` = `glpi_locations`.`id`)
-                  WHERE `glpi_reservationitems`.`is_active` = '1'
-                        AND `$itemtable`.`is_deleted` = '0'".
-                        getEntitiesRestrictRequest(" AND",$itemtable,'',$_SESSION['glpiactiveentities'],$item->maybeRecursive())."
-                  ORDER BY `$itemtable`.`entities_id`,`$itemtable`.`name`";
+                   WHERE `glpi_reservationitems`.`is_active` = '1'
+                         AND `$itemtable`.`is_deleted` = '0'".
+                         getEntitiesRestrictRequest(" AND", $itemtable, '',
+                                                    $_SESSION['glpiactiveentities'],
+                                                    $item->maybeRecursive())."
+                   ORDER BY `$itemtable`.`entities_id`,
+                            `$itemtable`.`name`";
 
-         if ($result = $DB->query($query)) {
+         if ($result=$DB->query($query)) {
             while ($row=$DB->fetch_array($result)) {
                echo "<tr class='tab_bg_2'><td>";
                echo "<input type='checkbox' name='item[".$row["id"]."]' value='".$row["id"]."'></td>";
-               $typename=$item->getTypeName();
+               $typename = $item->getTypeName();
                if ($itemtype == 'Peripheral') {
                   $item->getFromDB($row['items_id']);
                   if (isset($item->fields["peripheraltypes_id"])
                      && $item->fields["peripheraltypes_id"]!=0) {
 
-                     $typename=Dropdown::getDropdownName("glpi_peripheraltypes",
-                                             $item->fields["peripheraltypes_id"]);
+                     $typename = Dropdown::getDropdownName("glpi_peripheraltypes",
+                                                           $item->fields["peripheraltypes_id"]);
                   }
                }
                echo "<td><a href='reservation.php?reservationitems_id=".$row['id']."'>$typename - ".
@@ -311,27 +325,29 @@ class ReservationItem extends CommonDBTM {
                echo "<td>".$row["location"]."</td>";
                echo "<td>".nl2br($row["comment"])."</td>";
                if ($showentity) {
-                  echo "<td>".Dropdown::getDropdownName("glpi_entities",$row["entities_id"])."</td>";
+                  echo "<td>".Dropdown::getDropdownName("glpi_entities", $row["entities_id"])."</td>";
                }
                echo "</tr>\n";
-               $ok=true;
+               $ok = true;
             }
          }
       }
       if ($ok) {
          echo "<tr class='tab_bg_1 center'><td colspan='".($showentity?"5":"4")."'>";
-         echo "<input type='submit' value=\"".$LANG['buttons'][8]."\" class='submit'></td></tr>\n";
+         echo "<input type='submit' value='".$LANG['buttons'][8]."' class='submit'></td></tr>\n";
       }
       echo "</table>\n";
       echo "<input type='hidden' name='id' value=''>";
       echo "</form></div>\n";
    }
 
+
    static function cronInfo($name) {
       global $LANG;
 
       return array('description' => $LANG['setup'][707]);
    }
+
 
    /**
     * Cron action on reservation : alert on end of reservations
@@ -341,15 +357,15 @@ class ReservationItem extends CommonDBTM {
     * @return 0 : nothing to do 1 : done with success
     **/
    static function cronReservation($task=NULL) {
-      global $DB,$CFG_GLPI,$LANG;
+      global $DB, $CFG_GLPI, $LANG;
 
       if (!$CFG_GLPI["use_mailing"]) {
          return 0;
       }
 
-      $message=array();
-      $cron_status = 0;
-      $items_infos = array();
+      $message        = array();
+      $cron_status    = 0;
+      $items_infos    = array();
       $items_messages = array();
 
       foreach (Entity::getEntitiesToNotify('use_reservations_alert') as $entity => $value) {
@@ -357,65 +373,70 @@ class ReservationItem extends CommonDBTM {
 
 
          // Reservation already begin and reservation ended in $value hours
-         $query_end = "SELECT `glpi_reservationitems`.*, `glpi_reservations`.`end` as `end`,
-                              `glpi_reservations`.`id` as `resaid`
+         $query_end = "SELECT `glpi_reservationitems`.*,
+                              `glpi_reservations`.`end` AS `end`,
+                              `glpi_reservations`.`id` AS `resaid`
                        FROM `glpi_reservations`
-                       LEFT JOIN `glpi_alerts` ON (`glpi_reservations`.`id` = `glpi_alerts`.`items_id`
-                                                     AND `glpi_alerts`.`itemtype` = 'Reservation'
-                                                     AND `glpi_alerts`.`type`='".Alert::END."')
-                       LEFT JOIN `glpi_reservationitems` ON (
-                                 `glpi_reservations`.`reservationitems_id` = `glpi_reservationitems`.`id`)
-                       WHERE `glpi_reservationitems`.`entities_id`='".$entity."'
-                         AND (UNIX_TIMESTAMP(`glpi_reservations`.`end`) - $secs) < UNIX_TIMESTAMP()
-                         AND `glpi_reservations`.`begin` < NOW()
-                         AND `glpi_alerts`.`date` IS NULL";
-            foreach ($DB->request($query_end) as $data) {
-               $item_resa = new $data["itemtype"]();
-               if ($item_resa->getFromDB($data["items_id"])) {
-                  $message .= $LANG['reservation'][40]." ".
-                                 $item_resa->getTypeName()." - ".$item_resa->getName()."<br />";
-                  $data['item_name'] = $item_resa->getName();
-                  $data['entity'] = $entity;
-                  $items_infos[$entity][$data['resaid']] = $data;
+                       LEFT JOIN `glpi_alerts`
+                           ON (`glpi_reservations`.`id` = `glpi_alerts`.`items_id`
+                               AND `glpi_alerts`.`itemtype` = 'Reservation'
+                               AND `glpi_alerts`.`type` = '".Alert::END."')
+                       LEFT JOIN `glpi_reservationitems`
+                           ON (`glpi_reservations`.`reservationitems_id` = `glpi_reservationitems`.`id`)
+                       WHERE `glpi_reservationitems`.`entities_id` = '$entity'
+                             AND (UNIX_TIMESTAMP(`glpi_reservations`.`end`) - $secs) < UNIX_TIMESTAMP()
+                             AND `glpi_reservations`.`begin` < NOW()
+                             AND `glpi_alerts`.`date` IS NULL";
 
-                  if (!isset($items_messages[$entity])) {
-                     $items_messages[$entity] = $LANG['reservation'][40]."<br />";
-                  }
-                  $items_messages[$entity] .= $message;
+         foreach ($DB->request($query_end) as $data) {
+            $item_resa = new $data["itemtype"]();
+            if ($item_resa->getFromDB($data["items_id"])) {
+               $message .= $LANG['reservation'][40]." ". $item_resa->getTypeName()." - ".
+                           $item_resa->getName()."<br />";
+               $data['item_name'] = $item_resa->getName();
+               $data['entity'] = $entity;
+               $items_infos[$entity][$data['resaid']] = $data;
+
+               if (!isset($items_messages[$entity])) {
+                  $items_messages[$entity] = $LANG['reservation'][40]."<br />";
                }
+               $items_messages[$entity] .= $message;
             }
+         }
       }
-      foreach ($items_infos as $entity => $items) {
 
+      foreach ($items_infos as $entity => $items) {
          $resitem = new ReservationItem;
-         if (NotificationEvent::raiseEvent("alert",new Reservation(),
-                                           array('entities_id'=>$entity,'items'=>$items))) {
+         if (NotificationEvent::raiseEvent("alert", new Reservation(),
+                                           array('entities_id' => $entity,
+                                                 'items'       => $items))) {
             $message = $items_messages[$entity];
             $cron_status = 1;
             if ($task) {
                $task->log(Dropdown::getDropdownName("glpi_entities",
-                                                      $entity).":  $message\n");
+                                                    $entity).":  $message\n");
                $task->addVolume(1);
             } else {
                addMessageAfterRedirect(Dropdown::getDropdownName("glpi_entities",
                                                                  $entity).":  $message");
             }
 
-            $alert=new Alert();
+            $alert = new Alert();
             $input["itemtype"] = 'Reservation';
-            $input["type"]=Alert::END;
+            $input["type"] = Alert::END;
             foreach ($items as $resaid => $item) {
-               $input["items_id"]=$resaid;
+               $input["items_id"] = $resaid;
                $alert->add($input);
                unset($alert->fields['id']);
             }
+
          } else {
             if ($task) {
-               $task->log(Dropdown::getDropdownName("glpi_entities",$entity).
+               $task->log(Dropdown::getDropdownName("glpi_entities", $entity).
                           ":  Send reservationitem alert failed\n");
             } else {
-               addMessageAfterRedirect(Dropdown::getDropdownName("glpi_entities",$entity).
-                                       ":  Send reservationitem alert failed",false,ERROR);
+               addMessageAfterRedirect(Dropdown::getDropdownName("glpi_entities", $entity).
+                                       ":  Send reservationitem alert failed", false, ERROR);
             }
          }
       }
@@ -423,6 +444,5 @@ class ReservationItem extends CommonDBTM {
    }
 
 }
-
 
 ?>
