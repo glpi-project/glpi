dac7cfbe6a02a1618cd5282a026fc6979a1aa442
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-08-06 12:24:34 +0000

    coding standard
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@12062 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/authmail.class.php
+++ inc/authmail.class.php
@@ -48,22 +48,28 @@ class AuthMail extends CommonDBTM {
       return $LANG['login'][3];
    }
 
+
    function canCreate() {
       return haveRight('config', 'w');
    }
+
 
    function canView() {
       return haveRight('config', 'r');
    }
 
+
    function prepareInputForUpdate($input) {
+
       if (isset ($input['mail_server']) && !empty ($input['mail_server'])) {
          $input["connect_string"] = constructMailServerConfig($input);
       }
       return $input;
    }
 
+
    function prepareInputForAdd($input) {
+
       if (isset ($input['mail_server']) && !empty ($input['mail_server'])) {
          $input["connect_string"] = constructMailServerConfig($input);
       }
@@ -78,10 +84,11 @@ class AuthMail extends CommonDBTM {
       $ong[1] = $LANG['title'][26];
 
       if ($this->fields['id'] > 0) {
-         $ong[12]=$LANG['title'][38];
+         $ong[12] = $LANG['title'][38];
       }
       return $ong;
    }
+
 
    function getSearchOptions() {
       global $LANG;
@@ -123,9 +130,9 @@ class AuthMail extends CommonDBTM {
       $tab[16]['name']      = $LANG['common'][25];
       $tab[16]['datatype']  = 'text';
 
-
       return $tab;
    }
+
 
    /**
     * Print the auth mail form
@@ -133,7 +140,7 @@ class AuthMail extends CommonDBTM {
     * @param $ID Integer : ID of the item
     * @param $options array
     *
-    *@return Nothing (display)
+    * @return Nothing (display)
     **/
    function showForm($ID, $options=array()) {
       global $LANG;
@@ -166,7 +173,6 @@ class AuthMail extends CommonDBTM {
          echo "<tr><th>" . $LANG['login'][3] . "</th><th>";
          echo ($ID>0?$LANG['common'][26]."&nbsp;: ".convDateTime($this->fields["date_mod"]):'&nbsp;');
          echo "</th></tr>";*/
-
          echo "<tr class='tab_bg_1'><td>" . $LANG['common'][16] . "&nbsp;:</td>";
          echo "<td><input size='30' type='text' name='name' value='" . $this->fields["name"] . "'>";
          echo "</td></tr>";
@@ -186,9 +192,9 @@ class AuthMail extends CommonDBTM {
 
          echo "</td></tr>";
 
-
          $this->showFormButtons($options);
          $this->addDivForTabs();
+
       } else {
          echo "<div class='center'>&nbsp;<table class='tab_cadre_fixe'>";
          echo "<tr><th colspan='2'>" . $LANG['setup'][162] . "</th></tr>";
@@ -198,6 +204,7 @@ class AuthMail extends CommonDBTM {
       }
    }
 
+
    function showFormTestMail ($ID) {
       global $LANG;
 
@@ -206,17 +213,18 @@ class AuthMail extends CommonDBTM {
          echo "<input type='hidden' name='imap_string' value=\"".$this->fields['connect_string']."\">";
          echo "<div class='center'><table class='tab_cadre'>";
          echo "<tr><th colspan='2'>" . $LANG['login'][21] . "</th></tr>";
+
          echo "<tr class='tab_bg_2'><td class='center'>" . $LANG['login'][6] . "</td>";
          echo "<td><input size='30' type='text' name='imap_login' value=''></td></tr>";
+
          echo "<tr class='tab_bg_2'><td class='center'>" . $LANG['login'][7] . "</td>";
          echo "<td><input size='30' type='password' name='imap_password' value=''
-                           autocomplete='off'></td>";
-         echo "</tr><tr class='tab_bg_2'><td class='center' colspan=2>";
-         echo "<input type='submit' name='test' class='submit'
-                  value=\"" . $LANG['buttons'][2] . "\" ></td></tr>";
-         echo "</table></div></form>";
-      }
+                    autocomplete='off'></td></tr>";
 
+         echo "<tr class='tab_bg_2'><td class='center' colspan=2>";
+         echo "<input type='submit' name='test' class='submit' value='".$LANG['buttons'][2]."'></td>";
+         echo "</tr></table></div></form>";
+      }
    }
 
 
@@ -232,23 +240,29 @@ class AuthMail extends CommonDBTM {
       $sql = "SELECT count(*)
               FROM `glpi_authmails`";
       $result = $DB->query($sql);
-      if ($DB->result($result,0,0) > 0) {
+
+      if ($DB->result($result, 0, 0) > 0) {
          return true;
       }
       return false;
    }
+
 
    /**
     * Test a connexion to the IMAP/POP server
     * @param $connect_string : mail server
     * @param $login : user login
     * @param $password : user password
+    *
     * @return authentification succeeded ?
    **/
-   static function testAuth($connect_string,$login,$password) {
+   static function testAuth($connect_string, $login, $password) {
+
       $auth = new Auth();
-      return $auth->connection_imap($connect_string, decodeFromUtf8($login), decodeFromUtf8($password));
+      return $auth->connection_imap($connect_string, decodeFromUtf8($login),
+                                    decodeFromUtf8($password));
    }
+
 
    /**
     * Authentify a user by checking a specific mail server
@@ -256,14 +270,15 @@ class AuthMail extends CommonDBTM {
     * @param $login : user login
     * @param $password : user password
     * @param $mail_method : mail_method array to use
+    *
     * @return identification object
    **/
-   static function mailAuth($auth, $login,$password,$mail_method) {
+   static function mailAuth($auth, $login, $password, $mail_method) {
 
       if (isset($mail_method["connect_string"]) && !empty ($mail_method["connect_string"])) {
          $auth->auth_succeded = $auth->connection_imap($mail_method["connect_string"],
-                                                                     decodeFromUtf8($login),
-                                                                     decodeFromUtf8($password));
+                                                       decodeFromUtf8($login),
+                                                       decodeFromUtf8($password));
          if ($auth->auth_succeded) {
             $auth->extauth = 1;
             $auth->user_present = $auth->user->getFromDBbyName(addslashes($login));
@@ -276,27 +291,30 @@ class AuthMail extends CommonDBTM {
       return $auth;
    }
 
+
    /**
     * Try to authentify a user by checking all the mail server
     * @param $auth : identification object
     * @param $login : user login
     * @param $password : user password
     * @param $auths_id : auths_id already used for the user
+    *
     * @return identification object
    **/
-   static function tryMailAuth($auth, $login,$password,$auths_id = 0) {
+   static function tryMailAuth($auth, $login, $password, $auths_id = 0) {
+
       if ($auths_id <= 0) {
          foreach ($auth->authtypes["mail"] as $mail_method) {
             if (!$auth->auth_succeded) {
-               $auth = AuthMail::mailAuth($auth, $login,$password,$mail_method);
+               $auth = AuthMail::mailAuth($auth, $login, $password, $mail_method);
             } else {
                break;
             }
          }
-      } else if(array_key_exists($auths_id,$auth->authtypes["mail"])){
+
+      } else if (array_key_exists($auths_id,$auth->authtypes["mail"])) {
          //Check if the mail server indicated as the last good one still exists !
-         $auth = AuthMail::mailAuth($auth, $login,$password,
-                                  $auth->authtypes["mail"][$auths_id]);
+         $auth = AuthMail::mailAuth($auth, $login, $password, $auth->authtypes["mail"][$auths_id]);
       }
       return $auth;
    }
