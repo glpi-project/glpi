6cf87c29b9b82cbc7df1248f94ba6651c4bc5661
moyooo <moyo@indepnet.net>
2010-09-03 12:28:56 +0000

    [0.78] fix ldap user import in a specific entity

--- inc/authldap.class.php
+++ inc/authldap.class.php
@@ -1952,6 +1952,11 @@ class AuthLDAP extends CommonDBTM {
                $_SESSION['ldap_import']['ldapservers_id'] = $entitydata->getField('ldapservers_id');
                $_SESSION['ldap_import']['basedn']         = $entitydata->getField('ldap_dn');
 
+               // No dn specified in entity : use standard one
+               if (empty($_SESSION['ldap_import']['basedn'])) {
+                  $_SESSION['ldap_import']['basedn'] = $authldap->getField('basedn');
+               }
+
                if ($entitydata->getField('entity_ldapfilter') != NOT_AVAILABLE) {
                   $_SESSION['ldap_import']['entity_filter'] =
                                                          $entitydata->getField('entity_ldapfilter');
@@ -2045,7 +2050,7 @@ class AuthLDAP extends CommonDBTM {
                }
                echo "<tr class='tab_bg_2'><td>Basedn</td><td colspan='3'>";
                echo "<input type='text' name='basedn' value='".$_SESSION['ldap_import']['basedn'].
-                     "' size='90' ".(!$_SESSION['ldap_import']['basedn']?"disabled":"").">";
+                     "' size='90'>";
                echo "</td></tr>";
                echo "<tr class='tab_bg_2'><td>".$LANG['setup'][263]."</td><td colspan='3'>";
                echo "<input type='text' name='ldap_filter' value='".
