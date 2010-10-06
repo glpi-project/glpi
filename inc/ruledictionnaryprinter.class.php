dc932242e22eb37055f4d0f21fefbac05ff22fd5
Walid Nouh <wnouh@teclib.com>
2010-10-06 11:10:51 +0000

    see #2300 Add missing files

---
+++ inc/ruledictionnaryprinter.class.php
@@ -0,0 +1,153 @@
+<?php
+
+
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2010 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Walid Nouh
+// Purpose of file:
+// ----------------------------------------------------------------------
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+/**
+* Rule class store all informations about a GLPI rule :
+*   - description
+*   - criterias
+*   - actions
+*
+**/
+class RuleDictionnaryPrinter extends RuleCached {
+
+   // From Rule
+   public $right='rule_dictionnary_printer';
+   public $can_sort=true;
+
+   function getTitle() {
+      global $LANG;
+
+      return $LANG['rulesengine'][39];
+   }
+
+   function maxActionsCount() {
+      return 4;
+   }
+
+   function showCacheRuleHeader() {
+      global $LANG;
+
+      echo "<tr><th colspan='6'>" . $LANG['rulesengine'][100] . "&nbsp;: " . $this->fields["name"];
+      echo "</th></tr>";
+      echo "<tr><td class='tab_bg_1 b'>" . $LANG['rulesengine'][104] . "</td>";
+      echo "<td class='tab_bg_1 b'>" . $LANG['common'][5] . " " . $LANG['rulesengine'][108] . "</td>";
+      echo "<td class='tab_bg_1 b'>" . $LANG['rulesengine'][105] . "</td>";
+      echo "<td class='tab_bg_1 b'>" . $LANG['peripherals'][33] . "</td>";
+      echo "<td class='tab_bg_1 b'>" . $LANG['common'][5] . " " . $LANG['rulesengine'][105] . "</td>";
+      echo "<td class='tab_bg_1 b'>" . $LANG['rulesengine'][132] . "</td></tr>";
+   }
+
+   function showCacheRuleDetail($fields) {
+      global $LANG;
+
+      echo "<td class='tab_bg_2'>" . $fields["old_value"] . "</td>";
+      echo "<td class='tab_bg_2'>" . $fields["manufacturer"] . "</td>";
+      echo "<td class='tab_bg_2'>" .
+            ($fields["new_value"] != '' ? $fields["new_value"] : $LANG['rulesengine'][106]) . "</td>";
+      echo "<td class='tab_bg_2'>" .
+            ($fields["is_global"] != '' ? Dropdown::getGlobalSwitch($fields["is_global"]) : $LANG['rulesengine'][106]) . "</td>";
+      echo "<td class='tab_bg_2'>" .
+            ((isset ($fields["new_manufacturer"]) && $fields["new_manufacturer"] != '') ?
+             Dropdown::getDropdownName("glpi_manufacturers", $fields["new_manufacturer"]) :
+             $LANG['rulesengine'][106]) . "</td>";
+      echo "<td class='tab_bg_2'>";
+      if ($fields["ignore_ocs_import"] == '') {
+         echo "&nbsp;";
+      } else {
+         echo Dropdown::getYesNo($fields["ignore_ocs_import"]);
+      }
+      echo "</td>";
+   }
+
+   function getCriterias() {
+      global $LANG;
+      $criterias = array();
+      $criterias['name']['field'] = 'name';
+      $criterias['name']['name']  = $LANG['common'][16];
+      $criterias['name']['table'] = 'glpi_printers';
+
+      $criterias['manufacturer']['field'] = 'name';
+      $criterias['manufacturer']['name']  = $LANG['common'][5];
+      $criterias['manufacturer']['table'] = '';
+
+      $criterias['PORT']['field'] = 'PORT';
+      $criterias['PORT']['name']  = $LANG['setup'][175];
+      $criterias['PORT']['table'] = '';
+
+      $criterias['DRIVER']['field'] = 'DRIVER';
+      $criterias['DRIVER']['name']  = $LANG['printers'][32];
+      $criterias['DRIVER']['table'] = '';
+
+      $criterias['model']['field'] = 'model';
+      $criterias['model']['name']  = $LANG['common'][22];
+      $criterias['model']['table'] = '';
+
+      $criterias['type']['field'] = 'type';
+      $criterias['type']['name']  = $LANG['common'][17];
+      $criterias['type']['table'] = '';
+
+      $criterias['comment']['field'] = 'comment';
+      $criterias['comment']['name']  = $LANG['common'][25];
+      $criterias['comment']['table'] = '';
+
+      return $criterias;
+   }
+
+   function getActions() {
+      global $LANG;
+      $actions = array();
+      $actions['name']['name']          = $LANG['common'][16];
+      $actions['name']['force_actions'] = array('assign','regex_result');
+
+      $actions['_ignore_ocs_import']['name'] = $LANG['rulesengine'][132];
+      $actions['_ignore_ocs_import']['type'] = 'yesonly';
+
+      $actions['manufacturers_id']['name']  = $LANG['common'][5];
+      $actions['manufacturers_id']['table'] = 'glpi_manufacturers';
+      $actions['manufacturers_id']['type']  = 'dropdown';
+
+      $actions['is_global']['name']  = $LANG['peripherals'][33];
+      $actions['is_global']['type']  = 'dropdown_management';
+
+      return $actions;
+   }
+}
+
+?>
---
+++ inc/ruledictionnaryprintercollection.class.php
@@ -0,0 +1,184 @@
+<?php
+
+
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2010 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Walid Nouh
+// Purpose of file:
+// ----------------------------------------------------------------------
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+class RuleDictionnaryPrinterCollection extends RuleCachedCollection {
+   // From RuleCollection
+
+   public $stop_on_first_match = true;
+   public $right = 'rule_dictionnary_printer';
+   public $menu_type = 'dictionnary';
+   public $menu_option = 'printer';
+
+   /**
+    * Constructor
+   **/
+   function __construct() {
+      //Init cache system values
+      $this->initCache("glpi_rulecacheprinters", array("name"         => "old_value",
+                                                       "manufacturer" => "manufacturer"),
+                       array("name"               => "new_value",
+                             "manufacturers_id"       => "new_manufacturer",
+                             "_ignore_ocs_import" => "ignore_ocs_import",
+                             "is_global"          => "is_global"));
+   }
+
+   function getTitle() {
+      global $LANG;
+
+      return $LANG['rulesengine'][39];
+   }
+
+   function cleanTestOutputCriterias($output) {
+
+      //If output array contains keys begining with _ : drop it
+      foreach ($output as $criteria => $value) {
+         if ($criteria[0] == '_' && $criteria != '_ignore_ocs_import') {
+            unset ($output[$criteria]);
+         }
+      }
+      return $output;
+   }
+
+   function replayRulesOnExistingDB($offset = 0, $maxtime = 0, $items = array (),
+                                    $params = array ()) {
+      global $DB;
+
+      if (isCommandLine()) {
+         echo "replayRulesOnExistingDB started : " . date("r") . "\n";
+      }
+      $nb = 0;
+      $i = $offset;
+
+      //Select all the differents software
+      $sql = "SELECT DISTINCT `glpi_printers`.`name`,
+                     `glpi_manufacturers`.`name` AS manufacturer,
+                     `glpi_printers`.`manufacturers_id` AS manufacturers_id
+              FROM `glpi_printers`
+              LEFT JOIN `glpi_manufacturers`
+                  ON (`glpi_manufacturers`.`id` = `glpi_printers`.`manufacturers_id`)";
+
+      // Do not replay on trash and templates
+      $sql .= "WHERE `glpi_printers`.`is_deleted` = '0'
+                     AND `glpi_printers`.`is_template` = '0' ";
+       if (isset ($params['manufacturer']) && $params['manufacturer'] > 0) {
+         $sql .= " AND `manufacturers_id` = '" . $params['manufacturer'] . "'";
+      }
+      if ($offset) {
+         $sql .= " LIMIT " . intval($offset) . ",999999999";
+      }
+
+      $res = $DB->query($sql);
+      $nb = $DB->numrows($res) + $offset;
+      $step = ($nb > 1000 ? 50 : ($nb > 20 ? floor($DB->numrows($res) / 20) : 1));
+
+      while ($input = $DB->fetch_array($res)) {
+         if (!($i % $step)) {
+            if (isCommandLine()) {
+               echo date("H:i:s") . " replayRulesOnExistingDB : $i/$nb (" .
+                    round(memory_get_usage() / (1024 * 1024), 2) . " Mo)\n";
+            } else {
+               changeProgressBarPosition($i, $nb, "$i / $nb");
+            }
+         }
+         //If manufacturer is set, then first run the manufacturer's dictionnary
+         if (isset ($input["manufacturer"])) {
+            $input["manufacturer"] = Manufacturer::processName($input["manufacturer"]);
+         }
+         //Replay software dictionnary rules
+         $input = addslashes_deep($input);
+         $res_rule = $this->processAllRules($input, array (), array ());
+         $res_rule = addslashes_deep($res_rule);
+         //If the software's name or version has changed
+         if (self::somethingHasChanged($res_rule,$input)) {
+
+            $IDs = array ();
+            //Find all the softwares in the database with the same name and manufacturer
+            $sql = "SELECT `id`
+                    FROM `glpi_printers`
+                    WHERE `name` = '" . $input["name"] . "'
+                          AND `manufacturers_id` = '" . $input["manufacturers_id"] . "'";
+            $res_soft = $DB->query($sql);
+            if ($DB->numrows($res_soft) > 0) {
+               //Store all the software's IDs in an array
+               while ($result = $DB->fetch_array($res_soft)) {
+                  $IDs[] = $result["id"];
+               }
+               //Replay dictionnary on all the softwares
+               $this->replayDictionnaryOnSoftwaresByID($IDs, $res_rule);
+            }
+         }
+         $i++;
+         if ($maxtime) {
+            $crt = explode(" ", microtime());
+            if ($crt[0] + $crt[1] > $maxtime) {
+               break;
+            }
+         }
+      }
+
+      if (isCommandLine()) {
+         echo "replayRulesOnExistingDB : $i/$nb               \n";
+      } else {
+         changeProgressBarPosition($i, $nb, "$i / $nb");
+      }
+
+      if (isCommandLine()) {
+         echo "replayRulesOnExistingDB ended : " . date("r") . "\n";
+      }
+
+      return ($i == $nb ? -1 : $i);
+   }
+
+   static function somethingHasChanged($res_rule,$input)  {
+      if ((isset ($res_rule["name"]) && $res_rule["name"] != $input["name"])
+         || (isset ($res_rule["manufacturer"])) && $res_rule["manufacturer"] != ''
+            || isset($res_rule['is_global']) && $res_rule['is_global'] != '') {
+            return true;
+         }
+         else {
+            return false;
+         }
+
+   }
+}
+
+
+
+?>
