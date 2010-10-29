fb6956673c791a03649ed9b6b93856091f70bf18
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-10-29 08:56:52 +0000

    Try to fix mailcollector playing rules for followups
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@12928 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/mailcollector.class.php
+++ inc/mailcollector.class.php
@@ -568,6 +568,9 @@ class MailCollector  extends CommonDBTM {
                $tkt['content'] .= $content[$ID]."\n";
             }
 
+            // Do not play rules for followups
+            $play_rules = false;
+
          } else {
             unset($tkt['tickets_id']);
          }
