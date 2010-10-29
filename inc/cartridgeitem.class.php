fc1f0b396a4a077a96e0c1a0f9102ab326d2dc16
moyooo <moyo@indepnet.net>
2010-10-29 12:39:49 +0000

    see #2381 : manage several links with a table using several condition

--- inc/cartridgeitem.class.php
+++ inc/cartridgeitem.class.php
@@ -355,6 +355,10 @@ class CartridgeItem extends CommonDBTM {
       $tab[40]['field']     = 'name';
       $tab[40]['name']      = $LANG['setup'][96];
       $tab[40]['forcegroupby'] = true;
+      $tab[40]['joinparams']    = array('beforejoin' => 
+                                      array('table'      => 'glpi_cartridgeitems_printermodels',
+                                            'joinparams' => array('ischild' => true)));
+
       return $tab;
    }
 
