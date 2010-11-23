c1a0064e5310a419cd731a9795be094b9f9d2f34
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-11-23 12:53:55 +0000

    revert
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@13132 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -3280,7 +3280,8 @@ class Ticket extends CommonDBTM {
       $candeleteassign    = false;
       if ($ID
           && (haveRight("assign_ticket","1")
-              || haveRight("steal_ticket","1")) {
+              || haveRight("steal_ticket","1")
+              || (haveRight("own_ticket","1") && $this->countUsers(self::ASSIGN)==0))) {
          $rand_assign_ticket = mt_rand();
          echo "&nbsp;&nbsp;<a class='tracking'
                onClick=\"Ext.get('ticketactor$rand_assign_ticket').setDisplayed('block')\">\n";
@@ -3498,7 +3499,8 @@ class Ticket extends CommonDBTM {
 
             echo '<hr>';
 
-         } else if (haveRight("steal_ticket","1")) {
+         } else if (haveRight("steal_ticket","1")
+                    || (haveRight("own_ticket","1") && $this->countUsers(self::ASSIGN)==0)) {
             echo "$usericon&nbsp;";
             User::dropdown(array('name'        => '_users_id_assign',
                                  'value'       => $options["_users_id_assign"],
