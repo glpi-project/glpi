370e1de00e7a82a32f4a9dda39dedba9d4ee645e
moyooo <moyo@indepnet.net>
2010-12-10 13:55:49 +0000

    [0.78] fix kb add message

--- inc/commondbtm.class.php
+++ inc/commondbtm.class.php
@@ -663,15 +663,13 @@ class CommonDBTM extends CommonGLPI {
 
       if ($addMessAfterRedirect) {
 
-         // Do not display quotes
-         if (isset($this->fields['name'])) {
-            $this->fields['name']=stripslashes($this->fields['name']);
-         } else {
+         if (($name=$this->getName()) == NOT_AVAILABLE) {
             $this->fields['name']=$this->getTypeName()." : ".$LANG['common'][2]." ".$this->fields['id'];
-         }
+         } 
+         $display=(isset($this->input['_no_message_link'])?$this->getNameID():$this->getLink());
 
-         addMessageAfterRedirect($LANG['common'][70] . "&nbsp;: " .
-                  (isset($this->input['_no_message_link'])?$this->getNameID():$this->getLink()));
+         // Do not display quotes
+         addMessageAfterRedirect($LANG['common'][70]."&nbsp;: ".stripslashes($display));
       }
    }
 
