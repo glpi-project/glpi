9851857a963a782856087f7d66d6a1ae4bbc2f7f
walid <walid@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2010-12-23 09:22:25 +0000

    see #2316 Rename is_global to is_recursive
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@13439 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_0781_080.php
+++ install/update_0781_080.php
@@ -1201,10 +1201,10 @@ function update0781to080($output='HTML') {
       $query = "CREATE TABLE `glpi_fieldunicities` (
                   `id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
                   `name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+                  `is_recursive` TINYINT( 1 ) NOT NULL DEFAULT '0',
                   `itemtype` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
                   `entities_id` INT( 11 ) NOT NULL DEFAULT  '-1',
                   `fields` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-                  `is_global` TINYINT( 1 ) NOT NULL DEFAULT '0',
                   `is_active` TINYINT( 1 ) NOT NULL DEFAULT '0',
                   `comment` text COLLATE utf8_unicode_ci
                 ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci
@@ -1212,7 +1212,7 @@ function update0781to080($output='HTML') {
       $DB->query($query)
           or die("0.80 add table glpi_fieldunicities ".$LANG['update'][90]. $DB->error());
 
-      $ADDTODISPLAYPREF['FieldUnicity'] = array(1, 80, 4, 3, 7, 30);
+      $ADDTODISPLAYPREF['FieldUnicity'] = array(1, 80, 4, 3, 86, 30);
    }
 
    if ($migration->addField('glpi_mailcollectors', 'passwd',
