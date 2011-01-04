23762ed353373c4663830615b89def8ed2ae79e6
yllen <yllen>
2011-01-04 15:33:19 +0000

    fix update

--- install/update_0781_080.php
+++ install/update_0781_080.php
@@ -1200,14 +1200,14 @@ function update0781to080($output='HTML') {
    if (!TableExists('glpi_fieldunicities')) {
       $query = "CREATE TABLE `glpi_fieldunicities` (
                   `id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-                  `name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+                  `name` VARCHAR( 255 ) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
                   `is_recursive` TINYINT( 1 ) NOT NULL DEFAULT '0',
-                  `itemtype` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+                  `itemtype` VARCHAR( 255 ) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
                   `entities_id` INT( 11 ) NOT NULL DEFAULT  '-1',
-                  `fields` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+                  `fields` VARCHAR( 255 ) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
                   `is_active` TINYINT( 1 ) NOT NULL DEFAULT '0',
                   `comment` text COLLATE utf8_unicode_ci
-                ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci
+                ) ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
                   COMMENT = 'Stores field unicity criterias'";
       $DB->query($query)
           or die("0.80 add table glpi_fieldunicities ".$LANG['update'][90]. $DB->error());
