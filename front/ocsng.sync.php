f4005c22feb486395f28a547220b03fe0e6b1b40
walid <walid@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-01-07 07:07:47 +0000

    Improve OCSNG link navigation (menu)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@13530 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/ocsng.sync.php
+++ front/ocsng.sync.php
@@ -38,7 +38,7 @@ include (GLPI_ROOT . "/inc/includes.php");
 
 checkRight("ocsng","w");
 
-commonHeader($LANG['ocsng'][0], $_SERVER['PHP_SELF'], "utils", "ocsng");
+commonHeader($LANG['ocsng'][0], $_SERVER['PHP_SELF'], "utils", "ocsng","sync");
 
 $display_list = true;
 
