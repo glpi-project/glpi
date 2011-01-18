af2bac96d31217719a60fb83d2512c09d5562cb1
moyooo <moyo@indepnet.net>
2011-01-18 09:11:34 +0000

    minor fix

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -141,8 +141,7 @@ class Ticket extends CommonDBTM {
    function canSolve() {
       return (/*$this->fields["status"] != 'closed' /// TODO block solution edition on closed status ?
               &&*/ ($this->can($this->getField('id'), 'w')
-               && (isset($_SESSION['glpiactiveprofile']['helpdesk_status'])
-                   || is_null($_SESSION['glpiactiveprofile']['helpdesk_status'])) // Not set for post-only
+               && array_key_exists('helpdesk_status',$_SESSION['glpiactiveprofile']) // Not set for post-only
                && (!isset($_SESSION['glpiactiveprofile']['helpdesk_status'][$this->fields['status']]['solved'])
                    || $_SESSION['glpiactiveprofile']['helpdesk_status'][$this->fields['status']]['solved'])));
    }
