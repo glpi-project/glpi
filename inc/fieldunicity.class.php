ed2a9cf2cdb539240c4ee4c5c76321945ef54bbd
Walid Nouh <wnouh@teclib.com>
2011-02-09 08:08:01 +0000

    display itemtypename in field unicity list

--- inc/fieldunicity.class.php
+++ inc/fieldunicity.class.php
@@ -305,7 +305,7 @@ class FieldUnicity extends CommonDropdown {
       $tab[4]['field']         = 'itemtype';
       $tab[4]['name']          = $LANG['common'][17];
       $tab[4]['massiveaction'] = false;
-      $tab[4]['datatype']      = 'itemtype';
+      $tab[4]['datatype']      = 'itemtypename';
       $tab[4]['forcegroupby']  = true;
 
       $tab[5]['table']         = $this->getTable();
