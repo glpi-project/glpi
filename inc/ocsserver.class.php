8e625a59e1e126efa27ee0e1d6f8a7519a03a886
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-02-10 14:00:27 +0000

    fix ocs version check
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@13854 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ocsserver.class.php
+++ inc/ocsserver.class.php
@@ -1163,7 +1163,7 @@ class OcsServer extends CommonDBTM {
 
          if ($DBocs->numrows($result) != 1
              || ($DBocs->result($result, 0, 0) < 4020
-                 && !strstr($DBocs->result($result, 0, 0),'2.0') !==0)) { // hack for 2.0 RC
+                 && !(strpos($DBocs->result($result, 0, 0),'2.0') === 0))) { // hack for 2.0 RC
             return false;
          }
       }
