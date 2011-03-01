e6cf10fdd78aa2a5156b7d561ba63bfb9b4728a0
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-03-01 14:10:50 +0000

    use new method (Ticket::isAllowedStatus)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@13968 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -142,8 +142,7 @@ class Ticket extends CommonDBTM {
       return (/*$this->fields["status"] != 'closed' /// TODO block solution edition on closed status ?
               &&*/ ($this->can($this->getField('id'), 'w')
                && array_key_exists('helpdesk_status',$_SESSION['glpiactiveprofile']) // Not set for post-only
-               && (!isset($_SESSION['glpiactiveprofile']['helpdesk_status'][$this->fields['status']]['solved'])
-                   || $_SESSION['glpiactiveprofile']['helpdesk_status'][$this->fields['status']]['solved'])));
+               && self::isAllowedStatus($this->fields['status'],'solved']));
    }
 
 
