49478088caed38c441248f7c0b864bb3d7d7134b
moyooo <moyo@indepnet.net>
2011-03-08 09:02:47 +0000

    clean tag

--- 0.78-bugfixes/scripts/glpi_cron_curl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# @version $Id:
-# Little script for stimulate the pseudo cron of glpi 
-# YOU MUST EDIT THE URL 
-# Vous devez editez l'URL pour l'adapter à votre installation
-
-curl --silent --compressed http://YOURSERVER/glpi/front/cron.php
--- 0.78-bugfixes/scripts/glpi_cron_lynx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# # @version $Id:
-# Little script for stimulate the pseudo cron of glpi
-# YOU MUST EDIT THE URL 
-# Vous devez editez l'URL selon votre installation
-
-/usr/bin/lynx -source http://YOURSERVER/glpi/front/cron.php > /dev/null 2>&1
