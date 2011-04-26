3e8175094903b6f891ad3e50920079e56795f02a
tsmr <tsmr@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-04-26 12:39:51 +0000

    Profile display correction
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@14303 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/profile.class.php
+++ inc/profile.class.php
@@ -742,8 +742,9 @@ class Profile extends CommonDBTM {
       echo "</td>";
       echo "<td>".$LANG['profiles'][35]."&nbsp;:</td><td>";
       Dropdown::showYesNo("update_followups", $this->fields["update_followups"]);
-      echo "</td></tr>\n";
-
+      echo "</td>\n";
+      echo "<td colspan='2'></td></tr>\n";
+      
       echo "<tr class='tab_bg_5'><th colspan='6'>".$LANG['ocsconfig'][50]."</th><";
       echo "/tr>\n";
       echo "<td>".$LANG['profiles'][14]."&nbsp;:</td><td>";
