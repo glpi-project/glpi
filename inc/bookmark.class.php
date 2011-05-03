0863025754ec1030df9b2e32fff2150fa33cdb6d
moyooo <moyo@indepnet.net>
2011-05-03 07:00:43 +0000

    Fix bookmark save and load see #2776

--- inc/bookmark.class.php
+++ inc/bookmark.class.php
@@ -286,7 +286,7 @@ class Bookmark extends CommonDBTM {
             }
 
             // Meta search
-            if (isset($query_tab_save['field2']) && count($query_tab_save['field2'])) {
+            if (isset($query_tab_save['itemtype2']) && count($query_tab_save['itemtype2'])) {
                $meta_ok = Search::getMetaItemtypeAvailable($query_tab['itemtype']);
 
                unset($query_tab['field2']);
