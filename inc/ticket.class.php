2a6fdf3f817c76a42e5b7316373893a9e5710914
yllen <yllen>
2011-05-06 10:42:25 +0000

    [0.80] minor fix

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -528,7 +528,6 @@ class Ticket extends CommonDBTM {
       }
 
       // check mandatory fields
-      $mandatory_ok = true;
       if ($CFG_GLPI["is_ticket_title_mandatory"] && isset($input['name']) ) {
          $title = trim($input['name']);
          if (empty($title)) {
