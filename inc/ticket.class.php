6d33cc40a081386c9d960edebd36acaabc7ba5e3
moyooo <moyo@indepnet.net>
2011-05-13 07:54:24 +0000

    [0.80] fix SLA action

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -628,6 +628,26 @@ class Ticket extends CommonDBTM {
          $input = $ret;
       }
 
+      // Manage fields from auto update : map rule actions to standard ones
+      if (isset($input['_auto_update'])) {
+         if (isset($input['_users_id_assign'])) {
+            $input['_ticket_assign']['_type']    = 'user';
+            $input['_ticket_assign']['users_id'] = $input['_users_id_assign'];
+         }
+         if (isset($input['_users_id_requester'])) {
+            $input['_ticket_requester']['_type']    = 'user';
+            $input['_ticket_requester']['users_id'] = $input['_users_id_requester'];
+         }
+         if (isset($input['_groups_id_requester'])) {
+            $input['_ticket_assign']['_type']    = 'group';
+            $input['_ticket_assign']['users_id'] = $input['_groups_id_requester'];
+         }
+         if (isset($input['_groups_id_assign'])) {
+            $input['_ticket_requester']['_type']    = 'group';
+            $input['_ticket_requester']['users_id'] = $input['_groups_id_assign'];
+         }
+      }
+
       if (isset($input['_link'])) {
          $ticket_ticket = new Ticket_Ticket();
          if ($ticket_ticket->can(-1, 'w', $input['_link'])) {
@@ -724,7 +744,6 @@ class Ticket extends CommonDBTM {
       if (!isset($input['_auto_update']) && $lastupdater=getLoginUserID(true)) {
          $input['users_id_lastupdater'] = $lastupdater;
       }
-
 
       if (isset($input["items_id"])
           && $input["items_id"]>=0
