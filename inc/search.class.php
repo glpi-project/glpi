a503861c7e196e549bc264df2eac0a167d5b336a
moyooo <moyo@indepnet.net>
2011-05-26 10:10:45 +0000

    [0.80] statut search must exclude templates and deleted see #2808

--- inc/search.class.php
+++ inc/search.class.php
@@ -602,6 +602,15 @@ class Search {
                      $query_num = str_replace($CFG_GLPI["union_search_type"][$itemtype],
                                               $ctable, $tmpquery);
                      $query_num .= " AND $ctable.`states_id` > '0' ";
+                     // Add deleted if item have it
+                     if ($citem && $citem->maybeDeleted()) {
+                        $query_num .= " AND `$ctable`.`is_deleted` = '0' ";
+                     }
+
+                     // Remove template items
+                     if ($citem && $citem->maybeTemplate()) {
+                        $query_num .= " AND `$ctable`.`is_template` = '0' ";
+                     }
 
                   } else {// Ref table case
                      $reftable = getTableForItemType($itemtype);
@@ -671,6 +680,16 @@ class Search {
                   $tmpquery = str_replace($CFG_GLPI["union_search_type"][$itemtype],
                                           $ctable, $tmpquery);
                   $tmpquery .= " AND `$ctable`.`states_id` > '0' ";
+                  // Add deleted if item have it
+                  if ($citem && $citem->maybeDeleted()) {
+                     $tmpquery .= " AND `$ctable`.`is_deleted` = '0' ";
+                  }
+
+                  // Remove template items
+                  if ($citem && $citem->maybeTemplate()) {
+                     $tmpquery .= " AND `$ctable`.`is_template` = '0' ";
+                  }
+
                } else {// Ref table case
                   $reftable = getTableForItemType($itemtype);
 
