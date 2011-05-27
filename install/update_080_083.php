c9964c0ffac2e8dc710882f9fadbf890452cefd1
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-05-27 13:55:20 +0000

    revert r14385 (field already exists since 0.80)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@14589 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_080_083.php
+++ install/update_080_083.php
@@ -528,7 +528,6 @@ function update080to083($output='HTML') {
 
    $migration->addKey("glpi_reservations", array('reservationitems_id', 'group'), "resagroup");
 
-   $migration->addField("glpi_users", "token", "varchar( 255 ) NULL DEFAULT ''");
 
    if ($migration->addField("glpi_documents_items", "entities_id", "INT( 11 ) NOT NULL DEFAULT 0")) {
       $migration->addField("glpi_documents_items", "is_recursive", "TINYINT( 1 ) NOT NULL DEFAULT 0");
