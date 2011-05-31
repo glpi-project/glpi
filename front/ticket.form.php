70e8f5b8b18c02e5111a802ffc8d84f0deb2dc9b
moyooo <moyo@indepnet.net>
2011-05-31 15:41:42 +0000

    [0.80] fix ticket creation for normal profile

--- front/ticket.form.php
+++ front/ticket.form.php
@@ -164,10 +164,11 @@ if (isset($_GET["id"]) && $_GET["id"]>0) {
    commonHeader($LANG['job'][13],'',"maintain","ticket");
 
    $users_id_requester = getLoginUserID();
-   // No default requester if own ticket right = tech
-   if (haveRight('own_ticket',1)) {
+   // No default requester if own ticket right = tech and update_ticket right to update requester
+   if (haveRight('own_ticket',1) && haveRight('update_ticket',1)) {
       $users_id_requester = 0;
    }
+
    // Set default value...
    $values = array('_users_id_requester'       => $users_id_requester,
                    '_users_id_requester_notif' => array('use_notification' => 1),
