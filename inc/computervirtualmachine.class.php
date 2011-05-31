3513514a9c75d3324dd4abdc8c79867676271a53
remi <remi>
2011-05-31 10:54:35 +0000

    [0.80] clean

--- inc/computervirtualmachine.class.php
+++ inc/computervirtualmachine.class.php
@@ -316,11 +316,9 @@ class ComputerVirtualMachine extends CommonDBChild {
                                $LANG['help'][25]." = ". (empty($comp->fields['name'])
                                                          ? "($ID)" : $comp->fields['name']));
 
-         $vm = new self();
          foreach ($virtualmachines as $virtualmachine) {
             $href = "<a href='computervirtualmachine.form.php?id=".$virtualmachine['id']."'>";
             echo "<tr class='tab_bg_2'>";
-            $vm->fields = $virtualmachines;
             echo "<td>$href".$virtualmachine['name']."</a></td>";
             echo "<td>";
             echo Dropdown::getDropdownName('glpi_virtualmachinetypes',
@@ -382,12 +380,12 @@ class ComputerVirtualMachine extends CommonDBChild {
    **/
    static function getUUIDRestrictRequest($uuid) {
 
-      //More infos about uuid, please see wikipedia : 
+      //More infos about uuid, please see wikipedia :
       //http://en.wikipedia.org/wiki/Universally_unique_identifier
       //Some uuid are not conform, so preprocessing is necessary
       //A good uuid likes lik : 550e8400-e29b-41d4-a716-446655440000
-      
-      //Case one : for example some uuid are like that : 
+
+      //Case one : for example some uuid are like that :
       //56 4d 77 d0 6b ef 3d da-4d 67 5c 80 a9 52 e2 c9
       $pattern  = "/([\w]{2})\ ([\w]{2})\ ([\w]{2})\ ([\w]{2})\ ";
       $pattern .= "([\w]{2})\ ([\w]{2})\ ([\w]{2})\ ([\w]{2})-";
@@ -396,8 +394,8 @@ class ComputerVirtualMachine extends CommonDBChild {
       if (preg_match($pattern, $uuid)) {
          $uuid = preg_replace($pattern, "$1$2$3$4-$5$6-$7$8-$9$10-$11$12$13$14$15$16", $uuid);
       }
-      
-      //Case two : why this code ? Because some dmidecode < 2.10 is buggy. 
+
+      //Case two : why this code ? Because some dmidecode < 2.10 is buggy.
       //On unix is flips first block of uuid and on windows flips 3 first blocks...
       $in      = " IN ('".strtolower($uuid)."'";
       $regexes = array("/([\w]{2})([\w]{2})([\w]{2})([\w]{2})(.*)/" => "$4$3$2$1$5",
