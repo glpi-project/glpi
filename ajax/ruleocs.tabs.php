661c80297528cb2cf6f24a905e023d2f483e771d
tsmr <tsmr@thetsmr.fr>
2011-06-21 10:35:21 +0000

    Corrections for OCSMode

--- ajax/ruleocs.tabs.php
+++ ajax/ruleocs.tabs.php
@@ -39,7 +39,7 @@ include (GLPI_ROOT . "/inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
 header_nocache();
 
-checkRight('rule_ticket', 'r');
+checkRight('rule_ocs', 'r');
 
 $rule = new RuleOcs();
 include ("rule.common.tabs.php");
