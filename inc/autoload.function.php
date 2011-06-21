6b64a7f9e775c5281b6b75722d2ae88448aeaf81
remi <remi>
2011-06-21 15:23:50 +0000

    avoid warning

--- inc/autoload.function.php
+++ inc/autoload.function.php
@@ -110,7 +110,7 @@ function __autoload($classname) {
 
    if (file_exists("$dir$item.class.php")) {
       include_once ("$dir$item.class.php");
-      if ($_SESSION['glpi_use_mode']==DEBUG_MODE) {
+      if (isset($_SESSION['glpi_use_mode']) && $_SESSION['glpi_use_mode']==DEBUG_MODE) {
          $DEBUG_AUTOLOAD[] = $classname;
       }
 
