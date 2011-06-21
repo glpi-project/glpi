2d494db75d238d547b88ce8de5bba1a156ef8c4e
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-06-21 07:30:09 +0000

    fix upgrade
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@14716 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/install.php
+++ install/install.php
@@ -99,10 +99,6 @@ function choose_language() {
 function loadLang($LANGuage) {
    global $LANG;
 
-   if (isset($LANG)) {
-      unset($LANG);
-   }
-
    $file = GLPI_ROOT ."/locales/".$LANGuage.".php";
 
    if (file_exists($file)) {
