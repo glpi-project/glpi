034a4e53cff4d1f682aad63078cf626cf526ba97
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-07-01 09:17:49 +0000

    bug in computePreferences - Moyo to check please
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@14830 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- config/define.php
+++ config/define.php
@@ -314,7 +314,7 @@ $CFG_GLPI['user_pref_field'] = array('csv_delimiter', 'date_format', 'default_re
                                      'is_categorized_soft_expanded', 'is_ids_visible',
                                      'is_not_categorized_soft_expanded', 'language', 'list_limit',
                                      'names_format', 'number_format', 'personal_token',
-                                     'personal_token_date', 'priority_1', 'priority_2',
+                                     'priority_1', 'priority_2',
                                      'priority_3', 'priority_4', 'priority_5', 'priority_6',
                                      'refresh_ticket_list', 'show_count_on_tabs',
                                      'show_jobs_at_login', 'task_private', 'use_flat_dropdowntree');
