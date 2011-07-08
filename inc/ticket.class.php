a48bcc9fd7760b043bfac5d582f675c7e8183342
moyooo <moyo@indepnet.net>
2011-07-08 08:20:25 +0000

    clean satisfaction on ticket purge

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -494,6 +494,12 @@ class Ticket extends CommonDBTM {
       $DB->query($query1);
 
       $query1 = "DELETE
+                 FROM `glpi_ticketsatisfactions`
+                 WHERE `tickets_id` = '".$this->fields['id']."'";
+      $DB->query($query1);
+
+
+      $query1 = "DELETE
                  FROM `glpi_tickets_tickets`
                  WHERE `tickets_id_1` = '".$this->fields['id']."'
                      OR `tickets_id_2` = '".$this->fields['id']."'";
