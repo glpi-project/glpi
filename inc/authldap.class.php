dac1dbe1e851b05c662e0396e576d2a6cc2c7a61
yllen <yllen>
2011-08-05 11:15:47 +0000

    fix previous (first step for delete common.function)

--- inc/authldap.class.php
+++ inc/authldap.class.php
@@ -2558,7 +2558,7 @@ class AuthLDAP extends CommonDBTM {
             $values[$i*7] = $i.' '.$LANG['ldap'][56];
          }
          for ($i=3 ; $i < 13 ; $i++) {
-            $values[$i*28] = $i.' '.ucfirst($LANG['financial'][57]);
+            $values[$i*28] = $i.' '.Toolbox::ucfirst($LANG['financial'][57]);
          }
 
          Dropdown::showFromArray('days', $values, array('value' => $default));
---
+++ inc/toolbox.class.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * @version $Id:
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ --------------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+// class Toolbox
+class Toolbox {
+
+
+   /**
+    * Convert first caracter in upper
+    *
+    * @since version 0.83
+    *
+    * @param $str string to change
+    *
+    * @return string changed
+    */
+   static function ucfirst($str) {
+
+      // for foreign language
+      $str[0] = mb_strtoupper($str[0]);
+      return $str;
+    }
+
+}
+?>
