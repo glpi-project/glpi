01d1c35862b17d1dbacf62dee177b58beae7ff8f
yllen <yllen>
2011-08-08 10:51:43 +0000

    move common.function in Ajax.class

---
+++ inc/ajax.class.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * @version $Id:
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ --------------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+// class Ajax
+class Ajax {
+
+
+   /**
+    * Call from a popup Windows, refresh the dropdown in main window
+   **/
+   static function refreshDropdownPopupInMainWindow() {
+
+      if (isset($_SESSION["glpipopup"]["rand"])) {
+         echo "<script type='text/javascript' >\n";
+         echo "window.opener.update_results_".$_SESSION["glpipopup"]["rand"]."();";
+         echo "</script>";
+      }
+   }
+
+
+   /**
+    * Call from a popup Windows, refresh the dropdown in main window
+   **/
+   static function refreshPopupMainWindow() {
+
+      if (isset($_SESSION["glpipopup"]["rand"])) {
+         echo "<script type='text/javascript' >\n";
+         echo "window.opener.location.reload(true)";
+         echo "</script>";
+      }
+   }
+}
+?>
