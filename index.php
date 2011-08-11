f994b5a9a4997b807bc90fd55b82eb0d949e2efc
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-08-11 07:44:03 +0000

    fix fresh install
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@15370 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ define('GLPI_ROOT', '.');
 include (GLPI_ROOT . "/config/based_config.php");
 
 if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
+   include_once (GLPI_ROOT . "/inc/autoload.function.php");
    Html::redirect("install/install.php");
    die();
 
