939337e9aec8b859d43e7742197f81fc1df755b7
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-08-12 07:15:37 +0000

    bye bye auth.function
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@15384 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- tools/cliupdate.php
+++ tools/cliupdate.php
@@ -45,7 +45,6 @@ if (!defined('GLPI_ROOT')) {
 
 include_once (GLPI_ROOT . "/config/define.php");
 include_once (GLPI_ROOT . "/inc/autoload.function.php");
-include_once (GLPI_ROOT . "/inc/auth.function.php");
 include_once (GLPI_ROOT . "/inc/db.function.php");
 include_once (GLPI_ROOT . "/inc/display.function.php");
 include_once (GLPI_ROOT . "/config/based_config.php");
