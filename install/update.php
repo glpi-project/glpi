9396c6447223c1cdf0094d9a0fa59f4781e78066
yllen <yllen>
2011-08-18 14:51:11 +0000

    bye bye setup.function

--- install/update.php
+++ install/update.php
@@ -42,7 +42,6 @@ include_once (GLPI_ROOT . "/inc/autoload.function.php");
 include_once (GLPI_ROOT . "/inc/db.function.php");
 include_once (GLPI_ROOT . "/config/based_config.php");
 include_once (GLPI_CONFIG_DIR . "/config_db.php");
-include_once (GLPI_ROOT . "/inc/setup.function.php");
 Config::detectRootDoc();
 
 // Old itemtype for compatibility
