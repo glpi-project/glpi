04c83a6c99db9372565ece7ff15ec4bfdc0dc732
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-08-23 10:05:46 +0000

    more clean for last commit
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@15466 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/user.class.php
+++ inc/user.class.php
@@ -653,12 +653,6 @@ class User extends CommonDBTM {
             }
          }
       }
-      if (isset($input['_add_email']) && !empty($input['_add_email'])) {
-         $email = new UserEmail();
-         $email->add(array('users_id' => $input['id'],
-                           'email'    => $input['_add_email']));
-         Event::log($input['id'], "users", 4, "setup", $_SESSION["glpiname"]." ".$LANG['log'][37]);
-      }
       return $input;
    }
 
