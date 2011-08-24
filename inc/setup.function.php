3437f575bb067470ffdf8a86289afdfd5cc497cb
Walid Nouh <wnouh@teclib.com>
2011-08-24 13:14:25 +0000

    fixed #3075 Add HTTP_REMOTE_USER for SSO

--- inc/setup.function.php
+++ inc/setup.function.php
@@ -171,6 +171,10 @@ function showOtherAuthList($target) {
    echo "<option value='REDIRECT_REMOTE_USER' " .
           ($CFG_GLPI["existing_auth_server_field"]=="REDIRECT_REMOTE_USER" ? " selected " : "") .">".
           "REDIRECT_REMOTE_USER</option>\n";
+   //For apache mod_proxy
+   echo "<option value='HTTP_REMOTE_USER' " .
+          ($CFG_GLPI["existing_auth_server_field"]=="HTTP_REMOTE_USER" ? " selected " : "") .">".
+          "HTTP_REMOTE_USER</option>\n";
    echo "</select>";
    echo "</td></tr>\n";
 
