1b4203b13337c45fb9e60cd4019939069be7f47e
remi <remi>
2011-08-26 09:02:21 +0000

    [0.80] fix default entity for new license

--- inc/softwarelicense.class.php
+++ inc/softwarelicense.class.php
@@ -153,6 +153,11 @@ class SoftwareLicense extends CommonDBTM {
          $this->check(-1,'w');
          $this->fields['softwares_id'] = $softwares_id;
          $this->fields['number']       = 1;
+         $soft = new Software();
+         if ($soft->getFromDB($softwares_id)
+             && in_array($_SESSION['glpiactive_entity'], getAncestorsOf('glpi_entities', $soft->getEntityID()))) {
+            $this->fields['entities_id'] = $soft->getEntityID();
+         }
       }
 
       $this->showTabs($options);
