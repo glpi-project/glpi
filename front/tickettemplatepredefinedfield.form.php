2c6cd991f672831ddf7740c58bc01894fecda7b8
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-09-06 13:03:07 +0000

    missing file
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@15620 35612d4c-9d10-0410-837c-bf9dd1be60d7

---
+++ front/tickettemplatepredefinedfield.form.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ --------------------------------------------------------------------------
+ */
+
+define('GLPI_ROOT', '..');
+include (GLPI_ROOT . "/inc/includes.php");
+
+Session ::checkRight('tickettemplate','w');
+
+$item = new TicketTemplatePredefinedField();
+
+// Use masiveaction system to manage add value
+if (isset($_POST["massiveaction"])) {
+   $item->check(-1, 'w', $_POST);
+
+   if ($item->add($_POST)) {
+      Event::log($_POST["tickettemplates_id"], "tickettemplate", 4, "maintain",
+                 $_SESSION["glpiname"]." ".$LANG['log'][58]);
+   }
+   Html::back();
+
+} else if (isset($_POST["delete"])) {
+
+   if (isset($_POST["item"]) && count($_POST["item"])) {
+      foreach ($_POST["item"] as $key => $val) {
+         if ($val == 1) {
+            if ($item->can($key, 'w')) {
+               $item->delete(array('id' => $key));
+            }
+         }
+      }
+      Event::log($_POST["tickettemplates_id"], "tickettemplate", 4, "maintain",
+                 $_SESSION["glpiname"]." ".$LANG['log'][63]);
+   }
+   Html::back();
+
+}
+
+Html::displayErrorAndDie("lost");
+?>
