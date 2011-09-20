832239854c6968a52b4ff21668bc6f1dd7ee41d0
yllen <yllen>
2011-09-20 08:22:05 +0000

    fix create ticket for post-only

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -1155,15 +1155,18 @@ class Ticket extends CommonITILObject {
                }
             }
             // Auto assign tech/group from Category
-            if ($input['itilcategories_id']>0
-                && (!$input['_users_id_assign'] || !$input['_groups_id_assign'])) {
+            if ($input['itilcategories_id'] > 0
+                && ((!isset($input['_users_id_assign']) || !$input['_users_id_assign'])
+                    || (!isset($input['_groups_id_assign']) || !$input['_groups_id_assign']))) {
 
                $cat = new ITILCategory();
                $cat->getFromDB($input['itilcategories_id']);
-               if (!$input['_users_id_assign'] && $cat->isField('users_id')) {
+               if ((!isset($input['_users_id_assign']) || !$input['_users_id_assign'])
+                   && $cat->isField('users_id')) {
                   $input['_users_id_assign'] = $cat->getField('users_id');
                }
-               if (!$input['_groups_id_assign'] && $cat->isField('groups_id')) {
+               if ((!isset($input['_groups_id_assign']) || !$input['_groups_id_assign'])
+                   && $cat->isField('groups_id')) {
                   $input['_groups_id_assign'] = $cat->getField('groups_id');
                }
             }
