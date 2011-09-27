73c22c16168a44301da6d7bb92d0e761b1e2b522
moyooo <moyo@indepnet.net>
2011-09-27 10:19:26 +0000

    create links for reminder visibility see #3043

---
+++ inc/group_reminder.class.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Remi Collet
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+/// Class Group_Reminder
+class Group_Reminder extends CommonDBRelation {
+
+   // From CommonDBRelation
+   public $itemtype_1 = 'Reminder';
+   public $items_id_1 = 'reminders_id';
+   public $itemtype_2 = 'Group';
+   public $items_id_2 = 'groups_id';
+
+
+   /**
+    * Get groups for a reminder
+    *
+    * @param $reminders_id ID of the reminder 
+    *
+    * @return array of groups linked to a reminder
+   **/
+   static function getGroups($reminders_id) {
+      global $DB;
+
+      $groups = array();
+      $query = "SELECT `glpi_groups_reminders`.*
+                FROM `glpi_groups_reminders`
+                WHERE `reminders_id` = '$reminders_id'";
+
+      foreach ($DB->request($query) as $data) {
+         $groups[$data['groups_id']][] = $data;
+      }
+      return $groups;
+   }
+
+}
+
+?>
---
+++ inc/reminder_user.class.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2011 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Remi Collet
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+/// Class Reminder_User
+class Reminder_User extends CommonDBRelation {
+
+   // From CommonDBRelation
+   public $itemtype_1 = 'Reminder';
+   public $items_id_1 = 'reminders_id';
+   public $itemtype_2 = 'User';
+   public $items_id_2 = 'users_id';
+
+
+   /**
+    * Get users for a reminder
+    *
+    * @param $reminders_id ID of the reminder 
+    *
+    * @return array of users linked to a reminder
+   **/
+   static function getUsers($reminders_id) {
+      global $DB;
+
+      $users = array();
+      $query = "SELECT `glpi_reminders_users`.*
+                FROM `glpi_reminders_users`
+                WHERE `reminders_id` = '$reminders_id'";
+
+      foreach ($DB->request($query) as $data) {
+         $users[$data['users_id']][] = $data;
+      }
+      return $users;
+   }
+
+}
+
+?>
