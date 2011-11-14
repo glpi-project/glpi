b1a131da6b7e2ae23a2cb737113133ecd83110cf
Walid Nouh <wnouh@teclib.com>
2011-11-14 15:41:09 +0000

    [0.80] fix undefined $msg when no ocsservers defined

--- inc/ocsserver.class.php
+++ inc/ocsserver.class.php
@@ -830,16 +830,16 @@ class OcsServer extends CommonDBTM {
          echo "\n<tr class='tab_bg_2'><th>" . $LANG['ocsng'][0] . "</th></tr>\n";
          echo "<tr class='tab_bg_1'><td><pre>\n&nbsp;\n";
 
-         $msg = '';
          foreach ($ocsServers as $ocsServer) {
                $msg .= $LANG['ocsconfig'][2]." : '".$ocsServer['ocs_db_host']."'";
                $msg .= ', '.(self::checkOCSconnection($ocsServer['id'])?$LANG['ocsng'][18]
                                                                        :$LANG['ocsng'][18]);
                $msg .= ', '.$LANG['ocsconfig'][38]. " : ".$ocsServer['use_soft_dict'];
          }
+
+         echo wordwrap($msg."\n", $width, "\n\t\t");
+         echo "\n</pre></td></tr>";
       }
-      echo wordwrap($msg."\n", $width, "\n\t\t");
-      echo "\n</pre></td></tr>";
    }
 
 
