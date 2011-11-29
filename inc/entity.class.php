b494f014a3b1b2a6dea802d544fe0047cbd33399
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-11-29 13:52:00 +0000

    more search options for entity
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@16208 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/entity.class.php
+++ inc/entity.class.php
@@ -383,6 +383,24 @@ class Entity extends CommonTreeDropdown {
       $tab[28]['joinparams']    = array('jointype' => 'child');
       $tab[28]['datatype']      = 'text';
 
+      $tab[29]['table']         = 'glpi_entitydatas';
+      $tab[29]['field']         = 'use_licenses_alert';
+      $tab[29]['name']          = $LANG['setup'][264];
+      $tab[29]['massiveaction'] = false;
+      $tab[29]['joinparams']    = array('jointype' => 'child');
+
+      $tab[30]['table']         = 'glpi_entitydatas';
+      $tab[30]['field']         = 'use_contracts_alert';
+      $tab[30]['name']          = $LANG['setup'][246];
+      $tab[30]['massiveaction'] = false;
+      $tab[30]['joinparams']    = array('jointype' => 'child');
+
+      $tab[31]['table']         = 'glpi_entitydatas';
+      $tab[31]['field']         = 'use_infocoms_alert';
+      $tab[31]['name']          = $LANG['setup'][247];
+      $tab[31]['massiveaction'] = false;
+      $tab[31]['joinparams']    = array('jointype' => 'child');
+
       return $tab;
    }
 
