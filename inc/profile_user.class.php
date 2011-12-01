4667e735e68373750b00dab54321b845090d5b68
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-12-01 09:08:36 +0000

    revert r16251 and add a global solution for default entity value
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@16253 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/profile_user.class.php
+++ inc/profile_user.class.php
@@ -113,8 +113,7 @@ class Profile_User extends CommonDBTM {
 
          echo "<tr class='tab_bg_2'><td class='center'>";
          echo "<input type='hidden' name='users_id' value='$ID'>";
-         Dropdown::show('Entity', array('entity' => $_SESSION['glpiactiveentities'],
-                                        'value'  => $_SESSION['glpiactive_entity']));
+         Dropdown::show('Entity', array('entity' => $_SESSION['glpiactiveentities']));
          echo "</td><td class='center'>".$LANG['profiles'][22]."&nbsp;: ";
          Profile::dropdownUnder(array('value' => Profile::getDefault()));
          echo "</td><td class='center'>".$LANG['profiles'][28]."&nbsp;: ";
