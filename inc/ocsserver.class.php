0eee1ae08a4dc880ff59c2bb955574c48ea81b41
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-12-17 17:15:34 +0000

    Apply r16436
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@16499 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ocsserver.class.php
+++ inc/ocsserver.class.php
@@ -4438,7 +4438,11 @@ class OcsServer extends CommonDBTM {
                      $disk['device']     = $line['VOLUMN'];
                   }
 
-                  $disk['name']           = $disk['mountpoint'];
+                  if ($line['FILESYSTEM'] == "vmfs") {
+                     $disk['name'] = basename($line['TYPE']);
+                  } else {
+                     $disk['name']  = $disk['mountpoint'];
+                  }
                   $disk['filesystems_id'] = Dropdown::importExternal('Filesystem',
                                                                      $line["FILESYSTEM"]);
 
