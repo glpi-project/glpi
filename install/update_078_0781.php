902271ecc96a10ef59ce67807eb86d1cc786255f
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2011-12-22 18:30:25 +0000

    missing file
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@16577 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_078_0781.php
+++ install/update_078_0781.php
@@ -82,9 +82,7 @@ function update078to0781($output='HTML') {
                            WHERE `itemtype`='".$data['itemtype']."'
                               AND `items_id` IN (SELECT `id` FROM `$itemtable`
                               WHERE `entities_id`=$entID AND `is_recursive`=0)";
-                  $DB->query($query3) or die(sprintf(__('%1$s - Error during the database update: %2$s'),
-                        "0.78.1 update entities_id and is_recursive=0
-                        in glpi_reservationitems for ".$data['itemtype'], $DB->error()));
+                  $DB->queryOrDie($query3, "0.78.1 update entities_id and is_recursive=0 in glpi_reservationitems for ".$data['itemtype']);
 
                   // Recursive ones
                   $query3="UPDATE `glpi_reservationitems`
@@ -92,18 +90,14 @@ function update078to0781($output='HTML') {
                            WHERE `itemtype`='".$data['itemtype']."'
                               AND `items_id` IN (SELECT `id` FROM `$itemtable`
                               WHERE `entities_id`=$entID AND `is_recursive`=1)";
-                  $DB->query($query3) or die(sprintf(__('%1$s - Error during the database update: %2$s'),
-                        "0.78.1 update entities_id and is_recursive=1
-                        in glpi_reservationitems for ".$data['itemtype'], $DB->error()));
+                  $DB->queryOrDie($query3, "0.78.1 update entities_id and is_recursive=1 in glpi_reservationitems for ".$data['itemtype']);
                } else {
                   $query3="UPDATE `glpi_reservationitems`
                            SET `entities_id`=$entID
                            WHERE `itemtype`='".$data['itemtype']."'
                               AND `items_id` IN (SELECT `id` FROM `$itemtable`
                               WHERE `entities_id`=$entID)";
-                  $DB->query($query3) or die(sprintf(__('%1$s - Error during the database update: %2$s'),
-                        "0.78.1 update entities_id in glpi_reservationitems
-                        for ".$data['itemtype'], $DB->error()));
+                  $DB->queryOrDie($query3, "0.78.1 update entities_id in glpi_reservationitems for ".$data['itemtype']);
                }
             }
          }
@@ -126,18 +120,15 @@ function update078to0781($output='HTML') {
                     ADD `entities_id` INT NOT NULL DEFAULT '0' AFTER `id`,
                     ADD `is_recursive` TINYINT( 1 ) NOT NULL DEFAULT '0' AFTER `entities_id`,
                     ADD INDEX `entities_id` (`entities_id`),ADD INDEX `is_recursive` (`is_recursive`)";
-      $DB->query($query) or die(sprintf(__('%1$s - Error during the database update: %2$s'),
-                                       "0.78.1 add entities_id,is_recursive in glpi_knowbaseitemcategories", $DB->error()));
+      $DB->queryOrDie($query, "0.78.1 add entities_id,is_recursive in glpi_knowbaseitemcategories");
 
       // Set existing categories recursive global
       $query = "UPDATE `glpi_knowbaseitemcategories` SET `is_recursive` = '1'";
-      $DB->query($query) or die(sprintf(__('%1$s - Error during the database update: %2$s'),
-                                       "0.78.1 set value of is_recursive in glpi_knowbaseitemcategories", $DB->error()));
+      $DB->queryOrDie($query, "0.78.1 set value of is_recursive in glpi_knowbaseitemcategories");
 
       $query = "ALTER TABLE `glpi_knowbaseitemcategories` DROP INDEX `unicity` ,
                ADD UNIQUE `unicity` ( `entities_id`, `knowbaseitemcategories_id` , `name` ) ";
-      $DB->query($query) or die(sprintf(__('%1$s - Error during the database update: %2$s'),
-                                    "0.78.1 update unicity index on glpi_knowbaseitemcategories", $DB->error()));
+      $DB->queryOrDie($query, "0.78.1 update unicity index on glpi_knowbaseitemcategories");
    }
 
    // Display "Work ended." message - Keep this as the last action.
