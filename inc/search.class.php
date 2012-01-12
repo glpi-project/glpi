31b4b31b075c9aab4d59e19d5d011ce39bfd173b
remi <remi>
2012-01-12 11:09:56 +0000

    improve previous, for Suhosin users (debian like)

--- inc/search.class.php
+++ inc/search.class.php
@@ -1294,11 +1294,14 @@ class Search {
             if ($output_type == self::HTML_OUTPUT) {
                if ($isadmin) {
                   $max = ini_get('max_input_vars');  // Security limit since PHP 5.3.9
+                  if (!$max) {
+                     $max = ini_get('suhosin.post.max_vars');  // Security limit from Suhosin
+                  }
                   if ($max>0 && $max<($row_num+10)) {
                      echo "<table class='tab_cadre' width='80%'><tr class='tab_bg_1'><td><span class='b'>";
                      echo __('Selection too large, massive action disabled.')."</span>";
                      if ($_SESSION['glpi_use_mode'] == Session::DEBUG_MODE) {
-                        echo "<br>".__('To increase the limit: change max_input_vars in php configuration.');
+                        echo "<br>".__('To increase the limit: change max_input_vars or suhosin.post.max_vars in php configuration.');
                      }
                      echo "</td></tr></table>";
                   } else {
