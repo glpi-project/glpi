90e07ee42e708bb6e776f5e9b6c69877df7f3413
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-01-13 13:39:31 +0000

    better gettext
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@17030 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/networkport.class.php
+++ inc/networkport.class.php
@@ -536,7 +536,7 @@ class NetworkPort extends CommonDBChild {
          echo "</td></tr>\n";
 
       } else {
-         echo "<tr class='tab_bg_1'><td>". _n('Port number', 'Ports number', 2) ."</td>\n";
+         echo "<tr class='tab_bg_1'><td>". _n('Port number', 'Port numbers', 2) ."</td>\n";
          echo "<td>";
          echo "<input type='hidden' name='several' value='yes'>";
          echo "<input type='hidden' name='logical_number' value=''>\n";
@@ -649,7 +649,7 @@ class NetworkPort extends CommonDBChild {
 
 
    function getSearchOptions() {
-      
+
       $tab = array();
       $tab['common'] = __('Characteristics');
 
