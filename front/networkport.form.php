e2070dfaf3c224b79d89550aa796abd70f2f2b6e
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-02-06 12:53:24 +0000

    networkport header
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@17391 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/networkport.form.php
+++ front/networkport.form.php
@@ -248,7 +248,7 @@ if (isset($_POST["add"])) {
       $_GET["instantiation_type"] = "";
    }
    Session::checkRight("networking", "w");
-   Html::header(NetworkPort::getTypeName(2), $_SERVER['PHP_SELF'], "inventory");
+   Html::header(NetworkPort::getTypeName(2), $_SERVER['PHP_SELF'], "inventory", 'networking', 'networkport');
 
    $np->showForm($_GET["id"], $_GET);
    Html::footer();
