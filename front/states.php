0a7d50a485bdbbb6524761ebf0b15d3efb255e5f
yllen <yllen>
2012-02-07 15:06:33 +0000

    review code (st)

--- front/states.php
+++ front/states.php
@@ -40,7 +40,7 @@ if (!isset($_GET["synthese"])) {
 }
 Session::checkCentralAccess();
 
-Html::header(_n('Status of items', 'Status of items',2),$_SERVER['PHP_SELF'],"inventory","state");
+Html::header(State::getTypeName(2), $_SERVER['PHP_SELF'], "inventory", "state");
 
 if ($_GET["synthese"] == "yes") {
    State::showSummary($_SERVER['PHP_SELF']);
