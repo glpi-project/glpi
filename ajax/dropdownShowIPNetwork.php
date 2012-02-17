17590d4f43934a0e6968e18e7d3b22c2aab5d343
webmyster <webmyster@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-02-17 07:26:37 +0000

    Improvement of IPNetwork display from NetworkName Form
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@17536 35612d4c-9d10-0410-837c-bf9dd1be60d7

---
+++ ajax/dropdownShowIPNetwork.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * @version $Id: dropdownConnectEthernetPortDeviceType.php 17149 2012-01-24 11:12:05Z moyo $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2012 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Damien Touraine
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+define('GLPI_ROOT','..');
+include (GLPI_ROOT."/inc/includes.php");
+
+header("Content-Type: text/html; charset=UTF-8");
+Html::header_nocache();
+
+$network = new IPNetwork();
+
+if ($network->can($_POST['ipnetworks_id'], 'r')) {
+   echo "<br>\n";
+   echo "<a href='".$network->getLinkURL()."'>".$network->fields['completename']."</a><br>\n";
+
+   $address = $network->getAddress()->getTextual();
+   $netmask = $network->getNetmask()->getTextual();
+   $gateway = $network->getGateway()->getTextual();
+
+   $start   = new IPAddress();
+   $end     = new IPAddress();
+
+   $network->computeNetworkRange($start, $end);
+
+   echo __('IP network') . "&nbsp;: $address/$netmask<br>\n";
+   echo __('First/last addresses') . "&nbsp;: " . $start->getTextual() . "/" .
+        $end->getTextual();
+   if (!empty($gateway)) {
+      echo "<br>\n";
+      echo __('Gateway') . "&nbsp;: $gateway\n";
+   }
+}
+
+?>
