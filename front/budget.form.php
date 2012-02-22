adf73131fbed4dec9b40c8932e8b915fda1b0b82
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-02-22 20:50:24 +0000

    complete gettext
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@17590 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/budget.form.php
+++ front/budget.form.php
@@ -63,7 +63,7 @@ if (isset($_POST["add"])) {
    if ($budget->delete($_POST)) {
       Event::log($_POST["id"], "budget", 4, "financial",
                   //TRANS: %s is the user login
-                  sprintf(__('%s deletes the item'), $_SESSION["glpiname"]));
+                  sprintf(__('%s deletes an item'), $_SESSION["glpiname"]));
    }
    $budget->redirectToList();
 
@@ -73,7 +73,7 @@ if (isset($_POST["add"])) {
    if ($budget->restore($_POST)) {
       Event::log($_POST["id"], "budget", 4, "financial",
                   //TRANS: %s is the user login
-                  sprintf(__('%s restores the item'), $_SESSION["glpiname"]));
+                  sprintf(__('%s restores an item'), $_SESSION["glpiname"]));
    }
    $budget->redirectToList();
 
@@ -83,7 +83,7 @@ if (isset($_POST["add"])) {
    if ($budget->delete($_REQUEST,1)) {
       Event::log($_REQUEST["id"], "budget", 4, "financial",
                   //TRANS: %s is the user login
-                  sprintf(__('%s purges the item'), $_SESSION["glpiname"]));
+                  sprintf(__('%s purges an item'), $_SESSION["glpiname"]));
    }
    $budget->redirectToList();
 
@@ -93,7 +93,7 @@ if (isset($_POST["add"])) {
    if ($budget->update($_POST)) {
       Event::log($_POST["id"], "budget", 4, "financial",
                   //TRANS: %s is the user login
-                  sprintf(__('%s updates the item'), $_SESSION["glpiname"]));
+                  sprintf(__('%s updates an item'), $_SESSION["glpiname"]));
    }
    Html::back();
 
--- front/change.form.php
+++ front/change.form.php
@@ -57,7 +57,7 @@ if (isset($_POST["add"])) {
    $change->delete($_POST);
    Event::log($_POST["id"], "change", 4, "maintain",
               //TRANS: %s is the user login
-              sprintf(__('%s deletes the item'), $_SESSION["glpiname"]));
+              sprintf(__('%s deletes an item'), $_SESSION["glpiname"]));
    $change->redirectToList();
 
 } else if (isset($_POST["restore"])) {
@@ -66,7 +66,7 @@ if (isset($_POST["add"])) {
    $change->restore($_POST);
    Event::log($_POST["id"], "change", 4, "maintain",
               //TRANS: %s is the user login
-              sprintf(__('%s restores the item'), $_SESSION["glpiname"]));
+              sprintf(__('%s restores an item'), $_SESSION["glpiname"]));
    $change->redirectToList();
 
 } else if (isset($_REQUEST["purge"])) {
@@ -75,7 +75,7 @@ if (isset($_POST["add"])) {
 
    Event::log($_REQUEST["id"], "change", 4, "maintain",
               //TRANS: %s is the user login
-              sprintf(__('%s purges the item'), $_SESSION["glpiname"]));
+              sprintf(__('%s purges an item'), $_SESSION["glpiname"]));
    $change->redirectToList();
 
 } else if (isset($_POST["update"])) {
@@ -84,7 +84,7 @@ if (isset($_POST["add"])) {
    $change->update($_POST);
    Event::log($_POST["id"], "change", 4, "maintain",
               //TRANS: %s is the user login
-              sprintf(__('%s updates the item'), $_SESSION["glpiname"]));
+              sprintf(__('%s updates an item'), $_SESSION["glpiname"]));
 
    Html::back();
 
