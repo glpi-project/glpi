03d71740791c256ab0db6d62ee4551f033ec9209
moyooo <moyo@indepnet.net>
2012-02-22 20:50:24 +0000

    complete gettext

--- front/cartridgeitem.form.php
+++ front/cartridgeitem.form.php
@@ -58,7 +58,7 @@ if (isset($_POST["add"])) {
    if ($cartype->delete($_POST)) {
       Event::log($_POST["id"], "cartridges", 4, "inventory",
                  //TRANS: %s is the user login
-                 sprintf(__('%s deletes the item'), $_SESSION["glpiname"]));
+                 sprintf(__('%s deletes an item'), $_SESSION["glpiname"]));
    }
    $cartype->redirectToList();
 
@@ -68,7 +68,7 @@ if (isset($_POST["add"])) {
    if ($cartype->restore($_POST)) {
       Event::log($_POST["id"], "cartridges", 4, "inventory",
                  //TRANS: %s is the user login
-                 sprintf(__('%s restores the item'), $_SESSION["glpiname"]));
+                 sprintf(__('%s restores an item'), $_SESSION["glpiname"]));
    }
    $cartype->redirectToList();
 
@@ -78,7 +78,7 @@ if (isset($_POST["add"])) {
    if ($cartype->delete($_POST,1)) {
       Event::log($_POST["id"], "cartridges", 4, "inventory",
                  //TRANS: %s is the user login
-                 sprintf(__('%s purges the item'), $_SESSION["glpiname"]));
+                 sprintf(__('%s purges an item'), $_SESSION["glpiname"]));
    }
    $cartype->redirectToList();
 
@@ -88,7 +88,7 @@ if (isset($_POST["add"])) {
    if ($cartype->update($_POST)) {
       Event::log($_POST["id"], "cartridges", 4, "inventory",
                  //TRANS: %s is the user login
-                 sprintf(__('%s updates the item'), $_SESSION["glpiname"]));
+                 sprintf(__('%s updates an item'), $_SESSION["glpiname"]));
    }
    Html::back();
 
