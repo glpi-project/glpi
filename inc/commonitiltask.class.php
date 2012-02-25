73e72e3ea76cb72a3e3de4e76c9b66299d4590ad
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-02-25 09:33:34 +0000

    typo
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@17632 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/commonitiltask.class.php
+++ inc/commonitiltask.class.php
@@ -1088,7 +1088,7 @@ abstract class CommonITILTask  extends CommonDBTM {
       if ($this->maybePrivate()) {
          echo "<input type='hidden' name='is_private' value='".$_SESSION['glpitask_private']."'>";
       }
-      echo "<input type='submit' name='add' value=\""._sx('button',Add')."\" class='submit'>";
+      echo "<input type='submit' name='add' value=\""._sx('button', 'Add')."\" class='submit'>";
    }
 
 
