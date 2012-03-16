420e235ba5d8e7decc3cdf204b328439259b5edb
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-03-16 16:08:21 +0000

    fix entity update
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@17878 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/entity.class.php
+++ inc/entity.class.php
@@ -1765,8 +1765,6 @@ class Entity extends CommonTreeDropdown {
       if ($canedit) {
          echo "<tr>";
          echo "<td class='tab_bg_2 center' colspan='4'>";
-         echo "<input type='hidden' name='entities_id' value='$ID'>";
-
          echo "<input type='hidden' name='id' value='".$entity->fields["id"]."'>";
          echo "<input type='submit' name='update' value=\"".__s('Save')."\"
                   class='submit'>";
