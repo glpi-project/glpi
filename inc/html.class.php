a7a4565eaacf1844824f2b40ab8c7e0c1286837d
remi <remi>
2012-03-23 15:19:15 +0000

    first work on #3384 (not ok, need to set the correct default value)

--- inc/html.class.php
+++ inc/html.class.php
@@ -2064,13 +2064,16 @@ class Html {
 
       /// Search engine
       echo "<div id='c_recherche' >\n";
-      echo "<form method='get' action='".$CFG_GLPI["root_doc"]."/front/search.php'>\n";
-      echo "<div id='boutonRecherche'>";
-      echo "<input type='image' src='".$CFG_GLPI["root_doc"]."/pics/search.png' value='OK'
-             title=\"". __s('Post')."\"  alt=\"".__s('Post')."\"></div>";
-      echo "<div id='champRecherche'><input size='15' type='text' name='globalsearch'
-                                      value='". __s('Search')."' onfocus=\"this.value='';\"><";
-      echo "/div></form>";
+      if ($CFG_GLPI['allow_search_global']) {
+         echo "<form method='get' action='".$CFG_GLPI["root_doc"]."/front/search.php'>\n";
+         echo "<div id='boutonRecherche'>";
+         echo "<input type='image' src='".$CFG_GLPI["root_doc"]."/pics/search.png' value='OK'
+                title=\"". __s('Post')."\"  alt=\"".__s('Post')."\"></div>";
+         echo "<div id='champRecherche'><input size='15' type='text' name='globalsearch'
+                                         value='". __s('Search')."' onfocus=\"this.value='';\">";
+         echo "</div></form>";
+      }
+      echo "</div>";
 
       echo "<div class='sep'></div>\n";
       echo "</div>";
