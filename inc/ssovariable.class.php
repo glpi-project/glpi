7120e24b4071905003b236900904c8218b3556e3
Walid Nouh <wnouh@teclib.com>
2012-03-23 16:36:24 +0000

    Add files in previous commit

---
+++ inc/ssovariable.class.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2012 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Walid Nouh
+// Purpose of file:
+// ----------------------------------------------------------------------
+if (!defined('GLPI_ROOT')) {
+   die("Sorry. You can't access directly to this file");
+}
+
+class SsoVariable extends CommonDropdown {
+
+   var $refresh_page = true;
+
+   function canCreate() {
+      return Session::haveRight('config', 'w');
+   }
+
+   function canView() {
+      return Session::haveRight('config', 'r');
+   }
+
+   static function getTypeName($nb=0) {
+      return _n('Field storage of the login in the HTTP request',
+                'Fields storage of the login in the HTTP request', $nb);
+   }
+
+}
+?>
