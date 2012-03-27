a5df2ea594ccb7bc12a764f8ba0fc97a54d3e0fb
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-03-27 15:55:57 +0000

    review code + gettext
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18063 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/notimportedemail.class.php
+++ inc/notimportedemail.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * @version $Id$
  -------------------------------------------------------------------------
@@ -42,6 +41,7 @@ class NotImportedEmail extends CommonDBTM {
    const MATCH_NO_RULE = 0;
    const USER_UNKNOWN  = 1;
 
+
    function canCreate() {
       return Session::haveRight('config', 'w');
    }
@@ -59,48 +59,49 @@ class NotImportedEmail extends CommonDBTM {
 
    function getSearchOptions() {
 
-      $tab = array();
-      $tab[1]['table']         = 'glpi_notimportedemails';
-      $tab[1]['field']         = 'from';
-      $tab[1]['name']          = __('From email header');
-      $tab[1]['massiveaction'] = false;
+      $tab                       = array();
 
-      $tab[2]['table']         = 'glpi_notimportedemails';
-      $tab[2]['field']         = 'to';
-      $tab[2]['name']          = __('To email header');
-      $tab[2]['massiveaction'] = false;
+      $tab[1]['table']           = 'glpi_notimportedemails';
+      $tab[1]['field']           = 'from';
+      $tab[1]['name']            = __('From email header');
+      $tab[1]['massiveaction']   = false;
 
-      $tab[3]['table']         = 'glpi_notimportedemails';
-      $tab[3]['field']         = 'subject';
-      $tab[3]['name']          = __('Subject email header');
-      $tab[3]['massiveaction'] = false;
+      $tab[2]['table']           = 'glpi_notimportedemails';
+      $tab[2]['field']           = 'to';
+      $tab[2]['name']            = __('To email header');
+      $tab[2]['massiveaction']   = false;
 
-      $tab[4]['table']         = 'glpi_mailcollectors';
-      $tab[4]['field']         = 'name';
-      $tab[4]['name']          = __('Mails receiver');
-      $tab[4]['datatype']      = 'itemlink';
-      $tab[4]['itemlink_type'] = 'MailCollector';
+      $tab[3]['table']           = 'glpi_notimportedemails';
+      $tab[3]['field']           = 'subject';
+      $tab[3]['name']            = __('Subject email header');
+      $tab[3]['massiveaction']   = false;
 
-      $tab[5]['table']         = 'glpi_notimportedemails';
-      $tab[5]['field']         = 'messageid';
-      $tab[5]['name']          = __('Message-ID email header');
-      $tab[5]['massiveaction'] = false;
+      $tab[4]['table']           = 'glpi_mailcollectors';
+      $tab[4]['field']           = 'name';
+      $tab[4]['name']            = __('Mails receiver');
+      $tab[4]['datatype']        = 'itemlink';
+      $tab[4]['itemlink_type']   = 'MailCollector';
 
-      $tab[6]['table']          = 'glpi_users';
-      $tab[6]['field']          = 'name';
-      $tab[6]['name']           = __('Requester');
+      $tab[5]['table']           = 'glpi_notimportedemails';
+      $tab[5]['field']           = 'messageid';
+      $tab[5]['name']            = __('Message-ID email header');
+      $tab[5]['massiveaction']   = false;
 
-      $tab[16]['table']         = 'glpi_notimportedemails';
-      $tab[16]['field']         = 'reason';
-      $tab[16]['name']          = __('Reason of rejection');
-      $tab[16]['datatype']      = 'text';
-      $tab[16]['massiveaction'] = false;
+      $tab[6]['table']           = 'glpi_users';
+      $tab[6]['field']           = 'name';
+      $tab[6]['name']            = __('Requester');
 
-      $tab[19]['table']         = 'glpi_notimportedemails';
-      $tab[19]['field']         = 'date';
-      $tab[19]['name']          = __('Date');
-      $tab[19]['datatype']      = 'datetime';
-      $tab[19]['massiveaction'] = false;
+      $tab[16]['table']          = 'glpi_notimportedemails';
+      $tab[16]['field']          = 'reason';
+      $tab[16]['name']           = __('Reason of rejection');
+      $tab[16]['datatype']       = 'text';
+      $tab[16]['massiveaction']  = false;
+
+      $tab[19]['table']          = 'glpi_notimportedemails';
+      $tab[19]['field']          = 'date';
+      $tab[19]['name']           = __('Date');
+      $tab[19]['datatype']       = 'datetime';
+      $tab[19]['massiveaction']  = false;
 
       return $tab;
    }
