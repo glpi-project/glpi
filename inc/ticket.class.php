25f0c59dc57fc3e0f1dd1d55440a8cd8f6ab1d2f
remi <remi>
2012-03-28 14:41:33 +0000

    [0.83] remove unedded checkbox on ticket list (central, item, ...), post 28227

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4839,7 +4839,17 @@ class Ticket extends CommonITILObject {
    }
 
 
-   static function showShort($id, $followups, $output_type=HTML_OUTPUT, $row_num=0, $id_for_massaction=-1) {
+   /**
+    * Display a line for a ticket
+    *
+    * @param $id                 Integer  ID of the ticket
+    * @param $followups          Boolean  show followup columns
+    * @param $output_type        Integer  type of output
+    * @param $row_num            Integer  row number
+    * @param $id_for_massaction  Integer  default 0 means no massive action
+    *
+    */
+   static function showShort($id, $followups, $output_type=HTML_OUTPUT, $row_num=0, $id_for_massaction=0) {
       global $CFG_GLPI, $LANG;
 
       $rand = mt_rand();
@@ -4851,12 +4861,6 @@ class Ticket extends CommonITILObject {
       // Print links or not in case of user view
       // Make new job object and fill it from database, if success, print it
       $job = new self();
-
-      // If id is specified it will be used as massive aciton id
-      // Used when displaying ticket and wanting to delete a link data
-      if ($id_for_massaction==-1) {
-         $id_for_massaction = $id;
-      }
 
       $candelete   = Session::haveRight("delete_ticket", "1");
       $canupdate   = Session::haveRight("update_ticket", "1");
@@ -4889,7 +4893,7 @@ class Ticket extends CommonITILObject {
          }
 
          if (($candelete || $canupdate)
-             && $output_type == HTML_OUTPUT) {
+             && $output_type == HTML_OUTPUT && $id_for_massaction) {
 
             $sel = "";
             if (isset($_GET["select"]) && $_GET["select"] == "all") {
