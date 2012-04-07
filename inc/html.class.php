10afe9eb3eca41e7fd5a4365eddeb296d3029688
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-04-07 07:57:50 +0000

    simpler.., see #3452
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18204 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/html.class.php
+++ inc/html.class.php
@@ -4057,10 +4057,9 @@ class Html {
       if ($num > 0 && $num < $tot) {
          // TRANS %1$d %2$d are numbers (displayed, total)
          $cpt = sprintf(__('%1$d on %2$d'), $num, $tot);
-      } else if ($num) {
-         $cpt = $num;
       } else  {
          // $num is 0, so means configured to display nothing
+         // or $num == $tot
          $cpt = $tot;
       }
       return sprintf(__('%1$s (%2$s)'), $string, $cpt);
