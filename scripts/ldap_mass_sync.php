eb0597f08427fcf77b7375e635856aeda8fce1d3
Walid Nouh <wnouh@teclib.com>
2012-04-10 07:54:41 +0000

    [0.83] fixed #3459 LDAP script doen't ignore inactive directories

--- scripts/ldap_mass_sync.php
+++ scripts/ldap_mass_sync.php
@@ -97,11 +97,12 @@ if (!Toolbox::canUseLdap() || !countElementsInTable('glpi_authldaps')) {
 }
 
 $sql = "SELECT `id`, `name`
-        FROM `glpi_authldaps`";
+        FROM `glpi_authldaps`
+        WHERE `is_active`='1'";
 
 //Get the ldap server's id by his name
 if ($options['ldapservers_id'] != NOT_AVAILABLE) {
-   $sql .= " WHERE `id` = '" . $options['ldapservers_id']."'";
+   $sql .= " AND `id` = '" . $options['ldapservers_id']."'";
 }
 
 $result = $DB->query($sql);
