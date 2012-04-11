30dcfddeebb5e8c4a89d434ebc2455c4f37abca9
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-04-11 16:00:16 +0000

    fix migration in strict mode (apply r18233)
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18235 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_0803_083.php
+++ install/update_0803_083.php
@@ -1556,8 +1556,8 @@ function update0803to083() {
    foreach ($fieldparent as $field_parent) {
       if (FieldExists("glpi_entitydatas", $field_parent, false)) {
          $query = "UPDATE `glpi_entitydatas`
-                   SET `$field_parent` = -2
-                   WHERE `$field_parent` = -1";
+                   SET `$field_parent` = '-2'
+                   WHERE `$field_parent` = '-1'";
          $DB->queryOrDie($query, "0.83 new value for inherit parent -1 in glpi_entitydatas");
       }
    }
