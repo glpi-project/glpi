37bdcf59059563de5444a96b35df728c6a113937
moyooo <moyo@indepnet.net>
2012-04-17 07:30:19 +0000

    Add associated contracts to suppliers list (and from contracts too) see #3426

--- inc/supplier.class.php
+++ inc/supplier.class.php
@@ -275,6 +275,16 @@ class Supplier extends CommonDBTM {
       $tab[86]['name']     = __('Child entities');
       $tab[86]['datatype'] = 'bool';
 
+      $tab[29]['table']         = 'glpi_contracts';
+      $tab[29]['field']         = 'name';
+      $tab[29]['name']          = _n ('Associated contract', 'Associated contracts', 2);
+      $tab[29]['forcegroupby']  = true;
+      $tab[29]['datatype']      = 'itemlink';
+      $tab[29]['itemlink_type'] = 'Contract';
+      $tab[29]['massiveaction'] = false;
+      $tab[29]['joinparams']    = array('beforejoin'
+                                       => array('table'      => 'glpi_contracts_suppliers',
+                                                'joinparams' => array('jointype' => 'child')));
       return $tab;
    }
 
