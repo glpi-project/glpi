7ec31b767f9050b871e1b620626002a365ba0c3b
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-05-02 10:20:41 +0000

    CS
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18412 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ipnetwork.class.php
+++ inc/ipnetwork.class.php
@@ -198,7 +198,8 @@ class IPNetwork extends CommonImplicitTreeDropdown {
       // Or if $this->fields["network"] != $input["network"] we a updating the network
       $address = new IPAddress();
       $netmask = new IPNetmask();
-      if (!isset($this->fields["id"]) || ($input["network"] != $this->fields["network"])) {
+      if (!isset($this->fields["id"])
+          || ($input["network"] != $this->fields["network"])) {
          $network = explode ("/", $input["network"]);
          if (count($network) != 2) {
             return array('error' => __('Invalid input format for the network'),
