72830ecdcd748d3f2cc8c4bfc7e2c1ae698b3936
moyooo <moyo@indepnet.net>
2012-05-03 09:12:04 +0000

    [0.83] Missing supplier field on ticket creation fixed #3555

--- inc/commonitilobject.class.php
+++ inc/commonitilobject.class.php
@@ -2557,7 +2557,7 @@ abstract class CommonITILObject extends CommonDBTM {
 
       // Supplier
       if ($this->canAssign() && !$is_hidden['suppliers_id_assign']
-         && $this->fields["suppliers_id_assign"]) {
+         && ($this->fields["suppliers_id_assign"] || !$ID)) {
          echo self::getActorIcon('supplier', self::ASSIGN);
          /// For ticket templates : mandatories
          if (isset($options['_tickettemplate'])) {
