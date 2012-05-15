6f329a435482e121cfe98cacbccc307a2f43b399
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-05-15 07:11:44 +0000

    update filesystem type when change, see #3586
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18460 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ocsserver.class.php
+++ inc/ocsserver.class.php
@@ -4575,12 +4575,13 @@ class OcsServer extends CommonDBTM {
                      }
 
                   } else {
-                     // Only update sizes if needed
+                     // Only update if needed
                      $id = array_search(stripslashes($disk["name"]), $import_disk);
                      if ($d->getFromDB($id)) {
 
-                        // Update on total size change or variation of 5%
+                        // Update on type, total size change or variation of 5%
                         if (($d->fields['totalsize'] != $disk['totalsize'])
+                            || $d->fields['filesystems_id']!=$disk['filesystems_id']
                             || ((abs($disk['freesize']-$d->fields['freesize'])/$disk['totalsize']) > 0.05)) {
 
                            $toupdate['id']        = $id;
