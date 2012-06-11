300fb0300affa579cc1cda7f8a0383508ed526de
remi <remi>
2012-06-11 14:05:26 +0000

    [0.83] minor fix

--- tools/cliupdate.php
+++ tools/cliupdate.php
@@ -271,14 +271,14 @@ if (version_compare($current_version, GLPI_VERSION, 'ne')) {
    $plugin = new Plugin();
    $plugin->unactivateAll();
 
-   $migration->displayWarning("Migration Done.");
+   $migration->displayWarning("\nMigration Done.");
 
 } else if (in_array('--force', $_SERVER['argv'])) {
 
    include("../install/update_083_0831.php");
    update083to0831();
 
-   $migration->displayWarning("Forced migration Done.");
+   $migration->displayWarning("\nForced migration Done.");
 
 } else {
    $migration->displayWarning("No migration needed.");
