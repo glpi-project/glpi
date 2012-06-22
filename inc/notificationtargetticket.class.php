c06c62bb83bcdd2934c362e9b8fc24e867658a94
tsmr <tsmr@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-06-22 07:34:54 +0000

    Ticket notification : don't show auto close warning when autoclose = 0. fix #3681
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18683 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/notificationtargetticket.class.php
+++ inc/notificationtargetticket.class.php
@@ -242,7 +242,7 @@ class NotificationTargetTicket extends NotificationTargetCommonITILObject {
 
       $datas['##ticket.autoclose##']             = __('Never');
       $datas['##lang.ticket.autoclosewarning##'] = "";
-      if ($autoclose_value >= 0) {
+      if ($autoclose_value > 0) {
                $datas['##ticket.autoclose##'] = $autoclose_value;
       }
       if ($autoclose_value > 0) {
