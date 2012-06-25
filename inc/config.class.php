91f00146d1d9c31c8c748315d89ac29453d8d5f4
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-06-25 07:01:51 +0000

    add use_check_pref in config, see #3637
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18709 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/config.class.php
+++ inc/config.class.php
@@ -601,8 +601,9 @@ class Config extends CommonDBTM {
       echo "<tr class='tab_bg_2'>";
       echo "<td>" . __('Keep tickets when purging hardware in the inventory') . "</td><td>";
       Dropdown::showYesNo("keep_tickets_on_delete", $CFG_GLPI["keep_tickets_on_delete"]);
-      echo "</td><td colspan='2'>&nbsp;</td>";
-      echo "</tr>";
+      echo "</td><td>".__('Show preferences in ticket form (simplified interface)')."</td><td>";
+      Dropdown::showYesNo('use_check_pref', $CFG_GLPI['use_check_pref']);
+      echo "</td></tr>";
 
       echo "<tr class='tab_bg_2'>";
       echo "<td>" .__('Allow anonymous ticket creation (helpdesk.receiver)') . "</td><td>";
