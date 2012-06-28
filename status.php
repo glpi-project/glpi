8fbcf54f59ecbbb25e2dd268228c4e9f6a44da99
moyooo <moyo@indepnet.net>
2012-06-28 21:03:24 +0000

    CSRF prevention step 1 see #3704

--- status.php
+++ status.php
@@ -32,6 +32,7 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
+define('DO_NOT_CHECK_HTTP_REFERER', 1);
 define('GLPI_ROOT', '.');
 include (GLPI_ROOT . "/inc/includes.php");
 
