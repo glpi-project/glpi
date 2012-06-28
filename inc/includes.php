cd09c852059001be8fdd5af7941103354a642fc2
moyooo <moyo@indepnet.net>
2012-06-28 21:02:01 +0000

    [0.83] clean previous see #3704

--- inc/includes.php
+++ inc/includes.php
@@ -137,7 +137,6 @@ if (!defined('DO_NOT_CHECK_HTTP_REFERER')) {
    // Do not applyed for plugins on 0.83
    if (strstr($_SERVER['REQUEST_URI'],$CFG_GLPI['root_doc'].'/plugins/') === FALSE) {
       if (strstr($_SERVER['HTTP_REFERER'],$CFG_GLPI['root_doc']) === FALSE) {
-         echo $_SERVER['HTTP_REFERER'].' '.$CFG_GLPI['root_doc'];
          Session::addMessageAfterRedirect("Error calling the previous page from forbidden one.", false, ERROR);
          Html::back();
       };
