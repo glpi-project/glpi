9fb24613985163e8525e37510a41cfce9210c00d
webmyster <webmyster@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-07-15 07:42:26 +0000

    Reverse previous display of opposite networkport informations
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@18905 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/networkport.class.php
+++ inc/networkport.class.php
@@ -583,27 +583,13 @@ class NetworkPort extends CommonDBChild {
 
                   $t_row->addCell($c_name, $netport->fields["name"], NULL, $netport);
 
-                  $portsForName = array($netport);
-                  $instantiation = $netport->getInstantiation();
+                 $instantiation = $netport->getInstantiation();
                   if ($instantiation !== false) {
                      $instantiation->getInstantiationHTMLTable_($netport, $item, $t_row,
                                                                 $table_options);
-                     if ($instantiation->getType() == 'NetworkPortEthernet') {
-                        $opposite = new NetworkPort();
-                        if ($opposite->getFromDB($netport->getContact($netport->getID()))) {
-                           $portsForName[] = $opposite;
-                        }
-                     }
                      unset($instantiation);
                   }
-                  foreach ($portsForName as $port) {
-                     $options = $table_options;
-                     if ($port != $netport) {
-                        $options['canedit'] = false;
-                     }
-                     // TODO : we may display opposite informations differently ...
-                     NetworkName::getHTMLTableCellsForItem($t_row, $port, NULL, $options);
-                  }
+                  NetworkName::getHTMLTableCellsForItem($t_row, $netport, NULL, $table_options);
 
                }
 
