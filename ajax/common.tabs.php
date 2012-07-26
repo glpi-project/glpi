d5f2cbdde55184a91fc74288f4778b7b193bf0db
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-07-26 06:40:50 +0000

    remove translation for technical messages : to be discussed
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@19032 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- ajax/common.tabs.php
+++ ajax/common.tabs.php
@@ -78,10 +78,10 @@ if (isset($_POST['full_page_tab'])) {
 
    // I think that we should display this warning, because tabs are not prepare
    // for being used full space ...
-   if (!isset($_SESSION['glpi_warned_about_full_page_tab'])) {
-      //TRANS: Javascript text. Don't include any single or double quotes nor backslach.
-      $msg  = __('WARNING : full page tabs are only for debug/validation purpose !').'\n';
-      $msg .= __('Actions on this page may have undefined result.').'\n';
+   if (isset($_SESSION['glpi_warned_about_full_page_tab'])) {
+      // Debug string : not really need translation.
+      $msg  = 'WARNING : full page tabs are only for debug/validation purpose !\n';
+      $msg .= 'Actions on this page may have undefined result.';
       echo "<script type='text/javascript' >\n";
       echo "alert('$msg')";
       echo "</script>";
