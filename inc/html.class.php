a35e0605bd68513020d576be51d84bcaf93b997a
moyooo <moyo@indepnet.net>
2012-09-04 08:44:04 +0000

    more MA see #3722

--- inc/html.class.php
+++ inc/html.class.php
@@ -3073,7 +3073,7 @@ class Html {
          $width= '80%';
       }
 
-      $identifier = md5($url.$itemtype);
+      $identifier = md5($url.$itemtype.serialize($p['extraparams']));
       $max        = ini_get('max_input_vars');  // Security limit since PHP 5.3.9
       if (!$max) {
          $max = ini_get('suhosin.post.max_vars');  // Security limit from Suhosin
