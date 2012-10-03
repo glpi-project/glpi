c905b125ffebd83173cb851b4b02d50988aa7686
moyooo <moyo@indepnet.net>
2012-10-03 07:32:49 +0000

    [0.83] Reminder for helpdesk fixed #3915

--- front/helpdesk.public.php
+++ front/helpdesk.public.php
@@ -100,18 +100,7 @@ if (isset($_GET['create_ticket'])) {
 
    if (Session::haveRight("reminder_public","r")) {
       echo "<tr><td class='top' width='450px'>";
-      Reminder::showListForCentral($_SESSION["glpiactive_entity"]);
-      $entities = array_reverse(getAncestorsOf("glpi_entities", $_SESSION["glpiactive_entity"]));
-
-      foreach ($entities as $entity) {
-         Reminder::showListForCentral($entity, true);
-      }
-
-      foreach ($_SESSION["glpiactiveentities"] as $entity) {
-         if ($entity != $_SESSION["glpiactive_entity"]) {
-            Reminder::showListForCentral($entity, false);
-         }
-      }
+      Reminder::showListForCentral(false);
       echo "</td></tr>";
    }
 
