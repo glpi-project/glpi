754f34ce1a56a7f69b70cb72239f545572bca942
moyooo <moyo@indepnet.net>
2012-10-07 13:00:08 +0000

    improve lang : see #3902

--- ajax/resaperiod.php
+++ ajax/resaperiod.php
@@ -43,6 +43,7 @@ Html::header_nocache();
 Session::checkLoginUser();
 
 if (isset($_POST['type']) && isset($_POST['end'])) {
+
    echo "<table>";
    switch ($_POST['type']) {
       case 'day' :
@@ -70,8 +71,8 @@ if (isset($_POST['type']) && isset($_POST['end'])) {
       case 'month' :
          echo "<tr><td colspan='2'>";
          echo "<select name='periodicity[subtype]'>";
-         echo "<option value='date'>".__('Same date')."</option>\n";
-         echo "<option value='day'>".__('Same day')."</option>\n";
+         echo "<option value='date'>".__('Each month, same date')."</option>\n";
+         echo "<option value='day'>".__('Each month, same day of week')."</option>\n";
          echo "</select>";
          echo "</td></tr>";
          echo "<tr><td>".__('End date').'</td><td>';
