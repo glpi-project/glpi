30421de8fc4cf4a81d488153b87611e3b7ed9d0e
webmyster <webmyster@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-10-20 15:01:41 +0000

    Add little documentation
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@19554 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/commondbchild.class.php
+++ inc/commondbchild.class.php
@@ -391,7 +391,13 @@ abstract class CommonDBChild extends CommonDBConnexity {
     *
     * @TODO add ability to define specifically which field has been updated
     *
-    * @param $item the other item (ie. : $item1)
+    * @param $case :
+    *              - 'add' when this CommonDBChild is added (to and item)
+    *              - 'update values previous' old values of the CommonDBChild itself
+    *              - 'update values next' next values of the CommonDBChild itself
+    *              - 'update item previous' transfert : this is removed from the old item
+    *              - 'update item next' transfert : this is added to the new item
+    *              - 'delete' when this CommonDBChild is remove (from an item)
     *
     * @return (string) the name of the entry for the database (ie. : correctly slashed)
    **/
