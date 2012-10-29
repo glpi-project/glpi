4860991f39b757ab612e5082071d593ebef22eaf
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2012-10-29 22:47:53 +0000

    minor fix
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@19611 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ticketvalidation.class.php
+++ inc/ticketvalidation.class.php
@@ -323,7 +323,7 @@ class TicketValidation  extends CommonDBChild {
     *
     * @see CommonDBChild::getHistoryNameForItem
    **/
-   function getHistoryNameForItem($item, $case) {
+   function getHistoryNameForItem(CommonDBTM $item, $case) {
 
       $username = getUserName($this->fields["users_id_validate"]);
       switch ($case) {
