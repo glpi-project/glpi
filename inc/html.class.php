2dcc5b4a902ba2864625b2f153416566ca8e42e3
moyooo <moyo@indepnet.net>
2012-11-27 11:25:14 +0000

    [0.83] Prevent click jacking and directory listing fixed #4056 thanks to M.R.Vignesh kumar & Alok.J.Sudhakar.

--- inc/html.class.php
+++ inc/html.class.php
@@ -762,6 +762,9 @@ class Html {
 
       // Send UTF8 Headers
       header("Content-Type: text/html; charset=UTF-8");
+      // No frame to prevent click-jacking
+      header('x-frame-options:DENY');
+
       // Send extra expires header
       self::header_nocache();
 
