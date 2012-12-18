02d685ffa8a9172445a83117b905b9b439874707
moyooo <moyo@indepnet.net>
2012-12-18 14:01:57 +0000

    upgrade simplepie to 1.3.1

--- lib/simplepie/SimplePie/Misc.php
+++ lib/simplepie/SimplePie/Misc.php
@@ -33,7 +33,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package SimplePie
- * @version 1.3
+ * @version 1.3.1
  * @copyright 2004-2012 Ryan Parman, Geoffrey Sneddon, Ryan McCue
  * @author Ryan Parman
  * @author Geoffrey Sneddon
@@ -79,12 +79,12 @@ class SimplePie_Misc
 
 	public static function absolutize_url($relative, $base)
 	{
-            // Fix for GLPI : if $iri is false return $relative URL
-            // See https://github.com/simplepie/simplepie/issues/214
-            if ($iri = SimplePie_IRI::absolutize(new SimplePie_IRI($base), $relative)) {
-               return $iri->get_uri();
-            }
-            return false;
+		$iri = SimplePie_IRI::absolutize(new SimplePie_IRI($base), $relative);
+		if ($iri === false)
+		{
+			return false;
+		}
+		return $iri->get_uri();
 	}
 
 	/**
