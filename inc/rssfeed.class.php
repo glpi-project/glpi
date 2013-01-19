96a892f40ac74de6ef981e6d70a450c827292031
moyooo <moyo@indepnet.net>
2013-01-19 09:40:28 +0000

    gettext fix

--- inc/rssfeed.class.php
+++ inc/rssfeed.class.php
@@ -674,7 +674,7 @@ class RSSFeed extends CommonDBTM {
       echo Dropdown::getYesNo($this->fields['have_error']);
       echo "</td>";
       if ($this->fields['have_error']) {
-         echo "<td>".__('Founded RSS feeds');
+         echo "<td>".__('RSS feeds found');
          echo "</td><td>";
          $this->showDiscoveredFeeds();
          echo "</td>\n";
