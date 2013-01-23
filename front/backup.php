8dba2ea372b6cea58f65b82d32157640e3cb305d
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-01-23 15:41:20 +0000

    real double confirmation on restore DB to avoid wrong actions. fixed #4171
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@19993 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- front/backup.php
+++ front/backup.php
@@ -617,9 +617,11 @@ if (count($files)) {
 
            echo "</td>";
            echo "<td>&nbsp;";
+           // Multiple confirmation
+           $string = array();
            //TRANS: %s is the filename
-           $string = array(sprintf(__('Replace the current database with the backup file %s?'), $file));
-           $string[] = __('Warning, your actual database will be totaly overwriten by the database you want to restore !!!');
+           $string[] = array(sprintf(__('Replace the current database with the backup file %s?'), $file));
+           $string[] = array(__('Warning, your actual database will be totaly overwriten by the database you want to restore !!!'));
            
            echo "<a class='vsubmit' href=\"#\" ".HTML::addConfirmationOnAction($string,
                                           "window.location='".$CFG_GLPI["root_doc"].
