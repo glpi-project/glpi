3467fa0377eadb95cfe2e8ca1a0f3c5f1a309add
yllen <yllen>
2013-02-15 17:51:06 +0000

    fix frame for plugin

--- inc/html.class.php
+++ inc/html.class.php
@@ -805,8 +805,8 @@ class Html {
 
       // Send UTF8 Headers
       header("Content-Type: text/html; charset=UTF-8");
-      // No frame to prevent click-jacking
-      header('x-frame-options:DENY');
+      // Allow only frame from same server to prevent click-jacking
+      header('x-frame-options:SAMEORIGIN');
 
       // Send extra expires header
       self::header_nocache();
