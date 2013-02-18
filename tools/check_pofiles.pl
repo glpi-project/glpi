200ffd89c4534e0c2019b31812241cab54c33626
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-02-18 11:32:04 +0000

    add check po files script
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@20204 35612d4c-9d10-0410-837c-bf9dd1be60d7

---
+++ tools/check_pofiles.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+#!/usr/bin/perl -w 
+
+if (@ARGV!=0){
+print "USAGE check_pofiles.pl\n\n";
+
+exit();
+}
+
+opendir(DIRHANDLE,'locales')||die "ERROR: can not read current directory\n"; 
+foreach (readdir(DIRHANDLE)){ 
+	if ($_ ne '..' && $_ ne '.'){
+
+            if(!(-l "$dir/$_")){
+                     if (index($_,".po",0)==length($_)-3) {
+                        $lang=$_;
+                        $lang=~s/\.po//;
+                        
+                        `msgfmt -c locales/$_`;
+                     }
+            }
+
+	}
+}
+closedir DIRHANDLE; 
+
+#  
+#  
