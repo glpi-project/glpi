13f1637030767d5c8e8dabf18017efba567e121a
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-02-26 08:29:33 +0000

    add bottom line to rule list
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@20274 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/rulecollection.class.php
+++ inc/rulecollection.class.php
@@ -434,6 +434,25 @@ class RuleCollection extends CommonDBTM {
          $this->RuleList->list[$j]->showMinimalForm($target, $i==0, $i==$nb-1, $display_entities);
          Session::addToNavigateListItems($ruletype, $this->RuleList->list[$j]->fields['id']);
       }
+
+      echo "<tr>";
+      echo "<th>";
+      if ($canedit) {
+         Html::checkAllAsCheckbox('mass'.__CLASS__.$rand);
+      }
+      echo "</th>";
+      echo "<th>".__('Name')."</th>";
+      echo "<th>".__('Description')."</th>";
+      echo "<th>".__('Active')."</th>";
+
+      if ($display_entities) {
+         echo "<th>".__('Entity')."</th>\n";
+      }
+      if (!$display_entities) {
+         echo "<th colspan='2'>&nbsp;</th>";
+      }
+      echo "</tr>\n";
+      
       echo "</table>\n";
 
       if ($canedit && $nb) {
