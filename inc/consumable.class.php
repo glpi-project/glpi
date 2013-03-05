35321c4f8b301de0efcd06ea222b23297d7fa79c
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-03-05 12:02:38 +0000

    fix cartridge / consumable link
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@20345 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/consumable.class.php
+++ inc/consumable.class.php
@@ -46,7 +46,7 @@ class Consumable extends CommonDBTM {
    // From CommonDBTM
    static protected $forward_entity_to = array('Infocom');
 
-   var $no_form_page                   = false;
+   var $no_form_page                   = true;
 
 
    /**
