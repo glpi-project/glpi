bff0ffa9b922d91abf3486ebd8c2a008d8435513
webmyster <webmyster>
2013-03-27 14:03:59 +0000

    Fix toolbar menus

--- front/networkport.form.php
+++ front/networkport.form.php
@@ -137,8 +137,7 @@ if (isset($_POST["add"])) {
       $_GET["instantiation_type"] = "";
    }
    Session::checkRight("networking", "w");
-   Html::header(NetworkPort::getTypeName(2), $_SERVER['PHP_SELF'], "inventory", 'networking',
-                'networkport');
+   Html::header(NetworkPort::getTypeName(2), $_SERVER['PHP_SELF'], 'inventory');
 
    $np->showForm($_GET["id"], $_GET);
    Html::footer();
