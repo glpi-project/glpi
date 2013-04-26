617f3329992271823bf99740d064b5f75f5bf983
yllen <yllen>
2013-04-26 07:15:37 +0000

    [0.83] fix bug for task.status - see #4329

--- inc/notificationtargetticket.class.php
+++ inc/notificationtargetticket.class.php
@@ -94,7 +94,7 @@ class NotificationTargetTicket extends NotificationTargetCommonITILObject {
    function getMessageID() {
       return "GLPI-".$this->obj->getField('id').".".time().".".rand(). "@".php_uname('n');
    }
-   
+
    function addAdditionnalInfosForTarget() {
       global $DB;
 
@@ -390,17 +390,15 @@ class NotificationTargetTicket extends NotificationTargetCommonITILObject {
             $tmp['##task.date##']        = Html::convDateTime($task['date']);
             $tmp['##task.description##'] = $task['content'];
             $tmp['##task.time##']        = Ticket::getActionTime($task['actiontime']);
-
+            $tmp['##task.status##'] = Planning::getState($task['state']);
 
             $tmp['##task.user##']   = "";
             $tmp['##task.begin##']  = "";
             $tmp['##task.end##']    = "";
-            $tmp['##task.status##'] = "";
             if (!is_null($task['begin'])) {
                $tmp['##task.user##']   = Html::clean(getUserName($task['users_id_tech']));
                $tmp['##task.begin##']  = Html::convDateTime($task['begin']);
                $tmp['##task.end##']    = Html::convDateTime($task['end']);
-               $tmp['##task.status##'] = Planning::getState($task['state']);
             }
 
             $datas['tasks'][] = $tmp;
