20f3c41f7a95277684e2583b0cc3b8ade3fc8d1c
Alexandre Delaunay <adelaunay@teclib.com>
2013-04-29 10:05:08 +0000

    report r20856

--- install/update_0831_0833.php
+++ install/update_0831_0833.php
@@ -65,29 +65,15 @@ function update0831to0833() {
    $migration->displayMessage(sprintf(__('Change of the database layout - %s'),
                                       'Compute entities information on document links')); // Updating schema
 
-   $entities    = getAllDatasFromTable('glpi_entities');
-   $entities[0] = "Root";
-
-   foreach ($entities as $entID => $val) {
-      // Non recursive ones
-      $query3 = "UPDATE `glpi_documents_items`
-                  SET `entities_id` = $entID, `is_recursive` = 0
-                  WHERE `documents_id` IN (SELECT `id`
-                                          FROM `glpi_documents`
-                                          WHERE `entities_id` = $entID
-                                                AND `is_recursive` = 0)";
-      $DB->queryOrDie($query3, "0.83 update entities_id and is_recursive=0 in glpi_documents_items");
-
-      // Recursive ones
-      $query3 = "UPDATE `glpi_documents_items`
-                  SET `entities_id` = $entID, `is_recursive` = 1
-                  WHERE `documents_id` IN (SELECT `id`
-                                          FROM `glpi_documents`
-                                          WHERE `entities_id` = $entID
-                                                AND `is_recursive` = 1)";
-      $DB->queryOrDie($query3, "0.83 update entities_id and is_recursive=1 in glpi_documents_items");
-   }
-
+   $query_doc_i = "UPDATE `glpi_documents_items` as `doc_i`
+                   INNER JOIN `glpi_documents` as `doc`
+                     ON  `doc`.`id` = `doc_i`.`documents_id`
+                   SET `doc_i`.`entities_id` = `doc`.`entities_id`, 
+                      `doc_i`.`is_recursive` = `doc`.`is_recursive`";
+   $DB->query($query_doc_i)
+      or die("0.83 update entities_id and is_recursive in glpi_documents_items ".
+               $LANG['update'][90] . $DB->error());                     
+   
    // ************ Keep it at the end **************
    $migration->displayMessage('Migration of glpi_displaypreferences');
 
