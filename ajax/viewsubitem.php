7f41f3a1a04a106b6ddbeb3408007e9dd0a51e22
tsmr <tsmr@thetsmr.fr>
2013-05-03 15:24:13 +0000

    [0.83] Drop $AJAX_INCLUDE into viewsubitem.php

--- ajax/viewsubitem.php
+++ ajax/viewsubitem.php
@@ -32,7 +32,6 @@
 // Purpose of file:
 // ----------------------------------------------------------------------
 
-$AJAX_INCLUDE = 1;
 define('GLPI_ROOT','..');
 include (GLPI_ROOT."/inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
