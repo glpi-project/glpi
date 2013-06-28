f12cfd6747e8190f682392c18ca86035009a6fa4
yllen <yllen>
2013-06-28 18:19:25 +0000

    [0.83] fix migration for autoclose_delay

--- install/update_0782_080.php
+++ install/update_0782_080.php
@@ -1628,11 +1628,20 @@ function update0782to080() {
    or die("0.80 change autoclose ticket in glpi_configs " . $LANG['update'][90] . $DB->error());
 
    $query = "UPDATE `glpi_entitydatas`
+             SET `autoclose_delay` = '-2'
+             WHERE `autoclose_delay` = '-1'";
+
+   $DB->query($query)
+   or die("0.80 change autoclose ticket in glpi_entitydatas for inherit " . $LANG['update'][90] .
+         $DB->error());
+
+   $query = "UPDATE `glpi_entitydatas`
              SET `autoclose_delay` = '-1'
              WHERE `autoclose_delay` = '0'";
 
    $DB->query($query)
-   or die("0.80 change autoclose ticket in glpi_entitydatas " . $LANG['update'][90] . $DB->error());
+   or die("0.80 change autoclose ticket in glpi_entitydatas for not active " . $LANG['update'][90] .
+          $DB->error());
 
 
    // must always be at the end
