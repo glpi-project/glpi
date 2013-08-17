da2ea944bb6b6c3d7f2293c58b78ff92ef02e72a
moyooo <moyo@indepnet.net>
2013-08-17 06:27:36 +0000

    Save reservation comment fixed #4441

--- inc/reservation.class.php
+++ inc/reservation.class.php
@@ -741,7 +741,7 @@ class Reservation extends CommonDBChild {
       }
 
       echo "<tr class='tab_bg_2'><td>".__('Comments')."</td>";
-      echo "<td><textarea name='comment'rows='8' cols='30'>".$resa->fields["comment"]."</textarea>";
+      echo "<td><textarea name='comment' rows='8' cols='30'>".$resa->fields["comment"]."</textarea>";
       echo "</td></tr>\n";
 
       if (empty($ID)) {
