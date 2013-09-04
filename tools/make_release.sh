55c8fb2e1a3af4e7e6d7f6f806acffd24fbb4e03
moyooo <moyo@indepnet.net>
2013-09-04 18:42:18 +0000

    make release for 0.84.1

--- tools/make_release.sh
+++ tools/make_release.sh
@@ -79,6 +79,7 @@ echo "Delete bigdumps and older sql files";
 \rm install/mysql/glpi-0.78*;
 \rm install/mysql/glpi-0.80*;
 \rm install/mysql/glpi-0.83*;
+\rm install/mysql/glpi-0.84-*;
 \rm install/mysql/glpi-*-default*;
 \rm install/mysql/irm*;
 
