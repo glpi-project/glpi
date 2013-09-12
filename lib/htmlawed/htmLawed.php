5f70d3d7770ed8d140910d6dcf493b72a696a9f1
remi <remi@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-09-12 14:25:18 +0000

    htmlawed: 1.1.16: fix for a potential security vulnerability arising from specialy encoded space characters in URL schemes/protocols
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@21757 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- lib/htmlawed/htmLawed.php
+++ lib/htmlawed/htmLawed.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-htmLawed 1.1.15, 11 August 2013
+htmLawed 1.1.16, 29 August 2013
 Copyright Santosh Patnaik
 Dual licensed with LGPL 3 and GPL 2+
 A PHP Labware internal utility; www.bioinformatics.org/phplabware/internal_utilities/htmLawed
@@ -336,7 +336,7 @@ $c = isset($C['schemes'][$c]) ? $C['schemes'][$c] : $C['schemes']['*'];
 static $d = 'denied:';
 if(isset($c['!']) && substr($p, 0, 7) != $d){$p = "$d$p";}
 if(isset($c['*']) or !strcspn($p, '#?;') or (substr($p, 0, 7) == $d)){return "{$b}{$p}{$a}";} // All ok, frag, query, param
-if(preg_match('`^([a-z\d\-+.&#; ]+?)(:|&#(58|x3a);|%3a|\\\\0{0,4}3a).`i', $p, $m) && !isset($c[strtolower($m[1])])){ // Denied prot
+if(preg_match('`^([^:?[@!$()*,=/\'\]]+?)(:|&#(58|x3a);|%3a|\\\\0{0,4}3a).`i', $p, $m) && !isset($c[strtolower($m[1])])){ // Denied prot
  return "{$b}{$d}{$p}{$a}";
 }
 if($C['abs_url']){
@@ -698,7 +698,7 @@ return str_replace(array("\x01", "\x02", "\x03", "\x04", "\x05", "\x07"), array(
 
 function hl_version(){
 // rel
-return '1.1.15';
+return '1.1.16';
 // eof
 }
 
