dde17707c97d23e0037bcaa1a548c0ee0a6228df
moyooo <moyo@indepnet.net>
2013-10-02 05:35:30 +0000

    tru to fix see #4557

--- inc/authldap.class.php
+++ inc/authldap.class.php
@@ -1477,7 +1477,7 @@ class AuthLDAP extends CommonDBTM {
       }
 
       $glpi_users = array();
-      $sql        = "SELECT `id`, `name`, `date_sync`, `user_dn`
+      $sql        = "SELECT *
                      FROM `glpi_users`";
 
       if ($values['mode'] != self::ACTION_IMPORT) {
