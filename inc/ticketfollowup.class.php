815b029aa43678cf1e59d39e389dd0519d10c030
yllen <yllen>
2013-11-12 17:44:18 +0000

    [0.84] fix approbation of solution for post-only - see #4660

--- inc/ticketfollowup.class.php
+++ inc/ticketfollowup.class.php
@@ -711,7 +711,9 @@ class TicketFollowup  extends CommonDBTM {
 
       if (($ticket->fields["status"] == CommonITILObject::SOLVED)
           && $ticket->canApprove()
-          && $ticket->isAllowedStatus($ticket->fields['status'], Ticket::CLOSED)) {
+          && ($ticket->isAllowedStatus($ticket->fields['status'], Ticket::CLOSED)
+              || ($_SESSION['glpiactiveprofile']['interface'] == 'helpdesk'))) {
+
          echo "<form name='form' method='post' action='".$this->getFormURL()."'>";
          echo "<table class='tab_cadre_fixe'>";
          echo "<tr><th colspan='4'>". __('Approval of the solution')."</th></tr>";
