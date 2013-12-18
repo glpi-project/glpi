d7b8d4dc0b80d9da7dbc7a42b1883ed70033a221
moyooo <moyo@indepnet.net>
2013-12-18 11:20:12 +0000

    begin work on project tag for notif see #4562

--- inc/notificationtargetproject.class.php
+++ inc/notificationtargetproject.class.php
@@ -213,6 +213,250 @@ class NotificationTargetProject extends NotificationTarget {
                                             'usertype' => NotificationTarget::ANONYMOUS_USER));
          }
       }
-   }    
+   }
+
+   /**
+    * @see NotificationTarget::getDatasForTemplate()
+   **/
+   function getDatasForTemplate($event, $options=array()) {
+      //----------- Reservation infos -------------- //
+      $events                                  = $this->getAllEvents();
+
+      $this->datas['##project.action##']   = $events[$event];
+      $this->datas['##project.url##']      = $this->formatURL($options['additionnaloption']['usertype'],
+                                              "Project_".$item->getField("id"));
+      $datas["##project.name##"]           = $item->getField('name');
+      $datas["##project.code##"]           = $item->getField('code');
+      $datas["##project.description##"]    = $item->getField('content');
+      $datas["##project.comments##"]       = $item->getField('comment');
+      $datas["##project.creationdate##"]   = Html::convDateTime($item->getField('date'));
+      $datas["##project.lastupdatedate##"] = Html::convDateTime($item->getField('date_mod'));
+      $datas["##project.priority##"]       = CommonITILObject::getPriorityName($item->getField('priority'));
+      $datas["##project.percent##"]        = Dropdown::getValueWithUnit($item->getField('percent_done'),"%");
+      $datas["##project.planstartdate##"]  = Html::convDateTime($item->getField('plan_start_date'));
+      $datas["##project.planenddate##"]    = Html::convDateTime($item->getField('plan_end_date'));
+      $datas["##project.realstartdate##"]  = Html::convDateTime($item->getField('real_start_date'));
+      $datas["##project.realenddate##"]    = Html::convDateTime($item->getField('real_end_date'));
+
+      $datas["##project.plannedduration##"]   = Html::timestampToString(
+                                                   ProjectTask::getTotalPlannedDurationForProject($item->getID()),
+                                                   false);
+      $datas["##project.effectiveduration##"] = Html::timestampToString(
+                                                   ProjectTask::getTotalEffectiveDurationForProject($item->getID()),
+                                                   false);
+
+      $datas["##project.father##"] = '';
+      if ($item->getField('projects_id')) {
+         $datas["##project.father##"]
+                              = Dropdown::getDropdownName('glpi_projects',
+                                                          $item->getField('projects_id'));
+      }
+
+      $datas["##project.state##"] = '';
+      if ($item->getField('projectstates_id')) {
+         $datas["##project.state##"]
+                              = Dropdown::getDropdownName('glpi_projectstates',
+                                                          $item->getField('projectstates_id'));
+      }
+
+      $datas["##project.type##"] = '';
+      if ($item->getField('projecttypes_id')) {
+         $datas["##project.type##"]
+                              = Dropdown::getDropdownName('glpi_projecttypes',
+                                                          $item->getField('projecttypes_id'));
+      }
+
+      $datas["##project.manager##"] = '';
+      if ($item->getField('users_id')) {
+         $user_tmp = new User();
+         $user_tmp->getFromDB($item->getField('users_id'));
+         $datas["##project.manager##"] = $user_tmp->getName();
+      }
+
+      $datas["##project.managergroup##"] = '';
+      if ($item->getField('groups_id')) {
+         $datas["##project.managergroup##"]
+                              = Dropdown::getDropdownName('glpi_groups',
+                                                          $item->getField('groups_id'));
+      }
+
+
+      //Task infos
+      $restrict = "`projects_id`='".$item->getField('id')."'";
+      $restrict .= " ORDER BY `date` DESC, `id` ASC";
+
+      $tasks          = getAllDatasFromTable('glpi_projecttasks',$restrict);
+      $datas['tasks'] = array();
+      foreach ($tasks as $task) {
+         $tmp                            = array();
+         $tmp['##task.creationdate##']   = Html::convDateTime($task['date']);
+         $tmp['##task.lastupdatedate##'] = Html::convDateTime($task['date_mod']);
+         $tmp['##task.name##']           = $task['name'];
+         $tmp['##task.description##']    = $task['content'];
+         $tmp['##task.comments##']       = $task['comment'];
+
+         $tmp['##task.state##']          = Dropdown::getDropdownName('glpi_projectstates',
+                                                                     $task['projectstates_id']);
+         $tmp['##task.type##']           = Dropdown::getDropdownName('glpi_projecttasktypes',
+                                                                     $task['projecttasktypes_id']);
+         $tmp['##task.percent##']        = Dropdown::getValueWithUnit($task['percent_done'],"%");
+
+         $datas["##task.planstartdate##"]  = '';
+         $datas["##task.planenddate##"]    = '';
+         $datas["##task.realstartdate##"]  = '';
+         $datas["##task.realenddate##"]    = '';
+         if (!is_null($task['plan_start_date'])) {
+            $tmp['##task.planstartdate##']     = Html::convDateTime($task['plan_start_date']);
+         }
+         if (!is_null($task['plan_end_date'])) {
+            $tmp['##task.planenddate##']     = Html::convDateTime($task['plan_end_date']);
+         }
+         if (!is_null($task['real_start_date'])) {
+            $tmp['##task.realstartdate##']     = Html::convDateTime($task['real_start_date']);
+         }
+         if (!is_null($task['real_end_date'])) {
+            $tmp['##task.realenddate##']     = Html::convDateTime($task['real_end_date']);
+         }
+
+         $datas['tasks'][]             = $tmp;
+      }
+
+      $datas["##project.numberoftasks##"] = count($datas['tasks']);
+
+      //costs infos
+      $restrict  = "`projects_id`='".$item->getField('id')."'";
+      $restrict .= " ORDER BY `begin_date` DESC, `id` ASC";
+
+      $costs          = getAllDatasFromTable('glpi_projectcosts',$restrict);
+      $datas['costs'] = array();
+      $datas["##project.totalcost##"] = 0;
+      foreach ($costs as $cost) {
+         $tmp = array();
+         $tmp['##cost.name##']         = $cost['name'];
+         $tmp['##cost.comment##']      = $cost['comment'];
+         $tmp['##cost.datebegin##']    = Html::convDate($cost['begin_date']);
+         $tmp['##cost.dateend##']      = Html::convDate($cost['end_date']);
+         $tmp['##cost.cost##']         = Html::formatNumber($cost['cost']);
+         $tmp['##cost.budget##']       = Dropdown::getDropdownName('glpi_budgets',
+                                                                     $cost['budgets_id']);
+         $datas["##project.totalcost##"] += $cost['cost'];
+         $datas['costs'][]             = $tmp;
+
+         /// TODO add ticket costs ?
+      }
+      $datas["##project.numberofcosts##"] = count($datas['costs']);
+
+      $datas['log'] = array();
+      // Use list_limit_max or load the full history ?
+      foreach (Log::getHistoryData($item, 0, $CFG_GLPI['list_limit_max']) as $data) {
+         $tmp                               = array();
+         $tmp["##project.log.date##"]    = $data['date_mod'];
+         $tmp["##project.log.user##"]    = $data['user_name'];
+         $tmp["##project.log.field##"]   = $data['field'];
+         $tmp["##project.log.content##"] = $data['change'];
+         $datas['log'][]                    = $tmp;
+      }
+
+      $datas["##project.numberoflogs##"] = count($datas['log']);
+
+      /// TODO Team
+      /// TODO Change
+      /// TODO Items
+      /// TODO document
+      /// TODO contrats
+      
+      
+      $this->getTags();
+      foreach ($this->tag_descriptions[NotificationTarget::TAG_LANGUAGE] as $tag => $values) {
+         if (!isset($this->datas[$tag])) {
+            $this->datas[$tag] = $values['label'];
+         }
+      }
+   }
+
+
+   function getTags() {
+
+      $tags_all = array('project.url'               => __('URL'),
+                        'project.action'            => _n('Event', 'Events', 1),
+                        'project.name'              => __('Name'),
+                        'project.code'              => __('Code'),
+                        'project.description'       => __('Description'),
+                        'project.comments'          => __('Comments'),
+                        'project.creationdate'      => __('Creation date'),
+                        'project.lastupdatedate'    => __('Last update'),
+                        'project.planstartdate'     => __('Planned start date'),
+                        'project.planenddate'       => __('Planned end date'),
+                        'project.realstartdate'     => __('Real start date'),
+                        'project.realenddate'       => __('Real end date'),
+                        'project.priority'          => __('Priority'),
+                        'project.father'            => __('Father'),
+                        'project.manager'           => __('Manager'),
+                        'project.managergroup'      => __('Manager group'),
+                        'project.type'              => __('Type'),
+                        'project.state'             => _x('item', 'State'),
+                        'project.percent'           => __('Percent done'),
+                        'project.plannedduration'   => __('Planned duration'),
+                        'project.effectiveduration' => __('Effective duration'),
+                        'project.numberoftasks'     => _x('quantity', 'Number of tasks'),
+                        'task.date'                 => __('Opening date'),
+                        'task.name'                 => __('Name'),
+                        'task.description'          => __('Description'),
+                        'task.comments'             => __('Comments'),
+                        'task.creationdate'         => __('Creation date'),
+                        'task.lastupdatedate'       => __('Last update'),
+                        'task.type'                 => __('Type'),
+                        'task.state'                => _x('item', 'State'),
+                        'task.percent'              => __('Percent done'),
+                        'task.planstartdate'        => __('Planned start date'),
+                        'task.planenddate'          => __('Planned end date'),
+                        'task.realstartdate'        => __('Real start date'),
+                        'task.realenddate'          => __('Real end date'),
+                        'cost.name'                 => sprintf(__('%1$s: %2$s'), __('Cost'),
+                                                                        __('Name')),
+                        'cost.comment'              => sprintf(__('%1$s: %2$s'), __('Cost'),
+                                                                        __('Comments')),
+                        'cost.datebegin'            => sprintf(__('%1$s: %2$s'), __('Cost'),
+                                                                        __('Begin date')),
+                        'cost.dateend'              => sprintf(__('%1$s: %2$s'), __('Cost'),
+                                                                        __('End date')),
+                        'cost.cost'                 => __('Cost'),
+                        'cost.budget'               => sprintf(__('%1$s: %2$s'), __('Cost'),
+                                                                        __('Budget')),
+                        'project.totalcost'             => __('Total cost'),
+                        'project.numberofcosts'         => __('Number of costs'),
+                        'project.numberoflogs'   => sprintf(__('%1$s: %2$s'), __('Historical'),
+                                                                  _x('quantity', 'Number of items')),
+                        'project.log.date'       => sprintf(__('%1$s: %2$s'), __('Historical'),
+                                                                  __('Date')),
+                        'project.log.user'       => sprintf(__('%1$s: %2$s'), __('Historical'),
+                                                                  __('User')),
+                        'project.log.field'      => sprintf(__('%1$s: %2$s'), __('Historical'),
+                                                                  __('Field')),
+                        'project.log.content'    => sprintf(__('%1$s: %2$s'), __('Historical'),
+                                                                  _x('name', 'Update')),
+
+                        );
+
+      foreach ($tags_all as $tag => $label) {
+         $this->addTagToList(array('tag'   => $tag,
+                                   'label' => $label,
+                                   'value' => true));
+      }
+
+
+      //Foreach global tags
+      $tags = array('log'      => __('Historical'),
+                    'tasks'    => _n('Task', 'Tasks', 2),
+                    'costs'    => _n('Cost', 'Costs', 2));
+
+      foreach ($tags as $tag => $label) {
+         $this->addTagToList(array('tag'     => $tag,
+                                   'label'   => $label,
+                                   'value'   => false,
+                                   'foreach' => true));
+      }      
+      asort($this->tag_descriptions);
+   }
 }
 ?>
