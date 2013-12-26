1536fc8ae1417d507a084c588991420b6cdc266f
yllen <yllen>
2013-12-26 18:57:45 +0000

    [0.84] clean TODO

--- inc/contract.class.php
+++ inc/contract.class.php
@@ -1391,8 +1391,6 @@ class Contract extends CommonDBTM {
    static function getContractEndDate($id, $begin, $duration, $periodicity, $renewal) {
       global $DB;
 
-      /// TODO Need to use date firmat of the user for display
-
       $date = new DateTime($begin);
       $now  = new DateTime("now");
       $date->add(new DateInterval('P'.$duration.'M'));
@@ -1402,7 +1400,7 @@ class Contract extends CommonDBTM {
          }
       }
       if ($date < $now) {
-         return "<span class='red'>".$date->format('d-m-Y')."</span>";
+         return "<span class='red'>".Html::convDate($date->format('d-m-Y'))."</span>";
       }
 
       return  $date->format('d-m-Y');
