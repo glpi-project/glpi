74c59510568ec276dd87e09cb21524a0dff91ac2
Alexandre Delaunay <adelaunay@teclib.com>
2014-01-08 14:28:11 +0000

    [0.84]fix dbslave configform; see #4748

--- inc/config.class.php
+++ inc/config.class.php
@@ -534,7 +534,7 @@ class Config extends CommonDBTM {
       echo "<td><input type='text' name='_dbreplicate_dbuser' value='".$DBslave->dbuser."'></td>";
       echo "<td>" . __('Mysql password') . "</td>";
       echo "<td><input type='password' name='_dbreplicate_dbpassword' value='".
-                 $DBslave->dbpassword."'>";
+                 rawurldecode($DBSlave->dbpassword)."'>";
       echo "</td></tr>";
 
       echo "<tr class='tab_bg_2'>";
