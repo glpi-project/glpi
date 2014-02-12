24b594d04080025e29cfccb7ab04ef41eed7cb30
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2014-02-12 16:15:25 +0000

    update headers
    git-svn-id: https://forge.indepnet.net/svn/glpi/trunk@22656 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/backup.class.php
+++ inc/backup.class.php
@@ -3,7 +3,7 @@
  * @version $Id$
  -------------------------------------------------------------------------
  GLPI - Gestionnaire Libre de Parc Informatique
- Copyright (C) 2003-2013 by the INDEPNET Development Team.
+ Copyright (C) 2003-2014 by the INDEPNET Development Team.
 
  http://indepnet.net/   http://glpi-project.org
  -------------------------------------------------------------------------
@@ -23,7 +23,7 @@
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with GLPI. If not, see <http://www.gnu.org/licenses/.
+ along with GLPI. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  */
 
