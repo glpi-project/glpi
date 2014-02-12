b3e78a10c96d975caaac3298e040c3cbf4ea5ebe
moyooo <moyo@indepnet.net>
2014-02-12 11:57:59 +0000

    clean notepad search option index see #4519

--- inc/entity.class.php
+++ inc/entity.class.php
@@ -513,10 +513,10 @@ class Entity extends CommonTreeDropdown {
       $tab[16]['name']          = __('Comments');
       $tab[16]['datatype']      = 'text';
 
-      $tab[28]['table']         = $this->getTable();
-      $tab[28]['field']         = 'notepad';
-      $tab[28]['name']          = __('Notes');
-      $tab[28]['datatype']      = 'text';
+      $tab[90]['table']         = $this->getTable();
+      $tab[90]['field']         = 'notepad';
+      $tab[90]['name']          = __('Notes');
+      $tab[90]['datatype']      = 'text';
 
       $tab['advanced']         = __('Advanced information');
 
