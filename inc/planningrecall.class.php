1f1daf88107620f44dc8688f2ddb2a0336c7db37
moyooo <moyo@indepnet.net>
2014-04-09 13:00:46 +0000

    PlanningRecall when item does not exists see #4862

--- inc/planningrecall.class.php
+++ inc/planningrecall.class.php
@@ -363,7 +363,7 @@ class PlanningRecall extends CommonDBChild {
 
       $pr = new self();
       foreach ($DB->request($query) as $data) {
-         if ($pr->getFromDB($data['id'])) {
+         if ($pr->getFromDB($data['id']) && $pr->getItem()) {
             if (NotificationEvent::raiseEvent('planningrecall', $pr)) {
 
                $cron_status         = 1;
@@ -375,6 +375,9 @@ class PlanningRecall extends CommonDBChild {
 
                $alert->add($input);
             }
+         } else {
+            // Clean item
+            $pr->delete($data);
          }
       }
       return $cron_status;
