d7ab58fc0445a8d4602191250ce3399210a40698
yllen <yllen>
2014-04-17 12:22:22 +0000

    [0.84] can't download xml file - see #4876

--- front/backup.php
+++ front/backup.php
@@ -655,14 +655,17 @@ if (count($files)) {
            "<tr class='tab_bg_2'><td>$file&nbsp;</td>".
             "<td class='right'>".Toolbox::getSize($taille_fic)."</td>".
             "<td>&nbsp;" . Html::convDateTime(date("Y-m-d H:i",$date)) . "</td>".
-            "<td colspan=3>&nbsp;";
+            "<td>&nbsp;";
             //TRANS: %s is the filename
             $string = sprintf(__('Delete the file %s?'),$file);
             Html::showSimpleForm($_SERVER['PHP_SELF'], 'delfile', _x('button', 'Delete permanently'),
                                  array('file' => $file),'','',$string);
+            echo "<td>&nbsp;";
+            echo "<td>&nbsp;<a class='vsubmit' href=\"document.send.php?file=_dumps/$file\">".
+                            __('Download')."</a>".
+            "</td>";
             "<td>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;</td>".
-            "<td>&nbsp;<a href=\"document.send.php?file=_dumps/$file\">".__('Download')."</a>".
-            "</td></tr>";
+            "</tr>";
    }
 }
 closedir($dir);
