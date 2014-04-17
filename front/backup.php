b64abb5787d5fbf357cd49b829477b526db3e47d
yllen <yllen>
2014-04-17 12:12:27 +0000

    [0.84] can't delete xml file - see #4876

--- front/backup.php
+++ front/backup.php
@@ -655,13 +655,11 @@ if (count($files)) {
            "<tr class='tab_bg_2'><td>$file&nbsp;</td>".
             "<td class='right'>".Toolbox::getSize($taille_fic)."</td>".
             "<td>&nbsp;" . Html::convDateTime(date("Y-m-d H:i",$date)) . "</td>".
-            "<td>&nbsp;";
+            "<td colspan=3>&nbsp;";
             //TRANS: %s is the filename
             $string = sprintf(__('Delete the file %s?'),$file);
-           echo "<a href=\"#\" ".HTML::addConfirmationOnAction($string,
-                     "window.location='".$CFG_GLPI["root_doc"]."/front/backup.php?delfile=$file'").
-                ">"._x('button', 'Delete permanently')."</a>&nbsp;</td>";
-
+            Html::showSimpleForm($_SERVER['PHP_SELF'], 'delfile', _x('button', 'Delete permanently'),
+                                 array('file' => $file),'','',$string);
             "<td>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;</td>".
             "<td>&nbsp;<a href=\"document.send.php?file=_dumps/$file\">".__('Download')."</a>".
             "</td></tr>";
