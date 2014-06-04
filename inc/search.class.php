4aa59969aa33a2bda10c8fcd7b5d7f17b605b584
David Durieux <d.durieux@siprossii.com>
2014-06-04 23:11:28 +0000

    Add argument searchtype to _addWhere hook for plugins. references #4951

--- inc/search.class.php
+++ inc/search.class.php
@@ -2660,7 +2660,7 @@ class Search {
       if ($plug = isPluginItemType($itemtype)) {
          $function = 'plugin_'.$plug['plugin'].'_addWhere';
          if (function_exists($function)) {
-            $out = $function($link,$nott,$itemtype,$ID,$val);
+            $out = $function($link, $nott, $itemtype, $ID, $val, $searchtype);
             if (!empty($out)) {
                return $out;
             }
@@ -2920,7 +2920,7 @@ class Search {
             $plug     = $matches[1];
             $function = 'plugin_'.$plug.'_addWhere';
             if (function_exists($function)) {
-               $out = $function($link, $nott, $itemtype, $ID, $val);
+               $out = $function($link, $nott, $itemtype, $ID, $val, $searchtype);
                if (!empty($out)) {
                   return $out;
                }
