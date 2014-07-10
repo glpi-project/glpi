989acb99a46fcceddf4371df779197d2850daab4
moyooo <moyo@indepnet.net>
2014-07-10 17:12:01 +0000

    fix notif tag check

--- front/notification.tags.php
+++ front/notification.tags.php
@@ -38,7 +38,7 @@ if (!defined('GLPI_ROOT')) {
 Html::popHeader(__('List of available tags'), $_SERVER['PHP_SELF']);
 
 if (isset($_GET["sub_type"])) {
-   Session::checkRight("config", READ);
+   Session::checkCentralAccess();
    NotificationTemplateTranslation::showAvailableTags($_GET["sub_type"]);
    Html::ajaxFooter();
 } else {
