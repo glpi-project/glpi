c728e2971f68e1b394481eae0106ba2306fe9d8e
moyooo <moyo@indepnet.net>
2014-07-18 12:34:37 +0000

    [0.84] unwanted notification on update ticket (triggered on save button in self-service) fixed #5015

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -2995,7 +2995,7 @@ class Ticket extends CommonITILObject {
       $rand = mt_rand();
 
       if ($_SESSION["glpiactiveprofile"]["helpdesk_hardware"] == 0) {
-         echo "<input type='hidden' name='$myname' value='0'>";
+         echo "<input type='hidden' name='$myname' value=''>";
          echo "<input type='hidden' name='items_id' value='0'>";
 
       } else {
