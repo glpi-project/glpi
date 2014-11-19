c01320f967167ee8185f501b3a583ba25b45e3f0
moyooo <moyo@indepnet.net>
2014-11-19 12:31:22 +0000

    Datepicker - Today trouble fixed #5130

--- script.js
+++ script.js
@@ -754,3 +754,13 @@ $(function(){
         });
 
 });
+
+//Hack for Jquery Ui Date picker
+var _gotoToday = jQuery.datepicker._gotoToday;
+jQuery.datepicker._gotoToday = function(a){
+   var target = jQuery(a);
+   var inst = this._getInst(target[0]);
+   _gotoToday.call(this, a);
+   jQuery.datepicker._selectDate(a, jQuery.datepicker._formatDate(inst,inst.selectedDay, inst.selectedMonth, inst.selectedYear));
+};
+
