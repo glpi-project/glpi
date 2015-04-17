dcf1306ba71b2c50549764809f9c94e54ac7f6e1
yllen <yllen>
2015-04-17 10:44:51 +0000

    minot fix

--- front/stat.location.php
+++ front/stat.location.php
@@ -80,7 +80,7 @@ echo "<form method='get' name='form' action='stat.location.php'>";
 echo "<input type='hidden' name='itemtype' value='". $_GET['itemtype'] ."'>";
 
 echo "<table class='tab_cadre_fixe' ><tr class='tab_bg_2'><td rowspan='2' width='30%'>";
-$values = array(_sn('Dropdown','Dropdowns',2) => array('ComputerType'    => __('Type'),
+$values = array(_n('Dropdown','Dropdowns',2) => array('ComputerType'    => __('Type'),
                                                        'ComputerModel'   => __('Model'),
                                                        'OperatingSystem' => __('Operating system'),
                                                        'Location'        => __('Location')),
