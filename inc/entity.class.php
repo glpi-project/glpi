51d57f46dbe767dbf2cbc00d44f576b1a23a3ac2
yllen <nini.lasson@orange.fr>
2015-07-31 16:43:35 +0200

    minor fix

--- inc/entity.class.php
+++ inc/entity.class.php
@@ -17,7 +17,7 @@
  This file is part of GLPI.
 
  GLPI is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by$tab[61]
+ it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
