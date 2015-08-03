ae57d8d6dc6535b56668c8b656423ad52b081ac6
Valentin Collet <valcollet@hotmail.fr>
2015-08-03 11:35:12 +0200

    covers getItemForItemtype, getPlural, getSingular

--- tests/DbFunctionTest.php
+++ tests/DbFunctionTest.php
@@ -28,6 +28,7 @@
 
 /* Test for inc/db.function.php */
 
+
 class DbFunctionTest extends PHPUnit_Framework_TestCase {
 
    protected function setUp() {
@@ -39,6 +40,7 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
       require_once 'fixtures/pluginfoobar.php';
    }
 
+
    public function dataTableKey() {
       return array(
          array('foo', ''),
@@ -48,6 +50,7 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
       );
    }
 
+
    /**
     * @covers getForeignKeyFieldForTable
     * @dataProvider dataTableKey
@@ -55,6 +58,7 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
    public function testGetForeignKeyFieldForTable($table, $key) {
       $this->assertEquals($key, getForeignKeyFieldForTable($table));
    }
+
 
    /**
     * @covers isForeignKeyField
@@ -66,15 +70,18 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
          $this->assertTrue(isForeignKeyField($key));
       }
    }
+
+
    /**
     * @covers isForeignKeyField
     *
     */
    public function testIsForeignKeyFieldMore() {
-      $this->assertFalse(isForeignKeyField("FakeId"));
-      $this->assertFalse(isForeignKeyField("id_Another_Fake_Id"));
-      $this->assertTrue(isForeignKeyField("users_id_tech"));
+      $this->assertFalse(isForeignKeyField('FakeId'));
+      $this->assertFalse(isForeignKeyField('id_Another_Fake_Id'));
+      $this->assertTrue(isForeignKeyField('users_id_tech'));
    }
+
 
    /**
     * @covers getTableNameForForeignKeyField
@@ -86,6 +93,7 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
       }
    }
 
+
    public function dataTableType() {
       return array(
          array('glpi_computers', 'Computer', true),
@@ -95,6 +103,7 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
       );
    }
 
+
    /**
     * @covers getTableForItemType
     * @dataProvider dataTableType
@@ -102,6 +111,7 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
    public function testGetTableForItemType($table, $type, $classexists) {
       $this->assertEquals($table, getTableForItemType($type));
    }
+
 
    /**
     * @covers getItemTypeForTable
@@ -113,5 +123,54 @@ class DbFunctionTest extends PHPUnit_Framework_TestCase {
       } else {
          $this->assertEquals('UNKNOWN', getItemTypeForTable($table));
       }
+   }
+
+
+   /**
+    * @covers getItemForItemtype
+    * @dataProvider dataTableType
+    */
+   public function testGetItemForItemtype($table, $itemtype, $classexists) {
+      if ($classexists) {
+         $this->assertInstanceOf($itemtype, getItemForItemtype($itemtype));
+      } else {
+         $this->assertFalse(getItemForItemtype($itemtype));
+      }
+   }
+
+
+   public function dataPlural() {
+      return array(
+         array('model', 'models'),
+         array('address', 'addresses'),
+         array('computer', 'computers'),
+         array('thing', 'things'),
+         array('criteria', 'criterias'),
+         array('version', 'versions'),
+         array('config', 'configs'),
+         array('machine', 'machines'),
+         array('memory', 'memories'),
+         array('licence', 'licences'),
+      );
+   }
+
+
+    /**
+    * @covers getPlural
+    * @dataProvider dataPlural
+    */
+   public function testGetPlural($singular, $plural) {
+      $this->assertEquals($plural, getPlural($singular));
+      $this->assertEquals($plural, getPlural(getPlural($singular)));
+   }
+
+
+   /**
+    * @covers getSingular
+    * @dataProvider dataPlural
+    */
+   public function testGetSingular($singular, $plural) {
+      $this->assertEquals($singular, getSingular($plural));
+      $this->assertEquals($singular, getSingular(getSingular($plural)));
    }
 }
