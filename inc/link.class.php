bdbd4fcfc721d1f21dfebc94f8883b3fbf639e22
yllen <nini.lasson@orange.fr>
2015-08-14 17:18:03 +0200

    [0.85] fix data in link column - see #44

--- inc/link.class.php
+++ inc/link.class.php
@@ -532,7 +532,9 @@ class Link extends CommonDBTM {
       $tab[145]['joinparams']        = array('beforejoin'
                                               => array('table'      => 'glpi_links_itemtypes',
                                                        'joinparams' => array('jointype'
-                                                                              => 'itemtypeonly')));
+                                                                              => 'itemtypeonly')),
+                                             'condition'
+                                              => getEntitiesRestrictRequest('AND', 'NEWTABLE'));
 
       return $tab;
    }
