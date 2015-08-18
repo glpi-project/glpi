0fec7c79729198d6a224163b441ddf87cf32bf56
Alexandre Delaunay <adelaunay@teclib.com>
2015-08-18 11:19:40 +0200

    link to sla from ticket now checks sla right instead of config right; fix #72

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -3627,7 +3627,7 @@ class Ticket extends CommonITILObject {
                }
             }
             $slaoptions = array();
-            if (Session::haveRight('config', READ)) {
+            if (Session::haveRight('sla', READ)) {
                $slaoptions['link'] = Toolbox::getItemTypeFormURL('SLA').
                                           "?id=".$this->fields["slas_id"];
             }
