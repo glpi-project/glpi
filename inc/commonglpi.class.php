glpi-project/glpi

commit 37689bffc317514b6082daffd2de997d6a527733

fix margin on new object form with lefttab layout

inc/commonglpi.class.php
@@ -706,8 +706,10 @@ class CommonGLPI {
                               'params' => "_target=$target&amp;_itemtype=".$this->getType().
                                           "&amp;_glpi_tab=-1&amp;id=$ID$extraparamhtml");
          }
+         echo "<div class='".($this->isNewID($ID)?"new_form_tabs":"")."'>";
          Ajax::createTabs('tabspanel', 'tabcontent', $tabs, $this->getType(), $ID,
                           $this->taborientation);
+         echo "</div>";
       }
    }
 

css/styles.css
@@ -549,6 +549,10 @@ input.save {
    padding: 8px 0;
 }
 
+.layout_lefttab #page .new_form_tabs .ui-tabs-panel {
+   margin: 0;
+}
+
 
 /* Vertical split layout */
 @media screen and (min-width: 1250px) {

commit 078ae7b1479796aa14046702d3c1d273efb3667e

fix vsplit layout (regression in 37689bf)
missing change in previous commit

inc/commonglpi.class.php
@@ -677,6 +677,7 @@ class CommonGLPI {
          $extraparamhtml = "&amp;".Toolbox::append_params($cleaned_options,'&amp;');
          $extraparam     = "&".Toolbox::append_params($cleaned_options);
       }
+      echo "<div class='glpi_tabs ".($this->isNewID($ID)?"new_form_tabs":"")."'>";
       echo "<div id='tabspanel' class='center-h'></div>";
       $current_tab = 0;
       $onglets     = $this->defineAllTabs($options);
@@ -706,11 +707,11 @@ class CommonGLPI {
                               'params' => "_target=$target&amp;_itemtype=".$this->getType().
                                           "&amp;_glpi_tab=-1&amp;id=$ID$extraparamhtml");
          }
-         echo "<div class='".($this->isNewID($ID)?"new_form_tabs":"")."'>";
+
          Ajax::createTabs('tabspanel', 'tabcontent', $tabs, $this->getType(), $ID,
                           $this->taborientation);
-         echo "</div>";
       }
+      echo "</div>";
    }
 
 