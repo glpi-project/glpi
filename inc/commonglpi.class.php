1c0f560164b0bf346f6ac04166061d007533d454
btry <tbugier@teclib.com>
2015-09-24 15:49:19 +0200

    pick 572b9e78ad926d66b9737f7bf43c22a0a242e8b1

--- inc/commonglpi.class.php
+++ inc/commonglpi.class.php
@@ -1017,7 +1017,7 @@ class CommonGLPI {
 
       // in case of lefttab layout, we couldn't see "right error" message
       if ($this->get_item_to_display_tab) {
-         if (isset($_GET["id"]) && !$this->can($_GET["id"], READ)) {
+         if (isset($_GET["id"]) && $_GET["id"] && !$this->can($_GET["id"], READ)) {
             html::displayRightError();
          }
       }
