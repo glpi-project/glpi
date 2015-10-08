e19de265c3744fb9b1c1dcf6ac5457a3bafc5a3a
yllen <nini.lasson@orange.fr>
2015-10-08 18:30:08 +0200

    fix translation - fixed #128

--- inc/reservationitem.class.php
+++ inc/reservationitem.class.php
@@ -9,7 +9,7 @@
 
  based on GLPI - Gestionnaire Libre de Parc Informatique
  Copyright (C) 2003-2014 by the INDEPNET Development Team.
- 
+
  -------------------------------------------------------------------------
 
  LICENSE
@@ -493,7 +493,7 @@ class ReservationItem extends CommonDBChild {
                           `$itemtable`.`name` AS name,
                           `$itemtable`.`entities_id` AS entities_id,
                           $otherserial,
-                          `glpi_locations`.`completename` AS location,
+                          `glpi_locations`.`id` AS location,
                           `glpi_reservationitems`.`items_id` AS items_id
                    FROM `glpi_reservationitems`
                    $left
@@ -529,7 +529,7 @@ class ReservationItem extends CommonDBChild {
                }
                echo "<td><a href='reservation.php?reservationitems_id=".$row['id']."'>".
                           sprintf(__('%1$s - %2$s'), $typename, $row["name"])."</a></td>";
-               echo "<td>".$row["location"]."</td>";
+               echo "<td>".Dropdown::getDropdownName("glpi_locations", $row["location"])."</td>";
                echo "<td>".nl2br($row["comment"])."</td>";
                if ($showentity) {
                   echo "<td>".Dropdown::getDropdownName("glpi_entities", $row["entities_id"]).
