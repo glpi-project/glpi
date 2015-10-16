59d73fd2192007c8d31f2272572ca50f87f07b80
yllen <nini.lasson@orange.fr>
2015-10-16 15:15:42 +0200

    fix unchecking case in profile - fixed #180

--- inc/profileright.class.php
+++ inc/profileright.class.php
@@ -240,7 +240,7 @@ class ProfileRight extends CommonDBChild {
    function updateProfileRights($profiles_id, array $rights=array()) {
 
       foreach ($rights as $name => $right) {
-         if (isset($right) && $right) {
+         if (isset($right)) {
             if ($this->getFromDBByQuery("WHERE `profiles_id` = '$profiles_id'
                                                AND `name` = '$name'")) {
 
