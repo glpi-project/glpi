0db2511dd77358a2ad2105e7e3ee4e14a17a2398
yllen <nini.lasson@orange.fr>
2015-11-13 17:59:47 +0100

    [0.85]fix error - see #89

--- inc/fieldunicity.class.php
+++ inc/fieldunicity.class.php
@@ -589,13 +589,16 @@ class FieldUnicity extends CommonDropdown {
    **/
    function showDebug() {
 
-      $params = array('message'     => '',
+      $params = array(//'message'     => '',
                       'action_type' => true,
                       'action_user' => getUserName(Session::getLoginUserID()),
                       'entities_id' => $_SESSION['glpiactive_entity'],
                       'itemtype'    => get_class($this),
                       'date'        => $_SESSION['glpi_currenttime'],
-                      'refuse'      => true);
+                      'refuse'      => true,
+                      'label'       => array('name' => 'test'),
+                      'field'       => array('action_refuse' => true),
+                      'double'      => array());
 
       NotificationEvent::debugEvent($this, $params);
    }
