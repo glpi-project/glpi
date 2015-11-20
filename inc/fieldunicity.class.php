6f82b28bd882c0ca2c29ecb0570a8ec9609f942f
yllen <nini.lasson@orange.fr>
2015-11-20 17:55:53 +0100

    fix traduction - fixed #89

--- inc/fieldunicity.class.php
+++ inc/fieldunicity.class.php
@@ -9,7 +9,7 @@
 
  based on GLPI - Gestionnaire Libre de Parc Informatique
  Copyright (C) 2003-2014 by the INDEPNET Development Team.
- 
+
  -------------------------------------------------------------------------
 
  LICENSE
@@ -593,13 +593,15 @@ class FieldUnicity extends CommonDropdown {
    **/
    function showDebug() {
 
-      $params = array('message'     => '',
-                      'action_type' => true,
+      $params = array('action_type' => true,
                       'action_user' => getUserName(Session::getLoginUserID()),
                       'entities_id' => $_SESSION['glpiactive_entity'],
                       'itemtype'    => get_class($this),
                       'date'        => $_SESSION['glpi_currenttime'],
-                      'refuse'      => true);
+                      'refuse'      => true,
+                      'label'       => array('name' => 'test'),
+                      'field'       => array('action_refuse' => true),
+                      'double'      => array());
 
       NotificationEvent::debugEvent($this, $params);
    }
