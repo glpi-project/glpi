38437a8c112cc1cde7208e12ed8582857f5a53f2
yllen <nini.lasson@orange.fr>
2015-11-27 12:47:17 +0100

    fix richtext - fixed #140

--- inc/toolbox.class.php
+++ inc/toolbox.class.php
@@ -321,8 +321,8 @@ class Toolbox {
    **/
    static function clean_cross_side_scripting_deep($value) {
 
-      $in  = array('&lt;', '&gt;', '<', '>');
-      $out = array('&amp;lt;', '&amp;gt;', '&lt;', '&gt;');
+      $in  = array('&lt;', '&gt;', '<', '>', '&lt;p&gt;', '&lt;/p&gt;');
+      $out = array('&amp;lt;', '&amp;gt;', '&lt;', '&gt;', '', '');
 
       $value = ((array) $value === $value)
                   ? array_map(array(__CLASS__, 'clean_cross_side_scripting_deep'), $value)
