db43897f738e9dd056ed37a70998df6bedddded0
Alexandre Delaunay <adelaunay@teclib.com>
2016-01-11 09:33:53 +0100

    javascript CS

--- inc/ajax.class.php
+++ inc/ajax.class.php
@@ -329,19 +329,20 @@ class Ajax {
          echo "</div>";
          echo "<div id='loadingtabs$rand' class='invisible'>".
               "<div class='loadingindicator'>".__s('Loading...')."</div></div>";
-         $js = "$('#tabs$rand').tabs({ active: $selected_tab,
-         // Loading indicator
-         beforeLoad: function (event, ui) {
-                  ui.panel.html($('#loadingtabs$rand').html());
-                },
-         ajaxOptions: {type: 'POST'},
-
-         activate : function( event, ui ){
-            //  Get future value
-            var newIndex = ui.newTab.parent().children().index(ui.newTab);
-            $.get('".$CFG_GLPI['root_doc']."/ajax/updatecurrenttab.php',
-            { itemtype: '$type', id: '$ID', tab: newIndex });
-            }});";
+         $js = "$('#tabs$rand').tabs({ 
+            active: $selected_tab,
+            // Loading indicator
+            beforeLoad: function (event, ui) {
+               ui.panel.html($('#loadingtabs$rand').html());
+            },
+            ajaxOptions: {type: 'POST'},
+            activate : function( event, ui ){
+               //  Get future value
+               var newIndex = ui.newTab.parent().children().index(ui.newTab);
+               $.get('".$CFG_GLPI['root_doc']."/ajax/updatecurrenttab.php',
+                  { itemtype: '$type', id: '$ID', tab: newIndex });
+            }
+         });";
 
          if ($orientation=='vertical') {
             $js .=  "$('#tabs$rand').tabs().addClass( 'ui-tabs-vertical ui-helper-clearfix' );";
