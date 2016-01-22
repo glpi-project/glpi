5100c65cebd9a8acc879b0899f8321bb1161e3d4
tomolimo <olivier.moron@araymond.com>
2016-01-22 13:32:08 +0100

    To prevent rewrite of originator when already set
    To prevent rewrite of originator when already set.
    In particular if ticket is not created via web interface but for example via email, then the fix prevents the forcing of the originator to be equal to requester.

--- inc/commonitilobject.class.php
+++ inc/commonitilobject.class.php
@@ -1165,7 +1165,7 @@ abstract class CommonITILObject extends CommonDBTM {
       if (($uid = Session::getLoginUserID())
           && !isset($input['_auto_import'])) {
          $input["users_id_recipient"] = $uid;
-      } else if (isset($input["_users_id_requester"]) && $input["_users_id_requester"]) {
+      } else if (isset($input["_users_id_requester"]) && $input["_users_id_requester"] && !isset($input["users_id_recipient"])) {
          $input["users_id_recipient"] = $input["_users_id_requester"];
       }
 
