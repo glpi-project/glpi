c072aa68608940db83b9bb6c5db6ac3c4facd3a9
Alexandre Delaunay <adelaunay@teclib.com>
2016-01-26 09:48:05 +0100

    fix 0 in mail collector connection string; fix #397

--- inc/toolbox.class.php
+++ inc/toolbox.class.php
@@ -2195,7 +2195,7 @@ class Toolbox {
       if (isset($input['server_ssl']) && !empty($input['server_ssl'])) {
          $out .= $input['server_ssl'];
       }
-      if (isset($input['server_cert'])
+      if (isset($input['server_cert']) && !empty($input['server_cert'])
           && (!empty($input['server_ssl']) || !empty($input['server_tls']))) {
          $out .= $input['server_cert'];
       }
