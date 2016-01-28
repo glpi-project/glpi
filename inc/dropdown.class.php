dcf656abc6b95e9c9801d51b3340eefdf32f528b
tomolimo <olivier.moron@araymond.com>
2016-01-28 09:45:35 +0100

    When $elements is null then a fatal error occurs
    When $elements is null then a fatal error occurs

--- inc/dropdown.class.php
+++ inc/dropdown.class.php
@@ -1692,7 +1692,7 @@ class Dropdown {
 
       if ($param["display_emptychoice"]) {
          //array_unshift($elements, $param['emptylabel']); // cannot be used as it doesn't preserve keys when they are numbers
-         $elements = array( 0 => $param['emptylabel'] ) + $elements ;
+         $elements = array( 0 => $param['emptylabel'] ) + (is_array($elements)? $elements : array()) ;
       }
 
       if ($param["multiple"]) {
