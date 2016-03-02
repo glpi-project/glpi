37ae62338d47438023bbdcca664e1d4e077e8ba2
Alexandre Delaunay <adelaunay@teclib.com>
2016-03-02 15:46:57 +0100

    fix syntax error

--- inc/mailcollector.class.php
+++ inc/mailcollector.class.php
@@ -1061,7 +1061,7 @@ class MailCollector  extends CommonDBTM {
             $this->marubox = @imap_open($this->fields['host'], $this->fields['login'],
                                         Toolbox::decrypt($this->fields['passwd'], GLPIKEY),
                                         CL_EXPUNGE, 1, $option);
-            if (is_ressource($this->marubox)) {
+            if (is_resource($this->marubox)) {
                break;
             }
          }
