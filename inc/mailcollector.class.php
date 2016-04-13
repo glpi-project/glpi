b0a32fa3ef171e76ce6b31191f0b4f1aec64e0bd
Walid <wnouh@teclib.com>
2016-04-13 11:25:16 +0200

    fix #592 Email not moved to refused folder is no rule matched or recipient unknown

--- inc/mailcollector.class.php
+++ inc/mailcollector.class.php
@@ -584,11 +584,15 @@ class MailCollector  extends CommonDBTM {
                } else {
                   if (!$tkt['_users_id_requester']) {
                      $rejinput['reason'] = NotImportedEmail::USER_UNKNOWN;
+                     $rejected->add($rejinput);
 
                   } else {
                      $rejinput['reason'] = NotImportedEmail::MATCH_NO_RULE;
+                     $rejected->add($rejinput);
                   }
+                  $refused++;
                   $rejected->add($rejinput);
+                  $this->deleteMails($i, self::REFUSED_FOLDER);
                }
                $this->fetch_emails++;
             }
