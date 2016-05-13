49215f9f581023f8fe706f0ef598325ecab3a968
Remi Collet <fedora@famillecollet.com>
2016-05-13 15:31:56 +0200

    Test suite:
    - add a dataset on which tests can rely, for now:
        - a user
        - some entities
        - some printers
    - add a bootstrap file to load dataset when needed
    - add printer visibility test, using test dataset

---
+++ tests/bootstrap.php
@@ -0,0 +1,141 @@
+<?php
+/*
+-------------------------------------------------------------------------
+GLPI - Gestionnaire Libre de Parc Informatique
+Copyright (C) 2015 Teclib'.
+
+http://glpi-project.org
+
+based on GLPI - Gestionnaire Libre de Parc Informatique
+Copyright (C) 2003-2014 by the INDEPNET Development Team.
+
+-------------------------------------------------------------------------
+
+LICENSE
+
+This file is part of GLPI.
+
+GLPI is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+GLPI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------
+*/
+
+require_once __DIR__ . '/../inc/includes.php';
+
+define('TU_USER', '_test_user');
+define('TU_PASS', 'PhpUnit_4');
+
+function loadDataset() {
+
+   // Unit test data definition
+   $data = [
+      // dump the version to force reload
+      '_version' => 1,
+      // Type => array of entries
+      'Entity' => [
+         [
+            'name'        => '_test_root_entity',
+            'entities_id' => 0,
+         ], [
+            'name'        => '_test_child_1',
+            'entities_id' => '_test_root_entity',
+         ], [
+            'name'        => '_test_child_2',
+            'entities_id' => '_test_root_entity',
+         ]
+      ], 'Printer' => [
+         [
+            'name'         => '_test_printer_all',
+            'entities_id'  => '_test_root_entity',
+            'is_recursive' => 1    
+         ], [
+            'name'         => '_test_printer_ent0',
+            'entities_id'  => '_test_root_entity',
+            'is_recursive' => 0    
+         ], [
+            'name'         => '_test_printer_ent1',
+            'entities_id'  => '_test_child_1',
+            'is_recursive' => 0    
+         ], [
+            'name'         => '_test_printer_ent2',
+            'entities_id'  => '_test_child_2',
+            'is_recursive' => 0    
+         ]   
+      ], 'User' => [
+         [
+            'name'          => TU_USER,
+            'password'      => TU_PASS,
+            'password2'     => 'PhpUnit_4',
+            'entities_id'   => '_test_root_entity',
+            'profiles_id'   => 4, // TODO manage test profiles
+            '_entities_id'  => '_test_root_entity',
+            '_profiles_id'  => 4,
+            '_is_recursive' => 1
+         ]
+      ]
+   ];
+
+   // To bypass various right checks
+   $_SESSION['glpicronuserrunning'] = "cron_phpunit";
+   $_SESSION['glpi_use_mode']       = Session::NORMAL_MODE;
+   $CFG_GLPI['root_doc']            = '/glpi';
+
+   $conf = Config::getConfigurationValues('phpunit');
+   if (isset($conf['dataset']) && $conf['dataset']==$data['_version']) {
+      printf("\nGLPI dataset version %d already loaded\n\n", $data['_version']);
+   } else {
+      printf("\nLoading GLPI dataset version %d\n", $data['_version']);
+
+      $ids = array();
+      foreach ($data as $type => $inputs) {
+         if ($type[0] == '_') {
+            continue;
+         }
+         foreach($inputs as $input) {
+            // Resolve FK
+            foreach ($input as $k => $v) {
+               if (isForeignKeyField($k) && isset($ids[$v]) && !is_numeric($v)) {
+                  $input[$k] = $ids[$v];
+               }
+            }
+            if (isset($input['name']) && $item = getItemByTypeName($type, $input['name'])) {
+               $input['id'] = $ids[$input['name']] = $item->getField('id');
+               $item->update($input);
+               echo ".";
+            } else {
+               // Not found, create it
+               $item = getItemForItemtype($type);
+               $id = $item->add($input);
+               var_dump($input);
+               echo ".";
+               if (isset($input['name'])) {
+                  $ids[$input['name']] = $id;
+               }
+            }
+         }
+      }
+      echo "\nDone\n\n";
+      Config::setConfigurationValues('phpunit', ['dataset' => $data['_version']]);
+   }
+}
+
+function getItemByTypeName($type, $name, $onlyid=false) {
+
+   $item = getItemForItemtype($type);
+   if ($item->getFromDBByQuery("WHERE `name`='$name'")) {
+      return ($onlyid ? $item->getField('id') : $item);
+   }
+   return false;
+}
+
+loadDataset();
