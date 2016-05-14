e85e0ae4f9fba32b90727daecc3c57d66a74dc89
Remi Collet <fedora@famillecollet.com>
2016-05-14 18:51:55 +0200

    fix test file headers

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,34 +1,34 @@
 <?php
 /*
--------------------------------------------------------------------------
-GLPI - Gestionnaire Libre de Parc Informatique
-Copyright (C) 2015 Teclib'.
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2015-2016 Teclib'.
 
-http://glpi-project.org
+ http://glpi-project.org
 
-based on GLPI - Gestionnaire Libre de Parc Informatique
-Copyright (C) 2003-2014 by the INDEPNET Development Team.
+ based on GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2014 by the INDEPNET Development Team.
 
--------------------------------------------------------------------------
+ -------------------------------------------------------------------------
 
-LICENSE
+ LICENSE
 
-This file is part of GLPI.
+ This file is part of GLPI.
 
-GLPI is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-GLPI is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with GLPI. If not, see <http://www.gnu.org/licenses/>.
---------------------------------------------------------------------------
-*/
+ You should have received a copy of the GNU General Public License
+ along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
 
 if (!file_exists(GLPI_CONFIG_DIR . '/config_db.php')) {
    die("\nConfiguration file for tests not found\n\nrun: php tools/cliinstall.php --tests ...\n\n");
