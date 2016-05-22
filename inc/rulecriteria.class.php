glpi-project/glpi

commit b8fcf32650bd5df985381e69c05bc68f3bac58df

add associated item branch

inc/rulecriteria.class.php
@@ -448,11 +448,11 @@ class RuleCriteria extends CommonDBChild {
             $results = array();
             // Permit use < and >
             $pattern = Toolbox::unclean_cross_side_scripting_deep($pattern);
-            if (preg_match($pattern."i",$field,$results)>0) {
+            if (preg_match_all($pattern."i",$field,$results)>0) {
                // Drop $result[0] : complete match result
                array_shift($results);
                // And add to $regex_result array
-               $regex_result[]               = $results;
+               $regex_result[]               = $results[1];
                $criterias_results[$criteria] = $pattern;
                return true;
             }

.gitignore
@@ -9,3 +9,4 @@
 /tools/composer.lock
 /tools/vendor/
 /tools/api/
+/nbproject/private/

ajax/dropdownTrackingDeviceType.php
@@ -45,37 +45,42 @@ Session::checkLoginUser();
 if (isset($_POST["itemtype"])
     && CommonITILObject::isPossibleToAssignType($_POST["itemtype"])) {
    $table = getTableForItemType($_POST["itemtype"]);
-   $rand  = mt_rand();
-
+   if (isset($_POST["rand"])) {
+      $rand = $_POST["rand"];
+   } else {
+      $rand = mt_rand();
+   }
+   
    // Message for post-only
    if (!isset($_POST["admin"]) || ($_POST["admin"] == 0)) {
       echo "<br>".__('Enter the first letters (user, item name, serial or asset number)');
    }
-
+   
    echo "<br>";
    $field_id = Html::cleanId("dropdown_".$_POST['myname'].$rand);
-
    $p = array('itemtype'            => $_POST["itemtype"],
               'entity_restrict'     => $_POST['entity_restrict'],
               'table'               => $table,
-              'myname'              => $_POST["myname"]);
-
-   if(isset($_POST["used"]) && !empty($_POST["used"])){
-      if(isset($_POST["used"][$_POST["itemtype"]])){
+              'multiple'            => $_POST["multiple"],
+              'myname'              => $_POST["myname"], 
+              'rand'                => $_POST["rand"]);
+   
+   if (isset($_POST["used"]) && !empty($_POST["used"])) {
+      if (isset($_POST["used"][$_POST["itemtype"]])) {
          $p["used"] = $_POST["used"][$_POST["itemtype"]];
       }
    }
    
    echo Html::jsAjaxDropdown($_POST['myname'], $field_id,
-                              $CFG_GLPI['root_doc']."/ajax/getDropdownFindNum.php",
-                              $p);
+                             $CFG_GLPI['root_doc']."/ajax/getDropdownFindNum.php",
+                             $p);
+   
    // Auto update summary of active or just solved tickets
    $params = array('items_id' => '__VALUE__',
                    'itemtype' => $_POST['itemtype']);
-
+   
    Ajax::updateItemOnSelectEvent($field_id,"item_ticket_selection_information",
                                  $CFG_GLPI["root_doc"]."/ajax/ticketiteminformation.php",
                                  $params);
-
 }
 ?>

front/item_ticket.form.php
@@ -45,6 +45,10 @@ if (isset($_POST["add"])) {
    if (isset($_POST['my_items']) && !empty($_POST['my_items'])) {
       list($_POST['itemtype'], $_POST['items_id']) = explode('_', $_POST['my_items']);
    }
+   
+   if (isset($_POST['add_items_id'])) {
+      $_POST['items_id'] = $_POST['add_items_id'];
+   }
 
    if(!isset($_POST['items_id']) || empty($_POST['items_id'])){
       $message = sprintf(__('Mandatory fields are not filled. Please correct: %s'),
@@ -62,6 +66,12 @@ if (isset($_POST["add"])) {
    }
    Html::back();
 
+} elseif (isset($_POST["delete"])) {
+   $item_ticket = new Item_Ticket();
+   $deleted = $item_ticket->deleteByCriteria(array('tickets_id' => $_POST['tickets_id'],
+                                                   'items_id'   => $_POST['items_id'],
+                                                   'itemtype'   => $_POST['itemtype']));
+   Html::back();
 }
 
 Html::displayErrorAndDie("lost");

front/ticket.form.php
@@ -47,13 +47,6 @@ if (!isset($_GET['id'])) {
 if (isset($_POST["add"])) {
    $track->check(-1, CREATE, $_POST);
 
-   if (isset($_POST["my_items"]) && !empty($_POST["my_items"])) {
-      $splitter = explode("_",$_POST["my_items"]);
-      if (count($splitter) == 2) {
-         $_POST["itemtype"] = $splitter[0];
-         $_POST["items_id"] = $splitter[1];
-      }
-   }
    if ($id = $track->add($_POST)) {
       if ($_SESSION['glpibackcreated']) {
          Html::redirect($track->getFormURL()."?id=".$id);

front/tickettemplatepredefinedfield.form.php
@@ -44,7 +44,9 @@ $item = new TicketTemplatePredefinedField();
 // Use masiveaction system to manage add value
 if (isset($_POST["massiveaction"])) {
    $item->check(-1, UPDATE, $_POST);
-
+   if (isset($_POST['items_tickets_id']) && isset($_POST['add_items_id'])) {
+      $_POST['items_tickets_id'] = $_POST['items_tickets_id']."_".$_POST['add_items_id'];
+   }
    if ($item->add($_POST)) {
       Event::log($_POST["tickettemplates_id"], "tickettemplate", 4, "maintain",
                  //TRANS: %s is the user login

front/tracking.injector.php
@@ -61,20 +61,6 @@ if (isset($_POST["_type"]) && ($_POST["_type"] == "Helpdesk")) {
    Html::header(__('Simplified interface'), '', $_SESSION["glpiname"], "helpdesk", "tracking");
 }
 
-if (isset($_POST["my_items"]) && !empty($_POST["my_items"])) {
-   $splitter = explode("_",$_POST["my_items"]);
-   if (count($splitter) == 2) {
-      $_POST["itemtype"] = $splitter[0];
-      $_POST["items_id"] = $splitter[1];
-   }
-}
-
-if (!isset($_POST["itemtype"])
-    || (empty($_POST["items_id"]) && ($_POST["itemtype"] != 0))) {
-   $_POST["itemtype"] = '';
-   $_POST["items_id"] = 0;
-}
-
 if (isset($_POST['add'])) {
    if ($newID = $track->add($_POST)) {
       if (isset($_POST["_type"]) && ($_POST["_type"] == "Helpdesk")) {

inc/dropdown.class.php
@@ -448,8 +448,7 @@ class Dropdown {
     *
     * @param $name            name of the select box
     * @param $types     array of types to display
-    * @param $options   array Already used items ID: not to display in dropdown
-    * Parameters which could be used in options array :
+    * @param $options   Parameters which could be used in options array :
     *    - value      : integer / preselected value (default '')
     *    - used       : array / Already used items ID: not to display in dropdown (default empty)
     *    - emptylabel : Empty choice's label (default self::EMPTY_VALUE)
@@ -465,27 +464,25 @@ class Dropdown {
       $params['emptylabel']   = self::EMPTY_VALUE;
       $params['display']      = true;
       $params['width']        = '80%';
-
+      $params['rand']         = mt_rand();
+      
       if (is_array($options) && count($options)) {
          foreach ($options as $key => $val) {
             $params[$key] = $val;
          }
       }
 
+      $values = array();
       if (count($types)) {
          foreach ($types as $type) {
             if ($item = getItemForItemtype($type)) {
-               $options[$type] = $item->getTypeName(1);
+               $values[$type] = $item->getTypeName(1);
             }
          }
       }
-      asort($options);
-      return self::showFromArray($name, $options,
-                                 array('value'      => $params['value'],
-                                       'used'       => $params['used'],
-                                       'width'      => $params['width'],
-                                       'display'    => $params['display'],
-                                       'emptylabel' => $params['emptylabel']));
+      asort($values);
+      return self::showFromArray($name, $values,
+                                 $params);
    }
 
 

commit 99305370414f70164583d62c9d9248ac8b3ff28b

Fix regression on rulecriteria introduced in b8fcf32

inc/rulecriteria.class.php
@@ -452,7 +452,11 @@ class RuleCriteria extends CommonDBChild {
                // Drop $result[0] : complete match result
                array_shift($results);
                // And add to $regex_result array
-               $regex_result[]               = $results[1];
+               $res = array();
+               foreach($results as $data) {
+                  $res[] = $data[0];
+               }
+               $regex_result[]               = $res;
                $criterias_results[$criteria] = $pattern;
                return true;
             }