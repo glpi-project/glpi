91fc170848a49d94bf63213b44ee8633568f3c9f
Alexandre Delaunay <adelaunay@teclib.com>
2016-05-23 15:07:37 +0200

    escape undescore char for mysql text search (like); fix #672

--- inc/search.class.php
+++ inc/search.class.php
@@ -6122,6 +6122,9 @@ class Search {
       // Unclean to permit < and > search
       $val = Toolbox::unclean_cross_side_scripting_deep($val);
 
+      // escape _ char used as wildcard in mysql likes
+      $val = str_replace('_', '\\_', $val);
+
       if (($val == 'NULL') || ($val == 'null')) {
          $SEARCH = " IS $NOT NULL ";
 
