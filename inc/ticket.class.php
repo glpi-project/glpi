28f49f2e1b25fc655f589314f6b236cb702d0cc4
btry <tbugier@teclib.com>
2016-05-24 10:59:35 +0200

    show empty followups. Solution approbation is not empty because a content is added in such case (#674)
    * show empty followups. Solution approbation is not empty because a
    content is added in such case
    
    * remove the commented out code

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -6302,12 +6302,6 @@ class Ticket extends CommonITILObject {
       foreach ($timeline as $item) {
          $item_i = $item['item'];
 
-         // don't display empty followup (ex : solution approbation)
-         if (($item['type'] == 'TicketFollowup')
-             && empty($item_i['content'])) {
-            continue;
-         }
-
          $date = "";
          if (isset($item_i['date'])) {
             $date = $item_i['date'];
