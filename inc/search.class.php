31d67bcb42992ebae5ee450575f3e7130fb77084
tsmr <tsmr@thetsmr.fr>
2016-05-26 11:00:30 +0200

    Clean PHP-Errors

--- inc/search.class.php
+++ inc/search.class.php
@@ -192,7 +192,7 @@ class Search {
       }
       if (!$CFG_GLPI['allow_search_view']) {
          foreach ($p['criteria'] as $val) {
-            if ($val['field']) && $val['field'] == 'view') {
+            if (isset($val['field']) && $val['field'] == 'view') {
                Html::displayRightError();
             }
          }
@@ -227,7 +227,7 @@ class Search {
       if (count($p['criteria']) > 0) {
          foreach ($p['criteria'] as $key => $val) {
             if (!in_array($val['field'], $data['toview'])) {
-               if ($val['field']) && ($val['field'] != 'all') && ($val['field'] != 'view')) {
+               if (isset($val['field']) && ($val['field'] != 'all') && ($val['field'] != 'view')) {
                   array_push($data['toview'], $val['field']);
                } else if ($val['field'] == 'all'){
                   $data['search']['all_search'] = true;
@@ -431,7 +431,7 @@ class Search {
             // if real search (strlen >0) and not all and view search
             if (isset($criteria['value']) && (strlen($criteria['value']) > 0)) {
                // common search
-               if ($criteria['field']) && ($criteria['field'] != "all") && ($criteria['field'] != "view")) {
+               if (isset($criteria['field']) && ($criteria['field'] != "all") && ($criteria['field'] != "view")) {
                   $LINK    = " ";
                   $NOT     = 0;
                   $tmplink = "";
@@ -509,7 +509,7 @@ class Search {
 
                   $items = array();
 
-                  if ($criteria['field'] == "all") {
+                  if (isset($criteria['field']) && $criteria['field'] == "all") {
                      $items = $searchopt;
 
                   } else { // toview case : populate toview
