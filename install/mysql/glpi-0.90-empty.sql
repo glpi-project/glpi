f79c718b9f4246e8546d7a47f46b4f5ea7f43ce1
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2016-07-19 09:48:53 +0200

    0.90.4 in installation

--- install/mysql/glpi-0.90-empty.sql
+++ install/mysql/glpi-0.90-empty.sql
@@ -794,7 +794,7 @@ CREATE TABLE `glpi_configs` (
   UNIQUE KEY `unicity` (`context`,`name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO `glpi_configs` VALUES ('1','core','version','0.90.3');
+INSERT INTO `glpi_configs` VALUES ('1','core','version','0.90.4');
 INSERT INTO `glpi_configs` VALUES ('2','core','show_jobs_at_login','0');
 INSERT INTO `glpi_configs` VALUES ('3','core','cut','250');
 INSERT INTO `glpi_configs` VALUES ('4','core','list_limit','15');
