fc9363360a12328057b69a29a9f233f0ab113bf4
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2016-08-26 14:03:11 +0200

    document sending, no content dispoposition attachment for pictures; see #629

--- inc/document.class.php
+++ inc/document.class.php
@@ -456,11 +456,21 @@ class Document extends CommonDBTM {
       if (!file_exists($file)) {
          die("Error file ".$file." does not exist");
       }
+
+      // don't download picture files, see them inline
+      $attachment = "";
+      $filename_parts = explode(".", $this->fields['filename']);
+      $extension = array_pop($filename_parts);
+      $extension = strtolower($extension);
+      if (!in_array($extension, array('jpg', 'png', 'gif', 'bmp'))) {
+         $attachment = " attachment;";
+      }
+
       // Now send the file with header() magic
       header("Expires: Mon, 26 Nov 1962 00:00:00 GMT");
       header('Pragma: private'); /// IE BUG + SSL
       header('Cache-control: private, must-revalidate'); /// IE BUG + SSL
-      header("Content-disposition: attachment; filename=\"".$this->fields['filename']."\"");
+      header("Content-disposition:$attachment filename=\"".$this->fields['filename']."\"");
       header("Content-type: ".$this->fields['mime']);
 
       readfile($file) or die ("Error opening file $file");
