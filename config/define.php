1ed2035c18175b505393a2f4554e054010744cde
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2016-09-16 14:57:57 +0200

    force mailqueue form in lefttab layout; fix #996 (#998)

--- config/define.php
+++ config/define.php
@@ -339,7 +339,8 @@ $CFG_GLPI['layout_excluded_pages'] = array("profile.form.php",
                                            "config.form.php",
                                            "common.tabs.php",
                                            "transfer.form.php",
-                                           "entity.form.php");
+                                           "entity.form.php",
+                                           "queuedmail.form.php");
 
 $CFG_GLPI['lock_lockable_objects'] = array('Budget',  'Change', 'Contact', 'Contract', 'Document',
                                            'CartridgeItem', 'Computer', 'ConsumableItem', 'Entity',
