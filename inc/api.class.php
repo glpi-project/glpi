da3d1d5b8020cfa31738da696c4bb696a64ec839
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2016-10-03 14:25:25 +0200

    remove trailing spaces

--- inc/api.class.php
+++ inc/api.class.php
@@ -1297,7 +1297,7 @@ abstract class API extends CommonGLPI {
                             400,
                             "ERROR_RANGE_EXCEED_TOTAL");
       }
-      
+
       // fix end range
       if ($params['range'][1] > $cleaned_data['totalcount'] - 1) {
          $params['range'][1] = $cleaned_data['totalcount'] - 1;
