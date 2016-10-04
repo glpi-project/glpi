c360c9815f439140ddd9ef9e39bc33a2ad49fb6e
tsmr <tsmr@thetsmr.fr>
2016-10-04 12:04:39 +0200

    drop whitespace

--- inc/planning.class.php
+++ inc/planning.class.php
@@ -1126,7 +1126,6 @@ class Planning extends CommonGLPI {
          $group->getFromDB($actor[1]);
          $title = $group->getName();
       } else if($filter_data['type'] == 'event_filter') {
-         
          if (!($item = getItemForItemtype($filter_key))) {
             return false;
          }
