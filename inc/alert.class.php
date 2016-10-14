69e670302647b23266b3cec91cfe4b44bccfe968
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2016-10-14 17:07:04 +0200

    use self::getTable

--- inc/alert.class.php
+++ inc/alert.class.php
@@ -231,11 +231,11 @@ class Alert extends CommonDBTM {
       global $DB;
 
       if ($items_id) {
-         $iter = $DB->request(getTableForItemType(__CLASS__), ['FIELDS'   => 'date',
-                                                               'ORDER'    => 'date DESC',
-                                                               'LIMIT'    => 1,
-                                                               'itemtype' => $itemtype,
-                                                               'items_id' => $items_id]);
+         $iter = $DB->request(self::getTable(), ['FIELDS'   => 'date',
+                                                 'ORDER'    => 'date DESC',
+                                                 'LIMIT'    => 1,
+                                                 'itemtype' => $itemtype,
+                                                 'items_id' => $items_id]);
          if ($row = $iter->next()) {
             //TRANS: %s is the date
             echo sprintf(__('Alert sent on %s'), Html::convDateTime($row['date']));
