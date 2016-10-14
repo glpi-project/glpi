47e6f05a6faf466253a29c192723794dbf46bbe9
Valentin Hamon <vhamon@teclib.com>
2016-10-14 12:08:18 +0200

    Fix missing break in contract_item class

--- inc/contract_item.class.php
+++ inc/contract_item.class.php
@@ -317,7 +317,7 @@ class Contract_Item extends CommonDBRelation{
       switch ($item->getType()) {
          case 'Contract' :
             self::showForContract($item);
-
+            break;
          default :
             if (in_array($item->getType(), $CFG_GLPI["contract_types"])) {
                self::showForItem($item, $withtemplate);
