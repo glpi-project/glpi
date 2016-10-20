da5e5ede8e3109585d52ff0c5636484eaf3d8608
Stanislas Kita <skita@teclib.com>
2016-10-20 11:34:55 +0200

    add fields for child notion on software licences

--- install/update_0905_91.php
+++ install/update_0905_91.php
@@ -564,7 +564,9 @@ function update0905to91() {
    $migration->addField("glpi_softwarelicenses", "template_name", "string");
    $migration->addField("glpi_softwarelicenses", "states_id", "integer");
    $migration->addField("glpi_softwarelicenses", "manufacturers_id", "integer");
-
+   $migration->addField("glpi_softwarelicenses", "softwarelicenses_id", "integer");
+   $migration->addField("glpi_softwarelicenses", "completename", "text");
+   $migration->addField("glpi_softwarelicenses", "level", "integer");
    $migration->addKey("glpi_softwarelicenses", "locations_id");
    $migration->addKey("glpi_softwarelicenses", "users_id_tech");
    $migration->addKey("glpi_softwarelicenses", "users_id");
