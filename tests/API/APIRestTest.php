9702d3c5e327594dd6bd143c75bc3266f61cb54f
btry <tbugier@teclib.com>
2016-11-09 16:08:20 +0100

    update tests

--- tests/API/APIRestTest.php
+++ tests/API/APIRestTest.php
@@ -928,6 +928,7 @@ class APIRestTest extends PHPUnit_Framework_TestCase {
          $this->assertEquals(false, (bool) $computers_exist);
       }
 
+      // Test multiple delete with multi-status
       $input    = array();
       $computers_id_collection = [
             [
