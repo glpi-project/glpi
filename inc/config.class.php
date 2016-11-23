525ff138e8898b41dfeba27b3af89638226c8862
Xavier CAILLAUD <tsmr@users.noreply.github.com>
2016-11-23 15:52:06 +0100

    Fix display (tr) for templates / config / entity (#1305)

--- inc/config.class.php
+++ inc/config.class.php
@@ -1149,7 +1149,7 @@ class Config extends CommonDBTM {
       echo "<td>";
       Dropdown::showYesNo('highcontrast_css', $data['highcontrast_css']);
       echo "</td>";
-      echo "<td>";
+      echo "<td colspan='2'>";
       echo "</td></tr>";
 
       if ($oncentral) {
