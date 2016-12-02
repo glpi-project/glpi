4c28664523434cc75c95f89e3b706763481d1671
Johan Cwiklinski <jcwiklinski@teclib.com>
2016-12-02 12:15:01 +0100

    Add auto for is_recusrive value

--- tests/DbFunctionTest.php
+++ tests/DbFunctionTest.php
@@ -386,6 +386,10 @@ class DbFunctionTest extends DbTestCase {
       $it = new DBmysqlIterator(NULL, 'glpi_entities', getEntitiesRestrictCriteria('glpi_entities', '', '', true));
       $this->assertEquals('SELECT * FROM `glpi_entities` WHERE (`glpi_entities`.`id` IN (3, 0, 1))', $it->getSql());
 
+      //Child + parent -- automatic recusrivity detection
+      $it = new DBmysqlIterator(NULL, 'glpi_computers', getEntitiesRestrictCriteria('glpi_computers', '', '', 'auto'));
+      $this->assertEquals('SELECT * FROM `glpi_computers` WHERE (`glpi_computers`.`entities_id` IN (3) OR (`glpi_computers`.`is_recursive` = 1 AND `glpi_computers`.`entities_id` IN (0, 1)))', $it->getSql());
+
       // Child + parent without table
       $this->assertEquals("WHERE ( `entities_id` IN ('3')  OR (`is_recursive`='1' AND `entities_id` IN ('0','1')) ) ",
                           getEntitiesRestrictRequest('WHERE', '', '', '', true));
