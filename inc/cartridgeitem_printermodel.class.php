1d39ee18aa9616c19b6da584ecab525d762cec2c
gitdevmod <djdomics@gmail.com>
2016-12-27 16:18:27 +0100

    Create a link to display printer model

--- inc/cartridgeitem_printermodel.class.php
+++ inc/cartridgeitem_printermodel.class.php
@@ -189,7 +189,10 @@ class CartridgeItem_PrinterModel extends CommonDBRelation {
                Html::showMassiveActionCheckBox(__CLASS__, $data["id"]);
                echo "</td>";
             }
-            echo "<td class='center'>".$data['type']."</td>";
+            echo "<td class='center'><a href='".$CFG_GLPI['root_doc'].
+            "/front/printer.php?is_deleted=0&criteria[0][field]=40&criteria[0][searchtype]=equals&criteria[0][value]="
+            .$data['pmid']."'>".$data['type']."</a>";
+          
             echo "</tr>";
          }
          echo $header_begin.$header_bottom.$header_end;
