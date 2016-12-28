ce8d36de4da552223a5ea0da81b5845200f5632d
btry <tbugier@teclib.com>
2016-12-28 20:54:22 +0100

    add migration script

---
+++ install/update_911_912.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+GLPI - Gestionnaire Libre de Parc Informatique
+Copyright (C) 2015-2016 Teclib'.
+
+http://glpi-project.org
+
+based on GLPI - Gestionnaire Libre de Parc Informatique
+Copyright (C) 2003-2014 by the INDEPNET Development Team.
+
+-------------------------------------------------------------------------
+
+LICENSE
+
+This file is part of GLPI.
+
+GLPI is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+GLPI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------
+*/
+
+/** @file
+ * @brief
+*/
+
+/**
+ * Update from 9.1.1 to 9.1.2
+*
+* @return bool for success (will die for most error)
+**/
+function update911to912() {
+   global $DB, $migration, $CFG_GLPI;
+
+   $current_config   = Config::getConfigurationValues('core');
+   $updateresult     = true;
+   $ADDTODISPLAYPREF = array();
+
+   //TRANS: %s is the number of new version
+   $migration->displayTitle(sprintf(__('Update to %s'), '9.1.2'));
+   $migration->setVersion('9.1.2');
+
+   $backup_tables = false;
+   // table already exist but deleted during the migration
+   // not table created during the migration
+   $newtables     = array();
+
+   foreach ($newtables as $new_table) {
+      // rename new tables if exists ?
+      if (TableExists($new_table)) {
+         $migration->dropTable("backup_$new_table");
+         $migration->displayWarning("$new_table table already exists. ".
+               "A backup have been done to backup_$new_table.");
+         $backup_tables = true;
+         $query         = $migration->renameTable("$new_table", "backup_$new_table");
+      }
+   }
+   if ($backup_tables) {
+      $migration->displayWarning("You can delete backup tables if you have no need of them.",
+            true);
+   }
+
+   // give READ right on components to profiles having UPDATE right
+   $query = "UPDATE `glpi_profilerights`
+             SET `rights` = `rights` | 1
+             WHERE (`rights` & 2) = '2'
+                   AND `name` = 'device'";
+   $DB->queryOrDie($query, "grant READ right on components to profiles having UPDATE right");
+
+   //put you migration script here
+
+   // ************ Keep it at the end **************
+   $migration->executeMigration();
+
+   return $updateresult;
+}
