2ab587f438532446f820e7060c40bdf983f36477
tomolimo <olivier.moron@araymond.com>
2017-01-03 10:41:59 +0100

    Fixed password check that was not jquery compliant
    Fixed password check that was not jquery compliant
    Changed pwd.value into pwd.val() 
    Fixes #1495

--- inc/config.class.php
+++ inc/config.class.php
@@ -1337,7 +1337,7 @@ class Config extends CommonDBTM {
       echo "<script type='text/javascript' >\n";
       echo "function passwordCheck() {\n";
       echo "var pwd = ".Html::jsGetElementbyID($field).";";
-      echo "if (pwd.value.length < ".$CFG_GLPI['password_min_length'].") {
+      echo "if (pwd.val().length < ".$CFG_GLPI['password_min_length'].") {
             ".Html::jsGetElementByID('password_min_length').".addClass('red');
             ".Html::jsGetElementByID('password_min_length').".removeClass('green');
       } else {
@@ -1348,7 +1348,7 @@ class Config extends CommonDBTM {
       if ($CFG_GLPI["password_need_number"]) {
          $needs[] = "<span id='password_need_number' class='red'>".__('Digit')."</span>";
          echo "var numberRegex = new RegExp('[0-9]', 'g');
-         if (false == numberRegex.test(pwd.value)) {
+         if (false == numberRegex.test(pwd.val())) {
                ".Html::jsGetElementByID('password_need_number').".addClass('red');
                ".Html::jsGetElementByID('password_need_number').".removeClass('green');
          } else {
@@ -1359,7 +1359,7 @@ class Config extends CommonDBTM {
       if ($CFG_GLPI["password_need_letter"]) {
          $needs[] = "<span id='password_need_letter' class='red'>".__('Lowercase')."</span>";
          echo "var letterRegex = new RegExp('[a-z]', 'g');
-         if (false == letterRegex.test(pwd.value)) {
+         if (false == letterRegex.test(pwd.val())) {
                ".Html::jsGetElementByID('password_need_letter').".addClass('red');
                ".Html::jsGetElementByID('password_need_letter').".removeClass('green');
          } else {
@@ -1370,7 +1370,7 @@ class Config extends CommonDBTM {
       if ($CFG_GLPI["password_need_caps"]) {
          $needs[] = "<span id='password_need_caps' class='red'>".__('Uppercase')."</span>";
          echo "var capsRegex = new RegExp('[A-Z]', 'g');
-         if (false == capsRegex.test(pwd.value)) {
+         if (false == capsRegex.test(pwd.val())) {
                ".Html::jsGetElementByID('password_need_caps').".addClass('red');
                ".Html::jsGetElementByID('password_need_caps').".removeClass('green');
          } else {
@@ -1381,7 +1381,7 @@ class Config extends CommonDBTM {
       if ($CFG_GLPI["password_need_symbol"]) {
          $needs[] = "<span id='password_need_symbol' class='red'>".__('Symbol')."</span>";
          echo "var capsRegex = new RegExp('[^a-zA-Z0-9_]', 'g');
-         if (false == capsRegex.test(pwd.value)) {
+         if (false == capsRegex.test(pwd.val())) {
                ".Html::jsGetElementByID('password_need_symbol').".addClass('red');
                ".Html::jsGetElementByID('password_need_symbol').".removeClass('green');
          } else {
