94a1dbe5b2d30b7555f8297b824e185330270049
Anael Mobilia <contrib@anael.eu>
2017-01-18 05:36:59 +0100

    Delete old copyright to give a coherency (no copyright on .htaccess, .sql, .sh, .pl, .html, .MD files as we didn't handle it)

--- tools/make_plugin_release.sh
+++ tools/make_plugin_release.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
-# -------------------------------------------------------------------------
-# GLPI - Gestionnaire Libre de Parc Informatique
-# Copyright (C) 2003-2014 by the INDEPNET Development Team
-#
-# http://indepnet.net/   http://glpi-project.org
-# -------------------------------------------------------------------------
-#
-# LICENSE
-#
-# This file is part of GLPI.
-#
-# GLPI is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# GLPI is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GLPI. If not, see <http://www.gnu.org/licenses/>.
-# ------------------------------------------------------------------------
 
 if [ $# -lt 2 ]
 then
