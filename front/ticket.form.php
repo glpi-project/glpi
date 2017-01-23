faa512439e19fe662658c3d63f6f0f885558676c
yllen <nini.lasson@orange.fr>
2017-01-23 18:04:32 +0100

    fix PHP notice - see #1644

--- front/ticket.form.php
+++ front/ticket.form.php
@@ -183,9 +183,12 @@ if (isset($_GET["id"]) && ($_GET["id"] > 0)) {
 
 } else {
    Html::header(__('New ticket'),'',"helpdesk","ticket");
+   toolbox::logdebug("res", $_REQUEST);
    unset($_REQUEST['id']);
    // Add a ticket from item : format data
-   if (isset($_REQUEST['_add_fromitem'])) {
+   if (isset($_REQUEST['_add_fromitem'])
+       && isset($_REQUEST['itemtype'])
+       && isset($_REQUEST['items_id'])) {
       $_REQUEST['items_id'] = array($_REQUEST['itemtype'] => array($_REQUEST['items_id']));
    }
    $track->display($_REQUEST);
