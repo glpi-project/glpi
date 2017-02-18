a638f2105d84f51825b5b0e5472001035cf4aeb6
Thierry Bugier Pineau <tbugier@teclib.com>
2017-02-18 17:40:53 +0100

    enhance comment, fix typo

--- inc/html.class.php
+++ inc/html.class.php
@@ -5102,9 +5102,9 @@ class Html {
     *    - pasteZone           string   DOM ID of the paste zone
     *    - dropZone            string   DOM ID of the drop zone
     *    - rand                string   already computed rand value
-    *    - display             boolean  display or return the generated html
+    *    - display             boolean  display or return the generated html (default true)
     *
-    * @return mixed          the html if display paremeter is false or true
+    * @return void|string   the html if display parameter is false
    **/
    static function file($options=array()) {
       global $CFG_GLPI;
