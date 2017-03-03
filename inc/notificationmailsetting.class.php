992c776ee92d114329313aac34e677f61a4d98dc
Joseph Rafferty <joraff@gmail.com>
2017-03-03 10:00:02 -0600

    Remove a default value for better <- compatibility

--- inc/notificationmailsetting.class.php
+++ inc/notificationmailsetting.class.php
@@ -218,8 +218,7 @@ class NotificationMailSetting extends CommonDBTM {
 
          echo "<tr class='tab_bg_2'>";
          echo "<td >" . __('SMTP Max Delivery Retries') . "</td>";
-         echo "<td><input type='text' name='smtp_max_retries' size='5' value='" .
-                    ($CFG_GLPI["smtp_max_retries"]?: 30) . "'></td>";
+         echo "<td><input type='text' name='smtp_max_retries' size='5' value='" . $CFG_GLPI["smtp_max_retries"] . "'></td>";
 
          echo "</tr>";
 
