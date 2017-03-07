414d0f125f45bacaf3ae0bdb273b0cc16a74a901
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-03-07 08:48:42 +0100

    There is nothing to handle an 'upgrade' parameter

--- tools/cliupdate.php
+++ tools/cliupdate.php
@@ -35,7 +35,7 @@
 */
 
 if (in_array('--help', $_SERVER['argv'])) {
-   die("usage: ".$_SERVER['argv'][0]."  [ --upgrade | --force ] [ --optimize ] [ --lang=xx_XX ] [ --config-dir=/path/relative/to/script ]\n");
+   die("usage: ".$_SERVER['argv'][0]."  [ --force ] [ --optimize ] [ --lang=xx_XX ] [ --config-dir=/path/relative/to/script ]\n");
 }
 
 chdir(__DIR__);
