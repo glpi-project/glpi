f45d604bf123dfdfb333aa3949bfd75b3ff21558
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-03-22 13:22:32 +0100

    Use PHPMailer validator

--- inc/notificationmail.class.php
+++ inc/notificationmail.class.php
@@ -54,57 +54,15 @@ class NotificationMail implements NotificationInterface {
     * from http://www.linuxjournal.com/article/9585
    **/
    static function isUserAddressValid($address, $options=array('checkdns'=>false)) {
+      $isValid = \PHPMailer::ValidateAddress($address);
 
       $checkdns = $options['checkdns'];
-      $isValid  = true;
-      $atIndex  = strrpos($address, "@");
-
-      if (is_bool($atIndex) && !$atIndex) {
-         $isValid = false;
-
-      } else {
-         $domain    = substr($address, $atIndex+1);
-         $local     = substr($address, 0, $atIndex);
-         $localLen  = strlen($local);
-         $domainLen = strlen($domain);
-
-         if (($localLen < 1) || ($localLen > 64)) {
-            // local part length exceeded
+      if ($checkdns) {
+         $domain    = substr($address, strrpos($address, '@')+1);
+         if ($isValid
+               && !(checkdnsrr($domain, "MX") || checkdnsrr($domain, "A"))) {
+            // domain not found in DNS
             $isValid = false;
-         } else if (($domainLen < 1) || ($domainLen > 255)) {
-            // domain part length exceeded
-            $isValid = false;
-         } else if (($local[0] == '.') || ($local[$localLen-1] == '.')) {
-            // local part starts or ends with '.'
-            $isValid = false;
-         } else if (preg_match('/\\.\\./', $local)) {
-            // local part has two consecutive dots
-            $isValid = false;
-         } else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
-            // character not valid in domain part
-            $isValid = false;
-         } else if (preg_match('/\\.\\./', $domain)) {
-            // domain part has two consecutive dots
-            $isValid = false;
-         } else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
-                                str_replace("\\\\", "", $local))) {
-            // character not valid in local part unless
-            // local part is quoted
-            if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\", "", $local))) {
-               $isValid = false;
-            }
-         }
-
-         if ($checkdns) {
-            if ($isValid
-                && !(checkdnsrr($domain, "MX") || checkdnsrr($domain, "A"))) {
-               // domain not found in DNS
-               $isValid = false;
-            }
-
-         } else if (!preg_match('/\\./', $domain) || !preg_match("/[a-zA-Z0-9]$/", $domain)) {
-            // domain has no dots or do not end by alphenum char
-               $isValid = false;
          }
       }
       return $isValid;
