d6915d59d525488cc82c848c7e45e98fcaed5f57
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-04-13 14:43:16 +0200

    _e should not be used for assignment; fixes #1904

--- inc/commonglpi.class.php
+++ inc/commonglpi.class.php
@@ -853,7 +853,7 @@ class CommonGLPI {
          }
 
          if (!$glpilisttitle) {
-            $glpilisttitle = _e('List');
+            $glpilisttitle = __('List');
          }
          echo "<td><a href=\"".$glpilisturl."\" title='$glpilisttitle'>";
          echo Toolbox::substr($glpilisttitle, 0, 100)."...";
