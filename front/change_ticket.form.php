98a17d2cfe8d86536794d58a059a1983df661511
yllen <nini.lasson@orange.fr>
2017-04-25 21:00:01 +0200

    fix error message for relation change/ticket - see #2107

--- front/change_ticket.form.php
+++ front/change_ticket.form.php
@@ -42,6 +42,18 @@ Session::checkLoginUser();
 
 $item = new Change_Ticket();
 if (isset($_POST["add"])) {
+   if (!empty($_POST['tickets_id']) && empty($_POST['changes_id'])){
+      $message = sprintf(__('Mandatory fields are not filled. Please correct: %s'),
+                         __('Change'));
+      Session::addMessageAfterRedirect($message, false, ERROR);
+      Html::back();
+   }
+   if (empty($_POST['tickets_id']) && !empty($_POST['changes_id'])){
+      $message = sprintf(__('Mandatory fields are not filled. Please correct: %s'),
+                         __('Ticket'));
+      Session::addMessageAfterRedirect($message, false, ERROR);
+      Html::back();
+   }
    $item->check(-1, CREATE, $_POST);
 
    if ($newID = $item->add($_POST)) {
