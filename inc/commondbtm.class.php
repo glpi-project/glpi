8aa774f89cb730d3f9edb33bb367e6acaddb12bb
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-05-03 14:48:40 +0200

    Readd isActive and mayBeActive function; fixes  #807
    This reverts commit c7fbc8677f8a34f7003b86c4821754d45799dd62.

--- inc/commondbtm.class.php
+++ inc/commondbtm.class.php
@@ -2831,6 +2831,36 @@ class CommonDBTM extends CommonGLPI {
 
 
    /**
+    * Can object be activated
+    *
+    * @return boolean
+    **/
+   function maybeActive() {
+
+      if (!isset($this->fields['id'])) {
+         $this->getEmpty();
+      }
+      return array_key_exists('is_active', $this->fields);
+   }
+
+
+   /**
+    * Is the object active
+    *
+    * @return boolean
+    **/
+   function isActive() {
+
+      if ($this->maybeActive()) {
+         return $this->fields["is_active"];
+      }
+      // Return integer value to be used to fill is_active field
+      return 1;
+
+   }
+
+
+   /**
     * Is the object may be a template
     *
     * @return boolean
