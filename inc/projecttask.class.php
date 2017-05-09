09c5af499b0011a2fe207e061238f6ab75c22158
yllen <nini.lasson@orange.fr>
2017-05-09 22:12:59 +0200

    fix type in dropdown project task type - see #2180

--- inc/projecttask.class.php
+++ inc/projecttask.class.php
@@ -1010,7 +1010,7 @@ class ProjectTask extends CommonDBChild {
                                                ? getSonsOf('glpi_entities',
                                                            $task->fields['entities_id'])
                                                : $task->fields['entities_id']),
-                         );
+                         'checkright'      => true);
          $addrand = Dropdown::showSelectItemFromItemtypes($params);
 
          echo "</td>";
