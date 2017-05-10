36441df22eca37e94e1736034e85edd56267467e
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-05-10 08:17:55 +0200

    Revert "fix ticket tab of an item - see #2078"
    This reverts commit d13c4843f6759ecd27ea8187dd72c310dc97f6f4.

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -5294,12 +5294,11 @@ class Ticket extends CommonITILObject {
                                        AND `glpi_tickets_users`.`users_id`
                                             = '".Session::getLoginUserID()."')
                                    OR `glpi_groups_tickets`.`groups_id` IN (".implode(",",$_SESSION['glpigroups'])."))";
-
             }
-            // with this part all incoming ticket will be displayed even they are not selected in the ticket
-            // if (Session::haveRightsAnd(self::$rightname, [self::READASSIGN, self::ASSIGN])) {
-            //   $restrict .= " OR `glpi_tickets`.`status` = ".self::INCOMING.")";
-            //}
+
+            if (Session::haveRightsAnd(self::$rightname, [self::READASSIGN, self::ASSIGN])) {
+               $restrict .= " OR (glpi_tickets.status=".self::INCOMING.")";
+            }
 
             $order    = '`glpi_tickets`.`date_mod` DESC';
 
@@ -5321,8 +5320,8 @@ class Ticket extends CommonITILObject {
       $query = "SELECT ".self::getCommonSelect()."
                 FROM `glpi_tickets` ".self::getCommonLeftJoin()."
                 WHERE $restrict ".
-                      " AND glpi_tickets.is_deleted = 0 ".
                       getEntitiesRestrictRequest("AND","glpi_tickets")."
+                AND glpi_tickets.is_deleted = 0
                 ORDER BY $order
                 LIMIT ".intval($_SESSION['glpilist_limit']);
       $result = $DB->query($query);
