878a2f756a80231acaa90bfc7aa584356718db8c
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-05-15 11:38:54 +0200

    attempt to clean the mess in ticket update right

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -229,8 +229,7 @@ class Ticket extends CommonITILObject {
       if ($_SESSION["glpiactiveprofile"]["interface"] == "helpdesk") {
          return Session::haveRight(self::$rightname, CREATE);
       }
-      return Session::haveRightsOr(self::$rightname,
-                                   array(UPDATE, self::ASSIGN, self::STEAL, self::OWN));
+      return parent::canUpdate();
    }
 
 
@@ -428,18 +427,16 @@ class Ticket extends CommonITILObject {
    **/
    function canUpdateItem() {
 
-      if (!Session::haveAccessToEntity($this->getEntityID())) {
+      if (!$this->checkEntity()) {
          return false;
       }
 
-      if ($_SESSION["glpiactiveprofile"]["interface"] == "helpdesk"
-          && ($this->fields['status'] != self::INCOMING
-            || $this->numberOfFollowups() > 0
-            || $this->numberOfTasks() > 0
-          )
+      if ($this->fields['status'] == self::INCOMING
+          && $this->numberOfFollowups() == 0
+          && $this->numberOfTasks() == 0
           && ($this->isUser(CommonITILActor::REQUESTER,Session::getLoginUserID())
               || $this->fields["users_id_recipient"] === Session::getLoginUserID())) {
-         return false;
+         return true;
       }
 
       return static::canUpdate();
@@ -3854,9 +3851,9 @@ class Ticket extends CommonITILObject {
       // Put ticket template on $values for actors
       $values['_tickettemplate'] = $tt;
 
-      $canupdate                 = Session::haveRight(self::$rightname, UPDATE);
-      $canpriority               = Session::haveRight(self::$rightname, self::CHANGEPRIORITY);
-      $canstatus                 = $canupdate;
+      // check right used for this ticket
+      $canupdate   = !$ID || $this->canUpdateItem();
+      $canpriority = Session::haveRight(self::$rightname, self::CHANGEPRIORITY);
 
       if ($ID && in_array($this->fields['status'], $this->getClosedStatusArray())) {
          $canupdate = false;
@@ -3884,12 +3881,6 @@ class Ticket extends CommonITILObject {
       $colsize2 = '29';
       $colsize3 = '13';
       $colsize4 = '45';
-
-      $canupdate_descr = $canupdate
-                         || (($this->fields['status'] == self::INCOMING)
-                             && $this->isUser(CommonITILActor::REQUESTER, Session::getLoginUserID())
-                             && ($this->numberOfFollowups() == 0)
-                             && ($this->numberOfTasks() == 0));
 
       if (!$options['template_preview']) {
          echo "<form method='post' name='form_ticket' enctype='multipart/form-data' action='".
@@ -4040,7 +4031,7 @@ class Ticket extends CommonITILObject {
                                              $tt->getMandatoryMark('type'))."</th>";
       echo "<td width='$colsize2%'>";
       // Permit to set type when creating ticket without update right
-      if ($canupdate || !$ID) {
+      if ($canupdate) {
          $opt = array('value' => $this->fields["type"]);
          /// Auto submit to load template
          if (!$ID) {
@@ -4065,9 +4056,7 @@ class Ticket extends CommonITILObject {
                                              $tt->getMandatoryMark('itilcategories_id'))."</th>";
       echo "<td width='$colsize4%'>";
       // Permit to set category when creating ticket without update right
-      if ($canupdate
-          || !$ID
-          || $canupdate_descr) {
+      if ($canupdate) {
 
          $opt = array('value'  => $this->fields["itilcategories_id"],
                       'entity' => $this->fields["entities_id"]);
@@ -4121,7 +4110,7 @@ class Ticket extends CommonITILObject {
       echo $tt->getEndHiddenFieldText('status')."</th>";
       echo "<td width='$colsize2%'>";
       echo $tt->getBeginHiddenFieldValue('status');
-      if ($canstatus) {
+      if ($canupdate) {
          self::dropdownStatus(array('value'     => $this->fields["status"],
                                     'showtype'  => 'allowed'));
          TicketValidation::alertValidation($this, 'status');
@@ -4157,9 +4146,7 @@ class Ticket extends CommonITILObject {
       echo $tt->getEndHiddenFieldText('urgency')."</th>";
       echo "<td>";
 
-      if (($canupdate && $canpriority)
-          || !$ID
-          || $canupdate_descr) {
+      if ($canupdate && $canpriority) {
          // Only change during creation OR when allowed to change priority OR when user is the creator
          echo $tt->getBeginHiddenFieldValue('urgency');
          $idurgency = self::dropdownUrgency(array('value' => $this->fields["urgency"]));
@@ -4252,7 +4239,7 @@ class Ticket extends CommonITILObject {
       echo $tt->getEndHiddenFieldText('locations_id')."</th>";
       echo "<td>";
       echo $tt->getBeginHiddenFieldValue('locations_id');
-      if ($canupdate || !$ID) {
+      if ($canupdate) {
          Location::dropdown(array('value'  => $this->fields['locations_id'],
                                   'entity' => $this->fields['entities_id']));
       } else {
@@ -4285,8 +4272,7 @@ class Ticket extends CommonITILObject {
          echo $tt->getEndHiddenFieldValue('priority', $this);
       }
 
-      if ($canupdate
-          || $canupdate_descr) {
+      if ($canupdate) {
          $params = array('urgency'  => '__VALUE0__',
                          'impact'   => '__VALUE1__',
                          'priority' => $idpriority);
@@ -4321,7 +4307,7 @@ class Ticket extends CommonITILObject {
       } else {
          echo "<td>";
          echo $tt->getBeginHiddenFieldValue('items_id');
-         $values['_canupdate'] = $canupdate || $canupdate_descr;
+         $values['_canupdate'] = $canupdate;
          Item_Ticket::itemAddForm($this, $values);
          echo $tt->getEndHiddenFieldValue('items_id', $this);
          echo "</td>";
@@ -4351,16 +4337,13 @@ class Ticket extends CommonITILObject {
          $this->showActorsPartForm($ID, $values);
       }
 
-      $view_linked_tickets = ($ID || $canupdate);
-
       echo "<table class='tab_cadre_fixe' id='mainformtable4'>";
       echo "<tr class='tab_bg_1'>";
       echo "<th style='width:$colsize1%'>".$tt->getBeginHiddenFieldText('name');
       printf(__('%1$s%2$s'), __('Title'), $tt->getMandatoryMark('name'));
       echo $tt->getEndHiddenFieldText('name')."</th>";
       echo "<td colspan='3'>";
-      if (!$ID
-          || $canupdate_descr) {
+      if ($canupdate) {
          echo $tt->getBeginHiddenFieldValue('name');
          echo "<input type='text' style='width:98%' maxlength=250 name='name' ".
                 " value=\"".Html::cleanInputText($this->fields["name"])."\">";
@@ -4378,8 +4361,7 @@ class Ticket extends CommonITILObject {
       echo "<tr class='tab_bg_1'>";
       echo "<th style='width:$colsize1%'>".$tt->getBeginHiddenFieldText('content');
       printf(__('%1$s%2$s'), __('Description'), $tt->getMandatoryMark('content'));
-      if (!$ID
-          || $canupdate_descr) {
+      if ($canupdate) {
          $content = Toolbox::unclean_cross_side_scripting_deep(Html::entity_decode_deep($this->fields['content']));
          Html::showTooltip(nl2br(Html::Clean($content)));
       }
@@ -4396,14 +4378,14 @@ class Ticket extends CommonITILObject {
          $this->fields["content"] = $this->setRichTextContent($content_id,
                                                                $this->fields["content"],
                                                                $rand,
-                                                               !$canupdate_descr);
+                                                               !$canupdate);
          $rows = 10;
       } else {
          $this->fields["content"] = $this->setSimpleTextContent($this->fields["content"]);
       }
 
       echo "<div id='content$rand_text'>";
-      if ($CFG_GLPI['use_rich_text'] || $canupdate_descr) {
+      if ($CFG_GLPI['use_rich_text'] || $canupdate) {
          echo "<textarea id='$content_id' name='content' style='width:100%' rows='$rows'>".
                $this->fields["content"]."</textarea></div>";
          if (!$CFG_GLPI["use_rich_text"]) {
@@ -4418,7 +4400,7 @@ class Ticket extends CommonITILObject {
       echo "</tr>";
 
       echo "<tr class='tab_bg_1'>";
-      if ($view_linked_tickets) {
+      if ($canupdate) {
          echo "<th style='width:$colsize1%'>". _n('Linked ticket', 'Linked tickets', Session::getPluralNumber());
          $rand_linked_ticket = mt_rand();
          if ($canupdate) {
@@ -4499,16 +4481,16 @@ class Ticket extends CommonITILObject {
 
       echo "</table>";
 
-      if ((!$ID
-           || $canupdate
-           || $canupdate_descr
-           || Session::haveRightsOr(self::$rightname, array(self::ASSIGN, self::STEAL, DELETE, PURGE)))
+      if (($canupdate
+           || $canpriority
+           || $this->canAssign()
+           || $this->canAssignTome())
           && !$options['template_preview']) {
 
          if ($ID) {
-            if (Session::haveRightsOr(self::$rightname, array(UPDATE, DELETE, PURGE))
+            if (self::canPurge()
                 || $this->canDeleteItem()
-                || $this->canUpdateItem()) {
+                || $canupdate) {
                echo "<div class='center'>";
                if ($this->fields["is_deleted"] == 1) {
                   if (self::canPurge()) {
@@ -4516,7 +4498,7 @@ class Ticket extends CommonITILObject {
                             _sx('button', 'Restore')."'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
                   }
                } else {
-                  if (self::canUpdate() ) {
+                  if ($canupdate || $canpriority) {
                      echo "<input type='submit' class='submit' name='update' value='".
                             _sx('button', 'Save')."'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
                   }
@@ -4564,7 +4546,7 @@ class Ticket extends CommonITILObject {
          if (!isset($rand)) {
             $rand = mt_rand();
          }
-         if ($canupdate_descr) {
+         if ($canupdate) {
             echo Html::initImagePasteSystem($content_id, $rand);
          }
       }
