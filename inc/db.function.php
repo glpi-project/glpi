76dd9716009100dc053d59b6bcb5ecd0ab6b061b
tomolimo <olivier.moron@araymond.com>
2017-06-27 11:49:13 +0200

    Improve speed of getEntiesRestrictRequet when entity list is huge
    Added function getAncestorsOfArray, to retrieve ancestors for an array of entities
    Fixes #2367

--- inc/db.function.php
+++ inc/db.function.php
@@ -644,6 +644,96 @@ function getTreeValueName($table, $ID, $wholename="", $level=0) {
  * Get the ancestors of an item in a tree dropdown
  *
  * @param $table     string   table name
+ * @param $items_id  array  The IDs of the items
+ *
+ * @return array of IDs of the ancestors
+**/
+function getAncestorsOfArray($table, $items_id) {
+   global $DB;
+
+   // IDs to be present in the final array
+   $id_found      = array();
+   $parentIDfield = getForeignKeyFieldForTable($table);
+   $use_cache     = FieldExists($table, "ancestors_cache");
+
+   if ($use_cache
+       && is_array($items_id)) {
+
+      $query = "SELECT `id`, `ancestors_cache`, `$parentIDfield`
+                FROM `$table`
+                WHERE `id` IN ('".implode("','", $items_id)."')";
+
+      foreach( $DB->request( $query ) as $row ) {
+         if( $row['id'] > 0 ) {
+            $ancestors = $row['ancestors_cache'];
+            $parent    = $row[$parentIDfield];
+
+            // Return datas from cache in DB
+            if (!empty($ancestors)) {
+               $id_found = array_replace( $id_found, importArrayFromDB($ancestors, true));
+            } else {
+               $loc_id_found = array();
+               // Recursive solution for table with-cache
+               if ($parent > 0) {
+                  $loc_id_found = getAncestorsOf($table, $parent);
+               }
+
+               // ID=0 only exists for Entities
+               if (($parent > 0)
+                   || ($table == 'glpi_entities')) {
+                  $loc_id_found[$parent] = $parent;
+               }
+
+               // Store cache datas in DB
+               $query = "UPDATE `$table`
+                      SET `ancestors_cache` = '".exportArrayToDB($loc_id_found)."'
+                      WHERE `id` = '".$row['id']."'";
+               $DB->query($query);
+
+               $id_found = array_replace($id_found, $loc_id_found);
+            }
+         }
+      }
+
+      return $id_found;
+   }
+
+   if (is_array($items_id)) {
+      // Get the ancestors
+      // iterative solution for table without cache
+      foreach( $items_id as $id ) {
+         $IDf = $id;
+         while ($IDf > 0) {
+            // Get next elements
+            $query = "SELECT `$parentIDfield`
+                FROM `$table`
+                WHERE `id` = '$IDf'";
+
+            $result = $DB->query($query);
+            if ($DB->numrows($result)>0) {
+               $IDf = $DB->result($result,0,0);
+            } else {
+               $IDf = 0;
+            }
+
+            if (!isset($id_found[$IDf])
+                && (($IDf > 0) || ($table == 'glpi_entities'))) {
+               $id_found[$IDf] = $IDf;
+            } else {
+               $IDf = 0;
+            }
+         }
+      }
+   }
+
+   return $id_found;
+}
+
+
+/**
+ * Get the ancestors of an item in a tree dropdown
+ *
+ * @param $table     string   table name
  * @param $items_id  integer  The ID of the item
  *
  * @return array of IDs of the ancestors
@@ -1720,10 +1810,7 @@ function getEntitiesRestrictRequest($separator="AND", $table="", $field="",$valu
    if ($is_recursive) {
       $ancestors = array();
       if (is_array($value)) {
-         foreach ($value as $val) {
-            $ancestors = array_unique(array_merge(getAncestorsOf("glpi_entities", $val),
-                                                  $ancestors));
-         }
+         $ancestors = getAncestorsOfArray("glpi_entities", $value);         
          $ancestors = array_diff($ancestors, $value);
 
       } else if (strlen($value) == 0) {
