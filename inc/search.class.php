1d77d0117c87cfaf4cfcb5454cff92d8461a14cb
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-07-12 10:42:49 +0200

    Ensure start is an integer; fixes #2449

--- inc/search.class.php
+++ inc/search.class.php
@@ -5087,6 +5087,10 @@ class Search {
       $default_values["sort"]        = 1;
       $default_values["is_deleted"]  = 0;
 
+      if (isset($params['start'])) {
+         $params['start'] = (int)$params['start'];
+      }
+
       if ($CFG_GLPI['allow_search_view'] == 2) {
          $default_criteria = 'view';
       } else {
