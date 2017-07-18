1a1e9e15f0dd52d68f18e0d64ab1ded199f3c263
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-07-18 10:52:01 +0200

    ensure condition rule field is an integer; fix #2476

--- inc/rule.class.php
+++ inc/rule.class.php
@@ -2584,7 +2584,7 @@ class Rule extends CommonDBTM {
 
       $add_condition = '';
       if ($p['condition'] > 0) {
-         $add_condition = ' AND `condition` & '.$p['condition'];
+         $add_condition = ' AND `condition` & '. (int) $p['condition'];
       }
 
       $p['condition'] = "`sub_type` = '".$p['sub_type']."' $add_condition";
