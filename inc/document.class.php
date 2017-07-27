e227e48c0969e2e4a2d98eeecc34dca0be5584aa
Thierry Bugier Pineau <tbugier@teclib.com>
2017-07-27 14:00:54 +0200

    simplify condition

--- inc/document.class.php
+++ inc/document.class.php
@@ -306,13 +306,12 @@ class Document extends CommonDBTM {
 
 
    public function post_getFromDB() {
-      // Check the user can view this itemtype and can view this item
-      if (isAPI() && $this->canView() && $this->canViewItem()) {
-         if (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] == 'application/octet-stream'
-               || isset($_GET['alt']) && $_GET['alt'] == 'media') {
-            $this->send();
-            exit();
-         }
+      if (isAPI()
+          && (isset($_SERVER['HTTP_ACCEPT']) && $_SERVER['HTTP_ACCEPT'] == 'application/octet-stream'
+              || isset($_GET['alt']) && $_GET['alt'] == 'media')) {
+         // This is a API request to download the document
+         $this->send();
+         exit();
       }
    }
 
