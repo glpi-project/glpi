db094776b991226b1598dcaa21d357153fd85df7
yllen <nini.lasson@orange.fr>
2017-08-06 13:19:24 +0200

    fix recursion

--- inc/computer_item.class.php
+++ inc/computer_item.class.php
@@ -885,24 +885,18 @@ class Computer_Item extends CommonDBRelation{
       global $DB, $CFG_GLPI;
 
       // RELATION : computers -> items
-      $object = new $item();
-      $table = getTableForItemType($object->getType());
-
       $sql = "SELECT `itemtype`, GROUP_CONCAT(DISTINCT `items_id`) AS ids, `computers_id`
               FROM `glpi_computers_items`
-              WHERE `glpi_computers_items`.`itemtype` = '".$object->getType()."'
+              WHERE `glpi_computers_items`.`itemtype` = '".$item->getType()."'
                     AND `glpi_computers_items`.`items_id` = ".$item->fields['id']."
               GROUP BY `itemtype`";
       $res = $DB->query($sql);
 
       if ($res) {
          while ($data = $DB->fetch_assoc($res)) {
-            $comp = new Computer();
-            if ($comp->isEntityAssign()) {
-               if (countElementsInTable("glpi_computers", "`id` IN (".$data["computers_id"].")
-                                         AND `entities_id` NOT IN $entities") > 0) {
-                  return false;
-               }
+            if (countElementsInTable("glpi_computers", "`id` IN (".$data["computers_id"].")
+                                      AND `entities_id` NOT IN $entities") > 0) {
+               return false;
             }
          }
       }
