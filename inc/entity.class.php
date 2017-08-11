3190331816b6191d41469d6f0c6ed41149f420ba
yllen <nini.lasson@orange.fr>
2017-08-11 13:21:56 +0200

    fix entity creation - fixes #2598
    Add tests

--- inc/entity.class.php
+++ inc/entity.class.php
@@ -234,6 +234,13 @@ class Entity extends CommonTreeDropdown {
    function prepareInputForAdd($input) {
       global $DB;
 
+      $input['name'] = isset($input['name']) ? trim($input['name']) : '';
+      if (empty($input["name"])) {
+         Session::addMessageAfterRedirect(__("You can't add an entity without name"),
+                                          false, ERROR);
+         return false;
+      }
+
       $input = parent::prepareInputForAdd($input);
 
       $query = "SELECT MAX(`id`)+1 AS newID
