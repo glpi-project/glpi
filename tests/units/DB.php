495b06d009f9e65386cdb76b8262fbf8999f16fb
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-09-12 13:49:36 +0200

    Improve tests on DB

--- tests/units/DB.php
+++ tests/units/DB.php
@@ -87,4 +87,15 @@ class DB extends atoum {
          $this->string($dbu->getTableForItemType($type))->isIdenticalTo($table);
       }
    }
+
+   public function testEscape() {
+      $this
+         ->if($this->newTestedInstance)
+         ->then
+            ->string($this->testedInstance->escape('nothing to do'))->isIdenticalTo('nothing to do')
+            ->string($this->testedInstance->escape("shoul'be escaped"))->isIdenticalTo("shoul\\'be escaped")
+            ->string($this->testedInstance->escape("First\nSecond"))->isIdenticalTo("First\\nSecond")
+            ->string($this->testedInstance->escape("First\rSecond"))->isIdenticalTo("First\\rSecond")
+            ->string($this->testedInstance->escape('Hi, "you"'))->isIdenticalTo('Hi, \\"you\\"');
+   }
 }
