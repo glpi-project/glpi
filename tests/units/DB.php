228d14ca729ef4c3760543a7de0bd57486442113
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-09-12 13:29:17 +0200

    Test list_tables and check if relative itemtypes do exists

--- tests/units/DB.php
+++ tests/units/DB.php
@@ -67,4 +67,24 @@ class DB extends atoum {
                ->withType(E_USER_WARNING)
                ->exists();
    }
+
+   public function testListTables() {
+      $dbu = new \DbUtils();
+      $this->newTestedInstance();
+      $list = $this->testedInstance->list_tables();
+      $this->object($list)->isInstanceOf('mysqli_result');
+      $this->integer($this->testedInstance->numrows($list))->isGreaterThan(200);
+
+      //check if each table has a corresponding itemtype
+      while ($line = $this->testedInstance->fetch_array($list)) {
+         $this->array($line)
+            ->hasSize(2);
+         $table = $line[0];
+         $type = $dbu->getItemTypeForTable($table);
+
+         $this->object($item = $dbu->getItemForItemtype($type))->isInstanceOf('CommonDBTM');
+         $this->string(get_class($item))->isIdenticalTo($type);
+         $this->string($dbu->getTableForItemType($type))->isIdenticalTo($table);
+      }
+   }
 }
