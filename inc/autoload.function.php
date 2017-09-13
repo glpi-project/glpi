6e9171f353addeebac79d3231650113bc3fbfa80
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-09-13 11:42:45 +0200

    Add deprecated message

--- inc/autoload.function.php
+++ inc/autoload.function.php
@@ -310,6 +310,7 @@ function glpi_autoload($classname) {
    //hack for \Zend\Loader\SplAutoloader :(
    //@since 9.2 -- WILL BE REMOVED IN FUTURE RELEASE
    if ($classname === 'Zend\\Loader\\SplAutoloader') {
+      Toolbox::deprecated('Zend\Loader\SplAutoloader has been dropped from GLPI.');
       require_once GLPI_ROOT . '/lib/zend-splautoloader.class.php';
       return true;
    }
