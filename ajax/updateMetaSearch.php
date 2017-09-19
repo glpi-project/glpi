0c89d4a798465b00e4d7342de0941fa2c374173a
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-09-19 12:24:01 +0200

    Fix forbidden class call

--- ajax/updateMetaSearch.php
+++ ajax/updateMetaSearch.php
@@ -38,7 +38,7 @@ include ('../inc/includes.php');
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 
-if (!($item = getItemForItemtype($_POST['itemtype']))) {
+if (!isset($_POST['itemtype']) || empty($_POST['itemtype']) || !($item = getItemForItemtype($_POST['itemtype']))) {
    exit();
 }
 
