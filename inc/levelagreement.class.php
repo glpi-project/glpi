0cab19ad47e8e835e0efa6fc25e97584203f7007
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-09-20 16:46:12 +0200

    Typo

--- inc/levelagreement.class.php
+++ inc/levelagreement.class.php
@@ -296,7 +296,7 @@ abstract class LevelAgreement extends CommonDBChild {
                function delete_date$type$rand(e) {
                   e.preventDefault();
 
-                  if (nativeConfirm('".addslashes(__('Also delete date ?'))."')) {
+                  if (nativeConfirm('".addslashes(__('Also delete date?'))."')) {
                      submitGetLink('$ticket_url',
                                    ".json_encode(array_merge($fields, ['delete_date' => 1])).");
                   } else {
