c52fbf2429817a86e4f4e97443080716cc637f09
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-09-21 15:27:57 +0200

    Instanciate cache at the end, to prevent errors on some script calls

--- inc/config.php
+++ inc/config.php
@@ -47,26 +47,8 @@ Session::start();
 
 //init cache
 $GLPI_CACHE = false;
-try {
-   if (function_exists('apcu_fetch') && (!defined('TU_USER') || defined('CACHED_TESTS'))) {
-      //ZendCache does not works with PHP5 acpu...
-      $adapter = (version_compare(PHP_VERSION, '7.0.0') >= 0) ? 'apcu' : 'apc';
-      $GLPI_CACHE = Zend\Cache\StorageFactory::factory([
-         'adapter'   => $adapter,
-         'options'   => [
-            'namespace' => 'glpicache' . GLPI_VERSION
-         ]
-      ]);
-   }
-} catch (Exception $e) {
-   $GLPI_CACHE = false;
-   if (Session::DEBUG_MODE == $_SESSION['glpi_use_mode']) {
-      Toolbox::logDebug($e->getMessage());
-   }
-}
 
 Config::detectRootDoc();
-
 
 if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
    Session::loadLanguage();
@@ -306,4 +288,22 @@ if (!file_exists(GLPI_CONFIG_DIR . "/config_db.php")) {
       exit();
    }
 
+}
+
+try {
+   if (function_exists('apcu_fetch') && (!defined('TU_USER') || defined('CACHED_TESTS'))) {
+      //ZendCache does not works with PHP5 acpu...
+      $adapter = (version_compare(PHP_VERSION, '7.0.0') >= 0) ? 'apcu' : 'apc';
+      $GLPI_CACHE = Zend\Cache\StorageFactory::factory([
+         'adapter'   => $adapter,
+         'options'   => [
+            'namespace' => 'glpicache' . GLPI_VERSION
+         ]
+      ]);
+   }
+} catch (Exception $e) {
+   $GLPI_CACHE = false;
+   if (Session::DEBUG_MODE == $_SESSION['glpi_use_mode']) {
+      Toolbox::logDebug($e->getMessage());
+   }
 }
