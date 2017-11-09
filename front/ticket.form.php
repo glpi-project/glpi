2829ab5eb4b35abeed29876f9027697414d2ec05
tomolimo <olivier.moron@araymond.com>
2017-11-09 15:14:32 +0100

    Added unset for $_GET['id'] and $_POST['id'] (#3122)
    Fixes #3119

--- front/ticket.form.php
+++ front/ticket.form.php
@@ -232,6 +232,9 @@ if (isset($_GET["id"]) && ($_GET["id"] > 0)) {
 } else {
    Html::header(__('New ticket'), '', "helpdesk", "ticket");
    unset($_REQUEST['id']);
+   unset($_GET['id']);
+   unset($_POST['id']);
+   
    // alternative email must be empty for create ticket
    unset($_REQUEST['_users_id_requester_notif']['alternative_email']);
    unset($_REQUEST['_users_id_observer_notif']['alternative_email']);
