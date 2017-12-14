ebb98a19f06cf3c4cd008182ada29898a551e6bd
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-12-14 15:23:58 +0100

    Fix variable name

--- front/dcroom.form.php
+++ front/dcroom.form.php
@@ -52,7 +52,7 @@ if (isset($_POST["add"])) {
       Event::log($newID, "serverroms", 4, "inventory",
                  sprintf(__('%1$s adds the item %2$s'), $_SESSION["glpiname"], $_POST["name"]));
       if ($_SESSION['glpibackcreated']) {
-         Html::redirect($datacenter->getLinkURL());
+         Html::redirect($room->getLinkURL());
       }
    }
    Html::back();
