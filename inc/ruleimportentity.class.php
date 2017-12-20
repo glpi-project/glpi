1c9e239f58021ba8facd674597cae156e6f66b79
amma35 <amandine.manceau@infotel.com>
2017-12-20 17:34:41 +0100

    Add the "Group" action in "Rules for assigning an item to an entity" see #3332

--- inc/ruleimportentity.class.php
+++ inc/ruleimportentity.class.php
@@ -52,7 +52,7 @@ class RuleImportEntity extends Rule {
    **/
    function maxActionsCount() {
       // Unlimited
-      return 2;
+      return 3;
    }
 
    /**
@@ -165,6 +165,10 @@ class RuleImportEntity extends Rule {
       $actions['locations_id']['name']     = __('Location');
       $actions['locations_id']['type']     = 'dropdown';
       $actions['locations_id']['table']    = 'glpi_locations';
+
+      $actions['groups_id']['name']     = __('Group');
+      $actions['groups_id']['type']     = 'dropdown';
+      $actions['groups_id']['table']    = 'glpi_groups';
 
       $actions['_ignore_import']['name']   = __('To be unaware of import');
       $actions['_ignore_import']['type']   = 'yesonly';
