1645113d2d09ecb310df728fccce0c2ff144489b
Anael Mobilia <contrib@anael.eu>
2018-01-07 15:53:00 +0100

    Clean IE 6 specific JS code + remove some "historic" (12+ years) functions no more used

--- js/common.js
+++ js/common.js
@@ -50,29 +50,6 @@ function setdisplay(objet, statut) {
 
 
 /**
- * tester le type de navigateur
-**/
-function isIe() {
-
-   var ie     = false;
-   var appVer = navigator.appVersion.toLowerCase();
-   var iePos  = appVer.indexOf('msie');
-
-   if (iePos != -1) {
-      var is_minor = parseFloat(appVer.substring(iePos+5,appVer.indexOf(';',iePos)));
-      var is_major = parseInt(is_minor);
-   }
-   if (navigator.appName.substring(0,9) == "Microsoft") {
-      // Check if IE version is 6 or older
-      if (is_major <= 6) {
-         ie = true;
-      }
-   }
-   return ie;
-}
-
-
-/**
  * @param id
 **/
 function cleandisplay(id) {
@@ -80,9 +57,6 @@ function cleandisplay(id) {
    var e = document.getElementById(id);
    if (e) {
       setdisplay(e,'block');
-      if (isIe()) {
-         doHideSelect(e);
-      }
    }
 }
 
@@ -94,66 +68,7 @@ function cleanhide(id) {
 
    var e = document.getElementById(id);
    if (e) {
-      if (isIe()) {
-         doShowSelect(e);
-      }
       setdisplay(e,'none');
-   }
-}
-
-
-/**
- * @param id
-**/
-function completecleandisplay(id) {
-
-   var e = document.getElementById(id);
-   if (e) {
-      setdisplay(e,'block');
-
-      if (isIe()) {
-         e.onmouseleave = function(){ completecleanhide(id) };
-         hideSelect(0,0,document.documentElement.clientWidth,document.documentElement.clientHeight);
-      } else {
-         e.onmouseout = function(){ completecleanhide(id) };
-      }
-   }
-}
-
-
-/**
- * @param id
-**/
-function completecleanhide(id) {
-
-   var e = document.getElementById(id);
-   if (e) {
-      setdisplay(e,'none');
-      if (isIe()) {
-         showSelect(0,0,document.documentElement.clientWidth,document.documentElement.clientHeight);
-      }
-   }
-}
-
-
-/**
- * effacer tous les menus du menu principal
- * afficher les select du document
- *
- * @param idMenu
-**/
-function hidemenu(idMenu) {
-
-   var e = document.getElementById(idMenu);
-   var e = e.getElementsByTagName('ul');
-
-   for (var i=0; i<e.length; i++) {
-      if (e[i]) {
-         if (isIe()) {
-            doShowSelect(e[i]);
-         }
-         setdisplay(e[i],'none');
-      }
    }
 }
 
@@ -166,129 +81,6 @@ function hidemenu(idMenu) {
 function afterView(idMenu) {
 
    setdisplay(idMenu,'none');
-   if (isIe()) {
-      doShowSelect(idMenu);
-   }
-}
-
-
-/**
- * execute la fonction showSelect
- *
- * @param objet
-**/
-function doShowSelect(objet) {
-
-   if (objet) {
-      //correction du bugg sur IE
-      if (isIe()) {
-         if (setdisplay(objet,'block')) {
-            var selx = 0;
-            var sely = 0;
-            var selp;
-            selx = getLeft(objet);
-            sely = getTop(objet);
-            selw = objet.offsetWidth;
-            selh = objet.offsetHeight;
-            showSelect(selx,sely,selw,selh);
-         }
-         if (setdisplay(objet,'none')) {
-            return true;
-         }
-      }
-   }
-}
-
-
-/**
- * affiche les select du document
- *
- * @param x
- * @param y
- * @param w
- * @param h
-**/
-function showSelect(x,y,w,h) {
-
-   var selx,sely,selw,selh;
-   var sel = document.getElementsByTagName("SELECT");
-
-   for (var i=0; i<sel.length; i++) {
-      selx = 0;
-      sely = 0;
-      var selp;
-      selx = getLeft(sel[i]);
-      sely = getTop(sel[i]);
-      selw = sel[i].offsetWidth;
-      selh = sel[i].offsetHeight;
-      // || Manage position error computation
-      if ((((selx + selw) > x)
-           && (selx < (x + w))
-           && ((sely + selh) > y)
-           && (sely < (y + h)))
-          || (selx < 0)
-          || (sely < 0)) {
-         sel[i].style.visibility="visible";
-      }
-   }
-   return true;
-}
-
-
-/**
- * execute la fonction hideMenu
- *
- * @param objet
-**/
-function doHideSelect(object) {
-
-   var e = object;
-   if (isIe()) {
-      var selx = 0;
-      var sely = 0;
-      var selp;
-
-      selx = getLeft(e);
-      sely = getTop(e);
-      selw = e.offsetWidth;
-      selh = e.offsetHeight;
-      hideSelect(selx,sely,selw,selh);
-   }
-   return true;
-}
-
-
-/**
- * masque les select du document
- *
- * @param x
- * @param y
- * @param w
- * @param h
-**/
-function hideSelect(x,y,w,h) {
-
-   var selx,sely,selw,selh,i;
-   var sel = document.getElementsByTagName("SELECT");
-   for (i=0; i<sel.length; i++) {
-      selx = 0;
-      sely = 0;
-      var selp;
-      selx = getLeft(sel[i]);
-      sely = getTop(sel[i]);
-      selw = sel[i].offsetWidth;
-      selh = sel[i].offsetHeight;
-      // || Manage position error computation
-      if ((((selx + selw) > x)
-           && (selx < (x + w))
-           && ((sely + selh) > y)
-           && (sely < (y + h)))
-          || (selx < 0)
-          || (sely < 0 )) {
-         sel[i].style.visibility="hidden";
-      }
-   }
-   return true;
 }
 
 
@@ -306,19 +98,6 @@ function menuAff(id,idMenu){
       }
    }
    var m = m.getElementsByTagName('ul');
-   if (isIe()) {
-      //masquage des elements select du document
-      if (m) {
-         for (var i=1; i<10; i++) { //probleme dans le listage et le nomage des menus xhtml
-            //listage des elements li nommes du type smenu + i
-            var e = document.getElementById('menu'+i);
-            if (e) {
-               var smenu = e.getElementsByTagName('ul');
-               doShowSelect(smenu[0]);
-            }
-         }
-      }
-   }
 
    if (ssmenu) {
       var smenu = ssmenu.getElementsByTagName('ul');
@@ -329,43 +108,9 @@ function menuAff(id,idMenu){
          }
          setdisplay(smenu[0],'block');
          clearTimeout(timeoutglobalvar);
-         //timeoutglobalvar = setTimeout(function(){afterView(smenu[0])},1000);
-         if (isIe()) {
-            ssmenu.onmouseleave = function(){ timeoutglobalvar = setTimeout(function(){afterView(smenu[0])},300); };
-            doHideSelect(smenu[0]);
-         } else {
-            ssmenu.onmouseout = function(){ timeoutglobalvar = setTimeout(function(){afterView(smenu[0])},300); };
-         }
+         ssmenu.onmouseout = function(){ timeoutglobalvar = setTimeout(function(){afterView(smenu[0])},300); };
       }
    }
-}
-
-
-/**
- * @param URL_List
-**/
-function jumpTo(URL_List) {
-   var URL = URL_List.options[URL_List.selectedIndex].value;  window.location.href = URL;
-}
-
-
-browserName = navigator.appName;
-browserVer  = parseInt(navigator.appVersion);
-if (((browserName == "Netscape") && (browserVer >= 3))
-    || ((browserName == "Microsoft Internet Explorer") && (browserVer >= 4))) {
-   version = "n3";
-} else {
-   version = "n2";
-}
-
-
-function historyback() {
-   history.back();
-}
-
-
-function historyforward() {
-   history.forward();
 }
 
 
@@ -419,30 +164,6 @@ function unMarkCheckboxes(container_id) {
    return true;
 }
 
-
-/**
- * toggle all checkboxes inside the given element
- * the given element is usaly a table or a div containing the table or tables
- *
- * @param    container_id    DOM element
-**/
-function toggleCheckboxes( container_id ) {
-
-   var checkboxes = document.getElementById(container_id).getElementsByTagName('input');
-   for (var j=0; j<checkboxes.length; j++ ) {
-      checkbox = checkboxes[j];
-      if (checkbox && (checkbox.type == 'checkbox')) {
-         if (checkbox.disabled == false) {
-            if (checkbox.checked == false) {
-               checkbox.checked = true;
-            } else {
-               checkbox.checked = false;
-            }
-         }
-      }
-   }
-   return true;
-}
 
 /**
  * display "other" text input field in case of selecting "other" option
@@ -523,52 +244,6 @@ $.fn.shiftSelectable = function() {
       lastChecked = selected_checkbox;
    });
 };
-
-
-/**
- * @param text
- * @param where
-**/
-function confirmAction(text, where){
-
-   if (confirm(text)) {
-      window.location = where;
-   }
-}
-
-
-/**
- * Fonction permettant de connaître la position d'un objet par rapport au bord gauche de la page.
- * Cet objet peut être à l'intérieur d'un autre objet.
- *
- * @param MyObject
- *
- * @returns la distance par rapport au bord gauche de la page
-**/
-function getLeft(MyObject){
-
-   if (MyObject.offsetParent) {
-      return (MyObject.offsetLeft + getLeft(MyObject.offsetParent));
-   }
-   return (MyObject.offsetLeft);
-}
-
-
-/**
- * Fonction permettant de connaître la position d'un objet par rapport au bord haut de la page.
- * Cet objet peut être à l'intérieur d'un autre objet.
- *
- * @param MyObject
- *
- * @returns la distance par rapport au bord haut de la page
-**/
-function getTop(MyObject){
-
-   if (MyObject.offsetParent) {
-      return (MyObject.offsetTop + getTop(MyObject.offsetParent));
-   }
-   return (MyObject.offsetTop);
-}
 
 
 /**
@@ -998,7 +673,7 @@ var urlExists = function(url) {
       'async':   false,
       'success': function() {
          exist = true;
-      },
+      }
    });
 
    return exist;
@@ -1207,4 +882,4 @@ var showMapForLocation = function(elt) {
       //hide spinner
       map_elt.spin(false);
    });
-}
+};
