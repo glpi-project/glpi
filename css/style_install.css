418a30ff8f503aaeabaefcaaaf8ffdb222e92ab2
Anael Mobilia <AnaelMobilia@users.noreply.github.com>
2018-01-08 16:50:19 +0100

    Update copyright date to 2018 (#3390)
    * Update copyright to 2018 (modify_headers.pl)
    
    * Remove trailing line feed before licence header
    
    * revert licence.txt
    
    * Rename licence.txt to license.txt

--- css/style_install.css
+++ css/style_install.css
@@ -1,7 +1,7 @@
 /**
  * ---------------------------------------------------------------------
  * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2015-2017 Teclib' and contributors.
+ * Copyright (C) 2015-2018 Teclib' and contributors.
  *
  * http://glpi-project.org
  *
