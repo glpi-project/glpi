ede5e4af94156bbd44192c1830639f2149c0cc59
Anael Mobilia <AnaelMobilia@users.noreply.github.com>
2018-01-08 10:56:11 +0100

    Don't display GLPI version on login page (#3396)
    Fix #2976

--- inc/html.class.php
+++ inc/html.class.php
@@ -5567,16 +5567,19 @@ class Html {
    }
 
    /**
-    * Get copyright message as HTML (used in footers)
-    *
+    * Get copyright message in HTML (used in footers)
     * @since 9.1
-    *
-    * @return text
+    * @param boolean $withVersion include GLPI version ?
+    * @return string HTML copyright
     */
-   static function getCopyrightMessage() {
-      $message = "<a href=\"http://glpi-project.org/\" title=\"Powered By Teclib\" class=\"copyright\">";
-      $message .= "GLPI " . GLPI_VERSION .
-         " Copyright (C) 2015-" . GLPI_YEAR . " Teclib' and contributors".
+   static function getCopyrightMessage($withVersion = true) {
+      $message = "<a href=\"http://glpi-project.org/\" title=\"Powered by Teclib and contributors\" class=\"copyright\">";
+      $message .= "GLPI ";
+      // if required, add GLPI version (eg not for login page)
+      if ($withVersion) {
+          $message .= GLPI_VERSION . " ";
+      }
+      $message .= "Copyright (C) 2015-" . GLPI_YEAR . " Teclib' and contributors".
          " - Copyright (C) 2003-2015 INDEPNET Development Team".
          "</a>";
       return $message;
