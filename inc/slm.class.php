d1b624023f07347eb1d49a8f93dd37b997595caf
Amandine Manceau <amandine.manceau@infotel.com>
2018-01-09 15:08:25 +0100

    Rights issue in the SLM menu fix #3361 (#3362)

--- inc/slm.class.php
+++ inc/slm.class.php
@@ -171,11 +171,13 @@ class SLM extends CommonDBTM {
    static function getMenuContent() {
 
       $menu = [];
-      if (Config::canUpdate()) {
+      if (static::canView()) {
          $menu['title']           = self::getTypeName(2);
          $menu['page']            = '/front/slm.php';
          $menu['links']['search'] = '/front/slm.php';
-         $menu['links']['add']    = '/front/slm.form.php';
+         if (static::canCreate()) {
+            $menu['links']['add'] = '/front/slm.form.php';
+         }
 
          $menu['options']['sla']['title']           = SLA::getTypeName(1);
          $menu['options']['sla']['page']            = '/front/sla.php';
