5fb78439e6236c38c1200625fe2d3cd059f23905
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-01-16 14:29:47 +0100

    Try test update failure

---
+++ tests/test_fail_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# check tat an error in update scripts does return a non zero code
+testfail() {
+   sed -e "s/\$migration->setVersion('9.3');/\0\n\$DB->queryOrDie('SELECT * FROM notatable');/" \
+      -i install/update_92_93.php
+   php tools/cliupdate.php --config-dir=../tests
+   RETVAL=$?
+   if [ $RETVAL -ne 0 ]
+   then
+      return 0
+   else
+      return 1
+   fi
+}
+testfail
