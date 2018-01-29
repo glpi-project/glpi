5659928671a7d2522d892bda00093d52f3f035c8
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-01-29 09:32:33 +0100

    Ask if translations are up-to-date

--- tools/make_release.sh
+++ tools/make_release.sh
@@ -35,6 +35,13 @@ then
  exit ;
 fi
 
+read -p "Are translations up to date? [Y/n] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
 INIT_DIR=$1;
 RELEASE=$2;
 
