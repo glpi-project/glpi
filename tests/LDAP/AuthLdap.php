d3b248d41b1a0255bf7388ee41235c407742cd2c
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2018-03-09 10:20:14 +0100

    add sso tests (#3690)

--- tests/LDAP/AuthLdap.php
+++ tests/LDAP/AuthLdap.php
@@ -765,4 +765,41 @@ class AuthLDAP extends DbTestCase {
       $this->boolean($user->getFromDB($uid))->isTrue();
       $this->boolean((bool)$user->fields['is_deleted'])->isTrue();
    }
+
+   protected function ssoVariablesProvider() {
+      global $DB;
+
+      $iterator = $DB->request(\SsoVariable::getTable());
+      $sso_vars = [];
+      foreach ($iterator as $current) {
+         $sso_vars[] = [$current['id'], $current['name']];
+      }
+
+      return $sso_vars;
+   }
+
+   /**
+    * @dataProvider ssoVariablesProvider
+    */
+   public function testOtherAuth($sso_field_id, $sso_field_name) {
+      global $CFG_GLPI;
+
+      $config_values = \Config::getConfigurationValues('core', ['ssovariables_id']);
+      \Config::setConfigurationValues('core', [
+         'ssovariables_id' => $sso_field_id
+      ]);
+      $CFG_GLPI['ssovariables_id'] = $sso_field_id;
+      $_SERVER[$sso_field_name] = 'brazil6';
+
+      unset($_SESSION['glpiname']);
+
+      $auth = new \Auth;
+      $this->boolean($auth->login("", ""))->isTrue();
+      $this->string($_SESSION['glpiname'])->isEqualTo('brazil6');
+
+      //reset config
+      \Config::setConfigurationValues('core', [
+         'ssovariables_id' => $config_values['ssovariables_id']
+      ]);
+   }
 }
