e6a369479179607300063039570ed6f0c26198b5
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-03-26 12:26:00 +0200

    CS

--- twig_extensions/GlpiDebug.php
+++ twig_extensions/GlpiDebug.php
@@ -73,23 +73,6 @@ class GlpiDebug extends \Twig_Extension
          </span>";
       }
 
-      /*
-<div class="box box-success box-solid">
-            <div class="box-header with-border">
-              <h3 class="box-title">Removable</h3>
-
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-              </div>
-              <!-- /.box-tools -->
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              The body of the box
-            </div>
-            <!-- /.box-body -->
-          </div>
-       */
       $panel .= "<div id='debugtabs' class='hidden box box-warning box-solid'>";
       $panel .= "      <div class='box-header with-border'>
               <h3 class='box-title'>Debug panel</h3>
