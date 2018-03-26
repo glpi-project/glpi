b5b4cdd9fbf969328d599f616153790671522e8f
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-03-26 09:04:53 +0200

    Item type must be specified; handle posted itemtype
    fixes #3749

--- inc/commondropdown.class.php
+++ inc/commondropdown.class.php
@@ -634,6 +634,7 @@ abstract class CommonDropdown extends CommonDBTM {
          echo "<form action='$target' method='post'>";
          echo "<table class='tab_cadre'><tr>";
          echo "<td><input type='hidden' name='id' value='$ID'>";
+         echo "<input type='hidden' name='itemtype' value='".$this->getType()."' />";
          echo "<input type='hidden' name='forcepurge' value='1'>";
          echo "<input class='submit' type='submit' name='purge'
                 value=\""._sx('button', 'Confirm')."\">";
@@ -666,6 +667,7 @@ abstract class CommonDropdown extends CommonDBTM {
                               'used'   => [$ID]]);
       }
       echo "<input type='hidden' name='id' value='$ID' />";
+      echo "<input type='hidden' name='itemtype' value='".$this->getType()."' />";
       echo "</td><td>";
       echo "<input class='submit' type='submit' name='replace' value=\""._sx('button', 'Replace')."\">";
       echo "</td><td>";
