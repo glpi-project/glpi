227197886c099741863ea97a18a1dd492c322bb5
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-03-26 09:04:53 +0200

    Item type must be specified; handle posted itemtype
    fixes #3749

--- front/device.form.php
+++ front/device.form.php
@@ -32,13 +32,17 @@
 
 include ('../inc/includes.php');
 
-if (!isset($_GET['itemtype']) || !class_exists($_GET['itemtype'])) {
+if ((!isset($_GET['itemtype']) || !class_exists($_GET['itemtype']))
+   && (!isset($_POST['itemtype']) || !class_exists($_POST['itemtype']))
+) {
    throw new \RuntimeException(
       'Missing or incorrect device type called!'
    );
 }
+
+$itemtype = isset($_POST['itemtype']) ? $_POST['itemtype'] : $_GET['itemtype'];
 $options = [
-   'itemtype' => $_GET['itemtype']
+   'itemtype' => $itemtype
 ];
-$dropdown = new $_GET['itemtype'];
+$dropdown = new $itemtype;
 include (GLPI_ROOT . "/front/dropdown.common.form.php");
