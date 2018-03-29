e5e52f01f2bc1904c7cf375be48e9dbf3aaf88b7
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-03-29 10:23:39 +0200

    Keep files infos; fixes #3741

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -1086,6 +1086,9 @@ class Ticket extends CommonITILObject {
                 $allowed_fields[] = 'itilcategories_id';
                 $allowed_fields[] = 'name';
                 $allowed_fields[] = 'items_id';
+                $allowed_fields[] = '_filename';
+                $allowed_fields[] = '_tag_filename';
+                $allowed_fields[] = '_prefix_filename';
             }
 
             if ($this->canSolve()) {
