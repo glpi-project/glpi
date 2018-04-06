9c75f1d569c1e67f76eedfdbd8bde4702ecb3248
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-04-06 07:08:34 +0200

    Drop not needed tab

--- inc/computer.class.php
+++ inc/computer.class.php
@@ -92,7 +92,6 @@ class Computer extends CommonDBTM {
 
       $ong = [];
       $this->addDefaultFormTab($ong)
-         ->addStandardTab(__CLASS__, $ong, $options)
          ->addStandardTab('Item_OperatingSystem', $ong, $options)
          ->addStandardTab('Item_Devices', $ong, $options)
          ->addStandardTab('Item_Disk', $ong, $options)
