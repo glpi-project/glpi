b599e8464f0b649ed0a23d508d88c71e2d28518d
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-04-09 14:03:31 +0200

    Add ticket position on change the same as add problem/project

--- inc/change_ticket.class.php
+++ inc/change_ticket.class.php
@@ -264,7 +264,7 @@ class Change_Ticket extends CommonDBRelation{
          echo "<table class='tab_cadre_fixe'>";
          echo "<tr class='tab_bg_2'><th colspan='2'>".__('Add a ticket')."</th></tr>";
 
-         echo "<tr class='tab_bg_2'><td class='right'>";
+         echo "<tr class='tab_bg_2'><td>";
          echo "<input type='hidden' name='changes_id' value='$ID'>";
          Ticket::dropdown(['used'        => $used,
                                 'entity'      => $change->getEntityID(),
