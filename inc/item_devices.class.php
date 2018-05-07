04a06a0bf9a49527c02cd9cf47ef1dcd3d85831a
Cédric Anne <cedric.anne@gmail.com>
2018-05-07 12:08:45 +0200

    Fix 'nodisplay' specificities in item devices list; closes #3247

--- inc/item_devices.class.php
+++ inc/item_devices.class.php
@@ -690,6 +690,8 @@ class Item_Devices extends CommonDBRelation {
                                             "<a href='" . $this->getLinkURL() . "'>$mode</a>");
 
          foreach ($this->getSpecificities() as $field => $attributs) {
+            $content = '';
+
             if (!empty($link[$field])) {
                // Check the user can view the field
                if (!isset($attributs['right'])) {
@@ -717,9 +719,8 @@ class Item_Devices extends CommonDBRelation {
                         $content = $link[$field];
                   }
                }
-            } else {
-               $content = '';
             }
+
             $spec_cell = $current_row->addCell($specificity_columns[$field], $content, $spec_cell);
          }
 
