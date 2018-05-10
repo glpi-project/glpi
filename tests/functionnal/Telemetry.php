b9f0f57688ad2a6ee9e61fc98c6a0087f1121291
Anael Mobilia <contrib@anael.eu>
2018-05-11 00:47:56 +0200

    Fix tests

--- tests/functionnal/Telemetry.php
+++ tests/functionnal/Telemetry.php
@@ -41,7 +41,7 @@ class Telemetry extends DbTestCase {
    public function testGrabGlpiInfos() {
       $expected = [
          'uuid'               => 'TO BE SET',
-         'type'               => 'install',
+         'type'               => 'update',
          'version'            => GLPI_VERSION,
          'plugins'            => [],
          'default_language'   => 'en_GB',
