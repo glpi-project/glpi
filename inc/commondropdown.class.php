30da6a0639433ee171857cf892437532e4511193
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-05-30 09:57:18 +0200

    Do not use locations id on item update; fixes #3584

--- inc/commondropdown.class.php
+++ inc/commondropdown.class.php
@@ -201,7 +201,7 @@ abstract class CommonDropdown extends CommonDBTM {
       global $DB;
 
       // if item based on location, create item in the same entity as location
-      if (isset($input['locations_id'])) {
+      if (isset($input['locations_id']) && !isset($input['_is_update'])) {
          foreach ($DB->request("SELECT `entities_id`
                                 FROM `glpi_locations`
                                 WHERE `id` = ".$input['locations_id']) as $data) {
@@ -213,6 +213,9 @@ abstract class CommonDropdown extends CommonDBTM {
          // leading/ending space will break findID/import
          $input['name'] = trim($input['name']);
       }
+      if (isset($input['_is_update'])) {
+         unset($input['_is_update']);
+      }
       return $input;
    }
 
@@ -223,6 +226,8 @@ abstract class CommonDropdown extends CommonDBTM {
     * @see CommonDBTM::prepareInputForUpdate()
    **/
    function prepareInputForUpdate($input) {
+      //add a "metadata to find if we're on an update or a add
+      $input['_is_update'] = true;
       return self::prepareInputForAdd($input);
    }
 
