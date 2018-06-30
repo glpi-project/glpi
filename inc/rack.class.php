4a6eec9e0c5490887d0f9731225bbd39ec46d639
cconard96 <cconard96@gmail.com>
2018-06-30 11:38:09 -0400

    Restore previous rack class input checks in addition to the new checks

--- inc/rack.class.php
+++ inc/rack.class.php
@@ -891,16 +891,19 @@ JAVASCRIPT;
    }
 
    function prepareInputForAdd($input) {
-      if (isset($input["id"]) && ($input["id"] > 0)) {
-         $input["_oldID"] = $input["id"];
-      }
-      if (isset($input["size"]) && ($input["size"] == '')) {
-         unset($input["size"]);
-      }
-      unset($input['id']);
-      unset($input['withtemplate']);
+      if (prepareInput($input) {
+         if (isset($input["id"]) && ($input["id"] > 0)) {
+            $input["_oldID"] = $input["id"];
+         }
+         if (isset($input["size"]) && ($input["size"] == '')) {
+            unset($input["size"]);
+         }
+         unset($input['id']);
+         unset($input['withtemplate']);
 
-      return $input;
+         return $input;
+      }
+      return false;
    }
 
    function prepareInputForUpdate($input) {
