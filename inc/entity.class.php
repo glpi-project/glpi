999c33834812c7b54788282a058668c2fbba26a3
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-07-03 17:02:07 +0200

    Fix getscript call loading entities selector

--- inc/entity.class.php
+++ inc/entity.class.php
@@ -1107,7 +1107,7 @@ class Entity extends CommonTreeDropdown {
 
       echo "<script type='text/javascript'>";
       echo "   $(function() {
-                  $.getScript('{$CFG_GLPI["root_doc"]}/lib/jqueryplugins/jstree/jstree.min.js', function(data, textStatus, jqxhr) {
+                  $.getScript('{$CFG_GLPI["root_doc"]}/lib/jqueryplugins/jstree/jstree.min.js').done(function(data, textStatus, jqxhr) {
                      $('#tree_projectcategory$rand')
                      // call `.jstree` with the options object
                      .jstree({
