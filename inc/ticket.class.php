b509056d06de2502d3124122f94a5530abd3eb30
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-07-04 07:41:49 +0200

    Solutions can be approved without UPDATE right; fixes #4282

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -1058,6 +1058,7 @@ class Ticket extends CommonITILObject {
                 || $this->canAssignToMe()
                 || isset($input['_from_assignment'])) {
                 $allowed_fields[] = 'status';
+                $allowed_fields[] = '_accepted';
             }
             // for post-only with validate right or validation created by rules
             if (TicketValidation::canValidate($this->fields['id'])
