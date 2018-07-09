9464d2baf5816fcd7cf8f834a0e125b4cfd7ba77
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-07-09 16:22:30 +0200

    Fix script path

--- tests/test_fail_update.sh
+++ tests/test_fail_update.sh
@@ -3,7 +3,7 @@
 testfail() {
    sed -e "s/\$migration->setVersion('9.3');/\0\n\$DB->queryOrDie('SELECT * FROM notatable');/" \
       -i install/update_92_93.php
-   php tools/cliupdate.php --config-dir=../tests --dev
+   php scripts/cliupdate.php --config-dir=../tests --dev
    RETVAL=$?
    if [ $RETVAL -ne 0 ]
    then
