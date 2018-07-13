4deb9222ea8db8ab1d3afc5add7181c2137904e1
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-07-13 17:49:24 +0200

    Add transactions support

--- inc/dbmysql.class.php
+++ inc/dbmysql.class.php
@@ -1111,4 +1111,31 @@ class DBmysql {
       $raw = $req['version()'];
       return $raw;
    }
+
+   /**
+    * Starts a transaction
+    *
+    * @return boolean
+    */
+   public function beginTransaction() {
+      return $this->dbh->begin_transaction();
+   }
+
+   /**
+    * Commits a transaction
+    *
+    * @return boolean
+    */
+   public function commit() {
+      return $this->dbh->commit();
+   }
+
+   /**
+    * Roolbacks a transaction
+    *
+    * @return boolean
+    */
+   public function rollBack() {
+      return $this->dbh->rollback();
+   }
 }
