53c041fb63b209e9233480304d2a95c67e2ea8f9
Cédric Anne <cedric.anne@gmail.com>
2018-08-13 11:25:09 +0200

    Factorize FAQ checks; fix bug; use iterator when possible

--- inc/document.class.php
+++ inc/document.class.php
@@ -564,73 +564,25 @@ class Document extends CommonDBTM {
     * @return boolean
    **/
    function canViewFile($options) {
-      global $DB, $CFG_GLPI;
 
-      if (Session::getLoginUserID()) {
-
-         // Check if it is my doc
-         if ($this->can($this->fields["id"], READ)
-             || ($this->fields["users_id"] === Session::getLoginUserID())) {
-            return true;
-         }
-
-         if ($this->canViewFileFromReminder()) {
-            return true;
-         }
-
-         if ($this->canViewFileFromKnowbaseItem()) {
-            return true;
-         }
-
-         if (isset($options["tickets_id"])
-             && $this->canViewFileFromTicket($options["tickets_id"])) {
-            return true;
-         }
+      // Check if it is my doc
+      if (Session::getLoginUserID()
+          && ($this->can($this->fields["id"], READ)
+              || ($this->fields["users_id"] === Session::getLoginUserID()))) {
+         return true;
       }
 
-      // Public FAQ for not connected user
-      if ($CFG_GLPI["use_public_faq"]) {
-         $query = "SELECT *
-                   FROM `glpi_documents_items`
-                   LEFT JOIN `glpi_knowbaseitems`
-                        ON (`glpi_knowbaseitems`.`id` = `glpi_documents_items`.`items_id`)
-                   LEFT JOIN `glpi_entities_knowbaseitems`
-                        ON (`glpi_knowbaseitems`.`id` = `glpi_entities_knowbaseitems`.`knowbaseitems_id`)
-                   WHERE (`glpi_documents_items`.`itemtype` = 'KnowbaseItem'
-                          AND `glpi_documents_items`.`documents_id` = '".$this->fields["id"]."'
-                          OR `glpi_knowbaseitems`.`answer` LIKE '%document.send.php?docid=".$this->fields["id"]."%')
-                         AND `glpi_knowbaseitems`.`is_faq` = 1
-                         AND `glpi_entities_knowbaseitems`.`entities_id` = 0
-                         AND `glpi_entities_knowbaseitems`.`is_recursive` = 1";
+      if ($this->canViewFileFromReminder()) {
+         return true;
+      }
 
-         $result = $DB->query($query);
-         if ($DB->numrows($result) > 0) {
-            return true;
-         }
+      if ($this->canViewFileFromKnowbaseItem()) {
+         return true;
+      }
 
-         //copy/pasted images do not have entries in glpi_documents_items table
-         $result = $DB->request([
-            'FROM'      => 'glpi_knowbaseitems',
-            'COUNT'     => 'cpt',
-            'LEFT JOIN' => [
-               'glpi_entities_knowbaseitems' => [
-                  'FKEY' => [
-                     'glpi_knowbaseitems'          => 'id',
-                     'glpi_entities_knowbaseitems' => 'knowbaseitems_id'
-                  ]
-               ]
-            ],
-            'WHERE'     => [
-               'glpi_knowbaseitems.answer'                  => [
-                  'LIKE',
-                  '%document.send.php?docid='.$this->fields["id"].'%'
-               ],
-               'glpi_knowbaseitems.is_faq'                  => 1,
-               'glpi_entities_knowbaseitems.entities_id'    => 0,
-               'glpi_entities_knowbaseitems.is_recursive'   => 1
-            ]
-         ])->next();
-         return $result['cpt'] > 0;
+      if (isset($options["tickets_id"])
+          && $this->canViewFileFromTicket($options["tickets_id"])) {
+         return true;
       }
 
       return false;
@@ -641,10 +593,16 @@ class Document extends CommonDBTM {
     *
     * @global DBmysql $DB
     * @return boolean
+    *
+    * @TODO Use DBmysqlIterator instead of raw SQL
     */
    private function canViewFileFromReminder() {
 
       global $DB;
+
+      if (!Session::getLoginUserID()) {
+         return false;
+      }
 
       $query = "SELECT *
                 FROM `glpi_documents_items`
@@ -662,15 +620,22 @@ class Document extends CommonDBTM {
    /**
     * Check if file of current instance can be viewed from a KnowbaseItem.
     *
+    * @global array $CFG_GLPI
     * @global DBmysql $DB
     * @return boolean
     */
    private function canViewFileFromKnowbaseItem() {
 
-      global $DB;
+      global $CFG_GLPI, $DB;
+
+      // Knowbase items can be viewed by non connected user in case of public FAQ
+      if (!Session::getLoginUserID() && !$CFG_GLPI['use_public_faq']) {
+         return false;
+      }
 
       if (!Session::haveRight(KnowbaseItem::$rightname, READ)
-          && !Session::haveRight(KnowbaseItem::$rightname, KnowbaseItem::READFAQ)) {
+          && !Session::haveRight(KnowbaseItem::$rightname, KnowbaseItem::READFAQ)
+          && !$CFG_GLPI['use_public_faq']) {
          return false;
       }
 
@@ -695,7 +660,7 @@ class Document extends CommonDBTM {
 
       if (!Session::haveRight(KnowbaseItem::$rightname, READ)) {
          // Limit to FAQ only
-         $request['WHERE'][glpi_knowbaseitems.is_faq] = 1;
+         $request['WHERE']['glpi_knowbaseitems.is_faq'] = 1;
       }
 
       if (count($visibilityCriteria['LEFT JOIN']) > 0) {
@@ -725,17 +690,15 @@ class Document extends CommonDBTM {
             ]
          ],
          'WHERE'     => [
-            'glpi_knowbaseitems.answer'                  => [
-               'LIKE',
-               '%document.send.php?docid='.$this->fields["id"].'%'
+            'glpi_knowbaseitems.answer' => [
+               'LIKE', '%document.send.php?docid=' . $this->fields['id'] . '%'
             ],
-            'glpi_knowbaseitems.is_faq'                  => 1,
          ]
       ];
 
       if (!Session::haveRight(KnowbaseItem::$rightname, READ)) {
          // Limit to FAQ only
-         $request['WHERE'][glpi_knowbaseitems.is_faq] = 1;
+         $request['WHERE']['glpi_knowbaseitems.is_faq'] = 1;
       }
 
       if (count($visibilityCriteria['LEFT JOIN']) > 0) {
@@ -761,46 +724,76 @@ class Document extends CommonDBTM {
 
       global $DB;
 
+      if (!Session::getLoginUserID()) {
+         return false;
+      }
+
       $ticket = new Ticket();
 
       if (!$ticket->can($tickets_id, READ)) {
          return false;
       }
 
-      $iterator = $DB->request([
-         'FROM'   => Document_Item::getTable(),
-         'WHERE'  => [
+      $result = $DB->request([
+         'FROM'  => Document_Item::getTable(),
+         'COUNT' => 'cpt',
+         'WHERE' => [
             'items_id'     => $tickets_id,
             'itemtype'     => Ticket::class,
             'documents_id' => $this->fields['id']
          ]
-      ]);
+      ])->next();
 
-      if (count($iterator)) {
+
+      if ($result['cpt'] > 0) {
          return true;
       }
 
       // Check ticket and child items (followups, tasks, solutions) contents
-      $query = "SELECT *
-                FROM glpi_tickets AS tic
-                LEFT JOIN glpi_ticketfollowups AS fup
-                  ON fup.tickets_id = tic.id
-                LEFT JOIN glpi_tickettasks AS task
-                  ON task.tickets_id = tic.id
-                LEFT JOIN glpi_itilsolutions AS sol
-                  ON sol.items_id = tic.id
-                  AND sol.itemtype = 'Ticket'
-                WHERE tic.id = {$tickets_id}
-                AND (
-                  tic.content LIKE '%document.send.php?docid={$this->fields['id']}%'
-                  OR sol.content LIKE '%document.send.php?docid={$this->fields['id']}%'
-                  OR fup.content LIKE '%document.send.php?docid={$this->fields['id']}%'
-                  OR task.content LIKE '%document.send.php?docid={$this->fields['id']}%'
-                )";
+      $result = $DB->request([
+         'FROM'      => 'glpi_tickets',
+         'COUNT'     => 'cpt',
+         'LEFT JOIN' => [
+            'glpi_ticketfollowups' => [
+               'FKEY' => [
+                  'glpi_tickets'         => 'id',
+                  'glpi_ticketfollowups' => 'tickets_id'
+               ]
+            ],
+            'glpi_tickettasks'     => [
+               'FKEY' => [
+                  'glpi_tickets'     => 'id',
+                  'glpi_tickettasks' => 'tickets_id'
+               ]
+            ],
+            'glpi_itilsolutions'   => [
+               'FKEY' => [
+                  'glpi_tickets'       => 'id',
+                  'glpi_itilsolutions' => 'items_id',
+                  ['AND' => ['glpi_itilsolutions.itemtype' => 'Ticket']]
+               ]
+            ],
+         ],
+         'WHERE'     => [
+            'glpi_tickets.id' => $tickets_id,
+            'OR' => [
+               'glpi_tickets.content' => [
+                  'LIKE', '%document.send.php?docid=' . $this->fields['id'] . '%'
+               ],
+               'glpi_ticketfollowups.content' => [
+                  'LIKE', '%document.send.php?docid=' . $this->fields['id'] . '%'
+               ],
+               'glpi_tickettasks.content' => [
+                  'LIKE', '%document.send.php?docid=' . $this->fields['id'] . '%'
+               ],
+               'glpi_itilsolutions.content' => [
+                  'LIKE', '%document.send.php?docid=' . $this->fields['id'] . '%'
+               ]
+            ]
+         ]
+      ])->next();
 
-      $result = $DB->query($query);
-
-      return $DB->numrows($result) > 0;
+      return $result['cpt'] > 0;
    }
 
 
