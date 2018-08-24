9d0d3dc2178f21fe6c9311e39ac7f00843c65719
Cédric Anne <cedric.anne@gmail.com>
2018-08-24 11:01:12 +0200

    Fix logs history purge date criterion; fixes #4529 (#4531)

--- inc/purgelogs.class.php
+++ inc/purgelogs.class.php
@@ -364,7 +364,7 @@ class PurgeLogs extends CommonDBTM {
     */
    static function getDateModRestriction($month) {
       if ($month > 0) {
-         return ['date_mod' => new QueryExpression("DATE_ADD(NOW(), INTERVAL -$month MONTH)")];
+         return ['date_mod' => ['<=', new QueryExpression("DATE_ADD(NOW(), INTERVAL -$month MONTH)")]];
       } else if ($month == Config::DELETE_ALL) {
          return [1 => 1];
       } else if ($month == Config::KEEP_ALL) {
