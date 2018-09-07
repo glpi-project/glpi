75ea34128c7fbee234e674900c8b720f92df1ee1
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2018-09-07 10:52:46 +0200

    distinct kb items in dropdowns; fix 4851

--- inc/dropdown.class.php
+++ inc/dropdown.class.php
@@ -2649,8 +2649,11 @@ class Dropdown {
                break;
 
             case KnowbaseItem::getType():
-               $addjoin   .= KnowbaseItem::addVisibilityJoins();
-               //no break to reach default case.
+               $query = "SELECT DISTINCT `$table`.* $addselect
+                        FROM `$table`
+                        $addjoin ".KnowbaseItem::addVisibilityJoins()."
+                        $where";
+               break;
 
             case Project::getType():
                $addjoin .= Project::addVisibilityJoins();
