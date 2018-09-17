2ce778f33e403c103b5635dad3e6bc5c99f0455b
Cédric Anne <cedric.anne@gmail.com>
2018-09-17 14:08:36 +0200

    Fix missing checks when adding items on DC elements; fixes #4648

--- inc/item_enclosure.class.php
+++ inc/item_enclosure.class.php
@@ -285,125 +285,24 @@ class Item_Enclosure extends CommonDBRelation {
     * @return array
     */
    private function prepareInput($input) {
-      /*$error_detected = [];
-
-      $itemtype = $this->fields['itemtype'];
-      $items_id = $this->fields['items_id'];
-      $racks_id = $this->fields['racks_id'];
-      $position = $this->fields['position'];
-      $hpos = $this->fields['hpos'];
-      $orientation = $this->fields['orientation'];
+      $error_detected = [];
 
       //check for requirements
-      if ($this->isNewItem()) {
-         if (!isset($input['itemtype'])) {
-            $error_detected[] = __('An item type is required');
-         }
-
-         if (!isset($input['items_id'])) {
-            $error_detected[] = __('An item is required');
-         }
-
-         if (!isset($input['racks_id'])) {
-            $error_detected[] = __('A rack is required');
-         }
-
-         if (!isset($input['position'])) {
-            $error_detected[] = __('A position is required');
-         }
+      if (($this->isNewItem() && (!isset($input['itemtype']) || empty($input['itemtype'])))
+          || (isset($input['itemtype']) && empty($input['itemtype']))) {
+         $error_detected[] = __('An item type is required');
       }
-
-      if (isset($input['itemtype'])) {
-         $itemtype = $input['itemtype'];
+      if (($this->isNewItem() && (!isset($input['items_id']) || empty($input['items_id'])))
+          || (isset($input['items_id']) && empty($input['items_id']))) {
+         $error_detected[] = __('An item is required');
       }
-      if (isset($input['items_id'])) {
-         $items_id = $input['items_id'];
+      if (($this->isNewItem() && (!isset($input['enclosures_id']) || empty($input['enclosures_id'])))
+          || (isset($input['enclosures_id']) && empty($input['enclosures_id']))) {
+         $error_detected[] = __('An enclosure is required');
       }
-      if (isset($input['racks_id'])) {
-         $racks_id = $input['racks_id'];
-      }
-      if (isset($input['position'])) {
-         $position = $input['position'];
-      }
-      if (isset($input['hpos'])) {
-         $hpos = $input['hpos'];
-      }
-      if (isset($input['orientation'])) {
-         $orientation = $input['orientation'];
-      }
-
-      if (!count($error_detected)) {
-         //check if required U are available at position
-         $rack = new Rack();
-         $rack->getFromDB($racks_id);
-
-         $filled = $rack->getFilled($itemtype, $items_id);
-
-         $item = new $itemtype;
-         $item->getFromDB($items_id);
-         $model_class = $item->getType() . 'Model';
-         $modelsfield = strtolower($item->getType()) . 'models_id';
-         $model = new $model_class;
-         if ($model->getFromDB($item->fields[$modelsfield])) {
-            $item->model = $model;
-         } else {
-            $item->model = null;
-         }
-
-         $required_units = 1;
-         $width          = 1;
-         $depth          = 1;
-         if ($item->model != null) {
-            if ($item->model->fields['required_units'] > 1) {
-               $required_units = $item->model->fields['required_units'];
-            }
-            if ($item->model->fields['is_half_rack'] == 1) {
-               if ($this->isNewItem() && !isset($input['hpos']) || $input['hpos'] == 0) {
-                  $error_detected[] = __('You must define an horizontal position for this item');
-               }
-               $width = 0.5;
-            }
-            if ($item->model->fields['depth'] != 1) {
-               if ($this->isNewItem() && !isset($input['orientation'])) {
-                  $error_detected[] = __('You must define an orientation for this item');
-               }
-               $depth = $item->model->fields['depth'];
-            }
-         }
-
-         if ($position > $rack->fields['number_units'] ||
-            $position + $required_units  > $rack->fields['number_units'] + 1
-         ) {
-            $error_detected[] = __('Item is out of rack bounds');
-         } else if (!count($error_detected)) {
-            $i = 0;
-            while ($i < $required_units) {
-               $current_position = $position + $i;
-               if (isset($filled[$current_position])) {
-                  $width_overflow = false;
-                  $depth_overflow = false;
-                  if ($filled[$current_position]['width'] + $width > 1) {
-                     if ($depth > 0.5) {
-                        $width_overflow = true;
-                     }
-                  } else if ($filled[$current_position]['width'] <= 0.5 && $hpos == $filled[$current_position]['hpos']) {
-                     $error_detected[] = __('An item already exists at this horizontal position');
-                  }
-                  if ($filled[$current_position]['depth'] + $depth > 1) {
-                     if ($width > 0.5) {
-                        $depth_overflow = true;
-                     }
-                  } else if ($filled[$current_position]['depth'] <= 0.5 && $orientation == $filled[$current_position]['orientation']) {
-                     $error_detected[] = __('An item already exists for this orientation');
-                  }
-
-                  if ($width_overflow || $depth_overflow) {
-                     $error_detected[] = __('Not enougth space available to place item');
-                  }
-               }
-               ++$i;
-            }
-         }
+      if (($this->isNewItem() && (!isset($input['position']) || empty($input['position'])))
+          || (isset($input['position']) && empty($input['position']))) {
+         $error_detected[] = __('A position is required');
       }
 
       if (count($error_detected)) {
@@ -415,7 +314,7 @@ class Item_Enclosure extends CommonDBRelation {
             );
          }
          return false;
-      }*/
+      }
 
       return $input;
    }
