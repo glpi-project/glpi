988b4b17fe7fdad0f99b9f1a52b907a781448aa1
Ludek Finstrle <luf@seznam.cz>
2018-09-21 12:59:50 +0200

    Bug fix for multiple groups for used/managed items, resolve #4685

--- inc/user.class.php
+++ inc/user.class.php
@@ -3911,7 +3911,7 @@ class User extends CommonDBTM {
 
       $group_where = [];
       while ($data = $iterator->next()) {
-         $group_where[$field_group] = $data['groups_id'];
+         $group_where[$field_group][] = $data['groups_id'];
          $groups[$data["groups_id"]] = $data["name"];
       }
 
