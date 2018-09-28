be669837ff3b707dcb88db34cf31d37abefb96be
Johan Cwiklinski <trasher@x-tnd.be>
2018-09-28 14:49:54 +0200

    Load 3rd party libs earlier (#4711)
    Mainly to get lib constants available (so we can refine Logger level in local_define for example).

--- inc/includes.php
+++ inc/includes.php
@@ -34,7 +34,7 @@ if (!defined('GLPI_ROOT')) {
    define('GLPI_ROOT', dirname(__DIR__));
 }
 
-include_once (GLPI_ROOT . "/inc/autoload.function.php");
+include_once GLPI_ROOT . '/inc/based_config.php';
 
 // Init Timer to compute time of display
 $TIMER_DEBUG = new Timer();
