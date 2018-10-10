e23a31057c58589caa3f9032d28c8f9a5a2e2dd7
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2018-10-10 13:59:17 +0200

    remove scroll event on tab when reloading them

--- inc/ajax.class.php
+++ inc/ajax.class.php
@@ -457,10 +457,16 @@ class Ajax {
             function reloadTab(add) {
                forceReload$rand = true;
                var current_index = $('#tabs$rand').tabs('option','active');
+
+               // remove scroll event bind, select2 bind it on parent with scrollbars (the tab currently)
+               // as the select2 disapear with this tab reload, remove the event to prevent issues (infinite scroll to top)
+               $('#tabs$rand .ui-tabs-panel[aria-expanded=true]').unbind('scroll');
+
                // Save tab
                var currenthref = $('#tabs$rand ul>li a').eq(current_index).attr('href');
                $('#tabs$rand ul>li a').eq(current_index).attr('href',currenthref+'&'+add);
                $('#tabs$rand').tabs( 'load' , current_index);
+
                // Restore tab
                $('#tabs$rand ul>li a').eq(current_index).attr('href',currenthref);
             };";
