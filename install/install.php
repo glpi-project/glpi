e82a79754b0b441a6bf2b47a3c9a1f39c43da0a6
Johan Cwiklinski <jcwiklinski@teclib.com>
2018-12-07 11:37:36 +0100

    Missing JS on install page

--- install/install.php
+++ install/install.php
@@ -58,6 +58,7 @@ function header_html($etape) {
    echo Html::script("lib/jquery/js/jquery.js");
    echo Html::script('lib/jquery/js/jquery-ui-1.10.4.custom.js');
    echo Html::script("lib/jqueryplugins/select2/js/select2.js");
+   echo Html::script("js/common.js");
 
     // CSS
    echo Html::css('lib/jquery/css/smoothness/jquery-ui-1.10.4.custom.css');
