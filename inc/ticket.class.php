cec242b8064d892ea20bec13b664556db5010a5a
cconard96 <cconard96@gmail.com>
2018-12-28 08:49:10 -0500

    Fix satisfaction survey creation fixes #5115

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -1651,17 +1651,26 @@ class Ticket extends CommonITILObject {
           && ($delay == 0)
           && ($rate > 0)
           && (mt_rand(1, 100) <= $rate)) {
-         $inquest_id = $inquest->add(
-            [
-               'tickets_id'    => $this->fields['id'],
-               'date_begin'    => $_SESSION["glpi_currenttime"],
-               'entities_id'   => $this->fields['entities_id'],
-               'type'          => $type,
-               'max_closedate' => $max_closedate,
-            ]
-         );
+
+         $ticket_inquests = $inquest->find(['tickets_id' => $this->getID()]);
+
+         //Do not create a new satisfaction survey if one exists
+         if (!count($ticket_inquests)) {
+            $inquest_id = $inquest->add(
+               [
+                  'tickets_id'    => $this->fields['id'],
+                  'date_begin'    => $_SESSION["glpi_currenttime"],
+                  'entities_id'   => $this->fields['entities_id'],
+                  'type'          => $type,
+                  'max_closedate' => $max_closedate,
+               ]
+            );
+         } else {
+            $inquest_id = reset($ticket_inquests)['tickets_id'];
+         }
+
          // Redirect to created survey
-         HTML::redirect(TicketSatisfaction::getFormURLWithID($inquest_id));
+         Html::redirect(TicketSatisfaction::getFormURLWithID($inquest_id));
       }
    }
 
