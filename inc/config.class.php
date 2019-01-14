eeb7bf4e1cea34c740630ecc6ebb62c9fbe20cb5
Cédric Anne <cedric.anne@gmail.com>
2019-01-14 11:53:12 +0100

    Remove read/write of instance uuid in DB when initializing cache

--- inc/config.class.php
+++ inc/config.class.php
@@ -2970,8 +2970,8 @@ class Config extends CommonDBTM {
 
       if (!isset($opt['options']['namespace'])) {
          $namespace = "glpi_${optname}_" . GLPI_VERSION;
-         if ($DB && $DB->connected) {
-            $namespace .= Telemetry::getInstanceUuid();
+         if ($DB) {
+            $namespace .= md5($DB->dbhost . $DB->dbdefault);
          }
          $opt['options']['namespace'] = $namespace;
       }
