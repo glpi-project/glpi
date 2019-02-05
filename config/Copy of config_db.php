313a07648d1fca600d411d65d371939abd40069d
Cédric Anne <cedric.anne@gmail.com>
2019-02-05 16:24:13 +0100

    Try to reorganize tests configuration

---
+++ config/Copy of config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = '172.24.200.91';
+   public $dbuser     = 'glpi';
+   public $dbpassword = 'glpi';
+   public $dbdefault  = 'local.glpi_10_0_empty';
+}
