b66765762b6a3b637e6585aa2db069657d0ae86b
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-02-18 07:56:35 +0100

    Fix update from dev version, see #5362

--- inc/update.class.php
+++ inc/update.class.php
@@ -170,10 +170,6 @@ class Update extends CommonGLPI {
       $plugin = new Plugin();
       $plugin->unactivateAll();
 
-      if (!defined('GLPI_PREVER')) {
-         define('GLPI_PREVER', false);
-      }
-
       switch ($current_version) {
          case '0.1':
             include_once("update_to_031.php");
@@ -438,7 +434,7 @@ class Update extends CommonGLPI {
 
          case "9.3.2":
          case "9.3.3":
-         case GLPI_PREVER:
+         case "9.4.0-dev":
             include_once("{$updir}update_93_94.php");
             update93to94();
             break;
