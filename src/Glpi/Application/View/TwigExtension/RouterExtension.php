22674a0ad0892b4b2252b73af2b3764b298af449
Anael MOBILIA <contrib@anael.eu>
2019-03-06 11:58:50 +0100

    Update copyright header on files from 2018 to 2019

--- src/Glpi/Application/View/TwigExtension/RouterExtension.php
+++ src/Glpi/Application/View/TwigExtension/RouterExtension.php
@@ -1,10 +1,33 @@
 <?php
 /**
- * Slim Framework (http://slimframework.com)
+ * ---------------------------------------------------------------------
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2015-2019 Teclib' and contributors.
  *
- * @link      https://github.com/slimphp/Twig-View
- * @copyright Copyright (c) 2011-2015 Josh Lockhart
- * @license   https://github.com/slimphp/Twig-View/blob/master/LICENSE.md (MIT License)
+ * http://glpi-project.org
+ *
+ * based on GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2003-2014 by the INDEPNET Development Team.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * GLPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GLPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
  */
 namespace Glpi\Application\View\TwigExtension;
 
