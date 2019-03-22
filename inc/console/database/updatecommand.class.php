3db45aed5fd95cb97592669c55d1fc5e897172dc
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-03-22 08:45:24 +0100

    Bump version

--- inc/console/database/updatecommand.class.php
+++ inc/console/database/updatecommand.class.php
@@ -170,8 +170,8 @@ class UpdateCommand extends AbstractCommand implements ForceNoPluginsOptionComma
       } else if ($force) {
          // Replay last update script even if there is no schema change.
          // It can be used in dev environment when update script has been updated/fixed.
-         include_once(GLPI_ROOT . '/install/update_941_942.php');
-         update941to942();
+         include_once(GLPI_ROOT . '/install/update_94_95.php');
+         update94to95();
 
          $output->writeln('<info>' . __('Last migration replayed.') . '</info>');
       }
