8c9ce4bbb7046388cecf5bbefe6126f533396dd3
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-05-07 16:05:56 +0200

    Use local db instance

--- inc/search.class.php
+++ inc/search.class.php
@@ -3332,7 +3332,7 @@ JAVASCRIPT;
                      break;
                }
 
-               return " {$LINK} ({$DB->quoteName($NAME)} $operator {$DB->quoteValue($val)}) ";
+               return " {$LINK} ({$this->db->quoteName($NAME)} $operator {$this->db->quoteValue($val)}) ";
                break;
             case "count" :
             case "number" :
@@ -3600,7 +3600,7 @@ JAVASCRIPT;
     * @since 10.0.0 Method is no longer static
    **/
    public function addSelect($itemtype, $ID, $meta = 0, $meta_type = 0) {
-      global $DB, $CFG_GLPI;
+      global $CFG_GLPI;
 
       $searchopt   = &self::getOptions($itemtype);
       $table       = $searchopt[$ID]["table"];
@@ -3839,7 +3839,7 @@ JAVASCRIPT;
       if (isset($searchopt[$ID]["computation"])) {
          $tocompute = $searchopt[$ID]["computation"];
          $tocompute = str_replace($this->db->quoteName('TABLE'), 'TABLE', $tocompute);
-         $tocompute = str_replace("TABLE", $DB->quoteName("$table$addtable"), $tocompute);
+         $tocompute = str_replace("TABLE", $this->db->quoteName("$table$addtable"), $tocompute);
       }
       // Preformat items
       if (isset($searchopt[$ID]["datatype"])) {
@@ -4596,7 +4596,7 @@ JAVASCRIPT;
       if (isset($searchopt[$ID]["computation"])) {
          $tocompute = $searchopt[$ID]["computation"];
          $tocompute = str_replace($this->db->quoteName('TABLE'), 'TABLE', $tocompute);
-         $tocompute = str_replace("TABLE", $DB->quoteName("$table"), $tocompute);
+         $tocompute = str_replace("TABLE", $this->db->quoteName("$table"), $tocompute);
       }
 
       // Preformat items
