ad8246eab687fcfb024e003c182799083d3c1428
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2019-06-11 14:50:55 +0200

    missing ,

--- inc/define.php
+++ inc/define.php
@@ -489,7 +489,7 @@ $CFG_GLPI['javascript'] = [
       'config'    => ['colorpicker'],
       'commondropdown'  => [
          'ProjectState'         => ['colorpicker'],
-         'SolutionTemplate'     => ['tinymce']
+         'SolutionTemplate'     => ['tinymce'],
          'ITILFollowupTemplate' => ['tinymce']
       ],
       'notification'    => [
