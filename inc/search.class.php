c1860e6b20ac525432bfc7931d3428a1e51baad8
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2019-09-02 16:29:04 +0200

    Add responsible search option for user (#6260)
    * Fix legacy code
    
    * Exclude meta
    
    * Restrict to complexjoin
    
    * Search option for responsible (user)

--- inc/search.class.php
+++ inc/search.class.php
@@ -4054,19 +4054,21 @@ JAVASCRIPT;
 
       $inittable = $table;
       $addtable  = '';
+
+      $complexjoin = "";
+      if (isset($searchopt[$ID]['joinparams'])) {
+         $complexjoin = self::computeComplexJoinID($searchopt[$ID]['joinparams']);
+      }
+
       if (($table != 'asset_types')
-          && ($table != getTableForItemType($itemtype))
-          && ($searchopt[$ID]["linkfield"] != getForeignKeyFieldForTable($table))) {
+         && !empty($complexjoin)
+         && ($searchopt[$ID]["linkfield"] != getForeignKeyFieldForTable($table))) {
          $addtable = "_".$searchopt[$ID]["linkfield"];
          $table   .= $addtable;
       }
 
-      if (isset($searchopt[$ID]['joinparams'])) {
-         $complexjoin = self::computeComplexJoinID($searchopt[$ID]['joinparams']);
-
-         if (!empty($complexjoin)) {
-            $table .= "_".$complexjoin;
-         }
+      if (!empty($complexjoin)) {
+         $table .= "_".$complexjoin;
       }
 
       if ($meta
