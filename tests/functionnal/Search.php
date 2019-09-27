glpi-project/glpi

commit c1860e6b20ac525432bfc7931d3428a1e51baad8

Add responsible search option for user (#6260)
* Fix legacy code

* Exclude meta

* Restrict to complexjoin

* Search option for responsible (user)

inc/search.class.php
@@ -4054,19 +4054,21 @@ JAVASCRIPT;
 
       $inittable = $table;
       $addtable  = '';
+
+      $complexjoin = "";
+      if (isset($searchopt[$ID]['joinparams'])) {
+         $complexjoin = self::computeComplexJoinID($searchopt[$ID]['joinparams']);
+      }
+
       if (($table != 'asset_types')
-          && ($table != getTableForItemType($itemtype))
-          && ($searchopt[$ID]["linkfield"] != getForeignKeyFieldForTable($table))) {
+         && !empty($complexjoin)
+         && ($searchopt[$ID]["linkfield"] != getForeignKeyFieldForTable($table))) {
          $addtable = "_".$searchopt[$ID]["linkfield"];
          $table   .= $addtable;
       }
 
-      if (isset($searchopt[$ID]['joinparams'])) {
-         $complexjoin = self::computeComplexJoinID($searchopt[$ID]['joinparams']);
-
-         if (!empty($complexjoin)) {
-            $table .= "_".$complexjoin;
-         }
+      if (!empty($complexjoin)) {
+         $table .= "_".$complexjoin;
       }
 
       if ($meta

inc/user.class.php
@@ -3267,6 +3267,20 @@ class User extends CommonDBTM {
          ]
       ];
 
+      $tab[] = [
+         'id'                 => '99',
+         'table'              => 'glpi_users',
+         'field'              => 'name',
+         'linkfield'          => 'users_id_supervisor',
+         'name'               => __('Responsible'),
+         'datatype'           => 'dropdown',
+         'massiveaction'      => false,
+         'joinparams'         => [
+            // force complex join generation
+            'condition' => "AND 1=1"
+         ]
+      ];
+
       // add objectlock search options
       $tab = array_merge($tab, ObjectLock::rawSearchOptionsToAdd(get_class($this)));
 

commit 4f6187ae014bd9bb10ec264254195e025c5c52fb

fix bad sql query
regression in https://github.com/glpi-project/glpi/pull/6260

inc/search.class.php
@@ -4060,7 +4060,7 @@ JAVASCRIPT;
       }
 
       if (($table != 'asset_types')
-         && !empty($complexjoin)
+         && (($table == getTableForItemType($itemtype) && !empty($complexjoin)) || $table != getTableForItemType($itemtype))
          && ($searchopt[$ID]["linkfield"] != getForeignKeyFieldForTable($table))) {
          $addtable = "_".$searchopt[$ID]["linkfield"];
          $table   .= $addtable;

tests/functionnal/Search.php
@@ -1268,6 +1268,39 @@ class Search extends DbTestCase {
    public function testMakeTextSearchValue($value, $expected) {
       $this->string(\Search::makeTextSearchValue($value))->isIdenticalTo($expected);
    }
+
+   public function providerAddWhere() {
+      return [
+         [
+            'link' => ' ',
+            'nott' => 0,
+            'itemtype' => \User::class,
+            'id' => 99,
+            'searchtype' => 'equals',
+            'val' => '5',
+            'meta' => false,
+            'expected' => "   `glpi_users_users_id_supervisor_c49005e57f22539b078d72faca40cdf3`.`id` = '5'",
+         ],
+         [
+            'link' => ' AND ',
+            'nott' => 0,
+            'itemtype' => \CartridgeItem::class,
+            'id' => 24,
+            'searchtype' => 'equals',
+            'val' => '2',
+            'meta' => false,
+            'expected' => "  AND  (`glpi_users_users_id_tech`.`id` = '2') ",
+         ],
+      ];
+   }
+
+   /**
+    * @dataProvider providerAddWhere
+    */
+   public function testAddWhere($link, $nott, $itemtype, $ID, $searchtype, $val, $meta, $expected) {
+      $output = \Search::addWhere($link, $nott, $itemtype, $ID, $searchtype, $val, $meta);
+      $this->string($output)->isEqualTo($expected);
+   }
 }
 
 class DupSearchOpt extends \CommonDBTM {