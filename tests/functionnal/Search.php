7d63bc2f057e857e06e3ecc7b82cea2f84e25b98
Cédric Anne <cedric.anne@gmail.com>
2019-10-25 14:26:02 +0200

    Revert "Fix HAVING in search (#6204)" (#6461)
    * Do a test on all search options using them as criteria
    
    * Revert "Fix HAVING in search (#6204)"
    
    This reverts commit 365c1892c391059eb06b6f88d9b2b6c3bad6b5a9.
    
    * Remove SO tests on ReservationItem (does not have a generic search page)

--- tests/functionnal/Search.php
+++ tests/functionnal/Search.php
@@ -310,44 +310,6 @@ class Search extends DbTestCase {
                      ->contains("LEFT JOIN `glpi_entities`  AS `glpi_entities_");
    }
 
-   public function testHaving() {
-      $search_params = [
-         'reset'        => 'reset',
-         'is_deleted'   => 0,
-         'start'        => 0,
-         'search'       => 'Search',
-         'criteria'     => [
-            0 => [
-               'field'      => 1,
-               'searchtype' => 'contains',
-               'value'      => 'vm'
-            ],
-            1 => [
-               'link'       => 'OR',
-               'field'      => 18,
-               'searchtype' => 'contains',
-               'value'      => 4
-            ],
-         ]
-      ];
-
-      $data = $this->doSearch('Computer', $search_params);
-
-      // check for sql error (data key missing or empty)
-      $this->array($data)
-         ->hasKey('data')
-         ->array['last_errors']->isIdenticalTo([])
-         ->array['data']->isNotEmpty();
-
-      // Check having
-      $this->array($data)
-         ->hasKey('sql')
-         ->array['sql']
-         ->hasKey('search')
-         ->string['search']
-         ->contains("HAVING    (`glpi_computers`.`name`  LIKE '%vm%'  )  OR (`ITEM_Computer_18` = 4) ");
-   }
-
    public function testNestedAndMetaComputer() {
       $search_params = [
          'reset'      => 'reset',
@@ -454,7 +416,7 @@ class Search extends DbTestCase {
          ->contains("(`glpi_users`.`id` = '2')")
          ->contains("OR (`glpi_users`.`id` = '3')")
          // match having
-         ->matches("/HAVING.*\(`ITEM_Budget_2`\s+<>\s+5\)\s+AND\s+\(\(`ITEM_Printer_1`\s+NOT LIKE\s+'%HP%'\s+OR\s+`ITEM_Printer_1`\s+IS NULL\)\s*\)/");
+         ->matches("/HAVING\s*\(`ITEM_Budget_2`\s+<>\s+5\)\s+AND\s+\(\(`ITEM_Printer_1`\s+NOT LIKE\s+'%HP%'\s+OR\s+`ITEM_Printer_1`\s+IS NULL\)\s*\)/");
    }
 
    function testViewCriterion() {
@@ -544,14 +506,6 @@ class Search extends DbTestCase {
     * @return void
     */
    public function testSearchOptions() {
-
-      $displaypref = new \DisplayPreference();
-      // save table glpi_displaypreferences
-      $dp = getAllDatasFromTable($displaypref->getTable());
-      foreach ($dp as $line) {
-         $displaypref->delete($line, true);
-      }
-
       $itemtypeslist = $this->getClasses(
          'searchOptions',
          [
@@ -561,6 +515,7 @@ class Search extends DbTestCase {
             'SlaLevel',
             'OlaLevel',
             'Reservation',
+            'ReservationItem',
             'Event',
             'Glpi\\Event',
             'KnowbaseItem',
@@ -569,7 +524,6 @@ class Search extends DbTestCase {
          ]
       );
       foreach ($itemtypeslist as $itemtype) {
-         $number = 0;
          if (!file_exists('front/'.strtolower($itemtype).'.php')) {
             // it's the case where not have search possible in this itemtype
             continue;
@@ -580,53 +534,41 @@ class Search extends DbTestCase {
          $options = \Search::getCleanedOptions($itemtype);
          //but reload only items one because of mysql join limit
          $options = $item->searchOptions();
-         $compare_options = [];
-         foreach ($options as $key => $value) {
-            if (is_array($value) && count($value) == 1) {
-               $compare_options[$key] = $value['name'];
-            } else {
-               $compare_options[$key] = $value;
-            }
-         }
 
+         $all_criteria = [];
          foreach ($options as $key=>$data) {
-            if (is_int($key)) {
-               $input = [
-                   'itemtype' => $itemtype,
-                   'users_id' => 0,
-                   'num' => $key,
-               ];
-                $displaypref->add($input);
-               $number++;
+            if (!is_int($key) || (array_key_exists('nosearch', $data) && $data['nosearch'])) {
+               continue;
             }
-         }
-         $this->integer(
-            (int)countElementsInTable(
-               $displaypref->getTable(),
-               ['itemtype' => $itemtype, 'users_id' => 0]
-            )
-         )->isIdenticalTo($number);
+            $actions = \Search::getActionsFor($itemtype, $key);
+            $searchtype = array_keys($actions)[0];
 
-         // do a search query
+            $criterion = [
+               'field'      => $key,
+               'searchtype' => $searchtype,
+               'value'      => 0
+            ];
+
+            // do a search query based on current search option
+            $data = $this->doSearch(
+               $itemtype,
+               [
+                  'is_deleted'   => 0,
+                  'start'        => 0,
+                  'criteria'     => [$criterion],
+                  'metacriteria' => []
+               ]
+            );
+
+            $all_criteria[] = $criterion;
+         }
+
+         // do a search query with all criteria at the same time
          $search_params = ['is_deleted'   => 0,
                            'start'        => 0,
-                           'criteria'     => [],
+                           'criteria'     => $all_criteria,
                            'metacriteria' => []];
          $data = $this->doSearch($itemtype, $search_params);
-         // check for sql error (data key missing or empty)
-         $this->array($data)
-            ->hasKey('data')
-               ->array['last_errors']->isIdenticalTo([])
-               ->array['data']->isNotEmpty();
-      }
-      // restore displaypreference table
-      /// TODO: review, this can't work.
-      foreach (getAllDatasFromTable($displaypref->getTable()) as $line) {
-         $displaypref->delete($line, true);
-      }
-      $this->integer((int)countElementsInTable($displaypref->getTable()))->isIdenticalTo(0);
-      foreach ($dp as $input) {
-         $displaypref->add($input);
       }
    }
 
