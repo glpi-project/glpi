0d29d44f217a1e20340ca463a19a740103080855
Johan Cwiklinski <jcwiklinski@teclib.com>
2019-10-30 07:30:04 +0100

    Fix KB item form when not related to an ITIL item; fixes #6568

--- inc/knowbaseitem_item.class.php
+++ inc/knowbaseitem_item.class.php
@@ -129,9 +129,18 @@ class KnowbaseItem_Item extends CommonDBRelation {
          );
       }
 
-      if ($canedit
-          && !in_array($item->fields['status'], array_merge($item->getClosedStatusArray(),
-                                                            $item->getSolvedStatusArray()))) {
+      $ok_state = true;
+      if ($item instanceof CommonITILObject) {
+         $ok_state = !in_array(
+            $item->fields['status'],
+            array_merge(
+               $item->getClosedStatusArray(),
+               $item->getSolvedStatusArray()
+            )
+         );
+      }
+
+      if ($canedit && $ok_state) {
          echo '<form method="post" action="' . Toolbox::getItemTypeFormURL(__CLASS__) . '">';
          echo "<div class='center'>";
          echo "<table class=\"tab_cadre_fixe\">";
