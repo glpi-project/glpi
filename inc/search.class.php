711c8d3234380b07c06e4372210c451b74930fcf
Thierry Bugier <tbugier@teclib.com>
2019-11-25 16:11:33 +0100

    integer / string comparison bug
    int(0) == 'any string' always return true
    
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- inc/search.class.php
+++ inc/search.class.php
@@ -1390,7 +1390,7 @@ class Search {
                      }
 
                      if (!$handled) {
-                        if ($val == self::NULLVALUE) {
+                        if ($val === self::NULLVALUE) {
                            $newrow[$j][0][$fieldname] = null;
                         } else {
                            $newrow[$j][0][$fieldname] = $val;
