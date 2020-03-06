e9c2678036789fd6cb65bb9dac0efcbea3ff57aa
Adrien Clairembault <aclairembault@teclib.com>
2020-03-06 13:38:24 +0100

    Fix whitespace

--- inc/search.class.php
+++ inc/search.class.php
@@ -5498,7 +5498,7 @@ JAVASCRIPT;
          switch ($table.'.'.$field) {
             case "glpi_users.name" :
                if ($itemtype == 'Ticket'
-                  &&\Entity::getUsedConfig('anonymize_support_agents')
+                  && \Entity::getUsedConfig('anonymize_support_agents')
                   && \Session::getCurrentInterface() == 'helpdesk') {
                   return __("Helpdesk");
                }
