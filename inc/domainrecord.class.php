f1a3db0c392ef94577eb41e891cbfa1e54a45588
Johan Cwiklinski <jcwiklinski@teclib.com>
2020-03-13 10:08:51 +0100

    Fix condition

--- inc/domainrecord.class.php
+++ inc/domainrecord.class.php
@@ -253,7 +253,7 @@ class DomainRecord extends CommonDBChild {
          }
       }
 
-      if (!Session::isCron() && isset($input['domainrecordtypes_id']) || isset($this->fields['domainrecordtypes_id'])) {
+      if (!Session::isCron() && (isset($input['domainrecordtypes_id']) || isset($this->fields['domainrecordtypes_id']))) {
          if (!($_SESSION['glpiactiveprofile']['managed_domainrecordtypes'] == [-1])) {
             if (isset($input['domainrecordtypes_id']) && !(in_array($input['domainrecordtypes_id'], $_SESSION['glpiactiveprofile']['managed_domainrecordtypes']))) {
                //no right to use selected type
