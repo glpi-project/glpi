glpi-project/glpi

commit db3970feaafcb3d25f8b5784574966695323840c

Fix case insensitive class name in API

inc/apirest.class.php
@@ -335,7 +335,10 @@ class APIRest extends API {
                $this->parameters['parent_itemtype'] = $itemtype;
                $itemtype                            = $additional_itemtype;
             }
-            $itemtype = ucfirst($itemtype);
+
+            // Get case sensitive itemtype name
+            $rc = new \ReflectionClass($itemtype);
+            $itemtype = $rc->getName();
             return $itemtype;
          }
          $this->returnError(__("resource not found or not an instance of CommonDBTM"),

commit 0fa42a2fdfb3ab1c49949c33a698197743408d69

Fix bug introduced in #6850, no reflection for "AllAssets"

inc/apirest.class.php
@@ -336,6 +336,11 @@ class APIRest extends API {
                $itemtype                            = $additional_itemtype;
             }
 
+            // AllAssets
+            if ($all_assets && $this->url_elements[$index] == "AllAssets") {
+               return "AllAssets";
+            }
+
             // Get case sensitive itemtype name
             $rc = new \ReflectionClass($itemtype);
             $itemtype = $rc->getName();