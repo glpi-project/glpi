1fd8cbd6d1368edef4a765607a113e2c1300ca69
Adrien Clairembault <aclairembault@teclib.com>
2020-04-20 09:45:03 +0200

    Fix recursive project task visibility

--- inc/projecttask.class.php
+++ inc/projecttask.class.php
@@ -83,8 +83,7 @@ class ProjectTask extends CommonDBChild {
     * @return boolean
    **/
    function canViewItem() {
-
-      if (!Session::haveAccessToEntity($this->getEntityID())) {
+      if (!Session::haveAccessToEntity($this->getEntityID(), $this->isRecursive())) {
          return false;
       }
       $project = new Project();
