7d7b00fc29d85283226cfc90796b8ce98b6a9a29
Johan Cwiklinski <jcwiklinski@teclib.com>
2020-04-22 09:30:16 +0200

    Fix creating PDU from template; closes #7123

--- inc/pdu.class.php
+++ inc/pdu.class.php
@@ -350,4 +350,13 @@ class PDU extends CommonDBTM {
          ]
       );
    }
+
+   function prepareInputForAdd($input) {
+      if (isset($input["id"]) && ($input["id"] > 0)) {
+         $input["_oldID"] = $input["id"];
+      }
+      unset($input['id']);
+      unset($input['withtemplate']);
+      return $input;
+   }
 }
