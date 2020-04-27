d4fc21066aa6bec3a86a2bf234ed49efe6b1a11e
tsmr <thetsmr@gmail.com>
2020-04-27 14:35:27 +0200

    Add $CFG_GLPI["translate_reminders"]

--- install/update_94_95.php
+++ install/update_94_95.php
@@ -1955,6 +1955,7 @@ HTML
       );
    }
 
+   Config::setConfigurationValues('core', ['translate_reminders' => 0]);
    //Create remindertranslations table
    if (!$DB->tableExists('glpi_remindertranslations')) {
       $query = "CREATE TABLE `glpi_remindertranslations` (
