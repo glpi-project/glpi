01189af2c1d64753719eafae15bb35f20cea759b
adelaunay <delaunay.alexandre@gmail.com>
2020-04-28 10:29:19 +0200

    remove xss in kb comments

--- inc/knowbaseitem_comment.class.php
+++ inc/knowbaseitem_comment.class.php
@@ -82,15 +82,7 @@ class KnowbaseItem_Comment extends CommonDBTM {
     * @param $withtemplate    integer  withtemplate param (default 0)
    **/
    static function showForItem(CommonDBTM $item, $withtemplate = 0) {
-      global $DB, $CFG_GLPI;
-
-      $item_id = $item->getID();
-      $item_type = $item::getType();
-      if (isset($_GET["start"])) {
-         $start = intval($_GET["start"]);
-      } else {
-         $start = 0;
-      }
+      global $CFG_GLPI;
 
       // Total Number of comments
       if ($item->getType() == KnowbaseItem::getType()) {
@@ -309,9 +301,7 @@ class KnowbaseItem_Comment extends CommonDBTM {
          }
 
          $html .= "<div class='item_content'>";
-         $html .= "<p>";
-         $html .= Toolbox::unclean_cross_side_scripting_deep($comment['comment']);
-         $html .= "</p>";
+         $html .= "<p>{$comment['comment']}</p>";
          $html .= "</div>";
          $html .= "</div>"; // displayed_content
 
