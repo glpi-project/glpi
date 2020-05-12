dfef33f19c6cecce3aeb49d84afd5910e048278e
cconard96 <cconard96@gmail.com>
2020-05-11 21:02:26 -0400

    Enhance status endpoint
    Add ability to change the status format using the Accept header.
     - Supports application/json and text/plain (default) formats
    Move logic of status checking to a class.
    Added ability to filter out potentially sensitive information from the status result.
     - The public status endpoint only returns public information.
     - Private information is only returned through the CLI command but could be done from an API endpoint later.
     - Sensitive information includes the names of plugins, versions, etc as they could make it easier for someone to breach a GLPI instance.
    Add tests for status checker.
    Add CLI command glpi:system:status to get the system status.
     - Accepts the --format parameter with the values json or plain (default) to change the output format.
     - Accepts --private parameter with a true or false (default) value to specify if private data should be returned.

--- status.php
+++ status.php
@@ -30,6 +30,8 @@
  * ---------------------------------------------------------------------
  */
 
+use Glpi\System\Status\StatusChecker;
+
 define('DO_NOT_CHECK_HTTP_REFERER', 1);
 include ('./inc/includes.php');
 
@@ -39,197 +41,16 @@ $_SESSION['glpi_use_mode'] = Session::NORMAL_MODE;
 // Need to be used using :
 // check_http -H servername -u /glpi/status.php -s GLPI_OK
 
+$valid_response_types = ['text/plain', 'application/json'];
+$fallback_response_type = 'text/plain';
 
-// Plain text content
-header('Content-type: text/plain');
-
-$ok_master = true;
-$ok_slave  = true;
-$ok        = true;
-
-// Check slave server connection
-if (DBConnection::isDBSlaveActive()) {
-   $DBslave = DBConnection::getDBSlaveConf();
-   if (is_array($DBslave->dbhost)) {
-      $hosts = $DBslave->dbhost;
-   } else {
-      $hosts = [$DBslave->dbhost];
-   }
-
-   foreach ($hosts as $num => $name) {
-      $diff = DBConnection::getReplicateDelay($num);
-      if (abs($diff) > 1000000000) {
-         echo "GLPI_DBSLAVE_${num}_OFFLINE\n";
-         $ok_slave = false;
-      } else if (abs($diff)> HOUR_TIMESTAMP) {
-         echo "GLPI_DBSLAVE_${num}_PROBLEM\n";
-         $ok_slave = false;
-      } else {
-         echo "GLPI_DBSLAVE_${num}_OK\n";
-      }
-   }
-} else {
-   echo "No slave DB\n";
+if (!isset($_SERVER['HTTP_ACCEPT']) || !in_array($_SERVER['HTTP_ACCEPT'], $valid_response_types, true)) {
+   $_SERVER['HTTP_ACCEPT'] = $fallback_response_type;
 }
+header('Content-type: ' . $_SERVER['HTTP_ACCEPT']);
 
-// Check main server connection
-if (DBConnection::establishDBConnection(false, true, false)) {
-   echo "GLPI_DB_OK\n";
+if ($_SERVER['HTTP_ACCEPT'] === 'application/json') {
+   echo json_encode(StatusChecker::getFullStatus(true, true));
 } else {
-   echo "GLPI_DB_PROBLEM\n";
-   $ok_master = false;
-}
-
-// Slave and master ok;
-$ok = $ok_slave && $ok_master;
-
-// Check session dir (useful when NFS mounted))
-if (is_dir(GLPI_SESSION_DIR) && is_writable(GLPI_SESSION_DIR)) {
-   echo "GLPI_SESSION_DIR_OK\n";
-} else {
-   echo "GLPI_SESSION_DIR_PROBLEM\n";
-   $ok = false;
-}
-
-// Reestablished DB connection
-if (($ok_master || $ok_slave )
-    && DBConnection::establishDBConnection(false, false, false)) {
-
-   // Check LDAP Auth connections
-   $ldap_methods = getAllDataFromTable('glpi_authldaps', ['is_active' => 1]);
-
-   if (count($ldap_methods)) {
-      echo "Check LDAP servers:";
-
-      foreach ($ldap_methods as $method) {
-         echo " ".$method['name'];
-         if (AuthLDAP::tryToConnectToServer($method, $method["rootdn"],
-                                            Toolbox::sodiumDecrypt($method["rootdn_passwd"]))) {
-            echo "_OK";
-         } else {
-            echo "_PROBLEM";
-            $ok = false;
-         }
-         echo "\n";
-      }
-
-   } else {
-      echo "No LDAP server\n";
-   }
-
-   // Check IMAP Auth connections
-   $imap_methods = getAllDataFromTable('glpi_authmails', ['is_active' => 1]);
-
-   if (count($imap_methods)) {
-      echo "Check IMAP servers:";
-
-      foreach ($imap_methods as $method) {
-         echo " ".$method['name'];
-         $param = Toolbox::parseMailServerConnectString($method['connect_string'], true);
-         if ($param['ssl'] === true) {
-            $host = 'ssl://'.$host;
-         } else {
-            if ($param['tls'] === true) {
-               $host = 'tls://'.$host;
-            }
-         }
-         if ($fp = @fsockopen($host, $param['port'],
-                            $errno, $errstr, 1)) {
-            echo "_OK";
-         } else {
-            echo "_PROBLEM";
-            $ok = false;
-         }
-         fclose($fp);
-         echo "\n";
-      }
-
-   } else {
-      echo "No IMAP server\n";
-   }
-
-   // Check CAS
-   if (!empty($CFG_GLPI["cas_host"])) {
-      echo "CAS_SERVER";
-
-      $url = $CFG_GLPI["cas_host"];
-      if (!empty($CFG_GLPI["cas_port"])) {
-         $url .= ':'.intval($CFG_GLPI["cas_port"]);
-      }
-      $url .= '/'.$CFG_GLPI["cas_uri"];
-      $data = Toolbox::getURLContent($url);
-      if (!empty($data)) {
-         echo "_OK";
-      } else {
-         echo "_PROBLEM";
-         $ok = false;
-      }
-      echo "\n";
-   } else {
-      echo "No CAS server\n";
-   }
-
-   /// Check mailcollectors
-   $mailcollectors = getAllDataFromTable('glpi_mailcollectors', ['is_active' => 1]);
-   if (count($mailcollectors)) {
-      echo "Check mail collectors:";
-      $mailcol = new MailCollector();
-      foreach ($mailcollectors as $mc) {
-         echo " ".$mc['name'];
-         if ($mailcol->getFromDB($mc['id'])) {
-            try {
-               $mailcol->connect();
-               echo "_OK";
-            } catch (\Exception $e) {
-               echo "_PROBLEM";
-               $ok = false;
-            }
-            echo "\n";
-         }
-      }
-
-   } else {
-      echo "No mail collector\n";
-   }
-
-   // Check crontask
-   $crontasks = getAllDataFromTable(
-      'glpi_crontasks', [
-         'state'  => CronTask::STATE_RUNNING,
-         'OR'     => [
-            new \QueryExpression(
-               '(unix_timestamp(' . DBmysql::quoteName('lastrun') . ') + 2 * '.
-               DBmysql::quoteName('frequency') .' < unix_timestamp(now()))'
-            ),
-            new \QueryExpression(
-               '(unix_timestamp(' . DBmysql::quoteName('lastrun') . ') + 2 * '.
-               HOUR_TIMESTAMP . ' < unix_timestamp(now()))'
-            )
-         ]
-      ]
-   );
-   if (count($crontasks)) {
-      echo "Check crontasks:";
-      foreach ($crontasks as $ct) {
-         echo " ".$ct['name']."_PROBLEM\n";
-         $ok = false;
-      }
-   } else {
-      echo "Crontasks_OK\n";
-   }
-
-   // hook for plugin
-   $param = ['ok' => $ok];
-   Plugin::doHook("status", $param);
-   if (isset($param['ok'])) {
-      $ok = $param['ok'];
-   }
-}
-
-echo "\n";
-
-if ($ok) {
-   echo "GLPI_OK\n";
-} else {
-   echo "GLPI_PROBLEM\n";
+   echo StatusChecker::getFullStatus(true, false);
 }
