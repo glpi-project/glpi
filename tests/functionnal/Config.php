8e8b5156574980ab4b676bc81e7a2535b15b6fa4
Steven <Nevets82@users.noreply.github.com>
2020-06-02 09:33:43 +0200

    Fixed issue #7302 (#7303)
    - Added support for hyphen in language ID;
    - Added test for language ID with hyphen;

--- tests/functionnal/Config.php
+++ tests/functionnal/Config.php
@@ -502,6 +502,8 @@ class Config extends DbTestCase {
                ->isIdenticalTo('fr_FR')
             ->string($this->testedInstance->getLanguage('fr_FR'))
                ->isIdenticalTo('fr_FR')
+            ->string($this->testedInstance->getLanguage('fr-FR'))
+               ->isIdenticalTo('fr_FR')
             ->string($this->testedInstance->getLanguage('Français'))
                ->isIdenticalTo('fr_FR')
             ->string($this->testedInstance->getLanguage('french'))
