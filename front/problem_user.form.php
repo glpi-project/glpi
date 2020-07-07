90551a457fb24a5cb42fc87ffaa58eae1f4350bb
Cédric Anne <cedric.anne@gmail.com>
2020-07-07 09:34:16 +0200

    Fix form refresh after problem actor email update

--- front/problem_user.form.php
+++ front/problem_user.form.php
@@ -48,7 +48,14 @@ Html::popHeader(__('Email followup'), $_SERVER['PHP_SELF']);
 
 if (isset($_POST["update"])) {
    $link->check($_POST["id"], UPDATE);
-   $link->update($_POST);
+
+   if ($link->update($_POST)) {
+      echo "<script type='text/javascript' >\n";
+      echo "window.parent.location.reload();";
+      echo "</script>";
+   } else {
+      Html::back();
+   }
 
 } else if (isset($_POST['delete'])) {
    $link->check($_POST['id'], DELETE);
