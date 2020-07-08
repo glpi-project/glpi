685bd3b032b60a5af737bb8272bfa97eff3d4ee4
adelaunay <delaunay.alexandre@gmail.com>
2020-07-08 15:47:20 +0200

    fix setDate in planning (week shifting when selecting a monday), fix #7623

--- js/planning.js
+++ js/planning.js
@@ -894,7 +894,10 @@ var GLPIPlanning  = {
       $('#planning_datepicker').flatpickr({
          defaultDate: currentDate,
          onChange: function(selected_date) {
-            GLPIPlanning.calendar.gotoDate(selected_date[0]);
+            new_date = selected_date[0];
+            // shift to 12:00 to avoid timezone issues
+            new_date.setHours(12, 0, 0);
+            GLPIPlanning.calendar.gotoDate(new_date);
          }
       });
    },
