4971529d3f2a54b438648dc65364e400df9aa9ed
Cédric Anne <cedric.anne@gmail.com>
2020-07-16 15:39:09 +0200

    Fix update called via install process

--- install/update.php
+++ install/update.php
@@ -38,6 +38,8 @@ include_once (GLPI_ROOT . "/inc/based_config.php");
 include_once (GLPI_ROOT . "/inc/db.function.php");
 include_once (GLPI_CONFIG_DIR . "/config_db.php");
 
+global $DB, $GLPI, $GLPI_CACHE;
+
 $GLPI = new GLPI();
 $GLPI->initLogger();
 $GLPI->initErrorHandler();
