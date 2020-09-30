bf1347081aa815c55fc59bbcaab7e3757c1de0b5
Johan Cwiklinski <jcwiklinski@teclib.com>
2020-09-30 12:21:28 +0200

    Better solution than preg_match

--- inc/commondbchild.class.php
+++ inc/commondbchild.class.php
@@ -861,9 +861,10 @@ abstract class CommonDBChild extends CommonDBConnexity {
    }
 
    public static final function getItemField($itemtype): string {
-      if (preg_match('/^Rule.+/', $itemtype)) {
+      if (is_subclass_of($itemtype, 'Rule')) {
          $itemtype = 'Rule';
       }
+
       if (isset(static::$items_id) && getItemtypeForForeignKeyField(static::$items_id) == $itemtype) {
          return static::$items_id;
       }
