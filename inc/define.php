026e73e636faea2b0445ec4752198febfc04dec7
Johan Cwiklinski <jcwiklinski@teclib.com>
2020-10-07 08:15:39 +0200

    Bump version

--- inc/define.php
+++ inc/define.php
@@ -31,7 +31,7 @@
 */
 
 // Current version of GLPI
-define('GLPI_VERSION', '9.5.1');
+define('GLPI_VERSION', '9.5.2');
 if (substr(GLPI_VERSION, -4) === '-dev') {
    //for dev version
    define('GLPI_PREVER', str_replace('-dev', '', GLPI_VERSION));
