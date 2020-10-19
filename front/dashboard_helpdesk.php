acbc876e82bd6113f15a25aa15a8575f3c50f97a
AdrienClairembault <aclairembault@teclib.com>
2020-10-19 11:20:48 +0200

    Redirect assets and assistance menu if no dashboard

--- front/dashboard_helpdesk.php
+++ front/dashboard_helpdesk.php
@@ -39,10 +39,15 @@ include ('../inc/includes.php');
 
 
 Session::checkCentralAccess();
+$default = Glpi\Dashboard\Grid::getDefaultDashboardForMenu('helpdesk');
+
+// Redirect to "/front/ticket.php" if no dashboard found
+if ($default == "") {
+   Html::redirect($CFG_GLPI["root_doc"] . "/front/ticket.php");
+}
 
 Html::header(__('Helpdesk Dashboard'), $_SERVER['PHP_SELF'], "helpdesk", "dashboard");
 
-$default = Glpi\Dashboard\Grid::getDefaultDashboardForMenu('helpdesk');
 $dashboard = new Glpi\Dashboard\Grid($default);
 $dashboard->showDefault();
 
