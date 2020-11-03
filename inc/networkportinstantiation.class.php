e388c1e97807b110428246be164faea5a189359b
cconard96 <cconard96@gmail.com>
2020-11-03 06:25:24 -0500

    Revert $this usage
    fixes #8183

--- inc/networkportinstantiation.class.php
+++ inc/networkportinstantiation.class.php
@@ -191,7 +191,7 @@ class NetworkPortInstantiation extends CommonDBChild {
                                                     HTMLTableCell $father = null,
                                                     array $options = []) {
 
-      $this->getInstantiationHTMLTable($netport, $row, $father, $options);
+      self::getInstantiationHTMLTable($netport, $row, $father, $options);
       return null;
 
    }
