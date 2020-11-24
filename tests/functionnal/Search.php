43e172d3830291136da97371ff0aeaf7b9f6b1af
Cédric Anne <cedric.anne@gmail.com>
2020-11-24 08:39:35 +0100

    Add test case

--- tests/functionnal/Search.php
+++ tests/functionnal/Search.php
@@ -103,6 +103,14 @@ class Search extends DbTestCase {
          ->hasKey('data')
             ->array['last_errors']->isIdenticalTo([])
             ->array['data']->isNotEmpty();
+
+      $this->string($data['sql']['search'])
+         ->matches('/'
+            . 'LEFT JOIN\s*`glpi_items_softwareversions`\s*AS\s*`glpi_items_softwareversions_[^`]+_Software`\s*ON\s*\('
+            . '`glpi_items_softwareversions_[^`]+_Software`\.`items_id`\s*=\s*`glpi_computers`.`id`'
+            . '\s*AND\s*`glpi_items_softwareversions_[^`]+_Software`\.`itemtype`\s*=\s*\'Computer\''
+            . '\s*AND\s*`glpi_items_softwareversions_[^`]+_Software`\.`is_deleted`\s*=\s*0'
+            . '\)/im');
    }
 
    public function testMetaComputerUser() {
