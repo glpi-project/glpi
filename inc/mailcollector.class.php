f44aeca10af8d453334d7427b202bc0fc07604c8
Johan Cwiklinski <jcwiklinski@teclib.com>
2020-12-02 07:47:33 +0100

    Prevent object to be sanitized; closes #8345

--- inc/mailcollector.class.php
+++ inc/mailcollector.class.php
@@ -1059,7 +1059,6 @@ class MailCollector  extends CommonDBTM {
       } catch (Laminas\Mail\Storage\Exception\InvalidArgumentException $e) {
          $subject = null;
       }
-      $tkt['_message']  = $message;
 
       if (!Toolbox::seems_utf8($body)) {
          $tkt['content'] = Toolbox::encodeInUtf8($body);
@@ -1216,6 +1215,8 @@ class MailCollector  extends CommonDBTM {
       $tkt['content'] = LitEmoji::encodeShortcode($tkt['content']);
 
       $tkt = Toolbox::addslashes_deep($tkt);
+      //class must not ba passed through addslashes_deep
+      $tkt['_message'] = $message;
       return $tkt;
    }
 
