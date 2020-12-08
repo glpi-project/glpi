46ab474775edfacfb0cd62fe4dec342502445459
Cédric Anne <cedric.anne@gmail.com>
2020-12-08 12:20:53 +0100

    Bootstrap 5.0.0-beta1

--- inc/commonglpi.class.php
+++ inc/commonglpi.class.php
@@ -1076,7 +1076,7 @@ JAVASCRIPT;
             if (count($actions)) {
                echo "<div class='dropdown'>";
                echo "<button class='btn btn-sm btn-secondary dropdown-toggle' type='button'
-                             id='single-action' data-toggle='dropdown' aria-haspopup='true'
+                             id='single-action' data-bs-toggle='dropdown' aria-haspopup='true'
                              aria-expanded='false'>
                         ".__("Actions")."
                      </button>";
