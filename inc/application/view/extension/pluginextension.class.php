603fc1abe3bb1ff88d37ee4f5d301b40277ac5d3
adelaunay <delaunay.alexandre@gmail.com>
2020-12-09 11:43:07 +0100

    enhance login page

---
+++ inc/application/view/extension/pluginextension.class.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * ---------------------------------------------------------------------
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2015-2020 Teclib' and contributors.
+ *
+ * http://glpi-project.org
+ *
+ * based on GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2003-2014 by the INDEPNET Development Team.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * GLPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GLPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ */
+
+namespace Glpi\Application\View\Extension;
+
+use Plugin;
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\ExtensionInterface;
+use Twig\TwigFunction;
+
+/**
+ * @since x.x.x
+ */
+class PluginExtension extends AbstractExtension implements ExtensionInterface {
+   public function getFunctions() {
+      return [
+         new TwigFunction('displayLogin', [$this, 'displayLogin'], ['is_safe' => ['html']]),
+      ];
+   }
+
+   /**
+    * call display_login plugin hook to display aditionnal html on login page
+    *
+    * @return void
+    */
+   public function displayLogin() {
+      Plugin::doHook('display_login');
+   }
+}
