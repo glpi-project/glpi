649e5ebdeb48fb65ed942f9d6e98e02fea88975e
Cédric Anne <cedric.anne@gmail.com>
2020-12-16 10:55:55 +0100

    Fix "Field 'date' doesn't have a default value"

--- inc/mailcollector.class.php
+++ inc/mailcollector.class.php
@@ -736,6 +736,7 @@ class MailCollector  extends CommonDBTM {
                   'from'              => '',
                   'to'                => '',
                   'messageid'         => '',
+                  'date'              => $_SESSION["glpi_currenttime"],
                ];
 
                //prevent loop when message is read but when it's impossible to move / delete
@@ -767,7 +768,6 @@ class MailCollector  extends CommonDBTM {
                      $rejinput['subject']           = $DB->escape($this->cleanSubject($headers['subject']));
                      $rejinput['messageid']         = $headers['message_id'];
                   }
-                  $rejinput['date']              = $_SESSION["glpi_currenttime"];
                } catch (Throwable $e) {
                   $error++;
                   Toolbox::logInFile('mailgate', sprintf(__('Error during message parsing: %1$s').'<br/>', $e->getMessage()));
