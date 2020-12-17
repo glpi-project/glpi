f8da8b0fe93a24f8f1557c67dc3ea8064bdf178e
Cédric Anne <cedric.anne@gmail.com>
2020-12-17 16:05:20 +0100

    Fix CS

--- inc/search.class.php
+++ inc/search.class.php
@@ -1543,8 +1543,6 @@ class Search {
          Session::initNavigateListItems($data['itemtype']);
       }
 
-      
-
       TemplateRenderer::getInstance()->display('layout/parts/search/display_data.html.twig', [
          'data'                => $data,
          'union_search_type'   => $CFG_GLPI["union_search_type"],
