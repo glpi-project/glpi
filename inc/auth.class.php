90d34a14bcef2e45682b6109bac036a719e6e093
finalbeta <finalbeta@finalbeta.net>
2020-12-28 20:38:41 +0100

    Fix auto user creation for other authentication type; fixes #8455

--- inc/auth.class.php
+++ inc/auth.class.php
@@ -920,6 +920,9 @@ class Auth extends CommonGLPI {
                // Then ensure addslashes
                $input = Toolbox::addslashes_deep($input);
                unset ($this->user->fields);
+               if ($authtype == self::EXTERNAL && !isset($input["authtype"])) {
+                  $input["authtype"] = $authtype;
+               }
                $this->user->add($input);
             } else {
                // Auto add not enable so auth failed
