4e06f54bcd0be40ad1f1c1ef7a70e88705ee847a
Cédric Anne <cedric.anne@gmail.com>
2021-01-04 11:22:35 +0100

    happy new year 2021 GLPI :tada: (master branch)

--- inc/stat/statdataalwaysdisplay.class.php
+++ inc/stat/statdataalwaysdisplay.class.php
@@ -2,7 +2,7 @@
 /**
  * ---------------------------------------------------------------------
  * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2015-2020 Teclib' and contributors.
+ * Copyright (C) 2015-2021 Teclib' and contributors.
  *
  * http://glpi-project.org
  *
@@ -24,6 +24,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
  * ---------------------------------------------------------------------
