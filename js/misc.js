86bef4ff98def626bedd0381d68c8947542d636d
Cédric Anne <cedric.anne@gmail.com>
2021-01-05 12:00:29 +0100

    Fix licence headers

--- js/misc.js
+++ js/misc.js
@@ -1,7 +1,7 @@
 /**
  * ---------------------------------------------------------------------
  * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2015-2020 Teclib' and contributors.
+ * Copyright (C) 2015-2021 Teclib' and contributors.
  *
  * http://glpi-project.org
  *
@@ -28,7 +28,6 @@
  * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
  * ---------------------------------------------------------------------
  */
-
 
 /*
  * Redefine 'window.alert' javascript function by a jquery-ui dialog equivalent (but prettier).
