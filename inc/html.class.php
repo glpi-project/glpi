bd12f119765bdf0d941bf69945423dc85e9569f1
adelaunay <delaunay.alexandre@gmail.com>
2021-01-07 15:03:50 +0100

    display error and die with twig

--- inc/html.class.php
+++ inc/html.class.php
@@ -847,7 +847,7 @@ class Html {
     * @return void
    **/
    static function displayErrorAndDie ($message, $minimal = false) {
-      global $CFG_GLPI, $HEADER_LOADED;
+      global $HEADER_LOADED;
 
       if (!$HEADER_LOADED) {
          if ($minimal || !Session::getCurrentInterface()) {
@@ -860,10 +860,11 @@ class Html {
             self::helpHeader(__('Access denied'), '');
          }
       }
-      echo "<div class='center'><br><br>";
-      echo Html::image($CFG_GLPI["root_doc"] . "/pics/warning.png", ['alt' => __('Warning')]);
-      echo "<br><br><span class='b'>$message</span></div>";
-      self::nullFooter();
+
+      TemplateRenderer::getInstance()->display('display_and_die.html.twig', [
+         'title'   => __('Access denied'),
+         'message' => $message,
+      ]);
       exit ();
    }
 
