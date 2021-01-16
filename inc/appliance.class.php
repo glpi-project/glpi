6e19a0b41cf905205828e15bf498ea6bba966a40
cconard96 <cconard96@gmail.com>
2021-01-16 11:10:30 -0500

    Add contracts to domains and links to domains and appliances
    fixes #8533

--- inc/appliance.class.php
+++ inc/appliance.class.php
@@ -74,6 +74,7 @@ class Appliance extends CommonDBTM {
          ->addStandardTab('Ticket', $ong, $options)
          ->addStandardTab('Item_Problem', $ong, $options)
          ->addStandardTab('Change_Item', $ong, $options)
+         ->addStandardTab('Link', $ong, $options)
          ->addStandardTab('Notepad', $ong, $options)
          ->addStandardTab('Log', $ong, $options);
 
