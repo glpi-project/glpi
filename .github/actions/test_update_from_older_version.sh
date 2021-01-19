0ec8c7bedfe9648bb6cfc1787c1abd5febf6108c
Cédric Anne <cedric.anne@gmail.com>
2021-01-19 15:56:37 +0100

    Enhance test suite logs
    - Display ansi colors on console commands
    - Use tee + grep to preserve log output
    - Switch to en_GB for default lang

---
+++ .github/actions/test_update_from_older_version.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+bin/console glpi:database:configure --config-dir=./tests --ansi --no-interaction --reconfigure --db-name=glpitest080 --db-host=db --db-user=root
+
+# Execute update
+## First run should do the migration (with no warnings).
+bin/console glpi:database:update --config-dir=./tests --ansi --no-interaction --allow-unstable | tee ~/migration.log
+if [[ -n $(grep "Warning\|No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:database:update command FAILED" && exit 1; fi
+## Second run should do nothing.
+bin/console glpi:database:update --config-dir=./tests --ansi --no-interaction --allow-unstable | tee ~/migration.log
+if [[ -z $(grep "No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:database:update command FAILED" && exit 1; fi
+
+# Execute myisam_to_innodb migration
+## First run should do the migration (with no warnings).
+bin/console glpi:migration:myisam_to_innodb --config-dir=./tests --ansi --no-interaction | tee ~/migration.log
+if [[ -n $(grep "Warning\|No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:migration:myisam_to_innodb command FAILED" && exit 1; fi
+## Second run should do nothing.
+bin/console glpi:migration:myisam_to_innodb --config-dir=./tests --ansi --no-interaction | tee ~/migration.log
+if [[ -z $(grep "No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:migration:myisam_to_innodb command FAILED" && exit 1; fi
+
+# Execute timestamps migration
+## First run should do the migration (with no warnings).
+bin/console glpi:migration:timestamps --config-dir=./tests --ansi --no-interaction | tee ~/migration.log
+if [[ -n $(grep "Warning\|No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:migration:timestamps command FAILED" && exit 1; fi
+## Second run should do nothing.
+bin/console glpi:migration:timestamps --config-dir=./tests --ansi --no-interaction | tee ~/migration.log
+if [[ -z $(grep "No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:migration:timestamps command FAILED" && exit 1; fi
+
+# Execute utf8mb4 migration
+## First run should do the migration (with no warnings).
+bin/console glpi:migration:utf8mb4 --config-dir=./tests --ansi --no-interaction | tee ~/migration.log
+if [[ -n $(grep "Warning\|No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:migration:utf8mb4 command FAILED" && exit 1; fi
+## Second run should do nothing.
+bin/console glpi:migration:utf8mb4 --config-dir=./tests --ansi --no-interaction | tee ~/migration.log
+if [[ -z $(grep "No migration needed." ~/migration.log) ]]; then echo "bin/console glpi:migration:utf8mb4 command FAILED" && exit 1; fi
