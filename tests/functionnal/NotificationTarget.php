69f7c276caf8d6d3475829735ea4510ce165b017
adelaunay <delaunay.alexandre@gmail.com>
2021-01-22 12:03:42 +0100

    add missing test

--- tests/functionnal/NotificationTarget.php
+++ tests/functionnal/NotificationTarget.php
@@ -91,4 +91,119 @@ class NotificationTarget extends DbTestCase {
       $this->string($ntarget_child_1->getSubjectPrefix())->isEqualTo("[prefix_child_1] ");
       $this->string($ntarget_child_2->getSubjectPrefix())->isEqualTo("[prefix_child_2] ");
    }
+
+   public function testGetReplyTo() {
+      global $CFG_GLPI;
+
+      $this->login();
+
+      $root    = getItemByTypeName('Entity', 'Root entity', true);
+      $parent  = getItemByTypeName('Entity', '_test_root_entity', true);
+      $child_1 = getItemByTypeName('Entity', '_test_child_1', true);
+      $child_2 = getItemByTypeName('Entity', '_test_child_2', true);
+
+      $ntarget_parent  = new \NotificationTarget($parent);
+      $ntarget_child_1 = new \NotificationTarget($child_1);
+      $ntarget_child_2 = new \NotificationTarget($child_2);
+
+      // test global settings
+      $CFG_GLPI['admin_reply'] = 'test@global.tld';
+      $CFG_GLPI['admin_reply_name'] = 'test global';
+      $CFG_GLPI['from_email'] = '';
+
+      $this->array($ntarget_parent->getReplyTo())->isEqualTo([
+         'email' => 'test@global.tld',
+         'name'  => 'test global'
+      ]);
+      $this->array($ntarget_child_1->getReplyTo())->isEqualTo([
+         'email' => 'test@global.tld',
+         'name'  => 'test global'
+      ]);
+      $this->array($ntarget_child_2->getReplyTo())->isEqualTo([
+         'email' => 'test@global.tld',
+         'name'  => 'test global'
+      ]);
+
+      // test root entity settings
+      $entity  = new \Entity;
+      $this->boolean($entity->update([
+         'id'               => $root,
+         'admin_reply'      => "test@root.tld",
+         'admin_reply_name' => "test root",
+      ]))->isTrue();
+
+      $this->array($ntarget_parent->getReplyTo())->isEqualTo([
+         'email' => 'test@root.tld',
+         'name'  => 'test root'
+      ]);
+      $this->array($ntarget_child_1->getReplyTo())->isEqualTo([
+         'email' => 'test@root.tld',
+         'name'  => 'test root'
+      ]);
+      $this->array($ntarget_child_2->getReplyTo())->isEqualTo([
+         'email' => 'test@root.tld',
+         'name'  => 'test root'
+      ]);
+
+      // test parent entity settings
+      $this->boolean($entity->update([
+         'id'               => $parent,
+         'admin_reply'      => "test@parent.tld",
+         'admin_reply_name' => "test parent",
+      ]))->isTrue();
+
+      $this->array($ntarget_parent->getReplyTo())->isEqualTo([
+         'email' => 'test@parent.tld',
+         'name'  => 'test parent'
+      ]);
+      $this->array($ntarget_child_1->getReplyTo())->isEqualTo([
+         'email' => 'test@parent.tld',
+         'name'  => 'test parent'
+      ]);
+      $this->array($ntarget_child_2->getReplyTo())->isEqualTo([
+         'email' => 'test@parent.tld',
+         'name'  => 'test parent'
+      ]);
+
+      // test child_1 entity settings
+      $this->boolean($entity->update([
+         'id'               => $child_1,
+         'admin_reply'      => "test@child1.tld",
+         'admin_reply_name' => "test child1",
+      ]))->isTrue();
+
+      $this->array($ntarget_parent->getReplyTo())->isEqualTo([
+         'email' => 'test@parent.tld',
+         'name'  => 'test parent'
+      ]);
+      $this->array($ntarget_child_1->getReplyTo())->isEqualTo([
+         'email' => 'test@child1.tld',
+         'name'  => 'test child1'
+      ]);
+      $this->array($ntarget_child_2->getReplyTo())->isEqualTo([
+         'email' => 'test@parent.tld',
+         'name'  => 'test parent'
+      ]);
+
+      // test child_2 entity settings
+      $this->boolean($entity->update([
+         'id'               => $child_2,
+         'admin_reply'      => "test@child2.tld",
+         'admin_reply_name' => "test child2",
+      ]))->isTrue();
+
+      $this->array($ntarget_parent->getReplyTo())->isEqualTo([
+         'email' => 'test@parent.tld',
+         'name'  => 'test parent'
+      ]);
+      $this->array($ntarget_child_1->getReplyTo())->isEqualTo([
+         'email' => 'test@child1.tld',
+         'name'  => 'test child1'
+      ]);
+      $this->array($ntarget_child_2->getReplyTo())->isEqualTo([
+         'email' => 'test@child2.tld',
+         'name'  => 'test child2'
+      ]);
+
+   }
 }
