d86cb3a1ae6a92180562cbcb39cb192cda9c049e
Johan Cwiklinski <johan@x-tnd.be>
2021-01-27 11:33:22 +0100

    Fix XML inventory from refused logs

--- inc/inventory/inventory.class.php
+++ inc/inventory/inventory.class.php
@@ -55,7 +55,7 @@ class Inventory
    /** @var array */
    protected $data = [];
    /** @var array */
-   private $metadata;
+   private $metadata = [];
    /** @var array */
    private $errors = [];
    /** @var CommonDBTM */
