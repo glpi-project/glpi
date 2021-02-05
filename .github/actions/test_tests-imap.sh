0cea25475593e38d226b5497f74f93529e04e985
Johan Cwiklinski <johan@x-tnd.be>
2021-02-05 15:47:34 +0100

    Push code coverage to codecov service

--- .github/actions/test_tests-imap.sh
+++ .github/actions/test_tests-imap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -e
 
+ATOUM_ADDITIONNAL_OPTIONS=""
+if [[ -z "$COVERAGE_DIR" ]];
+  then ATOUM_ADDITIONNAL_OPTIONS="--no-code-coverage";
+fi
+
 vendor/bin/atoum \
   -p 'php -d memory_limit=512M' \
   --debug \
   --force-terminal \
   --use-dot-report \
   --bootstrap-file tests/bootstrap.php \
-  --no-code-coverage \
   --fail-if-skipped-methods \
+  $ATOUM_ADDITIONNAL_OPTIONS \
   --max-children-number 1 \
   -d tests/imap
