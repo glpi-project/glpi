e4821d5c0c034eb2fa52e292c19511ded9068be1
Cédric Anne <cedric.anne@gmail.com>
2021-02-12 13:03:28 +0100

    Enhance CI containers management
    - permit scripts to be executed from any path;
    - handle containers init in a unique script;
    - exec docker commands using docker-compose exec;
    - do healthcheck on all containers listed in compose files;
    - try to clean old DB and fixtures before recreating them;
    - clean only containers listed in compose files.

--- .github/actions/init_initialize-services-fixtures.sh
+++ .github/actions/init_initialize-services-fixtures.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -e
 
+ROOT_DIR=$(readlink -f "$(dirname $0)/../..")
+
 echo "Initialize LDAP fixtures"
-for f in `ls tests/LDAP/ldif/*.ldif`;
-  do cat $f | docker exec --interactive openldap ldapadd -x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=glpi,dc=org" -w insecure ;
+for f in `ls $ROOT_DIR/tests/LDAP/ldif/*.ldif`; do
+  # Delete all LDAP entries (in reverse ordre compared to creation)
+  # but ignore errors as these entries may not exists.
+  (tac $f | grep -E '^dn:' | sed -E "s/^dn: (.*)$/\\1/" | docker-compose exec -T openldap ldapdelete -x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=glpi,dc=org" -w insecure -c) \
+  || true
+done
+for f in `ls $ROOT_DIR/tests/LDAP/ldif/*.ldif`; do
+  cat $f | docker-compose exec -T openldap ldapadd -x -H ldap://127.0.0.1:3890/ -D "cn=Manager,dc=glpi,dc=org" -w insecure
 done
 
 echo "Initialize email fixtures"
-for f in `ls tests/emails-tests/*.eml`;
-  do cat $f | docker exec --user glpi --interactive dovecot getmail_maildir /home/glpi/Maildir/ ;
+docker-compose exec -T --user root dovecot doveadm expunge -u glpi mailbox 'INBOX' all
+docker-compose exec -T --user root dovecot doveadm purge -u glpi
+for f in `ls $ROOT_DIR/tests/emails-tests/*.eml`; do
+  cat $f | docker-compose exec -T --user glpi dovecot getmail_maildir /home/glpi/Maildir/
 done
