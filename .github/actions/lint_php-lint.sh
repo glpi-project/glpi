44be312e941b556698ac71f7e036af35034f4b69
Cédric Anne <cedric.anne@gmail.com>
2021-02-17 12:13:36 +0100

    Update webmozart/glob to 4.3.0

--- .github/actions/lint_php-lint.sh
+++ .github/actions/lint_php-lint.sh
@@ -10,12 +10,7 @@ vendor/bin/parallel-lint \
   .
 
 echo "Check for missing dependencies / bad symbols"
-# Alpine linux does not implement GLOB_BRACE.
-# We have to define it to 0 to prevent "Warning: Use of undefined constant GLOB_BRACE - assumed 'GLOB_BRACE'" error.
-# This is not a problem as long as we do not use braces in "scan-files" section of the config file.
-php -d memory_limit=1G \
-  -r 'define("GLOB_BRACE", 0); include "./vendor/maglnet/composer-require-checker/bin/composer-require-checker.php";' \
-  check --config-file=.composer-require-checker.config.json
+php -d memory_limit=1G vendor/bin/composer-require-checker check --config-file=.composer-require-checker.config.json
 
 echo "Check for coding standards violations"
 touch /home/glpi/phpcs.cache
