561031b88776d833db0ac82bfb7b53b53812496a
adelaunay <delaunay.alexandre@gmail.com>
2021-02-26 16:04:04 +0100

    continue working on ticket form

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -30,6 +30,7 @@
  * ---------------------------------------------------------------------
  */
 
+use Glpi\Api\Deprecated\TicketFollowup;
 use Glpi\Application\View\TemplateRenderer;
 use Glpi\Event;
 
@@ -4474,6 +4475,7 @@ class Ticket extends CommonITILObject {
       $this->initForm($ID, $options);
       TemplateRenderer::getInstance()->display('components/itilobject/layout.html.twig', [
          'item'     => $this,
+         'followup' => new ITILFollowup,
          'params'   => $options,
          'timeline' => $this->getTimelineItems(),
          'template' => $tt,
