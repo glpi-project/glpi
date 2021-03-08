39c0eb608d4a014e6212d8878894f93202600a26
Cédric Anne <cedric.anne@gmail.com>
2021-03-08 15:04:18 +0100

    Fix API errors on deprecation checks

--- inc/autoload.function.php
+++ inc/autoload.function.php
@@ -87,7 +87,7 @@ function isAPI() {
 function isPluginItemType($classname) {
 
    /** @var array $matches */
-   if (preg_match("/Plugin([A-Z][a-z0-9]+)([A-Z]\w+)/", $classname, $matches)) {
+   if (preg_match("/^Plugin([A-Z][a-z0-9]+)([A-Z]\w+)$/", $classname, $matches)) {
       $plug           = [];
       $plug['plugin'] = $matches[1];
       $plug['class']  = $matches[2];
