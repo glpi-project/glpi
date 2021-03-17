ec99422624326111c9c655a6be10d02ea43fba65
adelaunay <delaunay.alexandre@gmail.com>
2021-03-17 14:38:26 +0100

    display assign number of tickets in actor field

--- inc/dropdown.class.php
+++ inc/dropdown.class.php
@@ -3819,7 +3819,8 @@ class Dropdown {
             'id'       => "User_$ID",
             'text'     => $text,
             'title'    => sprintf(__('%1$s - %2$s'), $text, $user['name']),
-            'itemtype' => "User"
+            'itemtype' => "User",
+            'items_id' => $ID,
          ];
       }
 
@@ -3843,6 +3844,7 @@ class Dropdown {
       foreach ($groups['results'] as $group) {
          if (isset($group['children'])) {
             foreach ($group['children'] as &$children) {
+               $children['items_id'] = $children['id'];
                $children['id']       = "Group".$children['id'];
                $children['itemtype'] = "Group";
             }
@@ -3864,6 +3866,7 @@ class Dropdown {
          foreach ($suppliers['results'] as $supplier) {
             if (isset($supplier['children'])) {
                foreach ($supplier['children'] as &$children) {
+                  $children['items_id'] = $children['id'];
                   $children['id']       = "Supplier".$children['id'];
                   $children['itemtype'] = "Supplier";
                }
