943d1026dc6fb6896805d933b50f973447d02cb3
adelaunay <delaunay.alexandre@gmail.com>
2021-03-18 09:46:54 +0100

    set default value of actor fields

--- inc/dropdown.class.php
+++ inc/dropdown.class.php
@@ -3844,7 +3844,7 @@ class Dropdown {
          if (isset($group['children'])) {
             foreach ($group['children'] as &$children) {
                $children['items_id'] = $children['id'];
-               $children['id']       = "Group".$children['id'];
+               $children['id']       = "Group_".$children['id'];
                $children['itemtype'] = "Group";
             }
          }
@@ -3866,7 +3866,7 @@ class Dropdown {
             if (isset($supplier['children'])) {
                foreach ($supplier['children'] as &$children) {
                   $children['items_id'] = $children['id'];
-                  $children['id']       = "Supplier".$children['id'];
+                  $children['id']       = "Supplier_".$children['id'];
                   $children['itemtype'] = "Supplier";
                }
             }
