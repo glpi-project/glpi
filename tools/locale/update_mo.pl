84ff6476f4121869f9585ab37c22a079fcd4af6f
Cédric Anne <cedric.anne@gmail.com>
2021-03-22 09:02:43 +0100

    Make mo compile script callable from anywhere

--- tools/locale/update_mo.pl
+++ tools/locale/update_mo.pl
@@ -30,28 +30,25 @@
 #  * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
 #  * ---------------------------------------------------------------------
 # */
-if (@ARGV!=0){
-print "USAGE update_mo.pl\n\n";
 
-exit();
+use File::Basename;
+
+if (@ARGV != 0) {
+   print "USAGE update_mo.pl\n\n";
+   exit();
 }
 
-opendir(DIRHANDLE,'locales')||die "ERROR: can not read current directory\n"; 
-foreach (readdir(DIRHANDLE)){ 
-	if ($_ ne '..' && $_ ne '.'){
-
-            if(!(-l "$dir/$_")){
-                     if (index($_,".po",0)==length($_)-3) {
-                        $lang=$_;
-                        $lang=~s/\.po//;
-                        
-                        `msgfmt locales/$_ -o locales/$lang.mo`;
-                     }
-            }
-
-	}
+my $dirname = dirname(__FILE__) . '/../../locales';
+opendir(DIRHANDLE, $dirname) || die "ERROR: can not read locales directory\n";
+foreach (readdir(DIRHANDLE)) {
+   if ($_ ne '..' && $_ ne '.') {
+      if (!(-l "$dirname/$_")) {
+         if (index($_,".po",0) == length($_)-3) {
+            $lang=$_;
+            $lang=~s/\.po//;
+            `msgfmt $dirname/$_ -o $dirname/$lang.mo`;
+         }
+      }
+   }
 }
 closedir DIRHANDLE; 
-
-#  
-#  
