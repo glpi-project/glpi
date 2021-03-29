602c8c1262ab8bed6dc37ca4e9fea45b580adffd
adelaunay <delaunay.alexandre@gmail.com>
2021-03-29 14:18:53 +0200

    itilstatus in dropdown (templates for select2)

--- inc/html.class.php
+++ inc/html.class.php
@@ -4396,6 +4396,9 @@ JAVASCRIPT
          $placeholder = "placeholder: ".json_encode($params["placeholder"]).",";
       }
 
+      $templateresult    = $params["templateResult"] ?? "templateResult";
+      $templateselection = $params["templateSelection"] ?? "templateSelection";
+
       $js = "$(function() {
          $('#$id').select2({
             $placeholder
@@ -4481,8 +4484,8 @@ JAVASCRIPT
                // Return `null` if the term should not be displayed
                return null;
             },
-            templateResult: templateResult,
-            templateSelection: templateSelection,
+            templateResult: $templateresult,
+            templateSelection: $templateselection,
          })
          .bind('setValue', function(e, value) {
             $('#$id').val(value).trigger('change');
