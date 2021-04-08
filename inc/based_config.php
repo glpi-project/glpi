a1b8b11f9adccaf54d2a94e6212d774e66c1e2f6
Cédric Anne <cedric.anne@gmail.com>
2021-04-08 14:53:25 +0200

    better comment

--- inc/based_config.php
+++ inc/based_config.php
@@ -89,7 +89,7 @@ include_once (GLPI_ROOT . "/inc/autoload.function.php");
       // TODO set false before final release of 9.5.0 and remove this comment
       'GLPI_MARKETPLACE_PRERELEASES'      => false, // display pre-releases of plugins in marketplace
       'GLPI_MARKETPLACE_ALLOW_OVERRIDE'   => true, // allow marketplace to override a plugin found outside GLPI_MARKETPLACE_DIR
-      'GLPI_MARKETPLACE_MANUAL_DOWNLOADS' => true, // propose manual download of plugins that cannot be overrided by marketplace
+      'GLPI_MARKETPLACE_MANUAL_DOWNLOADS' => true, // propose manual download link of plugins which cannot be installed/updated by marketplace
       'GLPI_USER_AGENT_EXTRA_COMMENTS'    => '', // Extra comment to add to GLPI User-Agent
 
       // Other constants
