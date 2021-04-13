238e55da3d62a274fbb6f4ef4f42e3d3f61871f5
AdrienClairembault <aclairembault@teclib.com>
2021-04-13 09:44:21 +0200

    Revert typo

--- inc/commondbtm.class.php
+++ inc/commondbtm.class.php
@@ -4998,6 +4998,7 @@ class CommonDBTM extends CommonGLPI {
                   $options['entity'] = $_SESSION['glpiactiveentities'];
                }
                $itemtype = getItemTypeForTable($searchoptions['table']);
+
                return $itemtype::dropdown($options);
 
             case "right" :
