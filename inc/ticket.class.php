4e214225244f5bd5224351fbe502bd0d6d2f7f57
adelaunay <delaunay.alexandre@gmail.com>
2021-04-16 15:50:30 +0200

    canupdate right

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4666,6 +4666,11 @@ JAVASCRIPT;
          'ola'                => $ola,
          'glpi_has_sla'       => (count($sla->find(getEntitiesRestrictCriteria(Entity::getTable()))) > 0),
          'glpi_has_ola'       => (count($ola->find(getEntitiesRestrictCriteria(Entity::getTable()))) > 0),
+         'canupdate'          => $canupdate,
+         'can_requester'      => $can_requester,
+         'canpriority'        => $canpriority,
+         'canassign'          => $canassign,
+         'canassigntome'      => $canassigntome,
       ]);
 
       return true;
