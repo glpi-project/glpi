2840f0d09a64beaeafe91f70715f5e6e7e3673e4
SpectrumZX81 <81109422+SpectrumZX81@users.noreply.github.com>
2021-04-21 15:38:14 +0300

    'namespace' and 'use' statements removed

--- ajax/gantt.php
+++ ajax/gantt.php
@@ -30,9 +30,6 @@
  * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
  * ---------------------------------------------------------------------
  */
-namespace Glpi\Gantt;
-use \ProjectTaskLink;
-use \Exception;
 
 include ('../inc/includes.php');
 
@@ -44,7 +41,7 @@ if (isset($_REQUEST['id'])) {
 
 if (isset($_REQUEST['getData'])) {
    $itemArray = array();
-   $factory = new DataFactory();
+   $factory = new Glpi\Gantt\DataFactory();
    $factory->getItemsForProject($itemArray, $id);
    $links = $factory->getProjectTaskLinks($itemArray);
 
@@ -62,16 +59,16 @@ else if (isset($_REQUEST["updateTask"])) {
    $result;
    try {
       $updated = false;
-      $item = new Item();
+      $item = new Glpi\Gantt\Item();
       $task = $_POST["task"];
       $item->populateFrom($task);
-      $taskDAO = new TaskDAO();
+      $taskDAO = new Glpi\Gantt\TaskDAO();
       $updated = $taskDAO->updateTask($item);
       $result = (object)[
          'ok' => $updated
       ];
    }
-   catch (Exception $ex) {
+   catch (\Exception $ex) {
       $result = (object)[
          'ok' => false,
          'error' => $ex->getMessage()
@@ -84,17 +81,17 @@ else if (isset($_REQUEST["deleteTask"])) {
    try {
       $failed = array();
       $taskId = $_POST["taskId"];
-      $taskDAO = new TaskDAO();
+      $taskDAO = new Glpi\Gantt\TaskDAO();
       $taskDAO->deleteTask($failed, $taskId);
 
       if (count($failed) > 0)
-        throw new Exception("Some tasks may have not been deleted");
+        throw new \Exception("Some tasks may have not been deleted");
 
       $result = (object)[
           'ok' => true
       ];
    }
-   catch (Exception $ex) {
+   catch (\Exception $ex) {
       $result = (object)[
          'ok' => false,
          'error' => $ex->getMessage()
@@ -106,16 +103,16 @@ else if (isset($_REQUEST["updateProject"])) {
    $result;
    try {
       $updated = false;
-      $item = new Item();
+      $item = new Glpi\Gantt\Item();
       $project = $_POST["project"];
       $item->populateFrom($project);
-      $projectDAO = new ProjectDAO();
+      $projectDAO = new Glpi\Gantt\ProjectDAO();
       $updated = $projectDAO->updateProject($item);
       $result = (object)[
          'ok' => $updated
       ];
    }
-   catch (Exception $ex) {
+   catch (\Exception $ex) {
       $result = (object)[
          'ok' => false,
          'error' => $ex->getMessage()
@@ -127,13 +124,13 @@ else if (isset($_REQUEST["putInTrashbin"])) {
    $result;
    try {
       $projectId = $_POST["projectId"];
-      $projectDAO = new ProjectDAO();
+      $projectDAO = new Glpi\Gantt\ProjectDAO();
       $projectDAO->putInTrashbin($projectId);
       $result = (object)[
          'ok' => true
       ];
    }
-   catch (Exception $ex) {
+   catch (\Exception $ex) {
       $result = (object)[
          'ok' => false,
          'error' => $ex->getMessage()
@@ -144,10 +141,10 @@ else if (isset($_REQUEST["putInTrashbin"])) {
 else if (isset($_REQUEST["addTaskLink"])) {
    $result;
    try {
-      $taskLink = new ProjectTaskLink();
+      $taskLink = new \ProjectTaskLink();
 
       if ($taskLink->checkIfExist($_POST["taskLink"]))
-         throw new Exception("Link already exist!");
+         throw new \Exception("Link already exist!");
 
       $id = $taskLink->add($_POST["taskLink"]);
       $result = (object)[
@@ -155,7 +152,7 @@ else if (isset($_REQUEST["addTaskLink"])) {
          'id' => $id
       ];
    }
-   catch (Exception $ex) {
+   catch (\Exception $ex) {
       $result = (object)[
          'ok' => false,
          'error' => $ex->getMessage()
@@ -166,13 +163,13 @@ else if (isset($_REQUEST["addTaskLink"])) {
 else if (isset($_REQUEST["updateTaskLink"])) {
    $result;
    try {
-      $taskLink = new ProjectTaskLink();
+      $taskLink = new \ProjectTaskLink();
       $taskLink->update($_POST["taskLink"]);
       $result = (object)[
          'ok' => true
       ];
    }
-   catch (Exception $ex) {
+   catch (\Exception $ex) {
       $result = (object)[
          'ok' => false,
          'error' => $ex->getMessage()
@@ -183,13 +180,13 @@ else if (isset($_REQUEST["updateTaskLink"])) {
 else if (isset($_REQUEST["deleteTaskLink"])) {
    $result;
    try {
-      $taskLink = new ProjectTaskLink();
+      $taskLink = new \ProjectTaskLink();
       $taskLink->delete($_POST);
       $result = (object)[
          'ok' => true
       ];
    }
-   catch (Exception $ex) {
+   catch (\Exception $ex) {
       $result = (object)[
          'ok' => false,
          'error' => $ex->getMessage()
