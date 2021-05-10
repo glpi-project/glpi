7698dc4c3191ee620ceab181effbd36bfbec6b7d
AdrienClairembault <aclairembault@teclib.com>
2021-05-10 15:14:29 +0200

    Fix typo

--- inc/search.class.php
+++ inc/search.class.php
@@ -5610,7 +5610,7 @@ JAVASCRIPT;
                               if (Session::getCurrentInterface() == 'helpdesk'
                                  && $orig_id == 5 // -> Assigned user
                                  && !empty($anon_name = User::getAnonymizedName(
-                                    2,
+                                    $data[$ID][$k]['name'],
                                     $itemtype::getById($data['id'])->getEntityId()
                                  ))
                               ) {
