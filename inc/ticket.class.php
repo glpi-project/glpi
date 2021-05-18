0856b267df20d72614a96d657e092b9ca3225759
adelaunay <delaunay.alexandre@gmail.com>
2021-05-18 15:27:08 +0200

    fix reload of ticket form (after type/cat selection)

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -4643,8 +4643,6 @@ JAVASCRIPT;
          }
       }
 
-      $this->initForm($ID, $options);
-
       $sla = new SLA;
       $ola = new OLA;
 
@@ -4653,7 +4651,9 @@ JAVASCRIPT;
          'timeline_itemtypes' => $this->getTimelineItemtypes(),
          'params'             => $options,
          'timeline'           => $this->getTimelineItems(),
+         'itiltemplate_key'   => $tpl_key,
          'itiltemplate'       => $tt,
+         'predefined_fields'  => $predefined_fields,
          'ticket_ticket'      => new Ticket_Ticket,
          'sla'                => $sla,
          'ola'                => $ola,
