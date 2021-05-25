821433b1f4103e47f5ab4e4f5a9cce07e968ab0c
Johan Cwiklinski <johan@x-tnd.be>
2021-05-25 09:23:05 +0200

    Fix merge

--- inc/update.class.php
+++ inc/update.class.php
@@ -170,10 +170,6 @@ class Update {
       $plugin->unactivateAll();
 
       if (version_compare($current_version, '0.80', '<') || version_compare($current_version, GLPI_VERSION, '>')) {
-
-         case "9.5.5":
-            include_once "{$updir}update_955_956.php";
-            update955to956();
          $message = sprintf(
             __('Unsupported version (%1$s)'),
             $current_version
