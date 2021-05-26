4b6be72568b5cf73ca0a5870f8d856032716a4bc
Stanislas KITA <skita@teclib.com>
2021-05-26 08:43:46 +0200

    fix(tu): fix TU

--- tests/functionnal/Rule.php
+++ tests/functionnal/Rule.php
@@ -241,7 +241,7 @@ class Rule extends DbTestCase {
       $this->integer($rule->maxActionsCount())->isIdenticalTo(0);
 
       $rule = new \RuleTicket();
-      $this->integer($rule->maxActionsCount())->isIdenticalTo(31);
+      $this->integer($rule->maxActionsCount())->isIdenticalTo(32);
 
       $rule = new \RuleDictionnarySoftware();
       $this->integer($rule->maxActionsCount())->isIdenticalTo(4);
