0d5621f2bc68fd62056bc22d3bb62f55e9375c02
Cédric Anne <cedric.anne@gmail.com>
2021-06-02 15:57:11 +0200

    Fix dbversion when migration from < 9.2 fails

--- install/update_91_92.php
+++ install/update_91_92.php
@@ -1417,7 +1417,7 @@ Regards,',
    }
 
    //add db version
-   $migration->addConfig(['dbversion' => GLPI_SCHEMA_VERSION]);
+   $migration->addConfig(['dbversion' => '9.1.3']);
 
    // Add certificates management
    if (!$DB->tableExists('glpi_certificates')) {
