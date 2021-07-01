9d601f9080a7c56911283cd60df3ea6c8affc863
Johan Cwiklinski <johan@x-tnd.be>
2021-07-01 21:27:33 +0200

    Fix new node name

--- inc/inventory/inventory.class.php
+++ inc/inventory/inventory.class.php
@@ -555,7 +555,7 @@ class Inventory
             case 'cameras':
                $assettype = '\Glpi\Inventory\Asset\Camera';
                break;
-            case 'databases_instances':
+            case 'databases_services':
                $assettype = '\Glpi\Inventory\Asset\DatabaseInstance';
                break;
             default:
