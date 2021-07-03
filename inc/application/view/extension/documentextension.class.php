7e2493d42491fbdf0f7429e0e21ed429fbe0b4e2
cconard96 <cconard96@gmail.com>
2021-07-03 12:05:17 -0400

    Re-add image thumbnails on timeline

--- inc/application/view/extension/documentextension.class.php
+++ inc/application/view/extension/documentextension.class.php
@@ -49,7 +49,9 @@ class DocumentExtension extends AbstractExtension implements ExtensionInterface 
          new TwigFunction('getIconForFilename', [$this, 'getIconForFilename']),
          new TwigFunction('getSizeForFilePath', [$this, 'getSizeForFilePath']),
          new TwigFunction('getMaxUploadSize', [Document::class, 'getMaxUploadSize'], ['is_safe' => ['html']]),
-         new TwigFunction('showAvailableTypesLink', [DocumentType::class, 'showAvailableTypesLink'], ['is_safe' => ['html']])
+         new TwigFunction('showAvailableTypesLink', [DocumentType::class, 'showAvailableTypesLink'], ['is_safe' => ['html']]),
+         new TwigFunction('isImage', [$this, 'isImage'], ['is_safe' => ['html']]),
+         new TwigFunction('getImage', [$this, 'getImage'], ['is_safe' => ['html']]),
       ];
    }
 
@@ -77,5 +79,19 @@ class DocumentExtension extends AbstractExtension implements ExtensionInterface 
       }
 
       return "";
+   }
+
+   public function isImage(string $filepath, bool $is_relative = true): bool {
+      if ($is_relative) {
+         $filepath = GLPI_VAR_DIR."/".$filepath;
+      }
+      return Document::isImage($filepath);
+   }
+
+   public function getImage(string $filepath, string $context, bool $is_relative = true, $mwidth = null, $mheight = null): string {
+      if ($is_relative) {
+         $filepath = GLPI_VAR_DIR."/".$filepath;
+      }
+      return Document::getImage($filepath, $context, $mwidth, $mheight);
    }
 }
