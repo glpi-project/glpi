3c4c879d9607a30cb6e6ffc975f9e6af0a46b451
Thierry Bugier <tbugier@teclib.com>
2021-07-09 14:35:31 +0200

    fix dashboard filter
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- install/migrations/update_9.5.x_to_10.0.0/dashboard_filters.php
+++ install/migrations/update_9.5.x_to_10.0.0/dashboard_filters.php
@@ -44,9 +44,11 @@ if (!$DB->tableExists('glpi_dashboards_filters')) {
    $query = "CREATE TABLE `glpi_dashboards_filters` (
          `id` int NOT NULL AUTO_INCREMENT,
          `dashboards_dashboards_id` int NOT NULL DEFAULT '0',
+         `users_id` int NOT NULL DEFAULT '0',
          `filter` longtext,
          PRIMARY KEY (`id`),
-         KEY `dashboards_dashboards_id` (`dashboards_dashboards_id`)
+         KEY `dashboards_dashboards_id` (`dashboards_dashboards_id`),
+         KEY `users_id` (`users_id`)
       ) ENGINE = InnoDB ROW_FORMAT = DYNAMIC DEFAULT CHARSET = {$default_charset} COLLATE = {$default_collation};";
    $DB->queryOrDie($query, "10.0 add table glpi_dashboards_filters");
 }
