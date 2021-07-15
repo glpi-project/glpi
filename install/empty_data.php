eb142c8349b5a72a4a9523aa3c1443259c9ccb86
Johan Cwiklinski <johan@x-tnd.be>
2021-07-15 10:14:02 +0200

    Default display for agents list

--- install/empty_data.php
+++ install/empty_data.php
@@ -1740,6 +1740,7 @@ $ADDTODISPLAYPREF['NetworkPortType'] = [10, 11, 12];
 $ADDTODISPLAYPREF['NetworkPort'] = [3, 30, 31, 32, 33, 34, 35, 36, 38, 39, 40];
 $ADDTODISPLAYPREF['USBVendor'] = [10, 11];
 $ADDTODISPLAYPREF['PCIVendor'] = [10, 11];
+$ADDTODISPLAYPREF['Agent'] = [2, 4, 10, 8, 11, 6];
 
 foreach ($ADDTODISPLAYPREF as $type => $options) {
    $rank = 1;
