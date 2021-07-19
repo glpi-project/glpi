2b831112aa9f0bb8c06fe4d95c09fb2c70e246a8
adelaunay <delaunay.alexandre@gmail.com>
2021-07-19 15:40:21 +0200

    harmonize User::dropdown and Dropdown::show with their default value param

--- inc/user.class.php
+++ inc/user.class.php
@@ -4003,7 +4003,8 @@ JAVASCRIPT;
       }
 
       // Check default value for dropdown : need to be a numeric
-      if (strlen($p['value']) > 0 && !is_numeric($p['value']) && $p['value'] != 'myself') {
+      if ($p['value'] !== null
+          && ((strlen($p['value']) == 0) || !is_numeric($p['value']) && $p['value'] != 'myself')) {
          $p['value'] = 0;
       }
 
