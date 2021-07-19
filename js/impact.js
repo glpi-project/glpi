dbfe4de31e14174703a99d07903aba92f56d810c
adelaunay <delaunay.alexandre@gmail.com>
2021-07-19 16:33:07 +0200

    grid color of impact

--- js/impact.js
+++ js/impact.js
@@ -1578,7 +1578,7 @@ var GLPIImpact = {
          gridSpacing: 12,
          drawGrid: true,
          panGrid: true,
-         gridColor: getComputedStyle(document.documentElement).getPropertyValue('--input-border'),
+         gridColor: getComputedStyle(document.documentElement).getPropertyValue('--card-border-color'),
       });
 
       // Disable box selection as we don't need it
