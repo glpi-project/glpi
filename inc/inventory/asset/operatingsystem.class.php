3c0583551d28180f2d5e8d050b7cb9988f6b67df
Guillaume Bougard <gbougard@teclib.com>
2021-07-20 12:54:28 +0200

    Remove unused code on obsoleted properties

--- inc/inventory/asset/operatingsystem.class.php
+++ inc/inventory/asset/operatingsystem.class.php
@@ -66,20 +66,6 @@ class OperatingSystem extends InventoryAsset
          if (property_exists($this->extra_data['hardware'], 'winprodkey')) {
             $val->license_number = $this->extra_data['hardware']->winprodkey;
          }
-
-         if (property_exists($this->extra_data['hardware'], 'osname')) {
-            $val->full_name = $this->extra_data['hardware']->osname;
-         }
-
-         if (property_exists($this->extra_data['hardware'], 'osversion')) {
-            $val->version = $this->extra_data['hardware']->osversion;
-         }
-
-         if (property_exists($this->extra_data['hardware'], 'oscomments')
-                  && $this->extra_data['hardware']->oscomments != ''
-                  && !strstr($this->extra_data['hardware']->oscomments, 'UTC')) {
-            $val->service_pack = $this->extra_data['hardware']->oscomments;
-         }
       }
 
       if (property_exists($val, 'full_name')) {
