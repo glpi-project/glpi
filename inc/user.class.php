8f29200bb5ca26d43733a0e3cabfb31c8176ce5e
tomolimo <olivier.moron@araymond.com>
2021-08-25 14:11:04 +0200

    To create correct LDAP filter from user_dn
    ldap_escape() will escape the chars that must be escaped from the dn to be used in a LDAP filter.
    Otherwise some chars are missing, and some are escaped when not mandatory.

--- inc/user.class.php
+++ inc/user.class.php
@@ -1780,8 +1780,7 @@ class User extends CommonDBTM {
       $listgroups = [];
 
       //User dn may contain ( or ), need to espace it!
-      $user_dn = str_replace(["(", ")", "\,", "\+"], ["\(", "\)", "\\\,", "\\\+"],
-                             $user_dn);
+      $user_dn = ldap_escape($user_dn, "", LDAP_ESCAPE_FILTER);
 
       //Only retrive cn and member attributes from groups
       $attrs = ['dn'];
