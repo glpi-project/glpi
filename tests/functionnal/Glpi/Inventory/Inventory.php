25356d3177f76ffb5ffdfb9f02221824c9cbe9cd
Stanislas KITA <skita@teclib.com>
2021-09-06 09:49:41 +0200

    fix TU from rebase step

--- tests/functionnal/Glpi/Inventory/Inventory.php
+++ tests/functionnal/Glpi/Inventory/Inventory.php
@@ -1796,16 +1796,14 @@ class Inventory extends DbTestCase {
          'OFFSET' => $nblogsnow,
       ]);
 
-      $this->integer(count($logs))->isIdenticalTo(42);
+      $this->integer(count($logs))->isIdenticalTo(126);
 
       $expected_types_count = [
          \Log::HISTORY_DELETE_SUBITEM => 5,//networkport and networkname
          \Log::HISTORY_ADD_DEVICE => 41,
          \Log::HISTORY_CREATE_ITEM => 57, //virtual machines, os, manufacturer, net ports, net names, ...
          0 => 6, //Agent version, disks usage
-         \Log::HISTORY_CREATE_ITEM => 15, //virtual machines, os, manufacturer, net ports, net names, ...
-         \Log::HISTORY_DELETE_SUBITEM => 4,//net<orkport and networkname
-         \Log::HISTORY_ADD_SUBITEM => 9,//network port/name, ip adrress, VMs
+         \Log::HISTORY_ADD_SUBITEM => 10,//network port/name, ip adrress, VMs
          \Log::HISTORY_UPDATE_SUBITEM => 4,//disks usage
          \Log::HISTORY_DEL_RELATION => 2,//monitor-computer relation
          \Log::HISTORY_UPDATE_RELATION => 2,//kernel version
