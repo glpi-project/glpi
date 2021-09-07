f7f61e92a3e5edcf120ea275cdb9a5f1e49dfb56
adelaunay <delaunay.alexandre@gmail.com>
2021-09-07 16:11:45 +0200

    apply suggestions

--- inc/html.class.php
+++ inc/html.class.php
@@ -1860,7 +1860,6 @@ HTML;
          'is_debug_active'       => $_SESSION['glpi_use_mode'] == Session::DEBUG_MODE,
          'is_impersonate_active' => Session::isImpersonateActive(),
          'founded_new_version'   => $founded_new_version,
-         'language_name'         => User::showSwitchLangForm(),
          'user'                  => $user instanceof User ? $user : null,
       ];
    }
