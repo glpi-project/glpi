b6759a53fe3babefe275b2bdc57b72618e4acec4
Stanislas KITA <skita@teclib.com>
2021-09-13 09:52:42 +0200

    fix JS CS

--- js/cable.js
+++ js/cable.js
@@ -30,52 +30,52 @@
  */
 
 function refreshAssetBreadcrumb(itemtype, items_id, dom_to_update) {
-    // get asset breadcrumb
-    $.ajax({
-        method: 'GET',
-        url: CFG_GLPI.root_doc + "/ajax/cable.php",
-        data: {
-            action: 'get_item_breadcrum',
-            items_id: items_id,
-            itemtype: itemtype,
-        }
-    }).success(function(html_breadcrum) {
-        $('#' + dom_to_update).empty();
-        $('#' + dom_to_update).append(html_breadcrum);
-    });
+   // get asset breadcrumb
+   $.ajax({
+      method: 'GET',
+      url: CFG_GLPI.root_doc + "/ajax/cable.php",
+      data: {
+         action: 'get_item_breadcrum',
+         items_id: items_id,
+         itemtype: itemtype,
+      }
+   }).success(function(html_breadcrum) {
+      $('#' + dom_to_update).empty();
+      $('#' + dom_to_update).append(html_breadcrum);
+   });
 
 }
 
 function refreshNetworkPortDropdown(itemtype, items_id, dom_to_update) {
-    // get networkport dropdown
-    $.ajax({
-        method: 'GET',
-        url: CFG_GLPI.root_doc + "/ajax/cable.php",
-        data: {
-            action: 'get_networkport_dropdown',
-            items_id: items_id,
-            itemtype: itemtype,
-        }
-    }).success(function(html_data) {
-        $('#' + dom_to_update).empty();
-        $('#' + dom_to_update).append(html_data);
-    });
+   // get networkport dropdown
+   $.ajax({
+      method: 'GET',
+      url: CFG_GLPI.root_doc + "/ajax/cable.php",
+      data: {
+         action: 'get_networkport_dropdown',
+         items_id: items_id,
+         itemtype: itemtype,
+      }
+   }).success(function(html_data) {
+      $('#' + dom_to_update).empty();
+      $('#' + dom_to_update).append(html_data);
+   });
 }
 
 function refreshSocketDropdown(itemtype, items_id, socketmodels_id, dom_name, dom_to_update) {
-    // get networkport dropdown
-    $.ajax({
-        method: 'GET',
-        url: CFG_GLPI.root_doc + "/ajax/cable.php",
-        data: {
-            action: 'get_socket_dropdown',
-            items_id: items_id,
-            itemtype: itemtype,
-            socketmodels_id: socketmodels_id,
-            dom_name: dom_name
-        }
-    }).success(function(html_data) {
-        $('#' + dom_to_update).empty();
-        $('#' + dom_to_update).append(html_data);
-    });
+   // get networkport dropdown
+   $.ajax({
+      method: 'GET',
+      url: CFG_GLPI.root_doc + "/ajax/cable.php",
+      data: {
+         action: 'get_socket_dropdown',
+         items_id: items_id,
+         itemtype: itemtype,
+         socketmodels_id: socketmodels_id,
+         dom_name: dom_name
+      }
+   }).success(function(html_data) {
+      $('#' + dom_to_update).empty();
+      $('#' + dom_to_update).append(html_data);
+   });
 }
