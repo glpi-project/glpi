52e7fa1d75c3f1c43ee6617a3af352b94ad00a36
Rom1-B <rom1.biot@gmail.com>
2021-09-27 10:27:59 +0200

    optimize

--- front/change.form.php
+++ front/change.form.php
@@ -140,18 +140,8 @@ if (isset($_POST["add"])) {
    $change->display($_REQUEST);
 
    if (isset($_GET['id']) && ($_GET['id'] > 0)) {
-      $handled = false;
-      $url = KnowbaseItem::getFormURL() . '?_in_modal=1&item_itemtype=Change&item_items_id=' . $_GET['id'];
-      if (isset($_GET['_sol_to_kb'])) {
-         $handled = true;
-      } else if (isset($_GET['_fw_to_kb'])) {
-         $url .= '&fw_id=' . $_GET['_fw_to_kb'];
-         $handled = true;
-      } else if (isset($_GET['_task_to_kb'])) {
-         $url .= '&task_id=' . $_GET['_task_to_kb'];
-         $handled = true;
-      }
-      if ($handled) {
+      $url = KnowbaseItem::getFormURLWithParam($_GET) . '&_in_modal=1&item_itemtype=Ticket&item_items_id=' . $_GET['id'];
+      if (strpos($url, '_to_kb=') !== false) {
          Ajax::createIframeModalWindow(
             'savetokb',
             $url,
