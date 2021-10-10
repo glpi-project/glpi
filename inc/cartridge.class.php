54d89b43f6b837878a55d5878aa871828cb8acd6
cconard96 <cconard96@gmail.com>
2021-10-10 14:52:54 -0400

    Fix wrong cartridge delete action

--- inc/cartridge.class.php
+++ inc/cartridge.class.php
@@ -666,7 +666,7 @@ class Cartridge extends CommonDBChild {
       if ($canedit && $number) {
          $rand = mt_rand();
          Html::openMassiveActionsForm('mass'.__CLASS__.$rand);
-         $actions = ['delete' => _x('button', 'Delete permanently'),
+         $actions = ['purge' => _x('button', 'Delete permanently'),
                      'Infocom'.MassiveAction::CLASS_ACTION_SEPARATOR.'activate'
                               => __('Enable the financial and administrative information')
                           ];
