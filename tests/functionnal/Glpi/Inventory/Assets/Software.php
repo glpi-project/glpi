a5e7c149c3c7f920d457405acf9c3638439142fa
Johan Cwiklinski <johan@x-tnd.be>
2021-10-15 10:28:27 +0200

    Fix operating sytem extra in software
    Add tests on softwareversion with os

--- tests/functionnal/Glpi/Inventory/Assets/Software.php
+++ tests/functionnal/Glpi/Inventory/Assets/Software.php
@@ -61,6 +61,38 @@ class Software extends AbstractInventoryAsset {
   <QUERY>INVENTORY</QUERY>
   </REQUEST>",
             'expected'  => '{"arch": "x86_64", "comments": "GNU Image Manipulation Program", "filesize": 67382735, "from": "rpm", "name": "gimp", "publisher": "Fedora Project", "system_category": "Unspecified", "version": "2.8.22-7.fc28", "install_date": "2018-09-03", "manufacturers_id": "Feodra Project", "comment": "GNU Image Manipulation Program", "_system_category": "Unspecified", "operatingsystems_id": 0, "entities_id": 0, "is_template_item": 0, "is_deleted_item": 0, "is_recursive": 0, "date_install": "2018-09-03"}'
+         ],
+         [
+            'xml' => "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<REQUEST>
+  <CONTENT>
+    <OPERATINGSYSTEM>
+      <ARCH>x86_64</ARCH>
+      <BOOT_TIME>2017-02-20 08:11:53</BOOT_TIME>
+      <FULL_NAME>Fedora release 25 (Twenty Five)</FULL_NAME>
+      <HOSTID>007f0100</HOSTID>
+      <KERNEL_NAME>linux</KERNEL_NAME>
+      <KERNEL_VERSION>4.9.9-200.fc25.x86_64</KERNEL_VERSION>
+      <NAME>Fedora</NAME>
+      <VERSION>25</VERSION>
+    </OPERATINGSYSTEM>
+    <SOFTWARES>
+      <ARCH>x86_64</ARCH>
+      <COMMENTS>GNU Image Manipulation Program</COMMENTS>
+      <FILESIZE>67382735</FILESIZE>
+      <FROM>rpm</FROM>
+      <INSTALLDATE>03/09/2018</INSTALLDATE>
+      <NAME>gimp</NAME>
+      <PUBLISHER>Fedora Project</PUBLISHER>
+      <SYSTEM_CATEGORY>Unspecified</SYSTEM_CATEGORY>
+      <VERSION>2.8.22-7.fc28</VERSION>
+    </SOFTWARES>
+    <VERSIONCLIENT>FusionInventory-Inventory_v2.4.1-2.fc28</VERSIONCLIENT>
+  </CONTENT>
+  <DEVICEID>glpixps.teclib.infra-2018-10-03-08-42-36</DEVICEID>
+  <QUERY>INVENTORY</QUERY>
+  </REQUEST>",
+            'expected'  => '{"arch": "x86_64", "comments": "GNU Image Manipulation Program", "filesize": 67382735, "from": "rpm", "name": "gimp", "publisher": "Fedora Project", "system_category": "Unspecified", "version": "2.8.22-7.fc28", "install_date": "2018-09-03", "manufacturers_id": "Feodra Project", "comment": "GNU Image Manipulation Program", "_system_category": "Unspecified", "operatingsystems_id": 0, "entities_id": 0, "is_template_item": 0, "is_deleted_item": 0, "is_recursive": 0, "date_install": "2018-09-03"}'
          ]
       ];
    }
@@ -116,5 +148,47 @@ class Software extends AbstractInventoryAsset {
       $asset->handle();
       $this->boolean($sov->getFromDbByCrit(['items_id' => $computer->fields['id'], 'itemtype' => 'Computer']))
            ->isTrue('A software version has not been linked to computer!');
+
+      $version = new \SoftwareVersion();
+      $this->boolean($version->getFromDB($sov->fields['softwareversions_id']))->isTrue();
+      $this->integer($version->fields['operatingsystems_id'])->isIdenticalTo(0);
+
+      //convert data
+      $expected = $this->assetProvider()[1];
+
+      $converter = new \Glpi\Inventory\Converter;
+      $data = $converter->convert($expected['xml']);
+      $json = json_decode($data);
+
+      $computer = getItemByTypeName('Computer', '_test_pc01');
+      $asset = new \Glpi\Inventory\Asset\Software($computer, $json->content->softwares);
+      $osasset = new \Glpi\Inventory\Asset\OperatingSystem($computer, (array)$json->content->operatingsystem);
+      $osasset->prepare();
+      //handle
+      $osasset->handleLinks();
+      $osasset->handle();
+
+      $extra_data = (array)$json->content;
+      $extra_data[\Glpi\Inventory\Asset\OperatingSystem::class] = $osasset;
+
+      $asset->setExtraData($extra_data);
+      $result = $asset->prepare();
+      $expected = json_decode($expected['expected']);
+      $manu = new \Manufacturer();
+      $this->boolean($manu->getFromDbByCrit(['name' => $result[0]->publisher]))->isTrue();
+      $expected->manufacturers_id = $manu->fields['id'];
+      $this->object($result[0])->isEqualTo($expected);
+
+      //handle
+      $asset->handleLinks();
+      $asset->handle();
+      $this->boolean($sov->getFromDbByCrit(['items_id' => $computer->fields['id'], 'itemtype' => 'Computer']))
+         ->isTrue('A software version has not been linked to computer!');
+
+      $this->integer($sov->fields['softwareversions_id'])->isNotEqualTo($version->fields['id']);
+
+      $version = new \SoftwareVersion();
+      $this->boolean($version->getFromDB($sov->fields['softwareversions_id']))->isTrue();
+      $this->integer($version->fields['operatingsystems_id'])->isGreaterThan(0);
    }
 }
