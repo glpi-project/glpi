51c869d0a82308fea8f9b038c908052055b0a757
Cédric Anne <cedric.anne@gmail.com>
2021-10-25 12:10:12 +0200

    Skip platform requirements checks only on non stable PHP versions

--- .github/actions/init_build.sh
+++ .github/actions/init_build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -e
 
-PHP_MAJOR_VERSION="$(echo $PHP_VERSION | cut -d '.' -f 1,2)"
+# Stable versions contains only 3 groups of digits separated by a dot,
+# i.e. no "dev", "alpha", "beta, "rc", ... keyword.
+STABLE_REGEX="^[0-9]+\.[0-9]+\.[0-9]+$"
 
 # Validate composer config
 composer validate --strict
-if ! [[ "$PHP_MAJOR_VERSION" == "8.0" ]]; then
+if [[ "$PHP_VERSION" =~ $STABLE_REGEX ]]; then
   composer check-platform-reqs;
 fi
 
 # Install dependencies
-if [[ "$PHP_MAJOR_VERSION" == "8.0" ]]; then
+if ! [[ "$PHP_VERSION" =~ $STABLE_REGEX ]]; then
   COMPOSER_ADD_OPTS=--ignore-platform-reqs;
 fi
 bin/console dependencies install --composer-options="$COMPOSER_ADD_OPTS --prefer-dist --no-progress"
