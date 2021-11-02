3f49a4c874a6c684c1efe745db420015b72310c6
Cédric Anne <cedric.anne@gmail.com>
2021-11-02 08:19:16 +0100

    Update tests/functionnal/NotificationSettingConfig.php
    Co-authored-by: Johan Cwiklinski <trasher@x-tnd.be>

--- tests/functionnal/NotificationSettingConfig.php
+++ tests/functionnal/NotificationSettingConfig.php
@@ -84,7 +84,7 @@ class NotificationSettingConfig extends DbTestCase {
       $options = ['display' => false];
 
       $output = $settingconfig->showConfigForm($options);
-      $this->string($output)->matches("/^\s*$/"); // Only whitespaces, no real content
+      $this->string(trim($output))->isEmpty(); // Only whitespaces, no real content
 
       $this->login();
 
