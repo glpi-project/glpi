6e8f713ba5769c5c0a143372f264c041bc7db702
Thierry Bugier <tbugier@teclib.com>
2021-11-09 15:41:18 +0100

    simplify code
    Signed-off-by: Thierry Bugier <tbugier@teclib.com>

--- ajax/updatecurrenttab.php
+++ ajax/updatecurrenttab.php
@@ -40,15 +40,12 @@ if (!basename($_SERVER['SCRIPT_NAME']) == "helpdesk.faq.php") {
 if (isset($_GET['tab']) && isset($_GET['itemtype'])) {
 
    $tabs = Toolbox::getAvailablesTabs($_UGET['itemtype'], $_GET['id'] ?? null);
-   $selected_tab = '';
    $current      = 0;
    foreach (array_keys($tabs) as $key) {
       if ($current == $_GET['tab']) {
-         $selected_tab = $key;
+         Session::setActiveTab($_UGET['itemtype'], $key);
+         break;
       }
       $current++;
-   }
-   if (!empty($selected_tab)) {
-      Session::setActiveTab($_UGET['itemtype'], $selected_tab);
    }
 }
