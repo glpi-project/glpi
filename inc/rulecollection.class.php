91f737b2c57ecfa6d22409af10234bb93d34de7e
Cédric Anne <cedric.anne@gmail.com>
2021-11-22 08:47:45 +0100

    Fix rule order change query; fixes #9888

--- inc/rulecollection.class.php
+++ inc/rulecollection.class.php
@@ -804,7 +804,7 @@ class RuleCollection extends CommonDBTM {
             'FROM'   => 'glpi_rules',
             'WHERE'  => [
                'sub_type'  => $this->getRuleClassName(),
-               ['ranking'  => ['=>', $rank]],
+               ['ranking'  => ['>=', $rank]],
                ['ranking'  => ['<', $old_rank]]
             ]
          ]);
