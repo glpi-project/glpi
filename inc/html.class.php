5f83257989da33e841abdeb17aacc7f15dc8bfaf
Cédric Anne <cedric.anne@gmail.com>
2021-12-08 11:41:28 +0100

    Fix undefined variable, log error

--- inc/html.class.php
+++ inc/html.class.php
@@ -6581,6 +6581,7 @@ HTML;
          }
       );
 
+      $css = '';
       if (!isset($args['reload']) && !isset($args['nocache']) && $GLPI_CACHE->has($ckey)) {
          $css = $GLPI_CACHE->get($ckey);
       } else {
@@ -6593,6 +6594,7 @@ HTML;
                $GLPI_CACHE->set($fckey, $file_hash);
             }
          } catch (\Throwable $e) {
+            ErrorHandler::getInstance()->handleException($e, true);
             if (isset($args['debug'])) {
                $msg = 'An error occured during SCSS compilation: ' . $e->getMessage();
                $msg = str_replace(["\n", "\"", "'"], ['\00000a', '\0022', '\0027'], $msg);
