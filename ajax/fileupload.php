056939bc07db324e8287c0787dd4c6a982674b7d
Cédric Anne <cedric.anne@gmail.com>
2021-12-10 10:06:02 +0100

    Prevent deprecations to break file upload JSON response

--- ajax/fileupload.php
+++ ajax/fileupload.php
@@ -34,7 +34,13 @@
  * @since 9.2
  */
 
+use Glpi\Application\ErrorHandler;
+
 include ('../inc/includes.php');
 
 Session::checkLoginUser();
+
+// Ensure warnings will not break ajax output.
+ErrorHandler::getInstance()->disableOutput();
+
 GLPIUploadHandler::uploadFiles($_POST);
