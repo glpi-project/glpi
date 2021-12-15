80ec9fc47b2a823a4a8d3d4b529aeb4619dccff4
AdrienClairembault <aclairembault@teclib.com>
2021-12-15 11:40:04 +0100

    Fix headers for new items

--- src/Ajax.php
+++ src/Ajax.php
@@ -321,10 +321,18 @@ JAVASCRIPT;
             $html_tabs = "";
             $html_sele = "";
             $i = 0;
+
+            // Hide tabs on item creation
+            $display_class = "";
+            if (is_a($type, CommonDBTM::class, true) && $type::isNewId($ID)) {
+                $display_class = "d-none";
+            }
+
             foreach ($tabs as $val) {
+
                 $target = str_replace('\\', '_', $val['id']);
                 $html_tabs .= "<li class='nav-item $navitemml'>
-               <a class='nav-link justify-content-between $navlinkp' data-bs-toggle='tab' title='" . strip_tags($val['title']) . "' ";
+               <a class='nav-link justify-content-between $navlinkp $display_class' data-bs-toggle='tab' title='" . strip_tags($val['title']) . "' ";
                 $html_tabs .= " href='" . $val['url'] . (isset($val['params']) ? '?' . $val['params'] : '') . "' data-bs-target='#{$target}'>";
                 $html_tabs .= $val['title'] . "</a></li>";
 
