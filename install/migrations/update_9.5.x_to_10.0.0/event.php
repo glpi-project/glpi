ce17703e3809d5318f8ef5b2a5dcda122e7617f3
Cédric Anne <cedric.anne@gmail.com>
2022-01-05 13:21:10 +0100

    Replicate 9.5.7 migrations in 10.0.0 migration

---
+++ install/migrations/update_9.5.x_to_10.0.0/event.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2015-2021 Teclib' and contributors.
+ *
+ * http://glpi-project.org
+ *
+ * based on GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2003-2014 by the INDEPNET Development Team.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * GLPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GLPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+    die("Sorry. You can't access this file directly");
+}
+
+/**
+ * @var DB $DB
+ * @var Migration $migration
+ */
+
+/** Replace -1 values for glpi_events.items_id field */
+// Migration may have been missed if user installed 10.x version before 9.5.7 release date.
+$migration->addPostQuery(
+    $DB->buildUpdate(
+        'glpi_events',
+        ['items_id' => '0'],
+        ['items_id' => '-1', 'type' => 'system']
+    )
+);
+/** /Replace -1 values for glpi_events.items_id field */
---
+++ install/migrations/update_9.5.x_to_10.0.0/item_operatingsystem.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2015-2021 Teclib' and contributors.
+ *
+ * http://glpi-project.org
+ *
+ * based on GLPI - Gestionnaire Libre de Parc Informatique
+ * Copyright (C) 2003-2014 by the INDEPNET Development Team.
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * GLPI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GLPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+ * ---------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+    die("Sorry. You can't access this file directly");
+}
+
+/**
+ * @var DB $DB
+ * @var Migration $migration
+ */
+
+/** Replace -1 values for glpi_items_operatingsystems table foreign key fields */
+// Migration may have been missed if user installed 10.x version before 9.5.7 release date.
+foreach (['operatingsystems_id', 'operatingsystemversions_id', 'operatingsystemservicepacks_id'] as $item_os_fkey) {
+    $migration->addPostQuery(
+        $DB->buildUpdate(
+            'glpi_items_operatingsystems',
+            [$item_os_fkey => '0'],
+            [$item_os_fkey => '-1']
+        )
+    );
+}
+/** /Replace -1 values for glpi_items_operatingsystems table foreign key fields */
