0d3519e88eef14cc2c0c4b1074c2aeab8248f468
Guillaume Bougard <gbougard@teclib.com>
2022-01-05 11:55:32 +0100

    Revert not setting error on unsupported action query

--- src/Inventory/Request.php
+++ src/Inventory/Request.php
@@ -97,6 +97,7 @@ class Request extends AbstractRequest
             case self::DEPLOY_ACTION:
             case self::WOL_ACTION:
             default:
+                $this->addError("Query '$query' is not supported.", 400);
                 return false;
         }
         return true;
