c2c8cd24fe6da34ef2c56da3749e1d9833c674b6
Curtis Conard <cconard96@gmail.com>
2022-01-17 03:33:11 -0500

    Update index.php
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- index.php
+++ index.php
@@ -33,9 +33,6 @@
 
 // Check PHP version not to have trouble
 // Need to be the very fist step before any include
-if (version_compare(PHP_VERSION, '7.4.0') < 0) {
-    die('PHP >= 7.4.0 required');
-}
 if (
     version_compare(PHP_VERSION, '7.4.0', '<') ||
     version_compare(PHP_VERSION, '8.2.0', '>=')
