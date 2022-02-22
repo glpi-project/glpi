068170d53928b63b6dd4354b9b1d4e706706132c
Curtis Conard <cconard96@gmail.com>
2022-02-21 19:09:44 -0500

    Fix visible for problem dropdown visibility
    fixes #10484

--- inc/itilcategory.class.php
+++ inc/itilcategory.class.php
@@ -47,73 +47,103 @@ class ITILCategory extends CommonTreeDropdown {
 
    function getAdditionalFields() {
 
-      $tab = [['name'      => $this->getForeignKeyField(),
-                         'label'     => __('As child of'),
-                         'type'      => 'parent',
-                         'list'      => false],
-                   ['name'      => 'users_id',
-                         'label'     => __('Technician in charge of the hardware'),
-                         'type'      => 'UserDropdown',
-                         'right'     => 'own_ticket',
-                         'list'      => true],
-                   ['name'      => 'groups_id',
-                         'label'     => __('Group in charge of the hardware'),
-                         'type'      => 'dropdownValue',
-                         'condition' => ['is_assign' => 1],
-                         'list'      => true],
-                   ['name'      => 'knowbaseitemcategories_id',
-                         'label'     => __('Knowledge base'),
-                         'type'      => 'dropdownValue',
-                         'list'      => true],
-                   ['name'      => 'code',
-                         'label'     => __('Code representing the ticket category'),
-                         'type'      => 'text',
-                         'list'      => false],
-                   ['name'      => 'is_helpdeskvisible',
-                         'label'     => __('Visible in the simplified interface'),
-                         'type'      => 'bool',
-                         'list'      => true],
-                   ['name'      => 'is_incident',
-                         'label'     => __('Visible for an incident'),
-                         'type'      => 'bool',
-                         'list'      => true],
-                   ['name'      => 'is_request',
-                         'label'     => __('Visible for a request'),
-                         'type'      => 'bool',
-                         'list'      => true],
-                   ['name'      => 'is_problem',
-                         'label'     => __('Visible for a problem'),
-                         'type'      => 'bool',
-                         'list'      => true],
-                   ['name'      => 'is_change',
-                         'label'     => __('Visible for a change'),
-                         'type'      => 'bool',
-                         'list'      => true],
-                   ['name'      => 'tickettemplates_id_demand',
-                         'label'     => __('Template for a request'),
-                         'type'      => 'dropdownValue',
-                         'list'      => true],
-                   ['name'      => 'tickettemplates_id_incident',
-                         'label'     => __('Template for an incident'),
-                         'type'      => 'dropdownValue',
-                         'list'      => true],
-                   ['name'      => 'changetemplates_id',
-                         'label'     => __('Template for a change'),
-                         'type'      => 'dropdownValue',
-                         'list'      => true],
-                   ['name'      => 'problemtemplates_id',
-                         'label'     => __('Template for a problem'),
-                         'type'      => 'dropdownValue',
-                         'list'      => true],
-                  ];
+      $tab = [
+         [
+            'name' => $this->getForeignKeyField(),
+            'label' => __('As child of'),
+            'type' => 'parent',
+            'list' => false
+         ],
+         [
+            'name' => 'users_id',
+            'label' => __('Technician in charge of the hardware'),
+            'type' => 'UserDropdown',
+            'right' => 'own_ticket',
+            'list' => true
+         ],
+         [
+            'name' => 'groups_id',
+            'label' => __('Group in charge of the hardware'),
+            'type' => 'dropdownValue',
+            'condition' => ['is_assign' => 1],
+            'list' => true
+         ],
+         [
+            'name' => 'knowbaseitemcategories_id',
+            'label' => __('Knowledge base'),
+            'type' => 'dropdownValue',
+            'list' => true
+         ],
+         [
+            'name' => 'code',
+            'label' => __('Code representing the ticket category'),
+            'type' => 'text',
+            'list' => false
+         ],
+         [
+            'name' => 'is_helpdeskvisible',
+            'label' => __('Visible in the simplified interface'),
+            'type' => 'bool',
+            'list' => true
+         ],
+         [
+            'name' => 'is_incident',
+            'label' => __('Visible for an incident'),
+            'type' => 'bool',
+            'list' => true
+         ],
+         [
+            'name' => 'is_request',
+            'label' => __('Visible for a request'),
+            'type' => 'bool',
+            'list' => true
+         ],
+      ];
 
-      if (!Session::haveRightsOr('problem', [CREATE, UPDATE, DELETE,
-                                                  Problem::READALL, Problem::READMY])) {
+      $show_for_problem = Session::haveRightsOr('problem', [CREATE, UPDATE, DELETE, Problem::READALL, Problem::READMY]);
 
-         unset($tab[7]);
+      if ($show_for_problem) {
+         $tab[] = [
+            'name' => 'is_problem',
+            'label' => __('Visible for a problem'),
+            'type' => 'bool',
+            'list' => true
+         ];
       }
-      return $tab;
 
+      $tab = array_merge($tab, [
+         [
+            'name' => 'is_change',
+            'label' => __('Visible for a change'),
+            'type' => 'bool',
+            'list' => true
+         ],
+         [
+            'name' => 'tickettemplates_id_demand',
+            'label' => __('Template for a request'),
+            'type' => 'dropdownValue',
+            'list' => true
+         ],
+         [
+            'name' => 'tickettemplates_id_incident',
+            'label' => __('Template for an incident'),
+            'type' => 'dropdownValue',
+            'list' => true
+         ],
+         [
+            'name' => 'changetemplates_id',
+            'label' => __('Template for a change'),
+            'type' => 'dropdownValue',
+            'list' => true
+         ],
+         [
+            'name' => 'problemtemplates_id',
+            'label' => __('Template for a problem'),
+            'type' => 'dropdownValue',
+            'list' => true
+         ],
+      ]);
+      return $tab;
    }
 
 
