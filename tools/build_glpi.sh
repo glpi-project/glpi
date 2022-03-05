39b8a0ca00fa43c575c98096ac47371fb0c02891
tomamplius <thomas@lgy.fr>
2022-03-05 18:52:39 +0100

    Remove interaction from composer during GLPI build

--- tools/build_glpi.sh
+++ tools/build_glpi.sh
@@ -62,7 +62,7 @@ $WORKING_DIR/bin/console build:compile_scss
 
 echo "Remove dev files and directories"
 # Remove PHP dev dependencies that are not anymore used
-composer update nothing --ignore-platform-reqs --no-dev --no-scripts --working-dir=$WORKING_DIR
+composer update nothing --ansi --no-interaction --ignore-platform-reqs --no-dev --no-scripts --working-dir=$WORKING_DIR
 
 # Remove user generated files (i.e. cache and log from CLI commands ran during release)
 find $WORKING_DIR/files -depth -mindepth 2 ! -iname "remove.txt" -exec rm -rf {} \;
