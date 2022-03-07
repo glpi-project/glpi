7e95b2c75479e3c4585f85880f4c4e402ff0decd
ITSM Dev Team <devteam@itsm-ng.org>
2022-03-07 16:32:57 +0000

    Remove github actions for later rework

--- .github/actions/lint_js-lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-echo "Check for coding standards violations"
-node_modules/.bin/eslint . && echo "ESLint found no errors"
--- .github/actions/teardown_containers-cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-echo "Cleanup containers and volumes"
-docker-compose down --volumes
