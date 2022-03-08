636b72b23fa768c691b2f0bf3d8cc66c1a4e44ec
Stanislas KITA <skita@teclib.com>
2022-03-08 16:47:01 +0100

    add missing SO

--- src/Database.php
+++ src/Database.php
@@ -261,6 +261,14 @@ class Database extends CommonDBChild
             ]
         ];
 
+        $tab[] = [
+            'id'                 => '13',
+            'table'              => $this->getTable(),
+            'field'              => 'is_dynamic',
+            'name'               => __('Dynamic'),
+            'datatype'           => 'bool'
+        ];
+
         return $tab;
     }
 
@@ -341,6 +349,21 @@ class Database extends CommonDBChild
             'joinparams'         => [
                 'jointype'           => 'child'
             ]
+        ];
+
+        $tab[] = [
+            'id'                 => '173',
+            'table'              => self::getTable(),
+            'field'              => 'is_dynamic',
+            'linkfield'          => '',
+            'name'               => __('Dynamic'),
+            'datatype'           => 'bool',
+            'joinparams'         => [
+                'jointype'           => 'child'
+            ],
+            'massiveaction'      => false,
+            'forcegroupby'       => true,
+            'searchtype'         => ['equals']
         ];
 
         return $tab;
@@ -430,6 +453,7 @@ class Database extends CommonDBChild
             $header .= "<th>" . sprintf(__('%1$s (%2$s)'), __('Size'), __('Mio')) . "</th>";
             $header .= "<th>" . __('Is active') . "</th>";
             $header .= "<th>" . __('Has backup') . "</th>";
+            $header .= "<th>" . __('Is dynamic') . "</th>";
             $header .= "</tr>";
             echo $header;
 
@@ -441,6 +465,7 @@ class Database extends CommonDBChild
                 echo "<td>" . $row['size'] . "</td>";
                 echo "<td>" . Dropdown::getYesNo($db->fields['is_active']) . "</td>";
                 echo "<td>" . Dropdown::getYesNo($db->fields['is_onbackup']) . "</td>";
+                echo "<td>" . Dropdown::getYesNo($db->fields['is_dynamic']) . "</td>";
                 echo "</tr>";
                 Session::addToNavigateListItems('DatabaseInstance', $row['id']);
             }
