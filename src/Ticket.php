70a5145de57dab4190f68d6e8e81411732d69d61
Alexandre Delaunay <delaunay.alexandre@gamail.com>
2022-03-09 16:18:38 +0100

    check for static update right on ticket controller

--- src/Ticket.php
+++ src/Ticket.php
@@ -536,7 +536,7 @@ class Ticket extends CommonITILObject
        // if we don't have global UPDATE right, maybe we can own the current ticket
         if (
             !Session::haveRight(self::$rightname, UPDATE)
-            && (!$this->ownItem() && !$this->canAssign())
+            && (!$this->ownItem())
         ) {
            //we always return false, as ownItem() = true is managed by below self::canUpdate
             return false;
