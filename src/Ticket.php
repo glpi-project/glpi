91fefbb1c6a25616773a5dad37a18ee696e1a742
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2022-03-15 15:57:25 +0100

    remove autoassign (set_default_tech) from Ticket::prepareInputForAdd as it's already set in form

--- src/Ticket.php
+++ src/Ticket.php
@@ -1800,18 +1800,6 @@ class Ticket extends CommonITILObject
             }
         }
 
-       // fill auto-assign when no tech defined (only for tech)
-        if (
-            !isset($input['_auto_import'])
-            && (!isset($input['_skip_auto_assign']) || $input['_skip_auto_assign'] === false)
-            && isset($_SESSION['glpiset_default_tech']) && $_SESSION['glpiset_default_tech']
-            && Session::getCurrentInterface() == 'central'
-            && (!isset($input['_users_id_assign']) || $input['_users_id_assign'] == 0)
-            && Session::haveRight("ticket", Ticket::OWN)
-        ) {
-            $input['_users_id_assign'] = Session::getLoginUserID();
-        }
-
         $cat_id = $input['itilcategories_id'] ?? 0;
         if ($cat_id) {
             $input['itilcategories_id_code'] = ITILCategory::getById($cat_id)->fields['code'];
