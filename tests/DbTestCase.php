313dccdf90b75a3650dd801c4b53480d9f13b5c4
Cédric Anne <cedric.anne@gmail.com>
2022-04-07 14:31:13 +0200

    Rely on Sanitizer::dbUnescapeRecursive() in tests

--- tests/DbTestCase.php
+++ tests/DbTestCase.php
@@ -115,7 +115,7 @@ class DbTestCase extends \GLPITestCase
      */
     protected function checkInput(CommonDBTM $object, $id = 0, $input = [])
     {
-        $input = \Toolbox::stripslashes_deep($input); // slashes in input should not be stored in DB
+        $input = Sanitizer::dbUnescapeRecursive($input); // slashes in input should not be stored in DB
 
         $this->integer((int)$id)->isGreaterThan(0);
         $this->boolean($object->getFromDB($id))->isTrue();
