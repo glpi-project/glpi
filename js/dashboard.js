3a5f9ac7fa794f6bce41eddd503d1389b2ce5909
Jordan B <j0rd4n68@gmail.com>
2022-04-19 15:41:39 +0200

    fix(dashboard): code style

--- js/dashboard.js
+++ js/dashboard.js
@@ -804,7 +804,7 @@ var Dashboard = {
                 var precision    = count.data('precision');
                 var number       = count.children('.number');
                 var suffix       = count.children('.suffix').text();
-				var targetNumber = number.text();
+                var targetNumber = number.text();
 
                 // Some custom formats may contain text in the number field, no animation in this case
                 if (isNaN(number.text())) {
@@ -817,9 +817,9 @@ var Dashboard = {
                     step: function () {
                         number.text(this.Counter.toFixed(precision))+suffix;
                     },
-					complete: function () {
-						number.text(targetNumber.text())+suffix;
-					}
+                    complete: function () {
+                        number.text(targetNumber.text())+suffix;
+                    }
                 });
             });
     },
