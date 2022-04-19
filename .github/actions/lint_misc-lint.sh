42368320d277f09196f61f8274912449b6a89576
Cédric Anne <cedric.anne@gmail.com>
2022-04-19 13:34:10 +0200

    Fix translation lint

--- .github/actions/lint_misc-lint.sh
+++ .github/actions/lint_misc-lint.sh
@@ -4,5 +4,5 @@ echo "Check for missing headers"
 vendor/bin/licence-headers-check --ansi --no-interaction
 
 echo "Check for gettext errors/warnings"
-tools/locale/extract_template.sh 2>&1 | tee extract.log
+vendor/bin/extract-locales 2>&1 | tee extract.log
 if [[ -n $(grep "warning" extract.log) ]]; then exit 1; fi
