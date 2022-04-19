dbec808ee575ff2e8d178c21e50c5785cbc62af1
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2022-04-19 13:44:06 +0200

    Update js/dashboard.js
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- js/dashboard.js
+++ js/dashboard.js
@@ -209,10 +209,11 @@ var Dashboard = {
             var active = $(this).hasClass('active');
 
             if (active) {
-                var seconds = parseInt(CFG_GLPI.refresh_views || 30) * 60;
-                if (seconds <= 0) {
-                    seconds = 60;
+                var minutes = parseInt(CFG_GLPI.refresh_views);
+                if (minutes == 0 || Number.isNaN(minutes)) {
+                    minutes = 30;
                 }
+                var seconds = minutes * 60;
                 Dashboard.interval = setInterval(function() {
                     Dashboard.refreshDashboard();
                 }, seconds * 1000);
