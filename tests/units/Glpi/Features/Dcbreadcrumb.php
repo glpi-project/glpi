227baef57f0bf6a848f35e86fd969beaa47a04aa
Stanislas KITA <skita@teclib.com>
2022-05-12 10:27:24 +0200

    fix TU

--- tests/units/Glpi/Features/Dcbreadcrumb.php
+++ tests/units/Glpi/Features/Dcbreadcrumb.php
@@ -77,26 +77,52 @@ class DCBreadcrumb extends \DbTestCase
     {
         $this->login();
 
-       //create DataCenter
+        //create Location for Datacenter
+        $location_datacenter = new \Location();
+        $location_datacenter_name = 'Saint-Petersbourg';
+        $location_datacenter_id = $location_datacenter->add($location_datacenter_input = [
+            'name'         => $location_datacenter_name,
+        ]);
+        $this->checkInput($location_datacenter, $location_datacenter_id, $location_datacenter_input);
+
+        //create DataCenter
         $datacenter = new \Datacenter();
         $datacenter_name = 'test datacenter';
         $datacenter_id = $datacenter->add($datacenter_input = [
             'name'         => $datacenter_name,
+            'locations_id' => $location_datacenter_id,
         ]);
         $this->checkInput($datacenter, $datacenter_id, $datacenter_input);
 
-       //create DCRoom
+        //create Location for DCRoom
+        $location_dcroom = new \Location();
+        $location_dcroom_name = 'room1';
+        $location_dcroom_id = $location_dcroom->add($location_dcroom_input = [
+            'name'         => $location_dcroom_name,
+        ]);
+        $this->checkInput($location_dcroom, $location_dcroom_id, $location_dcroom_input);
+
+        //create DCRoom
         $DCroom = new \DCRoom();
         $DCroom_name = 'test room';
         $DCroom_id = $DCroom->add($DCroom_input = [
             'name'           => $DCroom_name,
             'vis_cols'       => 10,
             'vis_rows'       => 10,
-            'datacenters_id' => $datacenter_id
+            'datacenters_id' => $datacenter_id,
+            'locations_id'   => $location_dcroom_id,
         ]);
         $this->checkInput($DCroom, $DCroom_id, $DCroom_input);
 
-       //create Rack
+        //create Location for Rack
+        $location_rack = new \Location();
+        $location_rack_name = 'rack01';
+        $location_rack_id = $location_rack->add($location_rack_input = [
+            'name'         => $location_rack_name,
+        ]);
+        $this->checkInput($location_rack, $location_rack_id, $location_rack_input);
+
+        //create Rack
         $rack = new \Rack();
         $rack_name = 'test rack';
         $rack_id = $rack->add($rack_input = [
@@ -105,6 +131,7 @@ class DCBreadcrumb extends \DbTestCase
             'dcrooms_id'   => $DCroom_id,
             'number_units' => 42,
             'position' => 62,
+            'locations_id' => $location_rack_id,
         ]);
         $this->checkInput($rack, $rack_id, $rack_input);
 
@@ -122,37 +149,53 @@ class DCBreadcrumb extends \DbTestCase
         ]);
         $this->checkInput($Itemrack, $Itemrack_id, $Itemrack_input);
 
-        $DCBreadcrumb = \Computer::getDcBreadcrumbSpecificValueToDisplay($computer1->getID(), false, false);
-        $expected = sprintf(
-            "<div class=\"row\">
+        $DCBreadcrumb = \Computer::getDcBreadcrumbSpecificValueToDisplay($computer1->getID(), true, false);
+
+        $expected = sprintf("<div class=\"row\">
    
-            <div class=\"col-auto p-1\">
-               <i class='ti ti-building-warehouse'></i> %s
-      
-                     </div>
-                     <div class=\"col-auto p-1\">
-                  >
+      <div class=\"col-auto p-1\">
+         <i class='ti ti-building-warehouse'></i> %s
+
+                     <br>
+            <span class='p-0 float-left badge bg-blue-lt d-inline-block text-truncate' data-toggle='tooltip' title='Saint-Petersbourg' style='max-width: 100px;'>
+               <i class=\"ti ti-map-pin\"></i>%s
+            </span>
                </div>
-               
-            <div class=\"col-auto p-1\">
-               <i class='ti ti-building'></i> %s
-      
-                     </div>
-                     <div class=\"col-auto p-1\">
-                  >
+               <div class=\"col-auto p-1\">
+            >
+         </div>
+         
+      <div class=\"col-auto p-1\">
+         <i class='ti ti-building'></i> %s
+
+                     <br>
+            <span class='p-0 float-left badge bg-blue-lt d-inline-block text-truncate' data-toggle='tooltip' title='room1' style='max-width: 100px;'>
+               <i class=\"ti ti-map-pin\"></i>%s
+            </span>
                </div>
-               
-            <div class=\"col-auto p-1\">
-               <i class='ti ti-server'></i> %s
-      
-                     </div>
-                     <div class=\"col-auto pt-1 p-0\">
-                  (U%d)
+               <div class=\"col-auto p-1\">
+            >
+         </div>
+         
+      <div class=\"col-auto p-1\">
+         <i class='ti ti-server'></i> %s
+
+                     <br>
+            <span class='p-0 float-left badge bg-blue-lt d-inline-block text-truncate' data-toggle='tooltip' title='rack01' style='max-width: 100px;'>
+               <i class=\"ti ti-map-pin\"></i>%s
+            </span>
                </div>
-               </div>",
+               <div class=\"col-auto pt-1 p-0\">
+            (U%d)
+         </div>
+         </div>
+",
             $datacenter_name,
+            $location_datacenter_name,
             $DCroom_name,
+            $location_dcroom_name,
             $rack_name,
+            $location_rack_name,
             $rack_position
         );
         $this->string($DCBreadcrumb)->isIdenticalTo($expected);
