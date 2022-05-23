9cbb2e4a945c13909fea2d1136e48dfad31bb275
Cédric Anne <cedric.anne@gmail.com>
2022-05-23 15:05:47 +0200

    Fix some migrations

---
+++ install/migrations/update_10.0.0_to_10.0.1/schema_fixes.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2022 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+/**
+ * @var DB $DB
+ * @var Migration $migration
+ */
+
+
+// Remove '' default values on glpi_impactcontexts.positions
+// MySQL does not allow default values on TEXT fields, while MariaDB does
+// Default was removed in installation file GLPI 9.5.4, see #8415
+$migration->changeField('glpi_impactcontexts', 'positions', 'positions', 'mediumtext NOT NULL');
