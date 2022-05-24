c14398f0f599bbc419af1773404351c3b0f57953
tsmr <thetsmr@gmail.com>
2022-05-24 10:09:39 +0200

    Fix Requester Count Display

---
+++ ajax/ticketrequesterinformation.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2022 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+// Direct access to file
+if (strpos($_SERVER['PHP_SELF'], "ticketrequesterinformation.php")) {
+    $AJAX_INCLUDE = 1;
+    include('../inc/includes.php');
+    header("Content-Type: text/html; charset=UTF-8");
+    Html::header_nocache();
+}
+
+Session::checkLoginUser();
+
+$only_number = boolval($_REQUEST['only_number'] ?? false);
+
+if (isset($_REQUEST['users_id_requester']) && ($_REQUEST['users_id_requester'] > 0)) {
+    $ticket = new Ticket();
+
+    $options2 = [
+        'criteria' => [
+            [
+                'field'      => 4, // users_id_requester
+                'searchtype' => 'equals',
+                'value'      => $_REQUEST['users_id_requester'],
+                'link'       => 'AND',
+            ],
+            [
+                'field'      => 12, // status
+                'searchtype' => 'equals',
+                'value'      => 'notold',
+                'link'       => 'AND',
+            ],
+        ],
+        'reset' => 'reset',
+    ];
+
+    $url = $ticket->getSearchURL() . "?" . Toolbox::append_params($options2, '&amp;');
+    $nb  = $ticket->countActiveObjectsForUser($_REQUEST['users_id_requester']);
+
+    if ($only_number) {
+        if ($nb > 0) {
+            echo "<a href='$url'>" . $nb . "</a>";
+        }
+    } else {
+        echo "&nbsp;<a href='$url' title=\"" . __s('Processing') . "\">(";
+        printf(__('%1$s: %2$s'), __('Processing'), $nb);
+        echo ")</a>";
+    }
+}
