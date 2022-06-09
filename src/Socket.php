faea4c0d1161952b0bafaf7d3f971a239e9d6cd5
Curtis Conard <cconard96@gmail.com>
2022-06-09 05:40:54 -0400

    Force ID in other places

--- src/Socket.php
+++ src/Socket.php
@@ -603,7 +603,7 @@ class Socket extends CommonDBChild
         $parent = $this->fields['locations_id'];
         if ($parent) {
             $changes[0] = '0';
-            $changes[1] = addslashes($this->getNameID());
+            $changes[1] = addslashes($this->getNameID(['forceid' => true]));
             $changes[2] = '';
             Log::history($parent, 'Location', $changes, $this->getType(), Log::HISTORY_DELETE_SUBITEM);
         }
