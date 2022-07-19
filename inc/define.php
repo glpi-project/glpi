e10ebd3447fd02048d8d5151497b8f7ca265c655
Cédric Anne <cedric.anne@gmail.com>
2022-07-19 11:31:52 +0200

    Drop PHP 7.4 support

--- inc/define.php
+++ inc/define.php
@@ -52,7 +52,7 @@ if (!defined('GLPI_MARKETPLACE_PRERELEASES')) {
     define('GLPI_MARKETPLACE_PRERELEASES', preg_match('/-(dev|alpha\d*|beta\d*|rc\d*)$/', GLPI_VERSION) === 1);
 }
 
-define('GLPI_MIN_PHP', '7.4.0'); // Must also be changed in top of index.php
+define('GLPI_MIN_PHP', '8.0.0'); // Must also be changed in top of index.php
 define('GLPI_MAX_PHP', '8.2.0'); // (Exclusive) Must also be changed in top of index.php
 define('GLPI_YEAR', '2022');
 
