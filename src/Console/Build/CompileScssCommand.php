b258f5daf2d0adae2df64bd718a35a1a77dd5fd8
Curtis Conard <cconard96@gmail.com>
2022-07-22 09:26:38 -0400

    Ensure charset directive is first in compiled CSS (#12277)
    * Ensure charset directive is first in compiled CSS
    
    * Update src/Console/Build/CompileScssCommand.php
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- src/Console/Build/CompileScssCommand.php
+++ src/Console/Build/CompileScssCommand.php
@@ -135,7 +135,13 @@ class CompileScssCommand extends Command
                 ]
             );
 
-            $css = $licence_header . $css;
+            // Insert licence header after the "@charset" directive.
+            $css = preg_replace(
+                '/^(@charset [^;]+;)/',
+                '$0' . PHP_EOL . $licence_header,
+                $css,
+                1
+            );
 
             if ($dry_run) {
                 $message = sprintf('"%s" compiled successfully.', $file);
