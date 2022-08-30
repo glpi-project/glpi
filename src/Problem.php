19700bca5c2094719dd05a779459c7e79d150f97
VladoTTX <71901245+VladoTTX@users.noreply.github.com>
2022-08-30 10:29:03 +0200

    move canMassiveAction to common code

--- src/Problem.php
+++ src/Problem.php
@@ -158,22 +158,6 @@ class Problem extends CommonITILObject
                  || $this->isAllowedStatus($this->fields['status'], self::ASSIGNED));
     }
 
-    public function canMassiveAction($action, $field, $value)
-    {
-
-        switch ($action) {
-            case 'update':
-                switch ($field) {
-                    case 'status':
-                        if (!self::isAllowedStatus($this->fields['status'], $value)) {
-                            return false;
-                        }
-                        break;
-                }
-                break;
-        }
-        return true;
-    }
 
     public function pre_deleteItem()
     {
