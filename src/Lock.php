61a2019e593c4e7986cf96861a25086afa404f68
Stanislas KITA <skita@teclib.com>
2022-09-08 11:38:49 +0200

    CS

--- src/Lock.php
+++ src/Lock.php
@@ -536,7 +536,7 @@ class Lock extends CommonGLPI
     public function getTabNameForItem(CommonGLPI $item, $withtemplate = 0)
     {
         if ($item->isDynamic() && $item->can($item->fields['id'], UPDATE)) {
-            $nb = countElementsInTable(Lockedfield::getTable(), ['items_id' => $item->fields['id'], "itemtype" => $item->getType()]);
+            $nb = countElementsInTable(Lockedfield::getTable(), ['items_id' => $item->fields['id'], 'itemtype' => $item->getType()]);
             return self::createTabEntry(Lock::getTypeName(Session::getPluralNumber()), $nb);
         }
         return '';
