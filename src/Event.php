6fcca161db5933323a81a2fe16da1cb39f7cffe0
AdrienClairembault <aclairembault@teclib.com>
2022-09-27 12:42:52 +0200

    Events: search by itemtype 2

--- src/Event.php
+++ src/Event.php
@@ -44,6 +44,7 @@ use Document;
 use Glpi\Application\View\TemplateRenderer;
 use Html;
 use Infocom;
+use ITILSolution;
 use Session;
 use Toolbox;
 
@@ -510,7 +511,11 @@ class Event extends CommonDBTM
         $itemtypes = [];
         foreach (self::getUsedItemtypes() as $value) {
             $itemtype = self::getItemtypeFromType($value);
-            $itemtypes[$value] = $itemtype::getTypeName(1);
+            if ($itemtype) {
+                $itemtypes[$value] = $itemtype::getTypeName(1);
+            } else {
+                trigger_error("Unsupported type: $value", E_USER_WARNING);
+            }
         }
 
         return [
@@ -648,6 +653,10 @@ class Event extends CommonDBTM
         if (is_a($fallback_type, CommonGLPI::class, true)) {
             $mapping[$type] = $fallback_type;
             return $fallback_type;
+        }
+
+        if ($type == 'solution') {
+            return ITILSolution::class;
         }
 
         return null;
