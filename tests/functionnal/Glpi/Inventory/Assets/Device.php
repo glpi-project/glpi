50378f74a5d6349898959491aaf9f67d4f8dd488
Johan Cwiklinski <johan@x-tnd.be>
2022-10-07 11:24:08 +0200

    Add infocom test on HardDrive devices also. Remove debug info

--- tests/functionnal/Glpi/Inventory/Assets/Device.php
+++ tests/functionnal/Glpi/Inventory/Assets/Device.php
@@ -47,10 +47,9 @@ class Device extends AbstractInventoryAsset
     }
 
 
-    public function testInventoryUpdate()
+    public function testInventoryUpdateMemory()
     {
         global $DB;
-        $device_mem = new \DeviceMemory();
         $item_mem = new \Item_DeviceMemory();
         $info_com = new \Infocom();
 
@@ -104,7 +103,7 @@ class Device extends AbstractInventoryAsset
         $computer->getFromDB($computers_id);
 
         //memories present from the inventory source are dynamic
-        $memories = $item_mem->find(['itemtype' => 'Computer', 'items_id' => $computers_id, 'is_dynamic' => 1]);
+        $memories = $item_mem->find(['itemtype' => \Computer::class, 'items_id' => $computers_id, 'is_dynamic' => 1]);
 
         $this->integer(count($memories))->isIdenticalTo(2);
 
@@ -133,13 +132,116 @@ class Device extends AbstractInventoryAsset
 
         //memory present in the inventory source is still dynamic
         $memories = $item_mem->find(['itemtype' =>  \Computer::class, 'items_id' => $computers_id, 'is_dynamic' => 1]);
-        var_export($memories);
         $this->integer(count($memories))->isIdenticalTo(2);
 
         //check item_device_memory is the same from first step
         foreach ($data_to_check as $data_value) {
             $memories = $item_mem->find(['id' => $data_value['items_id'], "itemtype" => \Computer::class, 'is_dynamic' => 1]);
             $this->integer(count($memories))->isIdenticalTo(1);
+        }
+
+        //check infocom still exists
+        foreach ($data_to_check as $data_value) {
+            $info_coms = $info_com->find(['id' => $data_value['id'], "buy_date" => $infocom_buy_date, "value" => $infocom_value]);
+            $this->integer(count($info_coms))->isIdenticalTo(1);
+        }
+    }
+
+    public function testInventoryUpdateDrive()
+    {
+        global $DB;
+        $item_drv = new \Item_DeviceHardDrive();
+        $info_com = new \Infocom();
+
+        $xml_source = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<REQUEST>
+  <CONTENT>
+    <STORAGES>
+      <DESCRIPTION>PCI</DESCRIPTION>
+      <DISKSIZE>256060</DISKSIZE>
+      <FIRMWARE>BXV77D0Q</FIRMWARE>
+      <INTERFACE>IDE</INTERFACE>
+      <MANUFACTURER>Samsung</MANUFACTURER>
+      <MODEL>PM951 NVMe SAMSUNG 256GB</MODEL>
+      <NAME>nvme0n1</NAME>
+      <SERIALNUMBER>S29NNXAH146764</SERIALNUMBER>
+      <TYPE>disk</TYPE>
+    </STORAGES>
+    <STORAGES>
+      <DESCRIPTION>Lecteur de disque</DESCRIPTION>
+      <DISKSIZE>320072</DISKSIZE>
+      <FIRMWARE>GHBOA900</FIRMWARE>
+      <INTERFACE>IDE</INTERFACE>
+      <MANUFACTURER>Samsung</MANUFACTURER>
+      <MODEL>HGST HTS725032A7E630</MODEL>
+      <NAME>\\.\PHYSICALDRIVE0</NAME>
+      <SCSI_COID>0</SCSI_COID>
+      <SCSI_LUN>0</SCSI_LUN>
+      <SCSI_UNID>0</SCSI_UNID>
+      <SERIAL>131005TF0401Y11K4NNN</SERIAL>
+      <SERIALNUMBER>131005TF0401Y11K4NNN</SERIALNUMBER>
+      <TYPE>disk</TYPE>
+    </STORAGES>
+    <HARDWARE>
+      <NAME>pc002</NAME>
+    </HARDWARE>
+    <BIOS>
+      <SSN>ggheb7ne7</SSN>
+    </BIOS>
+    <VERSIONCLIENT>FusionInventory-Agent_v2.3.19</VERSIONCLIENT>
+  </CONTENT>
+  <DEVICEID>test-pc002</DEVICEID>
+  <QUERY>INVENTORY</QUERY>
+</REQUEST>";
+
+
+        $this->doInventory($xml_source, true);
+
+        //check created agent
+        $agents = $DB->request(['FROM' => \Agent::getTable()]);
+        $this->integer(count($agents))->isIdenticalTo(1);
+        $agent = $agents->current();
+        $computers_id = $agent['items_id'];
+        $this->integer($computers_id)->isGreaterThan(0);
+        $computer = new \Computer();
+        $computer->getFromDB($computers_id);
+
+        //drives present from the inventory source are dynamic
+        $drives = $item_drv->find(['itemtype' => \Computer::class, 'items_id' => $computers_id, 'is_dynamic' => 1]);
+
+        $this->integer(count($drives))->isIdenticalTo(2);
+
+        $data_to_check = [];
+
+        $infocom_buy_date = '2020-10-21';
+        $infocom_value = '300';
+        foreach ($drives as $result) {
+            $item_device_drive_id = $result['id'];
+            $input = [
+                "items_id" => $item_device_drive_id,
+                "itemtype" => \Item_DeviceHardDrive::class,
+                "entities_id" => $computer->fields['entities_id'],
+                "buy_date" => $infocom_buy_date,
+                "value" => $infocom_value
+            ];
+            //create infocom
+            $id = $info_com->add($input);
+            $this->integer($id)->isGreaterThan(0);
+            $input['id'] = $id;
+            $data_to_check[] = $input;
+        }
+
+        //redo an inventory
+        $this->doInventory($xml_source, true);
+
+        //drives present in the inventory source are still dynamic
+        $drives = $item_drv->find(['itemtype' =>  \Computer::class, 'items_id' => $computers_id, 'is_dynamic' => 1]);
+        $this->integer(count($drives))->isIdenticalTo(2);
+
+        //check item_device_memory is the same from first step
+        foreach ($data_to_check as $data_value) {
+            $drives = $item_drv->find(['id' => $data_value['items_id'], "itemtype" => \Computer::class, 'is_dynamic' => 1]);
+            $this->integer(count($drives))->isIdenticalTo(1);
         }
 
         //check infocom still exists
