5c9a2c3243f6e033dfa416700c377636a759de98
Thierry Bugier <tbugier@teclib.com>
2022-10-16 13:57:20 +0200

    allow bind filter events in modals
    A plugin may bind log filter events on its own DOM elements and trigger a glpi.tab.loaded event

--- js/log_filters.js
+++ js/log_filters.js
@@ -33,62 +33,79 @@
 
 /* global reloadTab */
 
+var bindShowFiltersBtn = function (target) {
+    $(target).off('click', '.show_filters').on('click', '.show_filters', showFilters);
+};
+
+var showFilters = function (event) {
+    event.preventDefault();
+
+    // Toggle filters
+    var container = event.target.closest('main');
+    if (event.target.closest('.modal-dialog') !== null) {
+        container = event.target.closest('.modal-dialog');
+    }
+    var tabs_panel = $(container).find('.nav.nav-tabs[role="tablist"]').attr('id');
+
+    if ($(event.target).closest('.show_filters').hasClass('active')) {
+        reloadTab('', tabs_panel);
+    } else {
+        reloadTab('filters[active]=1', tabs_panel);
+    }
+};
+
+var delay_timer = null;
+
+var bindFilterChange = function (target) {
+    // Workaround to prevent opening of dropdown when removing item using the "x" button.
+    // Without this workaround, orphan dropdowns remains in page when reloading tab.
+    $(target)
+    .off('select2:unselecting', '.filter_row .select2-hidden-accessible')
+    .on('select2:unselecting', '.filter_row .select2-hidden-accessible', function(ev) {
+        if (ev.params.args.originalEvent) {
+            ev.params.args.originalEvent.stopPropagation();
+        }
+    });
+
+    $(target)
+    .off('input', '.filter_row [name^="filters\\["]')
+    .on('input', '.filter_row [name^="filters\\["]', function() {
+        clearTimeout(delay_timer);
+        delay_timer = setTimeout(function() {
+            handleFilterChange(target);
+        }, 800);
+    });
+    $(target)
+    .off('change', '.filter_row select[name^="filters\\["]')
+    .on('change', '.filter_row select[name^="filters\\["]', function(event) {
+        handleFilterChange(target);
+    });
+
+    // prevent submit of parent form when pressing enter
+    $(target)
+    .off('keypress', '.filter_row [name^="filters\\["]')
+    .on('keypress', '.filter_row [name^="filters\\["]', function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            handleFilterChange(target);
+        }
+    });
+};
+
+var handleFilterChange = function (target) {
+    if (delay_timer !== null) {
+        clearTimeout(delay_timer);
+    }
+    // Prevent dropdown to remain in page after tab has been reload.
+    $('.filter_row .select2-hidden-accessible').select2('close');
+
+    var tabs_panel = $(target).find('.nav.nav-tabs[role="tablist"]').attr('id');
+    reloadTab($(target).find('[name^="filters\\["]').serialize(), tabs_panel);
+};
+
 $(function() {
-    var bindShowFiltersBtn = function () {
-        $('.show_filters').on('click', showFilters);
-    };
-
-    var showFilters = function (event) {
-        event.preventDefault();
-
-        // Toggle filters
-        if ($('.show_filters').hasClass('active')) {
-            reloadTab('');
-        } else {
-            reloadTab('filters[active]=1');
-        }
-    };
-
-    var delay_timer = null;
-
-    var bindFilterChange = function () {
-        // Workaround to prevent opening of dropdown when removing item using the "x" button.
-        // Without this workaround, orphan dropdowns remains in page when reloading tab.
-        $(document).on('select2:unselecting', '.filter_row .select2-hidden-accessible', function(ev) {
-            if (ev.params.args.originalEvent) {
-                ev.params.args.originalEvent.stopPropagation();
-            }
-        });
-
-        $('.filter_row [name^="filters\\["]').on('input', function() {
-            clearTimeout(delay_timer);
-            delay_timer = setTimeout(function() {
-                handleFilterChange();
-            }, 800);
-        });
-        $('.filter_row select[name^="filters\\["]').on('change', handleFilterChange);
-
-        // prevent submit of parent form when pressing enter
-        $('.filter_row [name^="filters\\["]').on('keypress', function(event) {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                handleFilterChange();
-            }
-        });
-    };
-
-    var handleFilterChange = function () {
-        if (delay_timer !== null) {
-            clearTimeout(delay_timer);
-        }
-        // Prevent dropdown to remain in page after tab has been reload.
-        $('.filter_row .select2-hidden-accessible').select2('close');
-
-        reloadTab($('[name^="filters\\["]').serialize());
-    };
-
-    $('main').on('glpi.tab.loaded', function() {
-        bindShowFiltersBtn();
-        bindFilterChange();
+    $('main').on('glpi.tab.loaded', function(event) {
+        bindShowFiltersBtn(event.target);
+        bindFilterChange(event.target);
     });
 });
