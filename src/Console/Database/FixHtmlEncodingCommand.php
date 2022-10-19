d32bb6e70448a0866b65924f3a85645de41cfa90
Thierry Bugier <tbugier@teclib.com>
2022-10-19 14:34:57 +0200

    fix bad encoding around emails in followups

--- src/Console/Database/FixHtmlEncodingCommand.php
+++ src/Console/Database/FixHtmlEncodingCommand.php
@@ -36,6 +36,7 @@
 namespace Glpi\Console\Database;
 
 use CommonDBTM;
+use ITILFollowup;
 use Glpi\Console\AbstractCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -77,6 +78,8 @@ class FixHtmlEncodingCommand extends AbstractCommand
      * @var integer
      */
     const ERROR_ROLLBACK_FILE_FAILED = 5;
+
+    private array $failed_items = [];
 
     protected function configure()
     {
@@ -124,42 +127,19 @@ class FixHtmlEncodingCommand extends AbstractCommand
         $this->checkArguments();
 
         $itemtype = $input->getOption('itemtype');
-        $item_ids = $input->getOption('id');
-        $fields = $input->getOption('field');
 
         if ($input->getOption('dump')) {
             $this->dumpObjects();
         }
 
-        $failed_items = [];
-        foreach ($item_ids as $item_id) {
-            $item = new $itemtype();
-            $item->getFromDB($item_id);
+        $this->fixItems();
 
-            // update the item
-            $update = [];
-            foreach ($fields as $field) {
-                $update[$field] = $this->doubleEncoding($item->fields[$field]);
-                $update[$field] = $DB->escape($update[$field]);
-            }
-
-            $success = $DB->update(
-                $itemtype::getTable(),
-                $update,
-                ['id' => $item->fields['id']],
-            );
-            if (!$success) {
-                $failed_items[] = $item_id;
-                continue;
-            }
-        }
-
-        if (count($failed_items) > 0) {
+        if (count($this->failed_items) > 0) {
             $this->output->writeln(
-                '<error>' . sprintf(__('Unable to update %s items'), count($failed_items)) . '</error>',
+                '<error>' . sprintf(__('Unable to update %s items'), count($this->failed_items)) . '</error>',
                 OutputInterface::VERBOSITY_QUIET
             );
-            foreach ($failed_items as $item_id) {
+            foreach ($this->failed_items as $item_id) {
                 $this->output->writeln(
                     '<error>' . sprintf(__('Itemtype %s ID %s'), $itemtype, $item_id) . '</error>',
                     OutputInterface::VERBOSITY_QUIET
@@ -275,6 +255,58 @@ class FixHtmlEncodingCommand extends AbstractCommand
         }
     }
 
+    private function fixItems()
+    {
+        $itemtype = $this->input->getOption('itemtype');
+        $item_ids = $this->input->getOption('id');
+
+        foreach ($item_ids as $item_id) {
+            $item = new $itemtype();
+            if (!$item->getFromDB($item_id)) {
+                $this->failed_items[] = $item_id;
+                continue;
+            }
+            $this->fixOneItem($item);
+        }
+    }
+
+    private function fixOneItem(CommonDBTM $item)
+    {
+        global $DB;
+
+        $itemtype = $this->input->getOption('itemtype');
+        $fields = $this->input->getOption('field');
+
+        // update the item
+        $update = [];
+        foreach ($fields as $field) {
+            $update[$field] = $this->fixOneField($item, $field);
+            $update[$field] = $DB->escape($update[$field]);
+        }
+
+        $success = $DB->update(
+            $itemtype::getTable(),
+            $update,
+            ['id' => $item->fields['id']],
+        );
+        if (!$success) {
+            $failed_items[] = $item->getID();
+        }
+    }
+
+    private function fixOneField(CommonDBTM $item, string $field)
+    {
+        $new_value = $item->fields[$field];
+
+        $new_value = $this->doubleEncoding($new_value);
+
+        if ($item::getType() == ITILFollowup::getType() && $field == 'content') {
+            $new_value = $this->emailsInOutlookGenetaredContent($new_value);
+        }
+
+        return $new_value;
+    }
+
     /**
      * Remove double encoding of HTML tags
      * character < is encoded &#38;lt; but should be encoded &#60;
@@ -297,5 +329,38 @@ class FixHtmlEncodingCommand extends AbstractCommand
             '&#62;',
         ];
         return preg_replace($pattern, $replace, $input);
+    }
+
+    /**
+     * Fix double encoded HTML entities in old followups generated from Outlook
+     * @see https://github.com/glpi-project/glpi/issues/8330
+     *
+     * @param string $input
+     * @return string
+     */
+    private function emailsInOutlookGenetaredContent(string $input)
+    {
+        $output = $input;
+
+        // Not very strict pattern for emails, but should be enough
+        // Capturing parentheses:
+        // 1: Triple encoded < character
+        // 2: email address
+        // 3: Triple encoded > character
+        $pattern = '/(&#38;amp;lt;)(?<email>[^@]*?@[a-zA-Z0-9\-.]*?)(&#38;amp;gt;)/';
+        $replace = '&amp;lt;${2}&amp;gt;';
+        $output = preg_replace($pattern, $replace, $output);
+        // Triple encoded should be now double encoded
+
+        // Not very strict pattern for emails, but should be enough
+        // Capturing parentheses:
+        // 1: Double encoded < character
+        // 2: email address
+        // 3: Double encoded > character
+        $pattern = '/(&amp;lt;)(?<email>[^@]*?@[a-zA-Z0-9\-.]*?)(&amp;gt;)/';
+        $replace = '&lt;${2}&gt;';
+        $output = preg_replace($pattern, $replace, $output);
+
+        return $output;
     }
 }
---
+++ tests/units/Glpi/Console/Database/FixHtmlEncodingCommand.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2022 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace tests\units\Glpi\Console\Database;
+
+use GLPITestCase;
+
+class FixHtmlEncodingCommand extends GLPITestCase
+{
+
+
+
+    public function providerDoubleEncoding()
+    {
+        yield [
+            'input'    => '&#38;lt;foo bar&#38;gt;',
+            'expected' => '&#60;foo bar&#62;',
+        ];
+    }
+
+    /**
+     * @DataProvider providerDoubleEncoding
+     *
+     * @return void
+     */
+    public function testDoubleEncoding($input, $expected)
+    {
+        $instance = $this->newTestedInstance();
+        $output = $this->callPrivateMethod($instance, 'doubleEncoding', $input);
+        $this->string($output)->isEqualTo($expected);
+    }
+
+    public function providerEmailsInOutlookGenetaredContent()
+    {
+        yield [
+            'input' => '&amp;lt;helpdesk@some-domain.com&amp;gt;',
+            'expected' => '&lt;helpdesk@some-domain.com&gt;',
+        ];
+
+        yield [
+            'input' => '&#38;amp;lt;helpdesk@some-domain.com&#38;amp;gt;',
+            'expected' => '&lt;helpdesk@some-domain.com&gt;',
+        ];
+    }
+
+    /**
+     * @DataProvider providerEmailsInOutlookGenetaredContent
+     *
+     * @param string $input
+     * @param string $expected
+     * @return void
+     */
+    public function testEmailsInOutlookGenetaredContent($input, $expected)
+    {
+        $instance = $this->newTestedInstance();
+        $output = $this->callPrivateMethod($instance, 'emailsInOutlookGenetaredContent', $input);
+        $this->string($output)->isEqualTo($expected);
+    }
+}
