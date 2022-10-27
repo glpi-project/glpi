bc7ad1e8f203e5e9d78ee5ddafbef37546421913
Stanislas <skita@teclib.com>
2022-10-27 15:36:25 +0200

    refactor code
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- ajax/itilfollowup.php
+++ ajax/itilfollowup.php
@@ -90,11 +90,7 @@ $template->fields['content'] = $template->getRenderedContent($parent);
 //need when template is used and when GLPI preselected type if defined
 $template->fields['requesttypes_name'] = "";
 if ($template->fields['requesttypes_id']) {
-    $dbUtils = new DbUtils();
-    $entityRestrict = $dbUtils->getEntitiesRestrictCriteria(getTableForItemType(RequestType::getType()), "", $parent->fields['entities_id'], true, false);
-    if (count($entityRestrict)) {
-        $entityRestrict = [$entityRestrict];
-    }
+    $entityRestrict = getEntitiesRestrictCriteria(getTableForItemType(RequestType::getType()), "", $parent->fields['entities_id'], true);
 
     $requesttype = new RequestType();
     if (
