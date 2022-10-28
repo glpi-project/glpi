0ac080b5eba6b5c28f0728c9c0f57bfc351a11ed
Johan Cwiklinski <jcwiklinski@teclib.com>
2022-10-28 07:08:03 +0200

    Feature/disable inventory (#13063)
    * Add configuration entry to (dis|en)able inventory
    * Try make message more comprehensible
    * Enable inventory for web tests
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- .github/actions/test_tests-web.sh
+++ .github/actions/test_tests-web.sh
@@ -2,6 +2,7 @@
 set -e -u -x -o pipefail
 
 php -S localhost:8088 tests/router.php &>/dev/null &
+bin/console glpi:config:set --config-dir=./tests/config --context=inventory enabled_inventory 1
 vendor/bin/atoum \
   -p 'php -d memory_limit=512M' \
   --debug \
@@ -13,3 +14,4 @@ vendor/bin/atoum \
   --fail-if-skipped-methods \
   --max-children-number 1 \
   -d tests/web
+bin/console glpi:config:set --config-dir=./tests/config --context=inventory enabled_inventory 0
--- install/migrations/update_10.0.3_to_10.0.4/config.php
+++ install/migrations/update_10.0.3_to_10.0.4/config.php
@@ -39,3 +39,9 @@
 
 // CAS version config was missing on fresh installations since GLPI 9.4.0
 $migration->addConfig(['cas_version' => 'CAS_VERSION_2_0']);
+
+$should_inventory_be_enabled = 0; //default value
+if (countElementsInTable(\Agent::getTable()) > 0) {
+    $should_inventory_be_enabled = 1;
+}
+$migration->addConfig(['enabled_inventory' => $should_inventory_be_enabled], 'inventory');
