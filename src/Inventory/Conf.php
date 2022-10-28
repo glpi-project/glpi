3f473efb268c1fc12bf735650c3bb11582943e11
stonebuzz <skita@teclib.com>
2022-10-28 15:01:20 +0200

    keep original value

--- src/Inventory/Conf.php
+++ src/Inventory/Conf.php
@@ -120,7 +120,7 @@ class Conf extends CommonGLPI
         'component_control'              => 1,
         'component_battery'              => 1,
         'component_simcard'              => 1,
-        'states_id_default'              => -1,
+        'states_id_default'              => 0,
         'location'                       => 0,
         'group'                          => 0,
         'vm_type'                        => 0,
