5bec4da97365a82b34478c3cddd182a2baf88d1e
Cédric Anne <cedric.anne@gmail.com>
2022-11-02 19:42:43 +0100

    Rename CI script to same name as in 10.0/bugfixes branch

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -144,7 +144,7 @@ $APPLICATION_ROOT/.github/actions/init_containers-start.sh
 $APPLICATION_ROOT/.github/actions/init_show-versions.sh
 
 # Install dependencies if required
-[[ -z "$BUILD" ]] || docker-compose exec -T app .github/actions/init_install-dependencies.sh
+[[ -z "$BUILD" ]] || docker-compose exec -T app .github/actions/init_build.sh
 
 # Run tests
 for TEST_SUITE in "${TESTS_TO_RUN[@]}";
