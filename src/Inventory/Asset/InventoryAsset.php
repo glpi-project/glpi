8879b0f326afdd7555f369f11879719fd98cdeed
stonebuzz <skita@teclib.com>
2022-11-09 16:49:24 +0100

    revert previous fix and keep partial

--- src/Inventory/Asset/InventoryAsset.php
+++ src/Inventory/Asset/InventoryAsset.php
@@ -193,7 +193,7 @@ abstract class InventoryAsset
         //compare current itemtype et mainasset itemtype to be sure
         //to get related lock
         if (get_class($this->item) == $itemtype) {
-            $items_id = $this->item->fields[static::getIndexName()] ?? 0;
+            $items_id = $this->item->fields['id'] ?? 0;
         }
         $locks = $lockedfield->getLockedNames($itemtype, $items_id);
 
