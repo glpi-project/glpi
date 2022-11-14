2711cf739e77f07e2756084efdf544e71b58efa4
Stanislas KITA <skita@teclib.com>
2022-11-14 16:27:54 +0100

    sanitize field

--- src/RuleCriteria.php
+++ src/RuleCriteria.php
@@ -375,6 +375,7 @@ class RuleCriteria extends CommonDBChild
      **/
     public static function match(RuleCriteria &$criterion, $field, &$criterias_results, &$regex_result)
     {
+        $field = Sanitizer::decodeHtmlSpecialChars($field);
 
         $condition = $criterion->fields['condition'];
         $pattern   = $criterion->fields['pattern'];
