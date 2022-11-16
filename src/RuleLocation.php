f0ccca631bfc5c38d13c1227cdd3499014e47109
stonebuzz <skita@teclib.com>
2022-11-16 15:05:05 +0100

    refactor code

--- src/RuleLocation.php
+++ src/RuleLocation.php
@@ -52,26 +52,24 @@ class RuleLocation extends Rule
 
     public function executeActions($output, $params, array $input = [])
     {
-        if (count($this->actions)) {
-            foreach ($this->actions as $action) {
-                switch ($action->fields["action_type"]) {
-                    case "assign":
-                        $output[$action->fields["field"]] = $action->fields["value"];
-                        break;
-                    case 'regex_result':
-                        if ($action->fields["field"] == "locations_id") {
-                            foreach ($this->regex_results as $regex_result) {
-                                $regexvalue          = RuleAction::getRegexResultById(
-                                    $action->fields["value"],
-                                    $regex_result
-                                );
-                                $compute_entities_id = $input['entities_id'] ?? 0;
-                                $location = new Location();
-                                $output['locations_id'] = $location->importExternal($regexvalue, $compute_entities_id);
-                            }
+        foreach ($this->actions as $action) {
+            switch ($action->fields["action_type"]) {
+                case "assign":
+                    $output[$action->fields["field"]] = $action->fields["value"];
+                    break;
+                case 'regex_result':
+                    if ($action->fields["field"] == "locations_id") {
+                        foreach ($this->regex_results as $regex_result) {
+                            $regexvalue          = RuleAction::getRegexResultById(
+                                $action->fields["value"],
+                                $regex_result
+                            );
+                            $compute_entities_id = $input['entities_id'] ?? 0;
+                            $location = new Location();
+                            $output['locations_id'] = $location->importExternal($regexvalue, $compute_entities_id);
                         }
-                        break;
-                }
+                    }
+                    break;
             }
         }
         return $output;
