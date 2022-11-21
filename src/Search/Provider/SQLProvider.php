90740e70f0da8ba85da1cb5c1a6a8b6b3a6f0362
Thierry Bugier <tbugier@teclib.com>
2022-11-21 14:59:26 +0100

    sqlprovider backward compatibility

--- src/Search/Provider/SQLProvider.php
+++ src/Search/Provider/SQLProvider.php
@@ -113,6 +113,10 @@ final class SQLProvider implements SearchProviderInterface
                 if ($plug = isPluginItemType($itemtype)) {
                     $default_select = \Plugin::doOneHook($plug['plugin'], 'addDefaultSelect', $itemtype);
                     if ($default_select !== "") {
+                        $default_select = trim($default_select);
+                        if (substr($default_select, -1) === ',') {
+                            $default_select = substr($default_select, 0, -1);
+                        }
                         $ret[] = new QueryExpression($default_select);
                     }
                 }
