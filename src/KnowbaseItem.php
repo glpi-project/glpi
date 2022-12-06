e3abd0a3c194fb92f7e62c7882b417e99288743a
Curtis Conard <cconard96@gmail.com>
2022-12-06 04:49:11 -0500

    Show full KB article

--- src/KnowbaseItem.php
+++ src/KnowbaseItem.php
@@ -2326,7 +2326,9 @@ class KnowbaseItem extends CommonDBVisible implements ExtraVisibilityCriteria
         } else {
             $answer = $this->fields["answer"];
         }
-        $answer = RichText::getEnhancedHtml($answer);
+        $answer = RichText::getEnhancedHtml($answer, [
+            'text_maxsize' => 0 // Show all text without read more button
+        ]);
 
         $callback = function ($matches) {
           //1 => tag name, 2 => existing attributes, 3 => title contents
