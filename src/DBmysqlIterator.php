0953aeb97d718f6b72abc96093992b71e537104a
Curtis Conard <cconard96@gmail.com>
2022-12-07 06:59:45 -0500

    Fix analysis of raw plugin joins

--- src/DBmysqlIterator.php
+++ src/DBmysqlIterator.php
@@ -723,6 +723,8 @@ class DBmysqlIterator implements SeekableIterator, Countable
                 }
                 return $fkey;
             }
+        } else if ($values instanceof QueryExpression) {
+            return $values->getValue();
         }
         trigger_error("BAD FOREIGN KEY, should be [ table1 => key1, table2 => key2 ] or [ table1 => key1, table2 => key2, [criteria]]", E_USER_ERROR);
     }
