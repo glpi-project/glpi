b9d9e84bd3e7c645d8819635af73250f1d6bf50e
Cédric Anne <cedric.anne@gmail.com>
2022-12-09 15:24:15 +0100

    Skip platform reqs on PHP 8.2

--- .github/actions/init_build.sh
+++ .github/actions/init_build.sh
@@ -5,15 +5,17 @@ set -e -u -x -o pipefail
 # i.e. no "dev", "alpha", "beta, "rc", ... keyword.
 STABLE_REGEX="^[0-9]+\.[0-9]+\.[0-9]+$"
 
+PHP_MAJOR_VERSION="$(echo $PHP_VERSION | cut -d '.' -f 1,2)"
+
 # Validate composer config
 composer validate --strict
-if [[ "$PHP_VERSION" =~ $STABLE_REGEX ]]; then
+if [[ "$PHP_VERSION" =~ $STABLE_REGEX && "$PHP_MAJOR_VERSION" -ne "8.2" ]]; then
   composer check-platform-reqs;
 fi
 
 # Install dependencies
 COMPOSER_ADD_OPTS=""
-if ! [[ "$PHP_VERSION" =~ $STABLE_REGEX ]]; then
+if ! [[ "$PHP_VERSION" =~ $STABLE_REGEX && "$PHP_MAJOR_VERSION" -ne "8.2" ]]; then
   COMPOSER_ADD_OPTS="--ignore-platform-reqs";
 fi
 bin/console dependencies install --composer-options="$COMPOSER_ADD_OPTS --prefer-dist --no-progress"
