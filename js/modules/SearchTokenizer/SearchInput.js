e4ea983fa94217507c73d92486bbc196f8558fa5
Curtis Conard <cconard96@gmail.com>
2022-12-13 18:44:54 -0500

    Fix lost token info after autocomplete transform

--- js/modules/SearchTokenizer/SearchInput.js
+++ js/modules/SearchTokenizer/SearchInput.js
@@ -214,7 +214,9 @@ export default class SearchInput {
 
         input.on('blur', '.search-input-tag-input', (e) => {
             const tag_input = $(e.target).closest('.search-input-tag-input');
-            this.tagifyInputNode(tag_input);
+            if (tag_input.length > 0 && tag_input.text().trim().length > 0) {
+                this.tagifyInputNode(tag_input);
+            }
         });
 
         input.on('keydown', '.search-input-tag-input', (e) => {
@@ -475,7 +477,9 @@ export default class SearchInput {
                 const term_text = $(`<span>${last_token.term}</span>`).text();
                 if (autocomplete_value.localeCompare(term_text, undefined, { sensitivity: 'accent' }) === 0) {
                     last_token.term = t;
-                    node.replaceWith($(this.tokenToTagHtml(last_token)));
+                    const replacement = $(this.tokenToTagHtml(last_token));
+                    replacement.data('token', last_token);
+                    node.replaceWith(replacement);
                 }
             });
         }
