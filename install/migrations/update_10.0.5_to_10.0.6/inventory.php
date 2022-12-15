2b0dfcc717891dc41b78774134f4efb0b954e7c7
Rom1-B <rbiot@teclib.com>
2022-12-15 14:09:55 +0100

    fix(update): PHP Warning: Undefined array key stale_agents_action

--- install/migrations/update_10.0.5_to_10.0.6/inventory.php
+++ install/migrations/update_10.0.5_to_10.0.6/inventory.php
@@ -40,7 +40,7 @@
 $migration->addConfig(["entities_id_default" => 0], 'inventory');
 
 $config = \Config::getConfigurationValues('inventory');
-if (is_numeric($config['stale_agents_action'])) {
+if (isset($config['stale_agents_action']) && is_numeric($config['stale_agents_action'])) {
     //convert stale_agents_action to an array
     $DB->update(
         'glpi_configs',
