284c0fb810c388b6392f1699c0bf71f1785720f3
Stanislas <skita@teclib.com>
2022-12-15 15:31:08 +0100

    fix(snmp): do not delete management port each time

--- src/Inventory/Asset/Printer.php
+++ src/Inventory/Asset/Printer.php
@@ -36,6 +36,7 @@
 
 namespace Glpi\Inventory\Asset;
 
+use Blacklist;
 use CommonDBTM;
 use Glpi\Toolbox\Sanitizer;
 use IPAddress;
@@ -365,20 +366,25 @@ class Printer extends NetworkEquipment
      */
     public static function needToBeUpdatedFromDiscovery(CommonDBTM $item, $val)
     {
-        if (property_exists($val, 'ips') && isset($val->ips[0])) {
-            $ip = $val->ips[0];
-            //try to find IP (get from discovery) from known IP of Printer
-            //if found refuse update
-            //if no, printer IP have changed so  we allow the update from discovery
-            $ipadress = new IPAddress($ip);
-            $tmp['mainitems_id'] = $item->fields['id'];
-            $tmp['mainitemtype'] = $item::getType();
-            $tmp['is_dynamic']   = 1;
-            $tmp['name']         = $ipadress->getTextual();
-            if ($ipadress->getFromDBByCrit(Sanitizer::sanitize($tmp))) {
-                return false;
+        if (property_exists($val, 'ips')) {
+            foreach ($val->ips as $ip) {
+                $blacklist = new Blacklist();
+                //exclude IP if needed
+                if ('' != $blacklist->process(Blacklist::IP, $ip)) {
+                    //try to find IP (get from discovery) from known IP of Printer
+                    //if found refuse update
+                    //if no, printer IP have changed so  we allow the update from discovery
+                    $ipadress = new IPAddress($ip);
+                    $tmp['mainitems_id'] = $item->fields['id'];
+                    $tmp['mainitemtype'] = $item::getType();
+                    $tmp['is_dynamic']   = 1;
+                    $tmp['name']         = $ipadress->getTextual();
+                    if ($ipadress->getFromDBByCrit(Sanitizer::sanitize($tmp))) {
+                        return false;
+                    }
+                    return true;
+                }
             }
-            return true;
         }
         return false;
     }
