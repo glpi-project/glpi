5abcc2ab29ed9466952705fb9afccc2bee8ccac6
François Legastelois <flegastelois@teclib.com>
2023-01-11 22:01:26 +0100

    fix: cs

--- install/migrations/update_10.0.5_to_10.0.6/cable.php
+++ install/migrations/update_10.0.5_to_10.0.6/cable.php
@@ -40,10 +40,10 @@
 
 $table = 'glpi_cables';
 if (!$DB->fieldExists($table, 'is_deleted', false)) {
-   $migration->addField($table, 'is_deleted', 'bool');
-   $migration->addKey($table, 'is_deleted');
+    $migration->addField($table, 'is_deleted', 'bool');
+    $migration->addKey($table, 'is_deleted');
 }
 
 $migration->updateRight('cable_management', READ | UPDATE | CREATE | DELETE | PURGE, [
-   'cable_management' => READ | UPDATE | CREATE | PURGE
+    'cable_management' => READ | UPDATE | CREATE | PURGE
 ]);
