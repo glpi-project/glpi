1230d151c814d8da21e40945c3a779cccf6731b1
Johan Cwiklinski <johan@x-tnd.be>
2023-01-26 14:14:24 +0100

    Do not use call()

--- src/APIClient.php
+++ src/APIClient.php
@@ -191,6 +191,7 @@ class APIClient extends CommonDBTM
      */
     public function showForm($ID, $options = [])
     {
+        $options['log_methods'] = self::getLogMethod();
         $this->initForm($ID, $options);
         TemplateRenderer::getInstance()->display('pages/setup/apiclient.html.twig', [
             'item'   => $this,
