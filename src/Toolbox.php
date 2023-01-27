24b5cf7e8211dcd80b52b80acd6fae04bec5190a
Rom1-B <rbiot@teclib.com>
2023-01-27 09:25:34 +0100

    fix: check URL with &

--- src/Toolbox.php
+++ src/Toolbox.php
@@ -3459,7 +3459,7 @@ HTML;
        // (or would require usage of a dedicated lib).
         return (preg_match(
             "/^(?:http[s]?:\/\/(?:[^\s`!(){};'\",<>«»“”‘’+]+|[^\s`!()\[\]{};:'\".,<>?«»“”‘’+]))$/iu",
-            $url
+            str_replace('&#38;', '&', $url)
         ) === 1);
     }
 
