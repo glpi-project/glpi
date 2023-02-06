459a91638f57d3346e79d4a7b80559e912a1034d
Cédric Anne <cedric.anne@gmail.com>
2023-02-06 11:26:00 +0100

    Add 64 bits integer support as optional requirement
    see #13980

---
+++ src/System/Requirement/IntegerSize.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2023 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace Glpi\System\Requirement;
+
+final class IntegerSize extends AbstractRequirement
+{
+    public function __construct()
+    {
+        $this->title = __('PHP maximal integer size');
+        $this->description = __('Support of 64 bits integers is required for IP addresses related operations (network inventory, API clients IP filtering, ...).');
+        $this->optional = true;
+    }
+
+    protected function check()
+    {
+        if (PHP_INT_SIZE < 8) {
+            $this->validated = false;
+            $this->validation_messages[] = __('OS or PHP is not relying on 64 bits integers, operations on IP addresses may produce unexpected results.');
+        } else {
+            $this->validated = true;
+            $this->validation_messages[] = __('OS and PHP are relying on 64 bits integers.');
+        }
+    }
+}
