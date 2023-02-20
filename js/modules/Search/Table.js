05e0f66673c355640229a8bed21e197cfd1a38fe
Rom1-B <rbiot@teclib.com>
2023-02-20 10:05:52 +0100

    review

--- js/modules/Search/Table.js
+++ js/modules/Search/Table.js
@@ -199,7 +199,7 @@ window.GLPI.Search.Table = class Table extends GenericView {
                 this.getElement().trigger('search_refresh', [this.getElement()]);
                 this.hideLoadingSpinner();
                 this.shiftSelectAllCheckbox();
-                this.reloadJS('rateit');
+                $('.rateit').rateit();
             }, () => {
                 handle_search_failure();
             });
@@ -211,14 +211,6 @@ window.GLPI.Search.Table = class Table extends GenericView {
     // permit to [shift] select checkboxes
     shiftSelectAllCheckbox() {
         $('#'+this.element_id+' tbody input[type="checkbox"]').shiftSelectable();
-    }
-
-    reloadJS(script) {
-        $.each($('script:empty[type="text/javascript"][src*="'+script+'"]'), function(index, el) {
-            var clone = el.cloneNode(true);
-            $(el).remove();
-            $(clone).appendTo('body');
-        });
     }
 
     registerListeners() {
