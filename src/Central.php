5372a5fc64c93b00e33ad9889bd7b7caaf3f1603
Cédric Anne <cedric.anne@gmail.com>
2023-03-14 14:32:25 +0100

    Force web root to be `/public`

--- src/Central.php
+++ src/Central.php
@@ -36,7 +36,6 @@
 use Glpi\Application\View\TemplateRenderer;
 use Glpi\Event;
 use Glpi\Plugin\Hooks;
-use Glpi\System\Requirement\SafeDocumentRoot;
 
 /**
  * Central class
@@ -515,11 +514,6 @@ class Central extends CommonGLPI
                 . ' '
                 . sprintf(__('Run the "%1$s" command to migrate them.'), 'php bin/console migration:unsigned_keys');
             }
-        }
-
-        $safe_doc_root_requirement = new SafeDocumentRoot();
-        if (!$safe_doc_root_requirement->isValidated()) {
-            $messages['warnings'] = array_merge($messages['warnings'], $safe_doc_root_requirement->getValidationMessages());
         }
 
         if ($DB->isSlave() && !$DB->first_connection) {
