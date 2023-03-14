d6f170c88fc0ce31428ccfc0c90c1905b56b2603
Cédric Anne <cedric.anne@gmail.com>
2023-03-14 16:10:11 +0100

    Remove obsolete `tests/bin/test-data-sanitization` command

--- .github/actions/test_update-from-older-version.sh
+++ .github/actions/test_update-from-older-version.sh
@@ -100,7 +100,6 @@ bin/console database:check_schema_integrity \
 bin/console database:check_schema_integrity \
   --config-dir=./tests/config --ansi --no-interaction \
   --check-all-migrations
-tests/bin/test-data-sanitization --ansi --no-interaction
 
 # Check updated data
 bin/console database:configure \
