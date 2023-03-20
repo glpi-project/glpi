ade745aa03224c3f3f01c03e334b60d36cbabecf
Stanislas <skita@teclib.com>
2023-03-20 14:32:00 +0100

    fix(KnowbaseItem): fix a regression on boolean operators removal (#14301)
    * fix(KnowbaseItem): fix a regression on boolean operators removal
    
    * fix TU
    
    * fix TU
    
    * revert old commit
    
    * add TU
    
    * fix comment
    
    * refactor code
    
    * revert previous commit
    
    * fix SQL query
    
    * refactor code
    
    * refactor code
    
    * fix CS
    
    * add comment
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * Update comment
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * Update comment
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * Update comment
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * Update comment
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * use mb_substr_count for multibyte strings
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * Use mb_substr_count for multibyte string
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * Update comment
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * Asterisks should be only located at end of a word
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * fix TU
    
    * add more TU and fix regex
    
    * fix CS
    
    * add unicode and cyrillic
    
    * fix regex
    
    * fix CS
    
    * Tweak regex and make tests on more complex strings
    
    * Fix categories on export
    
    * groupby is not needed anymore
    
    * remove dead code
    
    ---------
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -455,14 +455,14 @@ function loadDataset()
         ], 'KnowbaseItem' => [
             [
                 'name'     => '_knowbaseitem01',
-                'answer'   => 'Answer for Knowledge base entry _knowbaseitem01',
+                'answer'   => 'Answer for Knowledge base entry _knowbaseitem01 apple juice turnover',
                 'is_faq'   => 0,
                 'users_id' => TU_USER,
                 'date'     => '2016-11-17 12:27:48',
             ],
             [
                 'name'     => '_knowbaseitem02',
-                'answer'   => 'Answer for Knowledge base entry _knowbaseitem02',
+                'answer'   => 'Answer for Knowledge base entry _knowbaseitem02 apple macintosh strudel',
                 'is_faq'   => 0,
                 'users_id' => TU_USER,
                 'date'     => '2016-11-17 12:27:48',
