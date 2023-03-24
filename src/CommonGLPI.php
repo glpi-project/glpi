7ba735b456214748986eb7b65b4c1da37839e484
François Legastelois <flegastelois@teclib.com>
2023-03-24 15:42:35 +0100

    fix: short text, more title

--- src/CommonGLPI.php
+++ src/CommonGLPI.php
@@ -1077,14 +1077,13 @@ class CommonGLPI implements CommonGLPIInterface
                     'forceid' => $this instanceof CommonITILObject
                 ]);
                 if ($this->isField('is_deleted') && $this->fields['is_deleted']) {
-                    echo "<span class='mx-2 bg-danger status rounded-1' title=\"" . __s('This item is deleted!') . "\"
+                    $title = $this->isField('date_mod')
+                                ? sprintf(__s('Item has been deleted on %1$s'), Html::convDateTime($this->fields['date_mod']))
+                                : __s('Deleted');
+                    echo "<span class='mx-2 bg-danger status rounded-1' title=\"" . $title . "\"
                         data-bs-toggle='tooltip'>
                         <i class='ti ti-trash'></i>";
-                    if ($this->isField('date_mod')) {
-                        echo sprintf(__s('Deleted on %1$s'), Html::convDateTime($this->fields['date_mod']));
-                    } else {
-                        echo __s('This item is deleted!');
-                    }
+                        echo __s('Deleted');
                     echo "</span>";
                 }
                 echo "</h3>";
