85154b416827c9385b711f9de80668808bdcd097
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2023-03-29 15:06:09 +0200

    Fix missing history entry when adding a user to a group (#14404)
    * Fix missing history entry when adding a user to a group
    
    * post_purgeItem

--- src/Group_User.php
+++ src/Group_User.php
@@ -857,6 +857,8 @@ class Group_User extends CommonDBRelation
     {
         global $DB;
 
+        parent::post_addItem();
+
        // add new user to plannings
         $groups_id  = $this->fields['groups_id'];
         $planning_k = 'group_' . $groups_id . '_users';
@@ -915,6 +917,8 @@ class Group_User extends CommonDBRelation
     public function post_purgeItem()
     {
         global $DB;
+
+        parent::post_purgeItem();
 
        // remove user from plannings
         $groups_id  = $this->fields['groups_id'];
