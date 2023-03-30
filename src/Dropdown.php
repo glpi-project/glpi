34e27b4f3a2880772a332dadc287bee22c678330
Cédric Anne <cedric.anne@gmail.com>
2023-03-30 15:05:13 +0200

    Fix PHP 8.1 warning on floating numbers dropdown

--- src/Dropdown.php
+++ src/Dropdown.php
@@ -1754,11 +1754,12 @@ JAVASCRIPT;
         }
 
         $field_id = Html::cleanId("dropdown_" . $myname . $p['rand']);
-        if (!isset($p['toadd'][$p['value']])) {
+        $valuekey = Toolbox::isFloat($p['value']) ? (string)$p['value'] : $p['value'];
+        if (!isset($p['toadd'][$valuekey])) {
             $decimals = Toolbox::isFloat($p['value']) ? Toolbox::getDecimalNumbers($p['step']) : 0;
             $valuename = self::getValueWithUnit($p['value'], $p['unit'], $decimals);
         } else {
-            $valuename = $p['toadd'][$p['value']];
+            $valuename = $p['toadd'][$valuekey];
         }
         $param = ['value'               => $p['value'],
             'valuename'           => $valuename,
