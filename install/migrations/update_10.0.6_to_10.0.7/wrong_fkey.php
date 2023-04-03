8f80dbe8cac4cefda395328e1aa2a438e6cdaaed
Johan Cwiklinski <johan@x-tnd.be>
2023-04-03 10:56:04 +0200

    Fix

--- install/migrations/update_10.0.6_to_10.0.7/wrong_fkey.php
+++ install/migrations/update_10.0.6_to_10.0.7/wrong_fkey.php
@@ -36,10 +36,22 @@
 /**
  * @var Migration $migration
  */
+$default_key_sign = DBConnection::getDefaultPrimaryKeySignOption();
+
 $migration->dropKey('glpi_items_devicecameras_imageformats', 'item_devicecameras_id');
-$migration->changeField('glpi_items_devicecameras_imageformats', 'item_devicecameras_id', 'items_devicecameras_id', 'integer');
+$migration->changeField(
+    'glpi_items_devicecameras_imageformats',
+    'item_devicecameras_id',
+    'items_devicecameras_id',
+    "int {$default_key_sign} NOT NULL DEFAULT '0'"
+);
 $migration->addKey('glpi_items_devicecameras_imageformats', 'items_devicecameras_id');
 
 $migration->dropKey('glpi_items_devicecameras_imageresolutions', 'item_devicecameras_id');
-$migration->changeField('glpi_items_devicecameras_imageresolutions', 'item_devicecameras_id', 'items_devicecameras_id', 'integer');
+$migration->changeField(
+    'glpi_items_devicecameras_imageresolutions',
+    'item_devicecameras_id',
+    'items_devicecameras_id',
+    "int {$default_key_sign} NOT NULL DEFAULT '0'"
+);
 $migration->addKey('glpi_items_devicecameras_imageresolutions', 'items_devicecameras_id');
