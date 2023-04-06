bb4e51689ed4a10a31175da1af4f41ec3cb462ec
Curtis Conard <cconard96@gmail.com>
2023-04-06 09:10:54 -0400

    fix bulk create and update of some items via api

--- src/Api/API.php
+++ src/Api/API.php
@@ -1805,7 +1805,6 @@ abstract class API
         $itemtype = $this->handleDepreciation($itemtype);
 
         $input    = isset($params['input']) ? $params["input"] : null;
-        $item     = new $itemtype();
 
         if (is_object($input)) {
             $input = [$input];
@@ -1825,6 +1824,8 @@ abstract class API
             $failed       = 0;
             $index        = 0;
             foreach ($input as $object) {
+                // Use a new instance each time to avoid side effects with data from a previous item (See #14490)
+                $item     = new $itemtype();
                 $object      = $this->inputObjectToArray($object);
                 $current_res = [];
 
@@ -1933,9 +1934,7 @@ abstract class API
     protected function updateItems($itemtype, $params = [])
     {
         $itemtype = $this->handleDepreciation($itemtype);
-
         $input    = isset($params['input']) ? $params["input"] : null;
-        $item     = new $itemtype();
 
         if (is_object($input)) {
             $input = [$input];
@@ -1955,6 +1954,8 @@ abstract class API
             $failed       = 0;
             $index        = 0;
             foreach ($input as $object) {
+                // Use a new instance each time to avoid side effects with data from a previous item (See #14490)
+                $item     = new $itemtype();
                 $current_res = [];
                 if (isset($object->id)) {
                     if (!$item->getFromDB($object->id)) {
