74fe2bc07b0ca8bc3f8fe063c70fb6d86f5d6f36
AdrienClairembault <aclairembault@teclib.com>
2023-04-12 15:33:47 +0200

    Fix regex that detect namespaces

--- src/Toolbox/Sanitizer.php
+++ src/Toolbox/Sanitizer.php
@@ -219,6 +219,7 @@ class Sanitizer
 
     /**
      * Check wether the value correspond to a valid namespaced class (or a callable identifier related to a valid class).
+     * Note: also support the {namespace}${tab number} format used for tab identifications
      *
      * @param string $value
      *
@@ -227,8 +228,12 @@ class Sanitizer
     public static function isNsClassOrCallableIdentifier(string $value): bool
     {
         $class_match = [];
-        return preg_match('/^(?<class>(([a-zA-Z0-9_]+\\\)+[a-zA-Z0-9_]+))(:?:[a-zA-Z0-9_]+)?$/', $value, $class_match)
-            && class_exists($class_match['class']);
+
+        return preg_match(
+            '/^(?<class>(([a-zA-Z0-9_]+\\\)+[a-zA-Z0-9_]+))(:?:[a-zA-Z0-9_]+)?(\$[0-9]+)*$/',
+            $value,
+            $class_match
+        ) && class_exists($class_match['class']);
     }
 
     /**
