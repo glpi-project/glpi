35bac0f29b5688f490617a2e8ba0093be86effab
Cédric Anne <cedric.anne@gmail.com>
2023-04-12 11:45:34 +0200

    Compute `can_promote` early, to make it updatable using `show_in_timeline` hook

--- src/CommonITILObject.php
+++ src/CommonITILObject.php
@@ -6752,6 +6752,7 @@ abstract class CommonITILObject extends CommonDBTM
                 $followup_obj->getFromDB($followups_id);
                 if ($followup_obj->canViewItem() || $params['bypass_rights']) {
                     $followup['can_edit'] = $followup_obj->canUpdateItem();
+                    $followup['can_promote'] = Session::getCurrentInterface() === 'central' && $this instanceof Ticket && Ticket::canCreate();
                     $timeline["ITILFollowup_" . $followups_id] = [
                         'type' => ITILFollowup::class,
                         'item' => $followup,
@@ -6768,6 +6769,7 @@ abstract class CommonITILObject extends CommonDBTM
                 $task_obj->getFromDB($tasks_id);
                 if ($task_obj->canViewItem() || $params['bypass_rights']) {
                     $task['can_edit'] = $task_obj->canUpdateItem();
+                    $task['can_promote'] = Session::getCurrentInterface() === 'central' && $this instanceof Ticket && Ticket::canCreate();
                     $timeline[$task_obj::getType() . "_" . $tasks_id] = [
                         'type' => $taskClass,
                         'item' => $task,
