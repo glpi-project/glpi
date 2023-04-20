d748b79544bd53b57198984000ef88f23945e9b4
Stanislas <skita@teclib.com>
2023-04-20 15:45:58 +0200

    use NULL value by default
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- install/migrations/update_10.0.6_to_10.0.7/configs.php
+++ install/migrations/update_10.0.6_to_10.0.7/configs.php
@@ -41,4 +41,4 @@ $migration->displayMessage('Add new configurations / user preferences');
 $migration->addConfig([
     'use_flat_dropdowntree_on_search_result'   => 0,
 ]);
-$migration->addField('glpi_users', 'use_flat_dropdowntree_on_search_result', 'tinyint DEFAULT 0');
+$migration->addField('glpi_users', 'use_flat_dropdowntree_on_search_result', 'tinyint DEFAULT NULL');
