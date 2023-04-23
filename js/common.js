eeb3d6595e5a818f8c51b8ea60cc3858e4173072
Curtis Conard <cconard96@gmail.com>
2023-04-23 19:20:27 -0400

    improve workaround

--- js/common.js
+++ js/common.js
@@ -37,7 +37,7 @@
 
 var timeoutglobalvar;
 
-if (navigator.userAgent.indexOf('AppleWebKit') !== -1) {
+if (navigator.userAgent.indexOf('AppleWebKit') !== -1 && navigator.vendor.indexOf('Apple') !== -1) {
     // Workaround for select2 dropdownAutowidth not applying until the second time the dropdown is opened
     // See: https://github.com/glpi-project/glpi/issues/13433 and https://github.com/select2/select2/issues/4678
     const original_select2_fn = $.fn.select2;
@@ -45,11 +45,18 @@ if (navigator.userAgent.indexOf('AppleWebKit') !== -1) {
         const result = original_select2_fn.apply(this, arguments);
         if (typeof options === 'object') {
             // open and close the dropdown after initialization
-            this.select2('open');
-            this.select2('close');
+            result.on('select2:open', function () {
+                const el = $(this);
+                if (el.data('opened-before') === undefined) {
+                    el.data('opened-before', true);
+                    el.select2('close');
+                    el.select2('open');
+                }
+            });
         }
         return result;
     };
+    $.fn.select2.defaults = original_select2_fn.defaults;
 }
 
 /**
