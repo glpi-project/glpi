8f6000bc7d41de591212e65f6fdd9780c6e322c4
Johan Cwiklinski <johan@x-tnd.be>
2023-05-04 11:30:44 +0200

    Fix CS

--- src/Inventory/Asset/InventoryAsset.php
+++ src/Inventory/Asset/InventoryAsset.php
@@ -454,16 +454,15 @@ abstract class InventoryAsset
     final protected function cleanName(string $string): string
     {
         return trim(
+            preg_replace(
+                '/[\x{200B}-\x{200D}\x{FEFF}]/u', //remove invisible characters
+                '',
                 preg_replace(
-                    '/[\x{200B}-\x{200D}\x{FEFF}]/u', //remove invisible characters
-                    '',
-                    preg_replace(
-                        '/\s+/u', //replace with single standard whitespace
-                        ' ',
-                        $string
-                    )
+                    '/\s+/u', //replace with single standard whitespace
+                    ' ',
+                    $string
                 )
-
+            )
         );
     }
 }
