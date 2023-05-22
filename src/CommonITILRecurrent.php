afc9fcb97cb37be07d6eb18a4862af8531fd7576
Cédric Anne <cedric.anne@gmail.com>
2023-05-22 11:39:55 +0200

    Preserve predefined status of recurrent ticket/change
    fixes #13838

--- src/CommonITILRecurrent.php
+++ src/CommonITILRecurrent.php
@@ -553,9 +553,11 @@ abstract class CommonITILRecurrent extends CommonDropdown
     /**
      * Create an item based on the specified template
      *
+     * @param CommonITILObject|null $created_item   Will contain the created item instance
+     *
      * @return boolean
      */
-    public function createItem()
+    public function createItem(?CommonITILObject &$created_item = null)
     {
         $result = false;
         $concrete_class = static::getConcreteClass();
@@ -577,6 +579,10 @@ abstract class CommonITILRecurrent extends CommonDropdown
             $predefined = $fields->getPredefinedFields($this->fields[$tmpl_fk], true);
             $input = $this->handlePredefinedFields($predefined, $input);
 
+            if (array_key_exists('status', $predefined)) {
+                $input['_do_not_compute_status'] = true;
+            }
+
            // Set entity
             $input['entities_id'] = $this->fields['entities_id'];
             $input['_auto_import'] = true;
@@ -586,6 +592,7 @@ abstract class CommonITILRecurrent extends CommonDropdown
             $input  = Toolbox::addslashes_deep($input);
 
             if ($items_id = $item->add($input)) {
+                $created_item = $item;
                 $msg = sprintf(
                     __('%s %d successfully created'),
                     $concrete_class::getTypeName(1),
