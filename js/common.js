36a98212a171189693bfdc414996fe2832235e0a
Clément Cailly <clemcailly@gmail.com>
2023-05-31 12:14:28 +0200

    fix: JS lint

--- js/common.js
+++ js/common.js
@@ -1571,7 +1571,7 @@ function waitForElement(selector) {
             return resolve(document.querySelector(selector));
         }
 
-        const observer = new MutationObserver(mutations => {
+        const observer = new MutationObserver(() => {
             if (document.querySelector(selector)) {
                 resolve(document.querySelector(selector));
                 observer.disconnect();
