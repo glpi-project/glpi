e2b15a78e91a123a42230974f3a4b22018800548
Johan Cwiklinski <johan@x-tnd.be>
2023-06-05 14:53:44 +0200

    Fix soft manufacturer with and without accents

--- tests/functional/Glpi/Inventory/Assets/Software.php
+++ tests/functional/Glpi/Inventory/Assets/Software.php
@@ -1421,4 +1421,92 @@ class Software extends AbstractInventoryAsset
         ]))->isTrue();
         $this->integer($item_versions_id)->isIdenticalTo($item_version->fields['id']);
     }
+
+    public function testManufacturerSpecialCharacters()
+    {
+        $computer = new \Computer();
+        $soft = new \Software();
+        $version = new \SoftwareVersion();
+        $item_version = new \Item_SoftwareVersion();
+
+        //inventory with a software manufacturer with a special character
+        $xml_source = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<REQUEST>
+  <CONTENT>
+      <SOFTWARES>
+      <NAME>Any other software</NAME>
+      <VERSION>2.0.0</VERSION>
+      <PUBLISHER>Manufacture</PUBLISHER>
+    </SOFTWARES>
+    <SOFTWARES>
+      <NAME>Any software</NAME>
+      <VERSION>1.0.0</VERSION>
+      <PUBLISHER>Manufacturé</PUBLISHER>
+    </SOFTWARES>
+    <HARDWARE>
+      <NAME>pc002</NAME>
+    </HARDWARE>
+    <BIOS>
+      <SSN>ggheb7ne7</SSN>
+    </BIOS>
+    <VERSIONCLIENT>FusionInventory-Agent_v2.3.19</VERSIONCLIENT>
+  </CONTENT>
+  <DEVICEID>test-pc002</DEVICEID>
+  <QUERY>INVENTORY</QUERY>
+</REQUEST>";
+
+        //create manually a computer
+        $computers_id = $computer->add([
+            'name'   => 'pc002',
+            'serial' => 'ggheb7ne7',
+            'entities_id' => 0
+        ]);
+        $this->integer($computers_id)->isGreaterThan(0);
+
+        $this->doInventory($xml_source, true);
+
+        //check software has been created
+        $this->boolean(
+            $soft->getFromDBByCrit(['name' => 'Any software'])
+        )->isTrue();
+        $softwares_id = $soft->fields['id'];
+
+        //check version has been created
+        $this->boolean(
+            $version->getFromDBByCrit(['name' => '1.0.0'])
+        )->isTrue();
+        $this->integer($version->fields['softwares_id'])->isIdenticalTo($softwares_id);
+        $versions_id = $version->fields['id'];
+
+        //check computer-softwareverison relation has been created
+        $this->boolean($item_version->getFromDBByCrit([
+            "itemtype" => "Computer",
+            "items_id" => $computers_id,
+            "softwareversions_id" => $versions_id
+        ]))->isTrue();
+        $item_versions_id = $item_version->fields['id'];
+
+        //import again
+        $this->doInventory($xml_source, true);
+
+        //check software is the same
+        $this->boolean(
+            $soft->getFromDBByCrit(['name' => 'Any software'])
+        )->isTrue();
+        $this->integer($softwares_id)->isIdenticalTo($soft->fields['id']);
+
+        //check version is the same
+        $this->boolean(
+            $version->getFromDBByCrit(['name' => '1.0.0'])
+        )->isTrue();
+        $this->integer($versions_id)->isIdenticalTo($version->fields['id']);
+
+        //check computer-softwareverison relation is the same
+        $this->boolean($item_version->getFromDBByCrit([
+            "itemtype" => "Computer",
+            "items_id" => $computers_id,
+            "softwareversions_id" => $versions_id
+        ]))->isTrue();
+        $this->integer($item_versions_id)->isIdenticalTo($item_version->fields['id']);
+    }
 }
