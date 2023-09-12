d36e84b1b442edebc021b236b27af40f534953e2
AdrienClairembault <aclairembault@teclib.com>
2023-09-12 14:59:16 +0200

    Forms: basic fields

--- front/form/form.form.php
+++ front/form/form.form.php
@@ -40,5 +40,45 @@ include('../../inc/includes.php');
 // Only super admins for now - TODO add specific rights
 Session::checkRight("config", UPDATE);
 
-// Show requested form
-Form::displayFullPageForItem($_GET['id'] ?? 0, ['admin', Form::getType()], []);
+// Read parameters
+$id = $_REQUEST['id'] ?? null;
+
+if (isset($_POST["add"])) {
+    // Create form
+    $form = new Form();
+    $form->check($id, CREATE);
+
+    if ($form->add($_POST)) {
+        if ($_SESSION['glpibackcreated']) {
+            Html::redirect($form->getLinkURL());
+        }
+    }
+    Html::back();
+} elseif (isset($_POST["update"])) {
+    // Update form
+    $form = new Form();
+    $form->check($id, UPDATE);
+    $form->update($_POST);
+    Html::back();
+} elseif (isset($_POST["delete"])) {
+    // Delete form
+    $form = new Form();
+    $form->check($id, DELETE);
+    $form->delete($_POST);
+    $form->redirectToList();
+} elseif (isset($_POST["restore"])) {
+    // Restore form
+    $form = new Form();
+    $form->check($id, DELETE);
+    $form->restore($_POST);
+    $form->redirectToList();
+} elseif (isset($_POST["purge"])) {
+    // Purge form
+    $form = new Form();
+    $form->check($id, PURGE);
+    $form->delete($_POST, true);
+    $form->redirectToList();
+} else {
+    // Show requested form
+    Form::displayFullPageForItem($id, ['admin', Form::getType()], []);
+}
