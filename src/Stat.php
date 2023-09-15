3f64e4d08dff841bcb7675bde014dc303f2892a0
Johan Cwiklinski <johan@x-tnd.be>
2023-09-15 08:38:32 +0200

    Use already existing translatable string

--- src/Stat.php
+++ src/Stat.php
@@ -1925,7 +1925,7 @@ JAVASCRIPT
         if ($param['csv'] && $csv_link) {
             $chart_options['toolbox']['feature']['myCsvExport'] = [
                 'icon'    => 'path://M14,3v4a1,1,0,0,0,1,1h4 M17,21h-10a2,2,0,0,1,-2,-2v-14a2,2,0,0,1,2,-2h7l5,5v11a2,2,0,0,1,-2,2z M12,17v-6 M9.5,14.5l2.5,2.5l2.5,-2.5',
-                'title'   => __('Export as CSV'),
+                'title'   => __('Export to CSV'),
                 'onclick' => new Json_Expr(<<<JAVASCRIPT
                     function () {
                         location.href = '$csv_link';
@@ -2047,7 +2047,7 @@ JAVASCRIPT;
                 'feature' => [
                     'myCsvExport'    => [
                         'icon'    => 'path://M14,3v4a1,1,0,0,0,1,1h4 M17,21h-10a2,2,0,0,1,-2,-2v-14a2,2,0,0,1,2,-2h7l5,5v11a2,2,0,0,1,-2,2z M12,17v-6 M9.5,14.5l2.5,2.5l2.5,-2.5',
-                        'title'   => __('Export as CSV'),
+                        'title'   => __('Export to CSV'),
                         'onclick' => new Json_Expr(<<<JAVASCRIPT
                             function () {
                                 location.href = '$csv_link';
