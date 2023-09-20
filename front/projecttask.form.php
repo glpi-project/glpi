10f16c074aa0bc7e72babdd5167620848ba37c8e
Stanislas <skita@teclib.com>
2023-09-20 09:45:16 +0200

    fix(ProjectTask): add redirection to project on delete to prevent white page (#15610)
    * fix(ProjectTask): add redirection to project on delete to prevent white page
    
    * fix(ProjectTask): add missing 'restore' action

--- front/projecttask.form.php
+++ front/projecttask.form.php
@@ -71,6 +71,19 @@ if (isset($_POST["add"])) {
     } else {
         Html::redirect(ProjectTask::getFormURL() . "?projects_id=" . $task->fields['projects_id']);
     }
+} else if (isset($_POST["restore"])) {
+    $task->check($_POST["id"], DELETE);
+
+    $task->restore($_POST);
+    Event::log(
+        $_POST["id"],
+        "project",
+        4,
+        "maintain",
+        //TRANS: %s is the user login
+        sprintf(__('%s restores a task'), $_SESSION["glpiname"])
+    );
+    Html::back();
 } else if (isset($_POST["delete"])) {
     $task->check($_POST['id'], DELETE);
     $task->delete($_POST);
@@ -83,6 +96,7 @@ if (isset($_POST["add"])) {
         //TRANS: %s is the user login
         sprintf(__('%s delete a task'), $_SESSION["glpiname"])
     );
+    Html::redirect(Project::getFormURLWithID($task->fields['projects_id']));
 } else if (isset($_POST["purge"])) {
     $task->check($_POST['id'], PURGE);
     $task->delete($_POST, 1);
