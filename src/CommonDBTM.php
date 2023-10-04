3dbd91f6ce6229eb057cdf8516253913311ef42e
Curtis Conard <cconard96@gmail.com>
2023-10-04 08:56:23 -0400

    drop update list check

--- src/CommonDBTM.php
+++ src/CommonDBTM.php
@@ -680,9 +680,6 @@ class CommonDBTM extends CommonGLPI
                 unset($oldvalues[$field]);
             }
         }
-        if (count($tobeupdated) === 0) {
-            return true;
-        }
         $result = $DB->update(
             $this->getTable(),
             $tobeupdated,
