ff00ece336ace2c7211f905d46c7fc163588d634
Curtis Conard <cconard96@gmail.com>
2023-10-04 20:11:33 -0400

    fix other duplicates

--- install/migrations/update_10.0.10_to_10.0.11/duplicate_searchopt_fixes.php
+++ install/migrations/update_10.0.10_to_10.0.11/duplicate_searchopt_fixes.php
@@ -42,3 +42,6 @@ $migration->changeSearchOption('Computer', 129, 1429);
 $migration->changeSearchOption('Computer', 130, 1430);
 $migration->changeSearchOption('Computer', 131, 1431);
 $migration->changeSearchOption('Computer', 132, 1432);
+
+$migration->changeSearchOption('Domain', 30, 1400);
+$migration->changeSearchOption('Project', 119, 1400);
