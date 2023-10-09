59237dce278a752ed94d74014b48d04bdaa2517a
Rom1-B <rbiot@teclib.com>
2023-10-09 12:59:15 +0200

    fix(migration 10.0.9 to 10.0.10): PHP Error Allowed memory size

--- install/migrations/update_10.0.9_to_10.0.10/ldap_fields.php
+++ install/migrations/update_10.0.9_to_10.0.10/ldap_fields.php
@@ -64,7 +64,17 @@ foreach ($groups as $group) {
 /** /Fix non encoded LDAP fields in groups */
 
 /** Fix non encoded LDAP fields in users */
-$users = getAllDataFromTable('glpi_users', ['authtype' => 3]);
+$users = $DB->request([
+    'SELECT' => [
+        'glpi_users.id',
+        'glpi_users.user_dn',
+        'glpi_users.sync_field',
+    ],
+    'FROM'   => 'glpi_users',
+    'WHERE'  => [
+        'authtype' => 3,
+    ],
+]);
 foreach ($users as $user) {
     $updated = [];
     foreach (['user_dn', 'sync_field'] as $ldap_field) {
