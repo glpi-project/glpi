11ad4cb2f64813731fad6641eabf4925164ca1de
Curtis Conard <cconard96@gmail.com>
2023-10-11 17:15:56 -0400

    make public for plugins to add props to existing

--- src/Api/HL/Controller/AbstractController.php
+++ src/Api/HL/Controller/AbstractController.php
@@ -140,7 +140,7 @@ abstract class AbstractController
      * @param string|null $full_schema The name of the schema that represents the full object
      * @return array The schema
      */
-    protected static function getDropdownTypeSchema(string $class, ?string $field = null, string $name_field = 'name', ?string $full_schema = null): array
+    public static function getDropdownTypeSchema(string $class, ?string $field = null, string $name_field = 'name', ?string $full_schema = null): array
     {
         if ($field === null) {
             $field = $class::getForeignKeyField();
