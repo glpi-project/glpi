eefd3f561115c58c553ccb31b6764289a235e216
Cédric Anne <cedric.anne@gmail.com>
2023-10-18 13:58:35 +0200

    Fix DB breadcrumbs for PDU placed aside a rack
    fixes #15813

--- tests/units/Glpi/Features/Dcbreadcrumb.php
+++ tests/units/Glpi/Features/Dcbreadcrumb.php
@@ -35,106 +35,361 @@
 
 namespace tests\units\Glpi\Features;
 
+use CommonDBTM;
+use Datacenter;
+use DCRoom;
+use DbTestCase;
+use Enclosure;
+use Item_Enclosure;
+use Item_Rack;
+use PDU;
+use PDU_Rack;
+use Rack;
+use Toolbox;
+
 /**
  * Test for the {@link \Glpi\Features\Clonable} feature
  */
-class DCBreadcrumb extends \DbTestCase
+class DCBreadcrumb extends DbTestCase
 {
-    public function DCBreadcrumbProvider()
+    protected function itemtypeProvider()
     {
-        return [
-            [\Computer::class, true],
-            [\DCRoom::class, true],
-            [\Enclosure::class, true],
-            [\Monitor::class, true],
-            [\NetworkEquipment::class, true],
-            [\PassiveDCEquipment::class, true],
-            [\PDU::class, true],
-            [\Peripheral::class, true],
-            [\Rack::class, true],
-            [\Location::class, false]
+        /**
+         * @var array $CFG_GLPI
+         */
+        global $CFG_GLPI;
+
+        foreach ($CFG_GLPI["rackable_types"] as $itemtype) {
+            yield[
+                'class' => $itemtype,
+            ];
+        }
+
+        yield[
+            'class' => Rack::class,
+        ];
+        yield[
+            'class' => DCRoom::class,
         ];
     }
 
     /**
-     * @param $class
-     * @param $result
-     * @dataProvider DCBreadcrumbProvider
+     * @dataProvider itemtypeProvider
      */
-    public function testDCBreadcrumbProvider($class, $result)
+    public function testDCBreadcrumbProvider(string $class)
     {
-        $this->login();
-        $this->boolean(method_exists($class, 'getDcBreadcrumbSpecificValueToDisplay'))->isIdenticalTo($result);
+        $this->boolean(Toolbox::hasTrait($class, \Glpi\Features\DCBreadcrumb::class))->isTrue;
     }
 
-
-    /**
-     * Test DCBreadCrumb
-     */
-    public function testDCBreadcrumb()
+    protected function rackableTypeDcBreadcrumbProvider(): iterable
     {
-        $this->login();
+        /**
+         * @var array $CFG_GLPI
+         */
+        global $CFG_GLPI;
 
-       //create DataCenter
-        $datacenter = new \Datacenter();
-        $datacenter_name = 'test datacenter';
-        $datacenter_id = $datacenter->add($datacenter_input = [
-            'name'         => $datacenter_name,
-        ]);
-        $this->checkInput($datacenter, $datacenter_id, $datacenter_input);
+        $root_entity_id = $this->getTestRootEntity(true);
 
-       //create DCRoom
-        $DCroom = new \DCRoom();
-        $DCroom_name = 'test room';
-        $DCroom_id = $DCroom->add($DCroom_input = [
-            'name'           => $DCroom_name,
-            'vis_cols'       => 10,
-            'vis_rows'       => 10,
-            'datacenters_id' => $datacenter_id
-        ]);
-        $this->checkInput($DCroom, $DCroom_id, $DCroom_input);
-
-       //create Rack
-        $rack = new \Rack();
-        $rack_name = 'test rack';
-        $rack_id = $rack->add($rack_input = [
-            'name'         => $rack_name,
-            'entities_id'   => 0,
-            'dcrooms_id'   => $DCroom_id,
-            'number_units' => 42,
-            'position' => 62,
-        ]);
-        $this->checkInput($rack, $rack_id, $rack_input);
-
-       //load computer and add it to rack
-        $computer1 = getItemByTypeName('Computer', '_test_pc01');
-
-       //create Rack_Item
-        $Itemrack = new \Item_Rack();
-        $rack_position = 37;
-        $Itemrack_id = $Itemrack->add($Itemrack_input = [
-            'racks_id' => $rack_id,
-            'itemtype' => "Computer",
-            'items_id' => $computer1->getID(),
-            'position' => $rack_position,
-        ]);
-        $this->checkInput($Itemrack, $Itemrack_id, $Itemrack_input);
-
-        $DCBreadcrumb = \Computer::getDcBreadcrumbSpecificValueToDisplay($computer1->getID());
-        $this->string($DCBreadcrumb)->isIdenticalTo(
-            sprintf(
-                "<i class='ti ti-building-warehouse'></i> %s &gt; <i class='ti ti-building'></i> %s &gt; <i class='ti ti-server'></i> %s&nbsp;(U%d)",
-                $datacenter_name,
-                $DCroom_name,
-                $rack_name,
-                $rack_position
-            )
+        $datacenter = $this->createItem(
+            Datacenter::class,
+            [
+                'entities_id'   => $root_entity_id,
+                'name'          => 'Main DC',
+            ]
         );
 
-       //load computer without Rack link
-        $computer2 = getItemByTypeName('Computer', '_test_pc02');
+        foreach ($CFG_GLPI["rackable_types"] as $itemtype) {
+            $room = $this->createItem(
+                DCRoom::class,
+                [
+                    'entities_id'   => $root_entity_id,
+                    'name'          => 'DC room',
+                ]
+            );
+            $rack = $this->createItem(
+                Rack::class,
+                [
+                    'entities_id'   => $root_entity_id,
+                    'name'          => 'Rack #1',
+                    'number_units'  => 48,
+                ]
+            );
+            $enclosure = $this->createItem(
+                Enclosure::class,
+                [
+                    'entities_id'   => $root_entity_id,
+                    'name'          => 'Enclosure test',
+                ]
+            );
 
-        $DCBreadcrumb = \Computer::getDcBreadcrumbSpecificValueToDisplay($computer2->getID());
-        $this->string($DCBreadcrumb)->isIdenticalTo('&nbsp;');
+            $item_for_rack = $this->createItem(
+                $itemtype,
+                [
+                    'entities_id'   => $root_entity_id,
+                    'name'          => 'Item to be put in a rack',
+                ]
+            );
+
+            // Standalone Item
+            yield [
+                'item' => $item_for_rack,
+                'breadcrumbs' => [
+                ],
+            ];
+
+            // Item in a rack
+            $this->createItem(
+                Item_Rack::class,
+                [
+                    'racks_id'  => $rack->getID(),
+                    'itemtype'  => $itemtype,
+                    'items_id'  => $item_for_rack->getID(),
+                    'position'  => 7,
+                ]
+            );
+            yield [
+                'item' => $item_for_rack,
+                'breadcrumbs' => [
+                    "<i class='ti ti-server'></i> Rack #1&nbsp;(U7)",
+                ],
+            ];
+
+            if ($itemtype !== Enclosure::class) {
+                $item_for_enclosure = $this->createItem(
+                    $itemtype,
+                    [
+                        'entities_id'   => $root_entity_id,
+                        'name'          => 'Item to be put in an enclosure',
+                    ]
+                );
+
+                // Item in enclosure
+                $this->createItem(
+                    Item_Enclosure::class,
+                    [
+                        'enclosures_id' => $enclosure->getID(),
+                        'itemtype'      => $itemtype,
+                        'items_id'      => $item_for_enclosure->getID(),
+                        'position'      => 1,
+                    ]
+                );
+                yield [
+                    'item' => $item_for_enclosure,
+                    'breadcrumbs' => [
+                        "<i class='ti ti-columns'></i> Enclosure test&nbsp;(U1)"
+                    ],
+                ];
+
+                // Item in enclosure in rack
+                $this->createItem(
+                    Item_Rack::class,
+                    [
+                        'racks_id'  => $rack->getID(),
+                        'itemtype'  => Enclosure::class,
+                        'items_id'  => $enclosure->getID(),
+                        'position'  => 3,
+                    ]
+                );
+                yield [
+                    'item' => $item_for_enclosure,
+                    'breadcrumbs' => [
+                        "<i class='ti ti-columns'></i> Enclosure test&nbsp;(U1)",
+                        "<i class='ti ti-server'></i> Rack #1&nbsp;(U3)",
+                    ],
+                ];
+            }
+
+            // Place the rack in a room
+            $this->updateItem(Rack::class, $rack->getID(), ['dcrooms_id' => $room->getID(), 'position' => 5]);
+
+            // Item in a rack in a room
+            yield [
+                'item' => $item_for_rack,
+                'breadcrumbs' => [
+                    "<i class='ti ti-server'></i> Rack #1&nbsp;(U7)",
+                    "<i class='ti ti-building'></i> DC room",
+                ],
+            ];
+
+            if ($itemtype !== Enclosure::class) {
+                // Item in enclosure in rack in room
+                yield [
+                    'item' => $item_for_enclosure,
+                    'breadcrumbs' => [
+                        "<i class='ti ti-columns'></i> Enclosure test&nbsp;(U1)",
+                        "<i class='ti ti-server'></i> Rack #1&nbsp;(U3)",
+                        "<i class='ti ti-building'></i> DC room",
+                    ],
+                ];
+            }
+
+            // Place the room in a datacenter
+            $this->updateItem(DCRoom::class, $room->getID(), ['datacenters_id' => $datacenter->getID()]);
+
+            // Item in a rack in a room in a datacenter
+            yield [
+                'item' => $item_for_rack,
+                'breadcrumbs' => [
+                    "<i class='ti ti-server'></i> Rack #1&nbsp;(U7)",
+                    "<i class='ti ti-building'></i> DC room",
+                    "<i class='ti ti-building-warehouse'></i> Main DC",
+                ],
+            ];
+
+            if ($itemtype !== Enclosure::class) {
+                // Item in enclosure in rack in room in a datacenter
+                yield [
+                    'item' => $item_for_enclosure,
+                    'breadcrumbs' => [
+                        "<i class='ti ti-columns'></i> Enclosure test&nbsp;(U1)",
+                        "<i class='ti ti-server'></i> Rack #1&nbsp;(U3)",
+                        "<i class='ti ti-building'></i> DC room",
+                        "<i class='ti ti-building-warehouse'></i> Main DC",
+                    ],
+                ];
+            }
+        }
+    }
+
+    /**
+     * @dataProvider rackableTypeDcBreadcrumbProvider
+     */
+    public function testGetDcBreadcrumbForRackableType(CommonDBTM $item, array $breadcrumbs): void
+    {
+        $this->array($item->getDcBreadcrumb())->isEqualTo($breadcrumbs);
+    }
+
+    protected function pduAsideRackDcBreadcrumbProvider(): iterable
+    {
+        /**
+         * @var array $CFG_GLPI
+         */
+        global $CFG_GLPI;
+
+        $root_entity_id = $this->getTestRootEntity(true);
+
+        $datacenter = $this->createItem(
+            Datacenter::class,
+            [
+                'entities_id'   => $root_entity_id,
+                'name'          => 'Main DC',
+            ]
+        );
+        $room = $this->createItem(
+            DCRoom::class,
+            [
+                'entities_id'   => $root_entity_id,
+                'name'          => 'DC room',
+            ]
+        );
+        $rack = $this->createItem(
+            Rack::class,
+            [
+                'entities_id'   => $root_entity_id,
+                'name'          => 'Rack #1',
+                'number_units'  => 48,
+            ]
+        );
+        $pdu = $this->createItem(
+            PDU::class,
+            [
+                'entities_id' => $root_entity_id,
+                'name' => 'PDU aside the rack'
+            ]
+        );
+
+        $sides_mapping = [
+            PDU_Rack::SIDE_TOP      => 'On top',
+            PDU_Rack::SIDE_RIGHT    => 'On right',
+            PDU_Rack::SIDE_BOTTOM   => 'On bottom',
+            PDU_Rack::SIDE_LEFT     => 'On left',
+        ];
+
+        // PDU aside a rack
+        $pdu_rack = $this->createItem(
+            PDU_Rack::class,
+            [
+                'racks_id' => $rack->getID(),
+                'pdus_id'  => $pdu->getID(),
+                'position' => 12,
+                'side'     => PDU_Rack::SIDE_TOP,
+            ]
+        );
+        foreach ($sides_mapping as $side => $text) {
+            $this->updateItem(PDU_Rack::class, $pdu_rack->getID(), ['side' => $side]);
+            yield [
+                'item' => $pdu,
+                'breadcrumbs' => [
+                    "<i class='ti ti-server'></i> Rack #1&nbsp;{$text} (12)"
+                ],
+            ];
+        }
+
+        // Place the rack in a room
+        $this->updateItem(Rack::class, $rack->getID(), ['dcrooms_id' => $room->getID(), 'position' => 5]);
+
+        // PDU aside a rack in a room
+        foreach ($sides_mapping as $side => $text) {
+            $this->updateItem(PDU_Rack::class, $pdu_rack->getID(), ['side' => $side]);
+            yield [
+                'item' => $pdu,
+                'breadcrumbs' => [
+                    "<i class='ti ti-server'></i> Rack #1&nbsp;{$text} (12)",
+                    "<i class='ti ti-building'></i> DC room",
+                ],
+            ];
+        }
+
+        // Place the room in a datacenter
+        $this->updateItem(DCRoom::class, $room->getID(), ['datacenters_id' => $datacenter->getID()]);
+
+        // PDU aside a rack in a room in a datacenter
+        foreach ($sides_mapping as $side => $text) {
+            $this->updateItem(PDU_Rack::class, $pdu_rack->getID(), ['side' => $side]);
+            yield [
+                'item' => $pdu,
+                'breadcrumbs' => [
+                    "<i class='ti ti-server'></i> Rack #1&nbsp;{$text} (12)",
+                    "<i class='ti ti-building'></i> DC room",
+                    "<i class='ti ti-building-warehouse'></i> Main DC",
+                ],
+            ];
+        }
+    }
+
+    /**
+     * @dataProvider pduAsideRackDcBreadcrumbProvider
+     */
+    public function testGetDcBreadcrumbPduAsideRack(CommonDBTM $item, array $breadcrumbs): void
+    {
+        $this->array($item->getDcBreadcrumb())->isEqualTo($breadcrumbs);
+    }
+
+    public function testGetDcBreadcrumbForADcRoom(): void
+    {
+        $root_entity_id = $this->getTestRootEntity(true);
+
+        $datacenter = $this->createItem(
+            Datacenter::class,
+            [
+                'entities_id'   => $root_entity_id,
+                'name'          => 'Main DC',
+            ]
+        );
+        $room = $this->createItem(
+            DCRoom::class,
+            [
+                'entities_id'   => $root_entity_id,
+                'name'          => 'DC room',
+            ]
+        );
+
+        // Room NOT attached to a datacenter
+        $this->array($room->getDcBreadcrumb())->isEqualTo([]);
+
+        // Room attached to a datacenter
+        $this->updateItem(DCRoom::class, $room->getID(), ['datacenters_id' => $datacenter->getID()]);
+        $this->boolean($room->getFromDB($room->getID()))->isTrue();
+        $this->array($room->getDcBreadcrumb())->isEqualTo(["<i class='ti ti-building-warehouse'></i> Main DC"]);
     }
 }
