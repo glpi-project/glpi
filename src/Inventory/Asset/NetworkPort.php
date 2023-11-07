6d74dafe24f006353f0ce56bca7e8d648f0cc2a7
Stanislas <skita@teclib.com>
2023-11-07 14:34:08 +0100

    fix(Inventory): correctly handle Cisco stacked switch (#15759)

--- src/Inventory/Asset/NetworkPort.php
+++ src/Inventory/Asset/NetworkPort.php
@@ -799,11 +799,19 @@ class NetworkPort extends InventoryAsset
             $bkp_ports = $this->ports;
             $stack_id = $mainasset->getStackId();
             $need_increment_index = false;
+            $count_char = 0;
             foreach ($this->ports as $k => $val) {
                 $matches = [];
                 if (
                     preg_match('@[\w\s+]+(\d+)/[\w]@', $val->name, $matches)
                 ) {
+                    //reset increment when name lenght differ
+                    //Gi0/0 then Gi0/0/1, Gi0/0/2, Gi0/0/3
+                    if ($count_char && $count_char != strlen($val->name)) {
+                        $need_increment_index = false;
+                    }
+                    $count_char = strlen($val->name);
+
                     //in case when port is related to chassis index 0 (stack_id)
                     //ex : GigabitEthernet 0/1    Gi0/0/1
                     //GLPI compute stack_id by starting with 1 (see: NetworkEquipment->getStackedSwitches)
@@ -813,7 +821,6 @@ class NetworkPort extends InventoryAsset
                         //current NetworkEquipement must have the index incremented
                         $need_increment_index = true;
                     }
-
                     if ($matches[1] != $stack_id) {
                         //port attached to another stack entry, remove from here
                         unset($this->ports[$k]);
