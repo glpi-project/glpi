b814be230f4ac81779def89cb109b267adf803a9
kabassanov <kabassanov@users.noreply.github.com>
2023-11-08 10:10:21 +0100

    Update Item_Ticket.php
    Wrong code from another bug patch

--- src/Item_Ticket.php
+++ src/Item_Ticket.php
@@ -198,8 +198,7 @@ class Item_Ticket extends CommonItilObject_Item
         ];
 
         $opt = [];
-        unset($options['entities_id']);
-
+        
         foreach ($options as $key => $val) {
             if (!empty($val)) {
                 $params[$key] = $val;
