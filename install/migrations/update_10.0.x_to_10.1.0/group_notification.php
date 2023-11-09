a86c758c4dc79b74bdc5c95d19672cc0dd26d0da
Barbesange, Jason <jason.barbesange-ext@araymond.com>
2023-11-09 10:22:25 +0100

    add migration file (add use_notification column in ticket, problem and change groups table)

---
+++ install/migrations/update_10.0.x_to_10.1.0/group_notification.php
@@ -0,0 +1,45 @@
+<?php
+
+$table = 'glpi_groups_tickets';
+
+$field_to_add = 'use_notification';
+if (!$DB->fieldExists($table, $field_to_add)) {
+    $migration->addField(
+        $table,
+        $field_to_add,
+        'tinyint',
+        [
+            'value' => 0,
+        ]
+    );
+    $migration->addKey($table, $field_to_add);
+}
+$table = 'glpi_change_groups';
+
+$field_to_add = 'use_notification';
+if (!$DB->fieldExists($table, $field_to_add)) {
+    $migration->addField(
+        $table,
+        $field_to_add,
+        'tinyint',
+        [
+            'value' => 0,
+        ]
+    );
+    $migration->addKey($table, $field_to_add);
+}
+$table = 'glpi_groups_problems';
+
+$field_to_add = 'use_notification';
+if (!$DB->fieldExists($table, $field_to_add)) {
+    $migration->addField(
+        $table,
+        $field_to_add,
+        'tinyint',
+        [
+            'value' => 0,
+        ]
+    );
+    $migration->addKey($table, $field_to_add);
+}
+
