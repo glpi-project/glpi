c93aa98ae458b12e397fb19159f558b801d3efe2
Stanislas <skita@teclib.com>
2023-11-15 14:36:09 +0100

    fix(NetworkName): fix form with insufficient rights
    closes #15990

--- src/NetworkName.php
+++ src/NetworkName.php
@@ -475,25 +475,25 @@ class NetworkName extends FQDNLabel
         );
         echo "</td>\n";
 
-        echo "</tr><tr class='tab_bg_1'>\n";
+        echo "</tr>";
 
-        echo "<td>" . IPAddress::getTypeName(Session::getPluralNumber());
-        IPAddress::showAddChildButtonForItemForm($name, 'NetworkName__ipaddresses');
-        echo "</td>";
-        echo "<td>";
-        IPAddress::showChildsForItemForm($name, 'NetworkName__ipaddresses');
-        echo "</td>";
+        if ($name->isNewItem()) {
+            $canedit = $name->canCreate();
+        } else {
+            $canedit = $name->can($name->getID(), UPDATE);
+        }
 
-       // MoYo : really need to display it here ?
-       // make confure because not updatable
-       // echo "<td>".IPNetwork::getTypeName(Session::getPluralNumber())."&nbsp;";
-       // Html::showToolTip(__('IP network is not included in the database. However, you can see current available networks.'));
-       // echo "</td><td>";
-       // IPNetwork::showIPNetworkProperties($name->getEntityID());
-       // echo "</td>\n";
-        echo "<td colspan='2'>&nbsp;</td>";
-
-        echo "</tr>\n";
+        if ($canedit) {
+            echo "<tr class='tab_bg_1'>\n";
+            echo "<td>" . IPAddress::getTypeName(Session::getPluralNumber());
+            IPAddress::showAddChildButtonForItemForm($name, 'NetworkName__ipaddresses', $canedit);
+            echo "</td>";
+            echo "<td>";
+            IPAddress::showChildsForItemForm($name, 'NetworkName__ipaddresses', $canedit);
+            echo "</td>";
+            echo "<td colspan='2'>&nbsp;</td>";
+            echo "</tr>\n";
+        }
     }
 
 
