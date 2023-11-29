bfbb43ac0f9a0f7e23e314254b0ef4c83ce8c074
Stanislas <skita@teclib.com>
2023-11-29 11:51:32 +0100

    Fix(Inventory): fix RuleImportAsset for Phone

--- src/Phone.php
+++ src/Phone.php
@@ -117,6 +117,7 @@ class Phone extends CommonDBTM
         $this->addStandardTab('Reservation', $ong, $options);
         $this->addStandardTab('Domain_Item', $ong, $options);
         $this->addStandardTab('Appliance_Item', $ong, $options);
+        $this->addStandardTab('RuleMatchedLog', $ong, $options);
         $this->addStandardTab('Log', $ong, $options);
 
         return $ong;
