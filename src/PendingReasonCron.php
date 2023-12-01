1c56a0156bd813e7d6e283bf5c6afc4c136041b4
Clément Cailly <ccailly@teclib.com>
2023-12-01 16:01:19 +0100

    fix: adding slashes to escape fields

--- src/PendingReasonCron.php
+++ src/PendingReasonCron.php
@@ -149,10 +149,10 @@ class PendingReasonCron extends CommonDBTM
                     'itemtype' => $item::getType(),
                     'items_id' => $item->getID(),
                     'pendingreasons_id' => $pending_reason->getID(),
-                    'name' => $pending_reason->fields['name'],
-                    'content' => ITILFollowupTemplate::getById(
+                    'name' => addslashes($pending_reason->fields['name']),
+                    'content' => addslashes(ITILFollowupTemplate::getById(
                         $pending_reason->fields['itilfollowuptemplates_id']
-                    )->getRenderedContent($item),
+                    )->getRenderedContent($item)),
                 ]);
                 $task->addVolume(1);
 
