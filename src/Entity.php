4cb0ea6199973f31a141965e745a415dc716fa55
AdrienClairembault <aclairembault@teclib.com>
2024-01-04 18:13:12 +0100

    Deprecate cleanEntitySelectorCache instead of deleting it

--- src/Entity.php
+++ src/Entity.php
@@ -4112,4 +4112,17 @@ class Entity extends CommonTreeDropdown
         }
         return null;
     }
+
+    /**
+     * Clean caches related to entity selector.
+     *
+     * @since 10.0
+     *
+     * @return void
+     * @deprecated 10.0.12
+     */
+    public function cleanEntitySelectorCache()
+    {
+        Toolbox::deprecated('Entity::cleanEntitySelectorCache no longer has any effect as the entity selector is no longer cached as a unique entry');
+    }
 }
