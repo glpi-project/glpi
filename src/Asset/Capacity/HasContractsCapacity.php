bf109c645aa027e974831cbcabc06cd5b2ecc680
AdrienClairembault <aclairembault@teclib.com>
2024-01-23 15:20:02 +0100

    Capacity: has contracts

---
+++ src/Asset/Capacity/HasContractsCapacity.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace Glpi\Asset\Capacity;
+
+use CommonGLPI;
+use Contract;
+use Contract_Item;
+
+class HasContractsCapacity extends AbstractCapacity
+{
+    // #Override
+    public function getLabel(): string
+    {
+        return Contract::getTypeName();
+    }
+
+    // #Override
+    public function onClassBootstrap(string $classname): void
+    {
+        /** @var array $CFG_GLPI */
+        global $CFG_GLPI;
+
+        // Allow our item to be linked to contracts
+        $CFG_GLPI['contract_types'][] = $classname;
+
+        // Register the contracts tab into our item
+        CommonGLPI::registerStandardTab(
+            $classname,
+            Contract_Item::class,
+            20 // Tab shouldn't be too far from the top
+        );
+    }
+
+    // #Override
+    public function onCapacityDisabled(string $classname): void
+    {
+        /** @var array $CFG_GLPI */
+        global $CFG_GLPI;
+
+        // Unregister from contracts types
+        $CFG_GLPI['contract_types'] = array_diff(
+            $CFG_GLPI['contract_types'],
+            [$classname]
+        );
+
+        // Delete related contract data
+        $item_os = new Contract_Item();
+        $item_os->deleteByCriteria(
+            crit   : ['itemtype' => $classname],
+            force  : true,
+            history: false
+        );
+
+        // Clean history related to contracts (both sides of the relation)
+        $this->deleteFieldsLogsByItemtypeLink($classname, [
+            Contract::getType(),
+        ]);
+        $this->deleteFieldsLogsByItemtypeLink(Contract::getType(), [
+            $classname,
+        ]);
+
+        // Clean display preferences
+        $this->deleteDisplayPreferences(
+            $classname,
+            Contract::rawSearchOptionsToAdd()
+        );
+    }
+}
