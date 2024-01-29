a31d57309acac2440a00597d57a11249dc0ffbd3
Cédric Anne <cedric.anne@gmail.com>
2024-01-29 11:59:29 +0100

    Enhance test

--- tests/functional/Glpi/Asset/Capacity/IsRackableCapacity.php
+++ tests/functional/Glpi/Asset/Capacity/IsRackableCapacity.php
@@ -38,7 +38,9 @@ namespace tests\units\Glpi\Asset\Capacity;
 use DbTestCase;
 use DisplayPreference;
 use Entity;
+use Item_Rack;
 use Log;
+use Rack;
 
 class IsRackableCapacity extends DbTestCase
 {
@@ -48,19 +50,11 @@ class IsRackableCapacity extends DbTestCase
 
         $root_entity_id = getItemByTypeName(\Entity::class, '_test_root_entity', true);
 
-        $superadmin_p_id = getItemByTypeName(\Profile::class, 'Super-Admin', true);
-        $profiles_matrix = [
-            $superadmin_p_id => [
-                READ   => 1,
-            ],
-        ];
-
         $definition_1 = $this->initAssetDefinition(
             capacities: [
                 \Glpi\Asset\Capacity\IsRackableCapacity::class,
                 \Glpi\Asset\Capacity\HasNotepadCapacity::class,
-            ],
-            profiles: $profiles_matrix
+            ]
         );
         $classname_1  = $definition_1->getAssetClassName();
         $definition_2 = $this->initAssetDefinition(
@@ -73,8 +67,7 @@ class IsRackableCapacity extends DbTestCase
             capacities: [
                 \Glpi\Asset\Capacity\IsRackableCapacity::class,
                 \Glpi\Asset\Capacity\HasHistoryCapacity::class,
-            ],
-            profiles: $profiles_matrix
+            ]
         );
         $classname_3  = $definition_3->getAssetClassName();
 
@@ -93,7 +86,10 @@ class IsRackableCapacity extends DbTestCase
 
             $item = $this->createItem($classname, ['name' => __FUNCTION__, 'entities_id' => $root_entity_id]);
 
-            $so_keys = [180, 181];
+            $so_keys = [
+                180, // Name
+                181, // Position
+            ];
             if ($has_capacity) {
                 $this->array($item->getOptions())->hasKeys($so_keys);
             } else {
@@ -145,7 +141,7 @@ class IsRackableCapacity extends DbTestCase
             'number_units' => 40
         ]);
         $rack_item_1 = $this->createItem(
-            \Item_Rack::class,
+            Item_Rack::class,
             [
                 'itemtype'     => $item_1::getType(),
                 'items_id'     => $item_1->getID(),
@@ -153,8 +149,9 @@ class IsRackableCapacity extends DbTestCase
                 'position'     => 1
             ]
         );
+        $this->updateItem(Item_Rack::class, $rack_item_1->getID(), ['position' => 11]);
         $rack_item_2 = $this->createItem(
-            \Item_Rack::class,
+            Item_Rack::class,
             [
                 'itemtype'     => $item_2::getType(),
                 'items_id'     => $item_2->getID(),
@@ -162,6 +159,7 @@ class IsRackableCapacity extends DbTestCase
                 'position'     => 2
             ]
         );
+        $this->updateItem(Item_Rack::class, $rack_item_2->getID(), ['position' => 12]);
         $displaypref_1   = $this->createItem(
             DisplayPreference::class,
             [
@@ -180,33 +178,59 @@ class IsRackableCapacity extends DbTestCase
         );
 
         $item_1_logs_criteria = [
-            'itemtype'      => $classname_1,
+            'OR' => [
+                [
+                    'itemtype'      => $classname_1,
+                    'itemtype_link' => ['LIKE', 'Item_Rack%'],
+                ],
+                [
+                    'itemtype'      => $classname_1,
+                    'itemtype_link' => Rack::class,
+                ],
+                [
+                    'itemtype'      => Rack::class,
+                    'itemtype_link' => $classname_1,
+                ],
+            ]
         ];
         $item_2_logs_criteria = [
-            'itemtype'      => $classname_2,
+            'OR' => [
+                [
+                    'itemtype'      => $classname_2,
+                    'itemtype_link' => ['LIKE', 'Item_Rack%'],
+                ],
+                [
+                    'itemtype'      => $classname_2,
+                    'itemtype_link' => Rack::class,
+                ],
+                [
+                    'itemtype'      => Rack::class,
+                    'itemtype_link' => $classname_2,
+                ],
+            ]
         ];
 
         // Ensure relation, display preferences and logs exists, and class is registered to global config
-        $this->object(\Item_Rack::getById($rack_item_1->getID()))->isInstanceOf(\Item_Rack::class);
+        $this->object(Item_Rack::getById($rack_item_1->getID()))->isInstanceOf(Item_Rack::class);
         $this->object(DisplayPreference::getById($displaypref_1->getID()))->isInstanceOf(DisplayPreference::class);
-        $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(2); //create + add volume
-        $this->object(\Item_Rack::getById($rack_item_2->getID()))->isInstanceOf(\Item_Rack::class);
+        $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(3); // create + link + update
+        $this->object(Item_Rack::getById($rack_item_2->getID()))->isInstanceOf(Item_Rack::class);
         $this->object(DisplayPreference::getById($displaypref_2->getID()))->isInstanceOf(DisplayPreference::class);
-        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(2); //create + add volume
+        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(3); // create + link + update
         $this->array($CFG_GLPI['rackable_types'])->contains($classname_1);
         $this->array($CFG_GLPI['rackable_types'])->contains($classname_2);
 
         // Disable capacity and check that relations have been cleaned, and class is unregistered from global config
         $this->boolean($definition_1->update(['id' => $definition_1->getID(), 'capacities' => []]))->isTrue();
-        $this->boolean(\Item_Rack::getById($rack_item_1->getID()))->isFalse();
+        $this->boolean(Item_Rack::getById($rack_item_1->getID()))->isFalse();
         $this->boolean(DisplayPreference::getById($displaypref_1->getID()))->isFalse();
         $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(0);
         $this->array($CFG_GLPI['rackable_types'])->notContains($classname_1);
 
         // Ensure relations, logs and global registration are preserved for other definition
-        $this->object(\Item_Rack::getById($rack_item_2->getID()))->isInstanceOf(\Item_Rack::class);
+        $this->object(Item_Rack::getById($rack_item_2->getID()))->isInstanceOf(Item_Rack::class);
         $this->object(DisplayPreference::getById($displaypref_2->getID()))->isInstanceOf(DisplayPreference::class);
-        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(2);
+        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(3);
         $this->array($CFG_GLPI['rackable_types'])->contains($classname_2);
     }
 }
