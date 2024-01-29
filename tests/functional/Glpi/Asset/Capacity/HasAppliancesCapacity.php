f10b5472affff522ac4bd734c95a9b4b650a88f8
Curtis Conard <cconard96@gmail.com>
2024-01-29 09:55:39 -0500

    Add Appliances Capacity

--- tests/functional/Glpi/Asset/Capacity/HasAppliancesCapacity.php
+++ tests/functional/Glpi/Asset/Capacity/HasAppliancesCapacity.php
@@ -35,24 +35,35 @@
 
 namespace tests\units\Glpi\Asset\Capacity;
 
+use Appliance;
+use Appliance_Item;
 use DbTestCase;
 use DisplayPreference;
 use Entity;
 use Log;
+use Profile;
 
-class IsReservableCapacity extends DbTestCase
+class HasAppliancesCapacity extends DbTestCase
 {
     public function testCapacityActivation(): void
     {
         global $CFG_GLPI;
 
-        $root_entity_id = getItemByTypeName(\Entity::class, '_test_root_entity', true);
+        $root_entity_id = getItemByTypeName(Entity::class, '_test_root_entity', true);
+
+        $superadmin_p_id = getItemByTypeName(Profile::class, 'Super-Admin', true);
+        $profiles_matrix = [
+            $superadmin_p_id => [
+                READ   => 1,
+            ],
+        ];
 
         $definition_1 = $this->initAssetDefinition(
             capacities: [
-                \Glpi\Asset\Capacity\IsReservableCapacity::class,
+                \Glpi\Asset\Capacity\HasAppliancesCapacity::class,
                 \Glpi\Asset\Capacity\HasNotepadCapacity::class,
-            ]
+            ],
+            profiles: $profiles_matrix
         );
         $classname_1  = $definition_1->getAssetClassName();
         $definition_2 = $this->initAssetDefinition(
@@ -63,9 +74,10 @@ class IsReservableCapacity extends DbTestCase
         $classname_2  = $definition_2->getAssetClassName();
         $definition_3 = $this->initAssetDefinition(
             capacities: [
-                \Glpi\Asset\Capacity\IsReservableCapacity::class,
+                \Glpi\Asset\Capacity\HasAppliancesCapacity::class,
                 \Glpi\Asset\Capacity\HasHistoryCapacity::class,
-            ]
+            ],
+            profiles: $profiles_matrix
         );
         $classname_3  = $definition_3->getAssetClassName();
 
@@ -77,23 +89,31 @@ class IsReservableCapacity extends DbTestCase
 
         foreach ($has_capacity_mapping as $classname => $has_capacity) {
             // Check that the class is globally registered
+            $this->login(); // must be logged in to have class in Appliance::getTypes()
             if ($has_capacity) {
-                $this->array($CFG_GLPI['reservation_types'])->contains($classname);
+                $this->array($CFG_GLPI['appliance_types'])->contains($classname);
+                $this->array(Appliance::getTypes())->contains($classname);
             } else {
-                $this->array($CFG_GLPI['reservation_types'])->notContains($classname);
+                $this->array($CFG_GLPI['appliance_types'])->notContains($classname);
+                $this->array(Appliance::getTypes())->notContains($classname);
             }
 
             // Check that the corresponding tab is present on items
             $item = $this->createItem($classname, ['name' => __FUNCTION__, 'entities_id' => $root_entity_id]);
             $this->login(); // must be logged in to get tabs list
             if ($has_capacity) {
-                $this->array($item->defineAllTabs())->hasKey('Reservation$1');
+                $this->array($item->defineAllTabs())->hasKey('Appliance_Item$1');
             } else {
-                $this->array($item->defineAllTabs())->notHasKey('Reservation$1');
+                $this->array($item->defineAllTabs())->notHasKey('Appliance_Item$1');
             }
 
             // Check that the related search options are available
-            $so_keys = [81];
+            $so_keys = [
+                1210, // Name
+                1211, // Type
+                1212, // User
+                1213, // Group
+            ];
             if ($has_capacity) {
                 $this->array($item->getOptions())->hasKeys($so_keys);
             } else {
@@ -111,14 +131,14 @@ class IsReservableCapacity extends DbTestCase
 
         $definition_1 = $this->initAssetDefinition(
             capacities: [
-                \Glpi\Asset\Capacity\IsReservableCapacity::class,
+                \Glpi\Asset\Capacity\HasAppliancesCapacity::class,
                 \Glpi\Asset\Capacity\HasHistoryCapacity::class,
             ]
         );
         $classname_1  = $definition_1->getAssetClassName();
         $definition_2 = $this->initAssetDefinition(
             capacities: [
-                \Glpi\Asset\Capacity\IsReservableCapacity::class,
+                \Glpi\Asset\Capacity\HasAppliancesCapacity::class,
                 \Glpi\Asset\Capacity\HasHistoryCapacity::class,
             ]
         );
@@ -139,25 +159,35 @@ class IsReservableCapacity extends DbTestCase
             ]
         );
 
-        $reservation_item_1 = $this->createItem(
-            \ReservationItem::class,
+        $appliance = $this->createItem(
+            Appliance::class,
+            [
+                'name' => __FUNCTION__,
+                'entities_id' => $root_entity_id,
+            ]
+        );
+
+        $appliance_item_1 = $this->createItem(
+            Appliance_Item::class,
             [
                 'itemtype'     => $item_1::class,
                 'items_id'     => $item_1->getID(),
+                'appliances_id' => $appliance->getID(),
             ]
         );
-        $reservation_item_2 = $this->createItem(
-            \ReservationItem::class,
+        $appliance_item_2 = $this->createItem(
+            Appliance_Item::class,
             [
                 'itemtype'     => $item_2::class,
                 'items_id'     => $item_2->getID(),
+                'appliances_id' => $appliance->getID(),
             ]
         );
         $displaypref_1   = $this->createItem(
             DisplayPreference::class,
             [
                 'itemtype' => $classname_1,
-                'num'      => 81, // Reservable
+                'num'      => 1210, //Appliance name
                 'users_id' => 0,
             ]
         );
@@ -165,7 +195,7 @@ class IsReservableCapacity extends DbTestCase
             DisplayPreference::class,
             [
                 'itemtype' => $classname_2,
-                'num'      => 81, // Reservable
+                'num'      => 1210, //Appliance name
                 'users_id' => 0,
             ]
         );
@@ -178,26 +208,26 @@ class IsReservableCapacity extends DbTestCase
         ];
 
         // Ensure relation, display preferences and logs exists, and class is registered to global config
-        $this->object(\ReservationItem::getById($reservation_item_1->getID()))->isInstanceOf(\ReservationItem::class);
+        $this->object(\Appliance_Item::getById($appliance_item_1->getID()))->isInstanceOf(\Appliance_Item::class);
         $this->object(DisplayPreference::getById($displaypref_1->getID()))->isInstanceOf(DisplayPreference::class);
-        $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(2); //create + add volume
-        $this->object(\ReservationItem::getById($reservation_item_2->getID()))->isInstanceOf(\ReservationItem::class);
+        $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(2); //create + add appliance
+        $this->object(\Appliance_Item::getById($appliance_item_2->getID()))->isInstanceOf(\Appliance_Item::class);
         $this->object(DisplayPreference::getById($displaypref_2->getID()))->isInstanceOf(DisplayPreference::class);
-        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(2); //create + add volume
-        $this->array($CFG_GLPI['reservation_types'])->contains($classname_1);
-        $this->array($CFG_GLPI['reservation_types'])->contains($classname_2);
+        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(2); //create + add appliance
+        $this->array($CFG_GLPI['appliance_types'])->contains($classname_1);
+        $this->array($CFG_GLPI['appliance_types'])->contains($classname_2);
 
         // Disable capacity and check that relations have been cleaned, and class is unregistered from global config
         $this->boolean($definition_1->update(['id' => $definition_1->getID(), 'capacities' => []]))->isTrue();
-        $this->boolean(\ReservationItem::getById($reservation_item_1->getID()))->isFalse();
+        $this->boolean(\Appliance_Item::getById($appliance_item_1->getID()))->isFalse();
         $this->boolean(DisplayPreference::getById($displaypref_1->getID()))->isFalse();
         $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(0);
-        $this->array($CFG_GLPI['reservation_types'])->notContains($classname_1);
+        $this->array($CFG_GLPI['appliance_types'])->notContains($classname_1);
 
         // Ensure relations, logs and global registration are preserved for other definition
-        $this->object(\ReservationItem::getById($reservation_item_2->getID()))->isInstanceOf(\ReservationItem::class);
+        $this->object(\Appliance_Item::getById($appliance_item_2->getID()))->isInstanceOf(\Appliance_Item::class);
         $this->object(DisplayPreference::getById($displaypref_2->getID()))->isInstanceOf(DisplayPreference::class);
         $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(2);
-        $this->array($CFG_GLPI['reservation_types'])->contains($classname_2);
+        $this->array($CFG_GLPI['appliance_types'])->contains($classname_2);
     }
 }
