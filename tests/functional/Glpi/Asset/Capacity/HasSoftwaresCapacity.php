4cbdd426965568e304160146203d26b41e6cb3e2
Romain Lecouvreur <lecouvreurromain@gmail.com>
2024-01-31 14:33:53 +0100

    Add unit tests

---
+++ tests/functional/Glpi/Asset/Capacity/HasSoftwaresCapacity.php
@@ -0,0 +1,225 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace tests\units\Glpi\Asset\Capacity;
+
+use DbTestCase;
+use DisplayPreference;
+use Entity;
+use Item_SoftwareVersion;
+use Log;
+use Software;
+use SoftwareVersion;
+
+class HasSoftwaresCapacity extends DbTestCase
+{
+    public function testCapacityActivation(): void
+    {
+        global $CFG_GLPI;
+
+        $root_entity_id = getItemByTypeName(Entity::class, '_test_root_entity', true);
+
+        $superadmin_p_id = getItemByTypeName(\Profile::class, 'Super-Admin', true);
+        $profiles_matrix = [
+            $superadmin_p_id => [
+                READ   => 1,
+                CREATE => 1,
+            ],
+        ];
+
+        $definition_1 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\HasSoftwaresCapacity::class,
+                \Glpi\Asset\Capacity\HasSoftwaresCapacity::class,
+            ],
+            profiles: $profiles_matrix
+        );
+        $classname_1  = $definition_1->getAssetClassName();
+        $definition_2 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\HasHistoryCapacity::class,
+            ]
+        );
+        $classname_2  = $definition_2->getAssetClassName();
+        $definition_3 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\HasSoftwaresCapacity::class,
+                \Glpi\Asset\Capacity\HasHistoryCapacity::class,
+            ],
+            profiles: $profiles_matrix
+        );
+        $classname_3  = $definition_3->getAssetClassName();
+
+        $has_capacity_mapping = [
+            $classname_1 => true,
+            $classname_2 => false,
+            $classname_3 => true,
+        ];
+
+        foreach ($has_capacity_mapping as $classname => $has_capacity) {
+            // Check that the class is globally registered
+            if ($has_capacity) {
+                $this->array($CFG_GLPI['software_types'])->contains($classname);
+            } else {
+                $this->array($CFG_GLPI['software_types'])->notContains($classname);
+            }
+
+            // Check that the corresponding tab is present on items
+            $item = $this->createItem($classname, ['name' => __FUNCTION__, 'entities_id' => $root_entity_id]);
+            $this->login(); // must be logged in to get tabs list
+            if ($has_capacity) {
+                $this->array($item->defineAllTabs())->hasKey('Item_SoftwareVersion$1');
+            } else {
+                $this->array($item->defineAllTabs())->notHasKey('Item_SoftwareVersion$1');
+            }
+        }
+    }
+
+    public function testCapacityDeactivation(): void
+    {
+        /** @var array $CFG_GLPI */
+        global $CFG_GLPI;
+
+        $root_entity_id = getItemByTypeName(Entity::class, '_test_root_entity', true);
+
+        $superadmin_p_id = getItemByTypeName(\Profile::class, 'Super-Admin', true);
+        $profiles_matrix = [
+            $superadmin_p_id => [
+                READ   => 1,
+                CREATE => 1,
+            ],
+        ];
+
+        $definition_1 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\HasDomainsCapacity::class,
+                \Glpi\Asset\Capacity\HasSoftwaresCapacity::class,
+            ],
+            profiles: $profiles_matrix
+        );
+        $classname_1  = $definition_1->getAssetClassName();
+        $definition_2 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\HasDomainsCapacity::class,
+                \Glpi\Asset\Capacity\HasSoftwaresCapacity::class,
+            ],
+            profiles: $profiles_matrix
+        );
+        $classname_2  = $definition_2->getAssetClassName();
+
+        $item_1          = $this->createItem(
+            $classname_1,
+            [
+                'name' => __FUNCTION__,
+                'entities_id' => $root_entity_id,
+            ]
+        );
+        $item_2          = $this->createItem(
+            $classname_2,
+            [
+                'name' => __FUNCTION__,
+                'entities_id' => $root_entity_id,
+            ]
+        );
+
+        $software = $this->createItem(
+            Software::class,
+            [
+                'name' => 'Software',
+                'entities_id' => $root_entity_id,
+            ]
+        );
+        $software_version = $this->createItem(
+            SoftwareVersion::class,
+            [
+                'name' => 'V1.0',
+                'entities_id' => $root_entity_id,
+                'softwares_id' => $software->getID(),
+            ]
+        );
+        $item_software_1 = $this->createItem(
+            Item_SoftwareVersion::class,
+            [
+                'entities_id' => $root_entity_id,
+                'softwareversions_id' => $software_version->getID(),
+                'itemtype' => $item_1->getType(),
+                'items_id' => $item_1->getID(),
+            ]
+        );
+        $item_software_2 = $this->createItem(
+            Item_SoftwareVersion::class,
+            [
+                'entities_id' => $root_entity_id,
+                'softwareversions_id' => $software_version->getID(),
+                'itemtype' => $item_2->getType(),
+                'items_id' => $item_2->getID(),
+            ]
+        );
+
+        $displaypref_1   = $this->createItem(
+            DisplayPreference::class,
+            [
+                'itemtype' => $classname_1,
+                'users_id' => 0,
+            ]
+        );
+        $displaypref_2   = $this->createItem(
+            DisplayPreference::class,
+            [
+                'itemtype' => $classname_2,
+                'users_id' => 0,
+            ]
+        );
+
+        // Ensure relation, display preferences and logs exists, and class is registered to global config
+        $this->object(Item_SoftwareVersion::getById($item_software_1->getID()))->isInstanceOf(Item_SoftwareVersion::class);
+        $this->object(DisplayPreference::getById($displaypref_1->getID()))->isInstanceOf(DisplayPreference::class);
+        $this->object(Item_SoftwareVersion::getById($item_software_2->getID()))->isInstanceOf(Item_SoftwareVersion::class);
+        $this->object(DisplayPreference::getById($displaypref_2->getID()))->isInstanceOf(DisplayPreference::class);
+        //$this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(3);
+        $this->array($CFG_GLPI['software_types'])->contains($classname_1);
+        $this->array($CFG_GLPI['software_types'])->contains($classname_2);
+
+        // Disable capacity and check that relations have been cleaned, and class is unregistered from global config
+        $this->boolean($definition_1->update(['id' => $definition_1->getID(), 'capacities' => []]))->isTrue();
+        $this->boolean(Item_SoftwareVersion::getById($item_software_1->getID()))->isFalse();
+        $this->array($CFG_GLPI['software_types'])->notContains($classname_1);
+
+        // Ensure relations, logs and global registration are preserved for other definition
+        $this->object(Item_SoftwareVersion::getById($item_software_2->getID()))->isInstanceOf(Item_SoftwareVersion::class);
+        $this->object(DisplayPreference::getById($displaypref_2->getID()))->isInstanceOf(DisplayPreference::class);
+        $this->array($CFG_GLPI['software_types'])->contains($classname_2);
+    }
+}
