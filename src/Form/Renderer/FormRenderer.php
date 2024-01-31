6e8c1e8cf0a16f11c5805bffb643c58fd3b40b88
AdrienClairembault <aclairembault@teclib.com>
2024-01-31 14:34:38 +0100

    Use consistent name for all files

--- src/Form/Renderer/FormRenderer.php
+++ src/Form/Renderer/FormRenderer.php
@@ -53,7 +53,7 @@ final class FormRenderer
     public function render(Form $form): string
     {
         $twig = TemplateRenderer::getInstance();
-        return $twig->render('pages/admin/form/render_form.html.twig', [
+        return $twig->render('pages/admin/form/form_renderer.html.twig', [
             'form' => $form,
         ]);
     }
