837e0069798c6e901f109251060c97e6cf2d1ada
Curtis Conard <cconard96@gmail.com>
2024-02-10 10:50:34 -0500

    Use HL API to handle old status.php path

--- status.php
+++ status.php
@@ -33,42 +33,27 @@
  * ---------------------------------------------------------------------
  */
 
-use Glpi\System\Status\StatusChecker;
+use Glpi\Api\HL\Router;
+use Glpi\Application\ErrorHandler;
+use Glpi\Http\Request;
+use Glpi\Http\Response;
 
 include('./inc/includes.php');
 
 // Force in normal mode
 $_SESSION['glpi_use_mode'] = Session::NORMAL_MODE;
 
-// Need to be used using :
+// Example usage with Nagios check_http plugin:
 // check_http -H servername -u /glpi/status.php -s GLPI_OK
 
-$valid_response_types = ['text/plain', 'application/json'];
-$fallback_response_type = 'text/plain';
+// Redirect handling to the High-Level API (we may eventually remove this script)
 
-if (!isset($_SERVER['HTTP_ACCEPT']) || !in_array($_SERVER['HTTP_ACCEPT'], $valid_response_types, true)) {
-    $_SERVER['HTTP_ACCEPT'] = $fallback_response_type;
-}
-
-$format = $_SERVER['HTTP_ACCEPT'];
-if (isset($_REQUEST['format'])) {
-    switch ($_REQUEST['format']) {
-        case 'json':
-            $format = 'application/json';
-            break;
-        case 'plain':
-            $format = 'text/plain';
-            break;
-    }
-}
-
-if ($format === 'text/plain') {
-    Toolbox::deprecated('Plain-text status output is deprecated please use the JSON format instead by specifically setting the Accept header to "application/json". In the future, JSON output will be the default.');
-}
-header('Content-type: ' . $format);
-
-if ($format === 'application/json') {
-    echo json_encode(StatusChecker::getServiceStatus($_REQUEST['service'] ?? null, true, true));
-} else {
-    echo StatusChecker::getServiceStatus($_REQUEST['service'] ?? null, true, false);
+$request = new Request('GET', '/Status/All', getallheaders() ?? []);
+try {
+    $response = Router::getInstance()->handleRequest($request);
+    $response->send();
+} catch (\Throwable $e) {
+    ErrorHandler::getInstance()->handleException($e);
+    $response = new Response(500);
+    $response->send();
 }
