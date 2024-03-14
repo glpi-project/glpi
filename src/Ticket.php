a1a30222508b25739e72eab6ea3824d0bda58417
Cédric Anne <cedric.anne@gmail.com>
2024-03-14 09:50:11 +0100

    Revert "Fixed ticket creation with closed status to exclude technician"
    This reverts commit 09406faa84a24a220da26021a63f8fc4bfe31352.

--- src/Ticket.php
+++ src/Ticket.php
@@ -188,7 +188,6 @@ class Ticket extends CommonITILObject
         if (
             isset($this->fields['is_deleted']) && ($this->fields['is_deleted'] == 1)
             || isset($this->fields['status']) && in_array($this->fields['status'], $this->getClosedStatusArray())
-            || isset($this->input['status']) && in_array($this->input['status'], $this->getClosedStatusArray())
         ) {
             return false;
         }
