4e37294c9f44af7f47fbe4468b1ce5b555453594
Cédric Anne <cedric.anne@gmail.com>
2024-03-26 16:37:42 +0100

    Always store updated SMTP Oauth refresh token
    fixes !31976

---
+++ src/Mail/SMTP/OAuthTokenProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace Glpi\Mail\SMTP;
+
+use PHPMailer\PHPMailer\OAuth;
+use League\OAuth2\Client\Token\AccessToken;
+
+final class OAuthTokenProvider extends OAuth
+{
+    /**
+     * Get the granted access token.
+     *
+     * @return AccessToken|null
+     */
+    public function getOauthToken(): ?AccessToken
+    {
+        return $this->oauthToken;
+    }
+}
