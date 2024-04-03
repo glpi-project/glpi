604b082cc1afd686b62ebc7519902efc63093537
Cédric Anne <cedric.anne@gmail.com>
2024-04-03 10:13:27 +0200

    Replace usage of obsolete `docker-compose` method

--- .github/actions/init_initialize-9.5-db.sh
+++ .github/actions/init_initialize-9.5-db.sh
@@ -3,7 +3,7 @@ set -e -u -x -o pipefail
 
 ROOT_DIR=$(readlink -f "$(dirname $0)/../..")
 
-docker-compose exec -T db mysql --user=root --execute="DROP DATABASE IF EXISTS \`glpitest-9.5\`;"
-docker-compose exec -T db mysql --user=root --execute="CREATE DATABASE \`glpitest-9.5\`;"
-cat $ROOT_DIR/install/mysql/glpi-9.5.13-empty.sql | docker-compose exec -T db mysql --user=root glpitest-9.5
-cat $ROOT_DIR/tests/glpi-9.5-data.sql | docker-compose exec -T db mysql --user=root glpitest-9.5
+docker compose exec -T db mysql --user=root --execute="DROP DATABASE IF EXISTS \`glpitest-9.5\`;"
+docker compose exec -T db mysql --user=root --execute="CREATE DATABASE \`glpitest-9.5\`;"
+cat $ROOT_DIR/install/mysql/glpi-9.5.13-empty.sql | docker compose exec -T db mysql --user=root glpitest-9.5
+cat $ROOT_DIR/tests/glpi-9.5-data.sql | docker compose exec -T db mysql --user=root glpitest-9.5
--- .github/actions/init_initialize-imap-fixtures.sh
+++ .github/actions/init_initialize-imap-fixtures.sh
@@ -3,8 +3,8 @@ set -e -u -x -o pipefail
 
 ROOT_DIR=$(readlink -f "$(dirname $0)/../..")
 
-docker-compose exec -T --user root dovecot doveadm expunge -u glpi mailbox 'INBOX' all
-docker-compose exec -T --user root dovecot doveadm purge -u glpi
+docker compose exec -T --user root dovecot doveadm expunge -u glpi mailbox 'INBOX' all
+docker compose exec -T --user root dovecot doveadm purge -u glpi
 for f in `ls $ROOT_DIR/tests/emails-tests/*.eml`; do
-  cat $f | docker-compose exec -T --user glpi dovecot getmail_maildir /home/glpi/Maildir/
+  cat $f | docker compose exec -T --user glpi dovecot getmail_maildir /home/glpi/Maildir/
 done
