1da0804db658e1d76d84925a0e3228c3cd3608fe
Cédric Anne <cedric.anne@gmail.com>
2024-04-17 13:07:15 +0200

    merge ajax scripts

--- ajax/ticketiteminformation.php
+++ ajax/ticketiteminformation.php
@@ -41,5 +41,49 @@ if (strpos($_SERVER['PHP_SELF'], "ticketiteminformation.php")) {
     Html::header_nocache();
 }
 
-$obj = new Ticket();
-include(GLPI_ROOT . '/ajax/commonitilobjectiteminformation.php');
+if (isset($_POST["my_items"]) && !empty($_POST["my_items"])) {
+    $splitter = explode("_", $_POST["my_items"]);
+    if (count($splitter) == 2) {
+        $_POST["itemtype"] = $splitter[0];
+        $_POST["items_id"] = $splitter[1];
+    }
+}
+
+if (
+    isset($_POST['itemtype'])
+    && isset($_POST['items_id']) && ($_POST['items_id'] > 0)
+) {
+   // Security
+    if (!class_exists($_POST['itemtype'])) {
+        exit();
+    }
+
+    $days   = 3;
+
+    $ticket = new Ticket();
+    $data   = $ticket->getActiveOrSolvedLastDaysForItem(
+        $_POST['itemtype'],
+        $_POST['items_id'],
+        $days
+    );
+
+    $nb = count($data);
+    $badge_helper = sprintf(
+        _n(
+            '%s ticket in progress or recently solved on this item.',
+            '%s tickets in progress or recently solved on this item.',
+            $nb
+        ),
+        $nb
+    );
+    echo "<span class='badge badge-secondary' title='$badge_helper'>$nb</span>";
+
+    if ($nb) {
+        $content = '';
+        foreach ($data as $title) {
+            $content .= $title . '<br>';
+        }
+        echo '&nbsp;';
+        Html::showToolTip($content);
+    }
+}
