33ad98f5cce393c0fc3523f7eac6da4a0822f259
Romain B <8530352+Rom1-B@users.noreply.github.com>
2024-04-22 10:00:18 +0200

    fix(contract): expiration date in search (#16935)
    * fix(contract): expiration date in search
    
    * add test
    
    * lint
    
    * enhance test
    
    * glpi_currenttime

--- tests/functional/Contract.php
+++ tests/functional/Contract.php
@@ -99,4 +99,77 @@ class Contract extends DbTestCase
             )->isIdenticalTo(1, 'Missing relation with ' . $rel_class);
         }
     }
+
+    public function getSpecificValueToDisplayProvider()
+    {
+        $this->login();
+        $_SESSION['glpi_currenttime'] = '2024-04-22 10:00:00';
+        return [
+            [
+                'field' => '_virtual_expiration',
+                'values' => [
+                    'begin_date' => '2020-01-01',
+                    'duration' => 6,
+                    'renewal' => \Contract::RENEWAL_NEVER
+                ],
+                'expected' => "<span class='red'>2020-07-01</span>"
+            ],
+            [
+                'field' => '_virtual_expiration',
+                'values' => [
+                    'begin_date' => '2020-01-01',
+                    'duration' => 6,
+                    'renewal' => \Contract::RENEWAL_TACIT
+                ],
+                'expected' => "2024-07-01"
+            ],
+            [
+                'field' => '_virtual_expiration',
+                'values' => [
+                    'begin_date' => '2020-01-01',
+                    'duration' => 6,
+                    'renewal' => \Contract::RENEWAL_EXPRESS
+                ],
+                'expected' => "<span class='red'>2020-07-01</span>"
+            ],
+            [
+                'field' => '_virtual_expiration',
+                'values' => [
+                    'begin_date' => '2025-01-01',
+                    'duration' => 6,
+                    'renewal' => \Contract::RENEWAL_NEVER
+                ],
+                'expected' => '2025-07-01'
+            ],
+            [
+                'field' => '_virtual_expiration',
+                'values' => [
+                    'begin_date' => '2025-01-01',
+                    'duration' => 6,
+                    'renewal' => \Contract::RENEWAL_TACIT
+                ],
+                'expected' => '2025-07-01'
+            ],
+            [
+                'field' => '_virtual_expiration',
+                'values' => [
+                    'begin_date' => '2025-01-01',
+                    'duration' => 6,
+                    'renewal' => \Contract::RENEWAL_EXPRESS
+                ],
+                'expected' => '2025-07-01'
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getSpecificValueToDisplayProvider
+     */
+    public function testGetSpecificValueToDisplay($field, $values, $expected)
+    {
+        $this->login();
+        $this->setEntity('_test_root_entity', true);
+        $contract = new \Contract();
+        $this->string($contract->getSpecificValueToDisplay($field, $values))->isEqualTo($expected);
+    }
 }
