3b8b759598f9684ca504968251352a9c7ae6de13
Curtis Conard <cconard96@gmail.com>
2024-04-23 17:06:43 -0400

    Remove bad HTML sanitization

--- js/src/vue/Debug/Widget/SQLRequests.vue
+++ js/src/vue/Debug/Widget/SQLRequests.vue
@@ -60,7 +60,7 @@
                     request_id: request_id,
                     num: query['num'],
                     time: query['time'],
-                    query: escapeMarkupText(query['query']),
+                    query: query['query'],
                     rows: query['rows'],
                     warnings: escapeMarkupText(query['warnings']),
                     errors: escapeMarkupText(query['errors']),
