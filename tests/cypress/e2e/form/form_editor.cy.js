5d34fae0dd434869aed687142e89c8d13d6fb739
AdrienClairembault <aclairembault@teclib.com>
2024-05-15 11:40:28 +0200

    Add E2E tests for form creation

---
+++ tests/cypress/e2e/form/form_editor.cy.js
@@ -0,0 +1,74 @@
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+describe ('Form editor', () => {
+    beforeEach(() => {
+        cy.login();
+        cy.changeProfile('Super-Admin', true);
+    });
+    it('can create a form and fill its main details', () => {
+        // Go to form creation page
+        cy.visit('/front/form/form.php');
+        cy.findByRole('link', {'name': 'Add'}).click();
+
+        // Edit form details
+        cy.focused().type("My form name"); // Form name is focused by default
+        cy.findByRole('region', {'name': 'Form details'}).within(() => {
+            cy.findByRole('checkbox', {'name': 'Active'})
+                .should('not.to.be.checked')
+                .check()
+            ;
+
+            cy.findByLabelText("Form description")
+                .awaitTinyMCE()
+                .type("My form description")
+            ;
+        });
+
+        // Save form and reload page to force new data to be displayed.
+        cy.findByRole('button', {'name': 'Add'}).click();
+        cy.findByRole('alert').should('contain.text', 'Item successfully updated');
+        cy.reload();
+
+        // Validate that the new values are displayed
+        cy.findByRole('region', {'name': 'Form details'}).within(() => {
+            cy.findByRole('textbox', {'name': 'Form name'})
+                .should('have.value', 'My form name')
+            ;
+            cy.findByLabelText("Form description")
+                .awaitTinyMCE()
+                .should('have.text', 'My form description')
+            ;
+        });
+    });
+});
