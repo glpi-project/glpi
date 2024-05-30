b42afc25e0ace64b7c50afe8a58057a8348f54a1
Rom1-B <rbiot@teclib.com>
2024-05-30 14:43:27 +0200

    hide checkbox

--- src/Group_User.php
+++ src/Group_User.php
@@ -612,7 +612,12 @@ class Group_User extends CommonDBRelation
                 echo "\n<tr class='tab_bg_" . ($user->isDeleted() ? '1_2' : '1') . "'>";
                 if ($canedit) {
                     echo "<td width='10'>";
-                    Html::showMassiveActionCheckBox(__CLASS__, $data["linkid"]);
+                    if (
+                        Session::canViewAllEntities() 
+                        || Session::haveAccessToOneOfEntities(Profile_User::getUserEntities($data["id"], false))
+                    ) {
+                        Html::showMassiveActionCheckBox(__CLASS__, $data["linkid"]);
+                    }
                     echo "</td>";
                 }
                 echo "<td>" . $user->getLink();
@@ -682,7 +687,6 @@ class Group_User extends CommonDBRelation
             'condition' => [
                 'is_usergroup' => 1,
             ] + getEntitiesRestrictCriteria(Group::getTable(), '', '', true)
-            + getEntitiesRestrictCriteria(User::getTable(), '', '', true)
         ];
 
        // Define normalized action for add_item and remove_item
