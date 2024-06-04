aa0d44d345960b9f77f5ea45b9982d048e8af5ce
Romain B <8530352+Rom1-B@users.noreply.github.com>
2024-06-04 11:36:09 +0200

    fix(group): fix massive action checkbox visibility

--- src/Group_User.php
+++ src/Group_User.php
@@ -612,7 +612,9 @@ class Group_User extends CommonDBRelation
                 echo "\n<tr class='tab_bg_" . ($user->isDeleted() ? '1_2' : '1') . "'>";
                 if ($canedit) {
                     echo "<td width='10'>";
-                    Html::showMassiveActionCheckBox(__CLASS__, $data["linkid"]);
+                    if ($user->canUpdateItem()) {
+                        Html::showMassiveActionCheckBox(__CLASS__, $data["linkid"]);
+                    }
                     echo "</td>";
                 }
                 echo "<td>" . $user->getLink();
