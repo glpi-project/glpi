f28f0fbef377ce905e7ad4625400e2b5624b4e8d
Johan Cwiklinski <johan@x-tnd.be>
2024-06-05 15:21:35 +0200

    Keep only one directory for tests

--- tests/functional/CronTask.php
+++ tests/functional/CronTask.php
@@ -41,6 +41,68 @@ use DbTestCase;
 
 class CronTask extends DbTestCase
 {
+    public function testCronTemp()
+    {
+
+       //create some files
+        $Data = [
+            [
+                'name'    => GLPI_TMP_DIR . '/recent_file.txt',
+                'content' => 'content1',
+            ],
+            [
+                'name'    => GLPI_TMP_DIR . '/file1.txt',
+                'content' => 'content1',
+            ],
+            [
+                'name'    => GLPI_TMP_DIR . '/file2.txt',
+                'content' => 'content2',
+            ],
+            [
+                'name'    => GLPI_TMP_DIR . '/auto_orient/file3.txt',
+                'content' => 'content3',
+            ],
+            [
+                'name'    => GLPI_TMP_DIR . '/auto_orient/file4.txt',
+                'content' => 'content4',
+            ]
+        ];
+
+       //create auto_orient directory
+        if (!file_exists(GLPI_TMP_DIR . '/auto_orient/')) {
+            mkdir(GLPI_TMP_DIR . '/auto_orient/', 0755, true);
+        }
+
+        foreach ($Data as $Row) {
+            $file = fopen($Row['name'], 'c');
+            fwrite($file, $Row['content']);
+            fclose($file);
+
+           //change filemtime (except recent_file.txt)
+            if ($Row['name'] != GLPI_TMP_DIR . '/recent_file.txt') {
+                touch($Row['name'], time() - (HOUR_TIMESTAMP * 2));
+            }
+        }
+
+       // launch Cron for cleaning _tmp directory
+        $mode = - \CronTask::MODE_EXTERNAL; // force
+        \CronTask::launch($mode, 5, 'temp');
+
+        $nb_file = $this->getFileCountRecursively(GLPI_TMP_DIR);
+        $this->variable($nb_file)->isEqualTo(1); //recent_file.txt
+    }
+
+
+    public function getFileCountRecursively($path)
+    {
+
+        $dir = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator(
+            $dir,
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        return iterator_count($files);
+    }
     protected function registerProvider()
     {
         return [
