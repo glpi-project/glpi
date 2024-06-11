d2cfc92db67235bc1767524eb0c15b72f1c2539f
Clément Cailly <ccailly@teclib.com>
2024-06-11 09:55:56 +0200

    feat(forms): Convert default values when question type change

--- src/Glpi/Form/QuestionType/QuestionTypeLongText.php
+++ src/Glpi/Form/QuestionType/QuestionTypeLongText.php
@@ -45,6 +45,38 @@ use Override;
 final class QuestionTypeLongText extends AbstractQuestionType
 {
     #[Override]
+    public function getFormEditorJsOptions(): string
+    {
+        return <<<JS
+            {
+                "extractDefaultValue": function (question) {
+                    const textarea = question.find('[data-glpi-form-editor-question-type-specific]')
+                        .find('[name="default_value"], [data-glpi-form-editor-original-name="default_value"]');
+                    const inst = tinyMCE.get(textarea.attr('id'));
+
+                    if (inst) {
+                        return inst.getContent();
+                    }
+
+                    return '';
+                },
+                "convertDefaultValue": function (question, old_type, value) {
+                    // Only accept string values
+                    if (typeof value !== 'string') {
+                        return '';
+                    }
+
+                    const textarea = question.find('[data-glpi-form-editor-question-type-specific]')
+                        .find('[name="default_value"], [data-glpi-form-editor-original-name="default_value"]');
+                    textarea.val(value);
+
+                    return textarea.val();
+                }
+            }
+        JS;
+    }
+
+    #[Override]
     public function renderAdministrationTemplate(?Question $question): string
     {
         $template = <<<TWIG
@@ -63,6 +95,7 @@ final class QuestionTypeLongText extends AbstractQuestionType
                     'is_horizontal': false,
                     'full_width'   : true,
                     'no_label'     : true,
+                    'aria_label': aria_label,
                 }
             ) }}
 TWIG;
@@ -71,6 +104,7 @@ TWIG;
         return $twig->renderFromStringTemplate($template, [
             'question'    => $question,
             'placeholder' => __('Long text'),
+            'aria_label'  => __('Default value')
         ]);
     }
 
