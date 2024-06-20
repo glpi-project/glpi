c04d3f4340d2cff04c178ee9dd0a2637d07e90c0
Cédric Anne <cedric.anne@gmail.com>
2024-06-20 11:20:10 +0200

    Try to fix timezone access on Github Codespaces

---
+++ .devcontainer/initdb.sql
@@ -0,0 +1,4 @@
+# This SQL queries contained in this file will be executed on every database container startup.
+
+# Required for timezones usage
+GRANT SELECT ON `mysql`.`time_zone_name` TO 'glpi'@'localhost';
