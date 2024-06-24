46591c65d69484ec668233f5f074093544172623
Alex Rock Ancelet <pierstoval@gmail.com>
2024-06-24 11:00:34 +0200

    Fix phpstan bootstrap file

--- tests/phpstan_bootstrap.php
+++ tests/phpstan_bootstrap.php
@@ -4,4 +4,4 @@ $root = dirname(__DIR__);
 
 require $root . '/vendor/autoload.php';
 
-(new \Glpi\Config\LegacyConfigurators\BaseConfig($root))->execute();
+(new \Glpi\Config\LegacyConfigurators\BaseConfig($root, 'development'))->execute();
