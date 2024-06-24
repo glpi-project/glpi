f09d61c179314cbf44ffb0c34ea6cb7cd08733c0
Thetsmr <thetsmr@gmail.com>
2024-06-24 13:40:00 +0200

    move to bundles/cytoscape.js

--- src/Impact.php
+++ src/Impact.php
@@ -804,8 +804,6 @@ JS);
     {
         echo Html::css('public/lib/cytoscape.css');
         echo Html::script("public/lib/cytoscape.js");
-        echo Html::script("public/lib/popper.min.js");
-        echo Html::script("public/lib/cytoscape-popper.js");
     }
 
     /**
