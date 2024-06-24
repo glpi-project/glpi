ccbd32a5c02772ec05342bee9a5cd944d270560d
Alex Rock Ancelet <pierstoval@gmail.com>
2024-06-24 11:18:49 +0200

    Fix phpstan errors

--- src/Glpi/Http/LegacyPostRequestActionsListener.php
+++ src/Glpi/Http/LegacyPostRequestActionsListener.php
@@ -27,11 +27,13 @@ class LegacyPostRequestActionsListener implements EventSubscriberInterface
 
     private function triggerGlobalsDeprecation(): void
     {
+        /** @var int|bool|null $AJAX_INCLUDE */
         global $AJAX_INCLUDE;
         if (isset($AJAX_INCLUDE)) {
             \Toolbox::deprecated('Using the global "$AJAX_INCLUDE" variable has been removed. Use "$this->setAjax()" from your controllers instead.', version: "11.0");
         }
 
+        /** @var string|null $SECURITY_STRATEGY */
         global $SECURITY_STRATEGY;
         if (isset($SECURITY_STRATEGY)) {
             \Toolbox::deprecated('Using the global "$SECURITY_STRATEGY" variable has been removed. Use proper Route attributes in your controllers instead.', version: "11.0");
