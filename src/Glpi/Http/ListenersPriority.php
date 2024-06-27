7e99c1a61961649b697ce1518c6b10dc689db817
Cédric Anne <cedric.anne@gmail.com>
2024-06-27 10:36:18 +0200

    Prevent config providers to be executed when a JS/CSS asset is requested

--- src/Glpi/Http/ListenersPriority.php
+++ src/Glpi/Http/ListenersPriority.php
@@ -39,9 +39,9 @@ use Glpi\Config\LegacyConfigProviderListener;
 final class ListenersPriority
 {
     public const LEGACY_LISTENERS_PRIORITIES = [
-        LegacyRouterListener::class => 400,
+        LegacyAssetsListener::class         => 500,
+        LegacyRouterListener::class         => 400,
         LegacyConfigProviderListener::class => 350,
-        LegacyAssetsListener::class => 300,
     ];
 
     private function __construct()
