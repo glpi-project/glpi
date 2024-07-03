841c9fc6b19951c071bceff29db3828f288596e6
Cédric Anne <cedric.anne@gmail.com>
2024-07-03 14:11:55 +0200

    Do not disable GLPI error handler

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -36,10 +36,10 @@
 use Glpi\Application\ErrorHandler;
 use Glpi\Cache\CacheManager;
 use Glpi\Cache\SimpleCache;
-use Glpi\Socket;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
-ini_set('display_errors', 'On'); // Ensure errors happening during test suite bootstraping are always displayed
+// Ensure errors happening during test suite bootstraping are always displayed
+ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 define('GLPI_ROOT', __DIR__ . '/../');
@@ -89,16 +89,6 @@ if (file_exists(GLPI_CONFIG_DIR . DIRECTORY_SEPARATOR . CacheManager::CONFIG_FIL
     $GLPI_CACHE = new SimpleCache(new ArrayAdapter());
 }
 
-// Errors/exceptions that are not explicitely validated by `$this->error()` or `$this->exception` asserter will already make test fails.
-// There is no need to pollute the output with error messages.
-ini_set('display_errors', 'Off');
-ErrorHandler::getInstance()->disableOutput();
-// To ensure that errors/exceptions will be catched by `atoum`, unregister GLPI error/exception handlers.
-// Errors that are pushed directly to logs (SQL errors/warnings for instance) will still have to be explicitely
-// validated by `$this->has*LogRecord*()` asserters, otherwise it will make make test fails.
-set_error_handler(null);
-set_exception_handler(null);
-
 include_once __DIR__ . '/GLPITestCase.php';
 include_once __DIR__ . '/DbTestCase.php';
 //include_once __DIR__ . '/CsvTestCase.php';
@@ -115,3 +105,7 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php') && !file_exists(__DIR__ . '
 }
 
 loadDataset();
+
+// There is no need to pollute the output with error messages.
+ini_set('display_errors', 'Off');
+ErrorHandler::getInstance()->disableOutput();
