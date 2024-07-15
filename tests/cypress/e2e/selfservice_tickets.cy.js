c4ef930bc88fd65ad0241addc1120d3bf511f875
Alexandre Delaunay <adelaunay@teclib.com>
2024-07-15 16:05:33 +0200

    fix cypress tests

--- tests/cypress/e2e/selfservice_tickets.cy.js
+++ tests/cypress/e2e/selfservice_tickets.cy.js
@@ -55,9 +55,6 @@ describe('Self-Service Tickets', () => {
                 cy.get('textarea[name="content"]').type('This is my first ticket in GLPI. I sure hope it works.', {interactive: true});
             });
             cy.get('button').contains('Submit message').click();
-            cy.url().should('include', '/front/tracking.injector.php').then(() => {
-                cy.wrap(Cypress.$('main#page img')).should('have.attr', 'src', '/pics/ok.png');
-            });
         });
 
         cy.url().should('include', '/front/ticket.form.php');
