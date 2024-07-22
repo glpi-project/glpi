2c3769785bf983fa62a042e8b9726945ec3553d3
Cédric Anne <cedric.anne@gmail.com>
2024-07-22 15:56:43 +0200

    Fix warnings

--- ajax/treebrowse.php
+++ ajax/treebrowse.php
@@ -46,8 +46,8 @@ switch ($_REQUEST['action']) {
             'as_map'             => 0,
             'showmassiveactions' => true,
             'criteria'           => $_REQUEST['criteria'],
-            'sort'               => $_REQUEST['sort'],
-            'order'              => $_REQUEST['order'],
+            'sort'               => $_REQUEST['sort'] ?? [],
+            'order'              => $_REQUEST['order'] ?? [],
         ];
 
         $itemtype = $_REQUEST['itemtype'];
