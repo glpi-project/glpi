851d9bba21bd7d5f6ce6417f422f24c880cca294
Rom1-B <rbiot@teclib.com>
2024-07-30 15:33:34 +0200

    revert

--- ajax/search.php
+++ ajax/search.php
@@ -73,9 +73,13 @@ switch ($_REQUEST['action']) {
 
         $search_params = Search::manageParams($itemtype, $_REQUEST);
 
-        $results = Search::getDatas($itemtype, $search_params);
-        $results['searchform_id'] = $_REQUEST['searchform_id'] ?? null;
-        Search::displayData($results);
+        if (isset($search_params['browse']) && $search_params['browse'] == 1) {
+            $itemtype::showBrowseView($itemtype, $search_params, true);
+        } else {
+            $results = Search::getDatas($itemtype, $search_params);
+            $results['searchform_id'] = $_REQUEST['searchform_id'] ?? null;
+            Search::displayData($results);
+        }
         break;
 }
 
