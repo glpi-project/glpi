041745fe8baa3edf47c6d6e87d8e99f5c6d0587c
Curtis Conard <cconard96@gmail.com>
2024-08-05 11:33:38 -0400

    fix fallback img path

--- src/Impact.php
+++ src/Impact.php
@@ -1274,7 +1274,7 @@ JS);
         /** @var array $CFG_GLPI */
         global $CFG_GLPI;
 
-        if ($icon_path !== null) {
+        if (!empty($icon_path)) {
             // Special case for images returned dynamicly
             if (str_contains($icon_path, ".php")) {
                 return $icon_path;
@@ -1294,7 +1294,7 @@ JS);
         }
 
         // Fallback "default" icon
-        return "pics/impact/default.png";
+        return $CFG_GLPI['root_doc'] . "/pics/impact/default.png";
     }
 
     /**
