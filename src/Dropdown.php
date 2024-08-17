137fa6f2ee1234ef97e7cb14260092ee943f4221
Curtis Conard <cconard96@gmail.com>
2024-08-17 05:40:55 -0400

    improve perf searching ids in dropdowns

--- src/Dropdown.php
+++ src/Dropdown.php
@@ -3169,10 +3169,9 @@ JAVASCRIPT;
                 ];
 
                 if (
-                    $_SESSION['glpiis_ids_visible']
-                    && is_numeric($post['searchText']) && (int)$post['searchText'] == $post['searchText']
+                    $_SESSION['glpiis_ids_visible'] && (int) $post['searchText'] === $post['searchText']
                 ) {
-                    $orwhere[$table . '.' . $item->getIndexName()] = ['LIKE', "%{$post['searchText']}%"];
+                    $orwhere[$table . '.' . $item::getIndexName()] = ['LIKE', "{$post['searchText']}%"];
                 }
 
                 if ($item instanceof CommonDCModelDropdown) {
