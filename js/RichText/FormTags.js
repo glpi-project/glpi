b4f129e4e14e54bb6f15326ce4c3027600d628ef
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2024-08-21 09:58:48 +0200

    Add proper Form\TagListController

--- js/RichText/FormTags.js
+++ js/RichText/FormTags.js
@@ -85,7 +85,7 @@ GLPI.RichText.FormTags = class
     }
 
     async #fetchItems(filter) {
-        const url = CFG_GLPI.root_doc + '/ajax/form/form_tags.php';
+        const url = CFG_GLPI.root_doc + '/Form/TagList';
         const data = await $.get(url, {
             form_id: this.#form_id,
             filter: filter
