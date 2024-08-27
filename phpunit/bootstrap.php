8bbc07cefdb1abc459492c634f1e04c4d7f6f9cc
Johan Cwiklinski <johan@x-tnd.be>
2024-08-27 08:38:42 +0200

    Move abstract class to abstract dir

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -78,7 +78,7 @@ include_once __DIR__ . '/DbTestCase.php';
 //include_once __DIR__ . '/FrontBaseClass.php';
 include_once __DIR__ . '/RuleBuilder.php';
 include_once __DIR__ . '/InventoryTestCase.php';
-include_once __DIR__ . '/functional/CommonITILRecurrentTest.php';
+include_once __DIR__ . '/abstracts/CommonITILRecurrentTest.php';
 //include_once __DIR__ . '/functional/Glpi/ContentTemplates/Parameters/AbstractParameters.php';
 include_once __DIR__ . '/AbstractRightsDropdown.php';
 include_once __DIR__ . '/CommonDropdown.php';
