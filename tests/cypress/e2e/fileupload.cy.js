1f0a48617f128ed8b6b12022cdcee5b39a1a2456
AdrienClairembault <aclairembault@teclib.com>
2024-09-06 12:29:22 +0200

    Add E2E tests for file uploads

---
+++ tests/cypress/e2e/fileupload.cy.js
@@ -0,0 +1,56 @@
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+describe('File upload', () => {
+    beforeEach(() => {
+        cy.login();
+        cy.changeProfile('Super-Admin', true);
+        cy.visit('/front/document.form.php');
+    });
+
+    it('Can upload file', () => {
+        // Upload file
+        cy.get("input[type=file]").selectFile("fixtures/uploads/bar.txt");
+        cy.findByText('Upload successful').should('exist');
+        cy.findByRole("button", {'name': "Add"}).click();
+        cy.findByRole('textbox', {'name': "Name"}).should('have.value', 'bar.txt');
+
+        // Download file
+        cy.get('#main-form')
+            .findByRole('link', {'name': "bar.txt"})
+            .invoke('attr', 'target', '_self') // Cypress don't like new tabs
+            .click();
+        cy.readFile('cypress/downloads/bar.txt').then(content => {
+            cy.wrap('bar').should('eq', content);
+        });
+    });
+});
