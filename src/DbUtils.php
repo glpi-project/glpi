43b03905465f9b241514b80044586019ed7c5355
Alex Rock Ancelet <pierstoval@gmail.com>
2024-09-18 09:16:12 +0200

    Fix invalid check in DbUtils

--- src/DbUtils.php
+++ src/DbUtils.php
@@ -1500,7 +1500,7 @@ final class DbUtils
                         }
                         $acomment .= $country;
                     }
-                    if (trim($acomment != '')) {
+                    if (trim($acomment) !== '') {
                         $comment .= "<span class='b'>&nbsp;" . __('Address:') . "</span> " . $acomment . "<br/>";
                     }
                 }
