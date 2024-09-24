jswshakti/JSW-GLPI

Custom asset fields

install/migrations/update_10.0.x_to_11.0.0/assets.php
@@ -97,6 +97,7 @@ if (!$DB->tableExists('glpi_assets_assets')) {
             `template_name` varchar(255) DEFAULT NULL,
             `date_creation` timestamp NULL DEFAULT NULL,
             `date_mod` timestamp NULL DEFAULT NULL,
+            `custom_fields` json,
             PRIMARY KEY (`id`),
             KEY `assets_assetdefinitions_id` (`assets_assetdefinitions_id`),
             KEY `assets_assetmodels_id` (`assets_assetmodels_id`),
@@ -128,6 +129,7 @@ SQL;
     $migration->dropField('glpi_assets_assets', 'groups_id');
     $migration->dropKey('glpi_assets_assets', 'groups_id_tech');
     $migration->dropField('glpi_assets_assets', 'groups_id_tech');
+    $migration->addField('glpi_assets_assets', 'custom_fields', 'json', ['update' => $DB::quoteValue('{}')]);
 }
 
 if (!$DB->tableExists('glpi_assets_assetmodels')) {
@@ -174,6 +176,25 @@ if (!$DB->tableExists('glpi_assets_assettypes')) {
           KEY `name` (`name`),
           KEY `date_mod` (`date_mod`),
           KEY `date_creation` (`date_creation`)
+        ) ENGINE=InnoDB DEFAULT CHARSET={$default_charset} COLLATE={$default_collation} ROW_FORMAT=DYNAMIC;
+SQL;
+    $DB->doQueryOrDie($query);
+}
+
+if (!$DB->tableExists('glpi_assets_customfields')) {
+    $query = <<<SQL
+        CREATE TABLE `glpi_assets_customfields` (
+          `id` int {$default_key_sign} NOT NULL AUTO_INCREMENT,
+          `assets_assetdefinitions_id` int {$default_key_sign} NOT NULL,
+          `name` varchar(255) NOT NULL,
+          `label` varchar(255) NOT NULL,
+          `type` varchar(255) NOT NULL,
+          `field_options` json,
+          `itemtype` VARCHAR(255) NULL DEFAULT NULL,
+          `default_value` text,
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `unicity` (`assets_assetdefinitions_id`, `name`),
+          KEY `name` (`name`)
         ) ENGINE=InnoDB DEFAULT CHARSET={$default_charset} COLLATE={$default_collation} ROW_FORMAT=DYNAMIC;
 SQL;
     $DB->doQueryOrDie($query);

install/mysql/glpi-empty.sql
@@ -9863,6 +9863,7 @@ CREATE TABLE `glpi_assets_assets` (
   `template_name` varchar(255) DEFAULT NULL,
   `date_creation` timestamp NULL DEFAULT NULL,
   `date_mod` timestamp NULL DEFAULT NULL,
+  `custom_fields` json,
   PRIMARY KEY (`id`),
   KEY `assets_assetdefinitions_id` (`assets_assetdefinitions_id`),
   KEY `assets_assetmodels_id` (`assets_assetmodels_id`),
@@ -9933,6 +9934,21 @@ CREATE TABLE `glpi_groups_items` (
   UNIQUE KEY `unicity` (`groups_id`,`itemtype`,`items_id`, `type`),
   KEY `item` (`itemtype`, `items_id`),
   KEY `type` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+DROP TABLE IF EXISTS `glpi_assets_customfields`;
+CREATE TABLE `glpi_assets_customfields` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `assets_assetdefinitions_id` int unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `field_options` json,
+  `itemtype` VARCHAR(255) NULL DEFAULT NULL,
+  `default_value` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`assets_assetdefinitions_id`, `name`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS=1;

glpi-project/glpi

install/migrations/update_10.0.x_to_11.0.0/assets.php
@@ -101,6 +101,7 @@ if (!$DB->tableExists('glpi_assets_assets')) {
             `date_creation` timestamp NULL DEFAULT NULL,
             `date_mod` timestamp NULL DEFAULT NULL,
             `last_inventory_update` timestamp NULL DEFAULT NULL,
+            `custom_fields` json,
             PRIMARY KEY (`id`),
             KEY `assets_assetdefinitions_id` (`assets_assetdefinitions_id`),
             KEY `assets_assetmodels_id` (`assets_assetmodels_id`),
@@ -142,6 +143,7 @@ SQL;
     $migration->addField('glpi_assets_assets', 'autoupdatesystems_id', 'fkey');
     $migration->addKey('glpi_assets_assets', 'autoupdatesystems_id');
     $migration->addField('glpi_assets_assets', 'last_inventory_update', 'timestamp');
+    $migration->addField('glpi_assets_assets', 'custom_fields', 'json', ['update' => $DB::quoteValue('{}')]);
 }
 
 if (!$DB->tableExists('glpi_assets_assetmodels')) {
@@ -188,6 +190,25 @@ if (!$DB->tableExists('glpi_assets_assettypes')) {
           KEY `name` (`name`),
           KEY `date_mod` (`date_mod`),
           KEY `date_creation` (`date_creation`)
+        ) ENGINE=InnoDB DEFAULT CHARSET={$default_charset} COLLATE={$default_collation} ROW_FORMAT=DYNAMIC;
+SQL;
+    $DB->doQueryOrDie($query);
+}
+
+if (!$DB->tableExists('glpi_assets_customfielddefinitions')) {
+    $query = <<<SQL
+        CREATE TABLE `glpi_assets_customfielddefinitions` (
+          `id` int {$default_key_sign} NOT NULL AUTO_INCREMENT,
+          `assets_assetdefinitions_id` int {$default_key_sign} NOT NULL,
+          `name` varchar(255) NOT NULL,
+          `label` varchar(255) NOT NULL,
+          `type` varchar(255) NOT NULL,
+          `field_options` json,
+          `itemtype` VARCHAR(255) NULL DEFAULT NULL,
+          `default_value` text,
+          PRIMARY KEY (`id`),
+          UNIQUE KEY `unicity` (`assets_assetdefinitions_id`, `name`),
+          KEY `name` (`name`)
         ) ENGINE=InnoDB DEFAULT CHARSET={$default_charset} COLLATE={$default_collation} ROW_FORMAT=DYNAMIC;
 SQL;
     $DB->doQueryOrDie($query);

install/mysql/glpi-empty.sql
@@ -9869,6 +9869,7 @@ CREATE TABLE `glpi_assets_assets` (
   `date_creation` timestamp NULL DEFAULT NULL,
   `date_mod` timestamp NULL DEFAULT NULL,
   `last_inventory_update` timestamp NULL DEFAULT NULL,
+  `custom_fields` json,
   PRIMARY KEY (`id`),
   KEY `assets_assetdefinitions_id` (`assets_assetdefinitions_id`),
   KEY `assets_assetmodels_id` (`assets_assetmodels_id`),
@@ -9986,6 +9987,21 @@ CREATE TABLE `glpi_dropdowns_dropdowns` (
   KEY `level` (`level`),
   KEY `date_creation` (`date_creation`),
   KEY `date_mod` (`date_mod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+DROP TABLE IF EXISTS `glpi_assets_customfielddefinitions`;
+CREATE TABLE `glpi_assets_customfielddefinitions` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `assets_assetdefinitions_id` int unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `field_options` json,
+  `itemtype` VARCHAR(255) NULL DEFAULT NULL,
+  `default_value` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unicity` (`assets_assetdefinitions_id`, `name`),
+  KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS=1;