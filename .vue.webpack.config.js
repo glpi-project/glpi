237f88d824ea5e121699bd1708e07bae0c506aef
Cédric Anne <cedric.anne@gmail.com>
2024-10-02 13:51:50 +0200

    Update vue global config

--- .vue.webpack.config.js
+++ .vue.webpack.config.js
@@ -45,6 +45,7 @@ const config = {
         new webpack.DefinePlugin({
             __VUE_OPTIONS_API__: false, // We will only use composition API
             __VUE_PROD_DEVTOOLS__: false,
+            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
         }),
     ],
     resolve: {
