ae9c2d3488cfc3f46b72d08ace8d1591f8bea1df
Alex Rock Ancelet <pierstoval@gmail.com>
2024-10-16 18:00:44 +0200

    Attempt at merging dropdown and commondbtm entities checks in legacy routing

--- src/Glpi/Http/ListenersPriority.php
+++ src/Glpi/Http/ListenersPriority.php
@@ -52,7 +52,7 @@ final class ListenersPriority
         // Plugins dropdowns requires plugins to be initialized, therefore config must be already set.
         // Also, keep it after the `LegacyRouterListener` to not map to the generic dropdown controller if a
         // legacy script exists for the requested URI.
-        LegacyDropdownRouteListener::class => 300,
+        LegacyDbObjectRouteListener::class => 300,
     ];
 
     private function __construct()
