3e261c49cffe1cd038e7241c6acfff455c0a1d1f
Alexandre Delaunay <adelaunay@teclib.com>
2024-10-17 15:15:15 +0200

    avoid instanciating classes outside GLPI

--- front/commonitilobject_commonitilobject.form.php
+++ front/commonitilobject_commonitilobject.form.php
@@ -45,6 +45,12 @@ Session ::checkCentralAccess();
 if (isset($_POST['purge'], $_POST['id'])) {
     [$link_class_1, $link_class_2, $link_id] = explode('_', $_POST['id'], 3);
     $link_class = $link_class_1 . '_' . $link_class_2;
+
+    // limit to GLPI Classes
+    if (!class_exists($link_class) && !is_subclass_of($_POST["itemtype"], "CommonDBTM")) {
+        return;
+    }
+
     $itil_itil = new $link_class();
     $_POST['id'] = $link_id;
     $itil_itil->check($_POST['id'], PURGE);
