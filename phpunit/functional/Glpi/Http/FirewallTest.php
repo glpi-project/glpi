b475d9c373087c540ea360fdac5b5479262f5b94
Cédric Anne <cedric.anne@gmail.com>
2024-10-28 10:05:09 +0100

    Increase default firewall strategy for plugin legacy scripts

--- phpunit/functional/Glpi/Http/FirewallTest.php
+++ phpunit/functional/Glpi/Http/FirewallTest.php
@@ -34,12 +34,17 @@
 
 namespace tests\units\Glpi\Http;
 
-use org\bovigo\vfs\vfsStream;
+use Glpi\Exception\Http\AccessDeniedHttpException;
+use Glpi\Exception\SessionExpiredException;
 use Glpi\Http\Firewall;
+use KnowbaseItem;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Symfony\Component\HttpFoundation\Request;
 
-class FirewallTest extends \GLPITestCase
+class FirewallTest extends \DbTestCase
 {
-    public function testComputeFallbackStrategy()
+    public function testComputeFallbackStrategy(): void
     {
         vfsStream::setup(
             'glpi',
@@ -113,7 +118,7 @@ class FirewallTest extends \GLPITestCase
         );
 
         $default_for_core_legacy    = 'authenticated';
-        $default_for_plugins_legacy = 'no_check';
+        $default_for_plugins_legacy = 'authenticated';
         $default_for_symfony_routes = 'central_access';
 
         $default_mapping = [
@@ -137,26 +142,17 @@ class FirewallTest extends \GLPITestCase
             '/myplugindir/pluginb/Route/To/Something'   => $default_for_symfony_routes,
         ];
 
-        foreach ($default_mapping as $path => $expected_strategy) {
-            $this->dotestComputeFallbackStrategy(
-                root_doc:          '',
-                path:              $path,
-                expected_strategy: $expected_strategy,
-            );
-            $this->dotestComputeFallbackStrategy(
-                root_doc:          '/glpi',
-                path:              '/glpi' . $path,
-                expected_strategy: $expected_strategy,
-            );
-            $this->dotestComputeFallbackStrategy(
-                root_doc:          '/path/to/app',
-                path:              '/path/to/app' . $path,
-                expected_strategy: $expected_strategy,
-            );
-        }
-
-        // Hardcoded strategies
         foreach (['', '/glpi', '/path/to/app'] as $root_doc) {
+            // Default strategies
+            foreach ($default_mapping as $path => $expected_strategy) {
+                $this->dotestComputeFallbackStrategy(
+                    root_doc:          $root_doc,
+                    path:              $root_doc . $path,
+                    expected_strategy: $expected_strategy,
+                );
+            }
+
+            // Hardcoded strategies
             // `/front/central.php` has a specific strategy only if some get parameters are defined
             $this->dotestComputeFallbackStrategy(
                 root_doc:          $root_doc,
@@ -220,6 +216,47 @@ class FirewallTest extends \GLPITestCase
                     expected_strategy: 'no_check',
                 );
             }
+
+            // Specific strategies defined by plugins
+            Firewall::addPluginStrategyForLegacyScripts('myplugin', '#^.*/foo.php#', 'faq_access');
+            $this->dotestComputeFallbackStrategy(
+                root_doc:          $root_doc,
+                path:              $root_doc . '/marketplace/myplugin/ajax/foo.php',
+                expected_strategy: 'faq_access',
+            );
+            $this->dotestComputeFallbackStrategy(
+                root_doc:          $root_doc,
+                path:              $root_doc . '/marketplace/myplugin/front/foo.php',
+                expected_strategy: 'faq_access',
+            );
+            $this->dotestComputeFallbackStrategy(
+                root_doc:          $root_doc,
+                path:              $root_doc . '/marketplace/myplugin/front/dir/bar.php',
+                expected_strategy: $default_for_plugins_legacy, // does not match the pattern
+            );
+            Firewall::addPluginStrategyForLegacyScripts('myplugin', '#^/front/dir/#', 'helpdesk_access');
+            $this->dotestComputeFallbackStrategy(
+                root_doc:          $root_doc,
+                path:              $root_doc . '/marketplace/myplugin/ajax/foo.php',
+                expected_strategy: 'faq_access',
+            );
+            $this->dotestComputeFallbackStrategy(
+                root_doc:          $root_doc,
+                path:              $root_doc . '/marketplace/myplugin/front/foo.php',
+                expected_strategy: 'faq_access',
+            );
+            $this->dotestComputeFallbackStrategy(
+                root_doc:          $root_doc,
+                path:              $root_doc . '/marketplace/myplugin/front/dir/bar.php',
+                expected_strategy: 'helpdesk_access',
+            );
+            Firewall::addPluginStrategyForLegacyScripts('myplugin', '#^/PluginRoute$#', 'helpdesk_access');
+            $this->dotestComputeFallbackStrategy(
+                root_doc:          $root_doc,
+                path:              $root_doc . '/marketplace/myplugin/PluginRoute',
+                expected_strategy: $default_for_symfony_routes, // fallback strategies MUST NOT apply to symfony routes
+            );
+            Firewall::resetPluginsStrategies();
         }
     }
 
@@ -227,15 +264,182 @@ class FirewallTest extends \GLPITestCase
         string $root_doc,
         string $path,
         string $expected_strategy
-    ) {
+    ): void {
         $instance = new Firewall(
-            $root_doc,
             vfsStream::url('glpi'),
             [vfsStream::url('glpi/myplugindir'), vfsStream::url('glpi/marketplace')]
         );
+
+        $request = new Request();
+        $request->server->set('SCRIPT_FILENAME', $root_doc . '/index.php');
+        $request->server->set('SCRIPT_NAME', $root_doc . '/index.php');
+        $request->server->set('REQUEST_URI', $path);
+
         $this->assertEquals(
             $expected_strategy,
-            $instance->computeFallbackStrategy($path)
+            $instance->computeFallbackStrategy($request),
+            $path
         );
+    }
+
+    public static function provideStrategy(): iterable
+    {
+        yield ['strategy' => Firewall::STRATEGY_AUTHENTICATED];
+        yield ['strategy' => Firewall::STRATEGY_CENTRAL_ACCESS];
+        yield ['strategy' => Firewall::STRATEGY_FAQ_ACCESS];
+        yield ['strategy' => Firewall::STRATEGY_HELPDESK_ACCESS];
+    }
+
+    #[DataProvider('provideStrategy')]
+    public function testApplyStrategyWhenLoggedOut(string $strategy): void
+    {
+        $this->expectException(SessionExpiredException::class);
+
+        $instance = new Firewall();
+        $instance->applyStrategy($strategy);
+    }
+
+    #[DataProvider('provideStrategy')]
+    public function testApplyStrategyWhenSessionIsCorrupted(string $strategy): void
+    {
+        $this->login();
+
+        $_SESSION = [];
+
+        $this->expectException(SessionExpiredException::class);
+
+        $instance = new Firewall();
+        $instance->applyStrategy($strategy);
+    }
+
+    public static function provideStrategyResults(): iterable
+    {
+        $central_users = [
+            TU_USER     => TU_PASS,
+            'glpi'      => 'glpi',
+            'tech'      => 'tech',
+            'normal'    => 'normal',
+        ];
+
+        foreach ($central_users as $login => $pass) {
+            yield [
+                'strategy'      => Firewall::STRATEGY_AUTHENTICATED,
+                'credentials'   => [$login, $pass],
+                'exception'     => null,
+            ];
+            yield [
+                'strategy'      => Firewall::STRATEGY_CENTRAL_ACCESS,
+                'credentials'   => [$login, $pass],
+                'exception'     => null,
+            ];
+            yield [
+                'strategy'      => Firewall::STRATEGY_FAQ_ACCESS,
+                'credentials'   => [$login, $pass],
+                'exception'     => null,
+            ];
+            yield [
+                'strategy'      => Firewall::STRATEGY_HELPDESK_ACCESS,
+                'credentials'   => [$login, $pass],
+                'exception'     => new AccessDeniedHttpException('The current profile does not use the simplified interface'),
+            ];
+        }
+
+        $helpdesk_users = [
+            'post-only' => 'postonly',
+        ];
+        foreach ($helpdesk_users as $login => $pass) {
+            yield [
+                'strategy'      => Firewall::STRATEGY_AUTHENTICATED,
+                'credentials'   => [$login, $pass],
+                'exception'     => null,
+            ];
+            yield [
+                'strategy'      => Firewall::STRATEGY_CENTRAL_ACCESS,
+                'credentials'   => [$login, $pass],
+                'exception'     => new AccessDeniedHttpException('The current profile does not use the standard interface'),
+            ];
+            yield [
+                'strategy'      => Firewall::STRATEGY_FAQ_ACCESS,
+                'credentials'   => [$login, $pass],
+                'exception'     => null,
+            ];
+            yield [
+                'strategy'      => Firewall::STRATEGY_HELPDESK_ACCESS,
+                'credentials'   => [$login, $pass],
+                'exception'     => null,
+            ];
+        }
+    }
+
+    #[DataProvider('provideStrategyResults')]
+    public function testApplyStrategyWithUser(string $strategy, array $credentials, ?\Throwable $exception): void
+    {
+        $this->login(...$credentials);
+
+        if ($exception !== null) {
+            $this->expectExceptionObject($exception);
+        }
+
+        $instance = new Firewall();
+        $instance->applyStrategy($strategy);
+    }
+
+    public static function provideFaqAccessStrategyResults(): iterable
+    {
+        yield [
+            'use_public_faq'    => false,
+            'knowbase_rights'   => KnowbaseItem::READFAQ,
+            'exception'         => null,
+        ];
+
+        yield [
+            'use_public_faq'    => false,
+            'knowbase_rights'   => READ,
+            'exception'         => null,
+        ];
+
+        yield [
+            'use_public_faq'    => false,
+            'knowbase_rights'   => 0,
+            'exception'         => new AccessDeniedHttpException('Missing FAQ right'),
+        ];
+
+        yield [
+            'use_public_faq'    => true,
+            'knowbase_rights'   => KnowbaseItem::READFAQ,
+            'exception'         => null,
+        ];
+
+        yield [
+            'use_public_faq'    => true,
+            'knowbase_rights'   => READ,
+            'exception'         => null,
+        ];
+
+        yield [
+            'use_public_faq'    => true,
+            'knowbase_rights'   => 0,
+            'exception'         => null,
+        ];
+    }
+
+    #[DataProvider('provideFaqAccessStrategyResults')]
+    public function testApplyStrategyFaqAccess(bool $use_public_faq, int $knowbase_rights, ?\Throwable $exception): void
+    {
+        /** @var array $CFG_GLPI */
+        global $CFG_GLPI;
+
+        $this->login();
+
+        $CFG_GLPI['use_public_faq'] = $use_public_faq;
+
+        $_SESSION['glpiactiveprofile']['knowbase'] = $knowbase_rights;
+
+        if ($exception !== null) {
+            $this->expectExceptionObject($exception);
+        }
+
+        $instance = new Firewall();
+        $instance->applyStrategy(Firewall::STRATEGY_FAQ_ACCESS);
     }
 }
