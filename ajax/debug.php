31097d21495ea0619a0067a76c230b63dde06ba0
Johan Cwiklinski <johan@x-tnd.be>
2024-10-31 10:08:41 +0100

    Changes according to PR comments

--- ajax/debug.php
+++ ajax/debug.php
@@ -56,12 +56,11 @@ if (isset($_GET['ajax_id'])) {
     // as we have to delete profile from `$_SESSION` during the pull operation.
     session_write_close();
 
+    header('Content-Type: application/json');
     if ($profile) {
         $data = $profile->getDebugInfo();
         if ($data) {
-            header('Content-Type: application/json');
             echo json_encode($data);
-            return;
         }
     }
     return;
