b40bffe32cae0a0eeaa3f87afc39c3587b53bc0b
AdrienClairembault <aclairembault@teclib.com>
2024-11-06 16:08:38 +0100

    Remove -T option of docker compose exec

--- .github/actions/init_show-versions.sh
+++ .github/actions/init_show-versions.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-docker compose exec -T app php --version
-docker compose exec -T app php -r 'echo(sprintf("PHP extensions: %s\n", implode(", ", get_loaded_extensions())));'
-docker compose exec -T app composer --version
-docker compose exec -T app sh -c 'echo "node $(node --version)"'
-docker compose exec -T app sh -c 'echo "npm $(npm --version)"'
+docker compose exec app php --version
+docker compose exec app php -r 'echo(sprintf("PHP extensions: %s\n", implode(", ", get_loaded_extensions())));'
+docker compose exec app composer --version
+docker compose exec app sh -c 'echo "node $(node --version)"'
+docker compose exec app sh -c 'echo "npm $(npm --version)"'
 
 if [[ -n $(docker compose ps --all --services | grep "db") ]]; then
-  docker compose exec -T db mysql --version;
+  docker compose exec db mysql --version;
 fi
 if [[ -n $(docker compose ps --all --services | grep "redis") ]]; then
-  docker compose exec -T redis redis-server --version;
+  docker compose exec redis redis-server --version;
 fi
 if [[ -n $(docker compose ps --all --services | grep "memcached") ]]; then
-  docker compose exec -T memcached memcached --version;
+  docker compose exec memcached memcached --version;
 fi
