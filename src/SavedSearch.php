e1c1129ce35da14f955a2ece4f31879b15f61a85
MyuTsu <gaetan.langlois31@gmail.com>
2024-11-25 14:46:36 +0100

    fix(save-search): correct total calculation for itemtype

--- src/SavedSearch.php
+++ src/SavedSearch.php
@@ -1318,12 +1318,20 @@ class SavedSearch extends CommonDBTM implements ExtraVisibilityCriteria
         ) {
             $search = new Search();
            //Do the same as self::getParameters() but getFromDB is useless
-            $query_tab = [];
-            parse_str($this->getField('query'), $query_tab);
 
             $params = null;
             if (class_exists($this->getField('itemtype'))) {
-                $params = $search::manageParams($this->getField('itemtype'), $_GET);
+                $query_tab = null;
+                $save_search_params = $this->getParameters($this->getID());
+                if (isset($save_search_params['criteria'])) {
+                    $query_tab = $save_search_params['criteria'];
+                }
+                $params = Search::manageParams($this->getField('itemtype'), [
+                    'is_deleted'    => 0,
+                    'savedsearches_id' => $this->getID(),
+                    'itemtype'      => $this->getField('itemtype'),
+                    'criteria'      => $query_tab
+                ], false);
             }
 
             if (!$params) {
