539098019137747a3fd0e03ca34e826020bb1134
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2024-12-02 10:28:26 +0100

    Fix illustration manager tests

--- phpunit/functional/Glpi/UI/IllustrationManagerTest.php
+++ phpunit/functional/Glpi/UI/IllustrationManagerTest.php
@@ -100,11 +100,8 @@ final class IllustrationManagerTest extends GLPITestCase
         if (!str_contains($rendered_icon, 'var(--tblr-primary)')) {
             $errors[] = "The '$filename' illustration is missing the primary color.";
         }
-        if (!str_contains($rendered_icon, 'var(--tblr-primary)')) {
+        if (!str_contains($rendered_icon, 'var(--glpi-mainmenu-bg)')) {
             $errors[] = "The '$filename' illustration is missing the main menu color.";
-        }
-        if (!str_contains($rendered_icon, 'var(--tblr-primary)')) {
-            $errors[] = "The '$filename' illustration is missing the header color.";
         }
 
         if (count($errors)) {
