fed2f4d323ce58f3305252411f014234cbeecf92
Cédric Anne <cedric.anne@gmail.com>
2024-12-02 12:18:40 +0100

    Add `Global search` capacity

---
+++ tests/functional/Glpi/Asset/Capacity/AllowedInGlobalSearchCapacity.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace tests\units\Glpi\Asset\Capacity;
+
+use DbTestCase;
+use Entity;
+use Log;
+
+class AllowedInGlobalSearchCapacity extends DbTestCase
+{
+    public function testCapacityActivation(): void
+    {
+        global $CFG_GLPI;
+
+        $definition_1 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\AllowedInGlobalSearchCapacity::class,
+                \Glpi\Asset\Capacity\HasNotepadCapacity::class,
+            ]
+        );
+        $classname_1  = $definition_1->getAssetClassName();
+        $definition_2 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\HasHistoryCapacity::class,
+            ]
+        );
+        $classname_2  = $definition_2->getAssetClassName();
+        $definition_3 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\AllowedInGlobalSearchCapacity::class,
+                \Glpi\Asset\Capacity\HasHistoryCapacity::class,
+            ]
+        );
+        $classname_3  = $definition_3->getAssetClassName();
+
+        $has_capacity_mapping = [
+            $classname_1 => true,
+            $classname_2 => false,
+            $classname_3 => true,
+        ];
+
+        foreach ($has_capacity_mapping as $classname => $has_capacity) {
+            // Check that the class is globally registered
+            if ($has_capacity) {
+                $this->array($CFG_GLPI['globalsearch_types'])->contains($classname);
+            } else {
+                $this->array($CFG_GLPI['globalsearch_types'])->notContains($classname);
+            }
+        }
+    }
+
+    public function testCapacityDeactivation(): void
+    {
+        /** @var array $CFG_GLPI */
+        global $CFG_GLPI;
+
+        $root_entity_id = getItemByTypeName(Entity::class, '_test_root_entity', true);
+
+        $definition_1 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\AllowedInGlobalSearchCapacity::class,
+                \Glpi\Asset\Capacity\HasHistoryCapacity::class,
+            ]
+        );
+        $classname_1  = $definition_1->getAssetClassName();
+        $definition_2 = $this->initAssetDefinition(
+            capacities: [
+                \Glpi\Asset\Capacity\AllowedInGlobalSearchCapacity::class,
+                \Glpi\Asset\Capacity\HasHistoryCapacity::class,
+            ]
+        );
+        $classname_2  = $definition_2->getAssetClassName();
+
+        $this->createItem(
+            $classname_1,
+            [
+                'name' => __FUNCTION__,
+                'entities_id' => $root_entity_id,
+            ]
+        );
+        $this->createItem(
+            $classname_2,
+            [
+                'name' => __FUNCTION__,
+                'entities_id' => $root_entity_id,
+            ]
+        );
+
+        $item_1_logs_criteria = [
+            'itemtype'      => $classname_1,
+        ];
+        $item_2_logs_criteria = [
+            'itemtype'      => $classname_2,
+        ];
+
+        // Ensure relation, display preferences and logs exists, and class is registered to global config
+        $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(1); // create
+        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(1); // create
+        $this->array($CFG_GLPI['globalsearch_types'])->contains($classname_1);
+        $this->array($CFG_GLPI['globalsearch_types'])->contains($classname_2);
+
+        // Disable capacity and check that class is unregistered from global config
+        $this->disableCapacity($definition_1, \Glpi\Asset\Capacity\AllowedInGlobalSearchCapacity::class);
+        $this->integer(countElementsInTable(Log::getTable(), $item_1_logs_criteria))->isEqualTo(1); // create
+        $this->array($CFG_GLPI['globalsearch_types'])->notContains($classname_1);
+
+        // Ensure global config registration is preserved for other definition
+        $this->integer(countElementsInTable(Log::getTable(), $item_2_logs_criteria))->isEqualTo(1); // create
+        $this->array($CFG_GLPI['globalsearch_types'])->contains($classname_2);
+    }
+}
