b9815bdc94cd5c0de7ccb5ce0f7dfd7326012523
Clément Cailly <ccailly@teclib.com>
2024-12-02 16:37:41 +0100

    feat(forms): Horizontal layout

--- src/Html.php
+++ src/Html.php
@@ -4469,6 +4469,8 @@ JAVASCRIPT
             unset($params["width"]);
         }
 
+        $dropdownCssClass = $params["dropdownCssClass"] ?? '';
+
         $placeholder = json_encode($params["placeholder"] ?? '');
 
         $templateresult    = $params["templateResult"] ?? "templateResult";
@@ -4479,6 +4481,7 @@ JAVASCRIPT
                 type: 'adapt',
                 field_id: '{$id}',
                 width: '{$width}',
+                dropdown_css_class: '{$dropdownCssClass}',
                 placeholder: {$placeholder},
                 ajax_limit_count: {$CFG_GLPI['ajax_limit_count']},
                 templateresult: {$templateresult},
