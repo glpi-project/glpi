69655e377acf97ad75fa7416798058533d968bc2
Alexandre Delaunay <adelaunay@teclib.com>
2024-12-03 10:39:15 +0100

    Assets: Add project capacity; fix #18450

---
+++ src/Glpi/Asset/Capacity/HasProjectsCapacity.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+namespace Glpi\Asset\Capacity;
+
+use CommonGLPI;
+use Project;
+use Item_Project;
+
+class HasProjectsCapacity extends AbstractCapacity
+{
+    // #Override
+    public function getLabel(): string
+    {
+        return Project::getTypeName();
+    }
+
+    public function getIcon(): string
+    {
+        return Project::getIcon();
+    }
+
+    public function getCloneRelations(): array
+    {
+        return [
+            Item_Project::class
+        ];
+    }
+
+    public function isUsed(string $classname): bool
+    {
+        return parent::isUsed($classname)
+            && $this->countAssetsLinkedToPeerItem($classname, Item_Project::class) > 0;
+    }
+
+    public function getCapacityUsageDescription(string $classname): string
+    {
+        return sprintf(
+            __('%1$s projects attached to %2$s assets'),
+            $this->countPeerItemsUsage($classname, Item_Project::class),
+            $this->countAssetsLinkedToPeerItem($classname, Item_Project::class)
+        );
+    }
+
+    // #Override
+    public function onClassBootstrap(string $classname): void
+    {
+        // Allow our item to be linked to projects
+        $this->registerToTypeConfig('project_asset_types', $classname);
+
+        // Register the projects tab into our item
+        CommonGLPI::registerStandardTab(
+            $classname,
+            Item_Project::class,
+            20 // Tab shouldn't be too far from the top
+        );
+    }
+
+    // #Override
+    public function onCapacityDisabled(string $classname): void
+    {
+        // Unregister from contracts types
+        $this->unregisterFromTypeConfig('contract_types', $classname);
+
+        // Delete related project data
+        $project_item = new Item_Project();
+        $project_item->deleteByCriteria(
+            crit   : ['itemtype' => $classname],
+            force  : true,
+            history: false
+        );
+
+        // Clean history related to projects (both sides of the relation)
+        $this->deleteRelationLogs($classname, Project::getType());
+
+        // Clean display preferences
+        $this->deleteDisplayPreferences(
+            $classname,
+            Project::rawSearchOptionsToAdd()
+        );
+    }
+}
