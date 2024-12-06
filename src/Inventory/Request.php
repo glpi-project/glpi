27ffe2a2fc7346b58165d0a04a22d7fa7195124c
LAUNAY Samuel <107540223+Lainow@users.noreply.github.com>
2024-12-06 08:17:02 +0100

    Fix add inventory task info (#18479)
    * Add inventory task info
    
    * Remove useless line
    
    * Update src/Inventory/Request.php
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    ---------
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- src/Inventory/Request.php
+++ src/Inventory/Request.php
@@ -319,7 +319,24 @@ class Request extends AbstractRequest
         //For the moment it's the Agent who informs us about the active tasks
         $raw_data = $this->inventory->getRawData();
         if ($raw_data !== null && property_exists($raw_data, 'enabled-tasks')) {
-            foreach ($raw_data->{'enabled-tasks'} as $task) {
+            $enabled_tasks = $raw_data->{'enabled-tasks'};
+
+            // The following tasks depends on inventory.
+            // When they are enabled, we assume that inventory is enabled.
+            $taskneededinv = [
+                'esx',
+                'netdiscovery',
+                'netinventory',
+                'remoteinventory',
+            ];
+            if (
+                !empty(array_intersect($enabled_tasks, $taskneededinv)) &&
+                !in_array('inventory', $enabled_tasks)
+            ) {
+                $enabled_tasks[] = 'inventory';
+            }
+
+            foreach ($enabled_tasks as $task) {
                 $handle = $this->handleTask($task);
                 if (is_array($handle) && count($handle)) {
                     // Insert related task information under tasks list property
