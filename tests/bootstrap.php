449f7199938f2f936c3bb308901328c8de3745f2
Cédric Anne <cedric.anne@gmail.com>
2024-12-19 08:10:37 +0100

    check DB up-to-date status before running tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -48,7 +48,7 @@ define('GLPI_STRICT_DEPRECATED', true); //enable strict depreciations
 define('TU_USER', '_test_user');
 define('TU_PASS', 'PhpUnit_4');
 
-global $CFG_GLPI, $GLPI_CACHE;
+global $GLPI_CACHE;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -57,6 +57,10 @@ $kernel->boot();
 
 if (!file_exists(GLPI_CONFIG_DIR . '/config_db.php')) {
     echo("\nConfiguration file for tests not found\n\nrun: php bin/console database:install --env=testing ...\n\n");
+    exit(1);
+}
+if (!defined('SKIP_UPDATES') && !Update::isDbUpToDate()) {
+    echo 'The GLPI codebase has been updated. The update of the GLPI database is necessary.' . PHP_EOL;
     exit(1);
 }
 
