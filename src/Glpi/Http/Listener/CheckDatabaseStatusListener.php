e8f0d4f8f0d8474d46c57f0fbcce5a7658966a75
Cédric Anne <cedric.anne@gmail.com>
2024-12-19 10:26:13 +0100

    Use exceptions instead of rendering manually the error page

--- src/Glpi/Http/Listener/CheckDatabaseStatusListener.php
+++ src/Glpi/Http/Listener/CheckDatabaseStatusListener.php
@@ -36,12 +36,10 @@ namespace Glpi\Http\Listener;
 
 use Config;
 use DBmysql;
-use Glpi\Application\View\TemplateRenderer;
 use Glpi\Http\RequestPoliciesTrait;
 use Glpi\Kernel\ListenersPriority;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -76,46 +74,32 @@ final class CheckDatabaseStatusListener implements EventSubscriberInterface
             return;
         }
 
-        $response = null;
         if (!($DB instanceof DBmysql)) {
-            $show_link = file_exists($this->projectDir . '/install/install.php');
-
-            $response = $this->getErrorResponse(
-                message: sprintf(
-                    __('The database configuration file "%s" is missing or is corrupted. You have to either restart the install process, or restore this file.'),
-                    GLPI_CONFIG_DIR . '/config_db.php'
-                ),
-                link_url: $show_link ? $event->getRequest()->getBasePath() . '/install/install.php' : null,
-                link_text: $show_link ? __('Go to install page') : null,
+            $exception = new \Glpi\Exception\Http\HttpException(500);
+            $exception->setMessageToDisplay(
+                __('The database configuration file is missing or is corrupted. You have to either restart the install process, or restore this file.')
             );
-        } elseif (!$DB->connected) {
-            $response = $this->getErrorResponse(
-                message: __('The connection to the SQL server could not be established. Please check your configuration.'),
-            );
-        } elseif (!Config::isLegacyConfigurationLoaded()) {
-            $response = $this->getErrorResponse(
-                message: __('Unable to load the GLPI configuration from the database.'),
-            );
+            if (file_exists($this->projectDir . '/install/install.php')) {
+                $exception->setLinkText(__('Go to install page'));
+                $exception->setLinkUrl($event->getRequest()->getBasePath() . '/install/install.php');
+            }
+            throw $exception;
         }
 
-        if ($response !== null) {
-            $event->setResponse($response);
+        if (!$DB->connected) {
+            $exception = new \Glpi\Exception\Http\HttpException(500);
+            $exception->setMessageToDisplay(
+                __('The connection to the SQL server could not be established. Please check your configuration.')
+            );
+            throw $exception;
         }
-    }
 
-    private function getErrorResponse(string $message, ?string $link_url = null, ?string $link_text = null): Response
-    {
-        $content = TemplateRenderer::getInstance()->render(
-            'error_page.html.twig',
-            [
-                'header_method' => 'nullHeader',
-                'page_title'    => _n('Error', 'Errors', 1),
-                'message'       => $message,
-                'link_url'      => $link_url,
-                'link_text'     => $link_text,
-            ]
-        );
-
-        return new Response($content, 500);
+        if (!Config::isLegacyConfigurationLoaded()) {
+            $exception = new \Glpi\Exception\Http\HttpException(500);
+            $exception->setMessageToDisplay(
+                __('Unable to load the GLPI configuration from the database.')
+            );
+            throw $exception;
+        }
     }
 }
