1c14c1535e20602d748e0cc7f265e16ca1f410d0
Johan Cwiklinski <jcwiklinski@teclib.com>
2025-01-07 09:44:50 +0100

    HTML 5 doctype and headers for notifications (#18663)
    * HTML 5 doctype and headers for notifications
    
    closes #18217
    
    * Apply suggestions from code review
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    ---------
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- src/NotificationTemplate.php
+++ src/NotificationTemplate.php
@@ -273,11 +273,12 @@ class NotificationTemplate extends CommonDBTM
                     $css = $this->fields['css'] ?? '';
 
                     $lang['content_html'] =
-                     "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"
-                        'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>" .
+
+                     "<!DOCTYPE html>" .
                      "<html>
                         <head>
-                         <META http-equiv='Content-Type' content='text/html; charset=utf-8'>
+                         <meta charset='utf-8' />
+                         <meta name='viewport' content='width=device-width, initial-scale=1' />
                          <title>" . htmlescape($lang['subject']) . "</title>
                          <style type='text/css'>
                            {$css}
