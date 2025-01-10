b6d807837492b8ee96101fabb2c119b55c59dde8
Sebastien Monterisi <smonterisi@teclib.com>
2025-01-10 12:20:11 +0100

    update description to database:configure console command

--- src/Glpi/Console/Database/ConfigureCommand.php
+++ src/Glpi/Console/Database/ConfigureCommand.php
@@ -48,7 +48,7 @@ class ConfigureCommand extends AbstractConfigureCommand implements Configuration
 
         $this->setName('database:configure');
         $this->setAliases(['db:configure']);
-        $this->setDescription('Define database configuration');
+        $this->setDescription('Define database configuration and writes it to config_db.php');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
