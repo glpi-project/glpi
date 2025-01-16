e611a0f7f74e327f583478c8cdabbb309d2b4beb
Curtis Conard <cconard96@gmail.com>
2025-01-16 05:48:49 -0500

    add test

--- phpunit/functional/UserTest.php
+++ phpunit/functional/UserTest.php
@@ -2056,4 +2056,67 @@ class UserTest extends \DbTestCase
 
         $this->assertEquals(['name' => 'test'], $fields);
     }
+
+    public function testReapplyRightRules() {
+        $this->login();
+        $entities_id = $this->getTestRootEntity(true);
+
+        $user = new \User();
+        $user->getFromDB($_SESSION['glpiID']);
+
+        // Create a group that will be used to add a profile
+        $group = new \Group();
+        $groups_id = $group->add([
+            'name' => __FUNCTION__,
+            'entities_id' => $entities_id,
+        ]);
+
+        // Create a profile that will be added to the user
+        $profile = new \Profile();
+        $profiles_id = $profile->add([
+            'name' => __FUNCTION__,
+        ]);
+
+        // Create a rule that associates the profile to users with the group
+        $rule = new \RuleRight();
+        $rules_id = $rule->add([
+            'name' => __FUNCTION__,
+            'entities_id' => $entities_id,
+            'match' => 'AND',
+        ]);
+        (new \RuleCriteria())->add([
+            'rules_id' => $rules_id,
+            'criteria' => '_groups_id',
+            'condition' => 0,
+            'pattern' => $groups_id,
+        ]);
+        $action = new \RuleAction();
+        $action->add([
+            'rules_id' => $rules_id,
+            'action_type' => 'assign',
+            'field' => 'profiles_id',
+            'value' => $profiles_id,
+        ]);
+        $action->add([
+            'rules_id' => $rules_id,
+            'action_type' => 'assign',
+            'field' => 'entities_id',
+            'value' => $entities_id,
+        ]);
+
+        $this->assertNotContains($profiles_id, Profile_User::getUserProfiles($user->getID()));
+
+        $group_user = new \Group_User();
+        $group_user_id = $group_user->add([
+            'groups_id' => $groups_id,
+            'users_id' => $user->getID(),
+        ]);
+
+        $user->reapplyRightRules();
+        $this->assertContains($profiles_id, Profile_User::getUserProfiles($user->getID()));
+
+        $group_user->delete(['id' => $group_user_id]);
+        $user->reapplyRightRules();
+        $this->assertNotContains($profiles_id, Profile_User::getUserProfiles($user->getID()));
+    }
 }
