48d5767f3a7a01dab878110b9f0df18eb091bd65
Rom1-B <rbiot@teclib.com>
2025-01-20 13:57:42 +0100

    cedric

--- src/KnowbaseItem.php
+++ src/KnowbaseItem.php
@@ -568,30 +568,14 @@ class KnowbaseItem extends CommonDBVisible implements ExtraVisibilityCriteria, S
     {
         // Specific case for anonymous users + multi entities
         if (!Session::getLoginUserID()) {
-            $where = [
-                'OR' => [
-                    ['is_faq' => 1],
-                    [
-                        'is_token_url' => 1,
-                        'token' => $_GET['token'] ?? '',
-                    ],
-                ],
-            ];
+            $where = ['is_faq' => 1];
             if (Session::isMultiEntitiesMode()) {
                 $where[Entity_KnowbaseItem::getTableField('entities_id')] = 0;
                 $where[Entity_KnowbaseItem::getTableField('is_recursive')] = 1;
             }
         } else {
             $where = self::getVisibilityCriteriaKB();
-            $where[] = [
-                'OR' => [
-                    ['is_faq' => 1],
-                    [
-                        'is_token_url' => 1,
-                        'token' => $_GET['token'] ?? '',
-                    ],
-                ],
-            ];
+            $where['is_faq'] = 1;
         }
 
         return $where;
@@ -753,7 +737,16 @@ class KnowbaseItem extends CommonDBVisible implements ExtraVisibilityCriteria, S
             $input["name"] = __('New item');
         }
 
-        if (isset($input['is_token_url']) && $input['is_token_url'] && empty($this->fields['token'])) {
+        if (
+            (
+                isset($input['is_token_url'])
+                && $input['is_token_url']
+                && empty($this->fields['token'])
+            ) || (
+                isset($input['_reset_token_url'])
+                && $input['_reset_token_url']
+            )
+        ) {
             $input['token'] = Toolbox::getRandomString(20);
         }
 
@@ -894,7 +887,23 @@ class KnowbaseItem extends CommonDBVisible implements ExtraVisibilityCriteria, S
     {
         global $CFG_GLPI, $DB;
 
-        if (!$this->can($this->fields['id'], READ)) {
+        if (
+            $this->fields['is_token_url']
+            && isset($options['token'])
+            && $this->fields['token'] == $options['token']
+        ) {
+            $this->fields['answer'] = preg_replace(
+                [
+                    '/<img([^>]+src=["\'])([^"\']+)(["\'][^>]*)>/',
+                    '/<a([^>]+href=["\'])([^"\']+)(["\'][^>]*)>/'
+                ],
+                [
+                    '<img$1$2&token=' . $options['token'] . '$3>',
+                    '<a$1$2&token=' . $options['token'] . '$3>'
+                ],
+                $this->fields['answer']
+            );
+        } elseif (!$this->can($this->fields['id'], READ)) {
             return false;
         }
 
