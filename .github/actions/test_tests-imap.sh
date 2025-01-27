962ae5da29402cf2b7032ebe3602be91169edb45
Cédric Anne <cedric.anne@gmail.com>
2025-01-27 11:41:04 +0100

    Limit code coverage to PHPUnit unit/functional tests

--- .github/actions/test_tests-imap.sh
+++ .github/actions/test_tests-imap.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -e -u -x -o pipefail
 
-PHPUNIT_ADDITIONNAL_OPTIONS=""
-if [[ "$CODE_COVERAGE" = true ]]; then
-  export COVERAGE_DIR="coverage-imap"
-  PHPUNIT_ADDITIONNAL_OPTIONS="--coverage-filter src --coverage-clover phpunit/$COVERAGE_DIR/clover.xml"
-else
-  PHPUNIT_ADDITIONNAL_OPTIONS="--no-coverage";
-fi
-
-vendor/bin/phpunit $PHPUNIT_ADDITIONNAL_OPTIONS phpunit/imap
-
-unset COVERAGE_DIR
+vendor/bin/phpunit phpunit/imap $@
