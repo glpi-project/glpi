5091fc3de0eeb586a5c74ca4ba2bda450fa7f036
Cédric Anne <cedric.anne@gmail.com>
2025-01-27 11:41:04 +0100

    Limit code coverage to PHPUnit unit/functional tests

--- .github/actions/test_tests-cache.sh
+++ .github/actions/test_tests-cache.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 set -e -u -x -o pipefail
 
-PHPUNIT_ADDITIONNAL_OPTIONS=""
-if [[ "${CODE_COVERAGE:-}" = true ]]; then
-  export COVERAGE_DIR="coverage-functional"
-  PHPUNIT_ADDITIONNAL_OPTIONS="--coverage-filter src --coverage-clover phpunit/$COVERAGE_DIR/clover.xml"
-
-else
-  PHPUNIT_ADDITIONNAL_OPTIONS="--no-coverage";
-fi
-
 for CONFIG in {"--use-default","--dsn=memcached://memcached","--dsn=redis://redis"}; do
   php bin/console cache:configure \
     --ansi --no-interaction \
     $CONFIG
-  vendor/bin/phpunit --group cache $PHPUNIT_ADDITIONNAL_OPTIONS $@
+  vendor/bin/phpunit --group cache $@
 done
-
-unset COVERAGE_DIR
--- .github/actions/test_tests-ldap.sh
+++ .github/actions/test_tests-ldap.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e -u -x -o pipefail
 
-ATOUM_ADDITIONNAL_OPTIONS=""
-if [[ "$CODE_COVERAGE" = true ]]; then
-  export COVERAGE_DIR="coverage-ldap"
-else
-  ATOUM_ADDITIONNAL_OPTIONS="--no-code-coverage";
-fi
-
 vendor/bin/atoum \
   -p 'php -d memory_limit=512M' \
   --debug \
@@ -16,8 +9,6 @@ vendor/bin/atoum \
   --bootstrap-file tests/bootstrap.php \
   --fail-if-void-methods \
   --fail-if-skipped-methods \
-  $ATOUM_ADDITIONNAL_OPTIONS \
+  --no-code-coverage \
   --max-children-number 1 \
   -d tests/LDAP
-
-unset COVERAGE_DIR
