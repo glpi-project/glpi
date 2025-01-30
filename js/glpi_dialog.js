dc0a962838ae94ba99d568fe4e84d4da94b99cc6
Johan Cwiklinski <jcwiklinski@teclib.com>
2025-01-30 13:51:04 +0100

    Information is uncountable

--- js/glpi_dialog.js
+++ js/glpi_dialog.js
@@ -384,7 +384,7 @@ const glpi_toast_success = (message, caption, options = {}) => {
  * @param {ToastOptions} options Toast options
  */
 const glpi_toast_info = function(message, caption, options = {}) {
-    glpi_toast(caption || _n("Information", "Informations", 1), message, 'bg-info text-white border-0', options);
+    glpi_toast(caption || _n("Information", "Information", 1), message, 'bg-info text-white border-0', options);
 };
 
 /**
