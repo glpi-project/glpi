3c29e7c0fb729bd4ff32568d3843c512cfd51cec
Cédric Anne <cedric.anne@gmail.com>
2025-02-24 17:05:57 +0100

    Enhance the plugin migration framework
    - Move progress feedback messages below the progress bar.
    - Add missing spacing between icons and texts in command result output.
    - Do not update items when the plugin item is outdated.
    - Do not update items when there is actually no value change.
    - Never output a message in both progress indicator and result.
    - Add a method to validate DB fields existence.

--- phpunit/functional/Glpi/Migration/AbstractPluginMigrationTest.php
+++ phpunit/functional/Glpi/Migration/AbstractPluginMigrationTest.php
@@ -40,8 +40,10 @@ use DbTestCase;
 use Entity;
 use Glpi\Message\MessageType;
 use Glpi\Migration\AbstractPluginMigration;
+use Glpi\Migration\PluginMigrationResult;
 use Monitor;
 use Psr\Log\LoggerInterface;
+use ReflectionClass;
 
 class AbstractPluginMigrationTest extends DbTestCase
 {
@@ -132,7 +134,7 @@ class AbstractPluginMigrationTest extends DbTestCase
             protected function processMigration(): bool
             {
                 // ... do something
-                $this->addMessage(MessageType::Success, 'All data has been imported.');
+                $this->result->addMessage(MessageType::Success, 'All data has been imported.');
                 return true;
             }
         };
@@ -166,7 +168,7 @@ class AbstractPluginMigrationTest extends DbTestCase
         $instance = new class ($db, $logger) extends AbstractPluginMigration {
             protected function validatePrerequisites(): bool
             {
-                $this->addMessage(MessageType::Notice, 'Plugin\'s data can be imported.');
+                $this->result->addMessage(MessageType::Notice, 'Plugin\'s data can be imported.');
                 return true;
             }
 
@@ -215,7 +217,7 @@ class AbstractPluginMigrationTest extends DbTestCase
             protected function processMigration(): bool
             {
                 // ... do something
-                $this->addMessage(MessageType::Success, 'The migration simulation succeed.');
+                $this->result->addMessage(MessageType::Success, 'The migration simulation succeed.');
                 return true;
             }
         };
@@ -249,7 +251,7 @@ class AbstractPluginMigrationTest extends DbTestCase
         $instance = new class ($db, $logger) extends AbstractPluginMigration {
             protected function validatePrerequisites(): bool
             {
-                $this->addMessage(MessageType::Notice, 'Plugin\'s data can be imported.');
+                $this->result->addMessage(MessageType::Notice, 'Plugin\'s data can be imported.');
                 return true;
             }
 
@@ -279,6 +281,85 @@ class AbstractPluginMigrationTest extends DbTestCase
         $this->assertEquals($expected_messages, $result->getMessages());
     }
 
+    public function testCheckDbFieldsExists(): void
+    {
+        // Arrange
+        $db = $this->createMock(DBmysql::class);
+        $db->method('tableExists')->willReturnCallback(function ($table) {
+            return match ($table) {
+                'glpi_plugins_myplugin_items' => true,
+                'glpi_plugins_myplugin_tickets' => true,
+                'glpi_plugins_myplugin_bars' => true,
+                default => false,
+            };
+        });
+        $db->method('fieldExists')->willReturnCallback(function ($table, $field) {
+            return match ($table . '.' . $field) {
+                'glpi_plugins_myplugin_items.id' => true,
+                'glpi_plugins_myplugin_items.name' => true,
+                'glpi_plugins_myplugin_tickets.id' => true,
+                'glpi_plugins_myplugin_tickets.name' => true,
+                'glpi_plugins_myplugin_tickets.content' => true,
+                'glpi_plugins_myplugin_bars.id' => true,
+                default => false,
+            };
+        });
+
+        $instance = $this->getMockBuilder(AbstractPluginMigration::class)
+            ->setConstructorArgs([$db, $this->createMock(LoggerInterface::class)])
+            ->onlyMethods(['execute', 'validatePrerequisites', 'processMigration'])
+            ->getMock();
+
+        $reflected_class = new ReflectionClass(AbstractPluginMigration::class);
+
+        // Act
+        $reflected_class->getProperty('result')->setValue($instance, $result = new PluginMigrationResult());
+        $return_1 = $this->callPrivateMethod(
+            $instance,
+            'checkDbFieldsExists',
+            [
+                'glpi_plugins_myplugin_items' => ['id', 'name', 'items_id', 'plugin_myplugin_categories_id'],
+                'glpi_plugins_myplugin_tickets' => ['id', 'name', 'content'],
+                'glpi_plugins_myplugin_categories' => ['id', 'name'],
+                'glpi_plugins_myplugin_foos' => ['id', 'name'],
+                'glpi_plugins_myplugin_bars' => ['id', 'name'],
+            ]
+        );
+        $messages_1 = $result->getMessages();
+
+        $reflected_class->getProperty('result')->setValue($instance, $result = new PluginMigrationResult());
+        $return_2 = $this->callPrivateMethod(
+            $instance,
+            'checkDbFieldsExists',
+            [
+                'glpi_plugins_myplugin_items' => ['id', 'name'],
+                'glpi_plugins_myplugin_tickets' => ['id', 'name', 'content'],
+            ]
+        );
+        $messages_2 = $result->getMessages();
+
+        // Assert
+        $this->assertEquals(false, $return_1);
+        $expected_messages = [
+            [
+                'type' => MessageType::Error,
+                'message' => 'The database structure does not contain all the data required for migration.',
+            ],
+            [
+                'type' => MessageType::Error,
+                'message' => 'The following database tables are missing: `glpi_plugins_myplugin_categories`, `glpi_plugins_myplugin_foos`.',
+            ],
+            [
+                'type' => MessageType::Error,
+                'message' => 'The following database fields are missing: `glpi_plugins_myplugin_items.items_id`, `glpi_plugins_myplugin_items.plugin_myplugin_categories_id`, `glpi_plugins_myplugin_bars.name`.',
+            ],
+        ];
+        $this->assertEquals($expected_messages, $messages_1);
+
+        $this->assertEquals(true, $return_2);
+        $this->assertEquals([], $messages_2);
+    }
+
     public function testImportItem(): void
     {
         // Arrange
@@ -295,7 +376,7 @@ class AbstractPluginMigrationTest extends DbTestCase
             {
                 $entity_id = \getItemByTypeName(Entity::class, '_test_root_entity', true);
 
-                // Reconciliation criteria matches an existing item, it should be updated
+                // Reconciliation criteria matches an existing item, it should be updated ...
                 $this->importItem(
                     Computer::class,
                     [
@@ -306,6 +387,31 @@ class AbstractPluginMigrationTest extends DbTestCase
                     [
                         'name' => '_test_pc01',
                         'entities_id' => $entity_id,
+                    ]
+                );
+
+                // ... unless the values are identical
+                $this->importItem(
+                    Computer::class,
+                    [
+                        'name' => '_test_pc02',
+                        'comment' => 'Comment for computer _test_pc02',
+                    ],
+                    [
+                        'name' => '_test_pc02',
+                    ]
+                );
+
+                // ... or the plugin item is outdated
+                $this->importItem(
+                    Computer::class,
+                    [
+                        'name' => '_test_pc03',
+                        'comment' => 'Imported from myplugin',
+                        'date_mod' => '2015-12-03 14:56:34',
+                    ],
+                    [
+                        'name' => '_test_pc03',
                     ]
                 );
 
@@ -345,8 +451,10 @@ class AbstractPluginMigrationTest extends DbTestCase
         $result = $instance->execute(simulate: true);
 
         $computer_id_1 = \getItemByTypeName(Computer::class, '_test_pc01', true);
-        $computer_id_2 = \getItemByTypeName(Computer::class, 'a test computer', true);
-        $computer_id_3 = \getItemByTypeName(Computer::class, 'another test computer', true);
+        $computer_id_2 = \getItemByTypeName(Computer::class, '_test_pc02', true);
+        $computer_id_3 = \getItemByTypeName(Computer::class, '_test_pc03', true);
+        $computer_id_4 = \getItemByTypeName(Computer::class, 'a test computer', true);
+        $computer_id_5 = \getItemByTypeName(Computer::class, 'another test computer', true);
 
         // Assert
         $this->assertTrue($result->isFullyProcessed());
@@ -358,17 +466,25 @@ class AbstractPluginMigrationTest extends DbTestCase
             ],
             [
                 'type' => MessageType::Debug,
-                'message' => sprintf('Computer "a test computer" (%d) has been created.', $computer_id_2),
+                'message' => sprintf('Computer "_test_pc02" (%d) is already up-to-date, its update has been skipped.', $computer_id_2),
             ],
             [
                 'type' => MessageType::Debug,
-                'message' => sprintf('Computer "another test computer" (%d) has been created.', $computer_id_3),
+                'message' => sprintf('Computer "_test_pc03" (%d) is most recent on GLPI side, its update has been skipped.', $computer_id_3),
+            ],
+            [
+                'type' => MessageType::Debug,
+                'message' => sprintf('Computer "a test computer" (%d) has been created.', $computer_id_4),
+            ],
+            [
+                'type' => MessageType::Debug,
+                'message' => sprintf('Computer "another test computer" (%d) has been created.', $computer_id_5),
             ],
         ];
         $this->assertEquals($expected_messages, $result->getMessages());
 
-        $this->assertEquals([Computer::class => [$computer_id_2, $computer_id_3]], $result->getCreatedItemsIds());
-        $this->assertEquals([Computer::class => [$computer_id_1]], $result->getUpdatedItemsIds());
+        $this->assertEquals([Computer::class => [$computer_id_4, $computer_id_5]], $result->getCreatedItemsIds());
+        $this->assertEquals([Computer::class => [$computer_id_1, $computer_id_2, $computer_id_3]], $result->getReusedItemsIds());
     }
 
     public function testMapItem(): void
