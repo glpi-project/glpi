d61d2aafdff751486471a4f091a3687b218f58bb
Clément Cailly <ccailly@teclib.com>
2025-02-26 10:07:00 +0100

    fix(forms): PHP Lint

--- ajax/getFormQuestionActorsDropdownValue.php
+++ ajax/getFormQuestionActorsDropdownValue.php
@@ -55,7 +55,7 @@ if (Session::getCurrentInterface() !== 'central') {
     if (
         array_reduce(
             $questions,
-            fn($acc, $question) => $acc || Question::getById($question['id'])?->canViewItem(),
+            fn($acc, $question) => $acc || Question::getById($question['id'])->canViewItem(),
             false
         ) === false
     ) {
