c222ba41b8e9b0c26067b35c0acd1557754854ac
Romain B. <8530352+Rom1-B@users.noreply.github.com>
2025-02-27 11:33:33 +0100

    fix(dashboard): groups lost in session

--- src/Glpi/Dashboard/Grid.php
+++ src/Glpi/Dashboard/Grid.php
@@ -493,6 +493,28 @@ TWIG, $twig_params);
 
 
     /**
+     * Init embed session
+     */
+    public function initEmbedSession(array $params = [])
+    {
+        // load minimal session
+        $_SESSION["glpiactive_entity"]           = $params['entities_id'];
+        $_SESSION["glpiactive_entity_recursive"] = $params['is_recursive'];
+        $_SESSION["glpiname"]                    = 'embed_dashboard';
+        $_SESSION['glpigroups']                  = [];
+        if ($params['is_recursive']) {
+            $entities = getSonsOf("glpi_entities", $params['entities_id']);
+        } else {
+            $entities = [$params['entities_id']];
+        }
+        $_SESSION['glpiactiveentities']        = $entities;
+        $_SESSION['glpiactiveentities_string'] = "'" . implode("', '", $entities) . "'";
+
+        $_SESSION['glpi_use_mode'] = Session::NORMAL_MODE;
+    }
+
+
+    /**
      * Show an embeded dashboard.
      * We must check token validity to avoid displaying dashboard to invalid users
      *
@@ -519,21 +541,9 @@ TWIG, $twig_params);
         }
 
         self::$embed = true;
+        $this->initEmbedSession($params);
 
-       // load minimal session
-        $_SESSION["glpiactive_entity"]           = $params['entities_id'];
-        $_SESSION["glpiactive_entity_recursive"] = $params['is_recursive'];
-        $_SESSION["glpiname"]                    = 'embed_dashboard';
-        $_SESSION["glpigroups"]                  = [];
-        if ($params['is_recursive']) {
-            $entities = getSonsOf("glpi_entities", $params['entities_id']);
-        } else {
-            $entities = [$params['entities_id']];
-        }
-        $_SESSION['glpiactiveentities']        = $entities;
-        $_SESSION['glpiactiveentities_string'] = "'" . implode("', '", $entities) . "'";
-
-       // show embeded dashboard
+        // show embeded dashboard
         $this->show(true, $params['token']);
     }
 
