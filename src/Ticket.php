52902718cc1a3549134162e0d5baf876e67e9746
Rom1-B <rbiot@teclib.com>
2025-03-03 16:26:14 +0100

    fix(ticket): images disabled in massive solution

--- src/Ticket.php
+++ src/Ticket.php
@@ -2299,9 +2299,6 @@ JAVASCRIPT;
                     'editor_id'         => $content_id,
                     'enable_fileupload' => false,
                     'enable_richtext'   => true,
-                            // Uploaded images processing is not able to handle multiple use of same uploaded file, so until this is fixed,
-                            // it is preferable to disable image pasting in rich text inside massive actions.
-                    'enable_images'     => false,
                     'cols'              => 12,
                     'rows'              => 80
                 ]);
