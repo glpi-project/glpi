90e7bcd611c4f66ab9f4b07a49e9d1a50813aad0
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2025-03-21 23:09:03 +0100

    fix filter in search result display and prevent submit on Enter (#19248)

---
+++ tests/cypress/e2e/Setup/General/displaypreference_list.cy.js
@@ -0,0 +1,55 @@
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2025 Teclib' and contributors.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+describe('Display preference list', () => {
+    beforeEach(() => {
+        cy.login();
+        cy.changeProfile('Super-Admin');
+    });
+
+    it('Filter the displaypreference list', () => {
+        cy.visit('/front/config.form.php');
+        cy.findByRole('tab', { name: 'Search result display' }).click();
+
+        cy.get('#search-itemtype').clear();
+        cy.get('#search-itemtype').type('user');
+
+        cy.get('[data-itemtype=User').should('be.visible');
+        cy.get('[data-itemtype=Computer').should('not.be.visible');
+
+        cy.get('#search-itemtype').clear();
+        cy.get('#search-itemtype').type('computer');
+
+        cy.get('[data-itemtype=User').should('not.be.visible');
+        cy.get('[data-itemtype=Computer').should('be.visible');
+    });
+});
