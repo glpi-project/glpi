ca69a301d9a30c0992a48dda3c41a3b51ecb229f
Johan Cwiklinski <jcwiklinski@teclib.com>
2025-03-24 13:32:36 +0100

    Remove wrong copyright on test files

--- tests/cypress/e2e/dashboards.cy.js
+++ tests/cypress/e2e/dashboards.cy.js
@@ -6,7 +6,6 @@
  * http://glpi-project.org
  *
  * @copyright 2015-2025 Teclib' and contributors.
- * @copyright 2003-2014 by the INDEPNET Development Team.
  * @licence   https://www.gnu.org/licenses/gpl-3.0.html
  *
  * ---------------------------------------------------------------------
@@ -30,6 +29,7 @@
  *
  * ---------------------------------------------------------------------
  */
+
 describe('Dashboard', () => {
     beforeEach(() => {
         cy.login();
