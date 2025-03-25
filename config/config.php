29e48afec4cd1fc468c62bfea39574ffd2d849fb
Arun M <123149917+ArunMuthukumarasamy@users.noreply.github.com>
2025-03-25 16:33:06 +0530

    Create config.php
    config DB details

---
+++ config/config.php
@@ -0,0 +1,5 @@
+define("DB_HOST", "glpi-mysql-server.mysql.database.azure.com");
+define("DB_NAME", "glpidb");
+define("DB_USER", "glpiuser");
+define("DB_PASSWORD", "Password@123");
+define("DB_CHARSET", "utf8mb4");
