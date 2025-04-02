aa8034f7226a90218dbcbcf7ee357cab9832f216
RomainLvr <lecouvreurromain@gmail.com>
2025-04-02 11:12:21 +0200

    Fix getFriendlyNameFields() query to return only first name or last name if only one is defined

--- src/User.php
+++ src/User.php
@@ -6565,11 +6565,12 @@ HTML;
         $alias  = DBmysql::quoteName($alias);
         $name   = DBmysql::quoteName($table . '.' . self::getNameField());
 
-        return new QueryExpression("IF(
-            $first <> '' && $second <> '',
-            CONCAT($first, ' ', $second),
-            $name
-         ) AS $alias");
+        return new QueryExpression("CASE
+            WHEN $first <> '' AND $second <> '' THEN CONCAT($first, ' ', $second)
+            WHEN $first <> '' THEN $first
+            WHEN $second <> '' THEN $second
+            ELSE $name
+        END AS $alias");
     }
 
     public static function getIcon()
