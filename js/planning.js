13d574f15864ebbccf1793c2742f7754c2feee45
Stanislas <skita@teclib.com>
2025-04-15 12:20:37 +0200

    Force change if needed

--- js/planning.js
+++ js/planning.js
@@ -625,6 +625,16 @@ var GLPIPlanning  = {
             }
         });
 
+        // Load the last known view only if it is valid (else load default view)
+        const view = this.calendar.isValidViewType(options.default_view)
+        ? options.default_view
+        : default_options.default_view;
+
+        // force view only if it is not the same as the default use on itinitialization
+        if (options.default_view !== view) {
+            this.calendar.changeView(view);
+        }
+
         $('.planning_on_central a')
             .mousedown(function() {
                 disable_qtip = true;
