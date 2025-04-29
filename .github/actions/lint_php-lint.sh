54ca092625e583e66b48a5951d1268d6aead5e13
Johan Cwiklinski <johan@x-tnd.be>
2025-04-29 14:36:48 +0200

    Add php-cs-fixer (no composer.lock)

--- .github/actions/lint_php-lint.sh
+++ .github/actions/lint_php-lint.sh
@@ -13,10 +13,10 @@ vendor/bin/parallel-lint \
 
 php -d memory_limit=1G vendor/bin/composer-require-checker check --config-file=.composer-require-checker.config.json
 
-touch ~/phpcs.cache
-vendor/bin/phpcs \
-  --cache ~/phpcs.cache \
-  .
+PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer check \
+  --show-progress=dots \
+  --verbose \
+  --diff
 
 echo "Run code static analysis"
 vendor/bin/phpstan analyze \
