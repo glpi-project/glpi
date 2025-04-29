3b310338be7a3517cbdcd51e79458ed4f6fb5aca
Clément Cailly <ccailly@teclib.com>
2025-04-29 15:20:38 +0200

    fix: lint tests

--- src/Glpi/Assets/ImportMapGenerator.php
+++ src/Glpi/Assets/ImportMapGenerator.php
@@ -1,14 +1,13 @@
 <?php
 
 /**
- * --------------------------------------------------------------------
+ * ---------------------------------------------------------------------
  *
  * GLPI - Gestionnaire Libre de Parc Informatique
  *
  * http://glpi-project.org
  *
  * @copyright 2015-2025 Teclib' and contributors.
- * @copyright 2003-2014 by the INDEPNET Development Team.
  * @licence   https://www.gnu.org/licenses/gpl-3.0.html
  *
  * ---------------------------------------------------------------------
