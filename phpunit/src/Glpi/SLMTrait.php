474557156ead4dbe06f4908ee694272b52b8dbb3
Sebastien Monterisi <smonterisi@teclib.com>
2025-05-05 16:52:29 +0200

    rules : action to associate an ola + criterion to match an ola

---
+++ phpunit/src/Glpi/SLMTrait.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Glpi\PHPUnit\Tests\Glpi;
+
+use OLA;
+use SLM;
+
+trait SLMTrait
+{
+    // default delay for tto & ttr ola
+    // see @\LevelAgreement::getDefinitionTimeValues() for available unit values
+    const OLA_TTO_DELAY = [90, 'minute'];
+    const OLA_TTR_DELAY = [2, 'day'];
+
+    /**
+     * @param array $data
+     * @param int $ola_type
+     * @param \Group|null $group
+     * @param \SLM|null $slm
+     *
+     * @return array{ola: OLA, slm: SLM, group: \Group}
+     */
+    private function createOLA(array $data = [], int $ola_type = SLM::TTO, ?\Group $group = null, ?SLM $slm = null): array
+    {
+        assert(in_array($ola_type, array_keys(OLA::getTypes())));
+        $slm = $slm ?? $this->createSLM();
+        $group = $group ?? $this->createGroup();
+
+        [$amount, $unit] = match ($ola_type) {
+            SLM::TTO => self::OLA_TTO_DELAY,
+            SLM::TTR => self::OLA_TTR_DELAY,
+        };
+
+        $ola = $this->createItem(
+            Ola::class,
+            $data + [
+                //                'id' => 0,
+                'name' => 'OLA ' . time(),
+                //                'entities_id' => 0,
+                'is_recursive' => 1, // @todoseb voir avec quelqu'un le fonctionnement de l'entité 0 et de la récursivité. car la requete Item_Ola::getListForItem() dans \Ticket::getAssociatedOlas attend soit de la récursivité, soit une entité != 0
+                'type' => $ola_type,
+                'comment' => 'OLA comment ' . time(),
+                'number_time' => $amount,
+                'definition_time' => $unit,
+                //                'use_ticket_calendar' => 0,
+                //                'calendars_id' => 0,
+                //                'date_mod' => null,
+                //                'end_of_working_day' => 0,
+                //                'date_creation' => null,
+                'slms_id' => $slm->getID(),
+                'groups_id' => $group->getID(),
+            ]
+        );
+
+        return ['ola' => $ola, 'slm' => $slm, 'group' => $group];
+    }
+
+    private function createSLM(array $data = [], ?\Calendar $calendar = null): SLM
+    {
+        $calendar = $calendar ?? $this->createCalendar();
+
+        return $this->createItem(
+            SLM::class,
+            $data + [
+                'id' => 0,
+                'name' => 'SLM name ' . time(),
+                //                'entities_id' => 0,
+                //                'is_recursive' => 0,
+                'comment' => 'Slm comment text',
+                //                'use_ticket_calendar' => 0,
+                'calendars_id' => $calendar->getID(),
+                //                'date_mod' => null,
+                //                'date_creation' => null,
+            ]
+        );
+    }
+
+    private function createGroup(): \Group
+    {
+        return $this->createItem(\Group::class, [
+//            'id' => 0,
+//            'entities_id' => 0,
+//            'is_recursive' => 0,
+            'name' => 'Group name ' . time(),
+//            'code' => null,
+            'comment' => 'Group comment text ' . time(),
+//            'ldap_field' => null,
+//            'ldap_value' => null,
+//            'ldap_group_dn' => null,
+//            'date_mod' => null,
+//            'groups_id' => 0,
+//            'completename' => null,
+//            'level' => 0,
+//            'ancestors_cache' => null,
+//            'sons_cache' => null,
+//            'is_requester' => 1,
+//            'is_watcher' => 1,
+//            'is_assign' => 1,
+//            'is_task' => 1,
+//            'is_notify' => 1,
+//            'is_itemgroup' => 1,
+//            'is_usergroup' => 1,
+//            'is_manager' => 1,
+//            'date_creation' => null,
+//            'recursive_membership' => 0,
+//            '2fa_enforced' => 0,
+        ]);
+    }
+
+    /**
+     * Calendar with today & tomorrow set a working day (9:00 to 19:00)
+     */
+    private function createCalendar(): \Calendar
+    {
+        $calendar = $this->createItem(\Calendar::class, ['name' => 'Test Calendar ' . time()]);
+        // today
+        $this->createItem(
+            \CalendarSegment::class,
+            [
+                'calendars_id' => $calendar->getID(),
+                'day'          => (int)date('w'),
+                'begin'        => '09:00:00',
+                'end'          => '19:00:00'
+            ]
+        );
+        // tomorrow
+        $this->createItem(
+            \CalendarSegment::class,
+            [
+                'calendars_id' => $calendar->getID(),
+                'day'          => (int)date('w') === 6 ? 0 : (int)date('w') + 1, // day of the week number
+                'begin'        => '09:00:00',
+                'end'          => '19:00:00'
+            ]
+        );
+
+        return $calendar;
+    }
+
+}
