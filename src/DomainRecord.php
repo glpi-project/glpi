425235d1bde9ade4249331b4c15a00ebd2125bcc
Curtis Conard <cconard96@gmail.com>
2025-05-05 09:13:30 -0400

    Fix viewing unattached domain records

--- src/DomainRecord.php
+++ src/DomainRecord.php
@@ -42,6 +42,7 @@ class DomainRecord extends CommonDBChild
     public static $itemtype        = 'Domain';
     public static $items_id        = 'domains_id';
     public $dohistory              = true;
+    public static $mustBeAttached  = false;
 
     public static function getTypeName($nb = 0)
     {
@@ -261,6 +262,14 @@ class DomainRecord extends CommonDBChild
      */
     private function prepareInput($input, $add = false)
     {
+        if (($add && empty($input['domains_id'])) || (isset($input['domains_id']) && empty($input['domains_id']))) {
+            Session::addMessageAfterRedirect(
+                __('A domain is required'),
+                true,
+                ERROR
+            );
+            return false;
+        }
 
         if ($add) {
             if (isset($input['date_creation']) && empty($input['date_creation'])) {
@@ -364,6 +373,7 @@ class DomainRecord extends CommonDBChild
                     'value'  => $this->fields["domains_id"],
                     'entity' => $this->fields["entities_id"],
                     'rand'   => $rand,
+                    'display_emptychoice' => false,
                 ]
             );
         }
