e6a4d6833ccb5b4e1250288591f0aa2580f55c6a
Stanislas <skita@teclib.com>
2025-05-13 12:46:13 +0200

    fix CS

--- src/MassiveAction.php
+++ src/MassiveAction.php
@@ -1622,9 +1622,8 @@ class MassiveAction
                                         if ($item2->getFromDB($input[$input["field"]])) {
                                             $related_item = $item2;
                                         }
-
-                                    // Case 2: The field is not a foreign key, but the target class supports connexity (relations)
-                                    // Use getConnexityItem() to dynamically resolve the related object based on the main itemtype and id (items_id)
+                                        // Case 2: The field is not a foreign key, but the target class supports connexity (relations)
+                                        // Use getConnexityItem() to dynamically resolve the related object based on the main itemtype and id (items_id)
                                     } else if (is_a($item2, CommonDBConnexity::class, true)) {
                                         $related_item = $item2->getConnexityItem($item->getType(), $key);
                                     }
