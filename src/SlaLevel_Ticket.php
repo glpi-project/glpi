2cc1c18a7c96825f526a4373653c7bd3e2fd7b32
Sébastien Monterisi <smonterisi@teclib.com>
2025-05-21 14:49:24 +0200

    no need for $DB->escape
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- src/SlaLevel_Ticket.php
+++ src/SlaLevel_Ticket.php
@@ -328,7 +328,7 @@ class SlaLevel_Ticket extends CommonDBTM
         /** @var \DBmysql $DB */
         global $DB;
 
-        $now = $DB->escape($_SESSION['glpi_currenttime']);
+        $now = $_SESSION['glpi_currenttime'];
 
         $criteria = [
             'SELECT'    => 'glpi_slalevels_tickets.*',
