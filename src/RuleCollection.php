7b541ff7cfb2377b539423524fb0ae3ae9e357f5
Sebastien Monterisi <smonterisi@teclib.com>
2025-05-23 12:25:34 +0200

    wip
    stupid commit, git history needs a rewrite to be reviewed.

--- src/RuleCollection.php
+++ src/RuleCollection.php
@@ -852,11 +852,6 @@ TWIG, $twig_params);
 
         // Get actual ranking of Rule to move
         $ruleDescription->getFromDB($ID);
-        if(!isset($ruleDescription->fields["ranking"]))
-        {
-            dump($ruleDescription);
-//            throw new \RuntimeException('Rule ranking not found ');
-        }
         $old_rank = $ruleDescription->fields["ranking"];
 
         $max_ranking_criteria = [
