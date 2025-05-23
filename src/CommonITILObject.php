336c77d1a46d0e04827038be409df871feaabf37
Sebastien Monterisi <smonterisi@teclib.com>
2025-05-23 15:03:32 +0200

    fix form submission

--- src/CommonITILObject.php
+++ src/CommonITILObject.php
@@ -2994,14 +2994,16 @@ abstract class CommonITILObject extends CommonDBTM implements KanbanInterface, T
                                     ) {
                                         unset($mandatory_missing[$dateField]);
                                     }
-                                    [$dateField, $olaField] = OLA::getFieldNames($slmType);
-                                    if (
-                                        ($key == $dateField)
-                                        && isset($input[$olaField]) && ($input[$olaField] > 0)
-                                        && isset($mandatory_missing[$dateField])
-                                    ) {
-                                        unset($mandatory_missing[$dateField]);
-                                    }
+
+                                    // @todoseb a réimplementer
+//                                    [$dateField, $olaField] = OLA::getFieldNames($slmType);
+//                                    if (
+//                                        ($key == $dateField)
+//                                        && isset($input[$olaField]) && ($input[$olaField] > 0)
+//                                        && isset($mandatory_missing[$dateField])
+//                                    ) {
+//                                        unset($mandatory_missing[$dateField]);
+//                                    }
                                 }
                             }
 
