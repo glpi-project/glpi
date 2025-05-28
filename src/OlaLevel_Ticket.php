1c39efef0b97651f19933ae6331267f786f698e2
Cédric Anne <cedric.anne@gmail.com>
2025-05-28 09:57:55 +0200

    Update src/OlaLevel_Ticket.php

--- src/OlaLevel_Ticket.php
+++ src/OlaLevel_Ticket.php
@@ -323,7 +323,7 @@ class OlaLevel_Ticket extends CommonDBTM
         /** @var \DBmysql $DB */
         global $DB;
 
-        $now = $_SESSION['glpi_currenttime'];
+        $now = \Session::getCurrentTime();
         $criteria = [
             'SELECT'    => 'glpi_olalevels_tickets.*',
             'FROM'      => 'glpi_olalevels_tickets',
