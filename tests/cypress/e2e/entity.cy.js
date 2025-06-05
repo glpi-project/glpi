2175792c9c159832537fad768d432ec6e85484d4
Curtis Conard <cconard96@gmail.com>
2025-06-05 07:46:13 -0400

    Fix survey entity config

--- tests/cypress/e2e/entity.cy.js
+++ tests/cypress/e2e/entity.cy.js
@@ -33,25 +33,74 @@
 let entity_id;
 
 describe('Entity', () => {
-    beforeEach(() => {
+    it('Can configure assistance properties', () => {
         const unique_id = (new Date()).getTime();
         cy.createWithAPI("Entity", {
             name: `Test entity ${unique_id}`,
         }).then((id) => {
             entity_id = id;
             cy.login();
+            cy.visit(`/front/entity.form.php?id=${entity_id}&forcetab=Entity$5`);
+            cy.getDropdownByLabelText('Show tickets properties on helpdesk')
+                .should('have.text', 'Inheritance of the parent entity')
+                .selectDropdownValue('Yes')
+            ;
+            cy.findByRole('button', {'name': "Save"}).click();
+            cy.getDropdownByLabelText('Show tickets properties on helpdesk')
+                .should('have.text', 'Yes');
         });
     });
 
-    it('Can configure assistance properties', () => {
-        cy.visit(`/front/entity.form.php?id=${entity_id}&forcetab=Entity$5`);
-        cy.getDropdownByLabelText('Show tickets properties on helpdesk')
-            .should('have.text', 'Inheritance of the parent entity')
-            .selectDropdownValue('Yes')
-        ;
-        cy.findByRole('button', {'name': "Save"}).click();
-        cy.getDropdownByLabelText('Show tickets properties on helpdesk')
-            .should('have.text', 'Yes')
-        ;
+    it('Survey options change by type and rate', () => {
+        cy.login();
+        cy.visit(`/front/entity.form.php?id=1&forcetab=Entity$5`);
+        cy.findByLabelText('Configuring the satisfaction survey: Tickets').within(() => {
+            cy.getDropdownByLabelText('Configuring the satisfaction survey').selectDropdownValue('Internal survey');
+            cy.getDropdownByLabelText('Create survey after').should('be.visible');
+            cy.getDropdownByLabelText('Rate to trigger survey').should('be.visible');
+            cy.getDropdownByLabelText('Duration of survey').should('not.exist');
+            cy.getDropdownByLabelText('Max rate').should('not.exist');
+            cy.findByLabelText('Default rate').should('not.be.visible');
+            cy.findByLabelText('Comment required if score is <= to').should('not.be.visible');
+            // Flatpickr ruins labels
+            cy.findByText('For Tickets closed after').should('not.be.visible');
+            // Tags aren't shown in an input and therefore not technically labelable using 'label'
+            cy.findByText('Valid tags').should('not.be.visible');
+            cy.findByLabelText('URL').should('not.be.visible');
+
+            cy.getDropdownByLabelText('Rate to trigger survey').selectDropdownValue('10%');
+            cy.getDropdownByLabelText('Create survey after').should('be.visible');
+            cy.getDropdownByLabelText('Rate to trigger survey').should('be.visible');
+            cy.getDropdownByLabelText('Duration of survey').should('be.visible');
+            cy.getDropdownByLabelText('Max rate').should('be.visible');
+            cy.findByLabelText('Default rate').should('be.visible');
+            cy.findByLabelText('Comment required if score is <= to').should('be.visible');
+            cy.findByText('For Tickets closed after').should('exist');
+            cy.findByText('Valid tags').should('not.be.visible');
+            cy.findByLabelText('URL').should('not.be.visible');
+
+
+            cy.getDropdownByLabelText('Configuring the satisfaction survey').selectDropdownValue('External survey');
+            cy.getDropdownByLabelText('Create survey after').should('be.visible');
+            cy.getDropdownByLabelText('Rate to trigger survey').should('be.visible');
+            cy.getDropdownByLabelText('Duration of survey').should('not.exist');
+            cy.getDropdownByLabelText('Max rate').should('not.exist');
+            cy.findByLabelText('Default rate').should('not.be.visible');
+            cy.findByLabelText('Comment required if score is <= to').should('not.be.visible');
+            cy.findByText('For Tickets closed after').should('not.be.visible');
+            cy.findByText('Valid tags').should('not.be.visible');
+            cy.findByLabelText('URL').should('not.be.visible');
+
+            cy.getDropdownByLabelText('Rate to trigger survey').selectDropdownValue('10%');
+            cy.getDropdownByLabelText('Create survey after').should('be.visible');
+            cy.getDropdownByLabelText('Rate to trigger survey').should('be.visible');
+            cy.getDropdownByLabelText('Duration of survey').should('be.visible');
+            cy.getDropdownByLabelText('Max rate').should('be.visible');
+            cy.findByLabelText('Default rate').should('be.visible');
+            cy.findByLabelText('Comment required if score is <= to').should('be.visible');
+            cy.findByText('For Tickets closed after').should('exist');
+            cy.findByText('Valid tags').should('be.visible');
+            cy.findByLabelText('URL').should('be.visible');
+        });
     });
 });
