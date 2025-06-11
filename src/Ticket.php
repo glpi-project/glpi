45ee820aecc6eac775121b1e9fe007a8a30f9776
Sebastien Monterisi <smonterisi@teclib.com>
2025-06-11 16:33:01 +0200

    tests ( & implemetations)
    - ola tto is complete when a user of the ola group is assigned.
    - ola completion (end_time is set) when tickets is closed or solved

--- src/Ticket.php
+++ src/Ticket.php
@@ -54,6 +54,7 @@ use Glpi\RichText\UserMention;
  *  - _la_append : flag to know if _olas_id must be appended to the existing olas without removing the existing ones
  *
  *  - _groups_id_assign : assign the ticket to a group
+ *  - _users_id_assign : assign the ticket to a user
  **/
 class Ticket extends CommonITILObject
 {
@@ -6420,6 +6421,9 @@ JAVASCRIPT;
         }
     }
 
+    /**
+     * Update Ola data + Manage OLA level
+     */
     private function recomputeOlas(): void
     {
         // recompute all OLA for this ticket
