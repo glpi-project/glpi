bd7b338879c5c5c5131602f2378216ce6af998d3
adelaunay <delaunay.alexandre@gmail.com>
2025-06-13 14:17:14 +0200

    try to fix test

--- src/CommonDBTM.php
+++ src/CommonDBTM.php
@@ -5821,6 +5821,7 @@ TWIG, $twig_params);
             $ruleasset          = new RuleAssetCollection();
             $ruleasset->setEntity($this->input['entities_id'] ?? $this->fields['entities_id']);
             $input              = $this->input;
+            $original_input     = $input;
             $input['_itemtype'] = static::class;
 
             $user = new User();
@@ -5855,7 +5856,7 @@ TWIG, $twig_params);
             }
 
             // Set the condition (add or update)
-            $output = $ruleasset->processAllRules($input, $input, [], [
+            $output = $ruleasset->processAllRules($input, $original_input, [], [
                 'condition' => $condition,
             ]);
 
