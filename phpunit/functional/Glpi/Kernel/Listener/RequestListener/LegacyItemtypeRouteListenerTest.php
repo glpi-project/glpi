5b10c2ae4cd5b352771215c31b7c488947ebb4fa
Cédric Anne <cedric.anne@gmail.com>
2025-06-17 10:07:35 +0200

    Update phpunit/functional/Glpi/Kernel/Listener/RequestListener/LegacyItemtypeRouteListenerTest.php

--- phpunit/functional/Glpi/Kernel/Listener/RequestListener/LegacyItemtypeRouteListenerTest.php
+++ phpunit/functional/Glpi/Kernel/Listener/RequestListener/LegacyItemtypeRouteListenerTest.php
@@ -443,7 +443,7 @@ final class LegacyItemtypeRouteListenerTest extends TestCase
             'path_info' => '/plugins/tester/front/mypsr4dropdown.php',
             'class'     => \GlpiPlugin\Tester\MyPsr4Dropdown::class,
         ];
-                yield [
+        yield [
             'path_info' => '/plugins/tester/front/computer.php',
             'class'     => \GlpiPlugin\Tester\Computer::class,
         ];
