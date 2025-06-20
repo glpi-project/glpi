4e93d44d4af1d4fc242ca1aeeaeff1a9838834b0
Sebastien Monterisi <smonterisi@teclib.com>
2025-06-20 12:17:02 +0200

    continue to fix SearchOptions

--- src/CommonITILObject.php
+++ src/CommonITILObject.php
@@ -5072,6 +5072,7 @@ abstract class CommonITILObject extends CommonDBTM
                                     ),],
                             ],
                             // + ticket status is not WAITING
+                            // @todoseb ajouter le status SOLVED + CLOSED ? étrange de donner un ticket comme en retard alors qu'il est clos/résolu.
                             "{$itil_table}.status" => ['<>', self::WAITING],
                             // + one of the following conditions:
                             'OR' => [
