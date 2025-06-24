4e56f6aff60737cec07d1463f5a87a6172bd98ac
Sebastien Monterisi <smonterisi@teclib.com>
2025-06-24 15:06:59 +0200

    Ticket template can use multiple Olas
    Ticket template can use multiple Olas fixup

---
+++ src/Glpi/Form/Destination/CommonITILField/OLAField.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Glpi\Form\Destination\CommonITILField;
+
+use Glpi\DBAL\JsonFieldInterface;
+use Glpi\Form\AnswersSet;
+
+abstract class OLAField extends SLMField
+{
+    #[\Override]
+    public function applyConfiguratedValueToInputUsingAnswers(
+        JsonFieldInterface $config,
+        array $input,
+        AnswersSet $answers_set
+    ): array {
+        if (!$config instanceof SLMFieldConfig) {
+            throw new \InvalidArgumentException("Unexpected config class");
+        }
+
+        // Only one strategy is allowed
+        $strategy = current($config->getStrategies());
+
+        // Compute value according to strategy
+        $slm_id = $strategy->getSLMID($config);
+
+        // Do not edit input if invalid value was found
+        /** @var class-string<\OLA> $slm_class */
+        $slm_class = $this->getSLMClass();
+        if (!$slm_class::getById($slm_id)) {
+            return $input;
+        }
+
+        $input['_olas_id'] = [$slm_id];
+        $input['_la_update'] = true;
+
+        return $input;
+    }
+}
