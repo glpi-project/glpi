99d24776ee3a77b52cc6401b6bf73fddf10b946a
Cédric Anne <cedric.anne@gmail.com>
2025-06-25 16:31:16 +0200

    apply `InlineIsAInstanceOfRector`

--- rector.php
+++ rector.php
@@ -72,5 +72,6 @@ return RectorConfig::configure()
         CodeQuality\Foreach_\UnusedForeachValueToArrayKeysRector::class,
         CodeQuality\FuncCall\ChangeArrayPushToArrayAssignRector::class,
         CodeQuality\FuncCall\CompactToVariablesRector::class,
+        CodeQuality\FuncCall\InlineIsAInstanceOfRector::class,
     ])
 ;
