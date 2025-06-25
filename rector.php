210d29412b39622aee006badcbb9572291ade5e6
Cédric Anne <cedric.anne@gmail.com>
2025-06-25 14:46:42 +0200

    Add `rector/rector` checks to improve code quality

---
+++ rector.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2025 Teclib' and contributors.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/ajax',
+        __DIR__ . '/dependency_injection',
+        __DIR__ . '/front',
+        __DIR__ . '/inc',
+        __DIR__ . '/install',
+        __DIR__ . '/public',
+        __DIR__ . '/routes',
+        __DIR__ . '/src',
+        __DIR__ . '/tools',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withCache(
+        cacheClass: FileCacheStorage::class,
+        cacheDirectory: sys_get_temp_dir() . '/rector'
+    )
+    ->withParallel(timeoutSeconds: 300)
+    // FIXME apply it in another PR, it generates a huge diff ->withImportNames()
+    ->withRules([
+    ])
+;
