c63be7665d708bc6ae94a6cfe2b802a4aa5e3ea6
Sebastien Monterisi <smonterisi@teclib.com>
2025-06-27 14:26:00 +0200

    add GroupParameter in OlaParameter

--- src/Glpi/ContentTemplates/Parameters/LevelAgreementParameters.php
+++ src/Glpi/ContentTemplates/Parameters/LevelAgreementParameters.php
@@ -54,7 +54,6 @@ abstract class LevelAgreementParameters extends AbstractParameters
             new AttributeParameter("type", _n('Type', 'Types', 1)),
             new AttributeParameter("duration", __('Duration')),
             new AttributeParameter("unit", __('Duration unit')),
-            // @todoseb ajouter le group ici
         ];
     }
 
