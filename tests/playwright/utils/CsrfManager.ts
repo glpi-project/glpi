78c7886d38b78351421de6cc058d04c6ccd7d0ac
AdrienClairembault <aclairembault@teclib.com>
2025-07-02 16:50:07 +0200

    fixup! Debug unexpected issues

--- tests/playwright/utils/CsrfManager.ts
+++ tests/playwright/utils/CsrfManager.ts
@@ -60,7 +60,7 @@ export class CsrfManager
             'input[name="_glpi_csrf_token"]'
         ) as HTMLInputElement;
 
-        if (input.value === null) {
+        if (input === null || input.value) {
             throw new Error(`Unexpected body content: ${body}`);
         }
 
