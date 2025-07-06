e1e017da872e984a0ee6983b8ae0a8eba67dedcc
Curtis Conard <cconard96@gmail.com>
2025-07-06 16:19:22 -0400

    lint

--- .phpstan-baseline.php
+++ .phpstan-baseline.php
@@ -7186,7 +7186,7 @@ $ignoreErrors[] = [
 $ignoreErrors[] = [
 	'message' => '#^Property Profile\\:\\:\\$profileRight \\(array\\) does not accept null\\.$#',
 	'identifier' => 'assign.propertyType',
-	'count' => 2,
+	'count' => 1,
 	'path' => __DIR__ . '/src/Profile.php',
 ];
 $ignoreErrors[] = [
