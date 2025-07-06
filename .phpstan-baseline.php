27b372244478b5c12f568383d776cb16a1773ec4
Curtis Conard <cconard96@gmail.com>
2025-07-06 16:10:42 -0400

    update baseline

--- .phpstan-baseline.php
+++ .phpstan-baseline.php
@@ -7528,7 +7528,7 @@ $ignoreErrors[] = [
 $ignoreErrors[] = [
 	'message' => '#^Function strtotime is unsafe to use\\. It can return FALSE instead of throwing an exception\\. Please add \'use function Safe\\\\strtotime;\' at the beginning of the file to use the variant provided by the \'thecodingmachine/safe\' library\\.$#',
 	'identifier' => 'theCodingMachineSafe.function',
-	'count' => 42,
+	'count' => 43,
 	'path' => __DIR__ . '/src/Reservation.php',
 ];
 $ignoreErrors[] = [
