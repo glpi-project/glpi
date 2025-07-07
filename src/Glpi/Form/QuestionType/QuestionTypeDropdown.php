74bfb832b3ec5a112f417f48d72b63d2a674fbca
Clément Cailly <ccailly@teclib.com>
2025-07-07 16:26:55 +0200

    fix(forms): Add an empty option for the dropdown question type when no default value is set

--- src/Glpi/Form/QuestionType/QuestionTypeDropdown.php
+++ src/Glpi/Form/QuestionType/QuestionTypeDropdown.php
@@ -164,6 +164,7 @@ TWIG;
                     'init': init,
                     'no_label': true,
                     'multiple': false,
+                    'disabled': is_multiple_dropdown,
                     'display_emptychoice': true,
                     'field_class': 'single-preview-dropdown col-12' ~ (is_multiple_dropdown ? ' d-none' : ''),
                     'mb': '',
@@ -179,6 +180,7 @@ TWIG;
                     'init': init,
                     'no_label': true,
                     'multiple': true,
+                    'disabled': not is_multiple_dropdown,
                     'values': checked_values,
                     'field_class': 'multiple-preview-dropdown col-12' ~ (not is_multiple_dropdown ? ' d-none' : ''),
                     'mb': '',
@@ -248,11 +250,12 @@ TWIG;
                 values,
                 '',
                 {
-                    'no_label'  : true,
-                    'values'    : checked_values,
-                    'multiple'  : is_multiple,
-                    'mb'        : '',
-                    'aria_label': label,
+                    'no_label'           : true,
+                    'values'             : checked_values,
+                    'multiple'           : is_multiple,
+                    'mb'                 : '',
+                    'aria_label'         : label,
+                    'display_emptychoice': checked_values|length == 0,
                 }
             ) }}
 TWIG;
