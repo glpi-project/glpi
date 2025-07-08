7056532f1e5c829109949323b0351066c840d7bb
AdrienClairembault <aclairembault@teclib.com>
2025-07-08 13:33:26 +0200

    Fix minor phpstan issues for forms

--- .phpstan-baseline.php
+++ .phpstan-baseline.php
@@ -2,18 +2,6 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
-	'message' => '#^The overwriting exit point is on this line\\.$#',
-	'identifier' => 'finally.exitPoint',
-	'count' => 1,
-	'path' => __DIR__ . '/front/form/access_control.form.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^This throw is overwritten by a different one in the finally block below\\.$#',
-	'identifier' => 'finally.exitPoint',
-	'count' => 2,
-	'path' => __DIR__ . '/front/form/access_control.form.php',
-];
-$ignoreErrors[] = [
 	'message' => '#^Call to an undefined method Glpi\\\\Mail\\\\SMTP\\\\OauthProvider\\\\ProviderInterface\\:\\:getState\\(\\)\\.$#',
 	'identifier' => 'method.notFound',
 	'count' => 1,
