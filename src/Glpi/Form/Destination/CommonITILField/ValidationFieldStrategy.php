2f400b15b35995b1f1770b90c5025ba8803c09fc
Clément Cailly <ccailly@teclib.com>
2025-07-09 10:32:29 +0200

    feat(forms-destination): Add support for specific approval templates in validation field

--- src/Glpi/Form/Destination/CommonITILField/ValidationFieldStrategy.php
+++ src/Glpi/Form/Destination/CommonITILField/ValidationFieldStrategy.php
@@ -36,10 +36,13 @@
 namespace Glpi\Form\Destination\CommonITILField;
 
 use Glpi\Form\AnswersSet;
+use ITILValidationTemplate;
+use ITILValidationTemplate_Target;
 
 enum ValidationFieldStrategy: string
 {
     case NO_VALIDATION    = 'no_validation';
+    case SPECIFIC_VALUES  = 'specific_values';
     case SPECIFIC_ACTORS  = 'specific_actors';
     case SPECIFIC_ANSWERS = 'specific_answers';
 
@@ -47,7 +50,8 @@ enum ValidationFieldStrategy: string
     {
         return match ($this) {
             self::NO_VALIDATION    => __("No approval"),
-            self::SPECIFIC_ACTORS   => __("Specific actors"),
+            self::SPECIFIC_VALUES  => __("Specific Approval templates"),
+            self::SPECIFIC_ACTORS  => __("Specific actors"),
             self::SPECIFIC_ANSWERS => __("Answer from specific questions"),
         };
     }
@@ -58,6 +62,9 @@ enum ValidationFieldStrategy: string
     ): ?array {
         return match ($this) {
             self::NO_VALIDATION    => null,
+            self::SPECIFIC_VALUES  => $this->getActorsForSpecificValues(
+                $config->getSpecificValidationTemplateIds()
+            ),
             self::SPECIFIC_ACTORS  => $this->getActorsFromSpecificActors(
                 $config->getSpecificActors()
             ),
@@ -100,6 +107,41 @@ enum ValidationFieldStrategy: string
                 $actors = array_merge($actors, $actors_to_add);
             } elseif ($actors_to_add !== null) {
                 $actors[] = $actors_to_add;
+            }
+        }
+
+        return $actors;
+    }
+
+    /**
+     * Get actors for specific validation templates
+     *
+     * @param ?array<int> $specific_values Validation template IDs
+     */
+    private function getActorsForSpecificValues(
+        ?array $specific_values,
+    ): ?array {
+        if (empty($specific_values)) {
+            return null;
+        }
+
+        $actors = [];
+        foreach ($specific_values as $validation_template_id) {
+            $validation_template = ITILValidationTemplate::getById($validation_template_id);
+            if (!$validation_template) {
+                continue;
+            }
+
+            $targets = ITILValidationTemplate_Target::getTargets($validation_template_id);
+            if (empty($targets)) {
+                continue;
+            }
+
+            foreach ($targets as $target) {
+                $actors[] = [
+                    'itemtype' => $target['itemtype'],
+                    'items_id' => $target['items_id'],
+                ];
             }
         }
 
