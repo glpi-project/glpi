02c30a9cf61a14375352e67cc4467ca2080d2ff1
MyuTsu <gaetan.langlois31@gmail.com>
2025-07-15 13:32:21 +0200

    Add info alert about task templates creation order

--- src/ITILTemplatePredefinedField.php
+++ src/ITILTemplatePredefinedField.php
@@ -315,6 +315,9 @@ abstract class ITILTemplatePredefinedField extends ITILTemplateField
 
         if ($canedit) {
             echo "<div class='firstbloc'>";
+            echo "<tr class='tab_bg_2'><td><div class='alert alert-info'>" .
+                  __('Predefined task templates will be added according to their creation order') .
+                 "</div></td></tr>\n";
             echo "<form name='changeproblem_form$rand' id='changeproblem_form$rand' method='post'
                action='" . static::getFormURL() . "'>";
 
@@ -424,6 +427,8 @@ abstract class ITILTemplatePredefinedField extends ITILTemplateField
             Html::showMassiveActions($massiveactionparams);
             Html::closeForm();
         }
+                                                                echo "TEST";
+
         echo "</div>";
     }
 }
