b7488e16e3002bca0d9f816b9d2c99bc37174b5f
Sebastien Monterisi <smonterisi@teclib.com>
2025-07-17 09:27:24 +0200

    squashed (see description)
    - implement start_time & due_time for ola when affect
    - migration
    - refacto templates
    - group field in form & OLA list
    
    # Conflicts:
    #	src/Ticket.php

--- front/ticket.form.php
+++ front/ticket.form.php
@@ -42,7 +42,7 @@ use function Safe\json_decode;
 
 global $CFG_GLPI, $DB;
 
-$track = new Ticket();
+$ticket = new Ticket();
 
 if (!isset($_GET['id'])) {
     $_GET['id'] = "";
@@ -66,33 +66,37 @@ foreach ($date_fields as $date_field) {
     }
 }
 
+// form submitted (add & update)
 // as _actors virtual field stores json, bypass automatic escaping
 if (isset($_POST['_actors'])) {
     $_POST['_actors'] = json_decode($_POST['_actors'], true);
     $_REQUEST['_actors'] = $_POST['_actors'];
 }
 
+// form submitted (add)
 if (isset($_POST["add"])) {
-    $track->check(-1, CREATE, $_POST);
+    $ticket->check(-1, CREATE, $_POST);
 
-    if ($track->add($_POST)) {
+    if ($ticket->add($_POST)) {
         if ($_SESSION['glpibackcreated']) {
-            Html::redirect($track->getLinkURL());
+            Html::redirect($ticket->getLinkURL());
         }
     }
     Html::back();
-} elseif (isset($_POST['update'])) {
-    if (!$track::canUpdate()) {
+}
+// form submitted (update)
+elseif (isset($_POST['update'])) {
+    if (!$ticket::canUpdate()) {
         throw new AccessDeniedHttpException();
     }
-    $track->update($_POST);
+    $ticket->update($_POST);
 
     if (isset($_POST['kb_linked_id'])) {
         //if solution should be linked to selected KB entry
         $params = [
             'knowbaseitems_id' => $_POST['kb_linked_id'],
-            'itemtype'         => $track->getType(),
-            'items_id'         => $track->getID(),
+            'itemtype'         => $ticket->getType(),
+            'items_id'         => $ticket->getID(),
         ];
         $existing = $DB->request([
             'FROM' => 'glpi_knowbaseitems_items',
@@ -113,7 +117,7 @@ if (isset($_POST["add"])) {
         sprintf(__('%s updates an item'), $_SESSION["glpiname"])
     );
 
-    if ($track->can($_POST["id"], READ)) {
+    if ($ticket->can($_POST["id"], READ)) {
         $toadd = '';
         // Copy solution to KB redirect to KB
         if (isset($_POST['_sol_to_kb']) && $_POST['_sol_to_kb']) {
@@ -128,8 +132,8 @@ if (isset($_POST["add"])) {
     );
     Html::redirect($CFG_GLPI["root_doc"] . "/front/ticket.php");
 } elseif (isset($_POST['delete'])) {
-    $track->check($_POST['id'], DELETE);
-    if ($track->delete($_POST)) {
+    $ticket->check($_POST['id'], DELETE);
+    if ($ticket->delete($_POST)) {
         Event::log(
             $_POST["id"],
             "ticket",
@@ -139,10 +143,10 @@ if (isset($_POST["add"])) {
             sprintf(__('%s deletes an item'), $_SESSION["glpiname"])
         );
     }
-    $track->redirectToList();
+    $ticket->redirectToList();
 } elseif (isset($_POST['purge'])) {
-    $track->check($_POST['id'], PURGE);
-    if ($track->delete($_POST, true)) {
+    $ticket->check($_POST['id'], PURGE);
+    if ($ticket->delete($_POST, true)) {
         Event::log(
             $_POST["id"],
             "ticket",
@@ -152,10 +156,10 @@ if (isset($_POST["add"])) {
             sprintf(__('%s purges an item'), $_SESSION["glpiname"])
         );
     }
-    $track->redirectToList();
+    $ticket->redirectToList();
 } elseif (isset($_POST["restore"])) {
-    $track->check($_POST['id'], DELETE);
-    if ($track->restore($_POST)) {
+    $ticket->check($_POST['id'], DELETE);
+    if ($ticket->restore($_POST)) {
         Event::log(
             $_POST["id"],
             "ticket",
@@ -167,9 +171,9 @@ if (isset($_POST["add"])) {
     }
     Html::back();
 } elseif (isset($_POST['sla_delete'])) {
-    $track->check($_POST["id"], UPDATE);
+    $ticket->check($_POST["id"], UPDATE);
 
-    $track->deleteLevelAgreement("SLA", $_POST["id"], $_POST['type'], $_POST['delete_date']);
+    $ticket->deleteLevelAgreement("SLA", $_POST["id"], $_POST['type'], $_POST['delete_date']);
     Event::log(
         $_POST["id"],
         "ticket",
@@ -181,9 +185,9 @@ if (isset($_POST["add"])) {
 
     Html::redirect(Ticket::getFormURLWithID($_POST["id"]));
 } elseif (isset($_POST['ola_delete'])) {
-    $track->check($_POST["id"], UPDATE);
+    $ticket->check($_POST["id"], UPDATE);
 
-    $track->deleteLevelAgreement("OLA", $_POST["id"], $_POST['type'], $_POST['delete_date']);
+    $ticket->deleteLevelAgreement("OLA", $_POST["id"], $_POST['type'], $_POST['delete_date']);
     Event::log(
         $_POST["id"],
         "ticket",
@@ -196,8 +200,8 @@ if (isset($_POST["add"])) {
     Html::redirect(Ticket::getFormURLWithID($_POST["id"]));
 } elseif (isset($_POST['addme_as_actor'])) {
     $id = (int) $_POST['id'];
-    $track->check($id, READ);
-    $input = array_merge($track->fields, [
+    $ticket->check($id, READ);
+    $input = array_merge($ticket->fields, [
         'id' => $id,
         '_itil_' . $_POST['actortype'] => [
             '_type' => "user",
@@ -205,7 +209,7 @@ if (isset($_POST["add"])) {
             'use_notification' => 1,
         ],
     ]);
-    $track->update($input);
+    $ticket->update($input);
     Event::log(
         $id,
         "ticket",
@@ -216,13 +220,13 @@ if (isset($_POST["add"])) {
     );
     Html::redirect(Ticket::getFormURLWithID($id));
 } elseif (isset($_POST['delete_document'])) {
-    $track->getFromDB((int) $_POST['tickets_id']);
+    $ticket->getFromDB((int) $_POST['tickets_id']);
     $doc = new Document();
     $doc->getFromDB((int) $_POST['documents_id']);
     if ($doc->can($doc->getID(), UPDATE)) {
         $document_item = new Document_Item();
         $found_document_items = $document_item->find([
-            $track->getAssociatedDocumentsCriteria(),
+            $ticket->getAssociatedDocumentsCriteria(),
             'documents_id' => $doc->getID(),
         ]);
         foreach ($found_document_items as $item) {
@@ -232,6 +236,7 @@ if (isset($_POST["add"])) {
     Html::back();
 }
 
+// show form when editing a ticket
 $id = (int) $_GET['id'];
 if ($id > 0) {
     $available_options = ['_openfollowup'];
@@ -287,7 +292,7 @@ if ($id > 0) {
 
     if (isset($_GET['showglobalkanban']) && $_GET['showglobalkanban']) {
         Html::header(sprintf(__('%s Kanban'), Ticket::getTypeName(1)), '', "helpdesk", "ticket");
-        $track::showKanban(0);
+        $ticket::showKanban(0);
         Html::footer();
     } else {
         $menus = ["helpdesk", "ticket"];
