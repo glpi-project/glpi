f93172e09cb85988a3cc24f58351138711206504
Sebastien Monterisi <smonterisi@teclib.com>
2025-07-17 16:43:53 +0200

    fix wrong search option choice filter.

--- src/Ticket.php
+++ src/Ticket.php
@@ -2751,7 +2751,7 @@ JAVASCRIPT;
                     'table'              => Item_Ola::getTable(),
                     'joinparams'         => [
                         'jointype'           => 'itemtype_item',
-                        'condition'          => ['NEWTABLE.ola_type' => SLM::TTO],
+                        'condition'          => ['NEWTABLE.ola_type' => SLM::TTR],
                     ],
                 ],
             ],
