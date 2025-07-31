381f9a39306c5c098beeca7337920bffd06a31fd
AdrienClairembault <aclairembault@teclib.com>
2025-07-31 10:10:04 +0200

    Lazy load form editor dropdowns

--- js/modules/Forms/EditorController.js
+++ js/modules/Forms/EditorController.js
@@ -928,6 +928,20 @@ export class GlpiFormEditorController
             $(this).attr('data-glpi-loaded', "true");
         });
 
+        // Lazy load dropdowns
+        item_container.find('select[data-glpi-loaded=false]').each(function() {
+            // Get editor config for this field
+            const id = $(this).attr("id");
+            const config = window.select2_configs[id];
+            if (config.type === "ajax") {
+                setupAjaxDropdown(config);
+            } else if (config.type === "adapt") {
+                setupAdaptDropdown(config);
+            }
+            tinyMCE.init(config);
+            $(this).attr('data-glpi-loaded', "true");
+        });
+
         /**
          * Delay the activation of the new item to avoid a rendering bug.
          * I can't explain it, but without this delay,
