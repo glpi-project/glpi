34af97b6c1e1110052bf259004f1b057147eee34
Cédric Anne <cedric.anne@gmail.com>
2025-08-14 11:43:03 +0200

    Remove code coverage workflow
    closes #20666

--- .github/actions/test_tests-phpunit.sh
+++ .github/actions/test_tests-phpunit.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e -u -x -o pipefail
 
-PHPUNIT_ADDITIONNAL_OPTIONS=""
-if [[ "${CODE_COVERAGE:-}" = true ]]; then
-  PHPUNIT_ADDITIONNAL_OPTIONS="--coverage-clover phpunit/coverage/clover.xml"
-fi
-
-vendor/bin/phpunit $PHPUNIT_ADDITIONNAL_OPTIONS $@
+vendor/bin/phpunit $@
