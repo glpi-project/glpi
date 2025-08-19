d1678ce2b63ed13e9fa5725c5ca1dc53619d86da
Curtis Conard <cconard96@gmail.com>
2025-08-19 05:54:11 -0400

    project items list ui modernization

--- phpunit/functional/Item_ProjectTest.php
+++ phpunit/functional/Item_ProjectTest.php
@@ -39,6 +39,9 @@ use Glpi\Asset\Capacity;
 use Glpi\Asset\Capacity\IsProjectAssetCapacity;
 use Glpi\Features\Clonable;
 use Item_Project;
+use Project;
+use ProjectState;
+use Symfony\Component\DomCrawler\Crawler;
 use Toolbox;
 
 class Item_ProjectTest extends DbTestCase
@@ -75,6 +78,80 @@ class Item_ProjectTest extends DbTestCase
 
             $item = \getItemForItemtype($itemtype);
             $this->assertContains(Item_Project::class, $item->getCloneRelations(), $itemtype);
+        }
+    }
+
+    public function testShowForProjectAndAsset(): void
+    {
+        /** @var array $CFG_GLPI */
+        global $CFG_GLPI;
+
+        $this->login();
+
+        $project = $this->createItem(Project::class, [
+            'name'        => $this->getUniqueString(),
+            'entities_id' => $this->getTestRootEntity(true),
+            'priority'    => 4,
+            'code' => 'project_code',
+            'projectstates_id' => getItemByTypeName(ProjectState::class, 'Processing', true),
+            'percent_done' => 45,
+            'date_creation' => '2025-08-17',
+        ], ['date_creation']);
+
+        $assets = [];
+
+        foreach ($CFG_GLPI['project_asset_types'] as $itemtype) {
+            $assets[$itemtype] = $this->createItem($itemtype, [
+                'name' => __FUNCTION__,
+                'designation' => __FUNCTION__,
+                'entities_id' => $this->getTestRootEntity(true),
+                'serial' => "{$itemtype}-serial",
+                'otherserial' => "{$itemtype}-otherserial",
+            ], ['name', 'designation', 'entities_id', 'serial', 'otherserial']);
+            $this->createItem(Item_Project::class, [
+                'projects_id' => $project->getID(),
+                'itemtype'    => $itemtype,
+                'items_id'    => $assets[$itemtype]->getID(),
+            ]);
+        }
+
+        ob_start();
+        Item_Project::showForProject($project);
+        $out = ob_get_clean();
+
+        $crawler = new Crawler($out);
+        $rows = $crawler->filter('table tbody tr[data-itemtype="Item_Project"]');
+        $this->assertCount(count($CFG_GLPI['project_asset_types']), $rows);
+        $project_types = array_combine(
+            array_map(static fn($t) => $t::getTypeName(1), $CFG_GLPI['project_asset_types']),
+            $CFG_GLPI['project_asset_types'],
+        );
+        foreach ($rows as $row) {
+            $cells = (new Crawler($row))->filter('td');
+            $itemtype = $project_types[trim($cells->getNode(1)->textContent)];
+            $item = getItemForItemtype($itemtype);
+            $this->assertStringContainsString($item->isEntityAssign() ? 'Root entity > _test_root_entity' : '-', trim($cells->getNode(2)->textContent));
+            $this->assertStringContainsString(__FUNCTION__, trim($cells->getNode(3)->textContent));
+            $this->assertStringContainsString($item->isField('serial') ? "{$itemtype}-serial" : '-', trim($cells->getNode(4)->textContent));
+            $this->assertStringContainsString($item->isField('otherserial') ? "{$itemtype}-otherserial" : '-', trim($cells->getNode(5)->textContent));
+        }
+
+        foreach ($assets as $asset) {
+            ob_start();
+            $this->callPrivateMethod(new Item_Project(), 'showForAsset', $asset);
+            $out = ob_get_clean();
+
+            $crawler = new Crawler($out);
+            $rows = $crawler->filter('table tbody tr');
+            $this->assertCount(1, $rows);
+            $cells = (new Crawler($rows->getNode(0)))->filter('td');
+            $this->assertStringContainsString($project->getName(), trim($cells->getNode(0)->textContent));
+            $this->assertStringContainsString('High', trim($cells->getNode(1)->textContent));
+            $this->assertStringContainsString('project_code', trim($cells->getNode(2)->textContent));
+            $this->assertStringContainsString('Processing', trim($cells->getNode(3)->textContent));
+            $progress = new Crawler($cells->getNode(4));
+            $this->assertStringContainsString('45 %', $progress->filter('.progress-bar')->text());
+            $this->assertStringContainsString('2025-08-17', trim($cells->getNode(5)->textContent));
         }
     }
 }
