2dfc94ef3e88f8555c118c2fd085138ce4b711e4
Cédric Anne <cedric.anne@gmail.com>
2025-08-27 09:03:09 +0200

    Remove grant requirement on `mysql.time_zone_name` table

--- phpunit/functional/Glpi/System/Requirement/DbTimezonesTest.php
+++ phpunit/functional/Glpi/System/Requirement/DbTimezonesTest.php
@@ -36,89 +36,13 @@ namespace tests\units\Glpi\System\Requirement;
 
 class DbTimezonesTest extends \GLPITestCase
 {
-    public function testCheckWithUnavailableMysqlDb()
+    public function testCheckWithTimezonenameEmptyList()
     {
         $db = $this->getMockBuilder(\DB::class)
             ->disableOriginalConstructor()
-            ->onlyMethods(['request'])
+            ->onlyMethods(['getTimezones'])
             ->getMock();
-        $that = $this;
-
-        $db->method('request')->willReturnCallback(
-            function ($query) use ($that) {
-                $result = $this->getMockBuilder(\DBmysqlIterator::class)
-                    ->setConstructorArgs([null])
-                    ->onlyMethods(['count'])
-                    ->getMock();
-                if ($query === "SHOW DATABASES LIKE 'mysql'") {
-                    $result->method('count')->willReturn(0);
-                }
-                return $result;
-            }
-        );
-
-        $instance = new \Glpi\System\Requirement\DbTimezones($db);
-        $this->assertFalse($instance->isValidated());
-        $this->assertEquals(
-            ['Access to timezone database (mysql) is not allowed.'],
-            $instance->getValidationMessages()
-        );
-    }
-
-    public function testCheckWithUnavailableTimezonenameTable()
-    {
-        $db = $this->getMockBuilder(\DB::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['request'])
-            ->getMock();
-        $that = $this;
-
-        $db->method('request')->willReturnCallback(
-            function ($query) {
-                $result = $this->getMockBuilder(\DBmysqlIterator::class)
-                    ->setConstructorArgs([null])
-                    ->onlyMethods(['count'])
-                    ->getMock();
-                if ($query === "SHOW DATABASES LIKE 'mysql'") {
-                    $result->method('count')->willReturn(1);
-                } elseif ($query === "SHOW TABLES FROM `mysql` LIKE 'time_zone_name'") {
-                    $result->method('count')->willReturn(0);
-                }
-                return $result;
-            }
-        );
-
-        $instance = new \Glpi\System\Requirement\DbTimezones($db);
-        $this->assertFalse($instance->isValidated());
-        $this->assertEquals(
-            ['Access to timezone table (mysql.time_zone_name) is not allowed.'],
-            $instance->getValidationMessages()
-        );
-    }
-
-    public function testCheckWithTimezonenameEmptyTable()
-    {
-        $db = $this->getMockBuilder(\DB::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['request'])
-            ->getMock();
-
-        $db->method('request')->willReturnCallback(
-            function ($query) {
-                $result = $this->getMockBuilder(\DBmysqlIterator::class)
-                    ->setConstructorArgs([null])
-                    ->onlyMethods(['count', 'current'])
-                    ->getMock();
-                if ($query === "SHOW DATABASES LIKE 'mysql'") {
-                    $result->method('count')->willReturn(1);
-                } elseif ($query === "SHOW TABLES FROM `mysql` LIKE 'time_zone_name'") {
-                    $result->method('count')->willReturn(1);
-                } else {
-                    $result->method('current')->willReturn(['cpt' => 0]);
-                }
-                return $result;
-            }
-        );
+        $db->method('getTimezones')->willReturn([]);
 
         $instance = new \Glpi\System\Requirement\DbTimezones($db);
         $this->assertFalse($instance->isValidated());
@@ -132,25 +56,18 @@ class DbTimezonesTest extends \GLPITestCase
     {
         $db = $this->getMockBuilder(\DB::class)
             ->disableOriginalConstructor()
-            ->onlyMethods(['request'])
+            ->onlyMethods(['getTimezones'])
             ->getMock();
-
-        $db->method('request')->willReturnCallback(
-            function ($query) {
-                $result = $this->getMockBuilder(\DBmysqlIterator::class)
-                    ->setConstructorArgs([null])
-                    ->onlyMethods(['count', 'current'])
-                    ->getMock();
-                if ($query === "SHOW DATABASES LIKE 'mysql'") {
-                    $result->method('count')->willReturn(1);
-                } elseif ($query === "SHOW TABLES FROM `mysql` LIKE 'time_zone_name'") {
-                    $result->method('count')->willReturn(1);
-                } else {
-                    $result->method('current')->willReturn(['cpt' => 30]);
-                }
-                return $result;
-            }
-        );
+        $db->method('getTimezones')->willReturn([
+            'Africa/Abidjan',
+            'America/Cancun',
+            'Asia/Beirut',
+            'Atlantic/Faeroe',
+            'Australia/Canberra',
+            'Europe/Paris',
+            'Pacific/Noumea',
+            'UTC',
+        ]);
 
         $instance = new \Glpi\System\Requirement\DbTimezones($db);
         $this->assertTrue($instance->isValidated());
