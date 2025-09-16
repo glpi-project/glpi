0652fd83bcd85b9041018296ec9b81927e7d7428
Johan Cwiklinski <johan@x-tnd.be>
2025-09-16 11:18:19 +0200

    Finalize fix, use external twig file

--- src/CronTask.php
+++ src/CronTask.php
@@ -1959,18 +1959,13 @@ TWIG, ['msg' => __('Last run list')]);
         if (count($warnings) > 0) {
             $msg = __('Automatic actions may not be running as expected');
             $params = [
-                'msg' => $msg,
-                'warnings' => '<ul>' . implode('', array_map(static fn($warning) => '<li>' . htmlescape($warning) . '</li>', $warnings)) . '</ul>',
+                'status_message' => $msg,
+                'extra_message' => '<ul>' . implode('', array_map(static fn($warning) => '<li>' . htmlescape($warning) . '</li>', $warnings)) . '</ul>',
             ];
-            echo TemplateRenderer::getInstance()->renderFromStringTemplate(<<<TWIG
-                <span class="alert alert-warning p-1 m-0">
-                    <i class="ti ti-alert-triangle me-2"></i>
-                    <span>{{ msg }}</span>
-                    <span class="form-help" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-html="true" data-bs-content="{{ warnings }}">
-                        ?
-                    </span>
-                </span>
-TWIG, $params);
+            echo TemplateRenderer::getInstance()->display(
+                'components/search/status_area.html.twig',
+                $params
+            );
         }
     }
 }
