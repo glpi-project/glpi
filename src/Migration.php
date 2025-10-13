2c73a1a4c42305abb15863bf66f736cd3636ebea
Cédric Anne <cedric.anne@gmail.com>
2025-10-13 11:02:12 +0200

    more precise type

--- src/Migration.php
+++ src/Migration.php
@@ -1185,7 +1185,7 @@ class Migration
      *
      * @param string  $name   Right name
      * @param integer $rights Right to set (defaults to ALLSTANDARDRIGHT)
-     * @param array   $requiredrights Array of right name => value
+     * @param array<string, int>   $requiredrights Array of right name => value
      *                   A profile must have these rights in order to get the new right.
      *                   This array can be empty to add the right to every profile.
      *                   Default is ['config' => READ | UPDATE].
