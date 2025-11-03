9d49b56084075b3701379073f2a3ede3f4283e2e
Cédric Anne <cedric.anne@gmail.com>
2025-11-03 10:15:52 +0100

    Update ajax/updatecurrenttab.php
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- ajax/updatecurrenttab.php
+++ ajax/updatecurrenttab.php
@@ -34,6 +34,6 @@
  */
 
 // Manage tabs
-if (isset($_GET['itemtype'])) {
+if (isset($_GET['itemtype']) && isset($_GET['tab_key'])) {
     Session::setActiveTab($_GET['itemtype'], $_GET['tab_key']);
 }
