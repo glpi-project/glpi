f510dd4a67f399a04d0302be7a8ee4cc5a423296
Clément Cailly <ccailly@teclib.com>
2025-11-04 15:07:47 +0100

    fix: replace global logger with dependency injection

--- src/Glpi/Controller/Form/SubmitAnswerController.php
+++ src/Glpi/Controller/Form/SubmitAnswerController.php
@@ -47,6 +47,7 @@ use Glpi\Form\EndUserInputNameProvider;
 use Glpi\Form\Form;
 use Glpi\Http\Firewall;
 use Glpi\Security\Attribute\SecurityStrategy;
+use Psr\Log\LoggerInterface;
 use Session;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -56,6 +57,8 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SubmitAnswerController extends AbstractController
 {
     use CanCheckAccessPolicies;
+
+    public function __construct(private LoggerInterface $logger) {}
 
     #[SecurityStrategy(Firewall::STRATEGY_NO_CHECK)] // Some forms can be accessed anonymously
     #[Route(
@@ -88,8 +91,7 @@ final class SubmitAnswerController extends AbstractController
                 'links_to_created_items' => $links,
             ]);
         } catch (\Throwable $th) {
-            global $PHPLOGGER;
-            $PHPLOGGER->error(
+            $this->logger->error(
                 sprintf('An error occured during the form `%s` submission.', $form->getName()),
                 ['exception' => $th]
             );
