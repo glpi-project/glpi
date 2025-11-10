d781a463dfd2126ca8d79dcee49da175c47066c3
Johan Cwiklinski <johan@x-tnd.be>
2025-11-10 14:02:35 +0100

    Fix default value

--- install/migrations/update_11.0.2_to_11.0.3/processes.php
+++ install/migrations/update_11.0.2_to_11.0.3/processes.php
@@ -35,4 +35,4 @@
  * @var Migration $migration
  */
 // see #21349
-$migration->changeField('glpi_items_processes', 'virtualmemory', 'virtualmemory', 'bigint NOT NULL DEFAULT "1"');
+$migration->changeField('glpi_items_processes', 'virtualmemory', 'virtualmemory', 'bigint NOT NULL DEFAULT "0"');
