e55a15bfbe4fe7c1f442ffac9012fadb1c1e28b9
Herafia <llenik@teclib.com>
2025-11-13 17:05:41 +0100

    Remove verification: prepareInputForAdd already handles it

--- src/CommonDBTM.php
+++ src/CommonDBTM.php
@@ -5582,10 +5582,6 @@ class CommonDBTM extends CommonGLPI
 
                 $input2["entities_id"]             = $entities_id;
                 $input2["is_recursive"]            = $is_recursive;
-                // Fill in the document category only if the edited item has a default one
-                if (!empty($this->fields['documentcategories_id'])) {
-                    $input2['documentcategories_id'] = $this->fields['documentcategories_id'];
-                }
                 $input2["_only_if_upload_succeed"] = 1;
                 $input2["_filename"]               = [$file];
                 if (isset($this->input[$prefixUploadName][$key])) {
