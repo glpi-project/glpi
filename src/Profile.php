9b8d3a4c01be1fe817016cabb15b28e46081c6b9
MyuTsu <gaetan.langlois31@gmail.com>
2025-11-19 11:48:41 +0100

    review

--- src/Profile.php
+++ src/Profile.php
@@ -899,10 +899,6 @@ class Profile extends CommonDBTM implements LinkableToTilesInterface
                             $fn_get_rights(DeviceSimcard::class, 'central', [
                                 'label' => __('Simcard PIN/PUK'),
                                 'field' => 'devicesimcard_pinpuk',
-                                'rights'    => [
-                                    READ    => __('View all'),
-                                    UPDATE  => __('Update all'),
-                                ],
                             ]),
                         ],
                     ],
