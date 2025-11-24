74495d1a0e3b13464097dff45c90247c13e09516
Romain Lecouvreur <102067890+RomainLvr@users.noreply.github.com>
2025-11-24 11:22:58 +0100

    Fix - Service Levels rule criterias (#22038)

--- src/LevelAgreementLevel.php
+++ src/LevelAgreementLevel.php
@@ -228,20 +228,15 @@ abstract class LevelAgreementLevel extends RuleTicket
     public function getCriterias()
     {
 
-        $actions = parent::getActions();
+        $criteria = parent::getCriterias();
 
-        unset($actions['olas_id']);
-        unset($actions['slas_id']);
-        // Could not be used as criteria
-        unset($actions['users_id_validate_requester_supervisor']);
-        unset($actions['users_id_validate_assign_supervisor']);
-        unset($actions['affectobject']);
-        unset($actions['groups_id_validate']);
-        unset($actions['users_id_validate']);
-        unset($actions['validation_percent']);
-        $actions['status']['name']    = __('Status');
-        $actions['status']['type']    = 'dropdown_status';
-        return $actions;
+        unset($criteria['olas_id']);
+        unset($criteria['slas_id']);
+        unset($criteria['slas_id_ttr']);
+        unset($criteria['slas_id_tto']);
+        $criteria['status']['name']    = __('Status');
+        $criteria['status']['type']    = 'dropdown_status';
+        return $criteria;
     }
 
 
