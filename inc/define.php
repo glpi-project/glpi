6097270a17db6751152010ec8d553b34cd2d606c
Johan Cwiklinski <johan@x-tnd.be>
2025-12-04 09:37:18 +0100

    Bump version

--- inc/define.php
+++ inc/define.php
@@ -36,7 +36,7 @@
 use Glpi\SocketModel;
 
 // Current version of GLPI
-define('GLPI_VERSION', '10.0.22-dev');
+define('GLPI_VERSION', '10.0.22');
 
 $schema_file = sprintf('%s/install/mysql/glpi-empty.sql', GLPI_ROOT);
 define(
