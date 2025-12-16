b54aa6e21b67a5b584a2d08f7fc4cf2ada277b1e
Samuel Launay <107540223+Lainow@users.noreply.github.com>
2025-12-16 13:47:11 +0100

    Add documents tab to a few models (#22240)

--- src/autoload/CFG_GLPI.php
+++ src/autoload/CFG_GLPI.php
@@ -167,7 +167,9 @@ $CFG_GLPI['document_types']               = [Budget::class, CartridgeItem::class
     SoftwareLicense::class, Supplier::class, Ticket::class, User::class,
     Certificate::class, Cluster::class, ITILFollowup::class, ITILSolution::class,
     ChangeTask::class, ProblemTask::class, TicketTask::class, Appliance::class,
-    DatabaseInstance::class, Rack::class,
+    DatabaseInstance::class, Rack::class, ComputerModel::class, MonitorModel::class,
+    NetworkEquipmentModel::class, PeripheralModel::class, PhoneModel::class, PrinterModel::class,
+    DeviceGenericModel::class,
 ];
 
 $CFG_GLPI['consumables_types']            = [Group::class, User::class];
