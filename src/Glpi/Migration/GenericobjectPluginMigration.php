021ccfb9c81503f21750b3843dbef65b2bb4b400
Romain B. <8530352+Rom1-B@users.noreply.github.com>
2025-12-18 20:26:14 +0100

    Fix(Genericobject migration): error unicity contraint (#22424)

--- src/Glpi/Migration/GenericobjectPluginMigration.php
+++ src/Glpi/Migration/GenericobjectPluginMigration.php
@@ -857,6 +857,7 @@ class GenericobjectPluginMigration extends AbstractPluginMigration
                             $this->importItem(
                                 Group_Item::class,
                                 input: $group_input,
+                                reconciliation_criteria: $group_input,
                             );
                         }
                     }
@@ -871,6 +872,7 @@ class GenericobjectPluginMigration extends AbstractPluginMigration
                         $this->importItem(
                             Domain_Item::class,
                             input: $domain_input,
+                            reconciliation_criteria: $domain_input,
                         );
                     }
 
