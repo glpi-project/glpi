e5109338981a51e2751ccb91d8485e70579b9a0b
Benoit VIGNAL <git@benoit-vignal.fr>
2025-12-29 15:38:12 +0100

    feat: migrate tools to core

--- .github/actions/lint_misc-lint.sh
+++ .github/actions/lint_misc-lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -u -x -o pipefail
 
-bin/console tools:licence_headers_check --ansi --no-interaction
+bin/console tools:licence_headers_check
 
 bin/console tools:extract_locales 2>&1 | tee extract.log
 if [[ -n $(grep "warning" extract.log) ]]; then exit 1; fi
