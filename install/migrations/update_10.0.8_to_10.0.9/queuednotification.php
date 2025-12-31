4c042fcddd25c34dbf9c52abd22e5bf8d4e17422
François Legastelois <flegastelois@teclib.com>
2025-12-31 14:42:44 +0100

    feat: happy new year 2026

--- install/migrations/update_10.0.8_to_10.0.9/queuednotification.php
+++ install/migrations/update_10.0.8_to_10.0.9/queuednotification.php
@@ -7,7 +7,7 @@
  *
  * http://glpi-project.org
  *
- * @copyright 2015-2025 Teclib' and contributors.
+ * @copyright 2015-2026 Teclib' and contributors.
  * @licence   https://www.gnu.org/licenses/gpl-3.0.html
  *
  * ---------------------------------------------------------------------
@@ -31,6 +31,7 @@
  *
  * ---------------------------------------------------------------------
  */
+
 /**
  * @var DBmysql $DB
  * @var Migration $migration
