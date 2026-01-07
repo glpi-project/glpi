a93893fc3761c9e628a5d1935c3c7e44d055a191
Cédric Anne <cedric.anne@gmail.com>
2026-01-07 15:53:56 +0100

    Disallow safe URLs in webhooks calls

--- src/Webhook.php
+++ src/Webhook.php
@@ -1088,6 +1088,13 @@ class Webhook extends CommonDBTM implements FilterableInterface
     {
         global $CFG_GLPI;
 
+        if (!Toolbox::isUrlSafe($url)) {
+            return [
+                'status' => false,
+                'message' => sprintf(__('URL "%s" is not allowed by your administrator.'), $url),
+            ];
+        }
+
         $challenge_response = [];
         $options = [
             'base_uri'        => $url,
