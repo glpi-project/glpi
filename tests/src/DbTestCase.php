e29aebef6e5a75b93c1828d56679e016938c3c15
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2026-01-13 16:47:36 +0100

    Restore form document behavior from formcreator (#22642)

--- tests/src/DbTestCase.php
+++ tests/src/DbTestCase.php
@@ -47,6 +47,7 @@ use Glpi\Asset\CapacityConfig;
 use Glpi\Dropdown\DropdownDefinition;
 use Profile;
 use ProfileRight;
+use Ramsey\Uuid\Uuid;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use ReflectionClass;
@@ -744,6 +745,31 @@ class DbTestCase extends GLPITestCase
         ]);
         $this->updateItem(ProfileRight::class, $profile_right->fields['id'], [
             'rights' => $profile_right->fields['rights'] & ~$value,
+        ]);
+    }
+
+    protected function addDocumentToItem(
+        string $name,
+        string $content,
+        CommonDBTM $item,
+    ): Document {
+        $this->login();
+
+        $prefix = uniqid(more_entropy: true);
+        file_put_contents(GLPI_TMP_DIR . '/' . "$prefix" . $name, $content);
+        return $this->createItem(Document::class, [
+            'documentcategories_id' => 0,
+            '_filename'             => ["$prefix" . $name],
+            '_prefix_filename'      => [$prefix],
+            '_tag_filename'         => [Uuid::uuid4()],
+            'itemtype'              => $item::class,
+            'items_id'              => $item->getID(),
+        ], [
+            '_filename',
+            '_prefix_filename',
+            '_tag_filename',
+            'itemtype',
+            'items_id',
         ]);
     }
 }
