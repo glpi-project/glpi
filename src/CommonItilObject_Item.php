d5a33813020e8c682ab5c5ace349eb63bca07854
Johan Cwiklinski <trasher@x-tnd.be>
2026-01-14 11:05:03 +0100

    Update src/CommonItilObject_Item.php
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- src/CommonItilObject_Item.php
+++ src/CommonItilObject_Item.php
@@ -599,7 +599,7 @@ TWIG, $twig_params);
                 ],
             ];
             if ($type_item->maybeDeleted()) {
-                $criteria['WHERE'][$type . '.' . 'is_deleted'] = 0;
+                $criteria['WHERE'][$type::getTable() . '.' . 'is_deleted'] = 0;
             }
             $nb += countElementsInTable(static::getTable(), $criteria);
         }
