61f5472ff6f7eb6476613091204317ee91f4b05f
AdrienClairembault <aclairembault@teclib.com>
2026-01-15 16:59:52 +0100

    Add search role

--- js/src/vue/FuzzySearch/Modal.vue
+++ js/src/vue/FuzzySearch/Modal.vue
@@ -110,7 +110,9 @@
                         <i class="ti ti-alert-circle-filled fa-2x me-2"></i>
                         <p v-html="shortcut_message"></p>
                     </div>
-                    <input type="text" class="form-control" name="fuzzysearch_modal_search_menu" :placeholder="placeholder" v-model="input_text">
+                    <div role="search">
+                        <input type="text" class="form-control" name="fuzzysearch_modal_search_menu" :placeholder="placeholder" v-model="input_text">
+                    </div>
                     <ul class="results list-group mt-2">
                         <li v-for="result in results" :key="result.index" class="list-group-item">
                             <a :href="result.original.url" v-html="result.string"></a>
