071e944cdeea7dffb95cd08454678569c004f94e
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2026-01-21 11:28:39 +0100

    Regression test on tabler icons (#22701)
    * Regression test on tabler icons
    
    * Use route file
    
    * Move to src folder
    
    * Move template to test folder

--- src/Glpi/Application/Environment.php
+++ src/Glpi/Application/Environment.php
@@ -281,4 +281,12 @@ enum Environment: string
             self::E2E => true,
         };
     }
+
+    public function shouldEnableTestResources(): bool
+    {
+        return match ($this) {
+            default   => false,
+            self::E2E => true,
+        };
+    }
 }
