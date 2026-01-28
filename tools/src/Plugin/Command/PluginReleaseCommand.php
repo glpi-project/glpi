d24c40c10b1f77bdde0e5761131f4b87e81b009f
Cédric Anne <cedric.anne@gmail.com>
2026-01-28 16:30:19 +0100

    lint

--- tools/src/Plugin/Command/PluginReleaseCommand.php
+++ tools/src/Plugin/Command/PluginReleaseCommand.php
@@ -35,7 +35,6 @@
 namespace Glpi\Tools\Plugin\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
