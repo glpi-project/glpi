2157c155013514ef79c765cd4b2910ac7c8a3803
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2026-02-02 12:15:09 -0300

    Change return type of dropdown functions to int|string

--- src/Software.php
+++ src/Software.php
@@ -654,9 +654,9 @@ class Software extends CommonDBTM implements TreeBrowseInterface, AssignableItem
      * @param string $myname select name
      * @param int|array<int> $entity_restrict restrict to a defined entity
      *
-     * @return int random part of elements id
+     * @return int|string random part of elements id
      **/
-    public static function dropdownSoftwareToInstall($myname, $entity_restrict): int
+    public static function dropdownSoftwareToInstall($myname, $entity_restrict): int|string
     {
         global $DB;
 
@@ -748,7 +748,7 @@ class Software extends CommonDBTM implements TreeBrowseInterface, AssignableItem
      *
      * @return int random part of elements id
      **/
-    public static function dropdownLicenseToInstall($myname, $entity_restrict): int
+    public static function dropdownLicenseToInstall($myname, $entity_restrict): int|string
     {
         global $DB;
 
