0f829d3c72f888fa8986f1f88ea0e7df820c40c7
Adrien Clairembault <42734840+AdrienClairembault@users.noreply.github.com>
2026-02-02 10:02:24 +0100

    Add KB comments counter (#22869)

--- src/Glpi/Knowbase/EditorAction.php
+++ src/Glpi/Knowbase/EditorAction.php
@@ -42,5 +42,6 @@ final readonly class EditorAction
         public string $icon,
         public EditorActionType $type,
         public array $params = [],
+        public ?int $counter = null,
     ) {}
 }
