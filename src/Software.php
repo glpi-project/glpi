9140cb2a0b1d3bd5693c5a02e4e8590e5f0b8fa6
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2026-02-02 13:14:41 -0300

    Remove redundant type hint in return annotation

--- src/Software.php
+++ src/Software.php
@@ -654,7 +654,7 @@ class Software extends CommonDBTM implements TreeBrowseInterface, AssignableItem
      * @param string $myname select name
      * @param int|array<int> $entity_restrict restrict to a defined entity
      *
-     * @return int random part of elements id (int)
+     * @return int random part of elements id
      **/
     public static function dropdownSoftwareToInstall($myname, $entity_restrict)
     {
@@ -746,7 +746,7 @@ class Software extends CommonDBTM implements TreeBrowseInterface, AssignableItem
      * @param string $myname select name
      * @param int|array<int> $entity_restrict restrict to a defined entity
      *
-     * @return int random part of elements id (int)
+     * @return int random part of elements id
      **/
     public static function dropdownLicenseToInstall($myname, $entity_restrict)
     {
