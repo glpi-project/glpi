be722a5af07a310dd5a5a5a8707ab6e28b4661ec
Johan Cwiklinski <trasher@x-tnd.be>
2026-02-04 09:31:44 +0100

    Update src/CommonDBTM.php
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- src/CommonDBTM.php
+++ src/CommonDBTM.php
@@ -1908,7 +1908,7 @@ class CommonDBTM extends CommonGLPI
                     $DB->executeStatement($stmt, [$field]);
                 } catch (StatementException $e) {
                     if ($e->getCode() != 1062) {
-                        trigger_error('Unable to add locked field!', E_USER_WARNING);
+                        throw new \RuntimeException('Unable to add locked field!', code: $e->getCode(), previous: $e);
                     }
                 }
             }
