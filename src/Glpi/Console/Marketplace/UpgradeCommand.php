cd656f76196a3e8a9d30eba610883b1c8d152649
Johan Cwiklinski <trasher@x-tnd.be>
2026-03-11 09:02:26 +0100

    Update src/Glpi/Console/Marketplace/UpgradeCommand.php
    Co-authored-by: Curtis Conard <cconard96@gmail.com>

--- src/Glpi/Console/Marketplace/UpgradeCommand.php
+++ src/Glpi/Console/Marketplace/UpgradeCommand.php
@@ -51,7 +51,7 @@ class UpgradeCommand extends AbstractCommand
         parent::configure();
 
         $this->setName('marketplace:upgrade');
-        $this->setDescription(__('Download all plugins to their latest compatible versions, update all active plugins and reactivate those that are active.'));
+        $this->setDescription(__('Download all plugins to their latest compatible versions, update all active plugins and reactivate those that were active.'));
 
         $this->addOption(
             'username',
