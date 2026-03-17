59d6d19f43b3ca34e3d8fc42e5017258d919e06b
Curtis Conard <cconard96@gmail.com>
2026-03-17 08:32:17 -0400

    Fix creating change and problem from asset (#23371)

--- tests/functional/ProblemTest.php
+++ tests/functional/ProblemTest.php
@@ -35,7 +35,6 @@
 namespace tests\units;
 
 use CommonITILObject;
-use Computer;
 use Glpi\Tests\DbTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 use Problem;
@@ -44,53 +43,6 @@ use Problem;
 
 class ProblemTest extends DbTestCase
 {
-    public function testAddFromItem()
-    {
-        // add problem from a computer
-        $computer   = getItemByTypeName('Computer', '_test_pc01');
-        $problem     = new Problem();
-        $problems_id = $problem->add([
-            'name'           => "test add from computer \'_test_pc01\'",
-            'content'        => "test add from computer \'_test_pc01\'",
-            '_add_from_item' => true,
-            '_from_itemtype' => 'Computer',
-            '_from_items_id' => $computer->getID(),
-        ]);
-        $this->assertGreaterThan(0, $problems_id);
-        $this->assertTrue($problem->getFromDB($problems_id));
-
-        // check relation
-        $problem_item = new \Item_Problem();
-        $this->assertTrue($problem_item->getFromDBForItems($problem, $computer));
-    }
-
-    public function testAddFromItemFormFlow(): void
-    {
-        $computer = getItemByTypeName(Computer::class, '_test_pc01');
-        $problem  = new Problem();
-
-        // Simulate raw inputs as received by the front controller
-        $problems_id = $problem->add([
-            'name'           => 'test add from item form flow',
-            'content'        => 'test add from item form flow',
-            '_add_fromitem'  => true,
-            'itemtype'       => Computer::class,
-            'items_id'       => $computer->getID(),
-        ]);
-        $this->assertGreaterThan(0, $problems_id);
-
-        $item_problem = new \Item_Problem();
-        $this->assertTrue($item_problem->getFromDBForItems($problem, $computer));
-        $this->assertEquals(
-            1,
-            countElementsInTable(\Item_Problem::getTable(), [
-                'problems_id' => $problems_id,
-                'itemtype'    => Computer::class,
-                'items_id'    => $computer->getID(),
-            ])
-        );
-    }
-
     public function testAssignFromCategory()
     {
         $this->login('glpi', 'glpi');
