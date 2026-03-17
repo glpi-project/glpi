fdc662d2ed606dcee3bfb8fbfb1fa9da36e35e84
Sébastien Monterisi <smonterisi@teclib.com>
2026-03-17 15:10:00 +0100

    Fix visibility change in massive actions (#22733)
    fixes #21520

--- .phpstan-baseline.missingType.iterableValue.php
+++ .phpstan-baseline.missingType.iterableValue.php
@@ -29438,6 +29438,12 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/src/State.php',
 ];
 $ignoreErrors[] = [
+	'message' => '#^Method State\\:\\:getSpecificMassiveActions\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'identifier' => 'missingType.iterableValue',
+	'count' => 1,
+	'path' => __DIR__ . '/src/State.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Method State\\:\\:getUniqueCloneName\\(\\) has parameter \\$input with no value type specified in iterable type array\\.$#',
 	'identifier' => 'missingType.iterableValue',
 	'count' => 1,
@@ -29463,6 +29469,12 @@ $ignoreErrors[] = [
 ];
 $ignoreErrors[] = [
 	'message' => '#^Method State\\:\\:prepareInputForClone\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'identifier' => 'missingType.iterableValue',
+	'count' => 1,
+	'path' => __DIR__ . '/src/State.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method State\\:\\:processMassiveActionsForOneItemtype\\(\\) has parameter \\$ids with no value type specified in iterable type array\\.$#',
 	'identifier' => 'missingType.iterableValue',
 	'count' => 1,
 	'path' => __DIR__ . '/src/State.php',
