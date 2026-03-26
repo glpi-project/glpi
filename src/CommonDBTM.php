b0397e5b2fbf8977313f3d11701a4b2a84a9016a
Sebastien Monterisi <smonterisi@teclib.com>
2026-03-26 15:27:43 +0100

    csfix

--- src/CommonDBTM.php
+++ src/CommonDBTM.php
@@ -2944,7 +2944,7 @@ class CommonDBTM extends CommonGLPI
         return true;
     }
 
-    #[\Override]
+    #[Override]
     public function can($ID, int $right, ?array &$input = null, null &$reauth_needed = null): bool
     {
         if (Session::isInventory()) {
