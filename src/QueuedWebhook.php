468c2a8f3846126d2c5818bf867daf265819ef1f
Cédric Anne <cedric.anne@gmail.com>
2026-03-31 14:18:16 +0200

    Use effective body CRC token in webhooks

--- src/QueuedWebhook.php
+++ src/QueuedWebhook.php
@@ -188,7 +188,7 @@ class QueuedWebhook extends CommonDBChild
 
         if (GLPI_WEBHOOK_CRA_MANDATORY || $webhook->fields['use_cra_challenge']) {
             // Send CRA challenge
-            $result = $webhook::validateCRAChallenge($queued_webhook->fields['url'], 'validate_cra_challenge', $webhook->fields['secret']);
+            $result = $webhook::validateCRAChallenge($queued_webhook->fields['url'], (string) $queued_webhook->fields['body'], $webhook->fields['secret']);
             if ($result['status'] !== true) {
                 Toolbox::logInFile('webhook', "CRA challenge failed for webhook {$webhook->fields['name']} ({$webhook->getID()})");
                 return false;
