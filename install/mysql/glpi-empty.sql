fbed05044233197060d6f414f40e2bff509250e6
Herafia <llenik@teclib.com>
2026-04-03 15:57:18 +0200

    file glpi_empty

--- install/mysql/glpi-empty.sql
+++ install/mysql/glpi-empty.sql
@@ -5125,7 +5125,7 @@ CREATE TABLE `glpi_notimportedemails` (
   `mailcollectors_id` int unsigned NOT NULL DEFAULT '0',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `subject` text,
-  `messageid` varchar(255) NOT NULL,
+  `messageid` text,
   `reason` int NOT NULL DEFAULT '0',
   `users_id` int unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
