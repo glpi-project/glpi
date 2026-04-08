5de0e8c51ad8c3bf2eff05e1729c4dbb2518a344
Cédric Anne <cedric.anne@gmail.com>
2026-04-08 09:21:09 +0200

    Handle malformed agent credentials

--- src/Glpi/Agent/Communication/AbstractRequest.php
+++ src/Glpi/Agent/Communication/AbstractRequest.php
@@ -267,17 +267,23 @@ abstract class AbstractRequest
             } else {
                 $allowed = false;
                 // if Authorization start with 'Basic'
+                $matches = [];
                 if (preg_match('/^Basic\s+(.*)$/i', $authorization_header, $matches)) {
-                    $inventory_login = Config::getConfigurationValue('inventory', 'basic_auth_login');
-                    $inventory_password = (new GLPIKey())
-                        ->decrypt(Config::getConfigurationValue('inventory', 'basic_auth_password'));
-                    $agent_credential = base64_decode($matches[1]);
-                    [$agent_login, $agent_password] = explode(':', $agent_credential, 2);
+                    $agent_credentials = explode(':', base64_decode($matches[1]), 2);
                     if (
-                        $inventory_login == $agent_login
-                        && $inventory_password == $agent_password
+                        count($agent_credentials) !== 2
+                        || $agent_credentials[0] === ''
+                        || $agent_credentials[1] === ''
                     ) {
-                        $allowed = true;
+                        // Login and/or password is missing or empty
+                        $allowed = false;
+                    } else {
+                        $expected_login = Config::getConfigurationValue('inventory', 'basic_auth_login');
+                        $expected_password = (new GLPIKey())
+                            ->decrypt(Config::getConfigurationValue('inventory', 'basic_auth_password'));
+
+                        $allowed = $agent_credentials[0] === $expected_login
+                            && $agent_credentials[1] === $expected_password;
                     }
                 }
                 if (!$allowed) {
