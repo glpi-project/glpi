6f63488f92901511feda44748d0aaa15d4014b36
Curtis Conard <cconard96@gmail.com>
2026-04-10 08:26:06 -0400

    cache timezone list

--- src/DBmysql.php
+++ src/DBmysql.php
@@ -1908,36 +1908,43 @@ class DBmysql
      */
     public function getTimezones()
     {
+        global $GLPI_CACHE;
+
         $list = [];
 
-        $timezones = DateTimeZone::listIdentifiers();
-        $results_queries = [];
-        foreach ($timezones as $index => $timezone) {
-            $results_queries[] =  new QuerySubQuery([
-                'SELECT' => ['name', 'value'],
-                'FROM' => new QueryExpression(
-                    sprintf(
-                        '(SELECT %1$s as %2$s, CONVERT_TZ(%3$s, %4$s, %5$s) as %6$s) as %7$s',
-                        self::quoteValue($timezone),
-                        self::quoteName('name'),
-                        self::quoteValue('2000-01-01 00:00:00'),
-                        self::quoteValue('GMT'),
-                        self::quoteValue($timezone),
-                        self::quoteName('value'),
-                        self::quoteName(sprintf('timezone_%d', $index)),
-                    )
-                ),
-                'WHERE' => [
-                    ['NOT' => ['value' => null]],
-                ],
-            ]);
-        }
+        if (!$GLPI_CACHE->has('timezones')) {
+            $timezones = DateTimeZone::listIdentifiers();
+            $results_queries = [];
+            foreach ($timezones as $index => $timezone) {
+                $results_queries[] =  new QuerySubQuery([
+                    'SELECT' => ['name', 'value'],
+                    'FROM' => new QueryExpression(
+                        sprintf(
+                            '(SELECT %1$s as %2$s, CONVERT_TZ(%3$s, %4$s, %5$s) as %6$s) as %7$s',
+                            self::quoteValue($timezone),
+                            self::quoteName('name'),
+                            self::quoteValue('2000-01-01 00:00:00'),
+                            self::quoteValue('GMT'),
+                            self::quoteValue($timezone),
+                            self::quoteName('value'),
+                            self::quoteName(sprintf('timezone_%d', $index)),
+                        )
+                    ),
+                    'WHERE' => [
+                        ['NOT' => ['value' => null]],
+                    ],
+                ]);
+            }
 
-        $iterator = $this->request(['FROM' => new QueryUnion($results_queries)]);
-        foreach ($iterator as $row) {
-            $now = new DateTime();
-            $now->setTimezone(new DateTimeZone($row['name']));
-            $list[$row['name']] = $row['name'] . $now->format(" (T P)");
+            $iterator = $this->request(['FROM' => new QueryUnion($results_queries)]);
+            foreach ($iterator as $row) {
+                $now = new DateTime();
+                $now->setTimezone(new DateTimeZone($row['name']));
+                $list[$row['name']] = $row['name'] . $now->format(" (T P)");
+            }
+            $GLPI_CACHE->set('timezones', $list);
+        } else {
+            $list = $GLPI_CACHE->get('timezones');
         }
 
         return $list;
