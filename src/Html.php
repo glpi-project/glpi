8ae00b468f279256b6ff1232b57ed6bbb4451dc0
Laura <35998899+Herafia@users.noreply.github.com>
2026-04-10 16:16:55 +0200

    Update src/Html.php
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- src/Html.php
+++ src/Html.php
@@ -3785,7 +3785,7 @@ JS;
                         // Close TinyMCE toolbar dropdowns and blur active buttons when clicking outside editor UI elements
                         $(document).on('click', function(e) {
                             const target = $(e.target);
-                            const isEditorElementClicked = target.closest('[class^="tox-"]');
+                            const isEditorElementClicked = target.closest('[class*="tox-"]').length > 0;
 
                             if (!isEditorElementClicked) {
                                 $('.tox-tbtn.tox-tbtn--enabled[data-mce-name="overflow-button"]').trigger('click').trigger('blur');
