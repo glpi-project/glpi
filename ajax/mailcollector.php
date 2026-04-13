2df4f825740cb8828719bf4e684f81c69e59ecdf
Sébastien Monterisi <smonterisi@teclib.com>
2026-04-13 15:33:30 +0200

    right updated from READ to UPDATE
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- ajax/mailcollector.php
+++ ajax/mailcollector.php
@@ -43,7 +43,7 @@ include('../inc/includes.php');
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 
-Session::checkRight("config", READ);
+Session::checkRight("config", UPDATE);
 
 $mailcollector = new MailCollector();
 
