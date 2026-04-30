62d87ef46efa0ecb7c2965b6417b53157443cdf8
tremo <tarekrummo64@gmail.com>
2026-04-30 16:18:30 +0200

    check that entity_id is present

--- ajax/dropdownValidator.php
+++ ajax/dropdownValidator.php
@@ -93,7 +93,7 @@ if (isset($_POST["validatortype"])) {
                 }
 
                 $supervisor_entities = Profile_User::getUserEntities($requester->fields['users_id_supervisor']);
-                if (!in_array($_POST['entity'] ?? '', $supervisor_entities)) {
+                if (!isset($_POST['entity']) || !in_array($_POST['entity'], $supervisor_entities)) {
                     continue;
                 }
 
