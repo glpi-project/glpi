3549bd407cec2f872e11a779d7e73f495f9a1562
Langlois Gaëtan <64356364+MyvTsv@users.noreply.github.com>
2026-05-06 10:31:15 +0200

    fix(forms): fix specific ticket template not being applied (#24127)

--- src/CommonITILObject.php
+++ src/CommonITILObject.php
@@ -1893,6 +1893,11 @@ abstract class CommonITILObject extends CommonDBTM implements KanbanInterface, T
             }
         }
 
+        // If category, entity, or type fields are not updated, the template is not changed.
+        if (empty($input['itilcategories_id']) && empty($input['entities_id']) && empty($input['type'])) {
+            return $input;
+        }
+
         // First get ticket template associated: entity and type/category
         $tt = $this->getITILTemplateFromInput($input);
 
