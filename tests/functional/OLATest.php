5c7efcf816d8661952aed0be8fb12c0d5fb178c1
Sebastien Monterisi <smonterisi@teclib.com>
2026-05-20 17:00:37 +0200

    update test message expectation changed in previous commit

--- tests/functional/OLATest.php
+++ tests/functional/OLATest.php
@@ -562,7 +562,7 @@ class OLATest extends DbTestCase
 
         foreach ($removed_fields as $removed_field) {
             $this->expectException(RuntimeException::class);
-            $this->expectExceptionMessageMatches('/Input field .* is not used anymore.*/');
+            $this->expectExceptionMessageMatches('/Input fields ".*" are not used anymore/');
             $value = $removed_field === 'olalevels_id_ttr' ? 1 : date('Y-m-d 00:00:00');
 
             $this->createTicket(
