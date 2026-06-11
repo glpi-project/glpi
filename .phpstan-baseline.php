fd5580e980030ef0afd6b4cd64a921856b402676
eduardomozart <2974895+eduardomozart@users.noreply.github.com>
2026-06-11 18:05:59 -0300

    Update .phpstan-baseline.php

--- .phpstan-baseline.php
+++ .phpstan-baseline.php
@@ -9916,13 +9916,7 @@ $ignoreErrors[] = [
 $ignoreErrors[] = [
 	'message' => '#^Access to an undefined property object\\:\\:\\$designation\\.$#',
 	'identifier' => 'property.notFound',
-	'count' => 2,
-	'path' => __DIR__ . '/src/Glpi/Inventory/Asset/NetworkCard.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Access to an undefined property object\\:\\:\\$devicenetworkcardmodels_id\\.$#',
-	'identifier' => 'property.notFound',
-	'count' => 1,
+	'count' => 3,
 	'path' => __DIR__ . '/src/Glpi/Inventory/Asset/NetworkCard.php',
 ];
 $ignoreErrors[] = [
