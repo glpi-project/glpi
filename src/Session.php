059f7b9694b2213b5385839456264444882845e2
MyuTsu <gaetan.langlois31@gmail.com>
2026-06-11 17:39:44 +0200

    fix(Session): prevent PHP error in addToNavigateListItems when session key is unset

--- src/Session.php
+++ src/Session.php
@@ -366,7 +366,7 @@ class Session
      */
     public static function addToNavigateListItems($itemtype, $ID)
     {
-        if (!in_array($ID, $_SESSION['glpilistitems'][$itemtype])) {
+        if (empty($_SESSION['glpilistitems'][$itemtype]) || !in_array($ID, $_SESSION['glpilistitems'][$itemtype])) {
             $_SESSION['glpilistitems'][$itemtype][] = $ID;
         }
     }
