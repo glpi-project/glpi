ecfc91140dc7232e5026c5e571c3efdc204fb4e0
AdrienClairembault <aclairembault@teclib.com>
2026-07-01 16:01:39 +0200

    Fix importForm returning wrong form, causing flaky tests

--- tests/e2e/utils/FormImporter.ts
+++ tests/e2e/utils/FormImporter.ts
@@ -78,7 +78,7 @@ export class FormImporter
 
         const dom = new JSDOM(body);
         const link = dom.window.document.querySelector(
-            'a[href^="/front/form/form.form.php?id="]'
+            '[data-testid="import-results"] a[href^="/front/form/form.form.php?id="]'
         ) as HTMLAnchorElement;
 
         if (link === null) {
