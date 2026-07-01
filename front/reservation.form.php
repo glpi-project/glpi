10772015b9365b0816303f04fcd97bec2ac13045
f2cmb <2480194+f2cmb@users.noreply.github.com>
2026-07-01 15:08:53 +0200

    restore original wording

--- front/reservation.form.php
+++ front/reservation.form.php
@@ -44,7 +44,13 @@ global $CFG_GLPI;
 $rr = new Reservation();
 
 if (Session::getCurrentInterface() == "helpdesk") {
-    Html::helpHeader(Reservation::getTypeName(Session::getPluralNumber()));
+    Html::helpHeader(
+        sprintf(
+            '%s - %s',
+            Reservation::getTypeName(Session::getPluralNumber()),
+            __('Simplified interface')
+        )
+    );
 } else {
     Html::header(Reservation::getTypeName(Session::getPluralNumber()), '', "tools", "reservationitem");
 }
