25009e77547b32661510df416eb4e85710f79520
Cloud <kt286@qq.com>
2026-07-06 19:45:52 +0800

    fix(i18n): add tinymce language code mapping for mismatched locale codes
    Several GLPI locale codes (zh_CN, zh_TW, pt_BR, he_IL, nb_NO, nn_NO,
    hi_IN, bn_BD, km_KH, is_IS, ro_RO, uk_UA) don't match the file names
    used by tinymce-i18n. Add an explicit mapping to resolve the correct
    tinymce language file before falling back to the generic locale lookup.

--- src/Html.php
+++ src/Html.php
@@ -3515,7 +3515,29 @@ JS;
 
         $language = $_SESSION['glpilanguage'];
         if (!file_exists(GLPI_ROOT . "/public/lib/tinymce-i18n/langs6/$language.js")) {
-            $language = $CFG_GLPI["languages"][$_SESSION['glpilanguage']][2];
+            // Some GLPI language codes don't match tinymce-i18n file names.
+            // Use a dedicated mapping to find the correct tinymce language file.
+            $tinymce_lang_map = [
+                'fr_FR'  => 'fr_FR',
+                'fr_CA'  => 'fr_FR',
+                'fr_BE'  => 'fr_FR',
+                'he_IL'  => 'he_IL',
+                'hi_IN'  => 'hi',
+                'nb_NO'  => 'nb_NO',
+                'nn_NO'  => 'nb_NO',
+                'pt_BR'  => 'pt_BR',
+                'ro_RO'  => 'ro',
+                'uk_UA'  => 'uk',
+                'zh_CN'  => 'zh-Hans',
+                'zh_TW'  => 'zh-Hant',
+                'zh_HK'  => 'zh_HK',
+                'is_IS'  => 'is_IS'
+            ];
+            if (isset($tinymce_lang_map[$_SESSION['glpilanguage']])) {
+                $language = $tinymce_lang_map[$_SESSION['glpilanguage']];
+            } else {
+                $language = $CFG_GLPI["languages"][$_SESSION['glpilanguage']][2];
+            }
             if (!file_exists(GLPI_ROOT . "/public/lib/tinymce-i18n/langs6/$language.js")) {
                 $language = "en_GB";
             }
