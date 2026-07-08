5e69229ee7f1000bea7c15e5134453e125cd5ca1
Curtis Conard <cconard96@gmail.com>
2026-07-08 09:51:57 -0400

    fix own session check

--- src/Glpi/Controller/Security/SessionTrackerController.php
+++ src/Glpi/Controller/Security/SessionTrackerController.php
@@ -102,7 +102,7 @@ final class SessionTrackerController extends AbstractController
     public function revokeAllSessions(Request $request): Response
     {
         $users_id = $request->request->getInt('users_id', 0);
-        $is_own_sessions = $users_id === 0 || $users_id === Session::getLoginUserID();
+        $is_own_sessions = $users_id > 0 && $users_id === Session::getLoginUserID();
         if (!$is_own_sessions && !\OAuthClient::canUpdate()) {
             throw new AccessDeniedHttpException();
         }
