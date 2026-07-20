8459ef87b125abe205a861e4b94f1ed707867470
Adrien Clairembault <aclairembault@teclib.com>
2026-07-20 08:47:34 +0000

    Data migration

--- install/mysql/glpi-empty.sql
+++ install/mysql/glpi-empty.sql
@@ -4119,7 +4119,7 @@ CREATE TABLE `glpi_itilcategories` (
   `completename` text,
   `comment` text,
   `level` int NOT NULL DEFAULT '0',
-  `knowbaseitemcategories_id` int unsigned NOT NULL DEFAULT '0',
+  `knowbaseitems_id` int unsigned NOT NULL DEFAULT '0',
   `users_id` int unsigned NOT NULL DEFAULT '0',
   `groups_id` int unsigned NOT NULL DEFAULT '0',
   `code` varchar(255) DEFAULT NULL,
@@ -4140,7 +4140,7 @@ CREATE TABLE `glpi_itilcategories` (
   KEY `name` (`name`),
   KEY `entities_id` (`entities_id`),
   KEY `is_recursive` (`is_recursive`),
-  KEY `knowbaseitemcategories_id` (`knowbaseitemcategories_id`),
+  KEY `knowbaseitems_id` (`knowbaseitems_id`),
   KEY `users_id` (`users_id`),
   KEY `groups_id` (`groups_id`),
   KEY `is_helpdeskvisible` (`is_helpdeskvisible`),
@@ -7354,7 +7354,7 @@ CREATE TABLE `glpi_taskcategories` (
   `is_helpdeskvisible` tinyint NOT NULL DEFAULT '1',
   `date_mod` timestamp NULL DEFAULT NULL,
   `date_creation` timestamp NULL DEFAULT NULL,
-  `knowbaseitemcategories_id` int unsigned NOT NULL DEFAULT '0',
+  `knowbaseitems_id` int unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
   KEY `taskcategories_id` (`taskcategories_id`),
@@ -7364,7 +7364,7 @@ CREATE TABLE `glpi_taskcategories` (
   KEY `is_helpdeskvisible` (`is_helpdeskvisible`),
   KEY `date_mod` (`date_mod`),
   KEY `date_creation` (`date_creation`),
-  KEY `knowbaseitemcategories_id` (`knowbaseitemcategories_id`),
+  KEY `knowbaseitems_id` (`knowbaseitems_id`),
   KEY `level` (`level`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
