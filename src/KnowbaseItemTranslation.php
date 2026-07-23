eec4b74e3e4db6f1d3a38aad0eb3ec309567b8b6
Cédric Anne <cedric.anne@gmail.com>
2026-07-23 09:38:01 +0200

    Apply suggestion from @cedric-anne

--- src/KnowbaseItemTranslation.php
+++ src/KnowbaseItemTranslation.php
@@ -179,7 +179,7 @@ TWIG, $twig_params);
                     $(() => {
                         function addTranslation{{ item.getID() ~ rand }}() {
                             $('#viewtranslation{{ item.getID() ~ rand }}').load(
-                                '{{ path('ajax/viewsubitem.php')|e('js') }}',
+                                '{{ path('/ajax/viewsubitem.php')|e('js') }}',
                                 {
                                     type: 'KnowbaseItemTranslation',
                                     parenttype: '{{ get_class(item)|e('js') }}',
