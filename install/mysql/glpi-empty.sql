3dcd3e12c1ee3b9534a9cef961317054a7af3b83
AdrienClairembault <aclairembault@teclib.com>
2026-07-24 10:47:13 +0200

    Persist KB tree fold state
    This commit was reverted at the top to avoid conflicts, I've remade
    it from scratch at the end to make sure it take into account the new
    changes.

--- install/mysql/glpi-empty.sql
+++ install/mysql/glpi-empty.sql
@@ -8084,6 +8084,7 @@ CREATE TABLE `glpi_users` (
   `savedsearches_pinned` text,
   `timeline_order` char(20) DEFAULT NULL,
   `itil_layout` text,
+  `folded_knowbaseitems` json,
   `richtext_layout` char(20) DEFAULT NULL,
   `set_default_requester` tinyint DEFAULT NULL,
   `lock_autolock_mode` tinyint DEFAULT NULL,
