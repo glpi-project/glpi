77acf8c3f66b4227a58093b63643de43a509a249
f2cmb <2480194+f2cmb@users.noreply.github.com>
2026-07-24 10:04:24 +0200

    Add optional text-quote anchor fields to KB article comments

---
+++ install/migrations/update_11.0.x_to_12.0.0/knowbaseitem_comments_anchor.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2026 Teclib' and contributors.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
+/**
+ * @var DBmysql $DB
+ * @var Migration $migration
+ */
+
+// Add an optional text-quote anchor (W3C TextQuoteSelector-style) to comments,
+// so a comment can be attached to a specific selected passage of the article
+// instead of the whole article. Only populated on root (non-reply) comments.
+$table = 'glpi_knowbaseitems_comments';
+
+$migration->addField($table, 'anchor_prefix', 'string', [
+    'after' => 'parent_comment_id',
+    'value' => null,
+]);
+$migration->addField($table, 'anchor_exact', 'text', [
+    'after' => 'anchor_prefix',
+]);
+$migration->addField($table, 'anchor_suffix', 'string', [
+    'after' => 'anchor_exact',
+    'value' => null,
+]);
+$migration->addField($table, 'anchor_occurrence', 'integer', [
+    'after'     => 'anchor_suffix',
+    'nodefault' => true,
+    'null'      => true,
+]);
