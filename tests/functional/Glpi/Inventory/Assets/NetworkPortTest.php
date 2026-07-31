658e0ddc87cfaf78867e6de1a2db5178bdadc8ce
E <2974895+eduardomozart@users.noreply.github.com>
2026-07-31 04:36:29 -0300

    Migrate NetworkPort tests to NetworkCardTest

--- tests/functional/Glpi/Inventory/Assets/NetworkPortTest.php
+++ tests/functional/Glpi/Inventory/Assets/NetworkPortTest.php
@@ -36,11 +36,9 @@ namespace tests\units\Glpi\Inventory\Asset;
 
 use DateInterval;
 use DateTime;
-use Glpi\Inventory\Asset\NetworkCard;
 use Glpi\Inventory\Conf;
 use Glpi\Inventory\Converter;
 use Glpi\Inventory\Inventory;
-use Glpi\Inventory\MainAsset\Computer;
 use Glpi\Tests\AbstractInventoryAsset;
 use PHPUnit\Framework\Attributes\AllowMockObjectsWithoutExpectations;
 use PHPUnit\Framework\Attributes\DataProvider;
@@ -589,81 +587,6 @@ Compiled Mon 23-Jul-12 13:22 by prod_rel_team</COMMENTS>
         $lockedfield = new \Lockedfield();
         $this->assertTrue($lockedfield->isHandled($networkport));
         $this->assertEmpty($lockedfield->getLockedValues($networkport->getType(), $networkport->fields['id']));
-    }
-
-    public function testComputerNetworkPortMetrics()
-    {
-        $networkport = new \NetworkPort();
-        $networkmetric = new \NetworkPortMetrics();
-
-        $ifinbytes    = 3559673658;
-        $ifoutbytes   = 3257789612;
-        $ifouterrors  = 2316546841;
-        $ifinerrors   = 8974561231;
-
-        $xml_source = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
-        <REQUEST>
-          <CONTENT>
-            <NETWORKS>
-              <DESCRIPTION>Ethernet</DESCRIPTION>
-              <MACADDR>00:24:13:ea:a7:01</MACADDR>
-              <STATUS>Up</STATUS>
-              <TYPE>ethernet</TYPE>
-              <IFINERRORS>$ifinerrors</IFINERRORS>
-              <IFINBYTES>$ifinbytes</IFINBYTES>
-              <IFOUTERRORS>$ifouterrors</IFOUTERRORS>
-              <IFOUTBYTES>$ifoutbytes</IFOUTBYTES>
-            </NETWORKS>
-            <VERSIONCLIENT>FusionInventory-Inventory_v2.4.1</VERSIONCLIENT>
-          </CONTENT>
-          <DEVICEID>foo-computer</DEVICEID>
-          <QUERY>INVENTORY</QUERY>
-        </REQUEST>";
-
-        $computer = getItemByTypeName('Computer', '_test_pc01');
-        $computer_id = $computer->fields['id'];
-        $this->assertGreaterThan(0, $computer_id);
-
-        $converter = new Converter();
-        $data = $converter->convert($xml_source);
-        $json = json_decode($data);
-
-        $conf = new Conf();
-
-        $cardAsset = new NetworkCard($computer, $json->content->networks);
-        $cardAsset->setExtraData((array) $json->content);
-        $cardAsset->checkConf($conf);
-        $cardAsset->prepare();
-
-        // Simulate the MainAsset flow: collect NetworkCard ports and create them via handlePorts()
-        // on the Computer main asset, which calls portCreated() → handlePortMetrics().
-        $mainAsset = new Computer($computer, []);
-        $mainAsset->checkConf($conf);
-        $mainAsset->addNetworkPorts($cardAsset->getNetworkPorts());
-        $mainAsset->handlePorts($computer::class, $computer_id);
-
-        //get networkport
-        $this->assertTrue(
-            $networkport->getFromDbByCrit(['itemtype' => 'Computer', 'items_id' => $computer_id, 'instantiation_type' => 'NetworkPortEthernet', 'mac' => '00:24:13:ea:a7:01'])
-        );
-
-        //get networkport metric
-        $this->assertTrue($networkmetric->getFromDbByCrit(['networkports_id' => $networkport->fields['id']]));
-
-        $db_input = $networkmetric->fields;
-        unset($db_input['date_creation']);
-        unset($db_input['date_mod']);
-        unset($db_input['id']);
-
-        $expected_input = [
-            "date"            => date('Y-m-d'),
-            "ifinbytes"       => $ifinbytes,
-            "ifinerrors"      => $ifinerrors,
-            "ifoutbytes"      => $ifoutbytes,
-            "ifouterrors"     => $ifouterrors,
-            "networkports_id" => $networkport->fields['id'],
-        ];
-        $this->assertSame($expected_input, $db_input);
     }
 
     public function testNetworkPortEthernetSpeed()
