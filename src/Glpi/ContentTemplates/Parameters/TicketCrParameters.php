1cfe3087ce8aac3ad365d2ee51cf06248b75344f
LucasSFF <103195523+LucasSFF@users.noreply.github.com>
2026-08-09 01:11:36 -0300

    Refactor TicketCrParameters to improve code clarity
    Satisfy PHPStan and add license header

--- src/Glpi/ContentTemplates/Parameters/TicketCrParameters.php
+++ src/Glpi/ContentTemplates/Parameters/TicketCrParameters.php
@@ -1,4 +1,38 @@
 <?php
+
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2026 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+
 namespace Glpi\ContentTemplates\Parameters;
 
 use CommonDBTM;
@@ -16,7 +50,9 @@ class TicketCrParameters extends AbstractParameters
     {
         return __('Created tickets');
     }
-
+    /**
+	* @return array<class-string<CommonDBTM>>
+	*/
     protected function getTargetClasses(): array
     {
         return [Ticket::class];
@@ -36,12 +72,12 @@ class TicketCrParameters extends AbstractParameters
 
     protected function defineValues(CommonDBTM $ticket): array
     {
+        /** @var Ticket $ticket */
 	    $fields = $ticket->fields;
         $values= [
             'id'        => $fields['id'],
-            'ref'       => "#" . $fields['id'],
             'link'      => $ticket->getLink(),
-			'url'	=> ltrim($ticket->getLinkURL(), '/'),
+			'url'	    => ltrim($ticket->getLinkURL(), '/'),
 			'name'      => $fields['name'],
 			'content'   => $fields['content'],
 			'status'    => $ticket::getStatus($fields['status']),
