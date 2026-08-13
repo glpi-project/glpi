5f53e9ff04f743f01d15f2c48b0fd3aac981f6e4
Benoit VIGNAL <git@benoit-vignal.fr>
2026-08-13 13:38:09 +0200

    Fix removed accidentally part of licence header

--- phpunit/web/ApiDetectionTest.php
+++ phpunit/web/ApiDetectionTest.php
@@ -14,6 +14,8 @@
  *
  * LICENSE
  *
+ * This file is part of GLPI.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
