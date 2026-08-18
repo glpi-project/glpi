ba9e87d8942a34dc7ca86c6e39d41264768382b1
Julien Durand <jdurand@teclib.com>
2026-08-18 10:30:34 +0200

    xhore(45457): Simplified array of users and tokens to a simple list of user ids

--- src/Glpi/Console/User/VerifyUsersTokensCommand.php
+++ src/Glpi/Console/User/VerifyUsersTokensCommand.php
@@ -34,11 +34,12 @@
 
 namespace Glpi\Console\User;
 
+use Glpi\Console\AbstractCommand;
 use GLPIKey;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Glpi\Console\AbstractCommand;
 use User;
 
 class VerifyUsersTokensCommand extends AbstractCommand
@@ -53,20 +54,20 @@ class VerifyUsersTokensCommand extends AbstractCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
-        $token_fields = ['api_token', "cookie_token"];
+        $token_fields = ['api_token'];
 
-        foreach($token_fields as $token_field){
+        foreach ($token_fields as $token_field) {
             $token_name = explode("_", $token_field)[0];
-            $this->output->writeln(
-                '<info>'. sprintf(__('Checking %s tokens'), $token_name) .'</info>'
+            $output->writeln(
+                '<info>' . sprintf(__('Checking %s tokens'), $token_name) . '</info>'
             );
-            $tokens_in_error = $this->checkTokens($token_field, $token_name);
-            if (empty($tokens_in_error)) {
+            $users_with_token_in_error = $this->checkTokens($token_field, $token_name);
+            if ($users_with_token_in_error === []) {
                 continue;
             }
-            $this->output->writeln('<comment>' . __('Asking for permission to fix the malformed tokens') . '</comment>');
+            $output->writeln('<comment>' . __('Asking for permission to fix the malformed tokens') . '</comment>');
             $this->askForConfirmation();
-            $outcome = $this->fixTokens($tokens_in_error, $token_name);
+            $outcome = $this->fixTokens($users_with_token_in_error, $token_field, $token_name);
             if ($outcome === 1) {
                 // If we encountered an error, we stop the process.
                 return $outcome;
@@ -75,6 +76,12 @@ class VerifyUsersTokensCommand extends AbstractCommand
         return 0;
     }
 
+    /**
+     * Checks for malformed tokens in User table
+     * @param string $token_field Name of the field in User database table.
+     * @param string $token_name Display name of the token.
+     * @phpstan-return array<string> A list of users ids with malformed tokens.
+     */
     private function checkTokens(string $token_field, string $token_name): array
     {
         global $DB;
@@ -90,21 +97,21 @@ class VerifyUsersTokensCommand extends AbstractCommand
             ],
         ]);
 
-        $tokens_in_error = [];
-        foreach($iterator as $row){
-            if ($glpi_key->decrypt($row[$token_field]) === ''){
-                $tokens_in_error[] = $row;
+        $users_with_token_in_error = [];
+        foreach ($iterator as $row) {
+            if ($glpi_key->decrypt($row[$token_field]) === '') {
+                $users_with_token_in_error[] = $row['id'];
             }
         }
 
-        if (empty($tokens_in_error)){
+        if ($users_with_token_in_error === []) {
             $this->output->writeln(
                 '<info>' . sprintf(__('No malformed %s token found, all good !'), $token_name) . '</info>'
             );
-            return $tokens_in_error;
+            return $users_with_token_in_error;
         }
 
-        $count = count($tokens_in_error);
+        $count = count($users_with_token_in_error);
         $this->output->writeln(
             '<comment>' . sprintf(
                 _n('Found %d user with a malformed %s token.', 'Found %d users with a malformed %s token.', $count),
@@ -115,27 +122,35 @@ class VerifyUsersTokensCommand extends AbstractCommand
 
         $table = new Table($this->output);
         $table->setHeaders(['User ID']);
-        foreach ($tokens_in_error as $row) {
-            $table->addRow([$row['id']]);
+        foreach ($users_with_token_in_error as $user_id) {
+            $table->addRow([$user_id]);
         }
         $table->render();
 
-        return $tokens_in_error;
+        return $users_with_token_in_error;
     }
 
-    private function fixTokens(array $tokens_in_error, string $token_name): int
+    /**
+     * Fix tokens in a given array
+     * @param array<string> $users_with_token_in_error A list of user ids for which the token is in error
+     * @param string $token_field Name of the field in User database table.
+     * @param string $token_name The display name of the token
+     * @return int 1 in case of error, 0 otherwise
+     */
+    private function fixTokens(array $users_with_token_in_error, string $token_field, string $token_name): int
     {
         $failed = 0;
         $user = new User();
-        $progress_message = fn(array $row) => sprintf(__('Regenerating token for user %d...'), $row['id']);
-        foreach ($this->iterate($tokens_in_error, $progress_message) as $row) {
+        $progress_message = fn(int $user_id) => sprintf(__('Regenerating token for user %d...'), $user_id);
+        $regenerate_field = "_regenerate_{$token_field}";
+        foreach ($this->iterate($users_with_token_in_error, $progress_message) as $user_id) {
             $success = $user->update([
-                'id'                    => $row['id'],
-                '_regenerate_api_token' => true,
+                'id'                    => $user_id,
+                $regenerate_field       => true,
             ]);
             if (!$success) {
                 $this->outputMessage(
-                    '<error>' . sprintf(__('Failed to regenerate %s token for user %d.'), $token_name, $row['id']) . '</error>',
+                    '<error>' . sprintf(__('Failed to regenerate %s token for user %d.'), $token_name, $user_id) . '</error>',
                     OutputInterface::VERBOSITY_QUIET
                 );
                 $failed++;
