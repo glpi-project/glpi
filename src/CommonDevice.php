9fd4edd16b318a09f30284456d5ca46aeb79577d
E <2974895+eduardomozart@users.noreply.github.com>
2026-09-02 11:24:28 -0300

    Revert src/CommonDevice.php to match main branch

--- src/CommonDevice.php
+++ src/CommonDevice.php
@@ -171,6 +171,7 @@ abstract class CommonDevice extends CommonDropdown
                         $menu['options'][$key] = [
                             'title' => $val,
                             'page'  => $tmp::getSearchURL(false),
+                            'icon'  => $tmp::getIcon(),
                             'links' => [
                                 'search' => $tmp::getSearchURL(false),
                             ],
@@ -195,6 +196,7 @@ abstract class CommonDevice extends CommonDropdown
                             $menu['options'][$item_device_key] = [
                                 'title' => $itemTypeName,
                                 'page'  => $item_device_search_url,
+                                'icon'  => $itemClass::getIcon(),
                                 'links' => [
                                     'search' => $item_device_search_url,
                                 ],
