2c46e01a1ff915aaf2091c6b81b6d698d73eee54
Benoît VIGNAL <2380113+froozeify@users.noreply.github.com>
2026-09-09 15:51:10 +0200

    Remove deprecated password encryption fallback check (#25440)
    * Remove deprecated password encryption fallback check
    
    * Remove deprecated password encryption fallback check
    
    * Update src/Auth.php
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * test: Add password hash tests
    
    ---------
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- src/Auth.php
+++ src/Auth.php
@@ -324,6 +324,19 @@ class Auth extends CommonGLPI
     }
 
     /**
+     * Password is using and old encryption method like md5 or sha1
+     *
+     * @param string $hash Hash
+     *
+     * @return boolean
+     */
+    private static function passwordIsOutdated(string $hash): bool
+    {
+        $info = password_get_info($hash);
+        return !isset($info['algo']) || !$info['algo'];
+    }
+
+    /**
      * Check is a password match the stored hash
      *
      * @since 0.85
@@ -335,21 +348,7 @@ class Auth extends CommonGLPI
      */
     public static function checkPassword($pass, $hash)
     {
-
-        $tmp = password_get_info($hash);
-
-        if (isset($tmp['algo']) && $tmp['algo']) {
-            $ok = password_verify($pass, $hash);
-        } elseif (strlen($hash) == 32) {
-            $ok = md5($pass) === $hash;
-        } elseif (strlen($hash) == 40) {
-            $ok = sha1($pass) === $hash;
-        } else {
-            $salt = substr($hash, 0, 8);
-            $ok = ($salt . sha1($salt . $pass) === $hash);
-        }
-
-        return $ok;
+        return password_verify($pass, $hash);
     }
 
     /**
@@ -442,6 +441,11 @@ class Auth extends CommonGLPI
         if ($result->numrows() == 1) {
             $row = $result->current();
             $password_db = $row['password'];
+
+            if (self::passwordIsOutdated($password_db)) {
+                $this->addToError(__('For security reasons, your password has expired. Please contact your administrator to reset it.'));
+                return false;
+            }
 
             if (self::checkPassword($password, $password_db)) {
                 // Disable account if password expired
