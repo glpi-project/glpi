09da57d61392e0b15b4e68297971bfa2057d917b
Johan Cwiklinski <johan@x-tnd.be>
2026-09-11 11:51:45 +0200

    fix(search): compute the same table alias in WHERE/ORDER BY as in JOIN
    A `DropdownType` custom field targeting another custom asset joins
    `glpi_assets_assets` on itself, so the joined table equals the itemtype
    table and the `_custom_fields` part was dropped from the WHERE clause,
    leading to `Unknown column 'glpi_assets_assets_<hash>.name' in 'WHERE'`.
    
    Fixes #25026

--- src/Glpi/Search/Provider/SQLProvider.php
+++ src/Glpi/Search/Provider/SQLProvider.php
@@ -1169,21 +1169,20 @@ final class SQLProvider implements SearchProviderInterface
         $is_fkey_composite_on_self = getTableNameForForeignKeyField($opt["linkfield"]) === $table
             && $opt["linkfield"] !== getForeignKeyFieldForTable($table);
         $orig_table = SearchEngine::getOrigTableName($itemtype);
+        $complexjoin = isset($opt['joinparams']) ? Search::computeComplexJoinID($opt['joinparams']) : '';
         if (
-            ($table !== 'asset_types')
-            && ($is_fkey_composite_on_self || $table !== $orig_table)
+            (
+                (($table !== 'asset_types') && ($is_fkey_composite_on_self || $table !== $orig_table))
+                || !empty($complexjoin)
+            )
             && ($opt["linkfield"] !== getForeignKeyFieldForTable($table))
         ) {
             $addtable = "_" . $opt["linkfield"];
             $table   .= $addtable;
         }
 
-        if (isset($opt['joinparams'])) {
-            $complexjoin = Search::computeComplexJoinID($opt['joinparams']);
-
-            if (!empty($complexjoin)) {
-                $table .= "_" . $complexjoin;
-            }
+        if (!empty($complexjoin)) {
+            $table .= "_" . $complexjoin;
         }
 
         $addmeta = "";
@@ -4161,19 +4160,18 @@ final class SQLProvider implements SearchProviderInterface
             $is_fkey_composite_on_self = getTableNameForForeignKeyField($searchopt[$ID]["linkfield"]) == $table
                 && $searchopt[$ID]["linkfield"] != getForeignKeyFieldForTable($table);
             $orig_table = SearchEngine::getOrigTableName($itemtype);
+            $complexjoin = isset($searchopt[$ID]['joinparams'])
+                ? self::computeComplexJoinID($searchopt[$ID]['joinparams'])
+                : '';
             if (
-                ($is_fkey_composite_on_self || $table != $orig_table)
+                (($is_fkey_composite_on_self || $table != $orig_table) || !empty($complexjoin))
                 && ($searchopt[$ID]["linkfield"] != getForeignKeyFieldForTable($table))
             ) {
                 $addtable .= "_" . $searchopt[$ID]["linkfield"];
             }
 
-            if (isset($searchopt[$ID]['joinparams'])) {
-                $complexjoin = self::computeComplexJoinID($searchopt[$ID]['joinparams']);
-
-                if (!empty($complexjoin)) {
-                    $addtable .= "_" . $complexjoin;
-                }
+            if (!empty($complexjoin)) {
+                $addtable .= "_" . $complexjoin;
             }
 
             if (isset($CFG_GLPI["union_search_type"][$itemtype])) {
