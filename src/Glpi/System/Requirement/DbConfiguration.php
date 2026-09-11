75e0ebdb85588fe2611e8b9b2f2a3f21e8efc278
Mohammed Ghouse <mohammed@marifextech.com>
2026-09-12 02:08:10 +0530

    Check max_allowed_packet during installation
    A too-low max_allowed_packet makes the installation fail with an unclear "MySQL server has gone away" error while importing the default data. Check it in the DbConfiguration requirement so a clear message is shown instead.

--- src/Glpi/System/Requirement/DbConfiguration.php
+++ src/Glpi/System/Requirement/DbConfiguration.php
@@ -37,6 +37,7 @@ namespace Glpi\System\Requirement;
 
 use DBmysql;
 use mysqli_result;
+use Toolbox;
 
 /**
  * @since 10.0.0
@@ -58,17 +59,37 @@ class DbConfiguration extends AbstractRequirement
         $this->db = $db;
     }
 
+    /**
+     * Minimum `max_allowed_packet` value (bytes) required to import the default data.
+     * A lower value makes the installation fail with a "MySQL server has gone away" error.
+     */
+    private const MIN_MAX_ALLOWED_PACKET = 16 * 1024 * 1024;
+
     protected function check()
     {
-        $query = 'SELECT @@GLOBAL.' . $this->db->quoteName('innodb_page_size as innodb_page_size');
+        $query = 'SELECT @@GLOBAL.' . $this->db->quoteName('innodb_page_size as innodb_page_size')
+            . ', @@GLOBAL.' . $this->db->quoteName('max_allowed_packet as max_allowed_packet');
 
         /** @var mysqli_result $db_config_res */
         $db_config_res = $this->db->doQuery($query);
         $db_config = $db_config_res->fetch_assoc();
 
+        if (!is_array($db_config)) {
+            $this->validated = false;
+            $this->validation_messages[] = __('Unable to read the database configuration.');
+            return;
+        }
+
         $incompatibilities = [];
         if ((int) $db_config['innodb_page_size'] < 8192) {
             $incompatibilities[] = '"innodb_page_size" must be >= 8KB.';
+        }
+        if ((int) $db_config['max_allowed_packet'] < self::MIN_MAX_ALLOWED_PACKET) {
+            $incompatibilities[] = sprintf(
+                '"max_allowed_packet" must be >= %1$s (current value is %2$s).',
+                Toolbox::getSize(self::MIN_MAX_ALLOWED_PACKET),
+                Toolbox::getSize((int) $db_config['max_allowed_packet'])
+            );
         }
 
         if (count($incompatibilities) > 0) {
