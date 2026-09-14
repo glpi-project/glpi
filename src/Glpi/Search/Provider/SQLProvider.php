25de203aa9d92c1ee940eb58cdd213206e21910d
tomolimo <olivier.moron@araymond.com>
2026-09-14 10:44:17 +0200

    Update SQLProvider.php
    To be sure that alternative_email exists in join table. In order to prevent "Unknown column 'alternative_email' in 'IN/ALL/ANY subquery'" when a Group/User::dropdown multiple is used.

--- src/Glpi/Search/Provider/SQLProvider.php
+++ src/Glpi/Search/Provider/SQLProvider.php
@@ -1521,9 +1521,11 @@ final class SQLProvider implements SearchProviderInterface
                             ],
                         ],
                     ];
-                    $subquery_specific_username_anonymous = [
-                        'alternative_email' => ['LIKE', self::makeTextSearchValue($val)],
-                    ];
+                    if ($DB->getField($searchopt[$ID]['joinparams']['beforejoin']['table'], 'alternative_email')) {
+                        $subquery_specific_username_anonymous = [
+                            'alternative_email' => ['LIKE', self::makeTextSearchValue($val)],
+                        ];
+                    );
                     break;
                 } else {
                     $criteria = [
