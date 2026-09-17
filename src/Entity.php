dd6be61a8186852c967f3bb869c97ea6cb8433e9
mbressy <bressy.marie@gmail.com>
2026-09-17 14:26:40 +0000

    sanitize empty inquest_mandatory_comment same as default rate

--- src/Entity.php
+++ src/Entity.php
@@ -647,10 +647,14 @@ class Entity extends CommonTreeDropdown implements
                 unset($input[$max_key]);
             }
 
-            // `inquest_default_rate` is an integer column; an empty submitted value would break the SQL update.
+            // `inquest_default_rate` and `inquest_mandatory_comment` are integer columns; an empty submitted value would break the SQL update.
             $default_rate_key = 'inquest_default_rate' . $suffix;
             if (array_key_exists($default_rate_key, $input) && $input[$default_rate_key] === '') {
                 $input[$default_rate_key] = 0;
+            }
+            $mandatory_comment_key = 'inquest_mandatory_comment' . $suffix;
+            if (array_key_exists($mandatory_comment_key, $input) && $input[$mandatory_comment_key] === '') {
+                $input[$mandatory_comment_key] = 0;
             }
         }
 
--- tests/functional/EntityTest.php
+++ tests/functional/EntityTest.php
@@ -199,6 +199,22 @@ class EntityTest extends DbTestCase
         $this->assertSame(0, $result['inquest_default_rate_change']);
     }
 
+    public function testPrepareInputForUpdateSanitizesEmptyMandatoryComment(): void
+    {
+        $this->login();
+
+        $entity = new Entity();
+        $input = [
+            'id' => 1,
+            'inquest_mandatory_comment' => '',
+            'inquest_mandatory_comment_change' => '',
+        ];
+
+        $result = $entity->prepareInputForUpdate($input);
+        $this->assertSame(0, $result['inquest_mandatory_comment']);
+        $this->assertSame(0, $result['inquest_mandatory_comment_change']);
+    }
+
     /**
      * Test that entity creation generates sequential IDs.
      */
